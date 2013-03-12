79f859b116654f634a1825daefea76aea92d71a8
Max Glantzman <max.glantzman@soul-cycle.com>
2013-03-12 15:27:40 -0400

    Some customer filtering functionality...

--- extensions/cashregister/main.js
+++ extensions/cashregister/main.js
@@ -33,6 +33,8 @@ Extension.Router = Directus.SubRoute.extend({
 
     initialize: function() {
 
+      var routerProxy = this;
+
       var collections = {
           quickPicksCollection: new Product.QuickPicksCollection(),
           customerCollection: new User.Collection(),
@@ -45,16 +47,39 @@ Extension.Router = Directus.SubRoute.extend({
 
       this.v.subMain = new Backbone.Layout({
           prefix: 'extensions/cashregister/templates/',
+
           template: 'register-main',
+
+          events: {
+            'keyup #customer-filter': 'update_users_table',
+            'keyup #quickpicks-filter': 'update_products_table'
+          },
+
           views: {
               '.quick_picks_table': new Product.QuickPicksListView({collection: this.quickPicksCollection, activeProductsCollection: this.activeProductsCollection}),
               '.active_products_table': new Product.ActiveProductsListView({collection: this.activeProductsCollection}),
               '.customers_table': new User.ListView({collection: this.customerCollection })
           },
-          beforeRender: function() {
-            //console.log('beforeRender called in this.v.subMain');
-          }
-        });
+
+          update_users_table: _.debounce(function(e) {
+
+            var searchVal = e.currentTarget.value;
+
+            if (searchVal.length > 0) {
+              routerProxy.customerCollection.url = '/directus/api/1/extensions/cashregister/customers/' + searchVal;
+            } else {
+              routerProxy.customerCollection.url = '/directus/api/1/extensions/cashregister/customers';
+            }
+
+            routerProxy.customerCollection.fetch();
+          }, 800),
+
+          update_products_table: _.debounce(function(e) {
+
+            console.log('this doesnt work yet');
+
+          }, 800)
+      });
 
 
       this.v.main = new Backbone.Layout({
@@ -65,12 +90,6 @@ Extension.Router = Directus.SubRoute.extend({
           },
           serialize: function() {
             return {title: 'Cash Register'};
-          },
-          beforeRender: function() {
-            //console.log('beforeRender called in this.v.main');
-          },
-          afterRender: function() {
-           
           }
       });
 
