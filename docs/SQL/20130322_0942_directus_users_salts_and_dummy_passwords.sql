61f0e978f9880e9dcf9b06cad9dffe93e3549bc2
Daniel Bickett <dbickett@gmail.com>
2013-03-22 12:47:30 -0400

    added all usernames and unhashed passwords to SQL file as comment

--- docs/SQL/20130322_0942_directus_users_salts_and_dummy_passwords.sql
+++ docs/SQL/20130322_0942_directus_users_salts_and_dummy_passwords.sql
@@ -12,3 +12,13 @@ UPDATE `directus_users` SET `password` = '43fd7bd5c6a0a9ae3c701d435d44a1dadd0625
 UPDATE `directus_users` SET `password` = '021ac2a92752020fca43c8ebb42398332a75e920', `salt` = '514c5be267e43' WHERE `directus_users`.`id` = 4;
 UPDATE `directus_users` SET `password` = '6a9db215d6449ebd79d9c633efeccbb1cd833b64', `salt` = '514c5be30ae9d' WHERE `directus_users`.`id` = 5;
 UPDATE `directus_users` SET `password` = '90dcb187c22f6bf4f8c3d422dad170ff5870c363', `salt` = '514c5bdf61149' WHERE `directus_users`.`id` = 1;
+
+-- Δ
+-- email / pass
+-- - - - - - - -
+-- abby.kohn@soul-cycle.com / passwordabby
+-- ben@rngr.org / passwordben
+-- dwandrey@ideo.com / passworddann
+-- olov@rngr.org / passwordolov
+-- lasha@rngr.org / passwordlasha
+-- max.glantzman@soul-cycle.com / passwordmax
