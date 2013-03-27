5d8b08eaf85c5c36e02123290430baec840ed81d
Max Glantzman <max.glantzman@soul-cycle.com>
2013-03-27 15:59:32 -0400

    Payment types SQL file added and other register changes

--- extensions/cashregister/main.js
+++ extensions/cashregister/main.js
@@ -1,15 +1,15 @@
 define([
-    'app',
-    'backbone',
-    'underscore',
-    'core/directus',
+        'app',
+        'backbone',
+        'underscore',
+        'core/directus',
 
     '../../extensions/cashregister/accounting.min',
 
     // modules
     '../../extensions/cashregister/product',
-    '../../extensions/cashregister/user',
-    '../../extensions/cashregister/transaction'
+        '../../extensions/cashregister/user',
+        '../../extensions/cashregister/transaction'
 
 ],
 
@@ -45,30 +45,36 @@ function (app, Backbone, _, Directus, Accounting, Product, User, Transaction) {
             CashRegister.Views = {};
 
             CashRegister.Models.PaymentType = Backbone.Model.extend({
-                initialize: function() {
+                initialize: function () {
                     this.listenTo(this.collection.transaction, {
-                        'change:selected_payment_type':this.check_sel_or_not
+                        'change:selected_payment_type': this.check_sel_or_not
                     });
                 },
-                check_sel_or_not: function() {
-                    this.set({sel: this.collection.transaction.get('selected_payment_type') == this});
+                check_sel_or_not: function () {
+                    this.set({
+                        sel: this.collection.transaction.get('selected_payment_type') == this
+                    });
                 }
             });
 
             CashRegister.Collections.PaymentTypes = Backbone.Collection.extend({
                 url: '/directus/api/1/extensions/cashregister/payment_types',
                 model: CashRegister.Models.PaymentType,
-                initialize: function(coll, options) {
+                initialize: function (coll, options) {
                     this.transaction = options.transaction;
                 }
             });
 
-            this.paymentTypes = new CashRegister.Collections.PaymentTypes([], {transaction: this.transaction});
+            this.paymentTypes = new CashRegister.Collections.PaymentTypes([], {
+                transaction: this.transaction
+            });
 
             var collections = {
                 quickPicksCollection: new Product.QuickPicksCollection(),
-                customerCollection: new User.Collection({transaction: this.transaction}),
-                activeProductsCollection: this.transaction.activeProductsCollection,
+                customerCollection: new User.Collection({
+                    transaction: this.transaction
+                }),
+               // activeProductsCollection: this.transaction.get('activeProductsCollection'),
                 paymentTypes: this.paymentTypes
             };
 
@@ -81,19 +87,23 @@ function (app, Backbone, _, Directus, Accounting, Product, User, Transaction) {
 
                 template: 'register-main',
 
-                initialize: function(options) {
+                initialize: function (options) {
                     this.transaction = options.transaction;
+                    console.log("submain init", this, options);
                     this.paymentTypes = options.paymentTypesCollection;
                     this.listenTo(options.transaction, {
-                        'change:selectedRider':this.show_rider_detail,
-                        'change:userSearchSetting':this.set_user_search_view
+                        'change:selectedRider': this.show_rider_detail,
+                        'change:userSearchSetting': this.set_user_search_view
                     });
+                },
+
+                events: {
+                    'click #submit_transaction_button':'submit_order'
                 },
 
                 views: {
                     '.omnibox': new Transaction.Views.OmniBox({
                         customerCollection: this.customerCollection,
-                        activeProductsCollection: this.activeProductsCollection,
                         productCollection: this.quickPicksCollection,
                         transaction: this.transaction,
                         paymentTypes: this.paymentTypes
@@ -101,7 +111,7 @@ function (app, Backbone, _, Directus, Accounting, Product, User, Transaction) {
 
                     '.quick_picks_table': new Product.Views.QuickPicksList({
                         collection: this.quickPicksCollection,
-                        activeProductsCollection: this.activeProductsCollection
+                        transaction: this.transaction
                     }),
                     '.active_products_table': new Transaction.Views.ActiveProductsList({
                         model: this.transaction
@@ -112,41 +122,47 @@ function (app, Backbone, _, Directus, Accounting, Product, User, Transaction) {
                     })
                 },
 
-                show_rider_detail: function() {
-                    this.setView('.customers_table', new User.Views.Selected({model: this.transaction.get('selectedRider'), transaction: this.transaction, paymentTypes: this.paymentTypes }));
+                submit_order: function() {
+                    //this.transaction.save();
+                    console.log("ready to submit order...", this.transaction);
+                },
+
+                show_rider_detail: function () {
+                    this.setView('.customers_table', new User.Views.Selected({
+                        model: this.transaction.get('selectedRider'),
+                        transaction: this.transaction,
+                        paymentTypes: this.paymentTypes
+                    }));
                     this.render();
                 },
 
-                set_user_search_view: function() {
-                  var searchSetting = this.transaction.get('userSearchSetting');
-                  this.setView('.customers_table', new User.Views.List({
-                    collection: this.options.customerCollection,
-                    transaction: this.transaction
-                  }));
-                  switch (searchSetting) {
-                    case '':
-                      this.options.customerCollection.url = '/directus/api/1/extensions/cashregister/customers';
-                      this.options.customerCollection.fetch();
-                    break;
-                    case 'class':
-                      this.options.customerCollection.url = '/directus/api/1/extensions/cashregister/customers/class';
-                      this.options.customerCollection.fetch();
-                    break;
-                  }
+                set_user_search_view: function () {
+                    var searchSetting = this.transaction.get('userSearchSetting');
+                    this.setView('.customers_table', new User.Views.List({
+                        collection: this.options.customerCollection,
+                        transaction: this.transaction
+                    }));
+                    switch (searchSetting) {
+                        case '':
+                            this.options.customerCollection.url = '/directus/api/1/extensions/cashregister/customers';
+                            this.options.customerCollection.fetch();
+                            break;
+                        case 'class':
+                            this.options.customerCollection.url = '/directus/api/1/extensions/cashregister/customers/class';
+                            this.options.customerCollection.fetch();
+                            break;
+                    }
 
                 }
 
             });
 
-            this.v.subMain = new SubMain(
-              {
-                transaction: this.transaction, 
+            this.v.subMain = new SubMain({
+                transaction: this.transaction,
                 quickPicksCollection: this.quickPicksCollection,
-                activeProductsCollection: this.activeProductsCollection,
                 customerCollection: this.customerCollection,
                 paymentTypesCollection: this.paymentTypes
-              }
-            );
+            });
 
             this.v.main = new Backbone.Layout({
                 template: 'page',
@@ -168,39 +184,6 @@ function (app, Backbone, _, Directus, Accounting, Product, User, Transaction) {
         }
 
     });
-
-      var BarcodeScannerEvents = function() {
-           this.initialize.apply(this, arguments);
-      };
-
-      BarcodeScannerEvents.prototype = {
-        initialize: function(obj) {
-          console.log("obj",obj);
-           $(document).on({
-              keyup: $.proxy(this._keyup, this)
-           });
-        },
-        _timeoutHandler: 0,
-        _inputString: '',
-        _keyup: function (e) {
-        if (this._timeoutHandler) {
-            clearTimeout(this._timeoutHandler);
-            this._inputString += String.fromCharCode(e.which);
-        } 
-
-        this._timeoutHandler = setTimeout($.proxy(function () {
-            if (this._inputString.length <= 3) {
-                this._inputString = '';
-                return;
-            }
-
-            console.log('YES');
-            $(document).trigger('onbarcodescaned', this._inputString);
-            this._inputString = '';
-
-        }, this), 20);
-        }
-      };
 
     return Extension;
 });
