a94dbb2edcc7c3adce33905df73502f1680b1be3
Daniel Bickett <dbickett@gmail.com>
2013-03-29 16:10:03 -0400

    mid-swing re-factoring db#get_one_many and db#get_many_many

--- api/core/Directus/Db/TableGateway.php
+++ api/core/Directus/Db/TableGateway.php
@@ -3,6 +3,7 @@
 namespace Directus\Db;
 
 use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Expression;
 
 class TableGateway extends \Zend\Db\TableGateway\TableGateway {
 
@@ -40,27 +41,23 @@ class TableGateway extends \Zend\Db\TableGateway\TableGateway {
         $alias_fields = $this->filterSchemaAliasFields($table_schema); // (fmrly $alias_schema)
 
         $sql = new Sql($this->adapter);
-        $select = $sql->select();
+        $select = $sql->select()->from($this->table);
         // Where
         $select->where
             ->expression('-1 = ?', $params['id'])
             ->or
             ->equalTo('id', $params['id']);
         // And so on
-        $select->from($this->table)
-            ->group('id')
+        $select->group('id')
             ->order(implode(' ', array($params['order_column'], $params['order_direction'])))
             ->limit($params['per_page'])
             ->offset($params['skip']);
-
 
         // Table has `active` column?
         $has_active_column = $this->schemaHasActiveColumn($table_schema);
 
         if ($params['active'] && $has_active_column)
             $select->where->in('active', $params['active']);
-
-        // var_dump(@$sql->getSqlStringForSqlObject($select)); exit;
 
         // @todo figure out this warning
         $statement = @$sql->prepareStatementForSqlObject($select);
@@ -77,19 +74,176 @@ class TableGateway extends \Zend\Db\TableGateway\TableGateway {
             foreach ($table_schema as $col) {
                 // Run custom data casting.
                 $name = $col['column_name'];
-                $item[$name] = $this->parse_mysql_type($row[$name], $col['type']);
+                $item[$name] = $this->parseMysqlType($row[$name], $col['type']);
             }
             $table_entries[] = $item;
         }
 
-        // var_dump($table_entries);
-        // exit;
-
+        // Eager-load related ManyToOne records
         $table_entries = $this->loadManyToOneData($table_schema, $table_entries);
+
+        /**
+         * Fetching a set of data
+         */
+
+        if (-1 == $params['id']) {
+            $countActive = $this->count_active($this->table, !$has_active_column);
+            $set = array_merge($countActive, array(
+                'total'=> $foundRows,
+                'rows'=> $table_entries
+            ));
+            return $set;
+        }
+
+        /**
+         * Fetching one item
+         */
+
+        if (0 == count($table_entries))
+            throw new DirectusException('Item not found!', 404);
+
+        list($table_entry) = $table_entries;
+
+        foreach ($alias_fields as $alias) {
+            switch($alias['type']) {
+                case 'MANYTOMANY':
+                    $foreign_data = $this->get_many_many($this->table, $alias['table_related'],
+                        $alias['junction_table'], $alias['junction_key_left'], $alias['junction_key_right'],
+                        $params['id']);
+                    break;
+                case 'ONETOMANY':
+                    $foreign_data = $this->loadOneToManyData($alias['table_related'], $alias['junction_key_right'], $params['id']);
+                    break;
+            }
+            if(isset($foreign_data)) {
+                $column = $alias['column_name'];
+                $table_entry[$column] = $foreign_data;
+            }
+        }
 
         \Directus\View\JsonView::render($table_entries);
         exit;
+
+        return $table_entry;
     }
+
+    /**
+     *
+     * ASSOCIATION FUNCTIONS
+     *
+     **/
+
+    /**
+     * Fetch related rows
+     * This is used for fetching the related id's in one-many relationships and many-many relationships
+     *
+     * @param string $table
+     * @param string $column_name
+     * @param string $column_equals
+     */
+    private function loadOneToManyData($table, $column_name, $column_equals) {
+        // Run query
+        $sql = new Sql($this->adapter);
+        $select = $sql->select()->from($table);
+        $select->where->equalTo($column_name, $column_equals);
+        $statement = @$sql->prepareStatementForSqlObject($select); // @todo figure out this warning
+        $results = $statement->execute();
+        // Process results
+        foreach ($results as $row)
+            array_walk($row, array($this, 'parseMysqlType'));
+        return array('rows' => $results);
+    }
+
+    /**
+     * Given a table's schema and rows, iterate and replace all of its foreign
+     * keys with the contents of these foreign rows.
+     * @param  array $schema  Table schema array
+     * @param  array $entries Table rows
+     * @return array          Revised table rows, now including foreign rows
+     */
+    private function loadManyToOneData($table_schema, $table_entries) {
+        // Identify the ManyToOne columns
+        foreach ($table_schema as $col) {
+            $isManyToOneColumn = in_array($col['ui'], $this->many_to_one_uis);
+            if ($isManyToOneColumn) {
+                $foreign_id_column = $col['id'];
+                $foreign_table_name = ($col['ui'] == 'single_media') ? 'directus_media' : $col['options']['related_table'];
+
+                // @todo could really use a comment
+                $ids = array_map(function($row) use ($foreign_id_column) { return $row[$foreign_id_column]; }, $table_entries);
+                if (empty($ids))
+                    continue;
+
+                // Fetch the foreign data
+                $sql = new Sql($this->adapter);
+                $select = $sql->select()->from($foreign_table_name);
+                $select->where->in('id', $ids);
+                $statement = @$sql->prepareStatementForSqlObject($select); // @todo figure out this warning
+                $results = $statement->execute();
+
+                $foreign_table = array();
+                foreach ($results as $row) {
+                    // @todo I wonder if all of this looping and casting is necessary
+                    foreach ($row as $column => $value)
+                        $row[$column] = $this->parseMysqlType($value);
+                    $foreign_table[$row['id']] = $row;
+                }
+
+                // Replace foreign keys with foreight rows
+                foreach ($table_entries as &$parentRow) {
+                    $foreign_id = $parentRow[$foreign_id_column];
+                    $parentRow[$foreign_id_column] = null;
+                    // "Did we retrieve the foreign row with this foreign ID in our recent query of the foreign table"?
+                    if(array_key_exists($foreign_id, $foreign_table))
+                        $parentRow[$foreign_id_column] = $foreign_table[$foreign_id];
+                }
+            }
+        }
+        return $table_entries;
+    }
+
+     /**
+     * Get related rows
+     * This is used for fetching the related id's in one-many relationships and many-many relationships
+     * This makes _junction_id reserverd
+     */
+    /**
+     * Fetch foreign rows for an entry's ManyToMany relationships.
+     * @param  string $table_name
+     * @param  string $foreign_table
+     * @param  string $junction_table
+     * @param  string $junction_key_left
+     * @param  string $junction_key_right
+     * @param  string $column_equals
+     * @return array                      Foreign rowset
+     */
+    function get_many_many($table_name, $foreign_table, $junction_table, $junction_key_left, $junction_key_right, $column_equals) {
+        $data_set = array();
+        $sql = "SELECT JT.id, FT.* FROM $junction_table JT
+            LEFT JOIN $foreign_table FT
+            ON (JT.$junction_key_right = FT.id)
+            WHERE JT.$junction_key_left = $column_equals";
+        $sth = $this->dbh->prepare($sql);
+        $sth->execute();
+        while($row = $sth->fetch(PDO::FETCH_NUM)){
+            $junction_id = $row[0];
+            $data = array();
+            foreach ($row as $i => $value) {
+                $columnMeta = $sth->getColumnMeta($i);
+                $data[$columnMeta['name']] = is_numeric($value) ? (float)$value : $value;
+            }
+            array_push($data_set, array('id'=>$junction_id, 'data' => $data));
+        }
+        return array('rows' => $data_set);
+
+
+    }
+
+    /**
+     *
+     * HELPER FUNCTIONS
+     *
+     **/
 
     /**
      * Remove Directus-managed virtual/alias fields from the table schema array
@@ -124,79 +278,71 @@ class TableGateway extends \Zend\Db\TableGateway\TableGateway {
     }
 
     /**
-     * Given a table's schema and rows, iterate and replace all of its foreign
-     * keys with the contents of these foreign rows.
-     * @param  array $schema  Table schema array
-     * @param  array $entries Table rows
-     * @return array          Revised table rows, now including foreign rows
+     * Cast a php string to the same type as MySQL
+     * @param  string $mysql_data MySQL result data
+     * @param  string $mysql_type MySQL field type
+     * @return mixed              Value cast to PHP type
      */
-    private function loadManyToOneData($table_schema, $table_entries) {
-        $sql = new Sql($this->adapter);
-        // Identify the ManyToOne columns
-        foreach ($table_schema as $col) {
-        	$isManyToOneColumn = in_array($col['ui'], $this->many_to_one_uis);
-            if ($isManyToOneColumn) {
-                $foreign_id_column = $col['id'];
-                $foreign_table_name = ($col['ui'] == 'single_media') ? 'directus_media' : $col['options']['related_table'];
-
-                // @todo could really use a comment
-                $ids = array_map(function($row) use ($foreign_id_column) { return $row[$foreign_id_column]; }, $table_entries);
-                if (empty($ids))
-                    continue;
-
-                // Fetch the foreign data
-                $select = $sql->select()->from($foreign_table_name);
-                $select->where->in('id', $ids);
-                $statement = @$sql->prepareStatementForSqlObject($select); // @todo figure out this warning
-                $results = $statement->execute();
-
-                $foreign_table = array();
-                foreach ($results as $row) {
-                    // @todo I wonder if all of this looping and casting is necessary
-                    foreach ($row as $column => $value)
-                        $row[$column] = $this->parse_mysql_type($value);
-                    $foreign_table[$row['id']] = $row;
-                }
-
-                // Replace foreign keys with foreight rows
-                foreach ($table_entries as &$parentRow) {
-                    $foreign_id = $parentRow[$foreign_id_column];
-                    $parentRow[$foreign_id_column] = null;
-                    // "Did we retrieve the foreign row with this foreign ID in our recent query of the foreign table"?
-                    if(array_key_exists($foreign_id, $foreign_table))
-                        $parentRow[$foreign_id_column] = $foreign_table[$foreign_id];
-                }
-            }
-        }
-        return $table_entries;
-    }
-
-    /**
-     * Cast a php string to the same type as MySQL.
-     */
-    function parse_mysql_type($string, $type = NULL) {
-        $type = strtolower($type);
-        switch ($type) {
+    private function parseMysqlType($mysql_data, $mysql_type = null) {
+        $mysql_type = strtolower($mysql_type);
+        switch ($mysql_type) {
+            case null:
+                break;
             case 'blob':
             case 'mediumblob':
-                return base64_encode($string);
+                return base64_encode($mysql_data);
             case 'year':
             case 'int':
             case 'long':
             case 'tinyint':
-                return (int) $string;
+                return (int) $mysql_data;
             case 'float':
-                return (float) $string;
+                return (float) $mysql_data;
             case 'date':
             case 'datetime':
-                return date("r", strtotime($string));
-            case 'VAR_STRING':
-                return $string;
+                return date("r", strtotime($mysql_data));
+            case 'var_string':
+                return $mysql_data;
         }
-        // If type is not present & value is numeric, just cast numbers.
-        if (is_numeric($string))
-            return (float) $string;
-        return $string;
+        // If type is null & value is numeric, cast to integer.
+        if (is_numeric($mysql_data))
+            return (float) $mysql_data;
+        return $mysql_data;
+    }
+
+    /**
+     * @refactor
+     */
+    function count_active($tbl_name, $no_active=false) {
+        $result = array('active'=>0);
+        if ($no_active) {
+            $sql = "SELECT COUNT(*) as count, 'active' as active FROM $tbl_name";
+        } else {
+            $sql = "SELECT
+                CASE active
+                    WHEN 0 THEN 'trash'
+                    WHEN 1 THEN 'active'
+                    WHEN 2 THEN 'inactive'
+                END AS active,
+                COUNT(*) as count
+            FROM $tbl_name
+            GROUP BY active";
+        }
+        $sth = $this->dbh->prepare($sql);
+        // Test if there is an active column!
+        try {
+            $sth->execute();
+        } catch(Exception $e) {
+            if ($e->getCode() == "42S22" && strpos(strtolower($e->getMessage()),"unknown column")) {
+                return $this->count_active($tbl_name, true);
+            } else {
+                throw $e;
+            }
+        }
+        while($row = $sth->fetch(PDO::FETCH_ASSOC))
+            $result[$row['active']] = (int)$row['count'];
+        $total = 0;
+        return $result;
     }
 
 }
