da9f2b554dc2dda63ad38ea1799b443827600030
Daniel Bickett <dbickett@gmail.com>
2013-04-01 15:25:30 -0400

    api db refactor, revisions route, Db\Activity#fetchRevisions

--- api/core/Directus/Db/Activity.php
+++ api/core/Directus/Db/Activity.php
@@ -10,8 +10,8 @@ class Activity extends TableGateway {
         $columns = array('id','identifier','action','table_name','row_id','user','datetime','type');
 
         $sql = new Sql($this->adapter);
-        $select = $sql->select();
-        $select->from($this->table)
+        $select = $sql->select()
+            ->from($this->table)
             ->columns($columns)
             ->order('id DESC');
         $select
@@ -29,5 +29,38 @@ class Activity extends TableGateway {
 
         return array('rows' => $result);
     }
+
+    public function fetchRevisions($row_id, $table_name) {
+        $columns = array('id','action','user','datetime');
+
+        $sql = new Sql($this->adapter);
+        $select = $sql->select()
+            ->from($this->table)
+            ->columns($columns)
+            ->order('id DESC');
+        $select
+            ->where
+                ->equalTo('row_id', $row_id)
+                ->AND
+                ->equalTo('table_name', $table_name);
+
+        // @todo why can't we just fetchAll or s'thing?
+        $statement = @$sql->prepareStatementForSqlObject($select); // @todo figure out this warning
+        $rowset = $statement->execute();
+        $result = array();
+        while($row = $rowset->current())
+            $result[] = $row;
+
+        return $result;
+    }
+
+    // function get_revisions($params) {
+    //     $row_id = $params['id'];
+    //     $table_name = $params['table_name'];
+    //     $sql = "SELECT id,action,user,datetime FROM directus_activity WHERE row_id=$row_id AND table_name='$table_name' ORDER BY id DESC";
+    //     $sth = $this->dbh->prepare($sql);
+    //     $sth->execute();
+    //     return $sth->fetchAll(PDO::FETCH_ASSOC);
+    // }
 
 }
