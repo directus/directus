c39d395b19d4d20cb167de8fbfdc8be39588feac
Daniel Bickett <dbickett@gmail.com>
2013-04-01 17:18:10 -0400

    housekeeping: removed old mock DB accessors & unused files; added Db\TableGateway#findOneBy

--- api/core/Directus/Db/Users.php
+++ api/core/Directus/Db/Users.php
@@ -7,6 +7,8 @@ use Zend\Db\Sql\Expression;
 
 class Users extends TableGateway {
 
+    const GRAVATAR_SIZE = 100;
+
     public function fetchAllWithGroupData() {
         $sql = new Sql($this->adapter);
         $select = $sql->select();
@@ -29,6 +31,31 @@ class Users extends TableGateway {
             array_push($results, $row);
         }
         return array('rows' => $results);
+    }
+
+    /**
+     * Get either a Gravatar URL or complete image tag for a specified email address.
+     *
+     * @param string $email The email address
+     * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
+     * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+     * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+     * @param boole $img True to return a complete IMG tag False for just the URL
+     * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+     * @return String containing either just a URL or a complete image tag
+     * @source http://gravatar.com/site/implement/images/php/
+     */
+    public static function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+        $url = 'http://www.gravatar.com/avatar/';
+        $url .= md5( strtolower( trim( $email ) ) );
+        $url .= "?s=$s&d=$d&r=$r";
+        if ( $img ) {
+            $url = '<img src="' . $url . '"';
+            foreach ( $atts as $key => $val )
+                $url .= ' ' . $key . '="' . $val . '"';
+            $url .= ' />';
+        }
+        return $url;
     }
 
 }
