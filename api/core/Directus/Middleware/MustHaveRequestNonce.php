56046a110f1894a4619aaa8fb56bdf5c605a68e2
Daniel Bickett <dbickett@gmail.com>
2013-04-05 16:54:30 -0400

    whitespace cleanup, (" " * 4) > "\t"

--- api/core/Directus/Middleware/MustHaveRequestNonce.php
+++ api/core/Directus/Middleware/MustHaveRequestNonce.php
@@ -9,44 +9,43 @@ use Directus\Auth\RequestNonceProvider;
  */
 class MustHaveRequestNonce extends \Directus\Middleware {
 
-	/**
-	 * @var \Directus\Auth\RequestNonceProvider
-	 */
-	private $requestNonceProvider;
+    /**
+     * @var \Directus\Auth\RequestNonceProvider
+     */
+    private $requestNonceProvider;
 
-	/**
-	 * @param array                $routeWhitelist
-	 * @param RequestNonceProvider $requestNonceProvider
-	 */
-	public function __construct($routeWhitelist = array(), RequestNonceProvider $requestNonceProvider) {
-		parent::__construct($routeWhitelist);
-		$this->requestNonceProvider = $requestNonceProvider;
-	}
+    /**
+     * @param array                $routeWhitelist
+     * @param RequestNonceProvider $requestNonceProvider
+     */
+    public function __construct($routeWhitelist = array(), RequestNonceProvider $requestNonceProvider) {
+        parent::__construct($routeWhitelist);
+        $this->requestNonceProvider = $requestNonceProvider;
+    }
 
-	public function call() {
+    public function call() {
+        $outcome = parent::call();
 
-		$outcome = parent::call();
+        // Did \Directus\Middleware detect that the route matches the whitelist?
+        // If so, skip enforcement.
+        // NOTE: $this->next->call() has already run!
+        if(self::MATCHES_ROUTE_WHITELIST == $outcome)
+            return;
 
-		// Did \Directus\Middleware detect that the route matches the whitelist?
-		// If so, skip enforcement.
-		// NOTE: $this->next->call() has already run!
-		if(self::MATCHES_ROUTE_WHITELIST == $outcome)
-			return;
+        if(!$this->requestNonceProvider->requestHasValidNonce()) {
+            if('development' !== DIRECTUS_ENV) {
+                $this->app->halt(401, "Invalid request (nonce).");
+            }
+        }
 
-		if(!$this->requestNonceProvider->requestHasValidNonce()) {
-			if('development' !== DIRECTUS_ENV) {
-				$this->app->halt(401, "Invalid request (nonce).");
-			}
-		}
-
-		$response = $this->app->response();
+        $response = $this->app->response();
         $newNonces = $this->requestNonceProvider->getNewNoncesThisRequest();
 
         $nonce_options = $this->requestNonceProvider->getOptions();
 
-	    $response[$nonce_options['nonce_response_header']] = implode($newNonces, ",");
+        $response[$nonce_options['nonce_response_header']] = implode($newNonces, ",");
 
-		/** All good, proceed. */
-		$this->next->call();
-	}
+        /** All good, proceed. */
+        $this->next->call();
+    }
 }
