d566b24798136973d29d2437ec4adceeba4244c6
Daniel Bickett <dbickett@gmail.com>
2013-04-05 12:13:15 -0400

    Acl logic moved to Acl provider; Acl provider injection; new directus_privileges table structure

---
+++ api/core/Directus/Db/Privileges.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Directus\Db;
+
+use Directus\Acl;
+use Directus\Db\TableGateway\AclAwareTableGateway;
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Select;
+
+class Privileges extends AclAwareTableGateway {
+
+    public static $_tableName = "directus_privileges";
+
+    public function __construct(Acl $aclProvider, AdapterInterface $adapter) {
+        parent::__construct($aclProvider, self::$_tableName, $adapter);
+    }
+
+    public function fetchGroupPrivileges($group_id) {
+        $select = new Select($this->table);
+        $select->where->equalTo('group_id', $group_id);
+        $rowset = $this->selectWith($select);
+        $rowset = $rowset->toArray();
+        foreach($rowset as $field => &$value) {
+            if($this->aclProvider->isTableListValue($field))
+                $value = explode(",", $value);
+        }
+        return $rowset;
+    }
+
+}
