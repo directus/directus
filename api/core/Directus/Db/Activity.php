a21e44de967023c21818d5573bb54d52328f8a81
Daniel Bickett <dbickett@gmail.com>
2013-04-10 15:35:24 -0400

    record put refactored; foreign association updates need more attention
    milestone / semi-broken state

--- api/core/Directus/Db/Activity.php
+++ api/core/Directus/Db/Activity.php
@@ -10,11 +10,26 @@ use Zend\Db\Sql\Select;
 
 class Activity extends AclAwareTableGateway {
 
+    // Populates directus_activity.type
+    const TYPE_ENTRY    = "ENTRY";
+    const TYPE_MEDIA    = "MEDIA";
+    const TYPE_SETTINGS = "SETTINGS";
+    const TYPE_UI       = "UI";
+
+    // Populates directus_activity.action
+    const ACTION_ADD    = "ADD";
+    const ACTION_UPDATE = "UPDATE";
+
     public static $_tableName = "directus_activity";
 
     public function __construct(Acl $aclProvider, AdapterInterface $adapter) {
         parent::__construct($aclProvider, self::$_tableName, $adapter);
     }
+
+        // $action = isset($data['id']) ? 'UPDATE' : 'ADD';
+        // $master_item = find($schema,'master',true);
+        // $identifier = isset($master_item) ? $data[$master_item['column_name']] : null;
+        // $activity_id = $this->log_activity('ENTRY',$tbl_name, $action, $id, $identifier, $data, $parent_activity_id);
 
     public function fetchFeed() {
         $columns = array('id','identifier','action','table_name','row_id','user','datetime','type');
