469ee7e5cabdaafddacae64046ae45d9e5f93ec7
Daniel Bickett <dbickett@gmail.com>
2013-04-11 17:56:09 -0400

    AclAwareRowGateway#makeRowGatewayFromTableName; custom directus_media row gateway

---
+++ api/core/Directus/Db/RowGateway/DirectusMediaRowGateway.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Directus\Db\RowGateway;
+
+use Zend\Db\RowGateway\RowGateway;
+
+use Directus\Bootstrap;
+
+class DirectusMediaRowGateway extends AclAwareRowGateway {
+
+    /**
+     * Populate Data
+     *
+     * @param  array $rowData
+     * @param  bool  $rowExistsInDatabase
+     * @return AbstractRowGateway
+     */
+    public function populate(array $rowData, $rowExistsInDatabase = false)
+    {
+        // New record?
+        // Attribute the currently authenticated user as the uploader
+        if(!$rowExistsInDatabase) {
+            $currentUser = AuthProvider::getUserInfo();
+            $rowData['user'] = $currentUser['id'];
+        }
+
+        return parent::populate($rowData, $rowExistsInDatabase);
+    }
+
+}
