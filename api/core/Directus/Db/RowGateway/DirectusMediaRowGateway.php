d62e3cbe36b158c7706bc0aa01583d7462ad0561
Daniel Bickett <dbickett@gmail.com>
2013-04-12 17:44:12 -0400

    moved #enforceBlacklist to Acl class; prototype for edit enforcement on AclAwareTableGateway#executeUpdate; regression putting user FK on new foreign item in many-to-many relationship

--- api/core/Directus/Db/RowGateway/DirectusMediaRowGateway.php
+++ api/core/Directus/Db/RowGateway/DirectusMediaRowGateway.php
@@ -18,13 +18,19 @@ class DirectusMediaRowGateway extends AclAwareRowGateway {
      */
     public function populate(array $rowData, $rowExistsInDatabase = false)
     {
+        $this->logger()->info(__CLASS__."#".__FUNCTION__);
+        $this->logger()->info("pre-process");
+        $this->logger()->info(print_r($rowData, true));
         // New record?
         // Attribute the currently authenticated user as the uploader
         if(!$rowExistsInDatabase) {
+            $this->logger()->info("apparently row doesn't exist in database");
             $currentUser = AuthProvider::getUserInfo();
             $rowData['user'] = $currentUser['id'];
         }
-
+        else $this->logger()->info("\"rowExistsInDatabase\"");
+        $this->logger()->info("post-process");
+        $this->logger()->info(print_r($rowData, true));
         return parent::populate($rowData, $rowExistsInDatabase);
     }
 
