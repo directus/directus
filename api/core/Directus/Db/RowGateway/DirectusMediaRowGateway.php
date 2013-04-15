55c9aa8f690cdcada36ae9d451c54287718c786f
Daniel Bickett <dbickett@gmail.com>
2013-04-15 12:08:48 -0400

    fixed pre-insert/update RowGateway populate hook

--- api/core/Directus/Db/RowGateway/DirectusMediaRowGateway.php
+++ api/core/Directus/Db/RowGateway/DirectusMediaRowGateway.php
@@ -9,29 +9,15 @@ use Directus\Bootstrap;
 
 class DirectusMediaRowGateway extends AclAwareRowGateway {
 
-    /**
-     * Populate Data
-     *
-     * @param  array $rowData
-     * @param  bool  $rowExistsInDatabase
-     * @return AbstractRowGateway
-     */
-    public function populate(array $rowData, $rowExistsInDatabase = false)
+    public function preSaveDataHook(array $rowData, $rowExistsInDatabase = false)
     {
-        $this->logger()->info(__CLASS__."#".__FUNCTION__);
-        $this->logger()->info("pre-process");
-        $this->logger()->info(print_r($rowData, true));
         // New record?
         // Attribute the currently authenticated user as the uploader
         if(!$rowExistsInDatabase) {
-            $this->logger()->info("apparently row doesn't exist in database");
             $currentUser = AuthProvider::getUserInfo();
             $rowData['user'] = $currentUser['id'];
         }
-        else $this->logger()->info("\"rowExistsInDatabase\"");
-        $this->logger()->info("post-process");
-        $this->logger()->info(print_r($rowData, true));
-        return parent::populate($rowData, $rowExistsInDatabase);
+        return $rowData;
     }
 
 }
