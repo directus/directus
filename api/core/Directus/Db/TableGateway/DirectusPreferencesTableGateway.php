be5085999d14efcecf1f747d100059be07721d1c
Daniel Bickett <dbickett@gmail.com>
2013-04-25 14:02:22 -0400

    one more for #151, enforce non-empty sort, sort_order, and active values

--- api/core/Directus/Db/TableGateway/DirectusPreferencesTableGateway.php
+++ api/core/Directus/Db/TableGateway/DirectusPreferencesTableGateway.php
@@ -18,6 +18,12 @@ class DirectusPreferencesTableGateway extends AclAwareTableGateway {
         parent::__construct($aclProvider, self::$_tableName, $adapter);
     }
 
+    public static $defaultPreferencesValues = array(
+        "sort"          => "id",
+        "sort_order"    => "ASC",
+        "active"        => "1,2"
+    );
+
     public function fetchByUserAndTable($user_id, $table) {
         $sql = new Sql($this->adapter);
         $select = $sql->select()
@@ -36,9 +42,18 @@ class DirectusPreferencesTableGateway extends AclAwareTableGateway {
 
         if(1 === count($rowset)) {
             $preferences = current($rowset);
+            $newPreferencesData = false;
             if(empty($preferences['columns_visible'])) {
                 $columns_visible = TableSchema::getTableColumns($table, 6);
                 $preferences['columns_visible'] = implode(',', $columns_visible);
+            }
+            foreach(self::$defaultPreferencesValues as $field => $defaultValue) {
+                if(!isset($preferences[$field]) || empty($preferences[$field])) {
+                    $newPreferencesData = true;
+                    $preferences[$field] = $defaultValue;
+                }
+            }
+            if($newPreferencesData) {
                 // Insert to DB
                 $id = $db->set_entry(self::$_tableName, $preferences);
             }
@@ -46,17 +61,13 @@ class DirectusPreferencesTableGateway extends AclAwareTableGateway {
         }
 
         // User doesn't have any preferences for this table yet. Please create!
-
         $columns_visible = TableSchema::getTableColumns($table, 6);
-
         $data = array(
             'user' => $user_id,
             'columns_visible' => implode(',', $columns_visible),
-            'table_name' => $tbl_name,
-            'sort' => 'id',
-            'sort_order' => 'asc',
-            'active' => '1,2'
+            'table_name' => $tbl_name
         );
+        $data = array_merge($data, self::$defaultPreferencesValues);
         // Insert to DB
         $id = $db->set_entry(self::$_tableName, $data);
         return $data;
