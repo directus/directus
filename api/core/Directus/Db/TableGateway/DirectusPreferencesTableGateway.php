7b5acd518b260d3bffebad693d78ccf2e7679e18
Daniel Bickett <dbickett@gmail.com>
2013-05-02 16:19:00 -0400

    directus_media default sort and sort order if user lacks table preferences

--- api/core/Directus/Db/TableGateway/DirectusPreferencesTableGateway.php
+++ api/core/Directus/Db/TableGateway/DirectusPreferencesTableGateway.php
@@ -24,6 +24,31 @@ class DirectusPreferencesTableGateway extends AclAwareTableGateway {
         "active"        => "1,2"
     );
 
+    public static $defaultPreferencesValuesByTable = array(
+        "directus_media" => array(
+            "sort"              => "date_uploaded",
+            "sort_order"        => "DESC",
+            "columns_visible"   => "name,title,type,caption,size,user,date_uploaded"
+        )
+    );
+
+    public function applyDefaultPreferences($table, $preferences) {
+        // Table-specific default values
+        if(array_key_exists($table, self::$defaultPreferencesValuesByTable)) {
+            $tableDefaultPreferences = self::$defaultPreferencesValuesByTable[$table];
+            foreach($tableDefaultPreferences as $field => $defaultValue) {
+                $preferences[$field] = $defaultValue;
+            }
+        }
+        // Global default values
+        foreach(self::$defaultPreferencesValues as $field => $defaultValue) {
+            if(!isset($preferences[$field]) && ("0" !== $preferences[$field] && empty($preferences[$field]))) {
+                $preferences[$field] = $defaultValue;
+            }
+        }
+        return $preferences;
+    }
+
     public function fetchByUserAndTable($user_id, $table) {
         $sql = new Sql($this->adapter);
         $select = $sql->select()
@@ -43,16 +68,21 @@ class DirectusPreferencesTableGateway extends AclAwareTableGateway {
         if(1 === count($rowset)) {
             $preferences = current($rowset);
             $newPreferencesData = false;
+
+            // @todo enforce that the primary key is one of these
+            // @todo & enforce non-empty set (Same thing!)
             if(empty($preferences['columns_visible'])) {
+                $newPreferencesData = true;
                 $columns_visible = TableSchema::getTableColumns($table, 6);
                 $preferences['columns_visible'] = implode(',', $columns_visible);
             }
-            foreach(self::$defaultPreferencesValues as $field => $defaultValue) {
-                if(!isset($preferences[$field]) && ("0" !== $preferences[$field] && empty($preferences[$field]))) {
-                    $newPreferencesData = true;
-                    $preferences[$field] = $defaultValue;
-                }
+
+            $preferencesDefaultsApplied = $this->applyDefaultPreferences($table, $preferences);
+            if(count(array_diff($preferences, $preferencesDefaultsApplied))) {
+                $newPreferencesData = true;
             }
+            $preferences = $preferencesDefaultsApplied;
+
             if($newPreferencesData) {
                 // Insert to DB
                 $id = $db->set_entry(self::$_tableName, $preferences);
@@ -67,7 +97,7 @@ class DirectusPreferencesTableGateway extends AclAwareTableGateway {
             'columns_visible' => implode(',', $columns_visible),
             'table_name' => $table
         );
-        $data = array_merge($data, self::$defaultPreferencesValues);
+        $data = $this->applyDefaultPreferences($table, $data);
         // Insert to DB
         $id = $db->set_entry(self::$_tableName, $data);
         return $data;
