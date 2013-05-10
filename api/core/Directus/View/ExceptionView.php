950a22ceb02a2293f7c7fecf538182da165a20af
Daniel Bickett <dbickett@gmail.com>
2013-05-10 15:39:16 -0400

    gracefully catch all errors & report them as json instead of HTML

--- api/core/Directus/View/ExceptionView.php
+++ api/core/Directus/View/ExceptionView.php
@@ -11,31 +11,46 @@ class ExceptionView {
     public static function exceptionHandler($app, $exception) {
 
         $response = $app->response();
+        $response->header('Content-type', 'application/json');
+
+        $httpCode = 500;
+        $data = array();
 
         /**
          * Directus\Acl\Exception\AclException & subclasses
          */
         if($exception instanceof AclException || is_subclass_of($exception, "Directus\Acl\Exception\AclException")) {
-            $response->header('Content-type', 'application/json');
-            $app->halt(403, json_encode(array(
-                'message' => $exception->getMessage()
-            )));
+            $httpCode = 403;
+            $data = array('message' => $exception->getMessage());
         }
 
         /**
          * Directus\Db\Exception\RelationshipMetadataException
          */
         elseif($exception instanceof RelationshipMetadataException) {
-            $response->header('Content-type', 'application/json');
-            $app->halt(424, json_encode(array(
-                'message' => $exception->getMessage()
-            )));
+            $httpCode = 424;
+            $data = array('message' => $exception->getMessage());
         }
 
+        /**
+         * @todo treat production differently
+         */
         else {
-            // Re-throw
-            throw $exception;
+            $data = array(
+                'code' => $exception->getCode(),
+                'message' => $exception->getMessage(),
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'trace' => $exception->getTrace(),
+                'traceAsString' => $exception->getTraceAsString(),
+            );
         }
+
+        $data = json_encode($data);
+        if('production' !== DIRECTUS_ENV)
+            $data = JsonView::format_json($data);
+
+        $app->halt($httpCode, $data);
     }
 
 }
