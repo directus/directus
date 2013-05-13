b04d867fc57cb3338056f530a69807c8519b4c66
Daniel Bickett <dbickett@gmail.com>
2013-05-13 14:07:04 -0400

    one more formatting function

--- api/core/Directus/Util/Formatting.php
+++ api/core/Directus/Util/Formatting.php
@@ -33,7 +33,7 @@ class Formatting {
 			if (function_exists('mb_strtolower')) {
 				$title = mb_strtolower($title, 'UTF-8');
 			}
-			$title = utf8_uri_encode($title, 200);
+			$title = self::utf8_uri_encode($title, 200);
 		}
 
 		$title = strtolower($title);
@@ -94,6 +94,54 @@ class Formatting {
 			}
 		}
 		return true;
+	}
+
+	/**
+	 * Credit: WORDPRESS 3.6-BETA3
+	 * http://wpseek.com/utf8_uri_encode/
+	 * @param  string  $utf8_string
+	 * @param  integer $length      Max length of the string
+	 * @return string               String with Unicode encoded for URI.
+	 */
+	public static function utf8_uri_encode( $utf8_string, $length = 0 ) {
+		$unicode = '';
+		$values = array();
+		$num_octets = 1;
+		$unicode_length = 0;
+
+		$string_length = strlen( $utf8_string );
+		for ($i = 0; $i < $string_length; $i++ ) {
+
+			$value = ord( $utf8_string[ $i ] );
+
+			if ( $value < 128 ) {
+				if ( $length && ( $unicode_length >= $length ) )
+					break;
+				$unicode .= chr($value);
+				$unicode_length++;
+			} else {
+				if ( count( $values ) == 0 ) $num_octets = ( $value < 224 ) ? 2 : 3;
+
+				$values[] = $value;
+
+				if ( $length && ( $unicode_length + ($num_octets * 3) ) > $length )
+					break;
+				if ( count( $values ) == $num_octets ) {
+					if ($num_octets == 3) {
+						$unicode .= '%' . dechex($values[0]) . '%' . dechex($values[1]) . '%' . dechex($values[2]);
+						$unicode_length += 9;
+					} else {
+						$unicode .= '%' . dechex($values[0]) . '%' . dechex($values[1]);
+						$unicode_length += 6;
+					}
+
+					$values = array();
+					$num_octets = 1;
+				}
+			}
+		}
+
+		return $unicode;
 	}
 
 	/**
