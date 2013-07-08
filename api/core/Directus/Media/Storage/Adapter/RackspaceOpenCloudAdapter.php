af6a1cef023e0f302b7f4016f25d2118f02422e8
Daniel Bickett <dbickett@gmail.com>
2013-07-08 15:26:53 -0400

    functioning rackspace open cloud adapter

--- api/core/Directus/Media/Storage/Adapter/RackspaceOpenCloudAdapter.php
+++ api/core/Directus/Media/Storage/Adapter/RackspaceOpenCloudAdapter.php
@@ -2,27 +2,64 @@
 
 namespace Directus\Media\Storage\Adapter;
 
+use OpenCloud\Rackspace;
+
 class RackspaceOpenCloudAdapter extends Adapter {
 
-	protected function fileExists($fileName, $destination) {
-		// $path = $this->joinPaths($destination, $fileName);
-		// return file_exists($path);
-		return false;
-	}
+    protected static $requiredParams = array('api_user','api_key','region','endpoint');
 
-	protected function writeFile($localFile, $targetFileName, $destination) {
-		// $path = $this->joinPaths($destination, $targetFileName);
+    /**
+     * @var \OpenCloud\Rackspace
+     */
+    protected $conn;
+    protected $ostore;
+    protected $container;
 
-		// // @todo optionally use move_uploaded_file, for security
-		// // $move = move_uploaded_file($localFile, $path);
+    public function __construct(array $params = array()) {
+        parent::__construct($params);
+        if(!class_exists("\\OpenCloud\\Rackspace")) {
+            throw new \RuntimeException("Missing dependency \\OpenCloud\\Rackspace");
+        }
+        $this->conn = new Rackspace($params['endpoint'], array(
+            'username' => $params['api_user'],
+            'apiKey' => $params['api_key']
+        ));
+        $this->conn->SetDefaults('ObjectStore', 'cloudFiles', $params['region']);
+        $this->ostore = $this->conn->ObjectStore();
+    }
 
-		// // Don't overwrite!
-		// if(file_exists($path)) {
-		// 	return false;
-		// }
-		// $move = copy($localFile, $path);
-		// return $move;
-		return false;
-	}
+    protected function getObjectList() {
+        $objects = array();
+        $list = $this->container->ObjectList();
+        while ($item = $list->Next()) {
+            $objects[$item->name] = array(
+                'content_type' => $item->content_type,
+                'bytes' => $item->bytes,
+                'cdn-url' => $item->PublicUrl()
+            );
+        }
+        return $objects;
+    }
+
+    protected function fileExists($fileName, $destination) {
+        $this->container = $this->ostore->Container($destination);
+        $objects = $this->getObjectList();
+        return array_key_exists($fileName, $objects);
+    }
+
+    protected function writeFile($localFile, $targetFileName, $destination) {
+        $this->container = $this->ostore->Container($destination);
+        try {
+            $object = $this->container->DataObject();
+            $object->name = $targetFileName;
+            $object->SetData(file_get_contents($localFile));
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $object->content_type = finfo_file($finfo, $localFile);
+            $object->Create();
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
 
 }
