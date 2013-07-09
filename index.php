283d4bc8d6e522f4b0a8d807e2051d5b20937605
olov <olov@rngr.org>
2013-07-09 18:13:17 -0400

    bootstrap privileges data

--- index.php
+++ index.php
@@ -43,15 +43,65 @@ $data['authenticatedUser'] = AuthProvider::loggedIn() ? AuthProvider::getUserInf
 $data['tables'] = $db->get_tables();
 $data['users'] = $db->get_users();
 $data['groups'] = $db->get_entries("directus_groups");
-$data['privileges'] = $db->get_entries("directus_privileges");
 $data['settings'] = $db->get_settings('global');
-
 $data['page'] = '#tables';
 $data['path'] = DIRECTUS_PATH;
 $data['active_media'] = $db->count_active('directus_media');
 
-// Force array json encoding
+
+// @todo: maybe the $data['authenticatedUser'] could also provide group?
+function getGroupId() {
+	global $data;
+	$userId = $data['authenticatedUser']['id'];
+
+	foreach ($data['users']['rows'] as $user) {
+		if ($user['id'] == $userId) return $user['group']['id'];
+	}
+}
+
+// @todo: do a real mysql query instead
+function getMyTabPrivileges($tabPermissions, $groupId) {
+	foreach ($tabPermissions['rows'] as $row) {
+		if ($row['group_id'] == $groupId) return $row;
+	}
+	return array();
+}
+
+// @todo: do a real mysql query instead
+function getMyPrivileges($privileges, $groupId) {
+	return array_filter($privileges['rows'], function($row) use ($groupId) {
+		return ($row['group_id'] == $groupId);
+		//if ($row['group_id'] == $groupId) return $row;
+	});
+}
+
+// @todo: this is a very sloppy and temporary solution
+// bake it into Bootstrap?
+function filterPermittedExtensions($extensions, $blacklist) {
+
+	$blacklistArray = explode(',', $blacklist);
+
+	$permittedExtensions = array_filter($extensions, function($item) use ($blacklistArray) {
+		//@todo: id's should be a bit more sophisticated than this
+		$path = explode('/',$item);
+		$extensionId = $path[1];
+
+		return !in_array($extensionId, $blacklistArray);
+	});
+
+	return array_values($permittedExtensions);
+}
+
+
+$groupId = getGroupId();
+$data['tab_privileges'] = getMyTabPrivileges($db->get_entries('directus_tab_privileges'), $groupId);
 $data['extensions'] = array_values(Bootstrap::get('extensions'));
+$data['privileges'] = getMyPrivileges($db->get_entries("directus_privileges"),$groupId);
+
+if (array_key_exists('tab_blacklist',$data['tab_privileges'])) {
+	$data['extensions'] = filterPermittedExtensions($data['extensions'], $data['tab_privileges']['tab_blacklist']);
+}
+
 $data['ui'] = array_values(Bootstrap::get('uis'));
 
 $data = json_encode($data);
