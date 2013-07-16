49d20173d09b2af7d1d7d72594d5e75fbf667110
olov <olov@rngr.org>
2013-07-16 15:43:10 -0400

    should implement getStructure like everything else

--- app/core/collection.settings.js
+++ app/core/collection.settings.js
@@ -8,6 +8,12 @@ function(app, Backbone, Collection) {
 
 	var Settings = Collection.extend({
 
+		model: Backbone.Model.extend({
+			getStructure: function() {
+				return this.structure;
+			}
+		}),
+
 		//Convert from nested object to Collection
 		parse: function(result) {
 			return _.map(result, function(value, key) { value.id = key; return value; });
