a9c45537a7b16ebb540ec9099c70180cf6c5093d
olov <olov.sundstrom@gmail.com>
2013-08-03 21:59:59 +0200

    default content in directus_storage_adapters, #308

--- directus.sql
+++ directus.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.5.29)
 # Database: directus
-# Generation Time: 2013-08-03 18:56:31 +0000
+# Generation Time: 2013-08-03 19:58:49 +0000
 # ************************************************************
 
 
@@ -23,51 +23,10 @@
 # Dump of table directus_activity
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_activity`;
-
-CREATE TABLE `directus_activity` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `type` varchar(100) DEFAULT NULL,
-  `action` varchar(100) NOT NULL,
-  `identifier` varchar(100) DEFAULT NULL,
-  `table_name` varchar(100) NOT NULL DEFAULT '',
-  `row_id` int(10) DEFAULT NULL,
-  `user` int(10) NOT NULL DEFAULT '0',
-  `data` text,
-  `delta` text NOT NULL,
-  `parent_id` int(11) DEFAULT NULL,
-  `parent_changed` tinyint(1) NOT NULL COMMENT 'Did the top-level record in the change set alter (scalar values/many-to-one relationships)? Or only the data within its related foreign collection records? (*toMany)',
-  `datetime` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Contains history of revisions';
-
 
 
 # Dump of table directus_columns
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `directus_columns`;
-
-CREATE TABLE `directus_columns` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `table_name` varchar(64) NOT NULL DEFAULT '',
-  `column_name` varchar(64) NOT NULL DEFAULT '',
-  `data_type` varchar(64) DEFAULT NULL,
-  `ui` varchar(64) DEFAULT NULL,
-  `system` tinyint(1) NOT NULL DEFAULT '0',
-  `master` tinyint(1) NOT NULL DEFAULT '0',
-  `hidden_input` tinyint(1) NOT NULL DEFAULT '0',
-  `hidden_list` tinyint(1) NOT NULL DEFAULT '0',
-  `required` tinyint(1) NOT NULL DEFAULT '0',
-  `table_related` varchar(64) DEFAULT NULL,
-  `junction_table` varchar(64) DEFAULT NULL,
-  `junction_key_left` varchar(64) DEFAULT NULL,
-  `junction_key_right` varchar(64) DEFAULT NULL,
-  `sort` int(11) DEFAULT NULL,
-  `comment` varchar(1024) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `table-column` (`table_name`,`column_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 LOCK TABLES `directus_columns` WRITE;
 /*!40000 ALTER TABLE `directus_columns` DISABLE KEYS */;
@@ -101,16 +60,6 @@ UNLOCK TABLES;
 # Dump of table directus_groups
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_groups`;
-
-CREATE TABLE `directus_groups` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `restrict_to_ip_whitelist` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 LOCK TABLES `directus_groups` WRITE;
 /*!40000 ALTER TABLE `directus_groups` DISABLE KEYS */;
 
@@ -125,100 +74,25 @@ UNLOCK TABLES;
 # Dump of table directus_ip_whitelist
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_ip_whitelist`;
-
-CREATE TABLE `directus_ip_whitelist` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(250) DEFAULT NULL,
-  `description` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 
 # Dump of table directus_media
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `directus_media`;
-
-CREATE TABLE `directus_media` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(1) DEFAULT '1',
-  `name` varchar(255) DEFAULT NULL,
-  `url` varchar(2000) DEFAULT NULL,
-  `title` varchar(255) DEFAULT '',
-  `location` varchar(200) DEFAULT NULL,
-  `caption` text,
-  `type` varchar(50) DEFAULT '',
-  `charset` varchar(50) DEFAULT '',
-  `tags` varchar(255) DEFAULT '',
-  `width` int(5) DEFAULT '0',
-  `height` int(5) DEFAULT '0',
-  `size` int(20) DEFAULT '0',
-  `embed_id` varchar(200) DEFAULT NULL,
-  `user` int(11) NOT NULL,
-  `date_uploaded` datetime DEFAULT NULL,
-  `storage_adapter` int(11) unsigned DEFAULT NULL COMMENT 'FK `directus_storage_adapters`.`id`',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Directus Media Storage';
 
 
 
 # Dump of table directus_messages
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_messages`;
-
-CREATE TABLE `directus_messages` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(1) NOT NULL DEFAULT '1',
-  `subject` varchar(255) NOT NULL DEFAULT '',
-  `message` text NOT NULL,
-  `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `reply` int(10) NOT NULL DEFAULT '0',
-  `from` int(10) NOT NULL DEFAULT '0',
-  `to` varchar(255) NOT NULL DEFAULT '',
-  `viewed` varchar(255) NOT NULL DEFAULT ',',
-  `archived` varchar(255) NOT NULL DEFAULT ',',
-  `table` varchar(255) NOT NULL DEFAULT '',
-  `row` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 
 
 # Dump of table directus_preferences
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_preferences`;
-
-CREATE TABLE `directus_preferences` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user` int(11) DEFAULT NULL,
-  `table_name` varchar(64) DEFAULT NULL,
-  `columns_visible` varchar(300) DEFAULT NULL,
-  `sort` varchar(64) DEFAULT 'id',
-  `sort_order` varchar(5) DEFAULT 'asc',
-  `active` varchar(5) DEFAULT '3',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 
 # Dump of table directus_privileges
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `directus_privileges`;
-
-CREATE TABLE `directus_privileges` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `table_name` varchar(255) NOT NULL,
-  `permissions` varchar(500) DEFAULT NULL COMMENT 'Table-level permissions (insert, delete, etc.)',
-  `group_id` int(11) NOT NULL,
-  `read_field_blacklist` varchar(1000) DEFAULT NULL,
-  `write_field_blacklist` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `directus_privileges` WRITE;
 /*!40000 ALTER TABLE `directus_privileges` DISABLE KEYS */;
@@ -245,17 +119,6 @@ UNLOCK TABLES;
 # Dump of table directus_settings
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_settings`;
-
-CREATE TABLE `directus_settings` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `collection` varchar(250) DEFAULT NULL,
-  `name` varchar(250) DEFAULT NULL,
-  `value` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `Unique Collection and Name` (`collection`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 LOCK TABLES `directus_settings` WRITE;
 /*!40000 ALTER TABLE `directus_settings` DISABLE KEYS */;
 
@@ -277,87 +140,35 @@ UNLOCK TABLES;
 # Dump of table directus_social_feeds
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_social_feeds`;
-
-CREATE TABLE `directus_social_feeds` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(1) NOT NULL DEFAULT '1',
-  `type` tinyint(2) NOT NULL COMMENT 'Twitter (1), Instagram (2)',
-  `last_checked` datetime DEFAULT NULL,
-  `name` varchar(255) NOT NULL,
-  `foreign_id` varchar(255) NOT NULL,
-  `data` text NOT NULL COMMENT 'Feed metadata. JSON format.',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 
 # Dump of table directus_social_posts
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `directus_social_posts`;
-
-CREATE TABLE `directus_social_posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(1) NOT NULL DEFAULT '1',
-  `feed` int(11) NOT NULL COMMENT 'The FK ID of the feed.',
-  `datetime` datetime NOT NULL COMMENT 'The date/time this entry was published.',
-  `foreign_id` varchar(55) NOT NULL,
-  `data` text NOT NULL COMMENT 'The API response for this entry, excluding unnecessary feed metadata, which is stored on the directus_social_feeds table.',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `feed` (`feed`,`foreign_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 
 # Dump of table directus_storage_adapters
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_storage_adapters`;
+LOCK TABLES `directus_storage_adapters` WRITE;
+/*!40000 ALTER TABLE `directus_storage_adapters` DISABLE KEYS */;
 
-CREATE TABLE `directus_storage_adapters` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) NOT NULL,
-  `adapter_name` varchar(255) NOT NULL DEFAULT '',
-  `role` varchar(255) DEFAULT NULL COMMENT 'DEFAULT, THUMBNAIL, or Null. DEFAULT and THUMBNAIL should only occur once each.',
-  `public` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '1 for yes, 0 for no.',
-  `destination` varchar(255) NOT NULL DEFAULT '',
-  `url` varchar(2000) DEFAULT '' COMMENT 'Trailing slash required.',
-  `params` varchar(2000) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `directus_storage_adapters` (`id`, `key`, `adapter_name`, `role`, `public`, `destination`, `url`, `params`)
+VALUES
+	(1,'media','FileSystemAdapter','DEFAULT',1,'','',NULL),
+	(2,'thumbnails','FileSystemAdapter','THUMBNAIL',1,'','',NULL);
 
+/*!40000 ALTER TABLE `directus_storage_adapters` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table directus_tab_privileges
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_tab_privileges`;
-
-CREATE TABLE `directus_tab_privileges` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `group_id` int(11) DEFAULT NULL,
-  `tab_blacklist` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 
 
 # Dump of table directus_tables
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `directus_tables`;
-
-CREATE TABLE `directus_tables` (
-  `table_name` varchar(64) NOT NULL DEFAULT '',
-  `hidden` tinyint(1) NOT NULL DEFAULT '0',
-  `single` tinyint(1) NOT NULL DEFAULT '0',
-  `inactive_by_default` tinyint(1) NOT NULL DEFAULT '0',
-  `is_junction_table` tinyint(1) NOT NULL DEFAULT '0',
-  `footer` tinyint(1) DEFAULT '0',
-  `magic_owner_column` varchar(64) DEFAULT NULL,
-  PRIMARY KEY (`table_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `directus_tables` WRITE;
 /*!40000 ALTER TABLE `directus_tables` DISABLE KEYS */;
@@ -377,19 +188,6 @@ UNLOCK TABLES;
 
 # Dump of table directus_ui
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `directus_ui`;
-
-CREATE TABLE `directus_ui` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `table_name` varchar(64) DEFAULT NULL,
-  `column_name` varchar(64) DEFAULT NULL,
-  `ui_name` varchar(200) DEFAULT NULL,
-  `name` varchar(200) DEFAULT NULL,
-  `value` text,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `unique` (`table_name`,`column_name`,`ui_name`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `directus_ui` WRITE;
 /*!40000 ALTER TABLE `directus_ui` DISABLE KEYS */;
@@ -411,52 +209,6 @@ UNLOCK TABLES;
 # Dump of table directus_users
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `directus_users`;
-
-CREATE TABLE `directus_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(1) DEFAULT '1',
-  `first_name` varchar(50) DEFAULT '',
-  `last_name` varchar(50) DEFAULT '',
-  `email` varchar(255) DEFAULT '',
-  `password` varchar(255) DEFAULT '',
-  `salt` varchar(255) NOT NULL,
-  `token` varchar(255) DEFAULT '',
-  `reset_token` varchar(255) DEFAULT '',
-  `reset_expiration` datetime DEFAULT NULL,
-  `description` text,
-  `email_messages` tinyint(1) DEFAULT '1',
-  `last_login` datetime DEFAULT '0000-00-00 00:00:00',
-  `last_access` datetime DEFAULT NULL,
-  `last_page` varchar(255) DEFAULT '',
-  `ip` varchar(50) DEFAULT '',
-  `group` int(11) DEFAULT NULL,
-  `avatar` varchar(500) DEFAULT NULL,
-  `closest_studio` varchar(255) DEFAULT NULL,
-  `adp_id` int(11) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `office_phone` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
-  `city` varchar(255) DEFAULT NULL,
-  `state` varchar(2) DEFAULT NULL,
-  `zip` varchar(10) DEFAULT NULL,
-  `emergency_name` varchar(255) DEFAULT NULL,
-  `emergency_email` varchar(255) DEFAULT NULL,
-  `emergency_phone` varchar(255) DEFAULT NULL,
-  `emergency_name_2` varchar(255) DEFAULT NULL,
-  `emergency_email_2` varchar(255) DEFAULT NULL,
-  `emergency_phone_2` varchar(255) DEFAULT NULL,
-  `vestigial_username` varchar(255) DEFAULT NULL,
-  `vestigial_role` varchar(255) DEFAULT NULL,
-  `vestigial_password` varchar(255) DEFAULT NULL,
-  `vestigial_isretailadmin` varchar(255) DEFAULT NULL,
-  `vestigial_directorycategory` varchar(255) DEFAULT NULL,
-  `vestigial_image` varchar(255) DEFAULT NULL,
-  `vestigial_can_buy_old` varchar(255) DEFAULT NULL,
-  `vestigial_hide_in_directory` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
 LOCK TABLES `directus_users` WRITE;
 /*!40000 ALTER TABLE `directus_users` DISABLE KEYS */;
 
@@ -470,31 +222,6 @@ UNLOCK TABLES;
 
 # Dump of table ui_gallery
 # ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `ui_gallery`;
-
-CREATE TABLE `ui_gallery` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(1) NOT NULL DEFAULT '1',
-  `sort` int(11) NOT NULL,
-  `input_field` varchar(200) NOT NULL,
-  `slug` varchar(200) NOT NULL,
-  `datetime` datetime NOT NULL,
-  `date` date NOT NULL,
-  `time` time NOT NULL,
-  `numeric` int(11) NOT NULL,
-  `options` varchar(200) NOT NULL,
-  `tags` varchar(200) NOT NULL,
-  `radio_buttons` varchar(200) NOT NULL,
-  `checkbox` tinyint(1) NOT NULL,
-  `text_area` text NOT NULL,
-  `wysiwyg` text NOT NULL,
-  `password` varchar(200) NOT NULL DEFAULT '',
-  `color` varchar(255) DEFAULT NULL,
-  `slider` int(11) DEFAULT NULL,
-  `single_media` int(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 
