67dece2248f0a4064ec8ec2f086e4e7fb74468ba
olov <olov.sundstrom@gmail.com>
2013-08-14 14:07:23 +0200

    deleted example extension for now

--- extensions/example/api.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-$data = array(
-	array('symbol'=>'✌'),
-	array('symbol'=>'☂'),
-	array('symbol'=>'⚛'),
-	array('symbol'=>'♫')
-);
-header("Content-Type: application/json; charset=utf-8");
-echo json_encode($data);
