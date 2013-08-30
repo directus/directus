77c65f8213449eea00f50b7bbb37a9aa30301b5d
olov <olov@rngr.org>
2013-08-30 14:50:24 -0400

    fetch tab privileges

---
+++ api/core/Directus/Db/TableGateway/DirectusTabPrivilegesTableGateway.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Directus\Db\TableGateway;
+
+use Directus\Acl\Acl;
+use Directus\Db\TableGateway\AclAwareTableGateway;
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Select;
+
+class DirectusTabPrivilegesTableGateway extends AclAwareTableGateway {
+
+    public static $tableName = "directus_tab_privileges";
+
+    public function __construct(Acl $acl, AdapterInterface $adapter) {
+        parent::__construct($acl, self::$tableName, $adapter);
+    }
+
+    public function fetchAllByGroup($groupId) {
+        $select = new Select($this->getTable());
+        $select->limit(1);
+        $select->where->equalTo('group_id', $groupId);
+        $row = $this->selectWith($select)->toArray();
+        if(!$row) {
+            return array();
+        }
+        return $row[0];
+    }
+}
