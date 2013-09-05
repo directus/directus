7f2cf3f0121c61758277098ea9a2e7c04287aeeb
olov <olov@rngr.org>
2013-09-05 12:37:39 -0400

    messages draft

--- app/modules/messages.js
+++ app/modules/messages.js
@@ -10,12 +10,37 @@
 define([
   'app',
   'backbone',
-  'core/directus'
+  'core/directus',
+  'core/panes/pane.saveview'
 ],
 
-function(app, Backbone, Directus) {
+function(app, Backbone, Directus, SaveModule) {
 
   var Messages = app.module();
+
+  Messages.Views.Edit = Backbone.Layout.extend({
+
+    template: 'page',
+
+    serialize: function() {
+      var title = this.model.isNew() ? 'Compose New' : 'Reading Message';
+      return {title: title, sidebar: true};
+    },
+
+    beforeRender: function() {
+      this.setView('#sidebar', new SaveModule({model: this.model, buttonText: 'Send Message', single: true}));
+    },
+
+    afterRender: function() {
+      this.setView('#page-content', new Directus.EditView({model: this.model}));
+      this.model.fetch();
+      //this.setView('#page-content', new Directus.Table({collection: this.collection, navigate: true, hideColumnPreferences: true}));
+      //this.collection.fetch();
+    }
+
+  });
+
+
 
   Messages.Views.List = Backbone.Layout.extend({
 
@@ -25,9 +50,9 @@ function(app, Backbone, Directus) {
       return {title: 'Messages'};
     },
 
-    beforeRender: function() {
-      console.log(this.collection);
-      this.setView('#page-content', new Directus.Table({collection: this.collection}));
+    afterRender: function() {
+      this.setView('#page-content', new Directus.Table({collection: this.collection, navigate: true, hideColumnPreferences: true}));
+      this.collection.fetch();
     }
 
   });
