ac32b83b92290d7d9b70373d504cc143e3146e1c
olov <olov@rngr.org>
2013-09-11 18:17:26 -0400

    messages readview

--- app/modules/messages.js
+++ app/modules/messages.js
@@ -44,20 +44,85 @@ function(app, Backbone, Directus, SaveModule, EntriesCollection) {
 
   });
 
-  Messages.Views.Edit = Backbone.Layout.extend({
+  var ReadView = Backbone.Layout.extend({
+
+    template: 'messages-reading',
+
+    events: {
+      'click #messages-response-button': function() {
+        var collection = this.model.get('responses');
+        var Model = collection.model;
+
+        var attrs = {
+          'from': app.getCurrentUser().get('id'),
+          'subject': '',
+          'recepients': '0_7',
+          'datetime': (new Date()).toISOString(),
+          'response_to': this.model.id,
+          'message': $('#messages-response').val(),
+          'responses': []
+        };
+
+        var model = new Model(attrs, {collection: collection, parse: true, url: app.API_URL + 'messages/rows/'});
+        model.save();
+        collection.add(model);
+      }
+    },
+
+    serialize: function() {
+      console.log(this.model.toJSON());
+      return this.model.toJSON()
+    },
+
+    initialize: function() {
+      this.model.on('sync', this.render, this);
+      this.model.get('responses').on('add', this.render, this);
+    }
+
+  });
+
+
+  Messages.Views.Read = Backbone.Layout.extend({
+
+    template: 'page',
+
+    serialize: function() {
+      return {title: this.model.get('subject'), breadcrumbs: [{title: 'Messages', anchor: '#messages'}]};
+    },
+
+    beforeRender: function() {
+      //this.setView('#sidebar', new SaveModule({model: this.model, buttonText: 'Send Message', single: true}));
+    },
+
+    afterRender: function() {
+      var readView = new ReadView({model: this.model});
+      this.setView('#page-content', readView);
+      if (this.model.has('message')) {
+        readView.render();
+      }
+      //this.editView = new Directus.EditView({model: this.model});
+      //this.setView('#page-content', this.editView);
+      //this.editView.render();
+    }
+
+  });
+
+  Messages.Views.New = Backbone.Layout.extend({
 
     template: 'page',
 
     events: {
       'click #save-form': function(e) {
-        console.log(this.model.toJSON());
-        this.model.save(this.editView.data());
+        this.model.save(this.editView.data(), {success: function() {
+          app.router.go('#messages');
+        }});
+
       }
     },
 
     serialize: function() {
-      var title = this.model.isNew() ? 'Compose New' : 'Reading Message';
-      return {title: title, sidebar: true};
+      var title = 'Compose';
+      return {title: title, sidebar: true, breadcrumbs: [{ title: 'Messages', anchor: '#messages'}]};
     },
 
     beforeRender: function() {
@@ -67,14 +132,7 @@ function(app, Backbone, Directus, SaveModule, EntriesCollection) {
     afterRender: function() {
       this.editView = new Directus.EditView({model: this.model});
       this.setView('#page-content', this.editView);
-
-      if (!this.model.isNew()) {
-        this.model.fetch();
-      } else {
-        this.editView.render();
-      }
-      //this.setView('#page-content', new Directus.Table({collection: this.collection, navigate: true, hideColumnPreferences: true}));
-      //this.collection.fetch();
+      this.editView.render();
     }
 
   });
@@ -83,14 +141,23 @@ function(app, Backbone, Directus, SaveModule, EntriesCollection) {
 
     template: 'page',
 
+    events: {
+      'click #btn-top': function() {
+        app.router.go('#messages','new');
+      }
+    },
+
     serialize: function() {
-      return {title: 'Messages'};
+      return {
+        title: 'Messages',
+        buttonTitle: 'Compose'
+      };
     },
 
     afterRender: function() {
       var view = new Directus.Table({collection: this.collection, navigate: true, hideColumnPreferences: true});
       this.setView('#page-content', view);
-      view.render();
+      this.collection.fetch();
     }
 
   });
