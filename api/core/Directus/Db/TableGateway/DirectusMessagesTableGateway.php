3094a2ba101b1ef2436b1171d9324d0763ada5d4
olov <olov@rngr.org>
2013-09-11 18:16:07 -0400

    sendMessage fetchMessage methods

--- api/core/Directus/Db/TableGateway/DirectusMessagesTableGateway.php
+++ api/core/Directus/Db/TableGateway/DirectusMessagesTableGateway.php
@@ -8,6 +8,7 @@ use Zend\Db\Adapter\AdapterInterface;
 use Zend\Db\Sql\Sql;
 use Zend\Db\Sql\Select;
 use Zend\Db\Sql\Insert;
+use Zend\Db\Sql\Expression;
 use Zend\Db\Adapter\Adapter;
 
 class DirectusMessagesTableGateway extends AclAwareTableGateway {
@@ -18,14 +19,16 @@ class DirectusMessagesTableGateway extends AclAwareTableGateway {
         parent::__construct($acl, self::$_tableName, $adapter);
     }
 
-    public function sendMessage($payload, $recepients) {
+    public function sendMessage($payload, $recepients, $from) {
         $insert = new Insert($this->getTable());
         $insert
             ->columns(array('from', 'subject', 'message'))
             ->values(array(
-                'from' => 1,
-                'subject' => '',
-                'message' => 'a',
+                'from' => $from,
+                'subject' => $payload['subject'],
+                'message' => $payload['message'],
+                'datetime' => new Expression('NOW()'),
+                'response_to' => $payload['response_to']
                 ));
         $rows = $this->insertWith($insert);
 
@@ -34,24 +37,77 @@ class DirectusMessagesTableGateway extends AclAwareTableGateway {
         $values = array();
 
         foreach($recepients as $recepient) {
-            $values[] = "($recepient,$messageId,0)";
+            $values[] = "($messageId,$recepient,0)";
         }
 
         $valuesString = implode(',', $values);
 
         //@todo sanitize and implement ACL
-        $sql = "INSERT INTO directus_messages_recepients (`directus_users_id`, `directus_messages_id`, `read`) VALUES $valuesString";
+        $sql = "INSERT INTO directus_messages_recepients (`message_id`, `recepient`, `read`) VALUES $valuesString";
         $result = $this->adapter->query($sql, Adapter::QUERY_MODE_EXECUTE);
 
         return $messageId;
+    }
 
+    private function fetchResponses($messageId) {
+        $select = new Select($this->getTable());
+        $select
+            ->columns(array('id', 'from', 'subject', 'message', 'attachment'))
+            ->where
+                ->equalTo('directus_messages.response_to', $messageId);
+
+        return $this->selectWith($select)->toArray();
     }
 
     public function fetchMessage($id) {
         $select = new Select($this->getTable());
         $select
-            ->columns(array('from', 'subject', 'message'))
+            ->columns(array('id', 'from', 'subject', 'message', 'attachment'))
+            ->join('directus_messages_recepients', 'directus_messages.id = directus_messages_recepients.message_id', array('recepients' => new Expression("GROUP_CONCAT(recepient)")))
             ->where
-                ->equalTo('id', $id);
+                ->equalTo('directus_messages.id', $id);
+
+        $result = $this->selectWith($select)->toArray();
+        $result = $result[0];
+
+        $result['attachment'] = array();
+        $result['responses'] = array('rows' => $this->fetchResponses($id));
+
+        return $result;
+    }
+
+    public function fetchMessagesInbox($uid) {
+        $select = new Select('directus_messages');
+        $select
+            ->join('directus_messages_recepients', 'directus_messages.id = directus_messages_recepients.message_id', array('read'))
+            ->where
+                ->equalTo('directus_messages_recepients.recepient', $uid);
+
+        $result = $this->selectWith($select)->toArray();
+
+        $resultLookup = array();
+        $ids = array();
+
+        //->where->in('group', $ids);
+        // print_r($ids);
+
+        foreach ($result as $item) {
+            $item['responses'] = array('rows'=>array());
+            $resultLookup[$item['id']] = $item;
+            $ids[] = $item['id'];
+        }
+
+        foreach ($result as $item) {
+            if ($item['response_to'] != null) {
+                unset($resultLookup[$item['id']]);
+                $resultLookup[$item['response_to']]['responses']['rows'][] = $item;
+            }
+        }
+
+        return array_values($resultLookup);
+    }
+
+    public function fetchMessagesSent($uid) {
+
     }
 }
