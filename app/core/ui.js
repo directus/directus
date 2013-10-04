6faea603965ac490d33f8db04caf61c6631f3814
olov <olov@rngr.org>
2013-10-04 11:58:49 -0400

    auto load ui's

--- app/core/ui.js
+++ app/core/ui.js
@@ -1,4 +1,5 @@
 define([
+  "module",
   "core-ui/textinput",
   "core-ui/directus_columns",
   "core-ui/directus_media",
@@ -29,107 +30,115 @@ define([
   'core-ui/one_to_many',
   'core-ui/wysiwyg',
   'core-ui/directus_messages_recepients',
-  'core-ui/password'].concat(window.directusData.ui),
-function(textinput) {
+  'core-ui/password'],
+function(module, textinput) {
 
   "use strict";
 
   var ui = {};
 
-  ui.core = arguments;
+  ui.core = _.values(arguments).slice(1);
 
-  var Component = function(options) {
-    this.model = options.model;
-    this.collection = options.collection || this.model.collection;
-    this.structure = options.structure || this.structure;
-  };
+  module.exports = ui;
 
-  _.extend(Component.prototype, {
+  require(window.directusData.ui, function() {
 
-    getInput: function(attr) {
-      var schema = this.structure.get(attr);
+    //@todo keep the core and extend ui seperate
+    ui.core = _.union(ui.core, _.values(arguments));
+
+
+    var Component = function(options) {
+      this.model = options.model;
+      this.collection = options.collection || this.model.collection;
+      this.structure = options.structure || this.structure;
+    };
+
+    _.extend(Component.prototype, {
+
+      getInput: function(attr) {
+        var schema = this.structure.get(attr);
+        var View = _.where(ui.core, {id: schema.get('ui')})[0] || textinput;
+        var view = new View.Input({
+          model: this.model,
+          collection: this.collection,
+          settings: schema.options,
+          schema: schema,
+          structure: this.structure,
+          name: attr,
+          value: this.model.get(attr),
+          canWrite: _.has(this.model, 'canEdit') ? this.model.canEdit(attr) : true
+        });
+        return view;
+      }
+
+    });
+
+    // This is a shorter way to get the list way since
+    // It's total overkill to instanciate an object before
+    // It's also AB-FAB for templates!
+    ui.getList = function(model, attr) {
+      var collection = model.collection;
+      var structure = model.getStructure();
+      var schema = structure.get(attr);
+      var View = ui.getModelColumnInput(model, attr);
+      return View.list({
+          model: model,
+          collection: collection,
+          settings: schema.options,
+          schema: schema,
+          value: model.get(attr),
+          tagName: 'td'
+      });
+    };
+
+    ui.getModelColumnInput = function(model, attr) {
+      var collection = model.collection;
+      var structure = model.getStructure();
+      var schema = structure.get(attr);
       var View = _.where(ui.core, {id: schema.get('ui')})[0] || textinput;
-      var view = new View.Input({
-        model: this.model,
-        collection: this.collection,
-        settings: schema.options,
-        schema: schema,
-        structure: this.structure,
-        name: attr,
-        value: this.model.get(attr),
-        canWrite: _.has(this.model, 'canEdit') ? this.model.canEdit(attr) : true
+      return View;
+    };
+
+    ui.validate = function(model, attr, value) {
+      var collection = model.collection;
+      var structure = model.getStructure();
+      var schema = structure.get(attr);
+      var View = ui.getModelColumnInput(model, attr);
+      if (View.hasOwnProperty('validate')) {
+        return View.validate(value, {
+          model: model,
+          collection: collection,
+          settings: schema.options,
+          schema: schema,
+          tagName: 'td'
+        });
+      }
+    };
+
+    ui.initialize = function(model, options) {
+      return new Component(model, options);
+    };
+
+    // Return the settings for all UI-components. Usefult for caching.
+    ui.settings = function() {
+      var settings = {};
+      _.each(ui.core, function(ui) {
+        //ui.schema = new Backbone.Collection(ui.variables);
+        settings[ui.id] = ui;
       });
-      return view;
-    }
+      return settings;
+    };
+
+    ui.perType = function(type) {
+      return _.filter(ui.core, function(obj) { return (_.indexOf(obj.dataTypes, type) > -1); });
+    };
+
+    //Handlebars helper!
+    Handlebars.registerHelper("ui", function(model, attr, options) {
+      if (model.isNested) model = model.get('data');
+      var html = ui.getList(model, attr) || '';
+      return new Handlebars.SafeString(html);
+    });
 
   });
-
-  // This is a shorter way to get the list way since
-  // It's total overkill to instanciate an object before
-  // It's also AB-FAB for templates!
-  ui.getList = function(model, attr) {
-    var collection = model.collection;
-    var structure = model.getStructure();
-    var schema = structure.get(attr);
-    var View = ui.getModelColumnInput(model, attr);
-    return View.list({
-        model: model,
-        collection: collection,
-        settings: schema.options,
-        schema: schema,
-        value: model.get(attr),
-        tagName: 'td'
-    });
-  };
-
-  ui.getModelColumnInput = function(model, attr) {
-    var collection = model.collection;
-    var structure = model.getStructure();
-    var schema = structure.get(attr);
-    var View = _.where(ui.core, {id: schema.get('ui')})[0] || textinput;
-    return View;
-  };
-
-  ui.validate = function(model, attr, value) {
-    var collection = model.collection;
-    var structure = model.getStructure();
-    var schema = structure.get(attr);
-    var View = ui.getModelColumnInput(model, attr);
-    if (View.hasOwnProperty('validate')) {
-      return View.validate(value, {
-        model: model,
-        collection: collection,
-        settings: schema.options,
-        schema: schema,
-        tagName: 'td'
-      });
-    }
-  };
-
-  ui.initialize = function(model, options) {
-    return new Component(model, options);
-  };
-
-  // Return the settings for all UI-components. Usefult for caching.
-  ui.settings = function() {
-    var settings = {};
-    _.each(ui.core, function(ui) {
-      //ui.schema = new Backbone.Collection(ui.variables);
-      settings[ui.id] = ui;
-    });
-    return settings;
-  };
-
-  ui.perType = function(type) {
-    return _.filter(ui.core, function(obj) { return (_.indexOf(obj.dataTypes, type) > -1); });
-  };
-
-  //Handlebars helper!
-  Handlebars.registerHelper("ui", function(model, attr, options) {
-    if (model.isNested) model = model.get('data');
-    var html = ui.getList(model, attr) || '';
-    return new Handlebars.SafeString(html);
-  });
-
-  return ui;
 });
