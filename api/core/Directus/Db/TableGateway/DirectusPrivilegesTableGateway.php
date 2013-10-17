878b249f438356f3fa722ae241e01fd35d91ce94
olov <olov@rngr.org>
2013-10-16 20:25:48 -0400

    permissions first draft

--- api/core/Directus/Db/TableGateway/DirectusPrivilegesTableGateway.php
+++ api/core/Directus/Db/TableGateway/DirectusPrivilegesTableGateway.php
@@ -4,6 +4,7 @@ namespace Directus\Db\TableGateway;
 
 use Directus\Acl\Acl;
 use Directus\Db\TableGateway\AclAwareTableGateway;
+use Directus\Db\TableSchema;
 use Zend\Db\Adapter\AdapterInterface;
 use Zend\Db\Sql\Sql;
 use Zend\Db\Sql\Select;
@@ -32,6 +33,55 @@ class DirectusPrivilegesTableGateway extends AclAwareTableGateway {
         return $privilegesByTable;
     }
 
+    public function fetchPerTable($groupId) {
+        // Don't include tables that can't have privileges changed
+        $blacklist = array(
+            'directus_columns',
+            'directus_ip_whitelist',
+            'directus_messages_recepients',
+            'directus_preferences',
+            'directus_privileges',
+            'directus_settings',
+            'directus_social_feeds',
+            'directus_social_posts',
+            'directus_storage_adapters',
+            'directus_tab_privileges',
+            'directus_tables',
+            'directus_ui',
+            'directus_users_copy'
+        );
+
+
+        $select = new Select($this->table);
+        $select->where->equalTo('group_id', $groupId);
+        $select->group(array('group_id', 'table_name'));
+        $rowset = $this->selectWith($select);
+        $rowset = $rowset->toArray();
+
+        $tableSchema = new TableSchema();
+        $tables = $tableSchema->getTablenames();
+        $privileges = array();
+        $privilegesHash = array();
+
+        foreach ($rowset as $item) {
+            $privilegesHash[$item['table_name']] = $item;
+        }
+
+        foreach ($tables as $table) {
+            if (in_array($table, $blacklist)) {
+                continue;
+            }
+            if (array_key_exists($table, $privilegesHash)) {
+                $item = $privilegesHash[$table];
+            } else {
+                $item = array('table_name' => $table, 'group_id'=> $groupId);
+            }
+
+            $privileges[] = $item;
+        }
+
+        return $privileges;
+    }
 
     public function fetchGroupPrivilegesRaw($group_id) {
         $select = new Select($this->table);
