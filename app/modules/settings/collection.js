0f6c12c046c5bc012ce9978d0c2858090ac6716b
olov <olov@rngr.org>
2013-10-24 13:30:23 -0400

    settings collection

---
+++ app/modules/settings/collection.js
@@ -0,0 +1,28 @@
+define([
+  "app",
+  "backbone",
+  "core/collection"
+],
+
+function(app, Backbone, Collection) {
+
+	"use strict";
+
+	var Settings = Collection.extend({
+
+		model: Backbone.Model.extend({
+			getStructure: function() {
+				return this.structure;
+			}
+		}),
+
+		//Convert from nested object to Collection
+		parse: function(result) {
+			return _.map(result, function(value, key) { value.id = key; return value; });
+		}
+
+	});
+
+	return Settings;
+
+});
