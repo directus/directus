8dc9fb8cd60e4e83846ccc8d984ea0c57089feca
olov <olov@rngr.org>
2013-12-09 20:58:22 -0500

    simplify schemas

--- app/schema/fixed/groups.js
+++ app/schema/fixed/groups.js
@@ -2,47 +2,43 @@ define(function(require, exports, module) {
 
   "use strict";
 
-  var groups = module.exports;
+  module.exports = {
 
-  groups.table = {
     "id":"directus_groups",
     "table_name":"directus_groups",
     "hidden":true,
     "single":false,
     "inactive_by_default": "0",
-    "url": "api/1/groups"
+    "url": "api/1/groups",
+
+    "columns": [
+      {
+        "id":"id",
+        "column_name":"id",
+        "ui":"numeric",
+        "type":"INT",
+        "system":true,
+        "hidden_list":true,
+        "hidden_input":true
+      },
+      {
+        "id":"name",
+        "column_name":"name",
+        "ui":"numeric",
+        "type":"VARCHAR",
+        "system":true,
+        "hidden_list":true,
+        "hidden_input":true
+      },
+      {
+        "id":"description",
+        "column_name":"description",
+        "ui":"textinput",
+        "type":"VARCHAR",
+        "system":false,
+        "hidden_list":true,
+        "hidden_input":true
+      }
+    ]
   };
-
-  groups.structure = [
-    {
-      "id":"id",
-      "column_name":"id",
-      "ui":"numeric",
-      "type":"INT",
-      "system":true,
-      "hidden_list":true,
-      "hidden_input":true
-    },
-    {
-      "id":"name",
-      "column_name":"name",
-      "ui":"numeric",
-      "type":"VARCHAR",
-      "system":true,
-      "hidden_list":true,
-      "hidden_input":true
-    },
-    {
-      "id":"description",
-      "column_name":"description",
-      "ui":"textinput",
-      "type":"VARCHAR",
-      "system":false,
-      "hidden_list":true,
-      "hidden_input":true
-    }
-  ];
-
-  return groups;
-
 });
