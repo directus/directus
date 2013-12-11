0cc7066933458026383f6f08394093b1241b15ec
olov <olov@rngr.org>
2013-12-10 20:24:38 -0500

    test frameworks

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,8 @@ https://npmjs.org/package/grunt-contrib-requirejs
 
 module.exports = function(grunt) {
 
+    'use strict';
+
     grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
@@ -70,8 +72,70 @@ module.exports = function(grunt) {
                 // include: ["main"]
             }
           }
-        }
+        },
 
+        karma: {
+          options: {
+            basePath: process.cwd(),
+            singleRun: true,
+            captureTimeout: 7000,
+            autoWatch: true,
+            logLevel: "ERROR",
+
+            reporters: ["dots", "coverage"],
+            browsers: ["PhantomJS"],
+
+            // Change this to the framework you want to use.
+            frameworks: ["mocha"],
+
+            plugins: [
+              "karma-jasmine",
+              "karma-mocha",
+              "karma-qunit",
+              "karma-phantomjs-launcher",
+              "karma-coverage"
+            ],
+
+            preprocessors: {
+              "app/**/*.js": "coverage"
+            },
+
+            coverageReporter: {
+              type: "lcov",
+              dir: "test/coverage"
+            },
+
+            files: [
+              // You can optionally remove this or swap out for a different expect.
+              "assets/bower/chai/chai.js",
+              "assets/bower/requirejs/require.js",
+              "test/runner.js",
+
+              { pattern: "app/**/*.*", included: false },
+              // Derives test framework from Karma configuration.
+              {
+                pattern: "test/<%= karma.options.frameworks[0] %>/**/*.spec.js",
+                included: false
+              },
+              { pattern: "vendor/**/*.js", included: false }
+            ]
+          },
+
+          // This creates a server that will automatically run your tests when you
+          // save a file and display results in the terminal.
+          daemon: {
+            options: {
+              singleRun: false
+            }
+          },
+
+          // This is useful for running the tests just once.
+          run: {
+            options: {
+              singleRun: true
+            }
+          }
+        }
     });
 
     grunt.loadNpmTasks("grunt-contrib-clean");
@@ -80,6 +144,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-handlebars");
     grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.loadNpmTasks("grunt-karma");
+    grunt.loadNpmTasks("grunt-karma-coveralls");
 
     grunt.registerTask('default', [
         'clean',
