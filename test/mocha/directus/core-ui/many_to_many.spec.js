1b9f1693c9b107aeccd90b98fbd8e433b26b3838
olov <olov@rngr.org>
2013-12-11 20:02:55 -0500

    test

--- test/mocha/directus/core-ui/many_to_many.spec.js
+++ test/mocha/directus/core-ui/many_to_many.spec.js
@@ -2,7 +2,23 @@ define(function(require) {
 
   "use strict";
 
-  var ManyToMany = require('core-ui/many_to_many');
+  var ManyToMany = require('core-ui/many_to_many'),
+      SchemaManager = require("schema/SchemaManager"),
+      EntriesModel = require("core/entries/EntriesModel"),
+      artistData = JSON.parse(require('text!test/assets/data/artists_rows_1.json'));
+
+  var table = SchemaManager.getTable('artists');
+
+  var artist = new EntriesModel(artistData, {
+    structure: table.columns,
+    table: table,
+    parse: true
+  });
+
+  var options = {
+    model: artist,
+    name: 'albums_performer'
+  }
 
   describe("core-ui/many_to_many", function() {
 
@@ -17,6 +33,7 @@ define(function(require) {
       expect(ManyToMany.Input).to.be.a('function');
     });
 
+    var view = new ManyToMany.Input(options);
 
   });
 
