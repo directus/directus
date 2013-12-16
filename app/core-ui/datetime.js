561ceb3bc47a23c1f76f321b0188307d3414a568
olov <olov@rngr.org>
2013-12-16 11:45:42 -0500

    datetime

--- app/core-ui/datetime.js
+++ app/core-ui/datetime.js
@@ -1,21 +1,4 @@
-//  Datetime core UI component
-//  Directus 6.0
-
-//  (c) RANGER
-//  Directus may be freely distributed under the GNU license.
-//  For all details and documentation:
-//  http://www.getdirectus.com
-
-// Attribute          Type              Contains                                Example
-// -------------------------------------------------------------------------------------------------------------------------------------
-// options.schema     Backbone.Model    Structure/Schema for this table row     options.schema.get('type') [column_name, comment, type]
-// options.model      Backbone.Model    Data/Model for this table row           options.model.get('id') [any column in current table row]
-// options.value      String            Value for this field
-// options.settings   Backbone.Model    Saved values for current UI options     options.settings.get('length') [any key from this UI options]
-// options.name       String            Field name
-
-
-define(['app', 'backbone'], function(app, Backbone) {
+define(['app', 'backbone', 'moment', 'core/UIView'], function(app, Backbone, moment, UIView) {
 
   "use strict";
 
@@ -36,7 +19,7 @@ define(['app', 'backbone'], function(app, Backbone) {
                   input.date { \
                     display: inline; \
                     display: -webkit-inline-flex; \
-                    width: 140px; \
+                    width: 150px; \
                     padding-right: 4px; \
                     margin-right: 5px; \
                   } \
@@ -54,127 +37,88 @@ define(['app', 'backbone'], function(app, Backbone) {
                     \
                   } \
                   </style> \
-                  <input type="date" class="date" value="{{valueDate}}""> \
-                  <input type="time" class="time{{#if includeSeconds}} seconds{{/if}}" value="{{valueTime}}"> \
+                  <input type="date" class="date" {{#if hasDate}}value="{{valueDate}}"{{/if}}> \
+                  <input type="time" class="time{{#if includeSeconds}} seconds{{/if}}" {{#if hasDate}}value="{{valueTime}}"{{/if}}> \
                   <a class="now">Now</a> \
-                  <input class="merged" type="hidden" value="{{value}}" name="{{name}}" id="{{name}}">';
+                  <input class="merged" type="hidden" {{#if hasDate}}value="{{valueMerged}}"{{/if}} name="{{name}}" id="{{name}}">';
 
-  Module.Input = Backbone.Layout.extend({
+  //var format = 'ddd, DD MMM YYYY HH:mm:ss';
+  var format = 'YYYY-MM-DD HH:mm';
+
+  Module.Input = UIView.extend({
 
     tagName: 'fieldset',
 
     template: Handlebars.compile(template),
 
     events: {
-      'blur  input': 'updateValue',
-      'click .now': 'makeNow'
-    },
-
-    updateValue: function(e) {
-      var date = null;
-      var candidate = this.$el.find('input[type=date]').val() + ' ' +
-                      this.$el.find('input[type=time]').val();
-
-      try {
-        date = new Date(candidate);
-      } catch (err) {
-        // Do nothing if the date is bad
-        return;
-      }
-
-      this.value = date;
-      this.render();
-      this.$('input[type=hidden]').trigger('change');
+      'blur  input.date':   'updateValue',
+      'blur  input.time':   'updateValue',
+      'change  input.date': 'updateValue',
+      'change  input.time': 'updateValue',
+      'click .now':         'makeNow'
     },
 
     makeNow: function() {
-      this.value = new Date();
+      this.value = moment();
       this.render();
-      this.$('input[type=hidden]').trigger('change');
     },
 
+    updateValue: function() {
+      var time = this.$('input[type=time]').val();
+      var date = this.$('input[type=date]').val();
+      var val = date + ' ' + time;
+
+      if (moment(val).isValid()) {
+        this.$('#'+this.name).val(val.format(format));
+      } else {
+        this.$('#'+this.name).val('');
+      }
+    },
+
+    // The HTML5 date tag accepts RFC3339
+    // YYYY-MM-DD
+    // ---
+    // The HTML5 time tag acceps RFC3339:
+    // 17:39:57
     serialize: function() {
-      var value = this.value;
-      var data = {
-        value: null,
-        valueDate: null,
-        valueTime: null,
-        name: this.options.name,
-        note: this.options.schema.get('comment')
-      };
+      var data = {};
+      var date = this.value;
 
-      var nullValue = (this.value === undefined || this.value == 'Invalid Date' || this.value === null);
+      data.hasDate = this.value.isValid();
 
-      if (nullValue && this.autoPopulateWhenHiddenAndNull) {
-        this.makeNow();
-        this.model.set(this.options.name, this.value);
-      }
-
-
-      if (!nullValue) {
-
-        // Don't show corrupted dates
-        try {
-          value = new Date(value);
-          if (_.isNaN(value.getTime())) {
-            throw new Error();
-          }
-        } catch (e) {
-          return data;
-        }
-
-        var date = [
-          value.getFullYear(),
-          ('0'+(value.getMonth()+1)).slice(-2),
-          ('0'+value.getDate()).slice(-2)
-        ];
-        data.valueDate = date.join('-');
-
-        var time = [
-          ('0'+value.getHours()).slice(-2),
-          ('0'+value.getMinutes()).slice(-2)
-        ];
-
-        if (this.includeSeconds) {
-          time.push(('0'+value.getSeconds()).slice(-2));
-        }
-        data.valueTime = time.join(':');
-
-        // To guard against presumptive locale adjustments which are made by Daee.toISOString,
-        // render the zulu format manually. See directus6 issue #289:
-        // https://github.com/RNGR/directus6/issues/289
-        var zuluDate = data.valueDate + 'T' + data.valueTime + ':00.000Z';
-
-        data.value = zuluDate;
-      }
+      data.valueDate = date.format('YYYY-MM-DD');
+      data.valueTime = date.format('HH:mm');
+      data.valueMerged = date.format(format);
+      data.name = this.name;
+      data.note = this.columnSchema.get('comment');
 
       return data;
     },
 
     initialize: function(options) {
-      this.value = options.value;
-      this.includeSeconds = (options.settings && options.settings.has('include_seconds') && options.settings.get('include_seconds') == '1');
-      this.autoPopulateWhenHiddenAndNull = (options.settings && options.settings.has('auto-populate_when_hidden_and_null') && options.settings.get('auto-populate_when_hidden_and_null') == '1');
+      this.value = moment(this.model.get(this.name));
     }
 
   });
 
-  Module.validate = function(value,options) {
-    if (!_.isDate(value)) {
-      value = new Date(value);
-    }
-    if (!_.isNaN(value.getTime())) {
+  Module.validate = function(value, options) {
+    var m = moment(value);
+
+    console.log(value);
+
+    if (m.isValid() || value === '' || value === null || value === undefined) {
+      console.log(value === '');
       return;
     }
+
+    console.log(value);
+
     return "Not a valid date";
   };
 
   //@todo make contextual date a ui
   Module.list = function(options) {
-    if (options.settings.get('contextual_date_in_listview') == '1') {
-      var template = Handlebars.compile('{{contextualDate date}}');
-      return template({date: options.value});
-    }
     return options.value;
   };
 
