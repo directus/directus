05cf4d2be8c8533b3b30efa1f550e44b9e56ccc7
olov <olov@rngr.org>
2013-12-16 14:07:47 -0500

    bugfix for usertable

--- app/modules/users/users.js
+++ app/modules/users/users.js
@@ -106,7 +106,7 @@ function(app, Backbone, Directus, SaveModule) {
       var user = app.getCurrentUser();
       var userGroup = user.get('group');
 
-      if (!(parseInt(id) === user.id || userGroup === 0)) {
+      if (!(parseInt(id) === user.id || userGroup.id === 0)) {
         return;
       }
 
