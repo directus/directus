0b03f06ee26c525a6ec2909e6fad64dc0ca6d6d9
Daniel Bickett <dbickett@gmail.com>
2013-12-19 19:23:30 -0500

    composer.lock is tracked now, WIP mailer + configuration change
    req'd to pull

---
+++ api/configuration.example.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * High priority use case, not super planned out yet.
+ * This will be useful in the future as we do a better job organizing our application configuration.
+ * We need to distinguish between configuration and application constants.
+ */
+
+return array(
+
+	// Define this to send emails e.g. forgot password
+	'SMTP' => array(
+		'host' => '',
+		'port' => 25,
+		'username' => '',
+		'password' => ''
+	)
+
+);
