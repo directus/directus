1006617a11bcfb0dcd7d6cf3828e47108c166eea
Stephen Dotz <stephen.dotz@soul-cycle.com>
2014-01-10 18:30:56 -0500

    Jira: SOULSITE-322 Scalability & Caching Tweaks Add memcache provider, update config sample

---
+++ api/core/Directus/MemcacheProvider.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * User: sdotz
+ * Date: 1/9/14
+ * Time: 10:46 AM
+ */
+
+namespace Directus;
+
+use \Memcache;
+
+class MemcacheProvider {
+
+    const MEMCACHED_SERVER_CLOUD_1 = '166.78.77.0';
+    const MEMCACHED_SERVER_CLOUD_2 = '166.78.77.1';
+    const MEMCACHED_SERVER_CLOUD_3 = '166.78.77.2';
+    const DISTRIBUTED = false;
+    const COMPRESSION_FLAG = MEMCACHE_COMPRESSED;
+
+
+    private $mc;
+
+    public function __construct(){
+        if (extension_loaded('memcache')){
+            $this->mc = new Memcache();
+            if (!self::DISTRIBUTED){
+                $this->mc->addServer(MEMCACHED_SERVER, 11211);
+            }
+            else {
+                $this->mc->addServer(SELF::MEMCACHED_SERVER_CLOUD_1, 11211);
+                $this->mc->addServer(SELF::MEMCACHED_SERVER_CLOUD_2, 11211);
+                $this->mc->addServer(SELF::MEMCACHED_SERVER_CLOUD_3, 11211);
+            }
+            //$this->memcache->setCompressThreshold(100000);
+        }
+        else {
+            $this->mc = false;
+        }
+    }
+
+    public function set($key, $val, $compressionFlag,  $expire){
+        if ($this->mc) {
+            $setSuccess = $this->mc->set($key, $val, $compressionFlag,  $expire);
+            return $setSuccess;
+        }
+    }
+
+    public function get($key){
+        if ($this->mc) {
+            $cacheReturn = $this->mc->get($key);
+            return $cacheReturn;
+        }
+
+    }
+
+    public function getOrCache($key, $functionReturningVal, $expire = 300){
+        if ($this->mc){
+            $key = MEMCACHED_ENV_NAMESPACE . '/' . $key;
+            $cacheReturn = $this->get($key);
+            if (!$cacheReturn){
+                $val = $functionReturningVal();
+                $this->set($key, $val, self::COMPRESSION_FLAG, $expire);
+                return $val;
+            }
+            return $cacheReturn;
+        }
+    }
+
+    public function delete($key){
+        if ($this->mc){
+            $key = MEMCACHED_ENV_NAMESPACE . '/' . $key;
+            return $this->mc->delete($key);
+        }
+        return false;
+    }
+
+    public function flush(){
+        if ($this->mc){
+            return $this->mc->flush();
+        }
+        return false;
+    }
+
+    //Key returning functions
+    public static function getKeyWebappRoomMap($classId){
+        return "class_reservations_arranged_by_seat_id?class_id=$classId";
+    }
+    public static function getKeyBookerRoomMap($classId){
+        return "manager_room_map_data?class_id=$classId";
+    }
+    public static function getKeyShopCategories($categories){
+        return "shop/categories/$categories";
+    }
+    public static function getKeyShopIndex(){
+        return "shop";
+    }
+    public static function getKeyInstructors(){
+        return "instructors";
+    }
+    public static function getKeyStudios($regionId){
+        return "studios?region_id=$regionId";
+    }
+    public static function getKeyCommunityIndex(){
+        return 'community';
+    }
+    public static function getKeyRegionsById(){
+        return 'regions_by_id';
+    }
+    public static function getKeySubRegionsByRegion(){
+        return 'sub_regions_by_region';
+    }
+    public static function getKeyCommunityCategories(){
+        return 'community_categories';
+    }
+    public static function getKeyStudiosByRegion(){
+        return 'studios_by_region';
+    }
+}
