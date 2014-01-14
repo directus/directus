624f24e79ea8da54f152a22452c67a5fe88cbe63
Daniel Bickett <dbickett@gmail.com>
2014-01-14 12:10:46 -0500

    directus pdo, use buffered query

--- api/core/Directus/Bootstrap.php
+++ api/core/Directus/Bootstrap.php
@@ -124,7 +124,10 @@ class Bootstrap {
             'username'  => DB_USER,
             'password'  => DB_PASSWORD,
             'charset'   => 'utf8',
-            'driver_options'   => array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8; SET CHARACTER SET utf8;")
+            'driver_options'   => array(
+                \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8; SET CHARACTER SET utf8;"
+                \PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
+            )
         );
         $db = new \Zend\Db\Adapter\Adapter($dbConfig);
         return $db;
