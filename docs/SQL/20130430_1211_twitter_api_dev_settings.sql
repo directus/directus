1a05e1ad1c82ee0c83a5f5ea25ff860b79e657f8
Daniel Bickett <dbickett@gmail.com>
2014-03-12 14:14:49 -0400

    remove non-generic SQL

--- docs/SQL/20130430_1211_twitter_api_dev_settings.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `directus_settings` (`collection`, `name`, `value`)
-VALUES
-	('social', 'twitter_consumer_key', 'bkAL9M5e79yg1hoEN1Hk8g'),
-	('social', 'twitter_consumer_secret', '3ADyiBVmEBkzW5cZJ2zfJeLWAjnIvT4jSug7YSGL1A'),
-	('social', 'twitter_oauth_token', '292554761-xs2dXV9HzpHx7qkwryWTR4dDmeJ7e2QYD4IsDIM8'),
-	('social', 'twitter_oauth_token_secret', 'pwdsWL593rqO2FXQm7vgJ5sG0gVDuTxt7WLBZuc8KA');
