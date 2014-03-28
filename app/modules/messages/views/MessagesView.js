08c04143591e54ad75f217010bf67fbb0770af23
Jason El-Massih <jason@rngr.org>
2014-03-28 17:02:17 -0400

    Added ListView Back to MessagesView

--- app/modules/messages/views/MessagesView.js
+++ app/modules/messages/views/MessagesView.js
@@ -6,6 +6,44 @@ define([
 
 function(app, Backbone, BasePageView) {
 
+  var ListView = Backbone.Layout.extend({
+
+    template: 'messages-list',
+
+    events: {
+      'click tr': function(e) {
+        var id = $(e.target).closest('tr').attr('data-id');
+        app.router.go('#messages', id);
+      }
+    },
+
+    serialize: function() {
+      var data = this.collection.map(function(model) {
+        var data = model.toJSON();
+        var momentDate = moment(data.date_updated);
+        data.timestamp = parseInt(momentDate.format('X'), 10);
+        data.niceDate = momentDate.fromNow();
+        data.read = model.getUnreadCount() === 0;
+        data.responsesLength = data.responses.length;
+        data.from = parseInt(data.from, 10);
+        //console.log(_.map(data.responses, 'from'));
+        return data;
+      });
+
+      // Order the data by timestamp
+      data = _.sortBy(data, function(item) {
+        return -item.timestamp;
+      });
+
+      return {messages: data};
+    },
+
+    initialize: function() {
+      this.collection.on('sync', this.render, this);
+    }
+
+  });
+
   return BasePageView.extend({
 
     events: {
