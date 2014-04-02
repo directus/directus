e3e9d5b85f166be412dcdbdee71e8eff3baec528
Jason El-Massih <jason@rngr.org>
2014-04-02 17:04:38 -0400

    Added SlectionAction Actions to actually do stuff

--- app/core/widgets/SelectionActionWidget.js
+++ app/core/widgets/SelectionActionWidget.js
@@ -9,9 +9,9 @@ function(app, Backbone) {
   return Backbone.Layout.extend({
     template: Handlebars.compile(' \
       <ul class="tools left big-space"> \
-        <li class="tool"><span class="action">Active</span></li> \
-        <li class="tool"><span class="action inactive">Draft</span></li> \
-        <li class="tool div-right"><span class="action delete">Delete</span></li> \
+        <li class="tool"><span data-value="1" class="action">Active</span></li> \
+        <li class="tool"><span data-value="2" class="action inactive">Draft</span></li> \
+        <li class="tool div-right"><span data-value="0" class="action delete">Delete</span></li> \
         {{#if batchEdit}} \
         <li class="tool"><span class="action">Batch Edit</span></li> \
         {{/if}} \
@@ -21,6 +21,33 @@ function(app, Backbone) {
     tagName: 'li',
     attributes: {
       'class': 'input-and-button'
+    },
+
+    events: {
+      'click .action': function(e) {
+        var value = $(e.target).closest('span').attr('data-value');
+        console.log(value);
+
+        var collection = this.collection;
+
+        var $checked = $('.select-row:checked');
+        var expectedResponses = $checked.length;
+
+        var success = function() {
+          expectedResponses--;
+          if (expectedResponses === 0) {
+            collection.trigger('visibility');
+            collection.trigger('select');
+          }
+        };
+
+        $checked.each(function() {
+          var id = this.value;
+          console.log(id);
+          var model = collection.get(id);
+          model.save({active: value}, {silent: true, patch:true, validate:false, success: success});
+        });
+      }
     },
 
     serialize: function() {
