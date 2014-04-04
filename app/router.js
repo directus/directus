cc683c51fe369cf2ddf91c89e9c1e22031c5f00b
Jason El-Massih <jason@rngr.org>
2014-04-04 11:48:44 -0400

    Fixed Route Bug

--- app/router.js
+++ app/router.js
@@ -106,6 +106,8 @@ define(function(require, exports, module) {
       if (_.contains(this.tabBlacklist,'tables'))
         return this.notFound();
 
+      this.navigate('/tables'); //If going to / rewrite to tables
+
       this.setTitle('Tables');
       this.tabs.setActive('tables');
       this.v.main.setView('#content', new Table.Views.Tables({collection: SchemaManager.getTables()}));
