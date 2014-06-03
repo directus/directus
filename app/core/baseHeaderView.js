b3cf133fc31e4fe3f7827ec8cc270bca1503a6d9
Jason El-Massih <jason@rngr.org>
2014-06-03 19:22:37 -0400

    Added Dynamic class if tools in secondary toobarr

--- app/core/baseHeaderView.js
+++ app/core/baseHeaderView.js
@@ -68,6 +68,11 @@ function(app, Backbone) {
     afterRender: function() {
       $(window).bind('resize.app', _.bind(this.setMarginToHeaderHeight, this));
 
+      var secondaryToolbarWidgetCount = this.$el.find('#tools-secondary-left-insert').children().length + this.$el.find('#tools-secondary-right-insert').children().length;
+      if(secondaryToolbarWidgetCount > 0) {
+        this.$el.find('.secondary-row').addClass('has-tools');
+      }
+
       this.setMarginToHeaderHeight();
     },
 
