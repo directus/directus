1387e68a5d7624fe56180d5125719dc6b83e6240
Jason El-Massih <jason@rngr.org>
2014-06-13 12:20:11 -0400

    Updated installation file to  use new media naming.

--- installation/query.php
+++ installation/query.php
@@ -179,17 +179,17 @@ VALUES
   (4,'global','site_url','http://examplesite.dev/'),
   (5,'global','cms_color','#7ac943'),
   (6,'global','rows_per_page','200'),
-  (7,'media','storage_adapter','FileSystemAdapter'),
-  (8,'media','storage_destination',''),
-  (9,'media','thumbnail_storage_adapter','FileSystemAdapter'),
-  (10,'media','thumbnail_storage_destination',''),
-  (11,'media','allowed_thumbnails',''),
-  (12,'media','thumbnail_quality','100'),
-  (13,'media','thumbnail_size','200'),
+  (7,'files','storage_adapter','FileSystemAdapter'),
+  (8,'files','storage_destination',''),
+  (9,'fiels','thumbnail_storage_adapter','FileSystemAdapter'),
+  (10,'files','thumbnail_storage_destination',''),
+  (11,'files','allowed_thumbnails',''),
+  (12,'files','thumbnail_quality','100'),
+  (13,'files','thumbnail_size','200'),
   (14,'global','cms_thumbnail_url',''),
-  (15,'media','media_file_naming','media_id'),
-  (16,'media','media_title_naming','file_name'),
-  (17,'media','thumbnail_crop_enabled','1');";
+  (15,'files','file_file_naming','file_id'),
+  (16,'files','file_title_naming','file_name'),
+  (17,'files','thumbnail_crop_enabled','1');";
 
   $create_social_feeds = "CREATE TABLE `directus_social_feeds` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -332,7 +332,7 @@ function AddStorageAdapters($mysqli)
   $tempu = $_SESSION['temp_url'];
   $insert = "INSERT INTO `directus_storage_adapters` (`id`, `key`, `adapter_name`, `role`, `public`, `destination`, `url`, `params`)
   VALUES
-    (1, 'media', 'FileSystemAdapter', 'DEFAULT', 1, '$dd', '$du', NULL),
+    (1, 'files', 'FileSystemAdapter', 'DEFAULT', 1, '$dd', '$du', NULL),
     (2, 'thumbnails', 'FileSystemAdapter', 'THUMBNAIL', 1, '$td', '$tu', NULL),
     (3, 'temp', 'FileSystemAdapter', 'TEMP', 1, '$tempd', '$tempu', NULL);";
 
