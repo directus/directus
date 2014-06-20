b81d8526b966b44f89a43a5007a01f26c2e27a39
Jason El-Massih <jason@rngr.org>
2014-06-20 17:19:30 -0400

    Disable Float Cast

--- api/core/Directus/Db/TableGateway/RelationalTableGateway.php
+++ api/core/Directus/Db/TableGateway/RelationalTableGateway.php
@@ -742,10 +742,13 @@ class RelationalTableGateway extends AclAwareTableGateway {
         $rowset = $TableGateway->selectWith($select);
         $results = $rowset->toArray();
 
+
+        //Disableing Float Cast because WOuld Sometimes break if detecting infinite number
+        /*
         // Process results
         foreach ($results as &$row) {
             array_walk($row, array($this, 'castFloatIfNumeric'));
-        }
+        }*/
 
         return array('rows' => $results);
     }
