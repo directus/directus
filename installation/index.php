dde8224314ecaf4fc11f17fb29c1f7e49faaf738
Ben Haynes <ben@rngr.org>
2014-07-02 18:04:19 -0400

    install files – merge utility and design

--- installation/index.php
+++ installation/index.php
@@ -5,9 +5,13 @@ session_start();
 if(isset($_SESSION['step'])) {
   $step = $_SESSION['step'];
 } else {
-  $step = 1;
+  $step = 0;
 }
 
+if($step == 0 && isset($_POST['start'])) {
+  $_SESSION['step'] = 1;
+  $step = 1;
+}
 
 if($step == 1 && isset($_POST['email']) && isset($_POST['site_name']) && isset($_POST['password']) && isset($_POST['password_confirm'])) {
   if(!empty($_POST['email']) && !empty($_POST['site_name']) && !empty($_POST['password']) && !empty($_POST['directus_path'])) {
@@ -55,185 +59,236 @@ if($step == 3 && isset($_POST['default_dest'])) {
     $step = 4;
   }
 }
+?><!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+  <title>Install Directus</title>
+  <meta name="description" content="Directus">
+  <meta name="author" content="RANGER Studio LLC">
+
+  <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="install.css?v=1.0">
+</head>
+<body>
+  <form name="input" action="index.php" method="post">
+    <!--
+      <div>Default Adapter Destination</div>
+      <input class="bad" value="">
+    -->
+
+
+<?php
+if($step == 0) {
+  ?>
+  <div class="header">
+    <div class="container">
+      <img src="directus-logo.gif">
+      <div>Install Directus</div>
+    </div>
+  </div>
+
+  <div class="body">
+    <div class="container intro">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tristique, tellus faucibus sodales placerat, sem tellus laoreet sapien, vel adipiscing orci erat id libero.
+      Aenean suscipit, lacus a faucibus interdum, ligula neque laoreet mi, quis elementum risus ante ac magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+      posuere cubilia Curae; Ut sagittis risus in tincidunt commodo. Phasellus quis orci ut justo fringilla gravida nec eu tortor. Vestibulum ante ipsum primis in faucibus orci
+      luctus et ultrices posuere cubilia Curae; Curabitur pharetra a orci ac mattis.
+      <input type="hidden" name="start" value="true">
+      <button type="submit" class="button primary">Get Started</button>
+  <?php
+}
 
 if($step == 1) {
-  echo'<h1>Requirements</h1>';
-  if (version_compare(PHP_VERSION, '5.5.0', '<'))
-  {
+  ?>
+  <div class="header">
+    <div class="container">
+      <img src="directus-logo.gif">
+      <div>Project Info</div>
+    </div>
+  </div>
+
+  <div class="body">
+    <div class="container">
+  <?php
+  if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     die('Your host needs to use PHP 5.5.0 or higher to run this version of Directus!');
-  } else {
-    echo 'Correct PHP Version Installed<br><br>';
   }
 
   if (!defined('PDO::ATTR_DRIVER_NAME')) {
     die('Your host needs to have PDO enabled to run this version of Directus!');
-  } else {
-    echo 'PDO Installed! <br><br>';
   }
 
   if (!extension_loaded('gd') || !function_exists('gd_info')) {
     die('Your host needs to have GD Library enabled to run this version of Directus!');
-  } else {
-    echo 'GD LIbrary Installed! <br><br>';
   }
 
-  echo'<h1>Optional</h1>';
-  if(extension_loaded('imagick')) {
-    echo 'Imagick Installed<br><br>';
-  } else {
-    echo 'Imagick Not Installed (No Tif/PSD/PDF thumbnail support)<br><br>';
-  }
   ?>
-  <form name="input" action="index.php" method="post">
-  Site Name: <input type="text" name="site_name"><br>
-  Site Path: <input type="text" value="/directus/" name="directus_path"><br>
-  Email: <input type="email" name="email"><br>
-  Password: <input type="password" name="password"><br>
-  Password Confirm: <input type="password" name="password_confirm"><br>
-  <input type="submit" value="Submit">
-  </form>
+  Project Name<input type="text" name="site_name"><br>
+  Project Path<input type="text" value="/directus/" name="directus_path"><br>
+  Admin Email<input type="email" name="email"><br>
+  Admin Password<input type="password" name="password"><br>
+  Confirm Admin Password<input type="password" name="password_confirm"><br>
+
 <?php
+  if(extension_loaded('imagick')) {
+    //echo '<span class="note">Imagick Already Installed</span>';
+  } else {
+    echo '<span class="note">*Install Imagick to support TIFF, PSD, and PDF thumbnails</span>';
+  }
 }
 
 if($step == 2) {
-  echo'<h1>Database Configuration</h1>';
 ?>
+  <div class="header">
+    <div class="container">
+      <img src="directus-logo.gif">
+      <div>Database Configuration</div>
+    </div>
+  </div>
 
-<form name="input" action="index.php" method="post">
-  Host Name: <input type="text" name="host_name"><br>
-  username: <input type="text" name="username"><br>
-  Password: <input type="password" name="password"><br>
-  Database Name: <input type="text" name="db_name"><br>
-  DB Prefix: <input type="text" name="db_prefix"><br>
-  <input type="submit" value="Submit">
-</form>
-
+  <div class="body">
+    <div class="container">
+        Host Name<input type="text" name="host_name"><br>
+        Username<input type="text" name="username"><br>
+        Password<input type="password" name="password"><br>
+        Database Name<input type="text" name="db_name"><br>
+        Database Prefix<input type="text" name="db_prefix"><br>
 <?php
 }
 
 if($step == 4) {
 ?>
+  <div class="header">
+    <div class="container">
+      <img src="directus-logo.gif">
+      <div>Confirmation</div>
+    </div>
+  </div>
 
-<form name="input" action="index.php" method="post">
-  Install Sample Data:<br>
-  <input type="radio" name="install_sample" value="no">No<br>
-  <input type="radio" name="install_sample" value="yes">Yes
-  <br><br>
-  Email This Configuration To <?php echo $_SESSION['email'];?>: <input type="checkbox" name="send_config_email"><br>
+  <div class="body">
+    <div class="container">
+      Install Sample Data:<br>
+      <input type="radio" name="install_sample" value="no">No<br>
+      <input type="radio" name="install_sample" value="yes">Yes
+      <br><br>
+      Email This Configuration To <?php echo $_SESSION['email'];?>: <input type="checkbox" name="send_config_email"><br>
 
-<h3>Main Configuration</h3>
-<hr>
-<table>
-  <tbody>
-    <tr>
-      <td class="item">Site Name</td>
-      <td><?php echo $_SESSION['site_name'];?></td>
-    </tr>
-    <tr>
-      <td class="item">Admin Email</td>
-      <td><span><?php echo $_SESSION['email'];?></span>
-      </td>
-    </tr>
-    <tr>
-      <td class="item">Admin Password</td>
-      <td>***</td>
-    </tr>
-  </tbody>
-</table>
+      <h3>Main Configuration</h3>
+      <hr>
+      <table>
+        <tbody>
+          <tr>
+            <td class="item">Site Name</td>
+            <td><?php echo $_SESSION['site_name'];?></td>
+          </tr>
+          <tr>
+            <td class="item">Admin Email</td>
+            <td><span><?php echo $_SESSION['email'];?></span>
+            </td>
+          </tr>
+          <tr>
+            <td class="item">Admin Password</td>
+            <td>***</td>
+          </tr>
+        </tbody>
+      </table>
 
-<h3>Database Configuration</h3>
-<hr>
-<table>
-  <tbody>
-    <tr>
-      <td class="item">Host Name</td>
-      <td><?php echo $_SESSION['host_name'];?></td>
-    </tr>
-    <tr>
-      <td class="item">Username</td>
-      <td><span><?php echo $_SESSION['username'];?></span></td>
-    </tr>
-    <tr>
-      <td class="item">Password</td>
-      <td>***</td>
-    </tr>
-    <tr>
-      <td class="item">Database Name</td>
-      <td><?php echo $_SESSION['db_name'];?></td>
-    </tr>
-    <tr>
-      <td class="item">Database Prefix</td>
-      <td><?php echo $_SESSION['db_prefix'];?></td>
-    </tr>
-  </tbody>
-</table>
+      <h3>Database Configuration</h3>
+      <hr>
+      <table>
+        <tbody>
+          <tr>
+            <td class="item">Host Name</td>
+            <td><?php echo $_SESSION['host_name'];?></td>
+          </tr>
+          <tr>
+            <td class="item">Username</td>
+            <td><span><?php echo $_SESSION['username'];?></span></td>
+          </tr>
+          <tr>
+            <td class="item">Password</td>
+            <td>***</td>
+          </tr>
+          <tr>
+            <td class="item">Database Name</td>
+            <td><?php echo $_SESSION['db_name'];?></td>
+          </tr>
+          <tr>
+            <td class="item">Database Prefix</td>
+            <td><?php echo $_SESSION['db_prefix'];?></td>
+          </tr>
+        </tbody>
+      </table>
 
-<h3>Pre-Installation Check</h3>
-<hr>
-<table>
-  <tbody>
-    <tr>
-      <td class="item">PHP Version >= 5.5.0</td>
-      <td><span class="label label-success">Yes</span></td>
-    </tr>
-    <tr>
-      <td class="item">Database Support</td>
-      <td><span class="label label-success">Yes</span></td>
-    </tr>
-    <tr>
-      <td class="item">GD Support</td>
-      <td><span class="label label-success">Yes</span></td>
-    </tr>
-    <tr>
-      <td class="item">config.php Writable</td>
-      <td><?php if(is_writable('../api/config.php')) {echo('<span class="label label-success">Yes</span>');}else{echo('<span class="label label-important">No</span> Please make ../api/config.php writable');}?></td>
-    </tr>
-    <tr>
-      <td class="item">configuration.php Writable</td>
-      <td><?php if(is_writable('../api/configuration.php')) {echo('<span class="label label-success">Yes</span>');}else{echo('<span class="label label-important">No</span> Please make ../api/configuration.php writable');}?></td>
-    </tr>
-    <tr>
-      <td class="item">Logs Writable</td>
-      <td><?php if(is_writable('../api/logs/1.txt')) {echo('<span class="label label-success">Yes</span>');}else{echo('<span class="label label-important">No</span> Please Make ../api/logs/ Writable');}?></td>
-    </tr>
-    <tr>
-      <td class="item">mod_rewrite Enabled</td>
-      <td><?php if(in_array('mod_rewrite', apache_get_modules())) {echo('<span class="label label-success">Yes</span>');}else{echo('<span class="label label-important">No</span> Please Make Enable mod_rewrite');}?></td>
-    </tr>
-  </tbody>
-</table>
+      <h3>Pre-Installation Check</h3>
+      <hr>
+      <table>
+        <tbody>
+          <tr>
+            <td class="item">PHP Version >= 5.5.0</td>
+            <td><span class="label label-success">Yes</span></td>
+          </tr>
+          <tr>
+            <td class="item">Database Support</td>
+            <td><span class="label label-success">Yes</span></td>
+          </tr>
+          <tr>
+            <td class="item">GD Support</td>
+            <td><span class="label label-success">Yes</span></td>
+          </tr>
+          <tr>
+            <td class="item">config.php Writable</td>
+            <td><?php if(is_writable('../api/config.php')) {echo('<span class="label label-success">Yes</span>');}else{echo('<span class="label label-important">No</span> Please make ../api/config.php writable');}?></td>
+          </tr>
+          <tr>
+            <td class="item">configuration.php Writable</td>
+            <td><?php if(is_writable('../api/configuration.php')) {echo('<span class="label label-success">Yes</span>');}else{echo('<span class="label label-important">No</span> Please make ../api/configuration.php writable');}?></td>
+          </tr>
+          <tr>
+            <td class="item">Logs Writable</td>
+            <td><?php if(is_writable('../api/logs/1.txt')) {echo('<span class="label label-success">Yes</span>');}else{echo('<span class="label label-important">No</span> Please Make ../api/logs/ Writable');}?></td>
+          </tr>
+          <tr>
+            <td class="item">mod_rewrite Enabled</td>
+            <td><?php if(in_array('mod_rewrite', apache_get_modules())) {echo('<span class="label label-success">Yes</span>');}else{echo('<span class="label label-important">No</span> Please Make Enable mod_rewrite');}?></td>
+          </tr>
+        </tbody>
+      </table>
 
-<h3>Reccommended Optional Features</h3>
-<hr>
-<table>
-  <tbody>
-    <tr>
-      <td class="item">Imagick PHP Extension</td>
-      <td><?php if(extension_loaded('imagick')) {echo('<span class="label label-success">Yes</span>');} else {echo('<span class="label label-important">No</span>');}?></td>
-    </tr>
-  </tbody>
-</table>
-  <input type="submit" value="Install">
-</form>
+      <h3>Reccommended Optional Features</h3>
+      <hr>
+      <table>
+        <tbody>
+          <tr>
+            <td class="item">Imagick PHP Extension</td>
+            <td><?php if(extension_loaded('imagick')) {echo('<span class="label label-success">Yes</span>');} else {echo('<span class="label label-important">No</span>');}?></td>
+          </tr>
+        </tbody>
+      </table>
 
 <?php
 }
 
 if($step == 3) {
-
   ?>
-<h1>Storage Adapter Setup</h1>
-
-<form name="input" action="index.php" method="post">
-  Default Adapter Destination: <input type="text" name="default_dest" value="/var/www/media/"><br>
-  Default Adapter URL: <input type="text" name="default_url" value="http://localhost/media/"><br>
-  Thumbnail Adapter Destination: <input type="text" name="thumb_dest" value="/var/www/media/thumb/"><br>
-  Thumbnail Adapter URL: <input type="text" name="thumb_url" value="http://localhost/media/thumb/"><br>
-  Temp Adapter Destination: <input type="text" name="temp_dest" value="/var/www/media/temp/"><br>
-  Temp Adapter URL: <input type="text" name="temp_url" value="http://localhost/media/temp/"><br>
-  <input type="submit" value="Submit">
-</form>
-
+  <div class="header">
+    <div class="container">
+      <img src="directus-logo.gif">
+      <div>Storage Adapter Setup</div>
+    </div>
+  </div>
+  <div class="body">
+    <div class="container">
+        Default Adapter Destination<input type="text" name="default_dest" value="/var/www/media/" placeholder="/var/www/media/"><br>
+        Default Adapter URL<input type="text" name="default_url" value="http://localhost/media/" placeholder="http://localhost/media/"><br>
+        Thumbnail Adapter Destination<input type="text" name="thumb_dest" value="/var/www/media/thumb/" placeholder="/var/www/media/thumb/"><br>
+        Thumbnail Adapter URL<input type="text" name="thumb_url" value="http://localhost/media/thumb/" placeholder="http://localhost/media/thumb/"><br>
+        Temp Adapter Destination<input type="text" name="temp_dest" value="/var/www/media/temp/" placeholder="/var/www/media/temp/"><br>
+        Temp Adapter URL<input type="text" name="temp_url" value="http://localhost/media/temp/" placeholder="http://localhost/media/temp/"><br>
 <?php
 }
 
@@ -256,16 +311,29 @@ if($step == 5) {
 
 ?>
 
+      </div>
+    </div>
 
+    <div class="footer">
+      <div class="container">
+        <button class="button left<?PHP if($step == 0){echo " hide";}?>">Back</button>
+        <!--<div class="button right<?PHP if($step == 0){echo " hide";}?> primary disabled">Continue</div>-->
+        <button type="submit" class="button right<?PHP if($step == 0){echo " hide";}?> primary disabled">Continue</button>
 
+        <div class="breadcrumb">
+          <span class="<?PHP if($step == 1){echo "current";} elseif($step > 1){echo "complete";}?>">Project Info</span>
+          <span class="separator icon icon-chevron-right"></span>
+          <span class="<?PHP if($step == 2){echo "current";} elseif($step > 2){echo "complete";}?>">Database</span>
+          <span class="separator icon icon-chevron-right"></span>
+          <span class="<?PHP if($step == 3){echo "current";} elseif($step > 3){echo "complete";}?>">Storage Adapters</span>
+          <span class="separator icon icon-chevron-right"></span>
+          <span class="<?PHP if($step == 4){echo "current";} elseif($step > 4){echo "complete";}?>">Confirmation</span>
+        </div>
+      </div>
+    </div>
+  </form>
 
-
-
-
-
-
-
-
-
-
-
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script type="text/javascript" src="install.js"></script>
+</body>
+</html>
