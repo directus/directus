78e55b218f8b508a496fb9db38d79402b7522333
Jason El-Massih <jason@rngr.org>
2014-08-15 17:05:50 -0400

    Fixed up settings table to refresh only when finished saving new table.

--- app/modules/settings/views/TablesView.js
+++ app/modules/settings/views/TablesView.js
@@ -659,13 +659,16 @@ function(app, Backbone, Directus, BasePageView, ColumnModel, UIManager, Widgets,
 
     events: {
       'click #addBtn': function() {
+        var that = this;
         app.router.openModal({type: 'prompt', text: 'Please Enter the name of the Table you would like to add.', callback: function(tableName) {
           if(tableName && !app.schemaManager.getPrivileges(tableName)) {
             var model = new Backbone.Model();
             model.url = app.API_URL + 'privileges/1';
             model.set({group_id: 1, permissions: 'add,edit,bigedit,delete,bigdelete,alter,view,bigview', table_name: tableName, addTable: true});
             model.save();
-            location.reload();
+            that.listenToOnce(model, 'sync', function() {
+              location.reload();
+            });
           }
         }});
       }
