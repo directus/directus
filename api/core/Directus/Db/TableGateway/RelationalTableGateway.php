a037a60b68c97cb5418388ea40cb19453b50e2a5
Welling Guzman <hola@wellingguzman.com>
2015-01-17 00:37:04 -0500

    Let sort this out

--- api/core/Directus/Db/TableGateway/RelationalTableGateway.php
+++ api/core/Directus/Db/TableGateway/RelationalTableGateway.php
@@ -125,6 +125,7 @@ class RelationalTableGateway extends AclAwareTableGateway {
                 $draftRecord[$colName] = $recordData[$colName];
             }
         }
+
         $draftRecord = $TableGateway->addOrUpdateToManyRelationships($schemaArray, $draftRecord, $nestedLogEntries, $nestedCollectionRelationshipsChanged);
         $rowId = $draftRecord['id'];
 
@@ -375,10 +376,23 @@ class RelationalTableGateway extends AclAwareTableGateway {
                               $Where = new Where;
                               $Where->equalTo($junctionKeyLeft, $parentRow['id'])
                                       ->equalTo($junctionKeyRight, $junctionRow['data']['id']);
-                              if ($JunctionTable->select($Where)->count()) {
-                                  continue;
+
+                              $junctionRowResult = $JunctionTable->select($Where);
+                              if ($junctionRowResult->count()) {
+                                $junctionRowResultArray = $junctionRowResult->toArray();
+
+                                if(array_key_exists('sort', $junctionRow) && array_key_exists('sort', $junctionRowResultArray)) {
+                                    
+                                    print_r($junctionRowResultArray);
+                                    if($junctionRowResultArray['sort'] == $junctionRow['sort']) {
+                                        continue;
+                                    }
+                                // hard-coded check for sort diff
+                                // @todo fix this
+                                }
                               }
                             }
+                            
                             /** Update foreign record */
                             $foreignRecord = $ForeignTable->manageRecordUpdate($foreignTableName, $junctionRow['data'], self::ACTIVITY_ENTRY_MODE_CHILD, $childLogEntries, $parentCollectionRelationshipsChanged);
                             // Junction/Association row
