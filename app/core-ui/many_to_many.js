f4971f74ab2e75d658d6732d2062046a728306d7
Ben Haynes <ben@rngr.org>
2015-01-21 10:44:58 -0500

    simplify m2m ui buttons

--- app/core-ui/many_to_many.js
+++ app/core-ui/many_to_many.js
@@ -39,8 +39,8 @@ define(['app', 'backbone', 'core-ui/one_to_many', 'core/table/table.view', 'core
 
     template: Handlebars.compile(
       '<div class="related-table"></div>' +
-      '<div class="btn-row">{{#if showAddButton}}<button class="btn btn-primary margin-right-small" data-action="add" type="button">Add New {{{capitalize tableTitle}}} Item</button>{{/if}}' +
-      '{{#if showChooseButton}}<button class="btn btn-primary" data-action="insert" type="button">Choose Existing {{{capitalize tableTitle}}} Item</button>{{/if}}</div>'),
+      '<div class="btn-row">{{#if showAddButton}}<button class="btn btn-primary margin-right-small" data-action="add" type="button">Add New</button>{{/if}}' +
+      '{{#if showChooseButton}}<button class="btn btn-primary" data-action="insert" type="button">Choose Existing</button>{{/if}}</div>'),
 
     addRow: function() {
       this.addModel(new this.relatedCollection.nestedCollection.model({}, {collection: this.relatedCollection.nestedCollection, parse: true}));
