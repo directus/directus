c8df95e74a93aaa1a7c67615216cfc3ace5c588e
Welling Guzman <hola@wellingguzman.com>
2015-05-22 23:38:32 -0400

    add String helper (init)

---
+++ api/core/Directus/Util/String.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Directus\Util;
+
+class String
+{
+    // Source: http://stackoverflow.com/a/10473026/1772076
+    /**
+     * Return whether or not a string start with a specific string
+     * @param  String  $haystack
+     * @param  String  $needle
+     * @return Boolean
+     */
+    function startsWith($haystack, $needle)
+    {
+        // search backwards starting from haystack length characters from the end
+        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+    }
+
+    /**
+     * Return whether or not a string end with a specific string
+     * @param  String  $haystack
+     * @param  String  $needle
+     * @return Boolean
+     */
+    function endsWith($haystack, $needle) {
+        // search forward starting from end minus needle length characters
+        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+    }
+}
