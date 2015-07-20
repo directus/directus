d0104343bee621a5fd17d90258b296a54ef0b32e
Ben Haynes <ben@rngr.org>
2015-07-20 14:23:35 -0400

    Login meta

--- login.php
+++ login.php
@@ -32,9 +32,27 @@ $cacheBuster = Directus\Util\Git::getCloneHash($git);
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no,maximum-scale=1.0">
-  <title>directus</title>
-  <!-- Application styles. -->
-  <link rel="shortcut icon" href="favicon.ico">
+  <title>Directus Login</title>
+
+  <!-- Icons -->
+  <link rel="apple-touch-icon" sizes="57x57" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= DIRECTUS_PATH ?>assets/img/icons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="<?= DIRECTUS_PATH ?>assets/img/icons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?= DIRECTUS_PATH ?>assets/img/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="<?= DIRECTUS_PATH ?>assets/img/icons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= DIRECTUS_PATH ?>assets/img/icons/favicon-16x16.png">
+  <link rel="manifest" href="<?= DIRECTUS_PATH ?>assets/img/icons/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
   <!-- build:css assets/css/directus.min.css -->
   <link rel="stylesheet" href="assets/css/directus.css">
