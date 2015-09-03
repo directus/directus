3cc284f81607991fdcd0ebd5eec713a263353e2c
Mick Rippon <me@mickrippon.com>
2015-09-04 00:39:51 +1000

    Slider widget now respects default value

--- app/core-ui/slider.js
+++ app/core-ui/slider.js
@@ -60,6 +60,11 @@ define(['app', 'backbone'], function(app, Backbone) {
     },
 
     serialize: function() {
+      
+      if (this.options.model.isNew() && this.options.schema.has('default_value')) {
+        this.options.value = this.options.schema.get('default_value');
+      }
+
       return {
         value: this.options.value || '0',
         name: this.options.name,
