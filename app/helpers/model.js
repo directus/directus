75f1ee84d6c37aaea5c5e1e8012d4e48ca9d71de
Welling Guzman <hola@wellingguzman.com>
2015-11-13 00:11:45 -0400

    fix #1071 - check if primary_column is set

--- app/helpers/model.js
+++ app/helpers/model.js
@@ -9,7 +9,7 @@ define(function(require, exports) {
       model = model.collection.junctionStructure;
     }
 
-    if (model.table && model.table.get('primary_column') != 'id') {
+    if (model.table && model.table.has('primary_column') && model.table.get('primary_column') != 'id') {
       model.idAttribute = model.table.get('primary_column');
       model.id = model.get(model.idAttribute);
       model.collection._byId[model.id] = model;
