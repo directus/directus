ddb54934de60ba0c6f3b176dc00d33ca4238d03c
Welling Guzman <hola@wellingguzman.com>
2015-12-29 13:46:42 -0400

    update arrayUtils codestyle

--- api/core/Directus/Util/ArrayUtils.php
+++ api/core/Directus/Util/ArrayUtils.php
@@ -2,15 +2,17 @@
 
 namespace Directus\Util;
 
-class ArrayUtils {
+class ArrayUtils
+{
     /**
      * Return a copy of the object, filtered to only have values for the whitelisted keys (or array of valid keys).
      * @param  AssocArray  $array
      * @param  Array       $keys
      * @return AssocArray
      */
-    public static function pick($array, $keys) {
-        $result = array();
+    public static function pick($array, $keys)
+    {
+        $result = [];
 
         foreach($array as $key => $value) {
             if (in_array($key, $keys)) {
