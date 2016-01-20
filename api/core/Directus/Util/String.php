7edb5853c3df92e455769c3dc64487a8ff69ab53
Welling Guzman <hola@wellingguzman.com>
2016-01-19 22:54:19 -0400

    wrong static position

--- api/core/Directus/Util/String.php
+++ api/core/Directus/Util/String.php
@@ -11,7 +11,7 @@ class String
      * @param  String  $needle
      * @return Boolean
      */
-    public function static startsWith($haystack, $needle)
+    public static function startsWith($haystack, $needle)
     {
         // search backwards starting from haystack length characters from the end
         return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
@@ -23,7 +23,7 @@ class String
      * @param  String  $needle
      * @return Boolean
      */
-    public function static endsWith($haystack, $needle)
+    public static function endsWith($haystack, $needle)
     {
         // search forward starting from end minus needle length characters
         return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
