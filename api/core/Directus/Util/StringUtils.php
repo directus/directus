d510d13a6797c920526e0b4460669677d6a36c3a
Welling Guzman <hola@wellingguzman.com>
2016-01-20 00:16:31 -0400

    add generate random string

--- api/core/Directus/Util/StringUtils.php
+++ api/core/Directus/Util/StringUtils.php
@@ -28,4 +28,39 @@ class StringUtils
         // search forward starting from end minus needle length characters
         return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
     }
+
+    /**
+     * Return the length of the given string.
+     *
+     * @param  string  $value
+     * @return int
+     */
+    public static function length($value)
+    {
+        return mb_strlen($value);
+    }
+
+    /**
+     * Generate a "random" alpha-numeric string.
+     *
+     * From Laravel
+     * @param  int  $length
+     * @return string
+     */
+    public static function random($length = 16)
+    {
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $string = '';
+            while (($len = strlen($string)) < $length) {
+                $size = $length - $len;
+                $bytes = openssl_random_pseudo_bytes($size);
+                $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+            }
+
+            return $string;
+        }
+
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return substr(str_shuffle(str_repeat($pool, $length)), 0, $length);
+    }
 }
--- tests/api/Util/StringUtilsTest.php
+++ tests/api/Util/StringUtilsTest.php
@@ -17,4 +17,18 @@ class StringUtilsTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(StringUtils::endsWith($string, 'marston'));
         $this->assertFalse(StringUtils::endsWith($string, 'john'));
     }
+
+    public function testLength()
+    {
+        $this->assertEquals(10, StringUtils::length('mr. falcon'));
+    }
+
+    public function testRandom()
+    {
+        $length = 10;
+        $this->assertEquals(10, strlen(StringUtils::random($length)));
+        $this->assertEquals(16, strlen(StringUtils::random()));
+        $this->assertInternalType('string', StringUtils::random());
+        $this->assertEquals(1, strlen(StringUtils::random(1)));
+    }
 }
