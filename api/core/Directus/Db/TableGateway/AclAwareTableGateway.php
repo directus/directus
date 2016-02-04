29b20b32188b74b419b080d8e202715e98f3b3a0
Welling Guzman <hola@wellingguzman.com>
2016-02-03 21:52:42 -0400

    hooks parameters are not array anymore

--- api/core/Directus/Db/TableGateway/AclAwareTableGateway.php
+++ api/core/Directus/Db/TableGateway/AclAwareTableGateway.php
@@ -207,7 +207,7 @@ class AclAwareTableGateway extends \Zend\Db\TableGateway\TableGateway {
             $Update->where(array($TableGateway->primaryKeyFieldName => $recordData[$TableGateway->primaryKeyFieldName]));
             $TableGateway->updateWith($Update);
 
-            Hook::run('postUpdate', array($TableGateway, $recordData, $this->adapter, $this->acl));
+            Hook::run('postUpdate', $TableGateway, $recordData, $this->adapter, $this->acl);
         } else {
             $d = $recordData;
             unset($d['data']);
