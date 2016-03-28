32877c732af2b71a0286a8e378bc0019ec94e396
Welling Guzman <hola@wellingguzman.com>
2016-03-28 17:24:06 -0400

    remove markdown from UI Manager

--- app/core/UIManager.js
+++ app/core/UIManager.js
@@ -43,7 +43,6 @@ define(function(require, exports, module) {
     require('core-ui/directus_file'),
     require('core-ui/directus_file_title'),
     require('core-ui/map'),
-    require('core-ui/markdown'),
     require('core-ui/multiple_files'),
     require('core-ui/translation'),
     require('core-ui/template_chooser')
