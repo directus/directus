1690a3148722f5418ff5c7e24065ea70d419fc07
Welling Guzman <hola@wellingguzman.com>
2016-04-13 22:28:20 -0400

    CS Date Utils

--- api/core/Directus/Util/Date.php
+++ api/core/Directus/Util/Date.php
@@ -2,24 +2,29 @@
 
 namespace Directus\Util;
 
-class Date {
+class Date
+{
+    /**
+     * Given a date/time in UTC and a target timezone, yields a DateTime object converted from
+     * UTC to the target timezone.
+     *
+     * @param  mixed $datetime       \DateTime instance or String.
+     * @param  mized $targetTimeZone \DateTimeZone instance or String.
+     *
+     * @return \DateTime
+     */
+    public static function convertUtcDateTimeToTimeZone($datetime, $targetTimeZone)
+    {
+        if (!($datetime instanceof \DateTime)) {
+            $datetime = new \DateTime($datetime, new \DateTimeZone('UTC'));
+        }
 
-	/**
-	 * Given a date/time in UTC and a target timezone, yields a DateTime object converted from
-	 * UTC to the target timezone.
-	 * @param  mixed $datetime       \DateTime instance or String.
-	 * @param  mized $targetTimeZone \DateTimeZone instance or String.
-	 * @return \DateTime
-	 */
-	public static function convertUtcDateTimeToTimeZone($datetime, $targetTimeZone) {
-		if(!($datetime instanceof \DateTime)) {
-			$datetime = new \DateTime($datetime, new \DateTimeZone("UTC"));
-		}
-		if(!($targetTimeZone instanceof \DateTimeZone)) {
-			$targetTimeZone = new \DateTimeZone($targetTimeZone);
-		}
-		$datetime->setTimeZone($targetTimeZone);
-		return $datetime;
-	}
+        if (!($targetTimeZone instanceof \DateTimeZone)) {
+            $targetTimeZone = new \DateTimeZone($targetTimeZone);
+        }
 
+        $datetime->setTimeZone($targetTimeZone);
+
+        return $datetime;
+    }
 }
