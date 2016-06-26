ce2b5f5dc921f590038681aa387d276de5dd7c65
Welling Guzman <hola@wellingguzman.com>
2016-06-26 18:19:35 -0400

    fix indentation

--- installation/autoload.php
+++ installation/autoload.php
@@ -13,29 +13,29 @@
 */
 spl_autoload_register(function ($class) {
 
-// project-specific namespace prefix
-$prefix = 'Directus\\Installation\\';
+    // project-specific namespace prefix
+    $prefix = 'Directus\\Installation\\';
 
-// base directory for the namespace prefix
-$base_dir = __DIR__ . '/includes/';
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/includes/';
 
-// does the class use the namespace prefix?
-$len = strlen($prefix);
-if (strncmp($prefix, $class, $len) !== 0) {
-// no, move to the next registered autoloader
-return;
-}
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
 
-// get the relative class name
-$relative_class = substr($class, $len);
+    // get the relative class name
+    $relative_class = substr($class, $len);
 
-// replace the namespace prefix with the base directory, replace namespace
-// separators with directory separators in the relative class name, append
-// with .php
-$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
-// if the file exists, require it
-if (file_exists($file)) {
-require $file;
-}
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
 });
