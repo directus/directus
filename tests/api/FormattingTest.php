882585c387f1f620f71fe695ebbccf2dffa53486
Welling Guzman <hola@wellingguzman.com>
2016-06-29 22:22:08 -0400

    add more tests

--- tests/api/FormattingTest.php
+++ tests/api/FormattingTest.php
@@ -4,8 +4,23 @@ use Directus\Util\Formatting;
 
 class FormattingTest extends PHPUnit_Framework_TestCase
 {
+    public function testFilenameToTitle()
+    {
+        $path = __DIR__.'/a-test_file.txt';
+        $this->assertEquals('A Test File', Formatting::fileNameToFileTitle($path));
+    }
+
     public function testUnderscoreToCamelCase()
     {
-        $this->assertEquals(Formatting::underscoreToCamelCase('hello_world'), 'HelloWorld');
+        $this->assertEquals('HelloWorld', Formatting::underscoreToCamelCase('hello_world'));
+    }
+
+    public function testSanitizeTitle()
+    {
+        $title = 'this is a text example';
+        $this->assertEquals('this-is-a-text-example', Formatting::sanitize_title_with_dashes($title));
+
+        $title = 'this–is—a text example';
+        $this->assertEquals('this-is-a-text-example', Formatting::sanitize_title_with_dashes($title, '', 'save'));
     }
 }
--- tests/api/Util/DateUtilsTest.php
+++ tests/api/Util/DateUtilsTest.php
@@ -18,5 +18,25 @@ class DateUtilsTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(-11, DateUtils::daysLeft(1461380876, 1462380876, true));
         $this->assertEquals(-11, DateUtils::daysLeft(Date('Y-m-d', 1461380876), Date('Y-m-d', 1462380876), true));
         $this->assertEquals(-18, DateUtils::daysLeft(new DateTime('2016-03-29'), new DateTime('2016-04-16'), true));
+
+        $this->assertEquals(1, DateUtils::daysLeft(time()+DateUtils::DAY_IN_SECONDS));
+    }
+
+    public function testConvertUTCDateTimezone()
+    {
+        $time = '2016-06-28 16:13:18';
+        $timezone = 'America/New_York';
+        $result = DateUtils::convertUtcDateTimeToTimeZone($time, $timezone);
+
+        $this->assertInstanceOf('\DateTime', $result);
+        $this->assertEquals('2016-06-28 12:13:18', $result->format('Y-m-d H:i:s'));
+
+        $dateTime = new \DateTime($time, new DateTimeZone('UTC'));
+        $dateTimeZone = new \DateTimeZone($timezone);
+
+        $result = DateUtils::convertUtcDateTimeToTimeZone($dateTime, $dateTimeZone);
+
+        $this->assertInstanceOf('\DateTime', $result);
+        $this->assertEquals('2016-06-28 12:13:18', $result->format('Y-m-d H:i:s'));
     }
 }
