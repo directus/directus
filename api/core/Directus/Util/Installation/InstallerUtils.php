a38b7bae50b5eae9d094d4ab5e6d0928d4fc691d
Welling Guzman <hola@wellingguzman.com>
2016-07-28 16:44:32 -0400

    fix project name setting

--- api/core/Directus/Util/Installation/InstallerUtils.php
+++ api/core/Directus/Util/Installation/InstallerUtils.php
@@ -236,7 +236,7 @@ class InstallerUtils
             ],
             [
                 'collection' => 'global',
-                'name' => 'project',
+                'name' => 'project_name',
                 'value' => $data['directus_name']
             ],
             [
