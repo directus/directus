12e6a52146109e980975f859c67666256f3b1ad9
Welling Guzman <hola@wellingguzman.com>
2016-07-29 11:39:03 -0400

    update privileges migration

--- api/migrations/schema/20150204024327_CreateDirectusPrivilegesTable.php
+++ api/migrations/schema/20150204024327_CreateDirectusPrivilegesTable.php
@@ -19,96 +19,118 @@ class CreateDirectusPrivilegesTable extends Ruckusing_Migration_Base
 {
     public function up()
     {
-      $t = $this->create_table("directus_privileges", array(
-        "id"=>false,
-        )
-      );
+        $t = $this->create_table('directus_privileges', array(
+                'id' => false,
+            )
+        );
 
-      //columns
-      $t->column("id", "integer", array(
-          "unsigned"=>true,
-          "null"=>false,
-          "auto_increment"=>true,
-          "primary_key"=>true
-        )
-      );
-      $t->column("table_name", "string", array(
-          "limit"=>255,
-          "null"=>false,
-          "character"=>"latin1"
-        )
-      );
-      $t->column("permissions", "string", array(
-          "limit"=>500,
-          "default"=>NULL,
-          "character"=>"latin1",
-          "COMMENT"=>"Table-level permissions (insert, delete, etc.)"
-        )
-      );
-      $t->column("group_id", "integer", array(
-          "unsigned"=>true,
-          "null"=>false
-        )
-      );
-      $t->column("read_field_blacklist", "string", array(
-          "limit"=>1000,
-          "default"=>NULL,
-          "character"=>"latin1",
-        )
-      );
-      $t->column("write_field_blacklist", "string", array(
-          "limit"=>1000,
-          "default"=>NULL,
-          "character"=>"latin1",
-        )
-      );
-      $t->column("unlisted", "tinyinteger", array(
-          "limit"=>1,
-          "default"=>NULL
-        )
-      );
-      $t->column("status_id", "tinyinteger", array(
-          "limit"=>1,
-          "default"=>0,
-          "null"=>false
-        )
-      );
+        //columns
+        $t->column('id', 'integer', array(
+                'unsigned' => true,
+                'null' => false,
+                'auto_increment' => true,
+                'primary_key' => true
+            )
+        );
+        $t->column('table_name', 'string', array(
+                'limit' => 255,
+                'null' => false,
+                'character' => 'latin1'
+            )
+        );
+        $t->column('allow_view', 'tinyinteger', array(
+            'limit' => 1,
+            'null' => false,
+            'default' => 0,
+        ));
+        $t->column('allow_add', 'tinyinteger', array(
+            'limit' => 1,
+            'null' => false,
+            'default' => 0,
+        ));
+        $t->column('allow_edit', 'tinyinteger', array(
+            'limit' => 1,
+            'null' => false,
+            'default' => 0,
+        ));
+        $t->column('allow_delete', 'tinyinteger', array(
+            'limit' => 1,
+            'null' => false,
+            'default' => 0,
+        ));
+        $t->column('allow_alter', 'tinyinteger', array(
+            'limit' => 1,
+            'null' => false,
+            'default' => 0,
+        ));
+        $t->column('group_id', 'integer', array(
+                'unsigned' => true,
+                'null' => false
+            )
+        );
+        $t->column('read_field_blacklist', 'string', array(
+                'limit' => 1000,
+                'default' => NULL,
+                'character' => 'latin1',
+            )
+        );
+        $t->column('write_field_blacklist', 'string', array(
+                'limit' => 1000,
+                'default' => NULL,
+                'character' => 'latin1',
+            )
+        );
+        $t->column('listed', 'tinyinteger', array(
+                'limit' => 1,
+                'default' => NULL
+            )
+        );
+        $t->column('status_id', 'tinyinteger', array(
+                'limit' => 1,
+                'default' => 0,
+                'null' => false
+            )
+        );
 
-      $t->finish();
+        $t->finish();
 
-      $tables = [
-          'directus_activity',
-          'directus_columns',
-          'directus_groups',
-          'directus_files',
-          'directus_messages',
-          'directus_preferences',
-          'directus_privileges',
-          'directus_settings',
-          'directus_tables',
-          'directus_ui',
-          'directus_users',
-          'directus_social_feeds',
-          'directus_messages_recipients',
-          'directus_social_posts',
-          'directus_tab_privileges',
-          'directus_bookmarks'
-      ];
+        $tables = [
+            'directus_activity',
+            'directus_columns',
+            'directus_groups',
+            'directus_files',
+            'directus_messages',
+            'directus_preferences',
+            'directus_privileges',
+            'directus_settings',
+            'directus_tables',
+            'directus_ui',
+            'directus_users',
+            'directus_social_feeds',
+            'directus_messages_recipients',
+            'directus_social_posts',
+            'directus_tab_privileges',
+            'directus_bookmarks'
+        ];
 
-      foreach($tables as $table) {
-          $this->insert('directus_privileges', [
-              'table_name' => $table,
-              'permissions' => 'add,edit,bigedit,delete,bigdelete,alter,view,bigview',
-              'group_id' => 1,
-              'read_field_blacklist' => NULL,
-              'write_field_blacklist' => NULL,
-              'unlisted' => NULL
-          ]);
-      }
+        foreach ($tables as $table) {
+            $this->insert('directus_privileges', [
+                'table_name' => $table,
+                'allow_view' => 2,
+                'allow_add' => 1,
+                'allow_edit' => 2,
+                'allow_delete' => 2,
+                'allow_alter' => 1,
+                'group_id' => 1,
+                'read_field_blacklist' => NULL,
+                'write_field_blacklist' => NULL,
+                'unlisted' => NULL
+            ]);
+        }
     }//up()
 
     public function down()
     {
-      $this->drop_table("directus_privileges");
+        $this->drop_table('directus_privileges');
     }//down()
 }
