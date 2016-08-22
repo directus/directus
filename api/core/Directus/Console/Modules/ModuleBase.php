59dc2075481399386715d6ef22dd8a1e4d80b69e
Wedjaa <Wedjaa@users.noreply.github.com>
2016-08-22 14:38:03 -0500

    Modular CLI for Directus (#1210)
    * First commit - initial release
    
    * Fixed a couple of bugs. Enabled UserModule
    
    * Fixed code style; modules now extend a common class.
    
    Style fixed. Function names are now all camel cased.
    Created a ModuleBase that command modules extend.
    
    * Fix ModuleBase and extending modules
    
    Fixes some issues with how the life of an extending class works in real PHP.
    
    * Remove development noise
    
    Remove a couple of 'echo' used while checking the loading of modules.
    
    * Implement User and Setting helpers
    
    Implemented User and Setting helpers for setup. Now the 'install' command will
    *try* and be more intelligent. If settings already exists, try and update them
    instead of installing them from scratch.
    
    * Fix command help issue.
    
    Fixed and issue where command help was trying to call the old style help function.

---
+++ api/core/Directus/Console/Modules/ModuleBase.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * This file is part of Directus.
+ *
+ * Directus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Directus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Directus.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * CLI Base Module.
+ *
+ * This module provides the base implementation of the Module Interface.
+ *
+ * @category   Classes
+ * @package    Directus/Console/Modules
+ * @author     Fabio 'MrWHO' Torchetti <mrwho@wedjaa.net>
+ * @copyright  2016 Wedjaa Inc
+ * @license    https://www.gnu.org/licenses/gpl-3.0.en.html  GPLv3 License
+ *
+ */
+
+namespace Directus\Console\Modules;
+
+use Directus\Console\Exception\UnsupportedCommandException;
+
+class ModuleBase implements ModuleInterface
+{
+
+    public function getModuleName()
+    {
+        return $this->__module_name;
+    }
+
+    public function getInfo()
+    {
+        return $this->__module_name.': '.__t($this->__module_description);
+    }
+
+    public function getCommands()
+    {
+        return $this->commands_help;
+    }
+
+    public function getCommandHelp($command)
+    {
+        if (!array_key_exists($command, $this->help)) {
+            throw new UnsupportedCommandException($this->__module_name.':'. $command . __t(' command does not exists!'));
+        }
+        return $this->help[$command];
+    }
+
+    public function runCommand($command, $args, $extra)
+    {
+        $cmd_name = 'cmd'.ucwords($command);
+        if (!method_exists($this, $cmd_name)) {
+            throw new UnsupportedCommandException($this->__module_name.':'. $command . __t(' command does not exists!'));
+        }
+        $this->$cmd_name($args, $extra);
+    }
+}
