ad607a91a7f050d72f783e193ea91ab38a6cd696
Welling Guzman <hola@wellingguzman.com>
2016-08-23 21:18:39 -0400

    templates should be initiate outside dist

--- gulpfile.js
+++ gulpfile.js
@@ -241,7 +241,7 @@ gulp.task('submodules', function(cb) {
   executeCommand(cb, {
     command: 'git',
     args: ['submodule', 'update', '--init', 'api/migrations/templates'],
-    options: {cwd: './dist/'}
+    options: {cwd: './'}
   });
 });
 
