4e01e313952eec6cc126f0e81cf71128db1d2c67
Welling Guzman <hola@wellingguzman.com>
2016-09-02 19:50:46 -0400

    add status column constant

--- phpunit.php
+++ phpunit.php
@@ -5,3 +5,7 @@ $loader = require __DIR__.'/vendor/autoload.php';
 if (!defined('BASE_PATH')) {
     define('BASE_PATH', __DIR__);
 }
+
+if (!defined('STATUS_COLUMN_NAME')) {
+    define('STATUS_COLUMN_NAME', 'active');
+}
