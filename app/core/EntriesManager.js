d58aaedae56a6d6588e5b9a48b644aa2e72aa83c
Welling Guzman <hola@wellingguzman.com>
2016-09-19 21:40:59 -0400

    get new entries instance of core tables

--- app/core/EntriesManager.js
+++ app/core/EntriesManager.js
@@ -17,48 +17,64 @@ define(function(require, exports, module) {
   var apiURL = '',
       rowsPerPage = 100;
 
+  function getNewCoreInstance(tableName) {
+    switch (tableName) {
+      case 'directus_users':
+        return new UsersCollection([], _.extend({
+          rowsPerPage: 3000,
+          url: apiURL + 'tables/directus_users/rows',
+          filters: {columns_visible: ['avatar', 'first_name', 'last_name', 'group', 'email', 'position', 'location', 'phone', 'last_access'], active:1}
+        }, SchemaManager.getFullSchema('directus_users')));
+
+      case 'directus_files':
+        return new FilesCollection([], _.extend({
+          rowsPerPage: rowsPerPage,
+          url: apiURL + 'files',
+          filters: {columns_visible: ['name','title','size', 'type', 'url', 'user','date_uploaded', 'storage_adapter', 'width', 'height']}
+        }, SchemaManager.getFullSchema('directus_files')));
+
+      case 'directus_groups':
+        return new EntriesCollection([], _.extend({
+          rowsPerPage: rowsPerPage,
+          url: apiURL + 'groups/'
+        }, SchemaManager.getFullSchema('directus_groups')));
+
+      case 'directus_activity':
+        return new EntriesCollection([], _.extend({
+          rowsPerPage: rowsPerPage,
+          url: apiURL + 'activity/',
+          filters: {columns_visible: ['activity','datetime','user'], sort_order: 'DESC'}
+        }, SchemaManager.getFullSchema('directus_activity')));
+    }
+  }
+
   function setup(options) {
     apiURL = options.apiURL;
     rowsPerPage = options.rowsPerPage;
 
     // Setup Directus Users
-    usersInstance = new UsersCollection([], _.extend({
-      rowsPerPage: 3000,
-      url: apiURL + 'tables/directus_users/rows',
-      filters: {columns_visible: ['avatar', 'first_name', 'last_name', 'group', 'email', 'position', 'location', 'phone', 'last_access'], active:1}
-    }, SchemaManager.getFullSchema('directus_users')));
-
-    filesInstance = new FilesCollection([], _.extend({
-      rowsPerPage: rowsPerPage,
-      url: apiURL + 'files',
-      filters: {columns_visible: ['name','title','size', 'type', 'url', 'user','date_uploaded', 'storage_adapter', 'width', 'height']}
-    }, SchemaManager.getFullSchema('directus_files')));
-
-    activityInstance = new EntriesCollection([], _.extend({
-      rowsPerPage: rowsPerPage,
-      url: apiURL + 'activity/',
-      filters: {columns_visible: ['activity','datetime','user'], sort_order: 'DESC'}
-    }, SchemaManager.getFullSchema('directus_activity')));
-
-    groupsInstance = new EntriesCollection([], _.extend({
-      rowsPerPage: rowsPerPage,
-      url: apiURL + 'groups/'
-    }, SchemaManager.getFullSchema('directus_groups')));
+    usersInstance = getNewCoreInstance('directus_users');
+    filesInstance = getNewCoreInstance('directus_files');
+    activityInstance = getNewCoreInstance('directus_activity');
+    groupsInstance = getNewCoreInstance('directus_groups');
   }
 
   function getInstance(tableName, options) {
+    options = (options || {});
+    var newCoreInstance = options.getNewInstance === true;
+
     switch (tableName) {
       case 'directus_users':
-        return usersInstance;
+        return newCoreInstance ? getNewCoreInstance(tableName) : usersInstance;
 
       case 'directus_files':
-        return filesInstance;
+        return newCoreInstance ? getNewCoreInstance(tableName) : filesInstance;
 
       case 'directus_groups':
-        return groupsInstance;
+        return newCoreInstance ? getNewCoreInstance(tableName) : groupsInstance;
 
       case 'directus_activity':
-        return activityInstance;
+        return newCoreInstance ? getNewCoreInstance(tableName) : activityInstance;
     }
 
     options = options || {};
@@ -77,7 +93,15 @@ define(function(require, exports, module) {
 
   }
 
+  // @TODO: A better separation of this two methods
+  function getNewInstance(tableName, options) {
+    options = (options || {});
+    options.getNewInstance = true;
+    return getInstance(tableName, options);
+  }
+
   module.exports.getInstance = getInstance;
+  module.exports.getNewInstance = getNewInstance;
   module.exports.setup = setup;
 
   module.exports.FilesCollection = FilesCollection;
