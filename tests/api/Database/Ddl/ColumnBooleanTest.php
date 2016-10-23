f5c1932265fabb7a94c73ba4e10113319304c043
Welling Guzman <hola@wellingguzman.com>
2016-10-23 13:43:12 -0400

    add boolean column test

---
+++ tests/api/Database/Ddl/ColumnBooleanTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Directus\Database\Ddl\Column\Boolean;
+
+class BooleanTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetExpressionData()
+    {
+        $column = new Boolean('foo', false, 2);
+        $this->assertSame('foo', $column->getName());
+        $this->assertFalse($column->isNullable());
+        $this->assertSame(2, $column->getDefault());
+    }
+}
