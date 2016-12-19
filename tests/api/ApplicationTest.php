42b73d59b58a5b757669606a271dee149b6cb5bf
Welling Guzman <hola@wellingguzman.com>
2016-12-19 18:31:31 -0400

    test app providers

--- tests/api/ApplicationTest.php
+++ tests/api/ApplicationTest.php
@@ -16,6 +16,37 @@ class CallableClass
     }
 }
 
+class AServiceProvider implements \Directus\Application\ServiceProviderInterface
+{
+    protected $booted = false;
+    protected $app;
+
+    public function boot(\Directus\Application\Application $app)
+    {
+        $this->booted = true;
+    }
+
+    public function register(\Directus\Application\Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    public function isBooted()
+    {
+        return $this->booted;
+    }
+
+    public function setBooted($booted)
+    {
+        return $this->booted = (bool) $booted;
+    }
+}
+
 class ApplicationTests extends PHPUnit_Framework_TestCase
 {
     public function setUp()
@@ -105,6 +136,53 @@ class ApplicationTests extends PHPUnit_Framework_TestCase
         $app = new \Directus\Application\Application([]);
         $app->get('/bar', '\RouteMainClass:name');
         $app->call();
+    }
+
+    public function testProviders()
+    {
+        $app = new \Directus\Application\Application([]);
+        $app->get('/bar', function() {
+            echo 'xyz';
+        });
+
+        $provider = new AServiceProvider();
+
+        $this->assertFalse($provider->isBooted());
+        $this->assertNull($provider->getApp());
+
+        $app->register($provider);
+
+        $this->assertFalse($provider->isBooted());
+
+        ob_start(); $app->run(); ob_clean();
+
+        $this->assertTrue($provider->isBooted());
+        $this->assertSame($app, $provider->getApp());
+
+        $provider->setBooted(false);
+
+        ob_start(); $app->run(); ob_clean();
+        $this->assertFalse($provider->isBooted());
+
+        $app->boot();
+        $this->assertFalse($provider->isBooted());
+    }
+
+    public function testPostData()
+    {
+        \Slim\Environment::mock(array(
+            'SERVER_NAME' => 'getdirectus.com',
+            'REQUEST_METHOD' => 'GET',
+            'SCRIPT_NAME' => '/foo', //<-- Physical
+            'PATH_INFO' => '/bar', //<-- Virtual
+            'QUERY_STRING' => 'one=foo&two=bar',
+            'CONTENT_TYPE' => 'application/json',
+            'slim.input' => '{"name": "john"}',
+        ));
+
+        $app = new \Directus\Application\Application([]);
+        $post = $app->request();
+        $this->assertSame($post->post('name'), 'john');
     }
 
     public function testOutputFormat()
