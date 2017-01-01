30d36e5ddd626dfd2e423495c9b6c19b490bd265
Welling Guzman <hola@wellingguzman.com>
2017-01-01 18:24:11 -0400

    show item id

--- app/modules/tables/views/EditViewRightPane.js
+++ app/modules/tables/views/EditViewRightPane.js
@@ -9,6 +9,12 @@ define([
 
     template: 'modules/tables/edit-right-pane',
 
+    serialize: function() {
+      var model = this.baseView.model;
+
+      return model ? model.toJSON() : {};
+    },
+
     beforeRender: function() {
       this.baseView.rightSidebarView.$el.addClass('scroll-y wide no-title');
     }
