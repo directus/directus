1dba30df21d68935d065b845e0859fa2c786ce7b
Welling Guzman <hola@wellingguzman.com>
2017-01-01 19:14:23 -0400

    display columns on right pane

--- app/modules/tables/views/TableViewRightPane.js
+++ app/modules/tables/views/TableViewRightPane.js
@@ -1,7 +1,18 @@
 define([
-  'core/RightPane'
-], function(RightPane) {
+  'core/RightPane',
+  'underscore'
+], function(RightPane, _) {
   return RightPane.extend({
-    template: 'modules/tables/table-right-pane'
+    template: 'modules/tables/table-right-pane',
+
+    serialize: function() {
+      var model = this.baseView.collection;
+      var table = _.findStringKey(this.baseView, 'collection.table');
+      var data = model ? model.toJSON() : {};
+
+      data.columns = table.columns ? table.columns.toJSON() : {};
+
+      return data;
+    }
   });
 });
