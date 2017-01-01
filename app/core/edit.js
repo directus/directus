5ff3dba04c7010a294ca0f3017cd95f04d8bf071
Welling Guzman <hola@wellingguzman.com>
2017-01-01 06:07:07 -0400

    add missing Backbone import

--- app/core/edit.js
+++ app/core/edit.js
@@ -3,6 +3,7 @@ define(function(require, exports, module) {
   'use strict';
 
   var app = require('app');
+  var Backbone = require('backbone');
   var Handlebars = require('handlebars');
   var UIManager = require('./UIManager');
 
