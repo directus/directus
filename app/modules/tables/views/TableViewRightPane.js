f119651392662c06a589ee20514f2613775c35e9
Welling Guzman <hola@wellingguzman.com>
2017-01-04 23:40:36 -0400

    update right pane visible columns

--- app/modules/tables/views/TableViewRightPane.js
+++ app/modules/tables/views/TableViewRightPane.js
@@ -2,15 +2,58 @@ define([
   'core/RightPane',
   'underscore'
 ], function(RightPane, _) {
+
   return RightPane.extend({
+
     template: 'modules/tables/table-right-pane',
+
+    events: {
+      'click #save_columns': 'updateVisibleColumns'
+    },
+
+    updateVisibleColumns: function(event) {
+      var collection = this.collection;
+      var preferences = collection.preferences;
+
+      var $inputs = this.$('#reorder-columns input:checked');
+      var columns = _.map($inputs, function(input) {
+        return $(input).data('name');
+      });
+
+      preferences.save({'columns_visible': columns.join(',')},{
+        success: function() {
+          collection.trigger('visibility');
+        }
+      });
+
+      collection.filters.columns_visible = columns;
+      collection.fetch();
+    },
 
     serialize: function() {
       var collection = this.collection;
-      var table = collection.table;
+      var structure = collection.structure;
+      var preferences = collection.preferences;
       var data = collection ? collection.toJSON() : {};
+      var visibleColumns = preferences.get('columns_visible').split(',');
 
-      data.columns = table.columns ? table.columns.toJSON() : {};
+      data.columns = structure.chain()
+        .filter(function(model) {
+          return !model.get('system') && !model.get('hidden_list') && !model.get('hidden_input');
+        })
+        .map(function(model) {
+          var isVisible = _.contains(visibleColumns, model.id);
+          var isForeign = _.contains(['MANYTOMANY', 'ONETOMANY'], model.getRelationshipType());
+
+          return {
+            id: model.id,
+            name: model.get('column_name'),
+            visible: isVisible,
+            disabled: isForeign,
+            isForeign: isForeign
+          };
+        }, this)
+        .value();
 
       return data;
     }
