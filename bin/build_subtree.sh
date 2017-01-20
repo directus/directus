90f5accec60478c96910ac0921ed383db7328852
Welling Guzman <hola@wellingguzman.com>
2017-01-20 18:36:36 -0400

    update subtree script

--- bin/build_subtree.sh
+++ bin/build_subtree.sh
@@ -1,15 +1,21 @@
 git subsplit init git@github.com:directus/directus.git
 
-# ACL
-git subsplit publish --heads="development" --no-tags --debug api/core/Directus/Acl:git@github.com:directus/directus-acl.git
+# Collection
+git subsplit publish --heads="version/6.4" --no-tags --debug api/core/Directus/Collection:git@github.com:directus/directus-collection.git
+
+# Permissions
+git subsplit publish --heads="version/6.4" --no-tags --debug api/core/Directus/Permissions:git@github.com:directus/directus-permissions.git
 
 # Database
-git subsplit publish --heads="development" --no-tags --debug api/core/Directus/Db:git@github.com:directus/directus-database.git
+git subsplit publish --heads="version/6.4" --no-tags --debug api/core/Directus/Db:git@github.com:directus/directus-database.git
+
+# Filesystem
+git subsplit publish --heads="version/6.4" --no-tags --debug api/core/Directus/Filesystem:git@github.com:directus/directus-filesystem.git
 
 # Hooks
-git subsplit publish --heads="development" --no-tags --debug api/core/Directus/Hook:git@github.com:directus/directus-hook.git
+git subsplit publish --heads="version/6.4" --no-tags --debug api/core/Directus/Hook:git@github.com:directus/directus-hook.git
 
 # Utils
-git subsplit publish --heads="development" --no-tags --debug api/core/Directus/Util:git@github.com:directus/directus-php-utils.git
+git subsplit publish --heads="version/6.4" --no-tags --debug api/core/Directus/Util:git@github.com:directus/directus-php-utils.git
 
 rm -rf .subsplit
