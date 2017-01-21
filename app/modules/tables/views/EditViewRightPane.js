38da2702ed4caa1a0180124187f8620e5c74508b
Welling Guzman <hola@wellingguzman.com>
2017-01-21 00:22:17 -0400

    remove edit view right pane empty template file

--- app/modules/tables/views/EditViewRightPane.js
+++ app/modules/tables/views/EditViewRightPane.js
@@ -12,12 +12,6 @@ define([
       class: 'scroll-y wide no-title'
     },
 
-    template: 'modules/tables/edit-right-pane',
-
-    serialize: function() {
-      return this.model ? this.model.toJSON() : {};
-    },
-
     beforeRender: function() {
       this.baseView.rightSidebarView.$el.addClass('scroll-y wide no-title');
 
