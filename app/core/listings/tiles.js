131a1f5b47ac1e03a388651e556483b3e7405939
Welling Guzman <hola@wellingguzman.com>
2017-02-06 01:18:03 -0400

    update tiles view

--- app/core/listings/tiles.js
+++ app/core/listings/tiles.js
@@ -1,11 +1,11 @@
-define(['app', 'backbone'], function(app, Backbone) {
+define(['app', 'underscore', 'backbone', 'core/listings/baseView'], function(app, _, Backbone, BaseView) {
 
   return {
     id: 'tiles',
 
     icon: 'apps',
 
-    View: Backbone.Layout.extend({
+    View: BaseView.extend({
 
       template: 'core/listings/tiles',
 
@@ -14,9 +14,70 @@ define(['app', 'backbone'], function(app, Backbone) {
       },
 
       serialize: function() {
+        var titleColumn = this.getTitleColumn();
+        var items = this.collection.map(function(model) {
+          var item = {};
+
+          item.id = model.get('id');
+          item.title = model.get(titleColumn.id);
+
+          return item;
+        });
+
         return {
-          items: this.collection.toJSON()
+          items: items
         };
+      },
+
+      optionsStructure: function() {
+        var options = {
+          title: {}
+        };
+
+        _.each(this.titleColumns(), function(column) {
+          options.title[column.id] = column.id;
+        });
+
+        return [
+          {
+            id: 'title_column',
+            type: 'String',
+            required: true,
+            ui: 'select',
+            options: {
+              options: options.title
+            }
+          }
+        ]
+      },
+
+      getTitleColumn: function() {
+        var viewOptions = this.getViewOptions();
+        var column;
+
+        if (viewOptions.title_column) {
+          column = this.collection.structure.get(viewOptions.title_column);
+        } else {
+          column = _.first(this.titleColumns())
+        }
+
+        return column;
+      },
+
+      titleColumns: function() {
+        return this.collection.structure.filter(function(model) {
+          return _.contains(['VARCHAR'], model.get('type'));
+        });
+      },
+
+      onEnable: function() {
+        this.collection.on('sync', this.render, this);
+        this.collection.preferences.on('sync', this.render, this);
+      },
+
+      onDisable: function() {
+        this.collection.off('sync', this.render, this);
+        this.collection.preferences.off('sync', this.render, this);
       },
 
       initialize: function() {
