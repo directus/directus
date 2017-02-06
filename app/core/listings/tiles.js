c7b9c58280475648ee583e1db0063345b61fd86b
Welling Guzman <hola@wellingguzman.com>
2017-02-06 01:52:36 -0400

    WIP: Tiles columns options

--- app/core/listings/tiles.js
+++ app/core/listings/tiles.js
@@ -15,11 +15,17 @@ define(['app', 'underscore', 'backbone', 'core/listings/baseView'], function(app
 
       serialize: function() {
         var titleColumn = this.getTitleColumn();
+        var subTitleColumn = this.getSubTitleColumn();
+        var typeColumn = this.getTypeColumn();
+        var fileColumn = this.getFileColumn();
         var items = this.collection.map(function(model) {
           var item = {};
 
           item.id = model.get('id');
           item.title = model.get(titleColumn.id);
+          item.subtitle = model.get(subTitleColumn.id);
+          item.type = model.get(typeColumn.id);
+          item.thumb = model.has(fileColumn.id) ? model.get(fileColumn.id).get('thumbnail_url') : null;
 
           return item;
         });
@@ -31,11 +37,20 @@ define(['app', 'underscore', 'backbone', 'core/listings/baseView'], function(app
 
       optionsStructure: function() {
         var options = {
-          title: {}
+          title: {},
+          subtitle: {},
+          type: {},
+          file: {}
         };
 
         _.each(this.titleColumns(), function(column) {
           options.title[column.id] = column.id;
+          options.subtitle[column.id] = column.id;
+          options.type[column.id] = column.id;
+        });
+
+        _.each(this.fileColumns(), function(column) {
+          options.file[column.id] = column.id;
         });
 
         return [
@@ -46,6 +61,33 @@ define(['app', 'underscore', 'backbone', 'core/listings/baseView'], function(app
             ui: 'select',
             options: {
               options: options.title
+            }
+          },
+          {
+            id: 'subtitle_column',
+            type: 'String',
+            required: true,
+            ui: 'select',
+            options: {
+              options: options.subtitle
+            }
+          },
+          {
+            id: 'type_column',
+            type: 'String',
+            required: true,
+            ui: 'select',
+            options: {
+              options: options.type
+            }
+          },
+          {
+            id: 'file_column',
+            type: 'String',
+            required: true,
+            ui: 'select',
+            options: {
+              options: options.file
             }
           }
         ]
@@ -64,9 +106,54 @@ define(['app', 'underscore', 'backbone', 'core/listings/baseView'], function(app
         return column;
       },
 
+      getSubTitleColumn: function() {
+        var viewOptions = this.getViewOptions();
+        var column;
+
+        if (viewOptions.subtitle_column) {
+          column = this.collection.structure.get(viewOptions.subtitle_column);
+        } else {
+          column = _.first(this.titleColumns())
+        }
+
+        return column;
+      },
+
+      getTypeColumn: function() {
+        var viewOptions = this.getViewOptions();
+        var column;
+
+        if (viewOptions.type_column) {
+          column = this.collection.structure.get(viewOptions.type_column);
+        } else {
+          column = _.first(this.titleColumns())
+        }
+
+        return column;
+      },
+
+      getFileColumn: function() {
+        var viewOptions = this.getViewOptions();
+        var column;
+
+        if (viewOptions.file_column) {
+          column = this.collection.structure.get(viewOptions.file_column);
+        } else {
+          column = _.first(this.fileColumns())
+        }
+
+        return column;
+      },
+
       titleColumns: function() {
         return this.collection.structure.filter(function(model) {
           return _.contains(['VARCHAR'], model.get('type'));
+        });
+      },
+
+      fileColumns: function() {
+        return this.collection.structure.filter(function(model) {
+          return _.contains(['single_file'], model.get('ui'));
         });
       },
 
