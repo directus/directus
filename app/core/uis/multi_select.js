64929e576e6713c0c889f839ba55b2eeae656b5e
Welling Guzman <hola@wellingguzman.com>
2017-03-16 14:07:56 -0400

    set multi select checkbox inline - Fixes #1372

--- app/core/uis/multi_select.js
+++ app/core/uis/multi_select.js
@@ -13,8 +13,8 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
 
   var template = '{{#if cb_list}} \
                     {{#options}} \
-                            <input style="margin-top:1px;" name="{{key}}" type="checkbox" {{#if selected}}checked{{/if}}/> {{value}} \
-                    {{/options}}</select> \
+                            <input style="margin-top:1px;display: inline-block;" name="{{key}}" type="checkbox" {{#if selected}}checked{{/if}}/> {{value}} \
+                    {{/options}} \
                   {{else}} \
                     <select multiple> \
                         {{#options}} \
