232b04262f74e852aded968fa234b40a914f3ae8
Welling Guzman <hola@wellingguzman.com>
2017-03-20 02:08:37 -0400

    update directus_users interface

--- app/core/uis/directus_users.js
+++ app/core/uis/directus_users.js
@@ -15,7 +15,91 @@ define([
     templateSource: undefined,
     template: 'directus_users',
     events: {
+      'click .js-create-user': 'createUser',
+      'click .js-choose-user': 'chooseUser',
       'click .js-invite-user': 'invitationPrompt'
+    },
+
+    createUser: function () {
+      var EditView = require('modules/tables/views/EditView');
+      var collection = this.relatedCollection;
+      var model = new collection.model({}, {
+        collection: collection,
+        parse: true,
+        structure: collection.structure,
+        table: collection.table,
+        privileges: collection.privileges
+      });
+      var columnName = this.columnSchema.relationship.get('junction_key_right');
+      var id = this.model.id;
+      var view = new EditView({
+        model: model,
+        collectionAdd: true,
+        hiddenFields: [columnName],
+        parentField: {
+          name: columnName,
+          value: id
+        },
+        skipFetch: true
+      });
+
+      view.headerOptions.route.isOverlay = true;
+      view.headerOptions.route.breadcrumbs = [];
+      view.headerOptions.basicSave = true;
+
+      view.events['click .saved-success'] = function() {
+        this.save();
+      };
+      view.events['click #removeOverlay'] = function() {
+        app.router.removeOverlayPage(this);
+      };
+
+      app.router.overlayPage(view);
+
+      view.save = function() {
+        var data = view.editView.data();
+        data[columnName] = id;
+        model.set(data);
+
+        if (model.isValid()) {
+          app.router.removeOverlayPage(this);
+          collection.add(model, {nest: true});
+        }
+      };
+    },
+
+    chooseUser: function () {
+      var self = this;
+      var columnName = this.columnSchema.relationship.get('junction_key_right');
+      var collection = app.getEntries(this.relatedCollection.table.id);
+      var view = new Overlays.ListSelect({collection: collection});
+
+      app.router.overlayPage(view);
+      view.save = function() {
+        _.each(view.table.selection(), function (id) {
+          var user = self.relatedCollection.get(id);
+          var data = {};
+
+          if (!user) {
+            user = collection.get(id);
+          }
+
+          // data[columnName] = self.model.get('id');
+          var group;
+          if (user) {
+            // data = user.toJSON();
+            group = self.model.clone();
+            group.unset('permissions');
+            group.unset('users');
+            user.set(columnName, group);
+            self.relatedCollection.add(user, {nest: true});
+          }
+        }, this);
+
+        app.router.removeOverlayPage(this);
+      };
+
+      collection.fetch();
     },
 
     invitationPrompt: function() {
