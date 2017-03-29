d8a92315a22fa9b16d1fad94a496eacaf3dc6c06
Welling Guzman <hola@wellingguzman.com>
2017-03-29 15:47:55 -0400

    add groups/{id} patch endpoint

--- api/routes/A1/Groups.php
+++ api/routes/A1/Groups.php
@@ -6,6 +6,7 @@ use Directus\Application\Route;
 use Directus\Database\TableGateway\DirectusGroupsTableGateway;
 use Directus\Database\TableGateway\RelationalTableGateway as TableGateway;
 use Directus\Database\TableSchema;
+use Directus\Util\ArrayUtils;
 use Directus\View\JsonView;
 
 class Groups extends Route
@@ -53,6 +54,38 @@ class Groups extends Route
                 'success' => false
             ];
         }
+
+        return $this->app->response($response);
+    }
+
+    public function patchGroup($id)
+    {
+        $app = $this->app;
+        $acl = $app->container->get('acl');
+        $dbConnection = $app->container->get('zenddb');
+        $requestPayload = $app->request()->post();
+
+        $tableName = 'directus_groups';
+        $tableGateway = new TableGateway($tableName, $dbConnection, $acl);
+        $requestPayload['id'] = $id;
+
+        ArrayUtils::remove($requestPayload, 'permissions');
+        $users = ArrayUtils::get($requestPayload, 'users', []);
+        if ($users) {
+            $users = array_filter($users, function ($user) {
+                return ArrayUtils::has($user, 'id');
+            });
+
+            foreach ($users as &$user) {
+                $user = ArrayUtils::pick($user, ['id', 'group']);
+            }
+
+            $requestPayload['users'] = $users;
+        }
+
+        $newRecord = $tableGateway->manageRecordUpdate($tableName, $requestPayload);
+        $newGroupId = $newRecord['id'];
+        $response = $tableGateway->getEntries(['id' => $newGroupId]);
 
         return $this->app->response($response);
     }
