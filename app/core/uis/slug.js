2bac1328ced4c243da72c6f2c74d08d2959ee9b8
Welling Guzman <hola@wellingguzman.com>
2017-03-29 20:28:48 -0400

    update slug: add option to allow update only on creation

--- app/core/uis/slug.js
+++ app/core/uis/slug.js
@@ -7,7 +7,13 @@
 //  http://www.getdirectus.com
 /*jshint multistr: true */
 
-define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UIComponent, UIView, __t) {
+define([
+  'app',
+  'underscore',
+  'core/UIComponent',
+  'core/UIView',
+  'core/t'
+], function(app, _, UIComponent, UIView, __t) {
 
   'use strict';
 
@@ -23,29 +29,90 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
       }
     },
 
-    afterRender: function(e) {
-      if (this.options.settings.get("readonly") === "on") this.$("input").prop("readonly",true);
-      var mirrored_field = this.options.settings.get("mirrored_field");
-      var slug_field = this.$el.find('input');
-      $('#'+mirrored_field).keyup(function(){
-        var slug = $(this).val();
+    bindEvents: function () {
+      var mirroredField = this.getMirroredFieldName();
 
-        slug = slug.replace(/^\s+|\s+$/g, ''); // trim
-        slug = slug.toLowerCase();
+      if (this.canUpdateSlug()) {
+        $('.fields #' + mirroredField).on('keyup', _.bind(this.onKeyUp, this));
+      }
+    },
 
-        // Remove accents, swap ñ for n, etc
-        var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
-        var to   = "aaaaaeeeeeiiiiooooouuuunc------";
-        for (var i=0, l=from.length ; i<l ; i++) {
+    unbindEvents: function () {
+      var mirroredField = this.getMirroredFieldName();
+
+      $('.fields #' + mirroredField).off('keyup', _.bind(this.onKeyUp, this));
+    },
+
+    onKeyUp: function (event) {
+      var slug = $(event.currentTarget).val();
+
+      this.updateSlug(slug);
+    },
+
+    getSlugInput: function () {
+      if (!this.$slugField) {
+        this.$slugField = this.$el.find('input');
+      }
+
+      return this.$slugField;
+    },
+
+    getMirroredFieldName: function () {
+      var settings = this.options.settings;
+
+      return settings.get('mirrored_field');
+    },
+
+    canUpdateSlug: function () {
+      var settings = this.options.settings;
+      var mirroredField = this.getMirroredFieldName();
+
+      if (!mirroredField) {
+        return false;
+      }
+
+      return !(settings.get('only_creation') === true && !this.model.isNew());
+    },
+
+    updateSlug: function (slug) {
+      var $slugInput = this.getSlugInput();
+
+      slug = slug.replace(/^\s+|\s+$/g, ''); // trim
+      slug = slug.toLowerCase();
+
+      // TODO: Create a helper to remove tilde, accents, etc.
+      // Remove accents, swap ñ for n, etc
+      var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+      var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+      for (var i=0, l=from.length ; i<l ; i++) {
         slug = slug.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-        }
+      }
 
-        slug = slug.replace(/[^a-z0-9 \-]/g, '') // remove invalid chars
+      slug = slug.replace(/[^a-z0-9 \-]/g, '') // remove invalid chars
         .replace(/\s+/g, '-') // collapse whitespace and replace by -
         .replace(/-+/g, '-'); // collapse dashes
 
-        slug_field.val(slug);
-      });
+      $slugInput.val(slug);
+    },
+
+    afterRender: function () {
+      var $slugInput = this.$('input');
+      var value = this.options.value;
+      var mirroredField = this.getMirroredFieldName();
+      var model = this.options.model;
+
+      if (this.options.settings.get('readonly') === true) {
+        $slugInput.prop('readonly', true);
+      }
+
+      if (this.canUpdateSlug()) {
+        this.updateSlug(value || model.get(mirroredField) || '');
+        this.bindEvents();
+      }
+    },
+
+    cleanup: function () {
+      this.unbindEvents();
     },
 
     serialize: function() {
@@ -72,7 +139,9 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
       // Adjusts the max width of the input (Small, Medium, Large)
       {id: 'size', type: 'String', default_value:'large', ui: 'select', options: {options: {'large':__t('size_large'),'medium':__t('size_medium'),'small':__t('size_small')} }},
       // Enter the column name of the field the slug will pull it's value from
-      {id: 'mirrored_field', type: 'String', default_value: '', ui: 'textinput', char_length: 200}
+      {id: 'mirrored_field', type: 'String', default_value: '', ui: 'textinput', char_length: 200},
+      // Whether to update slug only on creation
+      {id: 'only_on_creation', type: 'Boolean', default_value: false, ui: 'checkbox', comment: __t('slug_only_on_creation_comment')}
     ],
     Input: Input,
     validate: function(value, options) {
