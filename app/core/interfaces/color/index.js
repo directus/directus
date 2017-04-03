67d16402dce2fcd2f7552bca662c309dd6be49a8
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-03 17:51:13 +0200

    New color interface! (#1388)
    * move interface to folder and create html template
    
    * allow user configurable palette
    
    * change input value on palette click
    
    * add styles to color palette and input
    
    * change input value according to selected palette color
    
    * add active class to selected color
    
    * apply active class when input value matches button color
    
    * add styling for color preview
    
    * update color preview color on input
    
    * fix preview and buttons not updating
    
    * [WIP] work on validation functions for css-colors
    
    * focus on hex for now
    
    * add invalid messages
    
    * add field is required message
    
    * add extra options
    
    * allow hex/rgb/hsl and update styling
    
    * cleanup styling
    
    * change reset color to black
    
    * cleanup html
    
    * style the color preview and palette
    
    * add rgb validation function
    
    * remove es6 bit from rgb valid function
    
    * add hex & rgb validation on change
    
    * add hsl color validation
    
    * add convertHexToRGB function
    
    * add rgb to hsl function
    
    * default to alpha = 1 in conversion
    
    * enable palette buttons
    
    * add checkerboard behind preview for alpha previews
    
    * add setpreviewcolor function
    
    * add (hacky) default values in setpreviewcolor
    
    * remove console.log
    
    * add empty afterRender and initialize methods
    
    * remove defaultpalette
    
    * remove broken list-preview
    
    * add hidden input field
    
    * add input-output conversion logic
    
    * fix save-conversion
    
    * show stored data in right format (bit :hankey:)
    
    * add disabled option for readonly to other value types
    
    * remove hash with # option for now
    
    * rename color to index
    
    * move validation functions to separate file
    
    * add instructional comments
    
    * move conversion to rgb functions to separate file
    
    * finish Color microlib
    
    * cleanup input using new color lib
    
    * implement alpha validation
    
    * minor fixes in validation and button-handling
    
    * show swatch | value in list
    
    * fix save / serialization & preview
    
    * fix input width bug
    
    * fix adding new item with empty values
    
    * tackle edgecase which saves / to db
    
    * add palette_only option
    
    * dont preset value when there isnt any

---
+++ app/core/interfaces/color/index.js
@@ -0,0 +1,274 @@
+//  Color core UI component
+//  Directus 6.0
+
+//  (c) RANGER
+//  Directus may be freely distributed under the GNU license.
+//  For all details and documentation:
+//  http://www.getdirectus.com
+
+// Attribute          Type              Contains                                Example
+// -------------------------------------------------------------------------------------------------------------------------------------
+// options.schema     Backbone.Model    Structure/Schema for this table row     options.schema.get('type') [column_name, comment, type]
+// options.model      Backbone.Model    Data/Model for this table row           options.model.get('id') [any column in current table row]
+// options.value      String            Value for this field
+// options.settings   Backbone.Model    Saved values for current UI options     options.settings.get('length') [any key from this UI options]
+// options.name       String            Field name
+/*jshint multistr: true */
+
+define(['app', 'core/UIComponent', 'core/UIView', 'core/t', 'core/interfaces/color/color'], function(app, UIComponent, UIView, __t, Color) {
+  'use strict';
+
+  function setPreviewColor(view, color) {
+    if (!view.options.settings.get('palette_only')) {
+      var rgbString = color.rgb.length === 4 ? 'rgba(' + color.rgb.join() + ')' : 'rgb(' + color.rgb.join() + ')';
+      view.$el.find('.color-preview')[0].style.boxShadow = 'inset 0 0 0 30px ' + rgbString;
+    }
+  }
+
+  function setInputValue(view, color, output) {
+    view.$el.find('.value').val(color[output]);
+  }
+
+  function showInvalidMessage(view) {
+    view.$el.find('.color-invalid')[0].innerHTML = __t('confirm_invalid_value');
+    setPreviewColor(view, Color([0, 0, 0, 0], 'rgb'));
+  }
+
+  function hideInvalidMessage(view) {
+    view.$el.find('.color-invalid')[0].innerHTML = '';
+  }
+
+  var Input = UIView.extend({
+    template: 'color/input',
+
+    events: {
+      // Disable enter button (would select first button after input == palette option)
+      'keypress input': function(event) {
+        if((event.which && event.which == 13) || (event.keyCode && event.keyCode == 13)) return false;
+      },
+
+      'click .color-preview': function(event) {
+        // Remove value from input
+        this.$el.find('input').val('');
+
+        // Reset color preview to black
+        setPreviewColor(this, Color([0,0,0], 'rgb'));
+
+        // Disable active button
+        this.$el.find('button').removeClass('active');
+      },
+
+      // Validate value on change
+      'input .color-text': function(event) {
+        var input = this.options.settings.get('input');
+        var allowAlpha = this.options.settings.get('allow_alpha');
+        var color;
+        var alphaValid = true;
+
+        switch(input) {
+          case 'hex':
+            color = Color(event.target.value, input);
+            if (!allowAlpha && color.length === 4) alphaValid = false;
+            if (!allowAlpha && color.length === 8) alphaValid = false;
+            break;
+
+          case 'rgb':
+            var values = [
+              +this.$el.find('input.red').val(),
+              +this.$el.find('input.green').val(),
+              +this.$el.find('input.blue').val()
+            ];
+            if (this.$el.find('input.alpha').val()) values.push(+this.$el.find('input.alpha').val());
+            color = Color(values, input);
+            if (!allowAlpha && color.length === 4) alphaValid = false;
+            break;
+
+          case 'hsl':
+            var values = [
+              +this.$el.find('input.hue').val(),
+              +this.$el.find('input.saturation').val(),
+              +this.$el.find('input.lightness').val()
+            ];
+            if (this.$el.find('input.alpha').val()) values.push(+this.$el.find('input.alpha').val());
+            color = Color(values, input);
+            if (!allowAlpha && color.length === 4) alphaValid = false;
+            break;
+        }
+
+        if (alphaValid && color) {
+          setPreviewColor(this, color);
+          setInputValue(this, color, this.options.settings.get('output'));
+          hideInvalidMessage(this);
+        } else {
+          showInvalidMessage(this)
+        }
+      },
+
+      'click .color-select': function(event) {
+          // Activate clicked on button
+          var button = event.target.className === 'material-icons' ? event.target.parentNode : event.target;
+          this.$el.find('button').removeClass('active');
+          button.classList.add('active');
+
+          // Fill input(s)
+          var input = this.options.settings.get('input');
+          var buttonValue = button.getAttribute('data-color');
+          var color = Color(buttonValue, 'hex');
+
+          switch(input) {
+            case 'hex':
+              this.$el.find('input').val(buttonValue);
+              break;
+            case 'rgb':
+              var rgba = color.rgb;
+              if (rgba.length !== 4) rgba[3] = 1;
+              this.$el.find('input.red').val(rgba[0]);
+              this.$el.find('input.green').val(rgba[1]);
+              this.$el.find('input.blue').val(rgba[2]);
+              this.$el.find('input.alpha').val(rgba[3]);
+              break;
+            case 'hsl':
+              var hsla = color.hsl;
+              if (hsla.length !== 4) hsla[3] = 1;
+              this.$el.find('input.hue').val(hsla[0]);
+              this.$el.find('input.saturation').val(hsla[1]);
+              this.$el.find('input.lightness').val(hsla[2]);
+              this.$el.find('input.alpha').val(hsla[3]);
+              break;
+          }
+
+          setPreviewColor(this, color);
+          setInputValue(this, color, this.options.settings.get('output'));
+      }
+    },
+
+    serialize: function() {
+      var input = this.options.settings.get('input');
+      var output = this.options.settings.get('output');
+      var userPalette = this.options.settings.get('palette') || [];
+
+      var value = '';
+      var outputValue = '';
+      var preview = 'rgba(0, 0, 0, 0)';
+
+      if (this.options.value && this.options.value.length > 1) {
+        var rawValue = this.options.value;
+        if (output === 'rgb' || output === 'hsl') rawValue = this.options.value.split(',').map(function(color) { return +color; });
+        var color = Color(rawValue, output);
+
+        value = color[input];
+        outputValue = color[output];
+        preview = color.rgb.length === 4 ? 'rgba(' + color.rgb + ')' : 'rgb(' + color.rgb + ')';
+      }
+
+      return {
+        value: value,
+        outputValue: outputValue,
+        preview: preview,
+        name: this.options.name,
+        comment: this.options.schema.get('comment'),
+        palette: userPalette.length ? userPalette.split(',') : false,
+        readonly: this.options.settings.get('readonly'),
+        input: input,
+        output: this.options.settings.get('output'),
+        hex: input === 'hex',
+        rgb: input === 'rgb',
+        hsl: input === 'hsl',
+        alpha: this.options.settings.get('allow_alpha'),
+        paletteOnly: this.options.settings.get('palette_only')
+      };
+    }
+  });
+
+  var Component = UIComponent.extend({
+    id: 'color',
+    dataTypes: ['VARCHAR'],
+    variables: [
+      {id: 'readonly', type: 'Boolean', default_value: false, ui: 'checkbox'},
+      {
+        id: 'input',
+        type: 'String',
+        default_value: 'hex',
+        ui: 'select',
+        options: {
+          options: {
+            hex: 'Hex',
+            rgb: 'RGB',
+            hsl: 'HSL'
+          }
+        }
+      },
+      {
+        id: 'output',
+        type: 'String',
+        default_value: 'hex',
+        ui: 'select',
+        options: {
+          options: {
+            hex: 'Hex',
+            rgb: 'RGB',
+            hsl: 'HSL'
+          }
+        }
+      },
+      {
+        id: 'listing',
+        type: 'String',
+        default_value: 'swatch',
+        ui: 'select',
+        options: {
+          options: {
+            swatch: 'Color Swatch',
+            value: 'Value'
+          }
+        }
+      },
+      {
+        id: 'allow_alpha',
+        type: 'Boolean',
+        default_value: false,
+        ui: 'checkbox'
+      },
+      {
+        id: 'palette_only',
+        type: 'Boolean',
+        default_value: false,
+        ui: 'checkbox'
+      },
+      {
+        id: 'palette',
+        type: 'String',
+        ui: 'tags'
+      }
+    ],
+    Input: Input,
+    validate: function(value, options) {
+      if (options.schema.isRequired() && _.isEmpty(value)) {
+        return __t('this_field_is_required');
+      }
+    },
+    list: function(options) {
+      var value;
+
+      switch(options.settings.get('output')) {
+        case 'hex':
+          value = options.value || false;
+          break;
+        case 'rgb':
+        case 'hsl':
+          value = options.value ? options.value.split(',').map(function(color) { return +color; }) : false;
+      }
+
+      if (value) {
+        var color = Color(value, options.settings.get('output')).rgb;
+        var rgb = color.length === 4 ? 'rgba(' + color + ')' : 'rgb(' + color + ')';
+        var swatch = '<div style="width: 20px; height: 20px; border-radius: 50%; background-color: ' + rgb + '"></div>';
+        return options.settings.get('listing') === 'swatch' ? swatch : value;
+      } else {
+        return '';
+      }
+    }
+  });
+
+  return Component;
+});
