7617a7d4d48a1ca62774f6069eecfa48f6bf6caf
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-08 22:44:59 +0200

    Minor codestyle improvements

--- app/core/interfaces/textinput/index.js
+++ app/core/interfaces/textinput/index.js
@@ -25,13 +25,17 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
     // Event Declarations
     events: {
       // Show character counter when input gains focus
-      'focus input': function() { this.$el.find('.char-count').removeClass('hide'); },
+      'focus input': function() {
+        this.$el.find('.char-count').removeClass('hide');
+      },
       // Update character counter when input changes
       'input input': 'updateMaxLength',
       // Validate keypress against validation_string
       'keypress input': 'validateString',
       // Hide character counter when input loses focus
-      'blur input': function() { this.$el.find('.char-count').addClass('hide'); }
+      'blur input': function() {
+        this.$el.find('.char-count').addClass('hide');
+      }
     },
 
     // Update the character counter with the remaining characters available
@@ -71,10 +75,10 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
       var chars;
       switch(this.options.settings.get('validation_type')) {
         case ('bl') :
-          chars = this.options.settings.get('validation_string').split("");
+          chars = this.options.settings.get('validation_string').split('');
           return $.inArray(String.fromCharCode(e.which), chars) === -1;
         case ('wl') :
-          chars = this.options.settings.get('validation_string').split("");
+          chars = this.options.settings.get('validation_string').split('');
           return $.inArray(String.fromCharCode(e.which), chars) !== -1;
       }
 
@@ -93,14 +97,14 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
       // Disables editing of the field while still letting users see the value (true = readonly)
       {id: 'readonly', type: 'Boolean', default_value: false, ui: 'checkbox'},
       // Adjusts the max width of the input (Small, Medium, Large)
-      {id: 'size', type: 'String', default_value: 'large', ui: 'select', options: {options: {'large':__t('size_large'),'medium':__t('size_medium'),'small':__t('size_small')} }},
+      {id: 'size', type: 'String', default_value: 'large', ui: 'select', options: {options: {'large': __t('size_large'), 'medium': __t('size_medium'), 'small': __t('size_small')}}},
       // Grayed out default placeholder text in the input when it's empty
       {id: 'placeholder_text', type: 'String', default_value: '', ui: 'textinput', char_length: 200},
       // Chooses the type of validation used on this field
       // * Character Blacklist: Choose the specific characters **not** allowed in the input
       // * Character Whitelist: Choose the specific characters allowed in the input
       // * RegEx: Create a regular expression to validate the value. Useful for emails, phone number formatting, or almost anything
-      {id: 'validation_type', type: 'String', ui: 'select', options: {options: {'bl':__t('character_blacklist'),'wl':__t('character_whitelist'),'rgx':__t('regex')} }, default_value:'rgx'},
+      {id: 'validation_type', type: 'String', ui: 'select', options: {options: {'bl': __t('character_blacklist'), 'wl': __t('character_whitelist'), 'rgx': __t('regex')} }, default_value: 'rgx'},
       // Holds the CSV list of Whitelist/Blacklist characters or the RegEx value (based on the above option)
       {id: 'validation_string', type: 'String', default_value: '', ui: 'textinput', char_length: 200, comment: __t('textinput_validation_string_comment')},
       // A message that is shown to the user if the validation fails
@@ -120,14 +124,14 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
 
       switch(options.settings.get('validation_type')) {
         case ('wl') :
-          var Regex = new RegExp("^["+options.settings.get('validation_string')+"]+$");
+          var Regex = new RegExp('^[' + options.settings.get('validation_string') + ']+$');
           if(!value.match(Regex)) {
             return validationMessage;
           }
           break;
         case ('bl') :
-          var chars = options.settings.get('validation_string').split("");
-          if(chars.length > 0 && value.match(chars.join("|"))) {
+          var chars = options.settings.get('validation_string').split('');
+          if(chars.length > 0 && value.match(chars.join('|'))) {
             return validationMessage;
           }
           break;
@@ -140,7 +144,7 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
       }
     },
     list: function(options) {
-      return (options.value) ? options.value.toString().replace(/<(?:.|\n)*?>/gm, '').substr(0,100) : '';
+      return (options.value) ? options.value.toString().replace(/<(?:.|\n)*?>/gm, '').substr(0, 100) : '';
     }
   });
 
