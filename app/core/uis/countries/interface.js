a6dac9d0b925f262c11b37fdf026c6736b30e6ad
Welling Guzman <hola@wellingguzman.com>
2017-04-09 21:49:38 -0400

    fix countries interface options

--- app/core/uis/countries/interface.js
+++ app/core/uis/countries/interface.js
@@ -1,21 +1,38 @@
 define(['app', 'underscore', 'core/uis/select'], function (app, _, SelectInterface) {
 
+  // set (forced) the countries list as select options
+  function setOptions(options) {
+
+    options.settings.set('options', app.countries);
+
+    return options;
+  }
+
   var originalInput = SelectInterface.prototype.Input;
 
   var Input = originalInput.extend({
     initialize: function () {
       originalInput.prototype.initialize.apply(this, arguments);
 
-      this.options.settings.set('options', app.countries);
+      this.options = setOptions(this.options);
     }
   });
 
   return SelectInterface.extend({
     id: 'countries',
+
     dataTypes: ['CHAR', 'VARCHAR'],
+
     variables: _.filter(SelectInterface.prototype.variables, function (variable) {
       return variable.id !== 'options';
     }),
-    Input: Input
+
+    Input: Input,
+
+    list: function (options) {
+      options = setOptions(options);
+
+      return SelectInterface.prototype.list.apply(this, [options]);
+    }
   });
 });
