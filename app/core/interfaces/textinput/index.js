61e332952e1be94d46a7465a305d1a6737566978
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-12 20:56:14 +0200

    Upgrade Textinput (#1394)
    * Split markup and logic
    
    * Replace en default validation message with localized one
    
    * Fix character count
    
    * Minor codestyle improvements
    
    * Remove jQuery dependency
    
    * Fix whitelist option
    
    * Show required message instead of validation when empty and required
    
    * make array check backwards compatible

--- app/core/interfaces/textinput/index.js
+++ app/core/interfaces/textinput/index.js
@@ -19,24 +19,23 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
 
   'use strict';
 
-  // Template Used for this UI
-  var template = '<div class="char-count-container {{size}}"> \
-                    <input type="text" placeholder="{{placeholder}}" value="{{value}}" name="{{name}}" id="{{name}}" maxLength="{{maxLength}}" class="{{size}}" {{#if readonly}}readonly{{/if}}/> \
-                  {{#if length}}<span class="char-count hide">{{characters}}</span>{{/if}}</div>';
-
   var Input = UIView.extend({
-    templateSource: template,
+    template: 'textinput/input',
 
     // Event Declarations
     events: {
       // Show character counter when input gains focus
-      'focus input': function() { this.$el.find('.char-count').removeClass('hide'); },
+      'focus input': function() {
+        this.$el.find('.char-count').removeClass('hide');
+      },
       // Update character counter when input changes
       'input input': 'updateMaxLength',
       // Validate keypress against validation_string
       'keypress input': 'validateString',
       // Hide character counter when input loses focus
-      'blur input': function() { this.$el.find('.char-count').addClass('hide'); }
+      'blur input': function() {
+        this.$el.find('.char-count').addClass('hide');
+      }
     },
 
     // Update the character counter with the remaining characters available
@@ -53,7 +52,7 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
       var value = this.options.value || '';
 
       // Fill in default value if this column has a default value.
-      if ( !value && this.options.model.isNew() && this.options.schema.has('default_value')) {
+      if (!value && this.options.model.isNew() && this.options.schema.has('default_value')) {
           value = this.options.schema.get('default_value');
       }
 
@@ -61,6 +60,7 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
         size: this.options.settings.get('size'),
         value: value,
         name: this.options.name,
+        length: length,
         maxLength: length,
         characters: length - value.toString().length,
         comment: this.options.schema.get('comment'),
@@ -71,22 +71,22 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
     // Validate String  Checks the passed in value against validation_string
     // @param e : keypress event object
     validateString: function(e) {
-      var validationMessage = this.options.settings.get('validation_message') || app.DEFAULT_VALIDATION_MESSAGE;
+      var validationMessage = this.options.settings.get('validation_message') || __t('confirm_invalid_value');
       var chars;
       switch(this.options.settings.get('validation_type')) {
         case ('bl') :
-          chars = this.options.settings.get('validation_string').split("");
-          return $.inArray(String.fromCharCode(e.which), chars) === -1;
+          chars = this.options.settings.get('validation_string').split('');
+          return chars.indexOf(String.fromCharCode(e.which)) === -1;
         case ('wl') :
-          chars = this.options.settings.get('validation_string').split("");
-          return $.inArray(String.fromCharCode(e.which), chars) !== -1;
+          chars = this.options.settings.get('validation_string').split('');
+          return chars.indexOf(String.fromCharCode(e.which)) !== -1;
       }
 
       return true;
     },
 
     initialize: function() {
-      this.maxCharLength = this.options.schema.get('char_length');
+      this.maxCharLength = this.options.schema.get('length');
     }
   });
 
@@ -97,26 +97,26 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
       // Disables editing of the field while still letting users see the value (true = readonly)
       {id: 'readonly', type: 'Boolean', default_value: false, ui: 'checkbox'},
       // Adjusts the max width of the input (Small, Medium, Large)
-      {id: 'size', type: 'String', default_value: 'large', ui: 'select', options: {options: {'large':__t('size_large'),'medium':__t('size_medium'),'small':__t('size_small')} }},
+      {id: 'size', type: 'String', default_value: 'large', ui: 'select', options: {options: {'large': __t('size_large'), 'medium': __t('size_medium'), 'small': __t('size_small')}}},
       // Grayed out default placeholder text in the input when it's empty
-      {id: 'placeholder_text', type: 'String', default_value: '', ui: 'textinput', char_length:200},
+      {id: 'placeholder_text', type: 'String', default_value: '', ui: 'textinput', char_length: 200},
       // Chooses the type of validation used on this field
       // * Character Blacklist: Choose the specific characters **not** allowed in the input
       // * Character Whitelist: Choose the specific characters allowed in the input
       // * RegEx: Create a regular expression to validate the value. Useful for emails, phone number formatting, or almost anything
-      {id: 'validation_type', type: 'String', ui: 'select', options: {options: {'bl':__t('character_blacklist'),'wl':__t('character_whitelist'),'rgx':__t('regex')} }, default_value:'rgx'},
+      {id: 'validation_type', type: 'String', ui: 'select', options: {options: {'bl': __t('character_blacklist'), 'wl': __t('character_whitelist'), 'rgx': __t('regex')} }, default_value: 'rgx'},
       // Holds the CSV list of Whitelist/Blacklist characters or the RegEx value (based on the above option)
-      {id: 'validation_string', type: 'String', default_value: '', ui: 'textinput', char_length:200, comment: __t('textinput_validation_string_comment')},
+      {id: 'validation_string', type: 'String', default_value: '', ui: 'textinput', char_length: 200, comment: __t('textinput_validation_string_comment')},
       // A message that is shown to the user if the validation fails
-      {id: 'validation_message', type: 'String', default_value: '', ui: 'textinput', char_length:200}
+      {id: 'validation_message', type: 'String', default_value: '', ui: 'textinput', char_length: 200}
     ],
     Input: Input,
     validate: function(value, options) {
-      var validationMessage = options.settings.get('validation_message') || app.DEFAULT_VALIDATION_MESSAGE;
+      var validationMessage = options.settings.get('validation_message') || __t('confirm_invalid_value');
 
       if (_.isEmpty(value)) {
         if (options.schema.get('required') === true) {
-          return validationMessage;
+          return __t('this_field_is_required');
         }
 
         return;
@@ -124,14 +124,14 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
 
       switch(options.settings.get('validation_type')) {
         case ('wl') :
-          var Regex = new RegExp("^["+options.settings.get('validation_string')+"]+$");
+          var Regex = new RegExp('^[' + options.settings.get('validation_string') + ']+$');
           if(!value.match(Regex)) {
             return validationMessage;
           }
           break;
         case ('bl') :
-          var chars = options.settings.get('validation_string').split("");
-          if(chars.length > 0 && value.match(chars.join("|"))) {
+          var chars = options.settings.get('validation_string').split('');
+          if(chars.length > 0 && value.match(chars.join('|'))) {
             return validationMessage;
           }
           break;
@@ -144,7 +144,7 @@ define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function(app, UICom
       }
     },
     list: function(options) {
-      return (options.value) ? options.value.toString().replace(/<(?:.|\n)*?>/gm, '').substr(0,100) : '';
+      return (options.value) ? options.value.toString().replace(/<(?:.|\n)*?>/gm, '').substr(0, 100) : '';
     }
   });
 
