c409ff6a7b61aec13128f35a183ad1d725714d78
WorksDev <noc@itworks.ovh>
2017-04-15 16:14:06 +0200

    Removed static Text, replaced with language identifiers

--- tests/api/Database/Object/TableTest.php
+++ tests/api/Database/Object/TableTest.php
@@ -18,6 +18,7 @@ class TableTest extends PHPUnit_Framework_TestCase
             'date_create_column' => 'created_at',
             'date_update_column' => 'updated_at',
             'footer' => null,
+            'list_view' => null,
             'column_groupings' => null,
             'filter_column_blacklist' => null,
             'primary_column' => 'name'
