f34f6947e4acbb849755938d01021d396dfffd36
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-26 21:11:08 +0200

    Upgrade select interface (#1426)
    * Reorganize select interface
    
    * Update path in uimanager
    
    * Fix minor codestyle issues
    
    * [WIP] Add setting for multi_select and dropdown/radio
    
    * Add styling for multi_select dropdown interface
    
    * Add html for radio/checkbox input type
    
    * Lint interface and component
    
    * Fix name location in handlebars
    
    * Add input_type and delimiter options
    
    * Save and retrieve checkbox values
    
    * Fix multi-select checkboxes saving , when non selected
    
    * Fix list not respecting show-setting
    
    * Style custom checkboxes / radio
    
    * Add focus style to dropdown single-select
    
    * Remove standalone multi_select interface
    
    * Remove standalone radiobuttons interface
    
    * Remove radiobuttons and multi_select from UIManager

---
+++ app/core/interfaces/select/component.js
@@ -0,0 +1,62 @@
+//  Select Core UI component
+//  Directus 6.0
+
+//  (c) RANGER
+//  Directus may be freely distributed under the GNU license.
+//  For all details and documentation:
+//  http://www.getdirectus.com
+
+define(['core/interfaces/select/interface', 'underscore', 'core/UIComponent', 'core/t', 'utils', 'select2'], function (Input, _, UIComponent, __t, Utils, select2) {
+  'use strict';
+
+  var SHOW_SELECT_OPTIONS = {
+    text: __t('select_ui_show_options_text'),
+    value: __t('select_ui_show_options_value')
+  };
+
+  var parseOptions = function (options) {
+    if (_.isString(options)) {
+      try {
+        options = JSON.parse(options);
+      } catch (err) {
+        options = {};
+        console.error(err);
+      }
+    }
+
+    return options;
+  };
+
+  return UIComponent.extend({
+    id: 'select',
+    dataTypes: ['VARCHAR', 'INT'],
+    variables: [
+      {id: 'options', default_value: '', ui: 'textarea', options: {rows: 25, placeholder_text: '{\n    "value1":"Option One",\n    "value2":"Option Two",\n    "value3":"Option Three"\n}'}, comment: __t('select_options_comment')},
+      {id: 'allow_null', type: 'Boolean', default_value: false, ui: 'checkbox'},
+      {id: 'select_multiple', type: 'Boolean', default_value: false, ui: 'checkbox'},
+      {id: 'display_search', type: 'String', default_value: 'auto', required: true, ui: 'select', options: {options: {auto: __t('Auto'), always: __t('Always'), never: __t('Never')}}},
+      {id: 'auto_search_limit', type: 'Number', ui: 'numeric', char_length: 20, default_value: 10, comment: __t('select_auto_search_limit_text')},
+      {id: 'list_value', type: 'String', default_value: 'value', ui: 'select', options: {options: SHOW_SELECT_OPTIONS}},
+      {id: 'placeholder_text', type: 'String', default_value: '', ui: 'textinput', char_length: 255, required: false, comment: __t('select_placeholder_text')},
+      {id: 'input_type', type: 'String', default_value: 'value', ui: 'select', default_value: 'dropdown', options: {options: {dropdown: 'Dropdown', radio: 'Radio Buttons / Checkboxes'}}},
+      {id: 'delimiter', type: 'String', default_value: ',', ui: 'textinput', char_length: 1, required: true}
+    ],
+    Input: Input,
+    validate: function (value, options) {
+      if (options.schema.isRequired() && Utils.isEmpty(value)) {
+        return __t('this_field_is_required');
+      }
+    },
+    list: function (options) {
+      var value = _.isString(options.value) ? options.value.replace(/<(?:.|\n)*?>/gm, '').substr(0, 100) : options.value;
+
+      if (options.settings.get('list_value') === 'text') {
+        options = parseOptions(options.settings.get('options'));
+
+        value = options[value];
+      }
+
+      return value;
+    }
+  });
+});
---
+++ app/core/interfaces/select/interface.js
@@ -0,0 +1,101 @@
+//  Select Core UI component
+//  Directus 6.0
+
+//  (c) RANGER
+//  Directus may be freely distributed under the GNU license.
+//  For all details and documentation:
+//  http://www.getdirectus.com
+
+define(['underscore', 'core/UIView', 'core/t', 'select2'], function (_, UIView, __t, select2) {
+  'use strict';
+
+  var parseOptions = function (options) {
+    if (_.isString(options)) {
+      try {
+        options = JSON.parse(options);
+      } catch (err) {
+        options = {};
+        console.error(err);
+      }
+    }
+
+    return options;
+  };
+
+  return UIView.extend({
+    template: 'select/input',
+
+    events: {
+      'change input[type=checkbox]': function () {
+        this.updateCheckboxValue();
+      }
+    },
+
+    updateCheckboxValue() {
+      var values = this.$el.find('input[type=checkbox]:checked');
+      var out = '';
+
+      if (values.length > 0) {
+        for (var i = 0; i < values.length; i++) {
+          out += $(values[i]).attr('name') + this.options.settings.get('delimiter');
+        }
+
+        out = out.substr(0, out.length - 1);
+      }
+
+      this.$el.find('input').val(out);
+    },
+
+    serialize: function () {
+      var value = this.options.value || '';
+      var values = value.split(this.options.settings.get('delimiter'));
+      var options = this.options.settings.get('options');
+
+      options = parseOptions(options);
+
+      options = _.map(options, function (value, key) {
+        var item = {};
+        item.value = value;
+        item.key = key;
+        item.selected = ($.inArray(item.key, values) !== -1);
+        return item;
+      });
+
+      return {
+        options: options,
+        name: this.options.name,
+        comment: this.options.schema.get('comment'),
+        readonly: !this.options.canWrite,
+        allow_null: this.options.settings.get('allow_null') === true,
+        display_search: this.options.settings.get('display_search'),
+        auto_search_limit: this.options.settings.get('auto_search_limit'),
+        placeholder_text: (this.options.settings.get('placeholder_text')) ? this.options.settings.get('placeholder_text') : __t('select_from_below'),
+        select_type: this.options.settings.get('input_type') === 'dropdown',
+        multiselect: this.options.settings.get('select_multiple') === true
+      };
+    },
+    initialize: function () {
+      var isMobile = navigator.userAgent.match(/(iP(hone|od|ad)|Android|IEMobile)/);
+      if (!isMobile) {
+        var self = this;
+        setTimeout(function () {
+          var options = {
+            placeholder: self.options.settings.get('placeholder_text'),
+            multiple: self.options.settings.get('select_multiple'),
+            width: '400px'
+          };
+          if (self.options.settings.get('display_search') === 'auto') {
+            options.minimumResultsForSearch = self.options.settings.get('auto_search_limit') || 10;
+          } else if (self.options.settings.get('display_search') === 'never') {
+            options.minimumResultsForSearch = Infinity;
+          }
+          if (self.options.settings.get('allow_null')) {
+            options.allowClear = true;
+          }
+          self.$el.find('select').select2(options);
+        }, 0);
+      }
+      this.updateCheckboxValue();
+    }
+  });
+});
