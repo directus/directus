c3b2282315c767e10c9a1f9932262e329b3c1e8c
Rijk van Zanten <rijkvanzanten@me.com>
2017-04-27 20:11:39 +0200

    Add custom material-icon buttons

--- app/core/interfaces/wysiwyg/interface.js
+++ app/core/interfaces/wysiwyg/interface.js
@@ -1,4 +1,67 @@
 define(['core/UIView', 'core/interfaces/wysiwyg/vendor/medium-editor.min'], function (UIView, MediumEditor) {
+  var buttonTypes = {
+    bold: {
+      name: 'bold',
+      action: 'bold',
+      aria: 'bold',
+      tagNames: ['b', 'strong'],
+      contentDefault: '<i class="material-icons">format_bold</i>'
+    },
+    italic: {
+      name: 'italic',
+      action: 'italic',
+      aria: 'italic',
+      tagNames: ['i', 'em'],
+      contentDefault: '<i class="material-icons">format_italic</i>'
+    },
+    underline: {
+      name: 'underline',
+      action: 'underline',
+      aria: 'underline',
+      tagNames: ['u'],
+      contentDefault: '<i class="material-icons">format_underline</i>'
+    },
+    strikethrough: {
+      name: 'strikethrough',
+      action: 'strikethrough',
+      aria: 'strikethrough',
+      tagNames: ['s', 'strike'],
+      contentDefault: '<i class="material-icons">strikethrough_s</i>'
+    },
+    subscript: 'subscript', // Use default
+    superscript: 'superscript', // Use default
+    anchor: 'anchor', // Use default
+    quote: {
+      name: 'quote',
+      action: 'append-blockquote',
+      aria: 'quote',
+      tagNames: ['blockquote', 'q'],
+      contentDefault: '<i class="material-icons">format_quote</i>'
+    },
+    pre: 'pre', // Use default
+    orderedlist: {
+      name: 'orderedlist',
+      action: 'insertorderedlist',
+      aria: 'orderedlist',
+      tagNames: ['ol'],
+      contentDefault: '<i class="material-icons">format_list_numbered</i>'
+    },
+    unorderedlist: {
+      name: 'unorderedlist',
+      action: 'insertunorderedlist',
+      aria: 'unorderedlist',
+      tagNames: ['ul'],
+      contentDefault: '<i class="material-icons">format_list_bulleted</i>'
+    },
+    h1: 'h1', // Use default
+    h2: 'h2', // Use default
+    h3: 'h3', // Use default
+    h4: 'h4', // Use default
+    h5: 'h5', // Use default
+    h6: 'h6', // Use default
+    removeFormat: 'removeFormat' // Use default
+  };
+
   return UIView.extend({
     template: 'wysiwyg/input',
     serialize: function () {
@@ -9,10 +72,11 @@ define(['core/UIView', 'core/interfaces/wysiwyg/vendor/medium-editor.min'], func
       };
     },
     afterRender: function () {
-      console.log(this.options.settings.get('buttons').split(','));
-      var editor = new MediumEditor('#wysiwyg-interface_' + this.options.name, {
+      new MediumEditor('#wysiwyg-interface_' + this.options.name, {
         toolbar: {
-          buttons: this.options.settings.get('buttons').split(',')
+          buttons: this.options.settings.get('buttons').split(',').map(function (buttonName) {
+            return buttonTypes[buttonName];
+          })
         }
       });
     }
