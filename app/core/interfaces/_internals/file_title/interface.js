4e165d3aea39a4c54e51fcaf3f1b64b14a4e84de
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-01 21:57:59 +0200

    Cleanup directus_file_title interface

---
+++ app/core/interfaces/_internals/file_title/interface.js
@@ -0,0 +1,38 @@
+define(['core/UIView'], function (UIView) {
+  'use strict';
+
+  return UIView.extend({
+    template: '_internals/file_title/input',
+
+    events: {
+      'focus input': function () {
+        this.$el.find('.char-count').removeClass('hide');
+      },
+      'input input': 'updateMaxLength',
+      'blur input': function () {
+        this.$el.find('.char-count').addClass('hide');
+      }
+    },
+
+    updateMaxLength: function (e) {
+      var length = this.options.schema.get('char_length') - e.target.value.length;
+      this.$el.find('.char-count').html(length);
+    },
+
+    serialize: function () {
+      var length = this.options.schema.get('char_length');
+      var value = this.options.value || '';
+      var readonly = false;
+
+      return {
+        size: this.options.settings.get('size'),
+        value: value,
+        name: this.options.name,
+        maxLength: length,
+        characters: length - value.toString().length,
+        comment: this.options.schema.get('comment'),
+        readonly: readonly
+      };
+    }
+  });
+});
