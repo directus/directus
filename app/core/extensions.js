d0fd9fb51268ae17a06c10ada647c7fe75c190d5
Welling Guzman <hola@wellingguzman.com>
2017-05-04 18:03:44 -0400

    fix base page view compoenent case on extensions

--- app/core/extensions.js
+++ app/core/extensions.js
@@ -1,4 +1,4 @@
-define(['backbone', 'core/directus', 'core/basePageView'], function(Backbone, Directus, BasePageView) {
+define(['backbone', 'core/directus', 'core/BasePageView'], function (Backbone, Directus, BasePageView) {
   return {
     Router: Directus.SubRoute,
     View: Backbone.Layout.extend({
