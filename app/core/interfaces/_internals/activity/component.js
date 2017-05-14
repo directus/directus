18f1a09ed8698c1e15c23d66f2b816d53060fc0d
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-14 21:35:48 +0200

    Remove intro comment _internals/activity

--- app/core/interfaces/_internals/activity/component.js
+++ app/core/interfaces/_internals/activity/component.js
@@ -1,11 +1,3 @@
-//  Directus User List View component
-//  Directus 6.0
-
-//  (c) RANGER
-//  Directus may be freely distributed under the GNU license.
-//  For all details and documentation:
-//  http://www.getdirectus.com
-
 define(['app', 'core/UIComponent', 'core/UIView', 'core/t'], function (app, UIComponent, UIView, __t) {
   'use strict';
 
