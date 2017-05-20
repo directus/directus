e4d47b134eb279270f81cdeb8e77a8d44ac9e44c
Ben Haynes <ben@rngr.org>
2017-05-20 11:51:14 -0400

    email template updates

--- api/core/helpers/mail.php
+++ api/core/helpers/mail.php
@@ -56,10 +56,26 @@ if (!function_exists('send_message_notification_email')) {
      *
      * @param $user
      * @param array $payload
+     *   [from] => 1
+     *   [responses] => Array()
+     *   [subject] => RE: Subject
+     *   [recipients] => 0_1,0_2,0_1
+     *   [response_to] => 3
+     *   [message] => Lorem ipsum!
+     *   [attachment] =>
+     *   [id] => 15
      */
     function send_message_notification_email($user, array $payload)
     {
-        $data = ['message' => $payload['message']];
+        $adapter = \Directus\Bootstrap::get('zendDb');
+        $acl = \Directus\Bootstrap::get('acl');
+        $table = new \Directus\Database\TableGateway\DirectusUsersTableGateway($adapter, $acl);
+        $sender = $table->find($payload['from']);
+        $data = [
+            'message' => $payload['message'],
+            'sender' => $sender,
+            'message_id' => ($payload['response_to'])? $payload['response_to'] : $payload['id']
+        ];
         Mail::send('mail/notification.twig.html', $data, function (Swift_Message $message) use ($user, $payload) {
             $message->setSubject($payload['subject']);
             $message->setTo($user['email']);
@@ -97,7 +113,7 @@ if (!function_exists('send_user_invitation_email')) {
         $data = ['token' => $token];
         Mail::send('mail/user-invitation.twig.html', $data, function (Swift_Message $message) use ($email) {
             // TODO: Add a proper invitation subject
-            $message->setSubject('Invitation');
+            $message->setSubject('Invitation to Directus');
             $message->setTo($email);
         });
     }
