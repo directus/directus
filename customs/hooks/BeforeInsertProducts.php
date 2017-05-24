f2625a36e61f8f17be8a40c4e969d79e12890a2f
Welling Guzman <hola@wellingguzman.com>
2017-05-24 16:54:21 -0400

    not dumping files

--- customs/hooks/BeforeInsertProducts.php
+++ customs/hooks/BeforeInsertProducts.php
@@ -17,7 +17,6 @@ class BeforeInsertProducts implements HookInterface
         // set the product sku before insert
         // $payload->set('sku', 'value');
 
-        file_put_contents('t-' . time() . '.txt', time());
         // make sure to return the payload
         return $payload;
     }
