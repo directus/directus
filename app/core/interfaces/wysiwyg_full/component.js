cda77c0e8909b5321c7f712fd1b093bc01b0466a
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-24 22:15:44 +0200

    Add options for format menu

--- app/core/interfaces/wysiwyg_full/component.js
+++ app/core/interfaces/wysiwyg_full/component.js
@@ -3,7 +3,74 @@ define(['./interface', 'core/UIComponent', 'core/t'], function (Input, UICompone
     id: 'wysiwyg_full',
     dataTypes: ['VARCHAR', 'TEXT', 'MEDIUMTEXT', 'LONGTEXT'],
     Input: Input,
-    variables: [],
+    variables: [
+      {id: 'show_element_path', ui: 'checkbox', default: true},
+      {
+        id: 'headings',
+        ui: 'select',
+        default_value: 'h2,h3,h4',
+        options: {
+          select_multiple: true,
+          input_type: 'radio',
+          options: {
+            h1: 'Heading 1',
+            h2: 'Heading 2',
+            h3: 'Heading 3',
+            h4: 'Heading 4',
+            h5: 'Heading 5',
+            h6: 'Heading 6'
+          }
+        }
+      },
+      {
+        id: 'inline',
+        ui: 'select',
+        default_value: 'bold,italic,underline',
+        options: {
+          select_multiple: true,
+          input_type: 'radio',
+          options: {
+            bold: 'Bold',
+            italic: 'Italic',
+            underline: 'Underline',
+            strikethrough: 'Strikethrough',
+            superscript: 'Superscript',
+            subscript: 'Subscript',
+            code: 'Code'
+          }
+        }
+      },
+      {
+        id: 'blocks',
+        ui: 'select',
+        default_value: 'p,blockquote',
+        options: {
+          select_multiple: true,
+          input_type: 'radio',
+          options: {
+            p: 'Paragraph',
+            blockquote: 'Blockquote',
+            div: 'Div',
+            pre: 'Pre'
+          }
+        }
+      },
+      {
+        id: 'alignment',
+        ui: 'select',
+        default_value: '',
+        options: {
+          select_multiple: true,
+          input_type: 'radio',
+          options: {
+            alignleft: 'Left',
+            aligncenter: 'Center',
+            alignright: 'Right',
+            alignjustify: 'Justify'
+          }
+        }
+      }
+    ],
     validate: function (value, options) {
       if (options.view.isRequired() && !value) {
         return __t('this_field_is_required');
--- app/core/interfaces/wysiwyg_full/interface.js
+++ app/core/interfaces/wysiwyg_full/interface.js
@@ -9,8 +9,83 @@ define(['core/UIView', 'tinyMCE', 'Utils'], function (UIView, tinyMCE, Utils) {
       };
     },
     afterRender: function () {
+      function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
+      var settings = this.options.settings;
+
+      var elementpath = Boolean(Number(settings.get('show_element_path')));
+
+      // Format menu (styleselect) options
+      var styleFormats = [];
+
+      var headings = settings.get('headings').split(',');
+
+      if (headings.length > 0) {
+        styleFormats.push({
+          title: 'Headings',
+          items: headings.map(function (heading) {
+            return {
+              title: 'Heading ' + heading.slice(-1), // Heading + last char of h1 thru h6 = 1...6
+              format: heading
+            };
+          })
+        });
+      }
+
+      var inline = settings.get('inline').split(',');
+
+      if (inline.length > 0) {
+        styleFormats.push({
+          title: 'Inline',
+          items: inline.map(function (option) {
+            return {
+              title: capitalizeFirstLetter(option),
+              icon: option,
+              format: option
+            };
+          })
+        });
+      }
+
+      var blocks = settings.get('blocks').split(',');
+
+      if (blocks.length > 0) {
+        styleFormats.push({
+          title: 'Blocks',
+          items: blocks.map(function (option) {
+            return {
+              title: option === 'p' ? 'Paragraph' : capitalizeFirstLetter(option),
+              format: option
+            };
+          })
+        });
+      }
+
+      var alignment = settings.get('alignment').split(',');
+
+      if (alignment.length > 0) {
+        styleFormats.push({
+          title: 'Alignment',
+          items: alignment.map(function (option) {
+            return {
+              title: capitalizeFirstLetter(option.substring(5)),
+              icon: option,
+              format: option
+            };
+          })
+        });
+      }
+
       this.editor = tinyMCE.init({
+        plugins: 'table',
         selector: '#wysiwyg_' + this.options.name,
+        branding: false,
+        elementpath: elementpath,
+        menubar: false,
+        toolbar: 'undo redo | styleselect | table',
+        style_formats: styleFormats,
         setup: function (editor) {
           var saveEditorContents = Utils.debounce(function () {
             editor.save();
@@ -26,6 +101,12 @@ define(['core/UIView', 'tinyMCE', 'Utils'], function (UIView, tinyMCE, Utils) {
           editor.on('NodeChange', saveEditorContents);
         }
       });
+    },
+    cleanup: function () {
+      // Remove tinyMCE
+      tinyMCE.remove();
+
+      delete this.editor;
     }
   });
 });
