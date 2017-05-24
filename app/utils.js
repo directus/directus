d7c8926667f55b3b141515a5ea0ede698bced5a0
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-24 20:01:20 +0200

    Add debounce function to Utils

--- app/utils.js
+++ app/utils.js
@@ -125,6 +125,73 @@ define(['underscore'], function (_) {
     return value == null || value === '';
   };
 
+  /**
+   * Returns a function, that, as long as it continues to be invoked, will not be triggerd.
+   * The function will be called after it stops being called for N ms.
+   * Based on https://github.com/component/debounce/blob/master/index.js which in turn is based on the
+   * original implemenation in Underscore (both licensed MIT)
+   */
+  Utils.debounce = function(func, wait, immediate) {
+    var timeout;
+    var args;
+    var context;
+    var timestamp;
+    var result;
+
+    if (null == wait) {
+      wait = 100;
+    }
+
+    function later() {
+      var last = Date.now() - timestamp;
+
+      if (last < wait && last >= 0) {
+        timeout = setTimeout(later, wait - last);
+      } else {
+        timeout = null;
+        if (!immediate) {
+          result = func.apply(context, args);
+          context = args = null;
+        }
+      }
+    }
+
+    var debounced = function () {
+      context = this;
+      args = arguments;
+      timestamp = Date.now();
+      var callNow = immediate && !timeout;
+      if (!timeout) {
+        timeout = setTimeout(later, wait);
+      }
+      if (callNow) {
+        result = func.apply(context, args);
+        context = args = null;
+      }
+
+      return result;
+    };
+
+    debounced.clear = function () {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+    };
+
+    debounced.flush = function () {
+      if (timeout) {
+        result = func.apply(context, args);
+        context = args = null;
+
+        clearTimeout(timeout);
+        timeout = null;
+      }
+    };
+
+    return debounced;
+  };
+
   Utils.isNothing = function (value) {
     return value === undefined
         || value === null
