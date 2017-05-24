8a137162ea06ceb4bc057c8ace3998346ee3d8c9
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-24 20:47:58 +0200

    Fix saving to DB

--- app/core/interfaces/wysiwyg_full/interface.js
+++ app/core/interfaces/wysiwyg_full/interface.js
@@ -1,4 +1,4 @@
-define(['core/UIView', 'tinyMCE'], function (UIView, tinyMCE) {
+define(['core/UIView', 'tinyMCE', 'Utils'], function (UIView, tinyMCE, Utils) {
   return UIView.extend({
     template: 'wysiwyg_full/input',
     serialize: function () {
@@ -10,7 +10,21 @@ define(['core/UIView', 'tinyMCE'], function (UIView, tinyMCE) {
     },
     afterRender: function () {
       this.editor = tinyMCE.init({
-        selector: '#wysiwyg_' + this.options.name
+        selector: '#wysiwyg_' + this.options.name,
+        setup: function (editor) {
+          var saveEditorContents = Utils.debounce(function () {
+            editor.save();
+          }, 500);
+
+          // All events on which the contents of the editor should be saved
+          editor.on('input', saveEditorContents);
+          editor.on('ExecCommand', saveEditorContents);
+          editor.on('blur', saveEditorContents);
+          editor.on('paste', saveEditorContents);
+          editor.on('undo', saveEditorContents);
+          editor.on('redo', saveEditorContents);
+          editor.on('NodeChange', saveEditorContents);
+        }
       });
     }
   });
