578f6ac5decf003263b5e323d8b9ff9ae90239a7
Rijk van Zanten <rijkvanzanten@me.com>
2017-05-27 19:19:01 +0200

    Add unit-option to slider interface

--- app/core/interfaces/slider/component.js
+++ app/core/interfaces/slider/component.js
@@ -7,7 +7,8 @@ define(['core/interfaces/slider/interface', 'core/UIComponent', 'core/t'], funct
     variables: [
       {id: 'minimum', type: 'Number', default_value: 0, ui: 'numeric'},
       {id: 'maximum', type: 'Number', default_value: 100, ui: 'numeric'},
-      {id: 'step', type: 'Number', default_value: 1, ui: 'numeric', comment: __t('slider_step_comment')}
+      {id: 'step', type: 'Number', default_value: 1, ui: 'numeric', comment: __t('slider_step_comment')},
+      {id: 'unit', default_value: '', ui: 'textinput', comment: 'Show unit next to slider value, e.g.: 15 Pounds'}
     ],
     Input: Input,
     validate: function (value, options) {
