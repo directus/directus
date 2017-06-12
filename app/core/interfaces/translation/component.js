7bd1e6392333b96505cb8e6d54360841bf098b6d
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-12 21:34:28 +0200

    Make language_code_name optional

--- app/core/interfaces/translation/component.js
+++ app/core/interfaces/translation/component.js
@@ -15,7 +15,7 @@ define(['core/UIComponent', 'core/UIView', 'core/t'], function (UIComponent, UIV
     variables: [
       {id: 'languages_table', type: 'String', default_value: '', ui: 'textinput', char_length: 255, comment: __t('translation_languages_table_comment'), required: true},
       {id: 'languages_name_column', type: 'String', default_value: '', ui: 'textinput', char_length: 255, comment: __t('translation_languages_name_column_comment'), required: true},
-      {id: 'languages_code_column', type: 'String', default_value: '', ui: 'textinput', char_length: 255, comment: __t('translation_languages_code_column_comment'), required: true},
+      {id: 'languages_code_column', type: 'String', default_value: '', ui: 'textinput', char_length: 255, comment: __t('translation_languages_code_column_comment'), required: false},
       {id: 'default_language_id', type: 'Number', ui: 'numeric', comment: __t('translation_default_language_id_comment'), required: true},
       {id: 'left_column_name', type: 'String', default_value: '', ui: 'textinput', char_length: 255, comment: __t('translation_left_column_name_comment'), required: true}
     ],
