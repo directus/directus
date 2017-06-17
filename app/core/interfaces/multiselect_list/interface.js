adf75fd3cb259e7786dd81b85df6391e5f674dc5
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-17 17:17:31 +0200

    Add multiselect_list interface (#1589)

---
+++ app/core/interfaces/multiselect_list/interface.js
@@ -0,0 +1,59 @@
+define(['core/UIView'], function (UIView) {
+  function parseOptions(options) {
+    if (_.isString(options)) {
+      try {
+        options = JSON.parse(options);
+      } catch (err) {
+        options = {};
+      }
+    }
+
+    return options;
+  }
+
+  return UIView.extend({
+    template: 'multiselect_list/input',
+    events: {
+      'change input[type=checkbox]': 'updateValue'
+    },
+    updateValue: function () {
+      var values = this.$('input[type=checkbox]:checked');
+      var delimiter = this.options.settings.get('delimiter');
+      var out = [];
+
+      if (values.length > 0) {
+        for (var i = 0; i < values.length; i++) {
+          out.push(values[i].value);
+        }
+
+        // Wrap values into delimiter
+        // easy to search values
+        out = delimiter + out.join(delimiter) + delimiter;
+      }
+
+      this.$('input#js-checkbox-values').val(out);
+      this.model.set(this.name, out);
+    },
+    serialize: function () {
+      var value = this.options.value || this.columnSchema.get('default_value') || '';
+      var values = value.split(this.options.settings.get('delimiter'));
+      var options = parseOptions(this.options.settings.get('options'));
+
+      var optionsArray = Object.keys(options).map(function (key) {
+        return {
+          key,
+          value: options[key],
+          selected: values.indexOf(key) > 0
+        };
+      });
+
+      return {
+        options: optionsArray,
+        name: this.options.name,
+        comment: this.options.schema.get('comment'),
+        readonly: !this.options.canWrite,
+        value: value
+      };
+    }
+  });
+});
