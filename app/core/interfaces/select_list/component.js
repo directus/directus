43e93603b306fcc5a5b62ac7e27102be1bae306f
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-17 16:17:55 +0200

    Add select_list interface

---
+++ app/core/interfaces/select_list/component.js
@@ -0,0 +1,21 @@
+define(['./interface', 'core/UIComponent', 'core/t', 'utils'], function (Input, UIComponent, __t, Utils) {
+  return UIComponent.extend({
+    id: 'select_list',
+    dataTypes: ['VARCHAR'],
+    variables: [
+      {id: 'options', default_value: '', ui: 'json', options: {rows: 25, placeholder_text: '{\n    "value1": "Option One",\n    "value2": "Option Two",\n    "value3": "Option Three"\n}'}, comment: __t('select_options_comment'), required: true}
+    ],
+    Input: Input,
+    validate: function (value, options) {
+      if (options.schema.isRequired() && Utils.isEmpty(value)) {
+        return __t('this_field_is_required');
+      }
+    },
+    list: function (options) {
+      // Convert default csv to csv with spaces => demo1,demo2 => demo1, demo2
+      return options.value.split(options.settings.get('delimiter')).reduce(function (string, value) {
+        return string + ', ' + value;
+      });
+    }
+  });
+});
