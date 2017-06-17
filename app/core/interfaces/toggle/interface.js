a5572ee6f7ee223d656a589f0fbdd8563b6fb791
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-17 10:52:01 +0200

    Fix checkbox style

--- app/core/interfaces/toggle/interface.js
+++ app/core/interfaces/toggle/interface.js
@@ -10,12 +10,6 @@ define(['underscore', 'utils', 'core/UIView', 'core/t'], function (_, Utils, UIV
       }
     },
 
-    updateValue: function () {
-      var value = (this.$el.find('input[type=checkbox]:checked').val() === undefined) ? 0 : 1;
-      this.$('input[type=hidden]').val(value);
-      this.model.set(this.name, value);
-    },
-
     isRequired: function () {
       var settings = this.options.settings;
 
@@ -37,7 +31,7 @@ define(['underscore', 'utils', 'core/UIView', 'core/t'], function (_, Utils, UIV
         name: this.options.name,
         selected: (value === true),
         label: this.options.settings.get('label'),
-        showAsCheckbox: this.options.settings.get('show_as_checkbox') === 1,
+        showAsCheckbox: this.options.settings.get('show_as_checkbox') == 1,
         readonly: !this.options.canWrite
       };
     }
