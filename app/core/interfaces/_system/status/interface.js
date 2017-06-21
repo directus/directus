2e740288172b30cb5fcdb34a2db8d8b543ce67fc
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-21 10:56:05 +0200

    [WIP] Work on input

--- app/core/interfaces/_system/status/interface.js
+++ app/core/interfaces/_system/status/interface.js
@@ -1,16 +1,7 @@
-define(['underscore', 'core/UIView'
-], function (_, UIView) {
-
-  'use strict';
-
-  var template = '<div class="status-group" style="padding: 12px 0;"> \
-                  {{#mapping}} \
-                    <label style="font-size:14px;margin-right:40px;display:inline-block;color:{{color}}" class="bold"><input style="display:inline-block;width:auto;margin-right:10px;" type="radio" {{#if ../readonly}}disabled{{/if}} name="{{../name}}" value="{{id}}" {{#if selected}}checked{{/if}}>{{name}}</label> \
-                  {{/mapping}} \
-                  </div>';
-
+define(['core/UIView'], function (UIView) {
   return UIView.extend({
     template: '_system/status/input',
+
     events: {
       'change input[type=radio]': function (event) {
         var statusValue = $(event.currentTarget).val();
@@ -23,29 +14,28 @@ define(['underscore', 'core/UIView'
     serialize: function () {
       var currentStatus = this.options.value;
       var model = this.model;
-      var data = {};
-      var statuses = [];
 
-      _.each(model.getStatusVisible(), function (status) {
+      var statuses = model.getStatusVisible().map(function (status) {
         var item = status.toJSON();
 
-        // NOTE: do not strictly compare as status can (will) be string
-        item.selected = status.get('id') == currentStatus;
+				// NOTE: do not strictly compare as status can (will) be string
+        item.selected = status.get('id') == currentStatus; // eslint-disable-line eqeqeq
         item.model = status;
         item.color = item.background_color || item.color;
 
-        statuses.push(item);
+        return item;
       });
 
       statuses.sort(function (a, b) {
         return a.sort < b.sort;
       });
 
-      data.name = this.options.name;
-      data.readonly = !this.options.canWrite;
-      data.mapping = statuses;
-
-      return data;
+      return {
+        name: this.options.name,
+        value: this.options.value,
+        readonly: !this.options.canWrite,
+        statuses: statuses
+      };
     }
   });
 });
