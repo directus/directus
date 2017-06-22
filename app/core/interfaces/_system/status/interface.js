66e52119f4f08b5d1a2ca7d6e85b833d1be5cccf
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-22 19:48:34 +0200

    Style _system/status interface (#1615)
    * Lint status interface component
    
    * Move input to separate file
    
    * [WIP] Work on input
    
    * Fix whitespacing / minification issue
    
    * Style status interface
    
    * Make width smaller
    
    * Change border-color to blue
    
    * Add :hover change color to blue

--- app/core/interfaces/_system/status/interface.js
+++ app/core/interfaces/_system/status/interface.js
@@ -4,16 +4,8 @@ define([
   'core/UIView'
 ], function (Utils, _, UIView) {
 
-  'use strict';
-
-  var template = '<div class="status-group" style="padding: 12px 0;"> \
-                  {{#mapping}} \
-                    <label style="font-size:14px;margin-right:40px;display:inline-block;color:{{color}}" class="bold"><input style="display:inline-block;width:auto;margin-right:10px;" type="radio" {{#if ../readonly}}disabled{{/if}} name="{{../name}}" value="{{id}}" {{#if selected}}checked{{/if}}>{{name}}</label> \
-                  {{/mapping}} \
-                  </div>';
-
   return UIView.extend({
-    templateSource: template,
+    template: '_system/status/input',
 
     events: {
       'change input[type=radio]': function (event) {
@@ -33,34 +25,33 @@ define([
 
     serialize: function () {
       var currentStatus = this.options.value;
-      var model = this.model;
-      var data = {};
-      var statuses = [];
 
       if (this.model.isNew() && Utils.isNothing(currentStatus)) {
         currentStatus = this.options.schema.get('default_value');
       }
 
-      _.each(model.getStatusVisible(), function (status) {
+      var model = this.model;
+
+      var statuses = model.getStatusVisible().map(function (status) {
         var item = status.toJSON();
 
-        // NOTE: do not strictly compare as status can (will) be string
-        item.selected = status.get('id') == currentStatus;
+				// NOTE: do not strictly compare as status can (will) be string
+        item.selected = status.get('id') == currentStatus; // eslint-disable-line eqeqeq
         item.model = status;
-        item.color = item.background_color || item.color;
 
-        statuses.push(item);
+        return item;
       });
 
       statuses.sort(function (a, b) {
         return a.sort < b.sort;
       });
 
-      data.name = this.options.name;
-      data.readonly = !this.options.canWrite;
-      data.mapping = statuses;
-
-      return data;
+      return {
+        name: this.options.name,
+        value: this.options.value,
+        readonly: !this.options.canWrite,
+        statuses: statuses
+      };
     }
   });
 });
