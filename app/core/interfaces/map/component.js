6abececda0789538e55e43562a166b888b062f6f
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-22 22:21:20 +0200

    Format options & add comments (map)

--- app/core/interfaces/map/component.js
+++ app/core/interfaces/map/component.js
@@ -6,19 +6,95 @@ define(['./interface', 'core/UIComponent', 'core/t'], function (Input, UICompone
     dataTypes: ['VARCHAR', 'ALIAS'],
     variables: [
       // Google API Key (Provided by Google)
-      {id: 'apiKey', type: 'String', default_value: '', ui: 'text_input', char_length: 200, required: true},
+      {
+        id: 'apiKey',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'Google API Key w/ Maps JS access',
+        default_value: '',
+        required: true,
+        char_length: 200
+      },
       // Column names to fill with respective item
-      {id: 'street_number_field', type: 'String', default_value: '', ui: 'text_input', char_length: 200},
-      {id: 'street_field', type: 'String', default_value: '', ui: 'text_input', char_length: 200},
-      {id: 'city_field', type: 'String', default_value: '', ui: 'text_input', char_length: 200},
-      {id: 'postal_code_field', type: 'String', default_value: '', ui: 'text_input', char_length: 200},
-      {id: 'state_field', type: 'String', default_value: '', ui: 'text_input', char_length: 200},
-      {id: 'stateCode_field', type: 'String', default_value: '', ui: 'text_input', char_length: 200},
-      {id: 'country_field', type: 'String', default_value: '', ui: 'text_input', char_length: 200},
-      {id: 'countryCode_field', type: 'String', default_value: '', ui: 'text_input', char_length: 200},
-      // Height of Map Element in Pixels
-      {id: 'mapHeight', type: 'Number', default_value: 400, ui: 'numeric', char_length: 4, comment: __t('map_mapHeight_comment')},
-      {id: 'showLatLng', type: 'Boolean', default_value: false, ui: 'toggle', comment: __t('map_showLatLng_comment')}
+      {
+        id: 'street_number_field',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'Street number column to fill with respective item',
+        default_value: '',
+        char_length: 200
+      },
+      {
+        id: 'street_field',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'Street column to fill with respective item',
+        default_value: '',
+        char_length: 200
+      },
+      {
+        id: 'city_field',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'City column to fill with respective item',
+        default_value: '',
+        char_length: 200
+      },
+      {
+        id: 'postal_code_field',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'Postal code column to fill with respective item',
+        default_value: '',
+        char_length: 200
+      },
+      {
+        id: 'state_field',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'State column to fill with respective item',
+        default_value: '',
+        char_length: 200
+      },
+      {
+        id: 'stateCode_field',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'State code column to fill with respective item',
+        default_value: '',
+        char_length: 200
+      },
+      {
+        id: 'country_field',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'Country column to fill with respective item',
+        default_value: '',
+        char_length: 200
+      },
+      {
+        id: 'countryCode_field',
+        ui: 'text_input',
+        type: 'String',
+        comment: 'Country code column to fill with respective item',
+        default_value: '',
+        char_length: 200
+      },
+      {
+        id: 'mapHeight',
+        ui: 'numeric',
+        type: 'Number',
+        comment: __t('map_mapHeight_comment'),
+        default_value: 400,
+        char_length: 4
+      },
+      {
+        id: 'showLatLng',
+        ui: 'toggle',
+        type: 'Boolean',
+        comment: __t('map_showLatLng_comment'),
+        default_value: false
+      }
     ],
     settings: [{
       collection: 'global',
