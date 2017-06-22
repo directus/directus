6654e02c89e09bcc09e6db850bde86747c17ce4c
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-22 20:19:34 +0200

    Add list_view_formatting option to checkboxes

--- app/core/interfaces/checkboxes/component.js
+++ app/core/interfaces/checkboxes/component.js
@@ -4,7 +4,8 @@ define(['./interface', 'core/UIComponent', 'core/t', 'utils'], function (Input, 
     dataTypes: ['VARCHAR'],
     variables: [
       {id: 'options', default_value: '', ui: 'json', options: {rows: 25, placeholder_text: '{\n    "value1": "Option One",\n    "value2": "Option Two",\n    "value3": "Option Three"\n}'}, comment: __t('select_options_comment'), required: true},
-      {id: 'delimiter', default_value: ',', ui: 'text_input', length: 1, required: true}
+      {id: 'delimiter', default_value: ',', ui: 'text_input', length: 1, required: true},
+      {id: 'list_view_formatting', ui: 'radio_buttons', default_value: 'text', options: {options: {text: 'Display Text', value: 'Value'}}}
     ],
     Input: Input,
     validate: function (value, options) {
@@ -14,9 +15,23 @@ define(['./interface', 'core/UIComponent', 'core/t', 'utils'], function (Input, 
     },
     list: function (options) {
       // Convert default csv to csv with spaces => demo1,demo2 => demo1, demo2
-      return options.value.split(options.settings.get('delimiter')).reduce(function (string, value) {
-        return string + ', ' + value;
-      });
+      var showAsText = options.settings.get('list_view_formatting') === 'text';
+      return options.value.split(options.settings.get('delimiter'))
+        .filter(function (value) {
+          // Filter out the first and last empty delimiter
+          return value.length > 0;
+        })
+        .map(function (value) {
+          if (showAsText) {
+            var displayOptions = JSON.parse(options.settings.get('options'));
+            return displayOptions[value];
+          }
+
+          return value;
+        })
+        .reduce(function (string, value) {
+          return string + ', ' + value;
+        });
     }
   });
 });
