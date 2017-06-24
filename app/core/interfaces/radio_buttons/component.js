ed44ad1b7f12a2e1becdabf88b4825d808d34d71
Rijk van Zanten <rijkvanzanten@me.com>
2017-06-24 15:44:01 +0200

    Add list_view_formatting option to select-interfaces (#1616)
    * Broaden check for value checkboxes
    
    * Add list_view_formatting option to checkboxes
    
    * Add list_view_formatting option to radio_buttons
    
    * Add list_view_formatting option to dropdown
    
    * Add list_view_formatting option to dropdown_multiselect

--- app/core/interfaces/radio_buttons/component.js
+++ app/core/interfaces/radio_buttons/component.js
@@ -3,7 +3,8 @@ define(['./interface', 'core/UIComponent', 'core/t', 'utils'], function (Input, 
     id: 'radio_buttons',
     dataTypes: ['VARCHAR'],
     variables: [
-      {id: 'options', default_value: '', ui: 'json', options: {rows: 25, placeholder_text: '{\n    "value1": "Option One",\n    "value2": "Option Two",\n    "value3": "Option Three"\n}'}, comment: __t('select_options_comment'), required: true}
+      {id: 'options', default_value: '', ui: 'json', options: {rows: 25, placeholder_text: '{\n    "value1": "Option One",\n    "value2": "Option Two",\n    "value3": "Option Three"\n}'}, comment: __t('select_options_comment'), required: true},
+      {id: 'list_view_formatting', ui: 'radio_buttons', default_value: 'text', options: {options: {text: 'Display Text', value: 'Value'}}}
     ],
     Input: Input,
     validate: function (value, options) {
@@ -12,6 +13,13 @@ define(['./interface', 'core/UIComponent', 'core/t', 'utils'], function (Input, 
       }
     },
     list: function (options) {
+      var showAsText = options.settings.get('list_view_formatting') === 'text';
+
+      if (showAsText) {
+        var displayOptions = JSON.parse(options.settings.get('options'));
+        return displayOptions[options.value];
+      }
+
       return options.value;
     }
   });
