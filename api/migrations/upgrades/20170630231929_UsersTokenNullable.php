b3e17f1379794216a4fbe2eb6e806e624ad84539
Nithin Alexander <nithinga@yahoo.co.in>
2017-07-05 14:32:39 +0530

    Remove migration error
    db:migrate - UsersTokenNullable - Error executing 'query' with:
    ALTER TABLE `directus_users` CHANGE `token` `token` varchar(255)
    
    Reason: Specified key was too long; max key length is 767 bytes

--- api/migrations/upgrades/20170630231929_UsersTokenNullable.php
+++ api/migrations/upgrades/20170630231929_UsersTokenNullable.php
@@ -6,6 +6,7 @@ class UsersTokenNullable extends Ruckusing_Migration_Base
     public function up()
     {
         $this->change_column('directus_users', 'token', 'string', [
+            'limit' => 128,
             'null' => true
         ]);
     }//up()
