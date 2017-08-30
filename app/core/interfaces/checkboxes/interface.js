c30d09ed495e45426e819973716128c8a572aef0
Seth J Gore <sethjgore@gmail.com>
2017-08-29 21:58:01 -0400

    Adding custom value option to checkboxes interface (#1767)
    * updated checkboxes
    
    * updated syntax formatting
    
    * removed main.css modification

--- app/core/interfaces/checkboxes/interface.js
+++ app/core/interfaces/checkboxes/interface.js
@@ -1,6 +1,7 @@
 /* global _ */
-define(['core/UIView', 'underscore'], function (UIView, _) {
-  function parseOptions(options) {
+define(['core/UIView', 'underscore'], function(UIView, _) {
+
+  function parseOptions (options) {
     if (_.isString(options)) {
       try {
         options = JSON.parse(options);
@@ -15,37 +16,70 @@ define(['core/UIView', 'underscore'], function (UIView, _) {
   return UIView.extend({
     template: 'checkboxes/input',
     events: {
-      'change input[type=checkbox]': 'updateValue'
+      'change input[type=checkbox]': 'update',
+      'change #customText': 'update'
     },
-    updateValue: function () {
-      var values = this.$('input[type=checkbox]:checked');
+    delimiterize: function (out) {
+
       var delimiter = this.options.settings.get('delimiter');
-      var out = [];
 
-      if (values.length > 0) {
-        for (var i = 0; i < values.length; i++) {
-          out.push(values[i].value);
-        }
-
-        // Wrap the values with a pair of delimiters to allow strict searching for a single value
-        if (this.options.settings.get('wrap_with_delimiter')) {
-          out = delimiter + out.join(delimiter) + delimiter;
-        } else {
-          out = out.join(delimiter);
-        }
+      if (this.options.settings.get('wrap_with_delimiter')) {
+        out = delimiter + out.join(delimiter) + delimiter;
       } else {
-        out = '';
+        out = out.join(delimiter);
       }
 
-      this.$('input#js-checkbox-values').val(out);
+      return out;
+    },
+    value: function () {
+      var checkedValues = this.$('input[type=checkbox]:checked');
+
+      var out = [];
+
+      if (checkedValues.length > 0) {
+
+        for (var i = 0; i < checkedValues.length; i++) {
+          out.push(checkedValues[i].value);
+        }
+
+        return out
+
+      } else {
+
+        out = '';
+
+        return out
+      }
+
+
+    },
+    customValue: function () {
+
+      var customValues = this.$('#customText')[0].value;
+
+      return customValues;
+    },
+
+    update: function () {
+      var out = []
+
+      out.push(this.value())
+
+      out.push(this.customValue())
+
+      out = this.delimiterize(out);
+
       this.model.set(this.name, out);
     },
     serialize: function () {
-      var value = typeof this.options.value === 'string' ? this.options.value : this.columnSchema.get('default_value') || '';
+      var value = typeof this.options.value === 'string' ? this.options.value :
+        this.columnSchema.get('default_value') || '';
+
       var values = value.split(this.options.settings.get('delimiter'));
+
       var options = parseOptions(this.options.settings.get('options'));
 
-      var optionsArray = Object.keys(options).map(function (key) {
+      var optionsArray = Object.keys(options).map(function(key) {
         return {
           key: key,
           value: options[key],
@@ -53,12 +87,20 @@ define(['core/UIView', 'underscore'], function (UIView, _) {
         };
       });
 
+      var customArray = {
+        key: 'custom',
+        value: values[values.length - 2]
+      }
+
       return {
         options: optionsArray,
+        custom: customArray,
         name: this.options.name,
         comment: this.options.schema.get('comment'),
         readOnly: this.options.settings.get('read_only') || !this.options.canWrite,
-        value: value
+        value: value,
+        allowCustomCheckboxes: this.options.settings.get(
+          'allow_custom_checkboxes')
       };
     }
   });
