f770e4aaf581486553fcbbf612ba7f68bd24c74d
Welling Guzman <hola@wellingguzman.com>
2017-10-16 19:00:05 -0400

    update FALSE to lowercase
    NULL pending

--- api/core/Directus/Util/StringUtils.php
+++ api/core/Directus/Util/StringUtils.php
@@ -53,7 +53,7 @@ class StringUtils
     public static function startsWith($haystack, $needle)
     {
         // search backwards starting from haystack length characters from the end
-        return $needle === '' || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+        return $needle === '' || strrpos($haystack, $needle, -strlen($haystack)) !== false;
     }
 
     /**
@@ -65,7 +65,8 @@ class StringUtils
     public static function endsWith($haystack, $needle)
     {
         // search forward starting from end minus needle length characters
-        return $needle === '' || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+        return $needle === ''
+            || (($temp = strlen($haystack) - strlen($needle)) >= 0&& strpos($haystack, $needle, $temp) !== false);
     }
 
     /**
