5031ce0315f7f2acb4323d7d971980c2a1d74edb
rijkvanzanten <rijkvanzanten@me.com>
2017-10-27 17:22:43 -0400

    Cleanup text_input and fix character count

--- app/core/interfaces/text_input/interface.js
+++ app/core/interfaces/text_input/interface.js
@@ -1,83 +1,95 @@
-/* global $ */
 define(['core/UIView'], function (UIView) {
   'use strict';
 
   return UIView.extend({
     template: 'text_input/input',
 
-    // Event Declarations
     events: {
-      // Show character counter when input gains focus
-      'focus input': function () {
-        this.$el.find('.char-count').removeClass('hide');
-      },
-      // Update character counter when input changes
-      'input input': 'onChangeInput',
-      // Validate keypress against validation_string
-      'keypress input': 'validateString',
-      // Hide character counter when input loses focus
-      'blur input': function () {
-        this.$el.find('.char-count').addClass('hide');
-      }
+      'focus input': 'toggleHideClass',
+      'blur input': 'toggleHideClass',
+      'input input': 'saveAndUpdateCharCount',
+      'keypress input': 'validateString'
     },
 
-    onChangeInput: function (event) {
-      var $input = $(event.currentTarget);
+    /**
+     * Serialize the data that's being send to the Handlebars template
+     * @return {Object} Locals for Handlebars template
+     */
+    serialize: function () {
+      var schema = this.options.schema;
+      var settings = this.options.settings;
 
-      // NOTE: change silently to prevent "widgets" to display unsaved changes
-      // Ex: the user first/last name in the sidebar
+      var name = this.options.name;
+      var length = schema.get('length');
+      var value = this.options.value || schema.get('default_value') || '';
+
+      var autoSize = settings.get('size') === 'auto';
+      var charsLeft = length - value.toString().length;
+      var placeholder = settings.get('placeholder') || '';
+      var readOnly = settings.get('read_only') || !this.options.canWrite;
+      var showCharacterCount = this.options.schema.get('length');
+      var size = settings.get('size');
+
+      return {
+        autoSize: autoSize,
+        charsLeft: charsLeft,
+        maxLength: length,
+        name: name,
+        placeholder: placeholder,
+        readOnly: readOnly,
+        showCharacterCount: showCharacterCount,
+        size: size,
+        value: value
+      };
+    },
+
+    /**
+     * Toggle the class 'hide' on the .char-count element
+     */
+    toggleHideClass: function () {
+      this.$el.find('.char-count').toggleClass('hide');
+    },
+
+    /**
+     * Save value to model and update character count div
+     * @param  {Object} event The input's input event
+     */
+    saveAndUpdateCharCount: function (event) {
+      var $input = this.$(event.currentTarget);
+
+      // Set silently to prevent "widgets" to display unsaved changes
+      //   Ex: the user first/last name in the sidebar
       this.model.set(this.name, $input.val(), {silent: true});
 
-      this.updateMaxLength($input.val().length);
-    },
-
-    // Update the character counter with the remaining characters available
-    updateMaxLength: function (length) {
-      if (this.maxCharLength) {
-        var charsLeft = this.maxCharLength - length;
+      var maxLength = this.options.schema.get('length');
+      if (maxLength) {
+        var charsLeft = maxLength - $input.val().length;
         this.$el.find('.char-count').html(charsLeft);
       }
     },
 
-    // Called before template is rendered, serialize returns an object that gets used as data for template string
-    serialize: function () {
-      var length = this.maxCharLength;
-      var value = this.options.value || this.options.schema.get('default_value') || '';
-
-      return {
-        size: this.options.settings.get('size'),
-        value: value,
-        name: this.options.name,
-        length: length,
-        maxLength: length,
-        characters: length - value.toString().length,
-        comment: this.options.schema.get('comment'),
-        readOnly: this.options.settings.get('read_only') || !this.options.canWrite,
-        autoSize: this.options.settings.get('size') === 'auto',
-        placeholder: (this.options.settings) ? this.options.settings.get('placeholder') : ''
-      };
-    },
-    // Validate String  Checks the passed in value against validation_string
-    // @param e : keypress event object
-    validateString: function (e) {
+    /**
+     * Checks the passed in value against the validation_string
+     * @param  {Object} event The input's keypress event
+     * @return {Boolean} Validates or not
+     */
+    validateString: function (event) {
       var chars;
+      var validationType = this.options.settings.get('validation_type');
+      var validationString = this.options.settings.get('validation_string');
 
-      switch (this.options.settings.get('validation_type')) {
+      switch (validationType) {
         case ('bl') :
-          chars = this.options.settings.get('validation_string').split('');
-          return chars.indexOf(String.fromCharCode(e.which)) === -1;
+          chars = validationString.split('');
+          return chars.indexOf(String.fromCharCode(event.which)) === -1;
         case ('wl') :
-          chars = this.options.settings.get('validation_string').split('');
-          return chars.indexOf(String.fromCharCode(e.which)) !== -1;
+          chars = validationString.split('');
+          return chars.indexOf(String.fromCharCode(event.which)) !== -1;
         default:
           break;
       }
 
       return true;
-    },
-
-    initialize: function () {
-      this.maxCharLength = this.options.schema.get('length');
     }
   });
 });
