65b7647c45bfe9eb9fbb11815733b486155b3d7b
rijkvanzanten <rijkvanzanten@me.com>
2020-02-07 16:35:09 -0500

    Use global vue plugins in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,8 @@
-import Vue from "vue";
-import VueCompositionAPI from "@vue/composition-api";
 import { addParameters } from '@storybook/vue';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
-import "../src/styles/_base.scss";
-import "../src/styles/_fonts.scss";
-import "../src/styles/_variables.scss";
-
-Vue.use(VueCompositionAPI);
+import "../src/styles/main.scss";
+import "../src/plugins";
 
 addParameters({
 	docs: {
