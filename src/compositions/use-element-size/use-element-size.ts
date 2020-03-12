101abb96348a2cac9e438d6d4d58c8d7f2493939
Rijk van Zanten <rijkvanzanten@me.com>
2020-03-12 13:58:41 -0400

    Use element size (#169)
    * Install resize observer
    
    * Add use-element-size composition
    
    * Rename function to match filename
    
    * Remove false statement from readme

---
+++ src/compositions/use-element-size/use-element-size.ts
@@ -0,0 +1,27 @@
+import { Ref, ref, isRef, onMounted, onUnmounted } from '@vue/composition-api';
+import { notEmpty } from '@/utils/is-empty';
+import { ResizeObserver } from 'resize-observer';
+
+export default function useElementSize<T extends Element>(target: T | Ref<T> | Ref<null>) {
+	const width = ref(0);
+	const height = ref(0);
+
+	const resizeObserver = new ResizeObserver(([entry]) => {
+		width.value = entry.contentRect.width;
+		height.value = entry.contentRect.height;
+	});
+
+	onMounted(() => {
+		const t = isRef(target) ? target.value : target;
+
+		if (notEmpty(t)) {
+			resizeObserver.observe(t);
+		}
+	});
+
+	onUnmounted(() => {
+		resizeObserver.disconnect();
+	});
+
+	return { width, height };
+}
