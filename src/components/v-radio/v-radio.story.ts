61d991f4bfe5b091711c54b4a7961298b1b734d6
Rijk van Zanten <rijkvanzanten@me.com>
2020-04-02 19:38:46 -0400

    Radio buttons (#293)
    * Add v-radio component
    
    * Add tests, update storybook

---
+++ src/components/v-radio/v-radio.story.ts
@@ -0,0 +1,35 @@
+import withPadding from '../../../.storybook/decorators/with-padding';
+import readme from './readme.md';
+import { defineComponent, ref } from '@vue/composition-api';
+import RawValue from '../../../.storybook/raw-value.vue';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
+
+export default {
+	title: 'Components / Radio',
+	decorators: [withPadding, withKnobs],
+	parameters: {
+		notes: readme,
+	},
+};
+
+export const basic = () =>
+	defineComponent({
+		components: { RawValue },
+		props: {
+			disabled: {
+				default: boolean('Disabled', false),
+			},
+		},
+		setup() {
+			const value = ref(null);
+			return { value };
+		},
+		template: `
+			<div>
+				<v-radio :disabled="disabled" value="red" v-model="value" label="Red" />
+				<v-radio :disabled="disabled" value="blue" v-model="value" label="Blue" />
+				<v-radio :disabled="disabled" value="green" v-model="value" label="Green" />
+				<raw-value>{{ value }}</raw-value>
+			</div>
+		`,
+	});
---
+++ src/components/v-radio/v-radio.vue
@@ -0,0 +1,110 @@
+<template>
+	<button
+		class="v-radio"
+		@click="emitValue"
+		type="button"
+		:aria-pressed="isChecked ? 'true' : 'false'"
+		:disabled="disabled"
+		:class="{ checked: isChecked }"
+	>
+		<v-icon :name="icon" />
+		<span class="label type-label">
+			<slot name="label">{{ label }}</slot>
+		</span>
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from '@vue/composition-api';
+
+export default defineComponent({
+	model: {
+		prop: 'inputValue',
+		event: 'change',
+	},
+	props: {
+		value: {
+			type: String,
+			required: true,
+		},
+		inputValue: {
+			type: String,
+			default: null,
+		},
+		label: {
+			type: String,
+			default: null,
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	setup(props, { emit }) {
+		const isChecked = computed<boolean>(() => {
+			return props.inputValue === props.value;
+		});
+
+		const icon = computed<string>(() => {
+			return isChecked.value ? 'radio_button_checked' : 'radio_button_unchecked';
+		});
+
+		return { isChecked, emitValue, icon };
+
+		function emitValue(): void {
+			emit('change', props.value);
+		}
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/mixins/no-wrap';
+
+.v-radio {
+	--v-radio-color: var(--primary);
+
+	display: flex;
+	align-items: center;
+	font-size: 0;
+	text-align: left;
+	background-color: transparent;
+	border: none;
+	border-radius: 0;
+	appearance: none;
+
+	.label:not(:empty) {
+		margin-left: 8px;
+
+		@include no-wrap;
+	}
+
+	& .v-icon {
+		--v-icon-color: var(--border-normal);
+	}
+
+	&:disabled {
+		cursor: not-allowed;
+
+		.label {
+			color: var(--foreground-subdued);
+		}
+
+		.v-icon {
+			--v-icon-color: var(--border-normal);
+		}
+	}
+
+	&:not(:disabled):hover {
+		.v-icon {
+			--v-icon-color: var(--border-normal);
+		}
+	}
+
+	&:not(:disabled).checked {
+		.v-icon {
+			--v-icon-color: var(--v-radio-color);
+		}
+	}
+}
+</style>
