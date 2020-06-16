e8cda9470be4ca64a7eb65ebc4b5abb5ce39c347
rijkvanzanten <rijkvanzanten@me.com>
2020-06-16 14:37:21 -0400

    Install body parser

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import bodyParser from 'body-parser';
 import { errorHandler } from './error';
 import itemsRouter from './routes/items';
 
-const app = express().use('/items', itemsRouter).use(errorHandler);
+const app = express().use(bodyParser.json()).use('/items', itemsRouter).use(errorHandler);
 
 export default app;
