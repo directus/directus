4b916248f366541570f7bf77d3d631abdff54129
rijkvanzanten <rijkvanzanten@me.com>
2020-06-19 18:32:34 -0400

    Add info endpoint

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -3,5 +3,6 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/ping', (req, res) => res.send('pong'));
+router.get('/info', (req, res) => res.json({ data: process.versions }));
 
 export default router;
