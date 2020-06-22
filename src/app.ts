a2e1e81e0ec464976c33ca3197806ce7386266bf
rijkvanzanten <rijkvanzanten@me.com>
2020-06-22 18:43:06 -0400

    Rename .env.example to example.env (for syntax highlighting)

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,15 @@ import webhooksRouter from './routes/webhooks';
 
 import notFoundHandler from './routes/not-found';
 
+import sendMail from './mail';
+
+sendMail({
+	to: 'rijkvanzanten@me.com',
+	subject: 'Test Email',
+	text: 'Hi there!',
+	html: '<h1>Hi there!</h1>',
+});
+
 const app = express()
 	.disable('x-powered-by')
 	.use(bodyParser.json())
