0c1fd6df4156ee3c9395abd172bbeba4f899fc76
rijkvanzanten <rijkvanzanten@me.com>
2020-07-07 11:30:49 -0400

    Rethrow JWT error as invalid creds

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,8 +1,9 @@
 import { RequestHandler } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
 import isJWT from '../utils/is-jwt';
 import database from '../database';
 import asyncHandler from 'express-async-handler';
+import { InvalidCredentialsException } from '../exceptions';
 
 /**
  * Verify the passed JWT and assign the user ID and role to `req`
@@ -11,12 +12,24 @@ const authenticate: RequestHandler = asyncHandler(async (req, res, next) => {
 	if (!req.token) return next();
 
 	if (isJWT(req.token)) {
-		const payload = jwt.verify(req.token, process.env.SECRET) as { id: string };
+		let payload: { id: string };
+
+		try {
+			payload = jwt.verify(req.token, process.env.SECRET) as { id: string };
+		} catch (err) {
+			if (err instanceof TokenExpiredError) {
+				throw new InvalidCredentialsException('Token expired.');
+			} else {
+				throw err;
+			}
+		}
+
 		const user = await database
 			.select('role')
 			.from('directus_users')
 			.where({ id: payload.id })
 			.first();
+
 		/** @TODO verify user status */
 		req.user = payload.id;
 		req.role = user.role;
