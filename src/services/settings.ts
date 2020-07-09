06d04fff50e029fa36d643aabb486a43994fda81
rijkvanzanten <rijkvanzanten@me.com>
2020-07-09 17:55:10 -0400

    Add item singleton support

--- src/services/settings.ts
+++ src/services/settings.ts
@@ -1,36 +1,11 @@
 import { Query } from '../types/query';
 import * as ItemsService from './items';
 import { Accountability } from '../types';
-import database, { schemaInspector } from '../database';
 
 export const readSettings = async (query: Query) => {
-	const settings = await ItemsService.readItems('directus_settings', {
-		...query,
-		limit: 1,
-	});
-
-	const settingsObj = settings[0];
-
-	if (!settingsObj) {
-		const settingsColumns = await schemaInspector.columnInfo('directus_settings');
-		const defaults = {};
-
-		for (const column of settingsColumns) {
-			defaults[column.name] = column.default_value;
-		}
-
-		return defaults;
-	}
-
-	return settingsObj;
+	return await ItemsService.readSingleton('directus_settings', query);
 };
 
 export const updateSettings = async (data: Record<string, any>, accountability: Accountability) => {
-	const record = await database.select('id').from('directus_settings').limit(1).first();
-
-	if (record) {
-		return await ItemsService.updateItem('directus_settings', record.id, data, accountability);
-	}
-
-	return await ItemsService.createItem('directus_settings', data, accountability);
+	return await ItemsService.upsertSingleton('directus_settings', data, accountability);
 };
