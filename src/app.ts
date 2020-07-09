05994e0eda05110ca05bb7bcae120c9fc44f8331
rijkvanzanten <rijkvanzanten@me.com>
2020-07-09 14:48:10 -0400

    Rename collection-presets to presets

--- src/app.ts
+++ src/app.ts
@@ -14,13 +14,13 @@ import activityRouter from './routes/activity';
 import assetsRouter from './routes/assets';
 import authRouter from './routes/auth';
 import collectionsRouter from './routes/collections';
-import collectionPresetsRouter from './routes/collection-presets';
 import extensionsRouter from './routes/extensions';
 import fieldsRouter from './routes/fields';
 import filesRouter from './routes/files';
 import foldersRouter from './routes/folders';
 import itemsRouter from './routes/items';
 import permissionsRouter from './routes/permissions';
+import presetsRouter from './routes/presets';
 import relationsRouter from './routes/relations';
 import revisionsRouter from './routes/revisions';
 import rolesRouter from './routes/roles';
@@ -45,13 +45,13 @@ const app = express()
 	.use('/activity', activityRouter)
 	.use('/assets', assetsRouter)
 	.use('/collections', collectionsRouter)
-	.use('/collection_presets', collectionPresetsRouter)
 	.use('/extensions', extensionsRouter)
 	.use('/fields', fieldsRouter)
 	.use('/files', filesRouter)
 	.use('/folders', foldersRouter)
 	.use('/items', itemsRouter)
 	.use('/permissions', permissionsRouter)
+	.use('/presets', presetsRouter)
 	.use('/relations', relationsRouter)
 	.use('/revisions', revisionsRouter)
 	.use('/roles', rolesRouter)
--- src/services/presets.ts
+++ src/services/presets.ts
@@ -4,16 +4,16 @@ import * as ItemsService from './items';
 /** @todo check if we want to save activity for collection presets */
 
 export const createCollectionPreset = async (data: Record<string, any>, query: Query) => {
-	const primaryKey = await ItemsService.createItem('directus_collection_presets', data);
-	return await ItemsService.readItem('directus_collection_presets', primaryKey, query);
+	const primaryKey = await ItemsService.createItem('directus_presets', data);
+	return await ItemsService.readItem('directus_presets', primaryKey, query);
 };
 
 export const readCollectionPresets = async (query: Query) => {
-	return await ItemsService.readItems('directus_collection_presets', query);
+	return await ItemsService.readItems('directus_presets', query);
 };
 
 export const readCollectionPreset = async (pk: string | number, query: Query) => {
-	return await ItemsService.readItem('directus_collection_presets', pk, query);
+	return await ItemsService.readItem('directus_presets', pk, query);
 };
 
 export const updateCollectionPreset = async (
@@ -21,10 +21,10 @@ export const updateCollectionPreset = async (
 	data: Record<string, any>,
 	query: Query
 ) => {
-	const primaryKey = await ItemsService.updateItem('directus_collection_presets', pk, data);
-	return await ItemsService.readItem('directus_collection_presets', primaryKey, query);
+	const primaryKey = await ItemsService.updateItem('directus_presets', pk, data);
+	return await ItemsService.readItem('directus_presets', primaryKey, query);
 };
 
 export const deleteCollectionPreset = async (pk: string | number) => {
-	await ItemsService.deleteItem('directus_collection_presets', pk);
+	await ItemsService.deleteItem('directus_presets', pk);
 };
