82f40e5379b12b7c16a489770b9f3833f71ebbaa
rijkvanzanten <rijkvanzanten@me.com>
2020-07-27 16:37:38 -0400

    Remove clear from start, add relations to seed

--- src/cli/commands/start.ts
+++ src/cli/commands/start.ts
@@ -1,13 +1,10 @@
 import logger from '../../logger';
 import getPort from 'get-port';
-import clear from 'clear';
 import dotenv from 'dotenv';
 
 export default async function start() {
 	dotenv.config();
 	const app = require('../../app').default;
-
-	clear();
 
 	const port = process.env.PORT || (await getPort({ port: 3000 }));
 
