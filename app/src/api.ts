e9f2e1757ad760a0e60a8fc6272c3dea61cd289e
rijkvanzanten <rijkvanzanten@me.com>
2020-07-30 16:17:58 -0400

    Prevent infinite refresh loop

--- app/src/api.ts
+++ app/src/api.ts
@@ -53,7 +53,7 @@ export const onError = async (error: RequestError) => {
 	/* istanbul ignore next */
 	const code = error.response?.data?.error?.code;
 
-	if (status === 401 && code === 'INVALID_CREDENTIALS' && error.response?.config?.url?.includes('refresh') === false) {
+	if (status === 401 && code === 'INVALID_CREDENTIALS' && error.request.responseURL.includes('refresh') === false) {
 		try {
 			await refresh();
 
