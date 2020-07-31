3895e367106abfdd67fb6ac069a100d7af64469c
rijkvanzanten <rijkvanzanten@me.com>
2020-07-31 16:09:13 -0400

    Split up collections in system/database

--- api/src/types/collection.ts
+++ api/src/types/collection.ts
@@ -1,11 +1,16 @@
 import { Field } from './field';
+import { Table } from 'knex-schema-inspector/lib/types/table';
 
 export type Collection = {
 	collection: string;
-	note: string | null;
-	hidden: boolean;
-	single: boolean;
-	icon: string | null;
-	translation: Record<string, string>;
 	fields?: Field[];
+	system: {
+		collection: string;
+		note: string | null;
+		hidden: boolean;
+		single: boolean;
+		icon: string | null;
+		translation: Record<string, string>;
+	} | null;
+	database: Table;
 };
