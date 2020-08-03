234f0aeb240506671c0b69c3b169f92b207c68eb
rijkvanzanten <rijkvanzanten@me.com>
2020-08-03 18:25:29 -0400

    Remove get-port

--- api/src/cli/commands/start.ts
+++ api/src/cli/commands/start.ts
@@ -1,12 +1,12 @@
 import logger from '../../logger';
-import getPort from 'get-port';
 import dotenv from 'dotenv';
 
 export default async function start() {
 	dotenv.config();
+
 	const app = require('../../app').default;
 
-	const port = process.env.PORT || (await getPort({ port: 3000 }));
+	const port = process.env.PORT;
 
 	app.listen(port, () => {
 		logger.info(`Server started at port ${port}`);
