e6aeaecf9265e10c44b7b5ea7a480535f8c75454
rijkvanzanten <rijkvanzanten@me.com>
2020-08-04 18:24:18 -0400

    Cleanup files routes / services

--- api/src/types/files.ts
+++ api/src/types/files.ts
@@ -1,6 +1,22 @@
 /** @todo finalize */
 export type File = {
 	id: string; // uuid
-	filename_disk: string;
 	storage: string;
+	filename_disk: string;
+	filename_download: string;
+	title: string | null;
+	type: string | null;
+	folder: string | null; // uuid
+	uploaded_by: string | null; // uuid
+	uploaded_on: Date;
+	charset: string | null;
+	filesize: number;
+	width: number | null;
+	height: number | null;
+	duration: number | null;
+	embed: string | null;
+	description: string | null;
+	location: string | null;
+	tags: string | null;
+	metadata: Record<string, any> | null;
 };
