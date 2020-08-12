3cc15ea2c301cf01e6ac84c36eea4ac9afef9a8a
rijkvanzanten <rijkvanzanten@me.com>
2020-08-12 14:01:17 -0400

    Fix seeder for view query on activity

--- api/src/services/relations.ts
+++ api/src/services/relations.ts
@@ -1,6 +1,10 @@
 import ItemsService from './items';
 import { AbstractServiceOptions } from '../types';
 
+/**
+ * @TODO update foreign key constraints when relations are updated
+ */
+
 export default class RelationsService extends ItemsService {
 	constructor(options?: AbstractServiceOptions) {
 		super('directus_relations', options);
