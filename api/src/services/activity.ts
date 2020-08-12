90be83cc0305347289e50bdc9c4cad41c227c9a8
rijkvanzanten <rijkvanzanten@me.com>
2020-08-12 13:27:02 -0400

    Add reminder to activity service

--- api/src/services/activity.ts
+++ api/src/services/activity.ts
@@ -1,6 +1,10 @@
 import ItemsService from './items';
 import { AbstractServiceOptions } from '../types';
 
+/**
+ * @TODO only return activity of the collections you have access to
+ */
+
 export default class ActivityService extends ItemsService {
 	constructor(options?: AbstractServiceOptions) {
 		super('directus_activity', options);
