dc2308e0ab9bac0da0e02e31bdadebe286a23f94
Nitwel <nitwel@arcor.de>
2020-08-13 10:16:10 +0200

    update config

--- api/jest.config.js
+++ api/jest.config.js
@@ -5,6 +5,5 @@ module.exports = {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  verbose: true
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
