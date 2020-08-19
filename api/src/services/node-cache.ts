b16d28ce23b6537ae54fa4b52124a1fec4f3a048
Tanya Byrne <t.byrne1@lancaster.ac.uk>
2020-08-19 18:47:45 +0100

    get set for node cache service

--- api/src/services/node-cache.ts
+++ api/src/services/node-cache.ts
@@ -3,7 +3,9 @@
  *  in case a new cache is used in future
  *  sdtTTL is the amount of time a cache should be
  *  stored, in seconds, before being deleted. Keep
- * this is mind
+ *  checkPeriod is the amount of seconds for the
+ *  autodelete to wait before checking
+ *  if cache needs to be deleted.
  */
 import NodeCache from 'node-cache';
 
@@ -18,6 +20,7 @@ export default class NodeCacheService {
 			useClones: false,
 		});
 	}
+	// need to get/set/delete cache
 
 	async flushCache() {
 		this.apiCache.flushAll();
