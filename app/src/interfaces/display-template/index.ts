b88c27dcfc9f3099a20453041f485a62e82cd45c
Nitwel <nitwel@arcor.de>
2020-08-20 16:12:20 +0200

    remove vue file and update readme

--- app/src/interfaces/display-template/index.ts
+++ app/src/interfaces/display-template/index.ts
@@ -9,31 +9,11 @@ export default defineInterface(({ i18n }) => ({
 	types: ['string'],
 	options: [
 		{
-			field: 'value',
-			name: i18n.t('value'),
-			type: 'string',
-			meta: {
-				width: 'full',
-				interface: 'text-input',
-				default_value: '',
-			},
-		},
-		{
-			field: 'disabled',
-			type: 'boolean',
-			name: i18n.t('disabled'),
-			meta: {
-				width: 'half',
-				interface: 'toggle',
-				default_value: false,
-			},
-		},
-		{
 			field: 'collection',
 			name: i18n.t('collection'),
 			type: 'string',
 			meta: {
-				width: 'half',
+				width: 'full',
 				interface: 'text-input',
 				default_value: '',
 			},
--- app/src/interfaces/display-template/readme.md
+++ app/src/interfaces/display-template/readme.md
@@ -6,7 +6,4 @@ This is the interface version of the v-field-template component.
 
 | Option        | Description                            | Default |
 |---------------|----------------------------------------|---------|
-| `placeholder` | Text to show when no input is entered  | `null`  |
-| `allow-none`  | Allow the user to deselect the value   | `false` |
-| `allow-other` | Allow the user to enter a custom value | `false` |
-| `choices`     | What choices to present to the user    | `null`  |
+| `*collection` | Fields of collection to use            | `null`  |
