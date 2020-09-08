c8c0342ae5e839e004449ba998cce68a52c3cc0a
rijkvanzanten <rijkvanzanten@me.com>
2020-09-08 13:33:18 -0400

    Replace service exceptions with serviceunavailableexception

--- api/src/exceptions/mem-cache-not-found.ts
+++
@@ -1,7 +0,0 @@
-import { BaseException } from './base';
-
-export class MemCacheNotFoundException extends BaseException {
-	constructor(message: string) {
-		super(message, 503, 'MEMCACHE_NOT_FOUND');
-	}
-}
--- api/src/exceptions/redis-not-found.ts
+++
@@ -1,7 +0,0 @@
-import { BaseException } from './base';
-
-export class RedisNotFoundException extends BaseException {
-	constructor(message: string) {
-		super(message, 503, 'REDIS_NOT_FOUND');
-	}
-}
