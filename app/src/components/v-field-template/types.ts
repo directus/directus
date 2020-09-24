4b43585dd31bc13dc8c84211f79d9357e6f38d00
rijkvanzanten <rijkvanzanten@me.com>
2020-09-24 16:08:15 -0400

    remove unused type

--- app/src/components/v-field-template/types.ts
+++ app/src/components/v-field-template/types.ts
@@ -4,7 +4,6 @@ import { TranslateResult } from 'vue-i18n';
 export type FieldTree = {
 	field: string;
 	name: string | TranslateResult;
-	fieldInfo?: Field;
 	disabled?: boolean;
 	children?: FieldTree[];
 };
