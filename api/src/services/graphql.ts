e3dc72b7794ce986ca28a5f6511528ee054cce63
rijkvanzanten <rijkvanzanten@me.com>
2020-09-30 21:38:08 -0400

    Add relational support for querying

--- api/src/services/graphql.ts
+++ api/src/services/graphql.ts
@@ -1,30 +1,13 @@
 import Knex from 'knex';
 import database from '../database';
 import { AbstractServiceOptions, Accountability, Collection, Field, Relation, Query } from '../types';
-import { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLScalarType, GraphQLResolveInfo, SelectionNode, FieldNode } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLScalarType, GraphQLResolveInfo, GraphQLID, FieldNode, GraphQLFieldConfigMap, GraphQLObjectTypeConfig } from 'graphql';
 import { CollectionsService } from './collections';
 import { FieldsService } from './fields';
 import { getGraphQLType } from '../utils/get-graphql-type';
 import { RelationsService } from './relations';
 import { ItemsService } from './items';
-
-type SchemaStructure = {
-	[collection: string]: {
-		name: string,
-		fields: Record<string, {
-			name: string,
-			type: GraphQLScalarType,
-		}>
-		$meta?: {
-			single: boolean,
-			relations: {
-				collection: string;
-				field: string;
-				m2o: boolean;
-			}[]
-		},
-	}
-}
+import { clone, cloneDeep, flatten } from 'lodash';
 
 export class GraphQLService {
 	accountability: Accountability | null;
@@ -38,7 +21,7 @@ export class GraphQLService {
 		this.knex = options?.knex || database;
 		this.fieldsService = new FieldsService(options);
 		this.collectionsService = new CollectionsService(options);
-		this.relationsService = new RelationsService(options);
+		this.relationsService = new RelationsService({ knex: this.knex });
 	}
 
 	async getSchema() {
@@ -46,65 +29,66 @@ export class GraphQLService {
 		const fieldsInSystem = await this.fieldsService.readAll();
 		const relationsInSystem = await this.relationsService.readByQuery({}) as Relation[];
 
-		const schemaStructure = this.generateSchemaStructure(collectionsInSystem, fieldsInSystem, relationsInSystem);
-
-		const schema = this.getGraphQLSchema(schemaStructure);
+		const schema = this.getGraphQLSchema(collectionsInSystem, fieldsInSystem, relationsInSystem);
 
 		return schema;
 	}
 
-	generateSchemaStructure(collections: Collection[], fields: Field[], relations: Relation[]) {
-		const schemaStructure: SchemaStructure = {};
+	getGraphQLSchema(collections: Collection[], fields: Field[], relations: Relation[]) {
+		const schema: any = {};
 
 		for (const collection of collections) {
-			schemaStructure[collection.collection] = {
-				name: collection.collection,
-				fields: {},
-				$meta: {
-					single: collection.meta?.single === true,
-					relations: [],
-				}
+			schema[collection.collection] = {
+				type: new GraphQLObjectType({
+					name: collection.collection,
+					fields: () => {
+						const fieldsObject: GraphQLFieldConfigMap<any, any> = {};
+						const fieldsInCollection = fields.filter((field) => field.collection === collection.collection);
+
+						for (const field of fieldsInCollection) {
+							const relationForField = relations.find((relation) => {
+								return relation.many_collection === collection.collection && relation.many_field === field.field ||
+									relation.one_collection === collection.collection && relation.one_field === field.field;
+							});
+
+							if (relationForField) {
+								const isM2O = relationForField.many_collection === collection.collection && relationForField.many_field === field.field;
+
+								if (isM2O) {
+									fieldsObject[field.field] = {
+										type: schema[relationForField.one_collection].type,
+									}
+								} else {
+									fieldsObject[field.field] = {
+										type: new GraphQLList(schema[relationForField.many_collection].type),
+									}
+								}
+							} else {
+								fieldsObject[field.field] = {
+									type: field.schema?.is_primary_key ? GraphQLID : getGraphQLType(field.type),
+								}
+							}
+						}
+
+						return fieldsObject;
+					},
+				}),
+				resolve: (source: any, args: any, context: any, info: GraphQLResolveInfo) => this.resolve(info)
 			}
 		}
 
-		for (const field of fields) {
-			schemaStructure[field.collection].fields[field.field] = {
-				name: field.field,
-				type: getGraphQLType(field.type),
-			}
-		}
+		const schemaWithLists = cloneDeep(schema);
 
-		for (const relation of relations) {}
-
-		return schemaStructure;
-	}
-
-	getGraphQLSchema(structure: SchemaStructure) {
-		const rootFields: any = {};
-
-		for (const [collection, value] of Object.entries(structure)) {
-			const meta = value.$meta;
-			delete value.$meta;
-
-			const object = new GraphQLObjectType(value);
-
-			if (meta?.single === true) {
-				rootFields[collection] = {
-					type: object,
-					resolve: (source: any, args: any, context: any, info: GraphQLResolveInfo) => this.resolve(info),
-				};
-			} else {
-				rootFields[collection] = {
-					type: new GraphQLList(object),
-					resolve: (source: any, args: any, context: any, info: GraphQLResolveInfo) => this.resolve(info),
-				};
+		for (const collection of collections) {
+			if (collection.meta?.single !== true) {
+				schemaWithLists[collection.collection].type = new GraphQLList(schemaWithLists[collection.collection].type);
 			}
 		}
 
 		return new GraphQLSchema({
 			query: new GraphQLObjectType({
 				name: 'Directus',
-				fields: rootFields,
+				fields: schemaWithLists,
 			}),
 		});
 	}
@@ -116,14 +100,25 @@ export class GraphQLService {
 		const selections = info.fieldNodes[0]?.selectionSet?.selections;
 		if (!selections) return null;
 
-		query.fields = selections
-			.filter((selection) => selection.kind === 'Field')
-			.map((selection) => {
-				return (selection as FieldNode).name.value;
-			});
+		query.fields = getFields(selections.filter((selection) => selection.kind === 'Field') as FieldNode[]);
 
 		const service = new ItemsService(collection, { knex: this.knex, accountability: this.accountability });
 		const result = await service.readByQuery(query);
+
 		return result;
+
+		function getFields(selections: FieldNode[], parent?: string): string[] {
+			return flatten(selections!
+				.map((selection) => {
+					const current = parent ? `${parent}.${selection.name.value}` : selection.name.value;
+
+					if (selection.selectionSet === undefined) {
+						return current;
+					} else {
+						const children = getFields(selection.selectionSet.selections.filter((selection) => selection.kind === 'Field') as FieldNode[], current);
+						return children;
+					}
+				}));
+		}
 	}
 }
