075f15340479483e1f1b593d302732cb39491519
mscbpi <ms@cbpi.ch>
2020-10-12 18:00:36 +0200

    Update env.ts
    convert numeric values to number

--- api/src/env.ts
+++ api/src/env.ts
@@ -62,6 +62,7 @@ function processValues(env: Record<string, any>) {
 		if (value === 'true') env[key] = true;
 		if (value === 'false') env[key] = false;
 		if (value === 'null') env[key] = null;
+		if (isNumeric(value)) env[key] = Number(value);
 	}
 
 	return env;
