54d23a92bdbdbc005cf788d4c41b630c4d3d0e1c
e01 <dimitrov.adrian@gmail.com>
2020-10-30 19:39:26 +0200

    Fix regression in display-template

--- app/src/interfaces/_system/display-template/display-template.vue
+++ app/src/interfaces/_system/display-template/display-template.vue
@@ -1,11 +1,17 @@
 <template>
-	<v-notice v-if="!collectionField && !collection" type="warning">
+	<v-notice v-if="!collectionField && !collectionName" type="warning">
 		{{ $t('interfaces.display-template.collection_field_not_setup') }}
 	</v-notice>
-	<v-notice v-else-if="!collection" type="warning">
+	<v-notice v-else-if="!collectionName" type="warning">
 		{{ $t('interfaces.display-template.select_a_collection') }}
 	</v-notice>
-	<v-field-template v-else :collection="collection" @input="$listeners.input" :value="value" :disabled="disabled" />
+	<v-field-template
+		v-else
+		:collection="collectionName"
+		@input="$listeners.input"
+		:value="value"
+		:disabled="disabled"
+	/>
 </template>
 
 <script lang="ts">
@@ -37,7 +43,10 @@ export default defineComponent({
 		const values = inject('values', ref<Record<string, any>>({}));
 
 		const collectionName = computed(() => {
-			if (!props.collectionField && !props.collection) return [];
+			if (!props.collectionField && !props.collection) return null;
+
+			if (props.collection) return props.collection;
+
 			const collectionName = props.collection || values.value[props.collectionField];
 			const collectionExists = !!collectionsStore.state.collections.find(
 				(collection) => collection.collection === collectionName
