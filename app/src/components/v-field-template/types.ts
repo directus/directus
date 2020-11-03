dca7df6fb252b8c8837ad00a10443438e6201310
rijkvanzanten <rijkvanzanten@me.com>
2020-11-03 15:16:07 -0500

    Fix v-field-select dropdown
    Ref #852

--- app/src/components/v-field-template/types.ts
+++ app/src/components/v-field-template/types.ts
@@ -1,9 +1,9 @@
-import { Field } from '@/types';
 import { TranslateResult } from 'vue-i18n';
 
 export type FieldTree = {
 	field: string;
 	name: string | TranslateResult;
+	key: string;
 	disabled?: boolean;
 	children?: FieldTree[];
 };
