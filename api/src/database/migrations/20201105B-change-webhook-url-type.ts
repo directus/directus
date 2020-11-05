dd4db04066477d5d068575b4a36fd1f18dc0d333
rijkvanzanten <rijkvanzanten@me.com>
2020-11-05 16:10:04 -0500

    Change webhook url from varchar to text
    Fixes #881

---
+++ api/src/database/migrations/20201105B-change-webhook-url-type.ts
@@ -0,0 +1,13 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+	await knex.schema.alterTable('directus_webhooks', (table) => {
+		table.text('url').alter();
+	});
+}
+
+export async function down(knex: Knex) {
+	await knex.schema.alterTable('directus_webhooks', (table) => {
+		table.string('url').alter();
+	});
+}
