d18b5800edd8990df6eec3ecc081b6475edc3587
rijkvanzanten <rijkvanzanten@me.com>
2020-11-09 13:34:45 -0500

    Fix cli command function name

--- api/src/cli/commands/count/index.ts
+++ api/src/cli/commands/count/index.ts
@@ -1,4 +1,4 @@
-export default async function rolesCreate(collection: string) {
+export default async function count(collection: string) {
 	const database = require('../../../database/index').default;
 
 	if (!collection) {
