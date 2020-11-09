0d734317adf5c1c0646fcf4a657c6f2bf7d63e7f
Ben Haynes <ben@rngr.org>
2020-11-09 18:52:04 -0500

    slider focus

--- app/src/components/v-slider/v-slider.vue
+++ app/src/components/v-slider/v-slider.vue
@@ -168,26 +168,27 @@ body {
 			}
 		}
 
-		&:hover {
-			input {
-				height: 4px;
-				&::-webkit-slider-thumb {
-					width: 12px;
-					height: 12px;
-					margin-top: -4px;
-					box-shadow: 0 0 0 4px var(--v-slider-thumb-color);
-				}
+		&:hover,
+		&:focus-within {
+		    input {
+		        height: 4px;
+		        &::-webkit-slider-thumb {
+		            width: 12px;
+		            height: 12px;
+		            margin-top: -4px;
+		            box-shadow: 0 0 0 4px var(--v-slider-thumb-color);
+		        }
 
-				&::-moz-range-thumb {
-					width: 12px;
-					height: 12px;
-					margin-top: -4px;
-					box-shadow: 0 0 0 4px var(--v-slider-thumb-color);
-				}
-			}
-			.thumb-label {
-				opacity: 1;
-			}
+		        &::-moz-range-thumb {
+		            width: 12px;
+		            height: 12px;
+		            margin-top: -4px;
+		            box-shadow: 0 0 0 4px var(--v-slider-thumb-color);
+		        }
+		    }
+		    .thumb-label {
+		        opacity: 1;
+		    }
 		}
 
 		.fill {
