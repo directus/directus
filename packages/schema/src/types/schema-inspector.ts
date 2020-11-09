427bd8564e0bf56bd507831c5e6c18ba195b9c12
rijkvanzanten <rijkvanzanten@me.com>
2020-11-09 11:21:43 -0500

    Include schema package

---
+++ packages/schema/src/types/schema-inspector.ts
@@ -0,0 +1,30 @@
+import Knex from 'knex';
+import { Table } from './table';
+import { Column } from './column';
+
+export interface Schema {
+  knex: Knex;
+
+  tables(): Promise<string[]>;
+
+  tableInfo(): Promise<Table[]>;
+  tableInfo(table: string): Promise<Table>;
+
+  hasTable(table: string): Promise<boolean>;
+
+  columns(table?: string): Promise<{ table: string; column: string }[]>;
+
+  columnInfo(): Promise<Column[]>;
+  columnInfo(table?: string): Promise<Column[]>;
+  columnInfo(table: string, column: string): Promise<Column>;
+
+  hasColumn(table: string, column: string): Promise<boolean>;
+  primary(table: string): Promise<string | null>;
+
+  // Not in MySQL
+  withSchema?(schema: string): void;
+}
+
+export interface SchemaConstructor {
+  new(knex: Knex): Schema;
+}
