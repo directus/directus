33c3f69d9ec4bef610febfc62280d4c074dd1f8e
rijkvanzanten <rijkvanzanten@me.com>
2020-11-10 09:29:41 -0500

    Fix naming in schema middleware

--- api/src/middleware/schema.ts
+++ api/src/middleware/schema.ts
@@ -1,17 +1,12 @@
-/**
- * Sanitize query parameters.
- * This ensures that query params are formatted and ready to go for the services.
- */
-
 import { RequestHandler } from 'express';
 import asyncHandler from 'express-async-handler';
 import { schemaInspector } from '../database';
 
-const sanitizeQueryMiddleware: RequestHandler = asyncHandler(async (req, res, next) => {
+const getSchema: RequestHandler = asyncHandler(async (req, res, next) => {
 	const schemaOverview = await schemaInspector.overview();
 	req.schema = schemaOverview;
 
 	return next();
 });
 
-export default sanitizeQueryMiddleware;
+export default getSchema;
