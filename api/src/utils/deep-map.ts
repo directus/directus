e8f0d7032284ebff0220bd5b9e85cf3c14aa93b7
rijkvanzanten <rijkvanzanten@me.com>
2020-11-12 16:09:51 -0500

    Replace deep-map, fix _and filter parsing

--- api/src/utils/deep-map.ts
+++ api/src/utils/deep-map.ts
@@ -1,9 +1,29 @@
-import { transform, isPlainObject } from 'lodash';
+export function deepMap(
+	object: Record<string, any>,
+	iterator: (value: any, key: string | number) => any,
+	context?: any
+): any {
+	if (Array.isArray(object)) {
+		return object.map(function (val, key) {
+			return typeof val === 'object'
+				? deepMap(val, iterator, context)
+				: iterator.call(context, val, key);
+		});
+	} else if (typeof object === 'object') {
+		const res: Record<string, any> = {};
 
-export function deepMap(obj: Record<string, any>, iterator: Function, context?: Function) {
-	return transform(obj, function (result: any, val, key) {
-		result[key] = isPlainObject(val)
-			? deepMap(val, iterator, context)
-			: iterator.call(context, val, key, obj);
-	});
+		for (var key in object) {
+			var val = object[key];
+
+			if (typeof val === 'object') {
+				res[key] = deepMap(val, iterator, context);
+			} else {
+				res[key] = iterator.call(context, val, key);
+			}
+		}
+
+		return res;
+	} else {
+		return object;
+	}
 }
