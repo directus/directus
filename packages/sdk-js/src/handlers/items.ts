14cc342de92837813662bdbc5a98f4efb6bb471a
rijkvanzanten <rijkvanzanten@me.com>
2020-11-24 10:33:10 -0500

    Improve typing of single parameter on items.read

--- packages/sdk-js/src/handlers/items.ts
+++ packages/sdk-js/src/handlers/items.ts
@@ -25,8 +25,11 @@ export class ItemsHandler {
 		return result.data;
 	}
 
-	async read<T extends Item>(query?: Query): Promise<Response<T | T[]>>;
+	async read<T extends Item>(): Promise<Response<T | T[]>>;
 	async read<T extends Item>(query: Query & { single: true }): Promise<Response<T>>;
+	async read<T extends Item>(
+		query: Query & { single: false | undefined }
+	): Promise<Response<T[]>>;
 	async read<T extends Item>(key: PrimaryKey, query?: Query): Promise<Response<T>>;
 	async read<T extends Item>(keys: PrimaryKey[], query?: Query): Promise<Response<T | T[]>>;
 	async read<T extends Item>(
