ee6aaffe92d6e5d3c838ddbbfce8aaca17b36c2b
Aleksandar Stanisic <aleksandar@tuta.io>
2020-11-24 23:05:11 +0100

    Improve types

--- packages/sdk-js/src/handlers/server.ts
+++ packages/sdk-js/src/handlers/server.ts
@@ -1,5 +1,30 @@
 import { AxiosInstance } from 'axios';
 
+export type ServerInfo = {
+	project: {
+		project_name: string;
+		project_logo: string | null;
+		project_color: string | null;
+		public_foreground: string | null;
+		public_background: string | null;
+		public_note: string | null;
+		custom_css: null;
+	};
+};
+
+export type OasData = {
+	openapi: string;
+	info: {
+		title: string;
+		description: string;
+		versions: string;
+	};
+	servers: { url: string; description: string }[];
+	tags: { name: string; description: string }[];
+	paths: Record<string, any>;
+	components: Record<string, any>;
+};
+
 export class ServerHandler {
 	private axios: AxiosInstance;
 
@@ -8,18 +33,18 @@ export class ServerHandler {
 	}
 
 	specs = {
-		oas: async () => {
+		oas: async (): Promise<OasData> => {
 			const result = await this.axios.get('/server/specs/oas');
 			return result.data;
 		},
 	};
 
-	async ping() {
+	async ping(): Promise<'pong'> {
 		await this.axios.get('/server/ping');
 		return 'pong';
 	}
 
-	async info() {
+	async info(): Promise<{ data: ServerInfo }> {
 		const result = await this.axios.get('/server/info');
 		return result.data;
 	}
