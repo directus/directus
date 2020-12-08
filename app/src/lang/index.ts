5db50352ab9f53b3ba2f67b66aa61450636d2917
rijkvanzanten <rijkvanzanten@me.com>
2020-12-08 11:59:29 -0500

    Cleanup translationsto yaml file

--- app/src/lang/index.ts
+++ app/src/lang/index.ts
@@ -5,7 +5,7 @@ import { RequestError } from '@/api';
 import availableLanguages from './available-languages.yaml';
 
 import enUSBase from './en-US/translations.yaml';
-import enUSDateFormats from './en-US/date-formats.yaml';
+import dateFormats from './date-formats.yaml';
 
 Vue.use(VueI18n);
 
@@ -15,9 +15,7 @@ export const i18n = new VueI18n({
 	messages: {
 		'en-US': enUSBase,
 	},
-	dateTimeFormats: {
-		'en-US': enUSDateFormats,
-	},
+	dateTimeFormats: dateFormats,
 	silentTranslationWarn: true,
 });
 
@@ -37,17 +35,7 @@ export async function setLanguage(lang: Language): Promise<boolean> {
 	if (loadedLanguages.includes(lang) === false) {
 		const translations = await import(`@/lang/${lang}/translations.yaml`).catch((err) => console.warn(err));
 		i18n.mergeLocaleMessage(lang, translations);
-
 		loadedLanguages.push(lang);
-
-		// The date-format json file may or may not exist, as it's not handled by Crowdin.
-		// If it doesn't exist, i18n will fall back to en-US as default
-		try {
-			const dateTimeFormats = await import(`@/lang/${lang}/date-format.yaml`);
-			i18n.setDateTimeFormat(lang, dateTimeFormats);
-		} catch {
-			console.log(`[setCurrentLanguage] ❌ Couldn't fetch date time formats for language "${lang}"`);
-		}
 	}
 
 	i18n.locale = lang;
