16a8aa7c471a71db2a5d3081ad930802b9b7381c
rijkvanzanten <rijkvanzanten@me.com>
2021-01-12 11:30:13 -0500

    Pipe graphql through respond middleware
    Fixes #3577

--- api/src/controllers/graphql.ts
+++ api/src/controllers/graphql.ts
@@ -1,20 +1,37 @@
-import { Router } from 'express';
+import { Router, Response } from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { GraphQLService } from '../services';
 import asyncHandler from 'express-async-handler';
+import { respond } from '../middleware/respond';
 
 const router = Router();
 
 router.use(
-	asyncHandler(async (req, res) => {
+	asyncHandler(async (req, res, next) => {
 		const service = new GraphQLService({
 			accountability: req.accountability,
 			schema: req.schema,
 		});
 		const schema = await service.getSchema();
 
-		graphqlHTTP({ schema, graphiql: true })(req, res);
-	})
+		/**
+		 * @NOTE express-graphql will attempt to respond directly on the `res` object
+		 * We don't want that, as that will skip our regular `respond` middleware
+		 * and therefore skip the cache. This custom response object overwrites
+		 * express' regular `json` function in order to trick express-graphql to
+		 * use the next middleware instead of respond with data directly
+		 */
+		const customResponse = {
+			...res,
+			json: function (payload: Record<string, any>) {
+				res.locals.payload = payload;
+				return next();
+			},
+		};
+
+		graphqlHTTP({ schema, graphiql: true })(req, customResponse as Response);
+	}),
+	respond
 );
 
 export default router;
