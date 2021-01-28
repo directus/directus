dbfc594857e41bcf294f6f0bedd680b6f96b791a
rijkvanzanten <rijkvanzanten@me.com>
2021-01-28 17:39:28 -0500

    Fix activity sidebar filters
    Fixes #3768

--- app/src/composables/use-preset/use-preset.ts
+++ app/src/composables/use-preset/use-preset.ts
@@ -27,7 +27,10 @@ export function usePreset(collection: Ref<string>, bookmark: Ref<number | null> 
 	const savePreset = async (preset?: Partial<Preset>) => {
 		busy.value = true;
 		const updatedValues = await presetsStore.savePreset(preset ? preset : localPreset.value);
-		localPreset.value.id = updatedValues.id;
+		localPreset.value = {
+			...localPreset.value,
+			id: updatedValues.id,
+		};
 		busy.value = false;
 		return updatedValues;
 	};
