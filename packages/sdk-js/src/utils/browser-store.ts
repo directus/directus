22bcf1244d3399cec82fd04f69d32499bcd66e2b
Aleksandar <19491605+apstanisic@users.noreply.github.com>
2021-01-30 00:13:15 +0100

    Fix sdk auth (#3337)
    * Fix format errors
    
    * Fix lint error
    
    * Fix auth token setter on server
    
    * Fix auth logout for JSON

---
+++ packages/sdk-js/src/utils/browser-store.ts
@@ -0,0 +1,11 @@
+import { AuthStorage } from '../types';
+
+export class BrowserStore implements AuthStorage {
+	async getItem(key: string): Promise<string | null> {
+		return window.localStorage.getItem(key);
+	}
+
+	async setItem(key: string, value: any): Promise<void> {
+		window.localStorage.setItem(key, value);
+	}
+}
