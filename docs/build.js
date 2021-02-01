4f2cb51da5fc9e0469e24ea98e4e5f4f02482725
rijkvanzanten <rijkvanzanten@me.com>
2021-02-01 17:14:48 -0500

    Remove copy-to-app

--- docs/build.js
+++ docs/build.js
@@ -11,24 +11,18 @@ async function build() {
 
 	const start = Date.now();
 
-	const assetsPath = path.resolve(__dirname, '../app/public/img/docs');
 	const distPath = path.resolve(__dirname, './dist');
 
 	await rimraf(distPath);
 
 	const tree = dirTree('.', { extensions: /\.md/, exclude: /(dist|node_modules)/ });
 
-	await fse.ensureDir(assetsPath);
 	await fse.ensureDir(distPath);
 
 	await fse.writeJSON('./dist/index.json', tree);
 
 	await copyfiles(['./**/*.md', distPath], { exclude: './node_modules/**/*.*' });
 	await copyfiles(['./assets/**/*.*', distPath], { exclude: './node_modules/**/*.*' });
-	
-	console.log('Copying docs assets to:', assetsPath);
-	
-	await copyfiles(['./assets/**/*.*', assetsPath], { exclude: './node_modules/**/*.*' });
 
 	const yamlFiles = [];
 	const filesInRoot = await fse.readdir(__dirname);
@@ -41,10 +35,7 @@ async function build() {
 
 	for (const yamlFile of yamlFiles) {
 		const yamlString = await fse.readFile(yamlFile, 'utf8');
-		await fse.writeJSON(
-			'./dist/' + yamlFile.replace('.yaml', '.json'),
-			yaml.safeLoad(yamlString)
-		);
+		await fse.writeJSON('./dist/' + yamlFile.replace('.yaml', '.json'), yaml.safeLoad(yamlString));
 	}
 
 	console.log(`Built docs in ${Date.now() - start} ms`);
