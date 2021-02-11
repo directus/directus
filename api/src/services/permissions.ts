59941de1eac164f12c4dc210f6492a54b6ea3e2a
rijkvanzanten <rijkvanzanten@me.com>
2021-02-11 12:26:15 -0500

    Add system minimal app access permissions into result of /permissions

--- api/src/services/permissions.ts
+++ api/src/services/permissions.ts
@@ -1,5 +1,8 @@
-import { AbstractServiceOptions, PermissionsAction } from '../types';
+import { AbstractServiceOptions, PermissionsAction, Query, Item, PrimaryKey } from '../types';
 import { ItemsService } from '../services/items';
+import { filterItems } from '../utils/filter-items';
+
+import { appAccessMinimalPermissions } from '../database/system-data/app-access-permissions';
 
 export class PermissionsService extends ItemsService {
 	constructor(options: AbstractServiceOptions) {
@@ -26,5 +29,34 @@ export class PermissionsService extends ItemsService {
 		}
 
 		return fieldsPerCollection;
+	}
+
+	async readByQuery(
+		query: Query,
+		opts?: { stripNonRequested?: boolean }
+	): Promise<null | Partial<Item> | Partial<Item>[]> {
+		const result = await super.readByQuery(query, opts);
+
+		if (Array.isArray(result) && this.accountability && this.accountability.app === true) {
+			result.push(...filterItems(appAccessMinimalPermissions, query.filter));
+		}
+
+		return result;
+	}
+
+	readByKey(keys: PrimaryKey[], query?: Query, action?: PermissionsAction): Promise<null | Partial<Item>[]>;
+	readByKey(key: PrimaryKey, query?: Query, action?: PermissionsAction): Promise<null | Partial<Item>>;
+	async readByKey(
+		key: PrimaryKey | PrimaryKey[],
+		query: Query = {},
+		action: PermissionsAction = 'read'
+	): Promise<null | Partial<Item> | Partial<Item>[]> {
+		const result = await super.readByKey(key as any, query, action);
+
+		if (Array.isArray(result) && this.accountability && this.accountability.app === true) {
+			result.push(...filterItems(appAccessMinimalPermissions, query.filter));
+		}
+
+		return result;
 	}
 }
