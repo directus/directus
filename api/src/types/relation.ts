5dd6c1186f6be37199daf0b2d4ffcad5112677e8
rijkvanzanten <rijkvanzanten@me.com>
2021-02-17 18:17:39 -0500

    Remove type

--- api/src/types/relation.ts
+++ api/src/types/relation.ts
@@ -3,11 +3,9 @@ export type Relation = {
 
 	many_collection: string;
 	many_field: string;
-	many_primary: string;
 
 	one_collection: string | null;
 	one_field: string | null;
-	one_primary: string | null;
 
 	one_collection_field: string | null;
 	one_allowed_collections: string | null;
--- app/src/types/relations.ts
+++ app/src/types/relations.ts
@@ -2,10 +2,10 @@ export type Relation = {
 	id: number;
 	many_collection: string;
 	many_field: string;
-	many_primary: string;
+
 	one_collection: string;
 	one_field: null | string;
-	one_primary: string;
+
 	junction_field: null | string;
 	one_collection_field: null | string;
 	one_allowed_collections: null | string[];
