defb9bb57d0b6406029bd97faa7bf9aa7d2ed3b4
rijkvanzanten <rijkvanzanten@me.com>
2021-03-10 16:58:04 -0500

    Validate correct env vars for oracle

--- api/src/utils/validate-env.ts
+++ api/src/utils/validate-env.ts
@@ -4,6 +4,8 @@ import env from '../env';
 export function validateEnv(requiredKeys: string[]) {
 	if (env.DB_CLIENT && env.DB_CLIENT === 'sqlite3') {
 		requiredKeys.push('DB_FILENAME');
+	} else if (env.DB_CLIENT && env.DB_CLIENT === 'oracledb') {
+		requiredKeys.push('DB_USER', 'DB_PASSWORD', 'DB_CONNECT_STRING');
 	} else {
 		if (env.DB_CLIENT === 'pg') {
 			if (!env.DB_CONNECTION_STRING) {
