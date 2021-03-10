81083bfb0e122e7a0e9bfa5fd4640ed7b7158987
rijkvanzanten <rijkvanzanten@me.com>
2021-03-10 17:26:09 -0500

    Allow creating unique fields

--- api/src/services/fields.ts
+++ api/src/services/fields.ts
@@ -368,6 +368,10 @@ export class FieldsService {
 			column.nullable();
 		}
 
+		if (field.schema?.is_unique === true) {
+			column.unique();
+		}
+
 		if (field.schema?.is_primary_key) {
 			column.primary();
 		}
