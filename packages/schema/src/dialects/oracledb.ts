f24c735ae47085f70ab51850c10ec8a6572be4b7
rijkvanzanten <rijkvanzanten@me.com>
2021-03-10 16:57:21 -0500

    Fix oracle overview query

--- packages/schema/src/dialects/oracledb.ts
+++ packages/schema/src/dialects/oracledb.ts
@@ -4,23 +4,25 @@ import { SchemaInspector } from '../types/schema';
 
 export default class Oracle extends KnexOracle implements SchemaInspector {
 	async overview() {
-		const columns = await this.knex.raw(
-			`
+		const columns = await this.knex.raw(`
 			SELECT
-				c.TABLE_NAME as table_name,
-				c.COLUMN_NAME as column_name,
-				c.DATA_DEFAULT as default_value,
-				c.NULLABLE as is_nullable,
-				c.DATA_TYPE as data_type,
-				pk.CONSTRAINT_TYPE as column_key
-			FROM DBA_TAB_COLUMNS as c
-			LEFT JOIN all_constraints as pk
-				ON c.TABLE_NAME = pk.TABLE_NAME
-				AND c.CONSTRAINT_NAME = pk.CONSTRAINT_NAME
-				AND c.OWNER = pk.OWNER
-			`
-		);
+				"USER_TAB_COLUMNS"."TABLE_NAME" AS table_name,
+				"USER_TAB_COLUMNS"."COLUMN_NAME" AS column_name,
+				"USER_TAB_COLUMNS"."DATA_DEFAULT" AS default_value,
+				"USER_TAB_COLUMNS"."NULLABLE" AS is_nullable,
+				"USER_TAB_COLUMNS"."DATA_TYPE" AS data_type,
+				"USER_TAB_COLUMNS"."DATA_PRECISION" AS numeric_precision,
+				"USER_TAB_COLUMNS"."DATA_SCALE" AS numeric_scale,
+				"USER_CONSTRAINTS"."CONSTRAINT_TYPE" AS column_key
+			FROM
+				"USER_TAB_COLUMNS"
+				LEFT JOIN "USER_CONS_COLUMNS" ON "USER_TAB_COLUMNS"."TABLE_NAME" = "USER_CONS_COLUMNS"."TABLE_NAME"
+					AND "USER_TAB_COLUMNS"."COLUMN_NAME" = "USER_CONS_COLUMNS"."COLUMN_NAME"
+				LEFT JOIN "USER_CONSTRAINTS" ON "USER_CONS_COLUMNS"."CONSTRAINT_NAME" = "USER_CONSTRAINTS"."CONSTRAINT_NAME";
+		`);
+
 		const overview: SchemaOverview = {};
+
 		for (const column of columns[0]) {
 			if (column.table_name in overview === false) {
 				overview[column.table_name] = {
@@ -30,6 +32,7 @@ export default class Oracle extends KnexOracle implements SchemaInspector {
 					columns: {},
 				};
 			}
+
 			overview[column.table_name].columns[column.column_name] = {
 				...column,
 				is_nullable: column.is_nullable === 'YES',
