e90268efebeb832f57f84204ae47fa2696c3e827
rijkvanzanten <rijkvanzanten@me.com>
2021-03-22 12:01:28 -0400

    Add description in setup

--- app/src/interfaces/tree-view/index.ts
+++ app/src/interfaces/tree-view/index.ts
@@ -4,6 +4,7 @@ import InterfaceTreeView from './tree-view.vue';
 export default defineInterface(({ i18n }) => ({
 	id: 'tree-view',
 	name: i18n.t('tree_view'),
+	description: i18n.t('interfaces.tree-view.description'),
 	icon: 'account_tree',
 	types: ['alias'],
 	groups: ['o2m'],
