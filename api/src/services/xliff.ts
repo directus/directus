97251fb5fd465246afdf1e711acbcf9c0f07b45b
yaroslavv <yaroslav.vysotsky@crescendotechnology.com>
2021-03-26 22:06:41 -0400

    index on ARC-4532: 6be4c9ffc ARC-4532:

--- api/src/services/xliff.ts
+++ api/src/services/xliff.ts
@@ -1,4 +1,4 @@
-import { AbstractServiceOptions, Accountability, SchemaOverview } from '../types';
+import { AbstractServiceOptions, Accountability, Relation, SchemaOverview } from '../types';
 import xliff from 'xliff';
 import { InvalidPayloadException, UnprocessableEntityException } from '../exceptions';
 import { FieldsService } from './fields';
@@ -45,56 +45,77 @@ export class XliffService {
 			throw new UnprocessableEntityException(`Information about translations cannot be found.`);
 		}
 		for (const relation of relations) {
-			const translationsCollection = <string>relation.many_collection;
-			const translationsKeyField = <string>relation.one_primary;
-			const translationsFieldName = <string>relation.one_field;
-			const translationsIdFieldName = <string>relation.many_primary;
-			const tranlsationsExternalKey = <string>relation.junction_field;
-			const translationsParentField = <string>relation.many_field;
-			const translations = data.map((r: any) => {
-				return {
-					id: r[translationsKeyField],
-					translationIds: r[translationsFieldName],
-				};
-			});
-			const resources = {};
-			const translationsIds = translations.reduce((acc: any[], val: any) => {
-				return val.translationIds.length > 0 ? [...acc, ...val.translationIds] : acc;
-			}, []);
-			const itemsService = new ItemsService(translationsCollection, {
-				accountability: this.accountability,
-				schema: this.schema,
-			});
-			// making query to bring records for all found translations for current language
-			const translationItems = await itemsService.readByQuery({
-				filter: {
-					[translationsIdFieldName]: { _in: translationsIds },
-					[tranlsationsExternalKey]: { _eq: this.language },
-				},
-				limit: -1,
-			});
-			// obtaining values of translatable fields and putting them to the dictionary
-			if (translationItems && translationItems.length > 0) {
-				translationItems.forEach((item: any) => {
-					const translation = translations.find((t: any) => t.translationIds.includes(item[translationsKeyField]));
-					if (translation) {
-						// removing system fields from output
-						const { id, status, sort, user_created, date_created, user_updated, date_updated, ...rest } = item;
-						// ensuring that key field was removed
-						delete rest[translationsKeyField];
-						delete rest[tranlsationsExternalKey];
-						delete rest[translationsParentField];
-						Object.keys(rest).forEach((key) => {
-							(resources as any)[`${translation.id}.${key}`] = {
-								source: rest[key],
-							};
-						});
-					}
-				});
-			}
-			(json.resources as any)[translationsCollection] = resources;
+			(json.resources as any)[relation.many_collection] = await this.getXliffResources(relation, data);
 		}
 		const output = await (this.version === 1 ? xliff.jsToXliff12(json) : xliff.js2xliff(json));
 		return output;
+	}
+
+	// prepares output for specific relation
+ private async getXliffResources(relation: Relation, data: any[]) {
+		const translationsCollection = <string>relation.many_collection;
+		const translationsKeyField = <string>relation.one_primary;
+		const tranlsationsExternalKey = <string>relation.junction_field;
+		const translationsParentField = <string>relation.many_field;
+		// getting items keys values
+		const itemsKeys = data.map((r: any) => r[translationsKeyField]);
+	  // initializing output object
+		const resources = {};
+		const itemsService = new ItemsService(translationsCollection, {
+			accountability: this.accountability,
+			schema: this.schema,
+		});
+		// making query to bring records for all found translations for current language
+		const translationItems = await itemsService.readByQuery({
+			filter: {
+				[translationsParentField]: { _in: itemsKeys },
+				[tranlsationsExternalKey]: { _eq: this.language },
+			},
+			limit: -1,
+		});
+		// obtaining values of translatable fields and putting them to the dictionary
+		if (translationItems && translationItems.length > 0) {
+			translationItems.forEach((item: any) => {
+					// removing system and key fields from output
+					const translatableData =
+						this.cleanupEmptyFields(
+							this.removeFields(
+								this.removeSystemFields(item),
+								translationsKeyField,
+								tranlsationsExternalKey,
+								translationsParentField
+							)
+						);
+					// populating output object with translations data
+					Object.keys(translatableData).forEach((key) => {
+						(resources as any)[`${item[translationsParentField]}.${key}`] = {
+							source: translatableData[key],
+						};
+					});
+			});
+		}
+		return resources;
+	}
+
+  // removes system fields from the passed item object
+	private removeSystemFields(item: any) {
+		const { id, status, sort, user_created, date_created, user_updated, date_updated, ...rest } = item;
+		return rest;
+	}
+
+	// removes specific fields from the passed item object
+	private removeFields(item: any, ...fields: string[]) {
+		return fields.reduce((acc: any, val: string) => {
+			const { [val]: removed, ...rest } = acc;
+			return rest;
+		}, item);
+	}
+
+	// replaces null/undefined values with empty strings
+	private cleanupEmptyFields(item: any) {
+		Object.keys(item).forEach((key) => {
+			if (!item[key]) item[key] = '';
+		});
+		return item;
 	}
 }
