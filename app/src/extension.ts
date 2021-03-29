a667ca4fd79814d249860700327915b25ecea131
Nitwel <nitwel@arcor.de>
2021-03-29 18:28:32 +0200

    replace translations recursive

--- app/src/extension.ts
+++ app/src/extension.ts
@@ -1,13 +1,21 @@
 import { computed, Ref } from '@vue/composition-api';
-import { cloneDeepWith } from 'lodash';
+import { cloneDeep } from 'lodash';
 import i18n from './lang';
 
 export function translateExtensions(extensions: Ref<Extension[]>) {
-	return computed(() => cloneDeepWith(extensions.value, translateStrings));
+	return computed({
+		get() {
+			return translateStrings(cloneDeep(extensions.value)) as Extension[];
+		},
+		set(newVal: Extension[]) {
+			extensions.value = newVal;
+		},
+	});
 }
 function translateStrings(obj: Record<string, any>) {
 	Object.entries(obj).forEach(([key, val]) => {
-		if (typeof val === 'string' && val.startsWith('$t:')) obj[key] = i18n.t(obj[key]);
+		if (val && typeof val === 'object') obj[key] = translateStrings(val);
+		if (val && typeof val === 'string' && val.startsWith('$t:')) obj[key] = i18n.t(val.replace('$t:', ''));
 	});
 	return obj;
 }
