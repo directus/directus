66a35e83e23b907465bb678abeddd13f1fb9cfe6
Joe Innes <joe@joeinn.es>
2021-04-12 21:30:05 +0200

    Add auth_valid_until storage interface

--- packages/sdk/src/storage.ts
+++ packages/sdk/src/storage.ts
@@ -2,6 +2,7 @@ export interface IStorage {
 	auth_token: string | null;
 	auth_expires: number | null;
 	auth_refresh_token: string | null;
+	auth_valid_until: number | null;
 
 	get(key: string): string | null;
 	set(key: string, value: string): string;
