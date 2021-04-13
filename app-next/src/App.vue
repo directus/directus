855d7e2c767bd3e27ef81c20ee490f78347c023c
rijkvanzanten <rijkvanzanten@me.com>
2021-04-13 19:00:21 -0400

    Setup empty vite environment

---
+++ app-next/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+	margin-top: 60px;
+	color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	text-align: center;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+</style>
