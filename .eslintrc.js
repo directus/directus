5089ece6cb736956192bfa6494848af0ef50b2b9
Pascal Jufer <pascal-jufer@bluewin.ch>
2021-04-23 15:27:50 +0200

    Enhance eslint config
    * Config with sensible settings for typescript and vue
    * Differentiate between js/ts files
    * Update lint-related packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,61 @@
+const defaultRules = {
+	'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	'prettier/prettier': 'error',
+	'comma-dangle': [
+		'error',
+		{
+			arrays: 'always-multiline',
+			exports: 'always-multiline',
+			functions: 'never',
+			imports: 'always-multiline',
+			objects: 'always-multiline',
+		},
+	],
+};
+
 module.exports = {
 	root: true,
 	env: {
+		browser: true,
 		node: true,
 	},
-	extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier-vue/recommended', 'plugin:vue/essential'],
 	plugins: ['@typescript-eslint', 'prettier'],
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'@typescript-eslint/camelcase': 0,
-		'@typescript-eslint/no-use-before-define': 0,
-		'@typescript-eslint/ban-ts-ignore': 0,
-		'@typescript-eslint/no-explicit-any': 0,
-		'@typescript-eslint/no-var-requires': 0,
-		'prettier/prettier': ['error', { singleQuote: true }],
-		'vue/valid-v-slot': 0,
-		'comma-dangle': [
-			'error',
-			{
-				arrays: 'always-multiline',
-				exports: 'always-multiline',
-				functions: 'never',
-				imports: 'always-multiline',
-				objects: 'always-multiline',
-			},
-		],
-	},
+	extends: ['eslint:recommended', 'prettier'],
+	rules: defaultRules,
 	parserOptions: {
-		parser: '@typescript-eslint/parser',
+		ecmaVersion: 2020,
 	},
+	overrides: [
+		{
+			files: ['rollup.config.js'],
+			parserOptions: {
+				sourceType: 'module',
+			},
+		},
+		{
+			files: ['*.ts', '*.vue'],
+			parser: 'vue-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+			extends: [
+				'plugin:vue/essential',
+				'eslint:recommended',
+				'plugin:@typescript-eslint/recommended',
+				'plugin:prettier-vue/recommended',
+				'prettier',
+			],
+			rules: {
+				...defaultRules,
+				'no-undef': 'off',
+				'@typescript-eslint/ban-ts-comment': 0,
+				'@typescript-eslint/no-explicit-any': 0,
+				'@typescript-eslint/no-var-requires': 0,
+				'@typescript-eslint/no-non-null-assertion': 0,
+				'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+				'vue/valid-v-slot': 0,
+			},
+		},
+	],
 };
