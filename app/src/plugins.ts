f493a0f938e7663d29adb9e9d4ff2831d38727a6
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2021-04-26 11:58:00 +0200

    Remove plugins.ts

--- app/src/plugins.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import VueCompositionAPI from 'vue';
-import VueRouter from 'vue-router';
-import PortalVue from 'portal-vue';
-
-Vue.use(VueCompositionAPI);
-Vue.use(VueRouter);
-Vue.use(PortalVue);
