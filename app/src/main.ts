409457b13c5a47a226860a9134bc860e153028de
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2021-04-29 12:48:44 +0200

    Fix environment logging

--- app/src/main.ts
+++ app/src/main.ts
@@ -53,7 +53,7 @@ async function init() {
 
 	console.group(`%c✨ Project Information`, 'color:DodgerBlue'); // groupCollapsed
 	console.info(`%cVersion: v${version}`, 'color:DodgerBlue');
-	console.info(`%cEnvironment: ${import.meta.env.DEV}`, 'color:DodgerBlue');
+	console.info(`%cEnvironment: ${import.meta.env.DEV ? 'development' : 'production'}`, 'color:DodgerBlue');
 	console.groupEnd();
 
 	// Prevent the browser from opening files that are dragged on the window
