2076c22226e2b02d0c16fbb1cf8217736c7dc299
rijkvanzanten <rijkvanzanten@me.com>
2021-05-03 12:20:55 -0400

    Setup layout boilerplate

---
+++ app/src/layouts/calendar/calendar.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>Calendar</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+	setup() {},
+});
+</script>
+
+<style lang="sass" scoped></style>
