538a400f7596c4fc93b25bf5dd266fce4d877716
rijkvanzanten <rijkvanzanten@me.com>
2021-05-03 13:00:30 -0400

    Install calendar

--- app/src/layouts/calendar/calendar.vue
+++ app/src/layouts/calendar/calendar.vue
@@ -1,13 +1,62 @@
 <template>
-	<div>Calendar</div>
+	<div class="calendar-layout">
+		<div ref="calendarEl" />
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import '@fullcalendar/core/vdom';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import { defineComponent, onMounted, onUnmounted, ref, watchEffect, watch } from '@vue/composition-api';
+import { useAppStore } from '@/stores/app';
 
 export default defineComponent({
-	setup() {},
+	setup() {
+		const appStore = useAppStore();
+
+		const calendarEl = ref<HTMLElement>();
+		const calendar = ref<Calendar>();
+
+		calendar.value?.updateSize();
+
+		onMounted(() => {
+			calendar.value = new Calendar(calendarEl.value!, {
+				plugins: [dayGridPlugin],
+				initialView: 'dayGridMonth',
+				headerToolbar: {
+					start: '',
+					center: '',
+					end: '',
+				},
+			});
+
+			calendar.value.render();
+		});
+
+		watch(appStore.state.sidebarOpen, () => calendar.value?.updateSize());
+
+		// To change options on the fly:
+		// watchEffect(() => {
+		// 	if (calendar.value) {
+		// 		calendar.value.pauseRendering();
+		// 		calendar.value.resetOptions({});
+		// 		calendar.value.resumeRendering();
+		// 	}
+		// });
+
+		onUnmounted(() => {
+			calendar.value?.destroy();
+		});
+
+		return { calendarEl };
+	},
 });
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.calendar-layout {
+	padding: var(--content-padding);
+	padding-top: 0;
+}
+</style>
