26da8d730e7619e6fb79f95a7ba857dd6ad59f63
rijkvanzanten <rijkvanzanten@me.com>
2021-05-05 16:28:24 -0400

    Rename image->file-image

--- app/src/interfaces/file-image/index.ts
+++ app/src/interfaces/file-image/index.ts
@@ -1,12 +1,12 @@
 import { defineInterface } from '@/interfaces/define';
-import InterfaceImage from './image.vue';
+import InterfaceFileImage from './image.vue';
 
 export default defineInterface({
-	id: 'image',
-	name: '$t:interfaces.image.image',
-	description: '$t:interfaces.image.description',
+	id: 'file-image',
+	name: '$t:interfaces.file-image.image',
+	description: '$t:interfaces.file-image.description',
 	icon: 'insert_photo',
-	component: InterfaceImage,
+	component: InterfaceFileImage,
 	types: ['uuid'],
 	groups: ['file'],
 	relational: true,
