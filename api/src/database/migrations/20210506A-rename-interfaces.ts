5963dadeace8efa525ba1a966309285b0b297978
rijkvanzanten <rijkvanzanten@me.com>
2021-05-06 15:06:14 -0400

    Add migration, fix dropdown name in system use

---
+++ api/src/database/migrations/20210506A-rename-interfaces.ts
@@ -0,0 +1,59 @@
+import { Knex } from 'knex';
+
+// [before, after]
+const changes = [
+	['button-links', 'presentation-links'],
+	['checkboxes', 'select-multiple-checkbox'],
+	['code', 'input-code'],
+	['color', 'select-color'],
+	['datetime', 'datetime'],
+	['divider', 'presentation-divider'],
+	['dropdown', 'select-dropdown'],
+	['dropdown-multiselect', 'select-multiple-dropdown'],
+	['file', 'file'],
+	['hash', 'input-hash'],
+	['icon', 'select-icon'],
+	['image', 'file-image'],
+	['m2a-builder', 'list-m2a'],
+	['many-to-many', 'list-m2m'],
+	['many-to-one', 'select-dropdown-m2o'],
+	['markdown', 'input-rich-text-md'],
+	['notice', 'presentation-notice'],
+	['numeric', 'input'],
+	['one-to-many', 'list-o2m'],
+	['radio-buttons', 'select-radio'],
+	['repeater', 'list'],
+	['slider', 'slider'],
+	['slug', 'input'],
+	['tags', 'tags'],
+	['text-input', 'input'],
+	['textarea', 'input-multiline'],
+	['toggle', 'boolean'],
+	['translations', 'translations'],
+	['tree-view', 'list-o2m-tree-view'],
+	['user', 'select-dropdown-m2o'],
+	['wysiwyg', 'input-rich-text-html'],
+
+	// System:
+	['collection', 'system-collection'],
+	['collections', 'system-collection-multiple'],
+	['display-template', 'system-display-template'],
+	['field', 'system-field'],
+	['interface', 'system-interface'],
+	['interface-options', 'system-interface-options'],
+	['scope', 'system-scope'],
+	['system-language', 'system-language'],
+	['tfa-setup', 'system-mfa-setup'],
+];
+
+export async function up(knex: Knex): Promise<void> {
+	for (const [before, after] of changes) {
+		await knex('directus_fields').update({ interface: after }).where({ interface: before });
+	}
+}
+
+export async function down(knex: Knex): Promise<void> {
+	for (const [before, after] of changes) {
+		await knex('directus_fields').update({ interface: before }).where({ interface: after });
+	}
+}
