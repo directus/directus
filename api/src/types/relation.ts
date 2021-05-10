7caf8ac80bc78162d4de60bfa45bc87eea9cfcb2
rijkvanzanten <rijkvanzanten@me.com>
2021-05-10 13:45:18 -0400

    Update api type for relation record

--- api/src/types/relation.ts
+++ api/src/types/relation.ts
@@ -3,8 +3,14 @@ import { ForeignKey } from 'knex-schema-inspector/dist/types/foreign-key';
 export type RelationRaw = {
 	id: number;
 
+	many_collection: string;
+	many_field: string;
+
+	one_collection: string | null;
 	one_field: string | null;
+
 	one_deselect_action: 'nullify' | 'delete';
+
 	one_collection_field: string | null;
 	one_allowed_collections: string | null;
 
@@ -22,6 +28,10 @@ export type Relation = {
 	meta?: {
 		id: number;
 
+		many_collection: string;
+		many_field: string;
+
+		one_collection: string | null;
 		one_field: string | null;
 		one_collection_field: string | null;
 		one_allowed_collections: string[] | null;
