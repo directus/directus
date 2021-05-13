eb3e4bdbc877f853269e5c2a65365de15b87cb39
Kematia <tommasobartolucci11@gmail.com>
2021-05-13 14:54:03 +0200

    now filters all the children elements

--- app/src/layouts/_custom/product_table/layout-filters/filters.ts
+++ app/src/layouts/_custom/product_table/layout-filters/filters.ts
@@ -1,4 +1,8 @@
-import { Filter } from '@/types';
+import { Field, Filter } from '@/types';
+import { generateNormalized } from '@/interfaces/_custom/many-to-one-dropdown/methods';
+import { useCollectionsStore, useRelationsStore } from '@/stores';
+import useCollection from '@/composables/use-collection';
+import _ from 'lodash';
 
 export function createStatusFilter(value: string) {
 	const filter: Filter = {
@@ -11,12 +15,33 @@ export function createStatusFilter(value: string) {
 	return createFilter(filter);
 }
 
-export function createCategoryFilter(value: string) {
+export async function createCategoryFilter(value: string, field: Field) {
+	if (field == null) throw Error('Field is null!');
+
+	const { relatedCollection } = useRelation(field);
+
+	const { tree, list } = await generateNormalized(
+		relatedCollection.collection,
+		'id',
+		field?.meta?.options?.parentField,
+		field?.meta?.options?.childrenField
+	);
+
+	const uuids = [];
+
+	for (const key in list) {
+		if (Object.prototype.hasOwnProperty.call(list, key)) {
+			const item = list[key];
+
+			if (item.includes(value)) uuids.push(item);
+		}
+	}
+
 	const filter: Filter = {
 		key: 'category',
 		field: 'category.id',
-		operator: 'eq',
-		value,
+		operator: 'in',
+		value: _.uniq(uuids.flat()).join(','),
 	};
 
 	return createFilter(filter);
@@ -24,4 +49,17 @@ export function createCategoryFilter(value: string) {
 
 function createFilter(filter: Filter): Filter {
 	return filter;
+}
+
+function useRelation(field: Field) {
+	const relationsStore = useRelationsStore();
+	const collectionsStore = useCollectionsStore();
+
+	const relation = relationsStore.getRelationsForField(field.collection, field.field)?.[0];
+
+	const relatedCollection = collectionsStore.getCollection(relation.one_collection)!;
+
+	const { primaryKeyField: relatedPrimaryKeyField } = useCollection(relatedCollection.collection);
+
+	return { relation, relatedCollection, relatedPrimaryKeyField };
 }
