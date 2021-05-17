622a4b0a6d534912401b6b2b9ac5f1ebdafb38db
Kematia <tommasobartolucci11@gmail.com>
2021-05-17 19:00:07 +0200

    support array values in display

--- app/src/displays/formatted-json-value/formatted-json-value.vue
+++ app/src/displays/formatted-json-value/formatted-json-value.vue
@@ -1,19 +1,40 @@
 <template>
-	<value-null v-if="!displayValue" />
+	<div>
+		<value-null v-if="!displayValue" />
 
-	<span v-else>
-		{{ displayValue }}
-	</span>
+		<v-menu v-else-if="displayValue.length > 1" show-arrow :disabled="displayValue.length === 0">
+			<template #activator="{ toggle }">
+				<span @click.stop="toggle" class="toggle" :class="{ subdued: displayValue.length === 0 }">
+					<span class="label">
+						{{ displayValue.length }}
+						<template v-if="displayValue.length >= 100">+</template>
+						{{ $t('items') }}
+					</span>
+				</span>
+			</template>
+
+			<v-list class="links">
+				<v-list-item v-for="(item, index) in displayValue" :key="index">
+					<v-list-item-content>
+						<render-template :template="format" :item="item" />
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-menu>
+
+		<span v-else>
+			<render-template class="label" :template="format" :item="displayValue[0]" />
+		</span>
+	</div>
 </template>
 
 <script lang="ts">
-import { render } from 'micromustache';
 import { defineComponent, computed } from '@vue/composition-api';
 
 export default defineComponent({
 	props: {
 		value: {
-			type: Object,
+			type: [Object, Array],
 			default: null,
 		},
 		format: {
@@ -24,10 +45,11 @@ export default defineComponent({
 	setup(props) {
 		const displayValue = computed(() => {
 			if (!props.value) return null;
-			try {
-				return render(props.format || '', props.value);
-			} catch (error) {
-				return null;
+
+			if (Array.isArray(props.value)) {
+				return props.value;
+			} else {
+				return [props.value];
 			}
 		});
 
@@ -35,3 +57,45 @@ export default defineComponent({
 	},
 });
 </script>
+<style lang="scss" scoped>
+.label {
+	position: relative;
+	z-index: 2;
+	height: var(--v-list-item-min-height);
+}
+.toggle {
+	position: relative;
+
+	&::before {
+		position: absolute;
+		top: -6px;
+		left: -6px;
+		z-index: 1;
+		width: calc(100% + 12px);
+		height: calc(100% + 12px);
+		background-color: var(--background-normal);
+		border-radius: var(--border-radius);
+		opacity: 0;
+		transition: opacity var(--fast) var(--transition);
+		content: '';
+	}
+
+	&:not(.subdued):hover::before {
+		opacity: 1;
+	}
+
+	&:not(.subdued):active::before {
+		background-color: var(--background-normal-alt);
+	}
+}
+
+.subdued {
+	color: var(--foreground-subdued);
+}
+
+.links {
+	.v-list-item-content {
+		height: var(--v-list-item-min-height);
+	}
+}
+</style>
