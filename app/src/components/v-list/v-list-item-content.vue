e361a660c10a3deb3b272de3e578b261f3e84d49
rijkvanzanten <rijkvanzanten@me.com>
2021-05-19 15:03:32 -0400

    Replace v-deep in v-list-item-content

--- app/src/components/v-list/v-list-item-content.vue
+++ app/src/components/v-list/v-list-item-content.vue
@@ -11,7 +11,7 @@ body {
 }
 </style>
 
-<style lang="scss" scoped>
+<style scoped>
 .v-list-item-content {
 	display: flex;
 	flex-basis: 0;
@@ -23,28 +23,26 @@ body {
 	padding: var(--v-list-item-content-padding);
 	overflow: hidden;
 	font-family: var(--v-list-item-content-font-family);
+}
 
-	.v-list.three-line &,
-	.v-list-item.three-line & {
-		align-self: stretch;
-	}
+.v-list.three-line .v-list-item-content,
+.v-list-item.three-line .v-list-item-content {
+	align-self: stretch;
+}
 
-	::v-deep {
-		& > * {
-			flex-basis: 100%;
-			flex-grow: 1;
-			flex-shrink: 0;
-			line-height: 1.4;
+.v-list-item-content > :deep(*) {
+	flex-basis: 100%;
+	flex-grow: 1;
+	flex-shrink: 0;
+	line-height: 1.4;
+}
 
-			&:not(:last-child) {
-				margin-bottom: 2px;
-			}
-		}
-	}
+.v-list-item-content > :slotted(*:not(:last-child)) {
+	margin-bottom: 2px;
+}
 
-	.v-list:not(.large) &,
-	.v-list-item:not(.large) & {
-		--v-list-item-content-padding: 4px 0;
-	}
+.v-list:not(.large) .v-list-item-content,
+.v-list-item:not(.large) .v-list-item-content {
+	--v-list-item-content-padding: 4px 0;
 }
 </style>
