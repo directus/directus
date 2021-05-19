5132de4301460089248f326e1ab4204bb186564a
rijkvanzanten <rijkvanzanten@me.com>
2021-05-19 14:39:15 -0400

    Remove v-deep from v-card-actions

--- app/src/components/v-card/v-card-actions.vue
+++ app/src/components/v-card/v-card-actions.vue
@@ -2,14 +2,14 @@
 	<div class="v-card-actions"><slot /></div>
 </template>
 
-<style lang="scss" scoped>
+<style scoped>
 .v-card-actions {
 	display: flex;
 	justify-content: flex-end;
 	padding: var(--v-card-padding);
+}
 
-	& ::v-deep > .v-button + .v-button {
-		margin-left: 12px;
-	}
+.v-card-actions > :slotted(.v-button + .v-button) {
+	margin-left: 12px;
 }
 </style>
