aae6d27758b8cc0dbfdc361f15010367b25bf528
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2021-05-30 16:23:47 +0200

    Refactor docs export and docs route registration

--- app/src/modules/docs/index.ts
+++ app/src/modules/docs/index.ts
@@ -1,5 +1,6 @@
 import { defineModule } from '@/modules/define';
-import files, { Directory } from '@directus/docs';
+import routerPassthrough from '@/utils/router-passthrough';
+import docs, { DocsRoutes } from '@directus/docs';
 import { RouteRecordRaw } from 'vue-router';
 import NotFound from './routes/not-found.vue';
 import StaticDocs from './routes/static.vue';
@@ -10,7 +11,7 @@ export default defineModule(() => {
 			path: '',
 			redirect: '/docs/getting-started/introduction/',
 		},
-		...parseRoutes(files),
+		...getRoutes(docs),
 		{
 			path: ':_(.+)+',
 			component: NotFound,
@@ -25,25 +26,21 @@ export default defineModule(() => {
 		order: 20,
 	};
 
-	function parseRoutes(directory: Directory): RouteRecordRaw[] {
-		const routes: RouteRecordRaw[] = [];
-
-		for (const doc of directory.children) {
-			if (doc.type === 'file') {
-				routes.push({
-					path: doc.path.replace('.md', ''),
+	function getRoutes(routes: DocsRoutes): RouteRecordRaw[] {
+		return routes.map((route) => {
+			if (!('children' in route)) {
+				return {
+					path: route.name,
 					component: StaticDocs,
-				});
-			} else if (doc.type === 'directory') {
-				if (doc.path && doc.children && doc.children.length > 0)
-					routes.push({
-						path: doc.path.replace('.md', ''),
-						redirect: '/' + doc.children![0].path.replace('.md', ''),
-					});
-
-				routes.push(...parseRoutes(doc));
+				};
+			} else {
+				return {
+					path: route.name,
+					redirect: `/docs/${route.children[0].path}`,
+					component: routerPassthrough(),
+					children: getRoutes(route.children),
+				};
 			}
-		}
-		return routes;
+		});
 	}
 });
--- docs/build.js
+++ docs/build.js
@@ -1,11 +1,40 @@
-const dirTree = require('directory-tree');
+const path = require('path');
 const fse = require('fs-extra');
+const dirTree = require('directory-tree');
 
 console.log('Building docs...');
+
 const tree = dirTree('.', {
 	extensions: /\.md$/,
 	exclude: /(node_modules|.vuepress|.vscode|dist)/,
 	normalizePath: true,
 });
-fse.writeJSONSync('index.json', tree);
+
+const index = `export default ${generateIndex(tree.children)};`;
+
+fse.ensureDirSync('dist');
+fse.writeFileSync('dist/index.js', index);
+
 console.log('Built docs');
+
+function generateIndex(tree) {
+	const children = tree
+		.map((child) => {
+			if (child.type === 'file') {
+				const baseName = path.basename(child.name, child.extension);
+				const basePath = path.join(path.dirname(child.path), path.basename(child.path, child.extension));
+
+				return `{name:'${baseName}',path:'${basePath}',import:()=>import('../${child.path}?raw')}`;
+			} else if (child.type === 'directory') {
+				const children = generateIndex(child.children);
+
+				if (children === '[]') return null;
+				return `{name:'${child.name}',path:'${child.path}',children:${children}}`;
+			} else {
+				return null;
+			}
+		})
+		.filter((child) => child !== null);
+
+	return `[${children.join(',')}]`;
+}
