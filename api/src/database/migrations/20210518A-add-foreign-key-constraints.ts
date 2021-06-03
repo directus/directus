e985143274e0cc373d7b61890bca6533f779e326
rijkvanzanten <rijkvanzanten@me.com>
2021-06-03 15:22:31 -0400

    Handle illegal/corrupt relational rows better
    Fixes #6013

--- api/src/database/migrations/20210518A-add-foreign-key-constraints.ts
+++ api/src/database/migrations/20210518A-add-foreign-key-constraints.ts
@@ -8,7 +8,7 @@ export async function up(knex: Knex): Promise<void> {
 
 	const foreignKeys = await inspector.foreignKeys();
 	const relations = await knex
-		.select<RelationMeta[]>('many_collection', 'many_field', 'one_collection')
+		.select<RelationMeta[]>('id', 'many_collection', 'many_field', 'one_collection')
 		.from('directus_relations');
 
 	const constraintsToAdd = relations.filter((relation) => {
@@ -18,11 +18,34 @@ export async function up(knex: Knex): Promise<void> {
 		return exists === false;
 	});
 
+	const corruptedRelations: number[] = [];
+
 	for (const constraint of constraintsToAdd) {
 		if (!constraint.one_collection) continue;
 
+		if (
+			(await inspector.hasTable(constraint.many_collection)) === false ||
+			(await inspector.hasTable(constraint.one_collection)) === false
+		) {
+			logger.warn(
+				`Ignoring ${constraint.many_collection}.${constraint.many_field}<->${constraint.one_collection}. Tables don't exist.`
+			);
+
+			corruptedRelations.push(constraint.id);
+			continue;
+		}
+
 		const currentPrimaryKeyField = await inspector.primary(constraint.many_collection);
 		const relatedPrimaryKeyField = await inspector.primary(constraint.one_collection);
+
+		if (constraint.many_field === currentPrimaryKeyField) {
+			logger.warn(
+				`Illegal relationship ${constraint.many_collection}.${constraint.many_field}<->${constraint.one_collection} encountered. Many field equals collections primary key.`
+			);
+			corruptedRelations.push(constraint.id);
+			continue;
+		}
+
 		if (!currentPrimaryKeyField || !relatedPrimaryKeyField) continue;
 
 		const rowsWithIllegalFKValues = await knex
@@ -87,10 +110,18 @@ export async function up(knex: Knex): Promise<void> {
 			});
 		} catch (err) {
 			logger.warn(
-				`Couldn't add foreign key constraint for ${constraint.many_collection}.${constraint.many_field}->${constraint.one_collection}`
+				`Couldn't add foreign key constraint for ${constraint.many_collection}.${constraint.many_field}<->${constraint.one_collection}`
 			);
 			logger.warn(err);
 		}
+	}
+
+	if (corruptedRelations.length > 0) {
+		logger.warn(
+			`Encountered one or more corrupted relationships. Please check the following rows in "directus_relations": ${corruptedRelations.join(
+				', '
+			)}`
+		);
 	}
 }
 
@@ -108,7 +139,7 @@ export async function down(knex: Knex): Promise<void> {
 			});
 		} catch (err) {
 			logger.warn(
-				`Couldn't drop foreign key constraint for ${relation.many_collection}.${relation.many_field}->${relation.one_collection}`
+				`Couldn't drop foreign key constraint for ${relation.many_collection}.${relation.many_field}<->${relation.one_collection}`
 			);
 			logger.warn(err);
 		}
