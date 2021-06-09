3c1d925632cc5e24d4892eb4df8c637331a0b2e0
rijkvanzanten <rijkvanzanten@me.com>
2021-06-09 14:57:45 -0400

    Improve url-domain check

--- api/src/utils/is-url-allowed.ts
+++ api/src/utils/is-url-allowed.ts
@@ -1,16 +1,29 @@
-import { toArray } from "./to-array";
-
-const DOMAIN_REGEX = /\/\/(.*?)\//;
+import { toArray } from './to-array';
+import logger from '../logger';
 
 /**
- * Check if url matches white list either exactly or by domain
+ * Check if url matches allow list either exactly or by domain+path
  */
 export default function isUrlAllowed(url: string, allowList: string | string[]): boolean {
-  const urlWhitelist = toArray(allowList) as unknown as any[];
+	console.log(url, allowList);
 
-  if (!url) return true;
-  if (urlWhitelist.includes(url)) return true;
-  if (urlWhitelist.includes(url.match(DOMAIN_REGEX)?.[1])) return true;
+	const urlAllowList = toArray(allowList);
 
-  return false;
+	if (urlAllowList.includes(url)) return true;
+
+	const parsedWhitelist = urlAllowList.map((allowedURL) => {
+		try {
+			const { hostname, pathname } = new URL(allowedURL);
+			return hostname + pathname;
+		} catch {
+			logger.warn(`Invalid URL used "${url}"`);
+		}
+	});
+
+	try {
+		const { hostname, pathname } = new URL(url);
+		return parsedWhitelist.includes(hostname + pathname);
+	} catch {
+		return false;
+	}
 }
