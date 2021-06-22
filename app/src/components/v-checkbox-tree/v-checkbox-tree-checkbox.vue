af84d4f122519ffc1891e5c0b2a8c036c788caa5
rijkvanzanten <rijkvanzanten@me.com>
2021-06-22 16:02:07 -0400

    Add checkboxes-tree interface

--- app/src/components/v-checkbox-tree/v-checkbox-tree-checkbox.vue
+++ app/src/components/v-checkbox-tree/v-checkbox-tree-checkbox.vue
@@ -6,6 +6,7 @@
 				:checked="groupCheckedStateOverride"
 				:label="text"
 				:value="value"
+				:disabled="disabled"
 				v-model="treeValue"
 			/>
 		</template>
@@ -23,6 +24,7 @@
 			:text="choice[itemText]"
 			:value="choice[itemValue]"
 			:children="choice[itemChildren]"
+			:disabled="disabled"
 			v-model="treeValue"
 		/>
 	</v-list-group>
@@ -88,13 +90,31 @@ export default defineComponent({
 			type: String,
 			default: 'children',
 		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
 	},
 	setup(props, { emit }) {
 		const visibleChildrenValues = computed(() => {
 			if (!props.search) return props.children?.map((child) => child[props.itemValue]);
+
 			return props.children
-				?.filter((child) => child[props.itemText].toLowerCase().includes(props.search.toLowerCase()))
+				?.filter(
+					(child) =>
+						child[props.itemText].toLowerCase().includes(props.search.toLowerCase()) ||
+						childrenHaveMatch(child.children)
+				)
 				?.map((child) => child[props.itemValue]);
+
+			function childrenHaveMatch(children: Record<string, any>[] | undefined): boolean {
+				if (!children) return false;
+				return children.some(
+					(child) =>
+						child[props.itemText].toLowerCase().includes(props.search.toLowerCase()) ||
+						childrenHaveMatch(child[props.itemChildren])
+				);
+			}
 		});
 
 		const childrenValues = computed(() => props.children?.map((child) => child[props.itemValue]) || []);
---
+++ app/src/interfaces/select-multiple-checkbox-tree/select-multiple-checkbox-tree.vue
@@ -0,0 +1,69 @@
+<template>
+	<div class="select-multiple-checkbox-tree">
+		<v-input v-model="search" type="text" :placeholder="t('search')">
+			<template #prepend>
+				<v-icon name="search" />
+			</template>
+
+			<template #append v-if="search">
+				<v-icon name="clear" clickable @click="search = ''" />
+			</template>
+		</v-input>
+
+		<v-checkbox-tree
+			@update:model-value="$emit('input', $event)"
+			:model-value="value"
+			:search="search"
+			:disabled="disabled"
+			:choices="choices"
+			:value-combining="valueCombining"
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+type Choice = {
+	text: string;
+	value: string | number;
+	children?: Choice[];
+};
+
+export default defineComponent({
+	emits: ['input'],
+	props: {
+		value: {
+			type: Array as PropType<string[]>,
+			default: () => [],
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		choices: {
+			type: Array as PropType<Choice[]>,
+			default: () => [],
+		},
+		valueCombining: {
+			type: String as PropType<'all' | 'branch' | 'leaf' | 'indeterminate' | 'exclusive'>,
+			default: 'all',
+		},
+	},
+	setup() {
+		const { t } = useI18n();
+		const search = ref('');
+
+		return { search, t };
+	},
+});
+</script>
+
+<style scoped>
+.select-multiple-checkbox-tree {
+	padding: 8px;
+	border: var(--border-width) solid var(--border-normal);
+	border-radius: var(--border-radius);
+}
+</style>
