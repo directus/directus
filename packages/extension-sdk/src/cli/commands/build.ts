1a089726caff68a42bdb67fdac57795f924d01a1
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2021-06-30 18:11:32 +0200

    Fix building extensions with style tags
    Fixes #6556

--- packages/extension-sdk/src/cli/commands/build.ts
+++ packages/extension-sdk/src/cli/commands/build.ts
@@ -5,6 +5,7 @@ import { rollup } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import styles from 'rollup-plugin-styles';
 import vue from 'rollup-plugin-vue';
 import { SHARED_DEPS } from '@directus/shared/constants';
 
@@ -14,7 +15,7 @@ export default async function build(options: { input: string; output: string }):
 	const bundle = await rollup({
 		input: options.input,
 		external: SHARED_DEPS,
-		plugins: [vue(), nodeResolve(), commonjs(), terser()],
+		plugins: [vue(), styles(), nodeResolve(), commonjs(), terser()],
 	});
 
 	await bundle.write({
