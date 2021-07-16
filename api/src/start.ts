752bcf1b83f0e71fb94e7c7b296bf77dbf10bec8
msaaddev <mrsaadirfan@gmail.com>
2021-07-16 15:23:55 +0500

    Notify user if an update is available

--- api/src/start.ts
+++ api/src/start.ts
@@ -1,6 +1,10 @@
 import emitter, { emitAsyncSafe } from './emitter';
 import env from './env';
 import logger from './logger';
+import checkForUpdate from 'update-check';
+import logSym from 'log-symbols';
+import chalk from 'chalk';
+import pkg from '../package.json';
 
 // If this file is called directly using node, start the server
 if (require.main === module) {
@@ -17,7 +21,23 @@ export default async function start(): Promise<void> {
 	const port = env.PORT;
 
 	server
-		.listen(port, () => {
+		.listen(port, async () => {
+			let update = null;
+
+			try {
+				update = await checkForUpdate(pkg);
+			} catch (err) {
+				// eslint-disable-next-line no-console
+				console.log(err);
+			}
+
+			if (update) {
+				// eslint-disable-next-line no-console
+				console.log(`${logSym.info} ${chalk.yellow.bold(`New Version Available`)} (${update.latest})`);
+				// eslint-disable-next-line no-console
+				console.log('You can update by running: ' + chalk.cyan(`npx ${pkg.name}@latest [command]`));
+			}
+
 			logger.info(`Server started at port ${port}`);
 			emitAsyncSafe('server.start');
 		})
