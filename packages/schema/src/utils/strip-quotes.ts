155723d65a0dfb19c5b7168695375ec44e722b8e
rijkvanzanten <rijkvanzanten@me.com>
2021-07-26 12:16:44 -0400

    Add return type to stripQuotes function

--- packages/schema/src/utils/strip-quotes.ts
+++ packages/schema/src/utils/strip-quotes.ts
@@ -1,7 +1,16 @@
-export function stripQuotes(value?: string | null) {
-	const trimmed = value?.trim();
-	if ((trimmed?.startsWith(`'`) && trimmed?.endsWith(`'`)) || (trimmed?.startsWith('"') && trimmed?.endsWith('"'))) {
+/**
+ * Strip leading/trailing quotes from a string
+ */
+export function stripQuotes(value?: string | null): string | null {
+	if (value == null) {
+		return null;
+	}
+
+	const trimmed = value.trim();
+
+	if ((trimmed.startsWith(`'`) && trimmed.endsWith(`'`)) || (trimmed.startsWith('"') && trimmed.endsWith('"'))) {
 		return trimmed.slice(1, -1);
 	}
+
 	return value;
 }
