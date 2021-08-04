8293f9ef2419775d17ad632c06fcd96e3c84a319
rijkvanzanten <rijkvanzanten@me.com>
2021-08-04 19:19:08 -0400

    Remove unused import

--- api/src/services/payload.ts
+++ api/src/services/payload.ts
@@ -1,5 +1,5 @@
 import argon2 from 'argon2';
-import { format, parse, parseISO } from 'date-fns';
+import { format, parseISO } from 'date-fns';
 import Joi from 'joi';
 import { Knex } from 'knex';
 import { clone, cloneDeep, isObject, isPlainObject, omit } from 'lodash';
