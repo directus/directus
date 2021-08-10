39e7d41c253ed5b8a5433516e7a0d9a2a0fc456a
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2021-08-10 19:30:02 +0200

    Expose extension-sdk cli functions

--- packages/extension-sdk/src/cli/index.ts
+++ packages/extension-sdk/src/cli/index.ts
@@ -4,20 +4,28 @@ import build from './commands/build';
 
 const pkg = require('../../../package.json');
 
-const program = new Command();
+if (require.main === module) {
+	run();
+}
 
-program.name('directus-extension').usage('[command] [options]');
-program.version(pkg.version, '-v, --version');
+function run() {
+	const program = new Command();
 
-program.command('create').arguments('<type> <name>').description('Scaffold a new Directus extension').action(create);
+	program.name('directus-extension').usage('[command] [options]');
+	program.version(pkg.version, '-v, --version');
 
-program
-	.command('build')
-	.description('Bundle a Directus extension to a single entrypoint')
-	.option('-t, --type <type>', 'overwrite the extension type read from package manifest')
-	.option('-i, --input <file>', 'overwrite the entrypoint read from package manifest')
-	.option('-o, --output <file>', 'overwrite the output file read from package manifest')
-	.option('-f, --force', 'ignore the package manifest')
-	.action(build);
+	program.command('create').arguments('<type> <name>').description('Scaffold a new Directus extension').action(create);
 
-program.parse(process.argv);
+	program
+		.command('build')
+		.description('Bundle a Directus extension to a single entrypoint')
+		.option('-t, --type <type>', 'overwrite the extension type read from package manifest')
+		.option('-i, --input <file>', 'overwrite the entrypoint read from package manifest')
+		.option('-o, --output <file>', 'overwrite the output file read from package manifest')
+		.option('-f, --force', 'ignore the package manifest')
+		.action(build);
+
+	program.parse(process.argv);
+}
+
+export { create, build };
