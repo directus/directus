87dc418b8e230097b8eddd4452141ee2ef8aaf69
rijkvanzanten <rijkvanzanten@me.com>
2021-08-11 14:12:02 -0400

    Fix and merge migrations

--- api/src/database/migrations/20210811A-add-geometry-config.ts
+++ api/src/database/migrations/20210811A-add-geometry-config.ts
@@ -3,11 +3,13 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_settings', (table) => {
 		table.json('basemaps');
+		table.string('mapbox_key');
 	});
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.alterTable('directus_webhooks', (table) => {
+	await knex.schema.alterTable('directus_settings', (table) => {
 		table.dropColumn('basemaps');
+		table.dropColumn('mapbox_key');
 	});
 }
