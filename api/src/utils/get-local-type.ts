f2137ffd09000a55948975c2958c55efac787f2f
Nitwel <nitwel@arcor.de>
2021-08-17 16:44:32 +0200

    revert #7287

--- api/src/utils/get-local-type.ts
+++ api/src/utils/get-local-type.ts
@@ -11,7 +11,7 @@ type LocalTypeEntry = {
 const localTypeMap: Record<string, LocalTypeEntry> = {
 	// Shared
 	boolean: { type: 'boolean' },
-	tinyint: { type: 'integer' },
+	tinyint: { type: 'boolean' },
 	smallint: { type: 'integer' },
 	mediumint: { type: 'integer' },
 	int: { type: 'integer' },
@@ -143,11 +143,6 @@ export default function getLocalType(
 	/** Handle MS SQL varchar(MAX) (eg TEXT) types */
 	if (column.data_type === 'nvarchar' && column.max_length === -1) {
 		return { type: 'text' };
-	}
-
-	/** Handle Boolean as TINYINT*/
-	if (column.data_type.toLowerCase() === 'tinyint(1)' || column.data_type.toLowerCase() === 'tinyint(0)') {
-		return { type: 'boolean' };
 	}
 
 	if (type) {
