b19dc98c7765ea5f4bfc75fdce0f73edbce48cfa
rijkvanzanten <rijkvanzanten@me.com>
2021-08-26 17:54:07 -0400

    Don't attempt validatePayload with invalid rule

--- app/src/components/v-form/v-form.vue
+++ app/src/components/v-form/v-form.vue
@@ -237,13 +237,10 @@ export default defineComponent({
 						const conditions = [...field.meta.conditions].reverse();
 
 						const matchingCondition = conditions.find((condition) => {
-							if (!condition.rule) return;
-							let errors = [];
-							try {
-								errors = validatePayload(condition.rule, values.value, { requireAll: true });
-							} catch (error) {
-								errors = [error];
-							}
+							if (!condition.rule || Object.keys(condition.rule).length !== 1) return;
+
+							const errors = validatePayload(condition.rule, values.value, { requireAll: true });
+
 							return errors.length === 0;
 						});
 
