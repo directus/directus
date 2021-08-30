3ceb785a181ada6f0db1d3930e84a525b113640b
Nitwel <nitwel@arcor.de>
2021-08-30 11:00:26 +0200

    dont show null string in input

--- app/src/components/v-input/v-input.vue
+++ app/src/components/v-input/v-input.vue
@@ -20,7 +20,7 @@
 					:max="max"
 					:step="step"
 					:disabled="disabled"
-					:value="String(modelValue)"
+					:value="modelValue === null ? '' : String(modelValue)"
 					v-on="listeners"
 				/>
 			</slot>
