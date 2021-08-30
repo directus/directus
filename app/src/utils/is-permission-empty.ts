2753f618296d725cd59c20038b9881438dfe1d4b
Nitwel <nitwel@arcor.de>
2021-08-30 17:47:05 +0200

    add permission prop check (#7698)

--- app/src/utils/is-permission-empty.ts
+++ app/src/utils/is-permission-empty.ts
@@ -4,6 +4,7 @@ export function isPermissionEmpty(perm: Permission): boolean {
 	return (
 		(perm.fields || []).length === 0 &&
 		Object.keys(perm.validation || {}).length === 0 &&
-		Object.keys(perm.presets || {}).length === 0
+		Object.keys(perm.presets || {}).length === 0 &&
+		Object.keys(perm.permissions || {}).length === 0
 	);
 }
