0fc1ba1b167426f966fb4117fdf800a54350038a
Joel Bohórquez <joel@frontporchsolutions.com>
2021-09-07 22:49:55 -0500

    add event auth.jwt.before

--- api/src/services/authentication.ts
+++ api/src/services/authentication.ts
@@ -160,9 +160,24 @@ export class AuthenticationService {
 			}
 		}
 
-		const payload = {
+		let payload = {
 			id: user.id,
 		};
+
+		const [customClaims] = await emitter.emitAsync('auth.jwt.before', payload, {
+			event: 'auth.jwt.before',
+			action: 'jwt',
+			schema: this.schema,
+			payload: payload,
+			accountability: this.accountability,
+			status: 'pending',
+			user: user?.id,
+			database: this.knex,
+		});
+
+		if (customClaims) {
+			payload = { ...payload, ...customClaims };
+		}
 
 		/**
 		 * @TODO
