7c28b5f2827b49007a1d6f0e1a5e8c8502df808a
rijkvanzanten <rijkvanzanten@me.com>
2021-09-08 18:00:36 -0400

    Add apply snapshot functionality

--- api/src/types/snapshot.ts
+++ api/src/types/snapshot.ts
@@ -1,6 +1,7 @@
 import { Collection } from './collection';
 import { Relation } from './relation';
 import { Field } from '@directus/shared/types';
+import { Diff } from 'deep-diff';
 
 export type Snapshot = {
 	version: number;
@@ -8,4 +9,22 @@ export type Snapshot = {
 	collections: Collection[];
 	fields: Field[];
 	relations: Relation[];
+};
+
+export type SnapshotDiff = {
+	collections: {
+		collection: string;
+		diff: Diff<Collection | undefined>[];
+	}[];
+	fields: {
+		collection: string;
+		field: string;
+		diff: Diff<Field | undefined>[];
+	}[];
+	relations: {
+		collection: string;
+		field: string;
+		related_collection: string | null;
+		diff: Diff<Relation | undefined>[];
+	}[];
 };
--- api/src/utils/get-snapshot-diff.ts
+++ api/src/utils/get-snapshot-diff.ts
@@ -1,39 +1,116 @@
-import { Snapshot, Collection, Relation } from '../types';
-import { Field } from '@directus/shared/types';
+import { Snapshot, SnapshotDiff } from '../types';
 import { diff } from 'deep-diff';
+import { orderBy } from 'lodash';
 
-export function getSnapshotDiff(current: Snapshot, after: Snapshot) {
-	const createCollections: Collection[] = [];
-	const updateCollections: Collection[] = [];
+export function getSnapshotDiff(current: Snapshot, after: Snapshot): SnapshotDiff {
+	const diffedSnapshot: SnapshotDiff = {
+		collections: orderBy(
+			[
+				...current.collections.map((currentCollection) => {
+					const afterCollection = after.collections.find(
+						(afterCollection) => afterCollection.collection === currentCollection.collection
+					);
 
-	const deleteCollections: string[] = current.collections
-		.filter(
-			(currentCollection) =>
-				!!after.collections.find((afterCollection) => afterCollection.collection === currentCollection.collection) ===
-				false
-		)
+					return {
+						collection: currentCollection.collection,
+						diff: diff(currentCollection, afterCollection),
+					};
+				}),
+				...after.collections
+					.filter((afterCollection) => {
+						const currentCollection = current.collections.find(
+							(currentCollection) => currentCollection.collection === afterCollection.collection
+						);
+
+						return !!currentCollection === false;
+					})
+					.map((afterCollection) => ({
+						collection: afterCollection.collection,
+						diff: diff(undefined, afterCollection),
+					})),
+			].filter((obj) => Array.isArray(obj.diff)) as SnapshotDiff['collections'],
+			'collection'
+		),
+		fields: orderBy(
+			[
+				...current.fields.map((currentField) => {
+					const afterField = after.fields.find(
+						(afterField) => afterField.collection === currentField.collection && afterField.field === currentField.field
+					);
+
+					return {
+						collection: currentField.collection,
+						field: currentField.field,
+						diff: diff(currentField, afterField),
+					};
+				}),
+				...after.fields
+					.filter((afterField) => {
+						const currentField = current.fields.find(
+							(currentField) =>
+								currentField.collection === afterField.collection && afterField.field === currentField.field
+						);
+
+						return !!currentField === false;
+					})
+					.map((afterField) => ({
+						collection: afterField.collection,
+						field: afterField.field,
+						diff: diff(undefined, afterField),
+					})),
+			].filter((obj) => Array.isArray(obj.diff)) as SnapshotDiff['fields'],
+			['collection']
+		),
+		relations: orderBy(
+			[
+				...current.relations.map((currentRelation) => {
+					const afterRelation = after.relations.find(
+						(afterRelation) =>
+							afterRelation.collection === currentRelation.collection && afterRelation.field === currentRelation.field
+					);
+
+					return {
+						collection: currentRelation.collection,
+						field: currentRelation.field,
+						related_collection: currentRelation.related_collection,
+						diff: diff(currentRelation, afterRelation),
+					};
+				}),
+				...after.relations
+					.filter((afterRelation) => {
+						const currentRelation = current.relations.find(
+							(currentRelation) =>
+								currentRelation.collection === afterRelation.collection && afterRelation.field === currentRelation.field
+						);
+
+						return !!currentRelation === false;
+					})
+					.map((afterRelation) => ({
+						collection: afterRelation.collection,
+						field: afterRelation.field,
+						related_collection: afterRelation.related_collection,
+						diff: diff(undefined, afterRelation),
+					})),
+			].filter((obj) => Array.isArray(obj.diff)) as SnapshotDiff['relations'],
+			['collection']
+		),
+	};
+
+	/**
+	 * When you delete a collection, we don't have to individually drop all the fields/relations as well
+	 */
+
+	const deletedCollections = diffedSnapshot.collections
+		.filter((collection) => collection.diff?.[0].kind === 'D')
 		.map(({ collection }) => collection);
 
-	for (const afterCollection of after.collections) {
-		const currentCollection = current.collections.find(
-			(currentCollection) => currentCollection.collection === afterCollection.collection
-		);
+	diffedSnapshot.fields = diffedSnapshot.fields.filter(
+		(field) => deletedCollections.includes(field.collection) === false
+	);
 
-		if (!currentCollection) {
-			createCollections.push(afterCollection);
-			continue;
-		}
+	diffedSnapshot.relations = diffedSnapshot.relations.filter(
+		(relation) => deletedCollections.includes(relation.collection) === false
+	);
 
-		const difference = diff(currentCollection, afterCollection);
-
-		if (difference) {
-			updateCollections.push(afterCollection);
-		}
-	}
-
-	return {
-		createCollections,
-		updateCollections,
-		deleteCollections,
-	};
+	return diffedSnapshot;
 }
