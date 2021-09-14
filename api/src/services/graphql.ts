cbd78d138d4ff5923df8d4b439c856574b1d3280
Gab <gkielwasser@gmail.com>
2021-09-14 18:28:39 +0200

    Store original error code in extensions

--- api/src/services/graphql.ts
+++ api/src/services/graphql.ts
@@ -1054,9 +1054,10 @@ export class GraphQLService {
 	 */
 	formatError(error: BaseException | BaseException[]) {
 		if (Array.isArray(error)) {
+			error[0].extensions.code = error[0].code;
 			return new GraphQLError(error[0].message, undefined, undefined, undefined, undefined, error[0]);
 		}
-
+		error.extensions.code = error.code;
 		return new GraphQLError(error.message, undefined, undefined, undefined, undefined, error);
 	}
 
