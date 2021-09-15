8eb745b87ef1e6b03b51289df2319f2d3c164dac
Timothy Stapleton <retrocede@outlook.com>
2021-09-14 19:55:37 -0700

    migration for public url

---
+++ api/src/database/migrations/20210905A-add-public-url.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_files', (table) => {
+		table.string('public_url').nullable().defaultTo(null);
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_files', (table) => {
+		table.dropColumn('public_url');
+	});
+}
