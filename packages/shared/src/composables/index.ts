d1ba549147759c3277cca88d209c673f11853ae1
rijkvanzanten <rijkvanzanten@me.com>
2021-09-15 16:24:50 -0400

    Sort index.ts exports

--- packages/shared/src/composables/index.ts
+++ packages/shared/src/composables/index.ts
@@ -1,5 +1,5 @@
-export * from './use-system';
 export * from './use-collection';
 export * from './use-filter-fields';
 export * from './use-items';
 export * from './use-sync';
+export * from './use-system';
