d679bdc571029d892c34a1abb7141712cd7836f3
SousVidal <sousvidal@gmail.com>
2021-09-17 15:25:07 +0200

    fix 5752 by setting 'item' as 'editsAtStart.value' when there is no primary key.

--- app/src/interfaces/list-o2m/list-o2m.vue
+++ app/src/interfaces/list-o2m/list-o2m.vue
@@ -377,15 +377,15 @@ export default defineComponent({
 				const pkField = relatedPrimaryKeyField.value?.field;
 				if (!pkField) return;
 				const hasPrimaryKey = pkField in item;
-
+				
 				const edits = (props.value || []).find(
 					(edit: any) =>
 						typeof edit === 'object' &&
 						relatedPrimaryKeyField.value?.field &&
 						edit[relatedPrimaryKeyField.value?.field] === item[relatedPrimaryKeyField.value?.field]
 				);
-
-				editsAtStart.value = edits || { [pkField]: item[pkField] || {} };
+				
+				editsAtStart.value = hasPrimaryKey ? edits || { [pkField]: item[pkField] || {} } : item;
 				currentlyEditing.value = hasPrimaryKey ? item[pkField] : '+';
 			}
 
