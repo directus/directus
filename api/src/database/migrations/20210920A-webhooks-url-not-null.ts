fcc9aa3156648d54663b781e5c7fabfe5b6fef9c
Aiden Foxx <aiden.foxx.mail@gmail.com>
2021-09-20 19:25:03 +0200

    Update 20210920A-webhooks-url-not-null.ts

--- api/src/database/migrations/20210920A-webhooks-url-not-null.ts
+++ api/src/database/migrations/20210920A-webhooks-url-not-null.ts
@@ -5,7 +5,7 @@ import Client_Oracledb from 'knex/lib/dialects/oracledb';
 export async function up(knex: Knex): Promise<void> {
 	if (knex.client instanceof Client_Oracledb) {
 		/* Oracle is already not nullable due to an oversight in
-			 "20201105B-change-webhook-url-type.ts" */
+		   "20201105B-change-webhook-url-type.ts" */
 		return;
 	}
 
@@ -17,7 +17,7 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
 	if (knex.client instanceof Client_Oracledb) {
 		/* Oracle is already not nullable due to an oversight in
-			 "20201105B-change-webhook-url-type.ts" */
+		   "20201105B-change-webhook-url-type.ts" */
 		return;
 	}
 
