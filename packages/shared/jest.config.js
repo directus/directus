a7228f2815193fd79d9442ae031fc9b39d928c1a
Jay Cammarano <67079013+jaycammarano@users.noreply.github.com>
2021-09-22 15:22:24 -0400

    Tests are written for get-relation-type.ts (#8245)
    * setup jest in shared. adjust-date tests.
    
    * define-extensions
    
    * filters to query
    
    * get-fields-from-template.test.ts
    base coverage
    
    * get filter operators for type
    
    * tests passing, 100% coverage
    
    * formatting
    
    * removed unneccessary tests

---
+++ packages/shared/jest.config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+module.exports = {
+	preset: 'ts-jest',
+	verbose: true,
+	setupFiles: ['dotenv/config'],
+	testURL: process.env.TEST_URL || 'http://localhost',
+	collectCoverageFrom: ['src/**/*.ts'],
+	testPathIgnorePatterns: ['dist'],
+};
