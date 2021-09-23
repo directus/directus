111515bc920f2df04e33f8e05b7c335891121c51
rijkvanzanten <rijkvanzanten@me.com>
2021-09-23 16:37:01 -0400

    Move session data blob to directus_sessions

--- api/src/database/migrations/20210903A-add-auth-provider.ts
+++ api/src/database/migrations/20210903A-add-auth-provider.ts
@@ -3,10 +3,13 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_users', (table) => {
 		table.string('provider', 128).notNullable().defaultTo('default');
-		table.string('external_identifier', 512).unique();
-		table.json('auth_data');
+		table.string('external_identifier').unique();
 
 		table.string('email', 128).nullable().alter();
+	});
+
+	await knex.schema.alterTable('directus_sessions', (table) => {
+		table.json('data');
 	});
 }
 
@@ -14,8 +17,11 @@ export async function down(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_users', (table) => {
 		table.dropColumn('provider');
 		table.dropColumn('external_identifier');
-		table.dropColumn('auth_data');
 
 		table.string('email', 128).notNullable().alter();
+	});
+
+	await knex.schema.alterTable('directus_sessions', (table) => {
+		table.dropColumn('data');
 	});
 }
