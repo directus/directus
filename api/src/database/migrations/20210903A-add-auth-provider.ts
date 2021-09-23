887f244ffc9e441ca7600d8d7aae1ad93c082f2b
rijkvanzanten <rijkvanzanten@me.com>
2021-09-23 16:21:52 -0400

    Use JSON for auth_data field

--- api/src/database/migrations/20210903A-add-auth-provider.ts
+++ api/src/database/migrations/20210903A-add-auth-provider.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_users', (table) => {
 		table.string('provider', 128).notNullable().defaultTo('default');
 		table.string('external_identifier', 512).unique();
-		table.text('auth_data');
+		table.json('auth_data');
 
 		table.string('email', 128).nullable().alter();
 	});
