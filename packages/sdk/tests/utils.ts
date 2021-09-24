0408aa0c3072c08a379480227002e6821e8aa253
Jay Cammarano <67079013+jaycammarano@users.noreply.github.com>
2021-09-24 13:07:23 -0400

    Update packages/sdk/tests/utils.ts
    Co-authored-by: José Varela <joselcvarela@gmail.com>

--- packages/sdk/tests/utils.ts
+++ packages/sdk/tests/utils.ts
@@ -102,7 +102,7 @@ export function generateHash(stringToHash: string): Promise<string> {
 	const buffer = 'string' as unknown as Buffer;
 	const argon2HashConfigOptions = { test: 'test', associatedData: buffer }; // Disallow the HASH_RAW option, see https://github.com/directus/directus/discussions/7670#discussioncomment-1255805
 	// test, if specified, must be passed as a Buffer to argon2.hash, see https://github.com/ranisalt/node-argon2/wiki/Options#test
-	'test' in argon2HashConfigOptions &&
-		(argon2HashConfigOptions.associatedData = Buffer.from(argon2HashConfigOptions.associatedData));
+	if ('test' in argon2HashConfigOptions)
+	  argon2HashConfigOptions.associatedData = Buffer.from(argon2HashConfigOptions.associatedData);
 	return argon2.hash(stringToHash, argon2HashConfigOptions);
 }
