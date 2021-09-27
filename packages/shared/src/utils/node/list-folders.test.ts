82b41eac4bd2856302fd737a03bbfdc13f893742
jaycammarano <jay.cammarano@gmail.com>
2021-09-27 17:48:32 -0400

    listFolders test updated to use random temp folder

--- packages/shared/src/utils/node/list-folders.test.ts
+++ packages/shared/src/utils/node/list-folders.test.ts
@@ -1,22 +1,20 @@
 import { listFolders } from '.';
+import { dirSync, SynchrounousResult } from 'tmp';
 
 describe('', () => {
+	let rootDir: SynchrounousResult;
+	let childDir: SynchrounousResult;
+
+	beforeEach(() => {
+		rootDir = dirSync({ unsafeCleanup: true });
+		childDir = dirSync({ dir: rootDir.name });
+	});
+
+	afterEach(() => {
+		rootDir.removeCallback();
+	});
 	it('returns all the subdirectories of the current directory', async () => {
-		expect(await listFolders('./')).toStrictEqual([
-			'coverage',
-			'displays',
-			'dist',
-			'endpoints',
-			'extension',
-			'hooks',
-			'interfaces',
-			'layouts',
-			'modules',
-			'node_modules',
-			'panels',
-			'src',
-			'utils',
-			'~',
-		]);
+		const childPath = childDir.name.split('/');
+		expect(await listFolders(rootDir.name)).toStrictEqual([childPath[childPath?.length - 1]]);
 	});
 });
