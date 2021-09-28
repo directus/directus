34860e19ca84409cf6fd231b11a645e17fc08661
Azri Kahar <azrikahar@outlook.com>
2021-09-28 17:31:27 +0800

    fix "add conditions to fields" migration

--- api/src/database/migrations/20210716A-add-conditions-to-fields.ts
+++ api/src/database/migrations/20210716A-add-conditions-to-fields.ts
@@ -7,7 +7,7 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.alterTable('directus_files', (table) => {
+	await knex.schema.alterTable('directus_fields', (table) => {
 		table.dropColumn('conditions');
 	});
 }
