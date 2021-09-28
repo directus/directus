f5c1dcf545a373798b449251a9e6c4cfe94a8204
jaycammarano <jay.cammarano@gmail.com>
2021-09-28 10:05:42 -0400

    moved before/after into describe

--- packages/shared/src/utils/node/ensure-extension-dirs.test.ts
+++ packages/shared/src/utils/node/ensure-extension-dirs.test.ts
@@ -5,15 +5,15 @@ import { dirSync, SynchrounousResult } from 'tmp';
 
 let rootDir: SynchrounousResult;
 
-beforeEach(() => {
-	rootDir = dirSync({ unsafeCleanup: true });
-});
-
-afterEach(() => {
-	rootDir.removeCallback();
-});
-
 describe('ensureExtensionDirs', () => {
+	beforeEach(() => {
+		rootDir = dirSync({ unsafeCleanup: true });
+	});
+
+	afterEach(() => {
+		rootDir.removeCallback();
+	});
+
 	const types = EXTENSION_TYPES as readonly ExtensionType[];
 	it('returns undefined if the folders exist', async () => {
 		expect(await ensureExtensionDirs(rootDir.name, types)).toBe(undefined);
