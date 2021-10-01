9f28c85bfa3f12df5447d2a383dcebd0f7c819ec
jaycammarano <jay.cammarano@gmail.com>
2021-10-01 19:48:42 -0400

    converted to faker

--- tests/api/items.test.ts
+++ tests/api/items.test.ts
@@ -3,12 +3,13 @@ import config from '../config';
 import { getDBsToTest } from '../get-dbs-to-test';
 import knex, { Knex } from 'knex';
 import { v4 as uuid } from 'uuid';
-
+import { address } from 'faker';
 describe('/items', () => {
 	const databases = new Map<string, Knex>();
 
 	const userID = uuid();
 	const roleID = uuid();
+
 	beforeAll(async () => {
 		const vendors = getDBsToTest();
 
@@ -36,10 +37,14 @@ describe('/items', () => {
 			});
 
 			await database!('directus_collections').insert({
-				collection: 'test_collection',
+				collection: 'address',
 			});
-			await database!.schema.createTableIfNotExists('test_collection', (table) => {
+			await database!.schema.createTableIfNotExists('address', (table) => {
 				table.uuid('id').primary();
+				table.string('streetName');
+				table.string('city');
+				table.string('zipCode');
+				table.string('country');
 			});
 		}
 	});
@@ -49,24 +54,49 @@ describe('/items', () => {
 			const database = databases.get(vendor);
 			await database!('directus_users').where('id', userID).del();
 			await database!('directus_roles').where('id', roleID).del();
-			await database!('directus_collections').where('collection', 'test_collection').del();
+			await database!('directus_collections').where('collection', 'address').del();
 
-			await database!.schema.dropTableIfExists('test_collection');
+			await database!.schema.dropTableIfExists('address');
 			connection.destroy();
 		}
 	});
 
+	const createAddress = () => ({
+		id: uuid(),
+		streetName: address.streetName(),
+		city: address.city(),
+		zipCode: address.zipCode(),
+		country: address.country(),
+	});
+
+	const seedAddress = async function (knex: Knex<any, unknown>, count: number): Promise<void> {
+		const fakeAddresses = [];
+
+		for (let i = 0; i < count; i++) {
+			fakeAddresses.push(createAddress());
+		}
+		await knex('address').insert(fakeAddresses);
+	};
+
 	describe('/:collection', () => {
 		it.each(getDBsToTest())('%p retrieves all items', async (vendor) => {
 			const url = `http://localhost:${config.ports[vendor]!}`;
+			seedAddress(databases.get(vendor)!, 10);
 
 			const response = await request(url)
-				.get('/items/test_collection')
+				.get('/items/address')
 				.set('Authorization', 'Bearer test_token')
 				.expect('Content-Type', /application\/json/)
 				.expect(200);
 
-			expect(response.body).toStrictEqual({ data: [] });
+			expect(response.body.data.length).toBe(10);
+			expect(Object.keys(response.body.data[0]).sort()).toStrictEqual([
+				'city',
+				'country',
+				'id',
+				'streetName',
+				'zipCode',
+			]);
 		});
 	});
 });
