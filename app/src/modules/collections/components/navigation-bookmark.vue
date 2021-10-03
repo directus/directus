82f18faff16ceb55a5cceb840c8e95d1b35eddf7
Alejandro Castaño González <alejandro@theseedsquad.com>
2021-10-03 14:29:31 +0200

    Update navigation-bookmark

--- app/src/modules/collections/components/navigation-bookmark.vue
+++ app/src/modules/collections/components/navigation-bookmark.vue
@@ -1,6 +1,8 @@
 <template>
 	<v-list-item :to="bookmark.to" query class="bookmark" @contextmenu.prevent.stop="activateContextMenu">
-		<v-list-item-icon><v-icon name="bookmark_outline" /></v-list-item-icon>
+		<v-list-item-icon>
+			<v-icon :name="bookmark.icon || 'bookmark_border'" :color="bookmark.color" />
+		</v-list-item-icon>
 		<v-list-item-content>
 			<v-text-overflow :text="bookmark.bookmark" />
 		</v-list-item-content>
