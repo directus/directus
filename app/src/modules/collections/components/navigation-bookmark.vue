404c6b65ef3bfc1571a52b74fd1869e1263de912
Alejandro Castaño González <alejandro@theseedsquad.com>
2021-10-03 14:37:05 +0200

    Allows to hide icon

--- app/src/modules/collections/components/navigation-bookmark.vue
+++ app/src/modules/collections/components/navigation-bookmark.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-list-item :to="bookmark.to" query class="bookmark" @contextmenu.prevent.stop="activateContextMenu">
-		<v-list-item-icon>
-			<v-icon :name="bookmark.icon || 'bookmark_border'" :color="bookmark.color" />
+		<v-list-item-icon v-if="bookmark.icon">
+			<v-icon :name="bookmark.icon" :color="bookmark.color" />
 		</v-list-item-icon>
 		<v-list-item-content>
 			<v-text-overflow :text="bookmark.bookmark" />
