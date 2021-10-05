fe5956f88cb9f377e8a73fa1f0ed3013d9abc2e7
Jose Varela <joselcvarela@gmail.com>
2021-10-05 08:16:35 +0100

    declare as ES module Remove rollup and use tsc for compilation. We only want to support ESM. It's the future.

--- packages/sdk/jest.config.js
+++ packages/sdk/jest.config.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
-module.exports = {
+export default {
 	preset: 'ts-jest',
 	verbose: true,
 	setupFiles: ['dotenv/config'],
