3028a494eea04319fd5462c850df962028b4cd2f
Azri Kahar <azrikahar@outlook.com>
2021-10-07 16:15:23 +0800

    update stylelint package to include prettier

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
+	extends: ['stylelint-config-standard', 'stylelint-config-rational-order', 'stylelint-config-prettier'],
 	plugins: ['stylelint-order', 'stylelint-scss'],
 	rules: {
 		indentation: 'tab',
