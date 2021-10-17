5aad1f700378a15eddbb53e228530891417f123c
Jay Cammarano <jay.cammarano@gmail.com>
2021-10-17 10:57:10 -0400

    added guest_events, changed user => guest

--- tests/setup/migrations/20211016054421_create_relational_tables.js
+++ tests/setup/migrations/20211016054421_create_relational_tables.js
@@ -10,9 +10,17 @@ exports.up = function (knex) {
 		table.integer('item');
 		table.string('collection');
 	});
+	knex.schema.createTable('guests_events', (table) => {
+		table.increments('id');
+		table.integer('guests_id').unsigned().references('id').inTable('artists');
+		table.integer('event_id').unsigned().references('id').inTable('events');
+		table.float('matchRating');
+		table.boolean('rsvp');
+	});
 };
 
 exports.down = function (knex) {
 	knex.schema.dropTable('artists_events');
+	knex.schema.dropTable('guests_events');
 	knex.schema.dropTable('tours_components');
 };
