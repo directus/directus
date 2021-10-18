3b71f93944787ce2a24f057ab1d3235afa6d116e
Jay Cammarano <jay.cammarano@gmail.com>
2021-10-18 09:52:05 -0400

    removing system_tables migration file

--- tests/setup/migrations/20211016054351_create_some_system_tables.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-
-/* Placeholder if this is a good idea. Dont think it is though bc columns change
- * and those changes would have to be manually mirrored here
- */
-
-exports.up = function (knex) {};
-
-exports.down = function (knex) {};
