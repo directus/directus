00e3baf70cdc97b957ae8ce5a68449ebb1eb7c26
Jay Cammarano <jay.cammarano@gmail.com>
2021-10-18 11:40:10 -0400

    added relational tables to collections

--- tests/setup/seeds/02_directus_relations.js
+++ tests/setup/seeds/02_directus_relations.js
@@ -18,16 +18,16 @@ exports.seed = function (knex) {
 			one_field: 'events_id',
 		},
 		{
-			many_collection: 'guests',
-			many_field: 'id',
-			one_collection: 'guests_events',
-			one_field: 'guests_id',
+			many_collection: 'guests_events',
+			many_field: 'guest_id',
+			one_collection: 'guests',
+			one_field: 'id',
 		},
 		{
-			many_collection: 'events',
-			many_field: 'id',
-			one_collection: 'guests_events',
-			one_field: 'events_id',
+			many_collection: 'guests_events',
+			many_field: 'events_id',
+			one_collection: 'events',
+			one_field: 'id',
 		},
 	]);
 };
