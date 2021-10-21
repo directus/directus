c18fd0efbd4de35128e54a73cd9d9a8ef71e1f0a
rijkvanzanten <rijkvanzanten@me.com>
2021-10-21 17:08:13 -0400

    Delete unused util

--- api/src/utils/is-email-allowed.ts
+++
@@ -1,9 +0,0 @@
-import isUrlAllowed from './is-url-allowed';
-
-/**
- * Check if email domain matches allow list
- */
-export default function isEmailAllowed(email: string, allowList: string | string[]): boolean {
-	const emailData = email.split('@');
-	return emailData.length > 1 && isUrlAllowed(emailData.pop()!, allowList);
-}
