7d180a45e0b4ec0a550dba1615af2ff0962b6a14
Jay Cammarano <jay.cammarano@gmail.com>
2021-10-22 16:06:48 -0400

    logical operators

--- tests/api/items/filters.test.ts
+++ tests/api/items/filters.test.ts
@@ -2,7 +2,7 @@ import config from '../../config';
 import { getDBsToTest } from '../../get-dbs-to-test';
 import request from 'supertest';
 import knex, { Knex } from 'knex';
-import { createGuest, seedTable } from '../../setup/utils/factories';
+import { createArtist, createGuest, seedTable } from '../../setup/utils/factories';
 
 describe('/items', () => {
 	const databases = new Map<string, Knex>();
@@ -54,37 +54,50 @@ describe('/items', () => {
 			});
 		});
 
-		// describe('Logical Operators', () => {
-		// 	// it.each(getDBsToTest())(
-		// 	// 	'%p returns users with name equality _AND favorite_artist equality',
-		// 	// 	async (vendor) => {}
-		// 	// );
-		// 	it.only.each(getDBsToTest())(
-		// 		'%p returns users with name equality _OR favorite_artist equality',
-		// 		async (vendor) => {
-		// 			const url = `http://localhost:${config.ports[vendor]!}`;
-		// 			const insertedArtist = await seedTable(databases.get(vendor)!, 10, 'artists', createArtist(), {
-		// 				select: ['id'],
-		// 			});
-		// 			const guest = createGuest();
-		// 			guest.favorite_artist = insertedArtist.length;
-		// 			await seedTable(databases.get(vendor)!, 10, 'guests', guest);
+		describe('Logical Operators', () => {
+			it.each(getDBsToTest())('%p returns users with name equality _AND favorite_artist equality', async (vendor) => {
+				const url = `http://localhost:${config.ports[vendor]!}`;
+				const insertedArtist = await seedTable(databases.get(vendor)!, 10, 'artists', createArtist, {
+					select: ['id'],
+				});
+				const pool1 = createGuest();
+				await seedTable(databases.get(vendor)!, 5, 'guests', pool1);
 
-		// 			const response = await request(url)
-		// 				.get(
-		// 					`/items/guests?filter={"_or": {"name": { "_eq": "${guest.name}" },"favorite_artist": { "_eq": "${guest.favorite_artist}"},},}`
-		// 				)
-		// 				.set('Authorization', 'Bearer AdminToken')
-		// 				.expect('Content-Type', /application\/json/)
-		// 				.expect(200);
+				pool1.favorite_artist = insertedArtist.length - 1;
+				await seedTable(databases.get(vendor)!, 5, 'guests', pool1);
 
-		// 			expect(response.body.data.length).toBeGreaterThanOrEqual(10);
-		// 			expect(response.body.data[0]).toMatchObject({
-		// 				birthday: expect.any(String),
-		// 				favorite_artist: expect.any(Number),
-		// 			});
-		// 		}
-		// 	);
-		// });
+				const response = await request(url)
+					.get(
+						`/items/guests?filter={"_and":[{"name": { "_eq": "${pool1.name}" }},{"favorite_artist": { "_eq": "${pool1.favorite_artist}" }}]}`
+					)
+					.set('Authorization', 'Bearer AdminToken')
+					.expect('Content-Type', /application\/json/)
+					.expect(200);
+
+				expect(response.body.data.length).toBe(5);
+			});
+			it.each(getDBsToTest())('%p returns users with name equality _OR favorite_artist equality', async (vendor) => {
+				const url = `http://localhost:${config.ports[vendor]!}`;
+				const insertedArtist = await seedTable(databases.get(vendor)!, 10, 'artists', createArtist, {
+					select: ['id'],
+				});
+				const pool1 = createGuest();
+				await seedTable(databases.get(vendor)!, 5, 'guests', pool1);
+
+				const pool2 = createGuest();
+				pool2.favorite_artist = insertedArtist.length - 1;
+				await seedTable(databases.get(vendor)!, 5, 'guests', pool2);
+
+				const response = await request(url)
+					.get(
+						`/items/guests?filter={"_or":[{"name": { "_eq": "${pool1.name}" }},{"favorite_artist": { "_eq": "${pool2.favorite_artist}" }},]}`
+					)
+					.set('Authorization', 'Bearer AdminToken')
+					.expect('Content-Type', /application\/json/)
+					.expect(200);
+
+				expect(response.body.data.length).toBe(10);
+			});
+		});
 	});
 });
