bb4edc97104190279a777acc4a11c632401cdea5
rijkvanzanten <rijkvanzanten@me.com>
2021-10-23 13:31:22 -0400

    Use function style interface options in m2m

--- app/src/interfaces/list-m2m/index.ts
+++ app/src/interfaces/list-m2m/index.ts
@@ -1,7 +1,7 @@
 import { defineInterface } from '@directus/shared/utils';
 import InterfaceListM2M from './list-m2m.vue';
-import Options from './options.vue';
 import PreviewSVG from './preview.svg?raw';
+import { ExtensionsOptionsContext } from '@directus/shared/types';
 
 export default defineInterface({
 	id: 'list-m2m',
@@ -13,7 +13,48 @@ export default defineInterface({
 	types: ['alias'],
 	localTypes: ['m2m'],
 	group: 'relational',
-	options: Options,
+	options: ({ relations }: ExtensionsOptionsContext) => {
+		return [
+			{
+				field: 'template',
+				name: '$t:display_template',
+				meta: {
+					interface: 'system-display-template',
+					options: {
+						collection: relations.o2m?.collection ?? null,
+					},
+				},
+			},
+			{
+				field: 'enableCreate',
+				name: '$t:creating_items',
+				schema: {
+					default_value: true,
+				},
+				meta: {
+					interface: 'boolean',
+					options: {
+						label: '$t:enable_create_button',
+					},
+					width: 'half',
+				},
+			},
+			{
+				field: 'enableSelect',
+				name: '$t:selecting_items',
+				schema: {
+					default_value: true,
+				},
+				meta: {
+					interface: 'boolean',
+					options: {
+						label: '$t:enable_select_button',
+					},
+					width: 'half',
+				},
+			},
+		];
+	},
 	recommendedDisplays: ['related-values'],
 	preview: PreviewSVG,
 });
