0c2a508764e4219602d6bd4eb55d68b834f37e61
rijkvanzanten <rijkvanzanten@me.com>
2021-10-25 13:11:16 -0400

    Remove unnecessary option component overrides

--- app/src/interfaces/list-o2m-tree-view/index.ts
+++ app/src/interfaces/list-o2m-tree-view/index.ts
@@ -1,6 +1,6 @@
 import { defineInterface } from '@directus/shared/utils';
-import Options from './options.vue';
 import InterfaceListO2MTreeView from './list-o2m-tree-view.vue';
+import { ExtensionsOptionsContext } from '@directus/shared/types';
 
 export default defineInterface({
 	id: 'list-o2m-tree-view',
@@ -12,5 +12,49 @@ export default defineInterface({
 	group: 'relational',
 	relational: true,
 	component: InterfaceListO2MTreeView,
-	options: Options,
+	options: ({ relations }: ExtensionsOptionsContext) => {
+		const collection = relations.o2m?.collection;
+
+		return [
+			{
+				field: 'template',
+				name: '$t:display_template',
+				meta: {
+					interface: 'system-display-template',
+					options: {
+						collectionName: collection,
+					},
+					width: 'full',
+				},
+			},
+			{
+				field: 'enableCreate',
+				name: '$t:creating_items',
+				schema: {
+					default_value: true,
+				},
+				meta: {
+					interface: 'boolean',
+					options: {
+						label: '$t:enable_create_button',
+					},
+					width: 'half',
+				},
+			},
+			{
+				field: 'enableSelect',
+				name: '$t:selecting_items',
+				schema: {
+					default_value: true,
+				},
+				meta: {
+					interface: 'boolean',
+					options: {
+						label: '$t:enable_select_button',
+					},
+					width: 'half',
+				},
+			},
+		];
+	},
 });
