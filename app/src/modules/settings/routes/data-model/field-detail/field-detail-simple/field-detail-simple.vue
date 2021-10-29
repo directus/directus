f7668bccf8b4150afb37ffe29d818fbac6ea40a3
Azri Kahar <azrikahar@outlook.com>
2021-10-29 21:55:20 +0800

    tweak fallback interface selectors

--- app/src/modules/settings/routes/data-model/field-detail/field-detail-simple/field-detail-simple.vue
+++ app/src/modules/settings/routes/data-model/field-detail/field-detail-simple/field-detail-simple.vue
@@ -236,12 +236,13 @@ export default defineComponent({
 }
 
 .preview .fallback {
-	--v-icon-color: var(--primary-50);
+	--v-icon-color: var(--primary-75);
 
 	display: block;
 	padding: 8px 16px;
+	border: 2px solid var(--primary);
 	border-radius: var(--border-radius);
-	box-shadow: 0 0 8px var(--primary-50);
+	box-shadow: 0 0 8px var(--primary-75);
 }
 
 .interface:hover .preview {
