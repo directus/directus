bc4e36c748d1aee595af5ee36d4727bad2da11a9
Ben Haynes <ben@rngr.org>
2021-10-29 10:16:40 -0400

    update field flow styling

--- app/src/modules/settings/routes/data-model/field-detail/field-detail-simple/field-detail-simple.vue
+++ app/src/modules/settings/routes/data-model/field-detail/field-detail-simple/field-detail-simple.vue
@@ -232,7 +232,7 @@ export default defineComponent({
 }
 
 .preview :deep(svg) .glow {
-	filter: drop-shadow(0 0 8px var(--primary-50));
+	filter: drop-shadow(0 0 4px var(--primary-50));
 }
 
 .preview .fallback {
@@ -240,6 +240,7 @@ export default defineComponent({
 
 	display: block;
 	padding: 8px 16px;
+	background-color: var(--background-page);
 	border: 2px solid var(--primary);
 	border-radius: var(--border-radius);
 	box-shadow: 0 0 8px var(--primary-75);
