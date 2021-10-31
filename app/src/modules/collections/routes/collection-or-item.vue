c3ec7e67d9db84525e1c2cdedff4af80624d3132
Jose Varela <joselcvarela@gmail.com>
2021-10-31 11:18:20 +0000

    add alt+n to collection creates a new item while in collection view

--- app/src/modules/collections/routes/collection-or-item.vue
+++ app/src/modules/collections/routes/collection-or-item.vue
@@ -10,9 +10,11 @@
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
+import { useRouter } from 'vue-router';
 import CollectionRoute from './collection.vue';
 import ItemRoute from './item.vue';
 import { useCollectionsStore } from '@/stores/';
+import useShortcut from '@/composables/use-shortcut';
 
 export default defineComponent({
 	components: {
@@ -35,13 +37,20 @@ export default defineComponent({
 	},
 	setup(props) {
 		const collectionsStore = useCollectionsStore();
+		const router = useRouter();
 
 		const isSingleton = computed(() => {
 			const collectionInfo = collectionsStore.getCollection(props.collection);
 			return !!collectionInfo?.meta?.singleton === true;
 		});
 
+		useShortcut('alt+n', createNew);
+
 		return { isSingleton };
+
+		function createNew() {
+			router.push(`/collections/${props.collection}/+`);
+		}
 	},
 });
 </script>
