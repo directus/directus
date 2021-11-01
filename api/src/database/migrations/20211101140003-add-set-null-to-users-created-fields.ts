158749b6c6a4f9de372fd43e1dfdc906c7bc6d19
Jay Cammarano <jay.cammarano@gmail.com>
2021-11-01 14:31:14 -0400

    fixed dropColumn()

--- api/src/database/migrations/20211101140003-add-set-null-to-users-created-fields.ts
+++ api/src/database/migrations/20211101140003-add-set-null-to-users-created-fields.ts
@@ -15,7 +15,15 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_activity', (table) => {
+		table.dropForeign('user');
+	});
 	await knex.schema.alterTable('directus_files', (table) => {
-		table.dropColumn('uploaded_by');
+		table.dropForeign('uploaded_by');
+		table.dropForeign('modified_by');
+	});
+	return await knex.schema.alterTable('directus_files', (table) => {
+		table.foreign('uploaded_by').references('directus_users.id');
+		table.foreign('modified_by').references('directus_users.id');
 	});
 }
