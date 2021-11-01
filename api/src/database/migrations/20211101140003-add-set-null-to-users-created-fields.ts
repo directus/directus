20d61f710c27178c0c945923ec00bc3d2e720605
Jay Cammarano <jay.cammarano@gmail.com>
2021-11-01 11:06:49 -0400

    SET NULL on created_by and modified_by

--- api/src/database/migrations/20211101140003-add-set-null-to-users-created-fields.ts
+++ api/src/database/migrations/20211101140003-add-set-null-to-users-created-fields.ts
@@ -1,11 +1,16 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_activity', (table) => {
+		table.foreign('user').references('directus_users.id').onDelete('SET NULL');
+	});
 	await knex.schema.alterTable('directus_files', (table) => {
 		table.dropForeign('uploaded_by');
+		table.dropForeign('modified_by');
 	});
 	return await knex.schema.alterTable('directus_files', (table) => {
 		table.foreign('uploaded_by').references('directus_users.id').onDelete('SET NULL');
+		table.foreign('modified_by').references('directus_users.id').onDelete('SET NULL');
 	});
 }
 
