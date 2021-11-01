0157d750649bfb7824679d0fe8a91eef86d9fbd3
Nitwel <nitwel@arcor.de>
2021-11-01 21:41:35 +0100

    Fix display selection in relational interfaces

--- app/src/modules/settings/routes/data-model/field-detail/store/index.ts
+++ app/src/modules/settings/routes/data-model/field-detail/store/index.ts
@@ -104,7 +104,6 @@ export const useFieldDetailStore = defineStore({
 				const relationsStore = useRelationsStore();
 
 				this.field = cloneDeep(fieldsStore.getField(collection, field)!);
-				console.log(this.field);
 				this.localType = getLocalTypeForField(collection, field)!;
 
 				const relations = relationsStore.getRelationsForField(collection, field);
