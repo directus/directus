efeaf7ee778ec3b474a1bf1b93590b0684fd77cb
rijkvanzanten <rijkvanzanten@me.com>
2021-11-01 14:49:42 -0400

    Add line breaks, cause little OCD

--- api/src/database/migrations/20211101A-add-set-null-to-users-created-fields.ts
+++ api/src/database/migrations/20211101A-add-set-null-to-users-created-fields.ts
@@ -4,10 +4,12 @@ export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_activity', (table) => {
 		table.foreign('user').references('directus_users.id').onDelete('SET NULL');
 	});
+
 	await knex.schema.alterTable('directus_files', (table) => {
 		table.dropForeign('uploaded_by');
 		table.dropForeign('modified_by');
 	});
+
 	return await knex.schema.alterTable('directus_files', (table) => {
 		table.foreign('uploaded_by').references('directus_users.id').onDelete('SET NULL');
 		table.foreign('modified_by').references('directus_users.id').onDelete('SET NULL');
@@ -18,10 +20,12 @@ export async function down(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_activity', (table) => {
 		table.dropForeign('user');
 	});
+
 	await knex.schema.alterTable('directus_files', (table) => {
 		table.dropForeign('uploaded_by');
 		table.dropForeign('modified_by');
 	});
+
 	return await knex.schema.alterTable('directus_files', (table) => {
 		table.foreign('uploaded_by').references('directus_users.id');
 		table.foreign('modified_by').references('directus_users.id');
