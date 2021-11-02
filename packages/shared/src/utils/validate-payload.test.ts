b63e62c102a5299f0ab7e599fed5351dd93a9972
Jay Cammarano <jay.cammarano@gmail.com>
2021-11-02 19:50:59 -0400

    typo

--- packages/shared/src/utils/validate-payload.test.ts
+++ packages/shared/src/utils/validate-payload.test.ts
@@ -7,7 +7,7 @@ describe('getValidationErrors', () => {
 		const mockPayload = { field: 'field' };
 		expect(validatePayload(mockFilter, mockPayload)).toBe(true);
 	});
-	it('returns an alse when there errors with an _and operator', () => {
+	it('returns an false when there errors with an _and operator', () => {
 		const mockFilter = { _and: [{ field: { _eq: 'field' } }] } as Filter;
 		const mockPayload = { field: 'test' };
 		expect(validatePayload(mockFilter, mockPayload)).toBe(false);
