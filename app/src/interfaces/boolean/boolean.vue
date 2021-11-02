ba726f5229c9e2d25b81cb3f4320a95c92b12911
Paul Boudewijn <paul@helderinternet.nl>
2021-11-02 12:55:48 +0100

    Update boolean.vue

--- app/src/interfaces/boolean/boolean.vue
+++ app/src/interfaces/boolean/boolean.vue
@@ -15,36 +15,36 @@
 </template>
 
 <script lang="ts">
-	import { defineComponent } from 'vue';
-	import { i18n } from '@/lang';
+import { defineComponent } from 'vue';
+import { i18n } from '@/lang';
 
-	export default defineComponent({
-		props: {
-			value: {
-				type: Boolean,
-				default: null,
-			},
-			disabled: {
-				type: Boolean,
-				default: false,
-			},
-			label: {
-				type: String,
-				default: () => i18n.global.t('enabled'),
-			},
-			iconOn: {
-				type: String,
-				default: 'check_box',
-			},
-			iconOff: {
-				type: String,
-				default: 'check_box_outline_blank',
-			},
-			color: {
-				type: String,
-				default: '#00C897',
-			},
+export default defineComponent({
+	props: {
+		value: {
+			type: Boolean,
+			default: null,
 		},
-		emits: ['input'],
-	});
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		label: {
+			type: String,
+			default: () => i18n.global.t('enabled'),
+		},
+		iconOn: {
+			type: String,
+			default: 'check_box',
+		},
+		iconOff: {
+			type: String,
+			default: 'check_box_outline_blank',
+		},
+		color: {
+			type: String,
+			default: '#00C897',
+		},
+	},
+	emits: ['input'],
+});
 </script>
