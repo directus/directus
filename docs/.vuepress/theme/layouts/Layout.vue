2b6c8d045e3c53117973aab32ac44c87d686be5e
Ben Haynes <ben@rngr.org>
2021-11-02 16:14:13 -0400

    moving snippet to correct element

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -21,12 +21,10 @@
 				<slot name="page-bottom" />
 			</template>
 		</Page>
+		<!-- Start of HubSpot Embed Code -->
+		<script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/20534155.js"></script>
+		<!-- End of HubSpot Embed Code -->
 	</div>
-
-	<!-- Start of HubSpot Embed Code -->
-	<script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/20534155.js"></script>
-	<!-- End of HubSpot Embed Code -->
-
 </template>
 
 <script>
