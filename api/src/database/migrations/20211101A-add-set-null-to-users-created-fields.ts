8b7d7ce80e8ad7effca690c0b5c8c337f7a6d404
Jay Cammarano <jay.cammarano@gmail.com>
2021-11-03 12:04:11 -0400

    fixed migration on mssql

--- api/src/database/migrations/20211101A-add-set-null-to-users-created-fields.ts
+++ api/src/database/migrations/20211101A-add-set-null-to-users-created-fields.ts
@@ -1,26 +1,18 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-	await knex.schema.alterTable('directus_activity', (table) => {
-		table.foreign('user').references('directus_users.id').onDelete('SET NULL');
-	});
-
 	await knex.schema.alterTable('directus_files', (table) => {
 		table.dropForeign('uploaded_by');
 		table.dropForeign('modified_by');
 	});
 
 	return await knex.schema.alterTable('directus_files', (table) => {
-		table.foreign('uploaded_by').references('directus_users.id').onDelete('SET NULL');
-		table.foreign('modified_by').references('directus_users.id').onDelete('SET NULL');
+		table.foreign('uploaded_by').references('directus_users.id').onDelete('NO ACTION').onUpdate('NO ACTION');
+		table.foreign('modified_by').references('directus_users.id').onDelete('NO ACTION').onUpdate('NO ACTION');
 	});
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.alterTable('directus_activity', (table) => {
-		table.dropForeign('user');
-	});
-
 	await knex.schema.alterTable('directus_files', (table) => {
 		table.dropForeign('uploaded_by');
 		table.dropForeign('modified_by');
