6ff630c474919b510314574fb8ef5169e56b2b06
Oreille <33065839+Oreilles@users.noreply.github.com>
2021-11-03 18:17:51 +0100

    Use primary key as default sort in map layout (#9403)

--- app/src/utils/sync-ref-property.ts
+++ app/src/utils/sync-ref-property.ts
@@ -4,13 +4,7 @@ import { computed, Ref } from 'vue';
 export function syncRefProperty<R, T extends keyof R>(ref: Ref<R>, key: T, defaultValue: R[T] | (() => R[T])) {
 	return computed<R[T]>({
 		get: () => {
-			if (ref.value?.[key]) {
-				return ref.value[key];
-			}
-
-			if (isFunction(defaultValue)) return defaultValue();
-
-			return defaultValue;
+			return ref.value?.[key] ?? (isFunction(defaultValue) ? defaultValue() : defaultValue);
 		},
 		set: (value: R[T]) => {
 			ref.value = Object.assign({}, ref.value, { [key]: value }) as R;
