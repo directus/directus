63ad920981fbc81968e6afbb25ed6a2fc350faaf
rijkvanzanten <rijkvanzanten@me.com>
2021-11-03 12:55:06 -0400

    Cleanup parse filter

--- packages/shared/src/utils/parse-filter.ts
+++ packages/shared/src/utils/parse-filter.ts
@@ -3,6 +3,7 @@ import { Accountability, Filter, User, Role } from '../types';
 import { toArray } from './to-array';
 import { adjustDate } from './adjust-date';
 import { deepMap } from './deep-map';
+import { isDynamicVariable } from '@directus/shared/utils';
 
 type ParseFilterContext = {
 	// The user can add any custom fields to user
@@ -29,24 +30,26 @@ export function parseFilter(
 			else return deepMap(toArray(val), applyFilter);
 		}
 
-		if (val && typeof val === 'string' && val.startsWith('$NOW')) {
-			if (val.includes('(') && val.includes(')')) {
-				const adjustment = val.match(REGEX_BETWEEN_PARENS)?.[1];
-				if (!adjustment) return new Date();
-				return adjustDate(new Date(), adjustment);
+		if (isDynamicVariable(val)) {
+			if (val.startsWith('$NOW')) {
+				if (val.includes('(') && val.includes(')')) {
+					const adjustment = val.match(REGEX_BETWEEN_PARENS)?.[1];
+					if (!adjustment) return new Date();
+					return adjustDate(new Date(), adjustment);
+				}
+
+				return new Date();
 			}
 
-			return new Date();
-		}
+			if (val.startsWith('$CURRENT_USER')) {
+				if (val === '$CURRENT_USER') return accountability?.user ?? null;
+				return get(context, val, null);
+			}
 
-		if (typeof val === 'string' && val.startsWith('$CURRENT_USER')) {
-			if (val === '$CURRENT_USER') return accountability?.user ?? null;
-			return get(context, val, null);
-		}
-
-		if (typeof val === 'string' && val.startsWith('$CURRENT_ROLE')) {
-			if (val === '$CURRENT_ROLE') return accountability?.role ?? null;
-			return get(context, val, null);
+			if (val.startsWith('$CURRENT_ROLE')) {
+				if (val === '$CURRENT_ROLE') return accountability?.role ?? null;
+				return get(context, val, null);
+			}
 		}
 
 		return val;
