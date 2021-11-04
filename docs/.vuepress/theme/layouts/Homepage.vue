fed2efbb925ed51d9b3ef38091bd4f135142a6c5
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2021-11-05 01:48:35 +0800

    fix docs homepage header responsiveness (#9450)

--- docs/.vuepress/theme/layouts/Homepage.vue
+++ docs/.vuepress/theme/layouts/Homepage.vue
@@ -581,8 +581,15 @@ export default {
 }
 
 @media (max-width: 700px) {
+	.header .text {
+		top: 20px;
+		right: 20px;
+		bottom: 20px;
+		left: 20px;
+	}
+
 	.header .text h1 {
-		font-size: 42px;
+		font-size: clamp(30px, 7vw, 42px);
 	}
 
 	.panel {
