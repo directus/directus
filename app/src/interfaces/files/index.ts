3cd32d533b5b0ab65f917489c2a79cb5ec80a307
Rémi Alvergnat <toilal.dev@gmail.com>
2021-11-04 18:20:55 +0100

    Fix invalid collection for Interface Display Template for M2M relationships (#9420)
    * Fix invalid collection for Interface Display Template (#9419)
    
    * use o2m ollection on m2m interface
    
    * apply same fix on files
    
    Co-authored-by: Jose Varela <joselcvarela@gmail.com>

--- app/src/interfaces/files/index.ts
+++ app/src/interfaces/files/index.ts
@@ -13,59 +13,61 @@ export default defineInterface({
 	types: ['alias'],
 	localTypes: ['files'],
 	group: 'relational',
-	options: ({ relations }: ExtensionsOptionsContext) => [
-		{
-			field: 'folder',
-			name: '$t:interfaces.system-folder.folder',
-			type: 'uuid',
-			meta: {
-				width: 'full',
-				interface: 'system-folder',
-				note: '$t:interfaces.system-folder.field_hint',
-			},
-			schema: {
-				default_value: undefined,
-			},
-		},
-		{
-			field: 'template',
-			name: '$t:display_template',
-			meta: {
-				interface: 'system-display-template',
-				options: {
-					collectionName: relations.m2o?.related_collection ?? null,
+	options: ({ relations }: ExtensionsOptionsContext) => {
+		return [
+			{
+				field: 'folder',
+				name: '$t:interfaces.system-folder.folder',
+				type: 'uuid',
+				meta: {
+					width: 'full',
+					interface: 'system-folder',
+					note: '$t:interfaces.system-folder.field_hint',
+				},
+				schema: {
+					default_value: undefined,
 				},
 			},
-		},
-		{
-			field: 'enableCreate',
-			name: '$t:creating_items',
-			schema: {
-				default_value: true,
-			},
-			meta: {
-				interface: 'boolean',
-				options: {
-					label: '$t:enable_create_button',
+			{
+				field: 'template',
+				name: '$t:display_template',
+				meta: {
+					interface: 'system-display-template',
+					options: {
+						collectionName: relations.o2m?.collection,
+					},
 				},
-				width: 'half',
 			},
-		},
-		{
-			field: 'enableSelect',
-			name: '$t:selecting_items',
-			schema: {
-				default_value: true,
-			},
-			meta: {
-				interface: 'boolean',
-				options: {
-					label: '$t:enable_select_button',
+			{
+				field: 'enableCreate',
+				name: '$t:creating_items',
+				schema: {
+					default_value: true,
 				},
-				width: 'half',
+				meta: {
+					interface: 'boolean',
+					options: {
+						label: '$t:enable_create_button',
+					},
+					width: 'half',
+				},
 			},
-		},
-	],
+			{
+				field: 'enableSelect',
+				name: '$t:selecting_items',
+				schema: {
+					default_value: true,
+				},
+				meta: {
+					interface: 'boolean',
+					options: {
+						label: '$t:enable_select_button',
+					},
+					width: 'half',
+				},
+			},
+		];
+	},
 	recommendedDisplays: ['related-values'],
 	preview: PreviewSVG,
 });
