a5e03ed5091ddd534f895e264d52251b03beabb9
Aiden Foxx <aiden.foxx.mail@gmail.com>
2021-11-09 09:44:36 +0100

    Fixed oracle special geometry migration

--- api/src/database/migrations/20211103B-update-special-geometry.ts
+++ api/src/database/migrations/20211103B-update-special-geometry.ts
@@ -1,8 +1,11 @@
 import { Knex } from 'knex';
+// @ts-ignore
+import Client_Oracledb from 'knex/lib/dialects/oracledb';
 
 export async function up(knex: Knex): Promise<void> {
+	const specialKey = knex.client instanceof Client_Oracledb ? '"special"' : 'special';
 	await knex('directus_fields')
-		.update({ special: knex.raw(`REPLACE(special, 'geometry,', 'geometry.')`) })
+		.update({ special: knex.raw(`REPLACE(${specialKey}, 'geometry,', 'geometry.')`) })
 		.where('special', 'like', '%geometry,Point%')
 		.orWhere('special', 'like', '%geometry,LineString%')
 		.orWhere('special', 'like', '%geometry,Polygon%')
@@ -12,8 +15,9 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
+	const specialKey = knex.client instanceof Client_Oracledb ? '"special"' : 'special';
 	await knex('directus_fields')
-		.update({ special: knex.raw(`REPLACE(special, 'geometry.', 'geometry,')`) })
+		.update({ special: knex.raw(`REPLACE(${specialKey}, 'geometry.', 'geometry,')`) })
 		.where('special', 'like', '%geometry.Point%')
 		.orWhere('special', 'like', '%geometry.LineString%')
 		.orWhere('special', 'like', '%geometry.Polygon%')
