cbd325de9c0f51069ce8c50e4757c897491ee4e5
Azri Kahar <azrikahar@outlook.com>
2021-11-09 15:31:28 +0800

    use import statements for monospace & serif fonts

--- app/src/interfaces/input-rich-text-html/get-editor-styles.ts
+++ app/src/interfaces/input-rich-text-html/get-editor-styles.ts
@@ -1,10 +1,27 @@
+import firaMono2 from '../../assets/fonts/FiraMono-Medium.woff2';
+import firaMono from '../../assets/fonts/FiraMono-Medium.woff';
+import merriweatherRegular2 from '../../assets/fonts/merriweather-regular.woff2';
+import merriweatherRegular from '../../assets/fonts/merriweather-regular.woff';
+
 function cssVar(name: string) {
 	return getComputedStyle(document.body).getPropertyValue(name);
 }
 
 export default function getEditorStyles(font: 'sans-serif' | 'serif' | 'monospace'): string {
 	return `
-@import url('https://fonts.googleapis.com/css2?family=Fira+Mono:wght@500&display=swap');
+@font-face {
+	font-family: 'Fira Mono';
+	font-style: normal;
+	src: url(${firaMono2}) format('woff2'),
+	url(${firaMono}) format('woff');
+}
+
+@font-face {
+	font-family: 'Merriweather';
+	font-style: normal;
+	src: url(${merriweatherRegular2}) format('woff2'),
+	url(${merriweatherRegular}) format('woff');
+}
 
 body {
 	color: ${cssVar('--foreground-normal')};
