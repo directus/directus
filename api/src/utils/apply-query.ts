4b277f87298ac1bdab1a98e5d9619a2ea0e56c45
Oreille <33065839+Oreilles@users.noreply.github.com>
2021-11-10 09:47:59 +0100

    Use CHAR(255) instead of CHAR

--- api/src/utils/apply-query.ts
+++ api/src/utils/apply-query.ts
@@ -221,7 +221,7 @@ export function applyFilter(
 							.on(
 								`${parentAlias || parentCollection}.${relation.field}`,
 								'=',
-								knex.raw(`CAST(?? AS CHAR)`, `${alias}.${schema.collections[pathScope].primary}`)
+								knex.raw(`CAST(?? AS CHAR(255))`, `${alias}.${schema.collections[pathScope].primary}`)
 							)
 							.andOnVal(relation.meta!.one_collection_field!, '=', pathScope);
 					});
