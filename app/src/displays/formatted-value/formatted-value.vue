170df242fb4ac1de08261321cbd12fb95870acb6
Alejandro Castaño González <alejandro@theseedsquad.com>
2021-11-13 09:11:50 +0100

    Update formatted-value display

--- app/src/displays/formatted-value/formatted-value.vue
+++ app/src/displays/formatted-value/formatted-value.vue
@@ -1,13 +1,40 @@
 <template>
 	<value-null v-if="displayValue === null || displayValue === undefined" />
 
-	<span v-else class="display-formatted-text" :class="[{ bold }, font]" :style="{ color }">
-		{{ displayValue }}
-	</span>
+	<div v-else class="display-formatted" :style="displayStyle">
+		<v-icon v-if="format.iconLeft" class="left" :name="format.iconLeft" :color="format.iconLeftColor" small />
+
+		<a
+			v-if="link"
+			class="link"
+			:href="href"
+			:class="[{ bold }, font]"
+			:style="valueStyle"
+			:target="linkTarget"
+			@click.stop
+		>
+			{{ displayValue }}
+		</a>
+
+		<span v-else class="value" :class="[{ bold }, font]" :style="valueStyle">
+			{{ displayValue }}
+		</span>
+
+		<v-icon v-if="format.iconRight" class="right" :name="format.iconRight" :color="format.iconRightColor" small />
+
+		<v-icon
+			v-if="clipboard"
+			v-tooltip="t('displays.formatted-value.clipboard_tooltip')"
+			class="button"
+			name="copy"
+			small
+			@click.stop="copyValue"
+		/>
+	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { defineComponent, computed, PropType } from 'vue';
 import formatTitle from '@directus/format-title';
 import { decode } from 'html-entities';
 import { useI18n } from 'vue-i18n';
@@ -19,35 +46,166 @@ export default defineComponent({
 			type: [String, Number],
 			default: null,
 		},
-		formatTitle: {
-			type: Boolean,
-			default: false,
+		textAlign: {
+			type: String,
+			default: 'left',
+			validator: (value: string) => ['left', 'center', 'right'].includes(value),
 		},
 		bold: {
 			type: Boolean,
 			default: false,
 		},
+		font: {
+			type: String,
+			default: 'sans-serif',
+			validator: (value: string) => ['sans-serif', 'serif', 'monospace'].includes(value),
+		},
 		color: {
 			type: String,
 			default: null,
 		},
-		font: {
+		backgroundColor: {
 			type: String,
-			default: 'sans-serif',
-			validator: (val: string) => ['sans-serif', 'serif', 'monospace'].includes(val),
+			default: null,
+		},
+		iconLeft: {
+			type: String,
+			default: null,
+		},
+		iconLeftColor: {
+			type: String,
+			default: null,
+		},
+		iconRight: {
+			type: String,
+			default: null,
+		},
+		iconRightColor: {
+			type: String,
+			default: null,
+		},
+		prefix: {
+			type: String,
+			default: null,
+		},
+		suffix: {
+			type: String,
+			default: null,
+		},
+		formatRules: {
+			type: Array as PropType<Record<string, any>[]>,
+			default: null,
+		},
+		formatTitle: {
+			type: Boolean,
+			default: false,
+		},
+		link: {
+			type: String,
+			default: '',
+		},
+		linkText: {
+			type: String,
+			default: '',
+		},
+		linkTarget: {
+			type: String,
+			default: '',
+		},
+		linkEmailSubject: {
+			type: String,
+			default: '',
+		},
+		linkEmailBody: {
+			type: String,
+			default: '',
+		},
+		linkEmailCC: {
+			type: String,
+			default: '',
+		},
+		linkEmailBCC: {
+			type: String,
+			default: '',
+		},
+		linkWhatsappText: {
+			type: String,
+			default: '',
+		},
+		clipboard: {
+			type: Boolean,
+			default: false,
 		},
 	},
 	setup(props) {
-		const { n } = useI18n();
+		const { t, n } = useI18n();
+
+		const matchedRules = computed(() => {
+			const leftValue = parseFloat(props.value.toString());
+			return (props.formatRules || []).filter((rule) => {
+				const rightValue = parseFloat(rule.value);
+				switch (rule.operator) {
+					case 'lt':
+						return leftValue < rightValue;
+					case 'lte':
+						return leftValue <= rightValue;
+					case 'gt':
+						return leftValue > rightValue;
+					case 'gte':
+						return leftValue >= rightValue;
+					case 'eq':
+						return leftValue == rightValue;
+					case 'neq':
+						return leftValue != rightValue;
+				}
+				return false;
+			});
+		});
+
+		const format = computed(() => {
+			const { color, backgroundColor, iconLeft, iconLeftColor, iconRight, iconRightColor } = props;
+			return matchedRules.value.reduce((format, rule) => Object.assign({}, format, rule), {
+				color,
+				backgroundColor,
+				iconLeft,
+				iconLeftColor,
+				iconRight,
+				iconRightColor,
+			});
+		});
+
+		const displayStyle = computed(() => {
+			const justifyContent = { left: 'flex-start', center: 'center', right: 'flex-end' }[props.textAlign];
+			const alignItems = 'center';
+			return { justifyContent, alignItems };
+		});
+
+		const valueStyle = computed(() => {
+			return { color: format.value.color };
+		});
 
 		const displayValue = computed(() => {
 			if (isNil(props.value) || props.value === '') return null;
 
 			if (typeof props.value === 'number') {
-				return n(props.value);
+				let value = n(props.value);
+
+				if (props.prefix) {
+					value = `${props.prefix}${value}`;
+				}
+
+				if (props.suffix) {
+					value = `${value}${props.suffix}`;
+				}
+
+				return value;
 			}
 
 			let value = String(props.value);
+
+			if (props.link) {
+				return props.linkText || value;
+			}
 
 			// Strip out all HTML tags
 			value = value.replace(/(<([^>]+)>)/gi, '');
@@ -62,36 +220,92 @@ export default defineComponent({
 			return value;
 		});
 
-		return { displayValue };
+		const href = computed(() => {
+			if (props.link === 'url') {
+				return props.value;
+			} else if (props.link === 'email') {
+				const params = Object.entries({
+					subject: props.linkEmailSubject,
+					body: props.linkEmailBody,
+					cc: props.linkEmailCC,
+					bcc: props.linkEmailBCC,
+				})
+					.filter(([_, value]) => value)
+					.map(([param, value]) => `${param}=${encodeURIComponent(value)}`);
+
+				return `mailto:${props.value}?${params.join('&')}`;
+			} else if (props.link === 'tel') {
+				return `tel:${props.value}`;
+			} else if (props.link === 'whatsapp') {
+				return `https://wa.me/${props.value}?text=${encodeURIComponent(props.linkWhatsappText)}`;
+			} else {
+				return '#';
+			}
+		});
+
+		return { t, format, displayValue, href, displayStyle, valueStyle, copyValue };
+
+		function copyValue() {
+			navigator.clipboard.writeText(props.value.toString());
+		}
 	},
 });
 </script>
 
 <style lang="scss" scoped>
-.display-formatted-text {
+.display-formatted {
+	display: flex;
+	flex-grow: 1;
 	overflow: hidden;
-	line-height: 26px;
+	line-height: 1;
 	white-space: nowrap;
 	text-overflow: ellipsis;
 
-	&.bold {
-		font-weight: 700;
+	& .value,
+	& .link {
+		&.bold {
+			font-weight: 700;
+		}
+
+		&.sans-serif {
+			font-family: var(--family-sans-serif);
+		}
+
+		&.serif {
+			font-family: var(--family-serif);
+		}
+
+		&.monospace {
+			font-family: var(--family-monospace);
+		}
 	}
 
-	&.subdued {
-		color: var(--foreground-subdued);
+	.link {
+		color: var(--primary-110);
+		font-weight: 500;
+		text-decoration: none;
 	}
 
-	&.sans-serif {
-		font-family: var(--family-sans-serif);
-	}
+	.v-icon {
+		&.left {
+			flex-shrink: 0;
+			margin-right: 2px;
+		}
 
-	&.serif {
-		font-family: var(--family-serif);
-	}
+		&.right {
+			flex-shrink: 0;
+			margin-left: 2px;
+		}
 
-	&.monospace {
-		font-family: var(--family-monospace);
+		&.button {
+			margin-left: 4px;
+			color: var(--foreground-subdued);
+			transition: color var(--fast) var(--transition);
+
+			&:hover {
+				color: var(--green-75);
+			}
+		}
 	}
 }
 </style>
--- app/src/displays/formatted-value/index.ts
+++ app/src/displays/formatted-value/index.ts
@@ -8,62 +8,416 @@ export default defineDisplay({
 	types: ['string', 'text', 'integer', 'float', 'decimal', 'bigInteger'],
 	icon: 'text_format',
 	component: DisplayFormattedValue,
-	options: [
-		{
-			field: 'formatTitle',
-			name: '$t:displays.formatted-value.format_title',
-			type: 'boolean',
-			meta: {
-				width: 'half',
-				interface: 'boolean',
-				options: {
-					label: '$t:displays.formatted-value.format_title_label',
+	options: ({ field }) => {
+		const options = [
+			{
+				field: 'clipboard',
+				name: '$t:displays.formatted-value.clipboard',
+				type: 'boolean',
+				meta: {
+					width: 'half',
+					interface: 'boolean',
+					options: {
+						label: '$t:displays.formatted-value.clipboard_label',
+					},
+				},
+				schema: {
+					default_value: false,
 				},
 			},
-			schema: {
-				default_value: false,
-			},
-		},
-		{
-			field: 'bold',
-			name: '$t:bold',
-			type: 'boolean',
-			meta: {
-				width: 'half',
-				interface: 'boolean',
-				options: {
-					label: '$t:displays.formatted-value.bold_label',
+			{
+				field: 'bold',
+				name: '$t:displays.formatted-value.bold',
+				type: 'boolean',
+				meta: {
+					width: 'half',
+					interface: 'boolean',
+					options: {
+						label: '$t:displays.formatted-value.bold_label',
+					},
+				},
+				schema: {
+					default_value: false,
 				},
 			},
-			schema: {
-				default_value: false,
-			},
-		},
-		{
-			field: 'color',
-			name: '$t:color',
-			meta: {
-				width: 'half',
-				interface: 'select-color',
-			},
-		},
-		{
-			field: 'font',
-			name: '$t:font',
-			meta: {
-				width: 'half',
-				interface: 'select-dropdown',
-				options: {
-					choices: [
-						{ text: '$t:sans_serif', value: 'sans-serif' },
-						{ text: '$t:serif', value: 'serif' },
-						{ text: '$t:monospace', value: 'monospace' },
-					],
+			{
+				field: 'font',
+				name: '$t:displays.formatted-value.font',
+				meta: {
+					width: 'half',
+					interface: 'select-dropdown',
+					options: {
+						choices: [
+							{ text: '$t:displays.formatted-value.font_sans_serif', value: 'sans-serif' },
+							{ text: '$t:displays.formatted-value.font_serif', value: 'serif' },
+							{ text: '$t:displays.formatted-value.font_monospace', value: 'monospace' },
+						],
+					},
+				},
+				schema: {
+					default_value: 'sans-serif',
 				},
 			},
-			schema: {
-				default_value: 'sans-serif',
+			{
+				field: 'textAlign',
+				name: '$t:displays.formatted-value.text_align',
+				type: 'string',
+				meta: {
+					interface: 'select-dropdown',
+					width: 'half',
+					options: {
+						choices: [
+							{ text: '$t:displays.formatted-value.text_align_left', value: 'left' },
+							{ text: '$t:displays.formatted-value.text_align_center', value: 'center' },
+							{ text: '$t:displays.formatted-value.text_align_right', value: 'right' },
+						],
+						allowOther: false,
+					},
+				},
+				schema: {
+					default_value: 'left',
+				},
 			},
-		},
-	],
+			{
+				field: 'color',
+				name: '$t:displays.formatted-value.color',
+				type: 'string',
+				meta: {
+					interface: 'select-color',
+					width: 'half',
+				},
+			},
+			{
+				field: 'backgroundColor',
+				name: '$t:displays.formatted-value.background_color',
+				type: 'string',
+				meta: {
+					interface: 'select-color',
+					width: 'half',
+				},
+			},
+			{
+				field: 'iconLeft',
+				name: '$t:displays.formatted-value.icon_left',
+				type: 'string',
+				meta: {
+					width: 'half',
+					interface: 'select-icon',
+				},
+			},
+			{
+				field: 'iconRight',
+				name: '$t:displays.formatted-value.icon_right',
+				type: 'string',
+				meta: {
+					width: 'half',
+					interface: 'select-icon',
+				},
+			},
+			{
+				field: 'iconLeftColor',
+				name: '$t:displays.formatted-value.icon_left_color',
+				meta: {
+					width: 'half',
+					interface: 'select-color',
+				},
+			},
+			{
+				field: 'iconRightColor',
+				name: '$t:displays.formatted-value.icon_right_color',
+				meta: {
+					width: 'half',
+					interface: 'select-color',
+				},
+			},
+		];
+
+		if (['bigInteger', 'integer', 'float', 'decimal'].includes(field.type)) {
+			options.push({
+				field: 'prefix',
+				name: '$t:displays.formatted-value.prefix',
+				type: 'string',
+				meta: {
+					width: 'half',
+					interface: 'input',
+					options: {
+						label: '$t:displays.formatted-value.prefix_label',
+						trim: false,
+					},
+				},
+			});
+
+			options.push({
+				field: 'suffix',
+				name: '$t:displays.formatted-value.suffix',
+				type: 'string',
+				meta: {
+					width: 'half',
+					interface: 'input',
+					options: {
+						label: '$t:displays.formatted-value.suffix_label',
+						trim: false,
+					},
+				},
+			});
+
+			options.push({
+				field: 'formatRules',
+				name: '$t:displays.formatted-value.format_rules',
+				type: 'json',
+				meta: {
+					interface: 'list',
+					options: {
+						template: '{{operator}}',
+						fields: [
+							{
+								field: 'operator',
+								name: '$t:displays.formatted-value.format_rules_operator',
+								meta: {
+									width: 'half',
+									interface: 'select-dropdown',
+									options: {
+										choices: [
+											{ text: '$t:operators.lt', value: 'lt' },
+											{ text: '$t:operators.lte', value: 'lte' },
+											{ text: '$t:operators.gt', value: 'gt' },
+											{ text: '$t:operators.gte', value: 'gte' },
+											{ text: '$t:operators.eq', value: 'eq' },
+											{ text: '$t:operators.neq', value: 'neq' },
+										],
+									},
+								},
+								schema: {
+									default_value: 'lt',
+								},
+							},
+							{
+								field: 'value',
+								name: '$t:value',
+								type: 'number',
+								meta: {
+									interface: 'input',
+									options: {
+										step: 'any',
+									},
+									width: 'half',
+								},
+							},
+							{
+								field: 'color',
+								name: '$t:displays.formatted-value.color',
+								type: 'string',
+								meta: {
+									interface: 'select-color',
+									width: 'half',
+								},
+							},
+							{
+								field: 'backgroundColor',
+								name: '$t:displays.formatted-value.background_color',
+								type: 'string',
+								meta: {
+									interface: 'select-color',
+									width: 'half',
+								},
+							},
+							{
+								field: 'iconLeft',
+								name: '$t:displays.formatted-value.icon_left',
+								type: 'string',
+								meta: {
+									width: 'half',
+									interface: 'select-icon',
+								},
+							},
+							{
+								field: 'iconRight',
+								name: '$t:displays.formatted-value.icon_right',
+								type: 'string',
+								meta: {
+									width: 'half',
+									interface: 'select-icon',
+								},
+							},
+							{
+								field: 'iconLeftColor',
+								name: '$t:displays.formatted-value.icon_left_color',
+								meta: {
+									width: 'half',
+									interface: 'select-color',
+								},
+							},
+							{
+								field: 'iconRightColor',
+								name: '$t:displays.formatted-value.icon_right_color',
+								meta: {
+									width: 'half',
+									interface: 'select-color',
+								},
+							},
+						],
+					},
+				},
+			});
+		}
+
+		if (['string', 'text'].includes(field.type)) {
+			options.push({
+				field: 'formatTitle',
+				name: '$t:displays.formatted-value.format_title',
+				type: 'boolean',
+				meta: {
+					width: 'half',
+					interface: 'boolean',
+					options: {
+						label: '$t:displays.formatted-value.format_title_label',
+					},
+				},
+				schema: {
+					default_value: false,
+				},
+			});
+
+			options.push({
+				field: 'link',
+				name: '$t:displays.formatted-value.link',
+				type: 'string',
+				meta: {
+					interface: 'select-dropdown',
+					width: 'half',
+					options: {
+						choices: [
+							{ text: '$t:displays.formatted-value.no_link', value: '' },
+							{ text: '$t:displays.formatted-value.link_url', value: 'url' },
+							{ text: '$t:displays.formatted-value.link_email', value: 'email' },
+							{ text: '$t:displays.formatted-value.link_tel', value: 'tel' },
+							{ text: '$t:displays.formatted-value.link_whatsapp', value: 'whatsapp' },
+						],
+						allowOther: false,
+					},
+				},
+				schema: {
+					default_value: '',
+				},
+			});
+
+			const display_options = field.meta.display_options || {};
+
+			if (display_options.link) {
+				options.push({
+					field: 'linkText',
+					name: '$t:displays.formatted-value.link_text',
+					type: 'string',
+					meta: {
+						width: 'half',
+						interface: 'input',
+						options: {
+							label: '$t:displays.formatted-value.link_text_label',
+							trim: true,
+						},
+					},
+				});
+			}
+
+			if (display_options.link === 'url') {
+				options.push({
+					field: 'linkTarget',
+					name: '$t:displays.formatted-value.link_target',
+					type: 'string',
+					meta: {
+						interface: 'select-dropdown',
+						width: 'half',
+						options: {
+							choices: [
+								{ text: '$t:displays.formatted-value.link_target_blank', value: '_blank' },
+								{ text: '$t:displays.formatted-value.link_target_self', value: '_self' },
+								{ text: '$t:displays.formatted-value.link_target_parent', value: '_parent' },
+								{ text: '$t:displays.formatted-value.link_target_top', value: '_top' },
+							],
+							allowOther: false,
+						},
+					},
+					schema: {
+						default_value: '_blank',
+					},
+				});
+			}
+
+			if (display_options.link === 'email') {
+				options.push({
+					field: 'linkEmailSubject',
+					name: '$t:displays.formatted-value.link_email_subject',
+					type: 'string',
+					meta: {
+						width: 'full',
+						interface: 'input',
+						options: {
+							label: '$t:displays.formatted-value.link_email_subject_label',
+							trim: true,
+						},
+					},
+				});
+
+				options.push({
+					field: 'linkEmailBody',
+					name: '$t:displays.formatted-value.link_email_body',
+					type: 'string',
+					meta: {
+						width: 'full',
+						interface: 'input-multiline',
+						options: {
+							label: '$t:displays.formatted-value.link_email_body_label',
+							trim: true,
+						},
+					},
+				});
+
+				options.push({
+					field: 'linkEmailCC',
+					name: '$t:displays.formatted-value.link_email_cc',
+					type: 'string',
+					meta: {
+						width: 'full',
+						interface: 'input',
+						options: {
+							label: '$t:displays.formatted-value.link_email_cc_label',
+							trim: true,
+						},
+					},
+				});
+
+				options.push({
+					field: 'linkEmailBCC',
+					name: '$t:displays.formatted-value.link_email_bcc',
+					type: 'string',
+					meta: {
+						width: 'full',
+						interface: 'input',
+						options: {
+							label: '$t:displays.formatted-value.link_email_bcc_label',
+							trim: true,
+						},
+					},
+				});
+			}
+
+			if (display_options.link === 'whatsapp') {
+				options.push({
+					field: 'linkWhatsappText',
+					name: '$t:displays.formatted-value.link_whatsapp_text',
+					type: 'string',
+					meta: {
+						width: 'full',
+						interface: 'input-multiline',
+						options: {
+							label: '$t:displays.formatted-value.link_whatsapp_text_label',
+							trim: true,
+						},
+					},
+				});
+			}
+		}
+
+		return options;
+	},
 });
