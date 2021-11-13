2414683d0751bc870361790ac540e20b613353af
Mattia Dalzocchio <mattiadalzocchio@me.com>
2021-11-13 08:47:47 +0100

    Add render-template-list component for array of templates.

---
+++ app/src/views/private/components/render-template-list/index.ts
@@ -0,0 +1,4 @@
+import RenderTemplateList from './render-template-list.vue';
+
+export { RenderTemplateList };
+export default RenderTemplateList;
---
+++ app/src/views/private/components/render-template-list/render-template-list.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="render-template-list">
+		<template v-for="(item, index) in items" :key="index">
+			<render-template :template="template" :item="item" :fields="fields" :collection="collection" />
+			<span v-if="index < items.length - 1" class="separator">{{ separator }}</span>
+		</template>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { Field } from '@directus/shared/types';
+
+export default defineComponent({
+	props: {
+		collection: {
+			type: String,
+			default: null,
+		},
+		fields: {
+			type: Array as PropType<Field[]>,
+			default: null,
+		},
+		items: {
+			type: Array as PropType<Array<Record<string, any>>>,
+			default: null,
+		},
+		template: {
+			type: String,
+			required: true,
+		},
+		separator: {
+			type: String,
+			required: false,
+			default: ',',
+		},
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+.render-template-list {
+	display: flex;
+	flex-wrap: wrap;
+
+	> * {
+		margin: 0;
+		padding: 0;
+	}
+}
+
+.separator {
+	padding-right: 4px;
+	padding-left: 4px;
+}
+</style>
