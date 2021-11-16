51d69f7da6abe0a31ba926087de68378443bcd30
ian <licitdev@gmail.com>
2021-11-16 11:52:27 +0800

    Allow null result from shared parseFilter

--- app/src/utils/parse-filter.ts
+++ app/src/utils/parse-filter.ts
@@ -12,5 +12,5 @@ export function parseFilter(filter: Record<string, any>): Record<string, any> {
 		user: userStore.currentUser.id,
 	};
 
-	return parseFilterShared(filter, accountability);
+	return parseFilterShared(filter, accountability) ?? {};
 }
