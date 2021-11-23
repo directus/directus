9798dd2abf35fc55033fff446a9ca7abe4ea480e
Jose Varela <joselcvarela@gmail.com>
2021-11-23 16:09:02 +0000

    listen contextmenu even if no menu attached In some cases we have an area where would like to not open the browser context menu. For example, an area where the context menu does not have any options because of conditions like permissions

--- app/src/directives/context-menu/context-menu.ts
+++ app/src/directives/context-menu/context-menu.ts
@@ -2,21 +2,16 @@ import { Directive, DirectiveBinding } from 'vue';
 
 function mounted(element: HTMLElement, binding: DirectiveBinding): void {
 	const contextMenu = binding.instance?.$refs[binding.value];
-	if (contextMenu) {
-		element.addEventListener('contextmenu', activateContextMenu(contextMenu));
-		element.addEventListener('focusout', deactivateContextMenu(contextMenu));
-	} else {
-		// eslint-disable-next-line no-console
-		console.error(`[v-context-menu] Couldn't find a context menu by the ref "${binding.value}".`);
-	}
+
+	element.addEventListener('contextmenu', activateContextMenu(contextMenu));
+	element.addEventListener('focusout', deactivateContextMenu(contextMenu));
 }
 
 function unmounted(element: HTMLElement, binding: DirectiveBinding): void {
 	const contextMenu = binding.instance?.$refs[binding.value];
-	if (contextMenu) {
-		element.removeEventListener('contextmenu', activateContextMenu(contextMenu));
-		element.removeEventListener('focusout', deactivateContextMenu(contextMenu));
-	}
+
+	element.removeEventListener('contextmenu', activateContextMenu(contextMenu));
+	element.removeEventListener('focusout', deactivateContextMenu(contextMenu));
 }
 
 const ContextMenu: Directive = {
@@ -30,12 +25,13 @@ function activateContextMenu(contextMenu: any) {
 	return (e: Event) => {
 		e.stopPropagation();
 		e.preventDefault();
-		contextMenu.activate(e);
+
+		if (contextMenu) contextMenu.activate(e);
 	};
 }
 
 function deactivateContextMenu(contextMenu: any) {
 	return (e: Event) => {
-		contextMenu?.deactivate(e);
+		if (contextMenu) contextMenu.deactivate(e);
 	};
 }
