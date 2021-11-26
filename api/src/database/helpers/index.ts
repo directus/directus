c20f40c6f36180a53dfaa6f6a5af37660988b729
Oreilles <oreilles.github@nitoref.io>
2021-11-26 22:02:39 +0100

    Refactor index.ts

--- api/src/database/helpers/index.ts
+++ api/src/database/helpers/index.ts
@@ -1,21 +1,15 @@
 import { getDatabaseClient } from '..';
 import { Knex } from 'knex';
 
-import { GeometryHelper } from './geometry/types';
-import { DateHelper } from './date/types';
-
 import * as dateHelpers from './date';
 import * as geometryHelpers from './geometry';
 
-export type Helpers = {
-	date: DateHelper;
-	st: GeometryHelper;
-};
-
-export function getHelpers(database: Knex): Helpers {
+export function getHelpers(database: Knex) {
 	const client = getDatabaseClient(database);
 	return {
 		date: new dateHelpers[client](database),
 		st: new geometryHelpers[client](database),
 	};
 }
+
+export type Helpers = ReturnType<typeof getHelpers>;
