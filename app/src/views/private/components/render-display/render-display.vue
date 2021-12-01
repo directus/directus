9c9e2eff5b0087aeb33bfdd7110110981c655062
Oreille <33065839+Oreilles@users.noreply.github.com>
2021-12-01 22:16:37 +0100

    Fix ability to change M2O field type and prevent incompatible interfaces in advanced mode (#10096)
    * Fix ability to change M2O field type and prevent incompatible interfaces in advanced mode
    
    * Removed unused imports
    
    * Rename interface variable

--- app/src/views/private/components/render-display/render-display.vue
+++ app/src/views/private/components/render-display/render-display.vue
@@ -16,9 +16,8 @@
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
-import { getDisplays } from '@/displays';
+import { getDisplay } from '@/displays';
 import ValueNull from '@/views/private/components/value-null';
-import { DisplayConfig } from '@directus/shared/types';
 
 export default defineComponent({
 	components: { ValueNull },
@@ -57,10 +56,7 @@ export default defineComponent({
 		},
 	},
 	setup(props) {
-		const { displays } = getDisplays();
-		const displayInfo = computed(
-			() => displays.value.find((display: DisplayConfig) => display.id === props.display) || null
-		);
+		const displayInfo = computed(() => getDisplay(props.display));
 		return { displayInfo };
 	},
 });
