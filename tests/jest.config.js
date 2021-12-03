2e95a164b7e69b552aad72e13a268800357f6af4
Jay Cammarano <jay.cammarano@gmail.com>
2021-12-03 13:23:01 -0500

    e2e jest.config moved

--- tests/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	preset: 'ts-jest',
-	verbose: true,
-	globalSetup: './e2e/setup/setup.ts',
-	globalTeardown: './e2e/setup/teardown.ts',
-	modulePathIgnorePatterns: ['e2e/setup/utils'],
-};
