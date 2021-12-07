962aef1432b2a70bb215588167da13c3563edb92
Jay Cammarano <jay.cammarano@gmail.com>
2021-12-07 15:48:15 -0500

    add nonadmin role and user seed+migration

--- tests/e2e/setup/seeds/04_directus_users.js
+++ tests/e2e/setup/seeds/04_directus_users.js
@@ -8,5 +8,13 @@ exports.seed = async function (knex) {
 			role: '5b935e65-d3db-4457-96f1-597e2fcfc7f3',
 			token: 'AdminToken',
 		},
+		{
+			id: 'a8057636-9b70-4804-bfec-19c88d1a3273',
+			email: 'test@user.com',
+			password: 'TestUserPassword',
+			status: 'active',
+			role: '214faee7-d6a6-4a4c-b1cd-f9e9bd0b6fb7',
+			token: 'UserToken',
+		},
 	]);
 };
