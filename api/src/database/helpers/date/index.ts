3cd0a8c3dd8a8329c828ef24359bf60fbe7b0fcf
Michael Schramm <michael.schramm@gmail.com>
2021-12-08 00:02:17 +0100

    rebase

--- api/src/database/helpers/date/index.ts
+++ api/src/database/helpers/date/index.ts
@@ -1,5 +1,6 @@
 export { DateHelperPostgres as postgres } from './dialects/postgres';
 export { DateHelperPostgres as redshift } from './dialects/postgres';
+export { DateHelperPostgres as cockroachdb } from './dialects/postgres';
 export { DateHelperOracle as oracle } from './dialects/oracle';
 export { DateHelperSQLite as sqlite } from './dialects/sqlite';
 export { DateHelperMySQL as mysql } from './dialects/mysql';
