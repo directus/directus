d71f90f48becab921b4c813d3b6535f988b7f5ac
Azri Kahar <azrikahar@outlook.com>
2021-12-08 18:38:04 +0800

    add custom panel extensions template & docs

---
+++ packages/extensions-sdk/templates/panel/typescript/src/index.ts
@@ -0,0 +1,23 @@
+import { definePanel } from '@directus/extensions-sdk';
+import PanelComponent from './panel.vue';
+
+export default definePanel({
+	id: 'custom',
+	name: 'Custom',
+	icon: 'box',
+	description: 'This is my custom panel!',
+	component: PanelComponent,
+	options: [
+		{
+			field: 'text',
+			name: 'Text',
+			type: 'string',
+			meta: {
+				interface: 'input',
+				width: 'full',
+			},
+		},
+	],
+	minWidth: 12,
+	minHeight: 8,
+});
