d69742cbb51f4648225b5211e9ad3ef6893134c9
eikaramba <eikaramba@users.noreply.github.com>
2021-12-17 01:53:06 +0100

    make linter happy
    Co-authored-by: Rijk van Zanten <rijkvanzanten@me.com>

--- api/src/middleware/respond.ts
+++ api/src/middleware/respond.ts
@@ -80,7 +80,7 @@ export const respond: RequestHandler = asyncHandler(async (req, res) => {
 
 	if (Buffer.isBuffer(res.locals.payload)) {
 		return res.end(res.locals.payload);
-	}else if(res.locals.payload){
+	} else if (res.locals.payload) {
 		return res.json(res.locals.payload);
 	} else {
 		return res.status(204).end();
