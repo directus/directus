ac16c367ed90872b59abbcd759c3d727b871c5d6
ian <licitdev@gmail.com>
2021-12-22 16:13:05 +0800

    Catch invalid URLs when replacing token

--- app/src/utils/replace-url-access-token.ts
+++ app/src/utils/replace-url-access-token.ts
@@ -1,14 +1,18 @@
 export function replaceUrlAccessToken(url: string, token: string | null | undefined): string {
-	const parsedUrl = new URL(url);
-	const params = new URLSearchParams(parsedUrl.search);
+	try {
+		const parsedUrl = new URL(url);
+		const params = new URLSearchParams(parsedUrl.search);
 
-	if (!token) {
-		params.delete('access_token');
-	} else {
-		params.set('access_token', token);
+		if (!token) {
+			params.delete('access_token');
+		} else {
+			params.set('access_token', token);
+		}
+
+		return Array.from(params).length > 0
+			? `${parsedUrl.origin}${parsedUrl.pathname}?${params.toString()}`
+			: `${parsedUrl.origin}${parsedUrl.pathname}`;
+	} catch {
+		return url;
 	}
-
-	return Array.from(params).length > 0
-		? `${parsedUrl.origin}${parsedUrl.pathname}?${params.toString()}`
-		: `${parsedUrl.origin}${parsedUrl.pathname}`;
 }
