a83d2adc36ce59652f43b62db63ebe7ab843c0c1
rijkvanzanten <rijkvanzanten@me.com>
2021-12-30 11:12:30 -0500

    Add comment on ipv4 v ipv6

--- api/src/utils/get-ip-from-req.ts
+++ api/src/utils/get-ip-from-req.ts
@@ -16,5 +16,6 @@ export function getIPFromReq(req: Request): string {
 		}
 	}
 
+	// IP addresses starting with ::ffff: are IPv4 addresses in IPv6 format. We can strip the prefix to get back to IPv4
 	return ip.startsWith('::ffff:') ? ip.substring(7) : ip;
 }
