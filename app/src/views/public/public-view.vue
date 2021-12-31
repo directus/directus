cbe139d7e9602fb5abf7717f17544595b859f36c
Ben Haynes <ben@rngr.org>
2021-12-30 19:40:38 -0500

    style tweaks (#10760)
    * style tweaks
    
    * fix linting
    
    * Make project descriptor configurable
    
    * Add missing import
    
    Co-authored-by: Azri Kahar <42867097+azrikahar@users.noreply.github.com>
    Co-authored-by: rijkvanzanten <rijkvanzanten@me.com>

--- app/src/views/public/public-view.vue
+++ app/src/views/public/public-view.vue
@@ -2,15 +2,15 @@
 	<div class="public-view" :class="{ branded: isBranded }">
 		<div class="container" :class="{ wide }">
 			<div class="title-box">
-				<div v-if="branding && branding.project_logo" class="logo" :style="{ backgroundColor: branding.project_color }">
+				<div v-if="branding?.project_logo" class="logo" :style="{ backgroundColor: branding.project_color }">
 					<img :src="logoURL" :alt="branding.project_name || 'Logo'" />
 				</div>
 				<div v-else class="logo" :style="{ backgroundColor: branding.project_color }">
 					<img src="./logo-light.svg" alt="Directus" class="directus-logo" />
 				</div>
 				<div class="title">
-					<h1 class="type-title">{{ branding && branding.project_name }}</h1>
-					<p class="subtitle">Admin App</p>
+					<h1 class="type-title">{{ branding?.project_name }}</h1>
+					<p class="subtitle">{{ branding?.project_descriptor ?? t('application') }}</p>
 				</div>
 			</div>
 
@@ -23,10 +23,10 @@
 		</div>
 		<div class="art" :style="artStyles">
 			<transition name="scale">
-				<img v-if="foregroundURL" class="foreground" :src="foregroundURL" :alt="branding && branding.project_name" />
+				<img v-if="foregroundURL" class="foreground" :src="foregroundURL" :alt="branding?.project_name" />
 			</transition>
 			<div class="note-container">
-				<div v-if="branding && branding.public_note" v-md="branding.public_note" class="note" />
+				<div v-if="branding?.public_note" v-md="branding.public_note" class="note" />
 			</div>
 		</div>
 	</div>
@@ -37,6 +37,7 @@ import { version } from '../../../package.json';
 import { defineComponent, computed } from 'vue';
 import { useServerStore } from '@/stores';
 import { getRootPath } from '@/utils/get-root-path';
+import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
 	props: {
@@ -46,6 +47,7 @@ export default defineComponent({
 		},
 	},
 	setup() {
+		const { t } = useI18n();
 		const serverStore = useServerStore();
 
 		const isBranded = computed(() => {
@@ -86,6 +88,7 @@ export default defineComponent({
 			foregroundURL,
 			logoURL,
 			isBranded,
+			t,
 		};
 	},
 });
@@ -109,6 +112,7 @@ export default defineComponent({
 		--input-height: 60px;
 		--input-padding: 16px; // (60 - 4 - 24) / 2
 
+		z-index: 2;
 		display: flex;
 		flex-direction: column;
 		justify-content: space-between;
@@ -123,7 +127,7 @@ export default defineComponent({
 		font-size: 15px;
 		line-height: 24px;
 		background-color: #fff;
-		box-shadow: 0 0 40px 0 rgb(0 0 0 / 0.25);
+		box-shadow: 0 0 40px 0 rgb(38 50 56 / 0.1);
 		transition: max-width var(--medium) var(--transition);
 
 		:slotted(.type-title) {
@@ -152,6 +156,7 @@ export default defineComponent({
 
 	.art {
 		position: relative;
+		z-index: 1;
 		display: none;
 		flex-grow: 1;
 		align-items: center;
@@ -182,7 +187,7 @@ export default defineComponent({
 				color: var(--white);
 				font-size: 15px;
 				line-height: 24px;
-				background-color: rgb(38 50 56 / 0.25);
+				background-color: rgb(38 50 56 / 0.2);
 				border-radius: 6px;
 				backdrop-filter: blur(2px);
 			}
@@ -210,18 +215,14 @@ export default defineComponent({
 			margin-left: 16px;
 
 			h1 {
-				color: var(--foreground-subdued);
-				color: var(--brand);
 				font-weight: 700;
-				font-size: 24px;
-				line-height: 24px;
+				font-size: 18px;
+				line-height: 18px;
 			}
 
 			.subtitle {
 				width: 100%;
 				color: var(--foreground-subdued);
-				color: var(--brand);
-				opacity: 0.6;
 			}
 		}
 	}
@@ -230,10 +231,10 @@ export default defineComponent({
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		width: 64px;
-		height: 64px;
+		width: 56px;
+		height: 56px;
 		background-color: var(--brand);
-		border-radius: var(--border-radius);
+		border-radius: calc(var(--border-radius) - 2px);
 
 		img {
 			width: 40px;
