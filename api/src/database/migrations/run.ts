directus/directus

commit 8a050188839862cbfd1f7c5c7494e117575ac84a

Left Join-ing on roles to allow users without roles to login (#10836)
* Left Join-ing on roles to allow users without roles to login

* moving filter logic to more appropriate location

api/src/database/migrations/run.ts
@@ -14,9 +14,7 @@ export default async function run(database: Knex, direction: 'up' | 'down' | 'la
 	let customMigrationFiles =
 		((await fse.pathExists(customMigrationsPath)) && (await fse.readdir(customMigrationsPath))) || [];
 
-	migrationFiles = migrationFiles.filter(
-		(file: string) => file.startsWith('run') === false && file.endsWith('.d.ts') === false
-	);
+	migrationFiles = migrationFiles.filter((file: string) => file.startsWith('run') === false && file.endsWith('.js'));
 	customMigrationFiles = customMigrationFiles.filter((file: string) => file.endsWith('.js'));
 
 	const completedMigrations = await database.select<Migration[]>('*').from('directus_migrations').orderBy('version');

api/src/services/authentication.ts
@@ -74,7 +74,7 @@ export class AuthenticationService {
 				'u.auth_data'
 			)
 			.from('directus_users as u')
-			.innerJoin('directus_roles as r', 'u.role', 'r.id')
+			.leftJoin('directus_roles as r', 'u.role', 'r.id')
 			.where('u.id', await provider.getUserID(cloneDeep(payload)))
 			.andWhere('u.provider', providerName)
 			.first();

tests/e2e/api/auth/login.test.ts
@@ -59,6 +59,29 @@ describe('auth', () => {
 					},
 				});
 			});
+			it.each(getDBsToTest())(
+				`%p returns an access_token, expires and a refresh_token for noRoleUser`,
+				async (vendor) => {
+					const url = `http://localhost:${config.ports[vendor]!}`;
+
+					const response = await request(url)
+						.post(`/auth/login`)
+						.send({
+							email: 'test@noroleuser.com',
+							password: 'TestNoRoleUserPassword',
+						})
+						.expect('Content-Type', /application\/json/)
+						.expect(200);
+
+					expect(response.body).toMatchObject({
+						data: {
+							access_token: expect.any(String),
+							expires: expect.any(Number),
+							refresh_token: expect.any(String),
+						},
+					});
+				}
+			);
 		});
 		describe('when incorrect credentials are provided', () => {
 			it.each(getDBsToTest())(`%p returns code: UNAUTHORIZED for incorrect password`, async (vendor) => {

tests/e2e/setup/seeds/04_directus_users.js
@@ -18,5 +18,12 @@ exports.seed = async function (knex) {
 			role: '214faee7-d6a6-4a4c-b1cd-f9e9bd0b6fb7',
 			token: 'UserToken',
 		},
+		{
+			id: 'cb8cd13b-037f-40ca-862a-ea1e1f4bfca3',
+			email: 'test@noroleuser.com',
+			password: await generateHash.hash('TestNoRoleUserPassword'),
+			status: 'active',
+			token: 'NoRoleUserToken',
+		},
 	]);
 };

commit 8b19a0888cd08447f129cc4f23efe5076c394834

Fix running migrations in dev mode
Fixes a regression introduced in #10836.

api/src/database/migrations/run.ts
@@ -14,7 +14,7 @@ export default async function run(database: Knex, direction: 'up' | 'down' | 'la
 	let customMigrationFiles =
 		((await fse.pathExists(customMigrationsPath)) && (await fse.readdir(customMigrationsPath))) || [];
 
-	migrationFiles = migrationFiles.filter((file: string) => file.startsWith('run') === false && file.endsWith('.js'));
+	migrationFiles = migrationFiles.filter((file: string) => /^[0-9]+[A-Z]-[^.]+\.(?:js|ts)$/.test(file));
 	customMigrationFiles = customMigrationFiles.filter((file: string) => file.endsWith('.js'));
 
 	const completedMigrations = await database.select<Migration[]>('*').from('directus_migrations').orderBy('version');