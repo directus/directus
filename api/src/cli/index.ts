ececeb0ea7370965559038215746c897eae41771
keesvanbemmel <keesvanbemmel@gmail.com>
2022-01-07 12:18:12 +0100

    Added option to schema command

--- api/src/cli/index.ts
+++ api/src/cli/index.ts
@@ -84,6 +84,7 @@ export async function createCli(): Promise<Command> {
 		.command('snapshot')
 		.description('Create a new Schema Snapshot')
 		.option('-y, --yes', `Assume "yes" as answer to all prompts and run non-interactively`, false)
+		.option('--sortyaml', `Sort the generated yaml file alphabetically`, false)
 		.addOption(new Option('--format <format>', 'JSON or YAML format').choices(['json', 'yaml']).default('yaml'))
 		.argument('<path>', 'Path to snapshot file')
 		.action(snapshot);
