490d697a22c679a98c672b1d04a09f24a5db35c9
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-01-07 17:54:47 +0800

    Make AND/OR language consistent in Filter UI

--- app/src/interfaces/_system/system-filter/nodes.vue
+++ app/src/interfaces/_system/system-filter/nodes.vue
@@ -57,10 +57,21 @@
 					<div class="header" :class="{ inline }">
 						<v-icon name="drag_indicator" class="drag-handle" small />
 						<div class="logic-type" :class="{ or: filterInfo[index].name === '_or' }">
-							<span class="key" @click="toggleLogic(index)">
-								{{ filterInfo[index].name === '_and' ? t('interfaces.filter.all') : t('interfaces.filter.any') }}
+							<span
+								v-tooltip="
+									filterInfo[index].name === '_and'
+										? t('interfaces.filter.logic_type_and_tooltip')
+										: t('interfaces.filter.logic_type_or_tooltip')
+								"
+								class="key"
+								@click="toggleLogic(index)"
+							>
+								{{
+									filterInfo[index].name === '_and'
+										? t('interfaces.filter.logic_type_and')
+										: t('interfaces.filter.logic_type_or')
+								}}
 							</span>
-							<span class="text">{{ t('interfaces.filter.of_the_following') }}</span>
 						</div>
 						<span class="delete">
 							<v-icon
