fb145bf646f676dcead2bafd1594f07f78d80d3b
Oreilles <33065839+oreilles@users.noreply.github.com>
2022-01-11 00:45:21 +0100

    Refactor e2e tests

--- tests/e2e/get-dbs-to-test.ts
+++ tests/e2e/get-dbs-to-test.ts
@@ -1,24 +1,12 @@
 /** @TODO once Oracle is officially supported, enable it here */
 export const allVendors = ['mssql', 'mysql', 'postgres', /* 'oracle', */ 'maria' /*, 'sqlite3'*/, 'postgres10'];
 
-export function getDBsToTest(): string[] {
-	const testVendors = process.env.TEST_DB || '*';
+const vendors = process.env.TEST_DB?.split(',').map((v) => v.trim()) ?? allVendors;
 
-	let vendors = [];
-
-	if (testVendors === '*') {
-		vendors = allVendors;
-	} else if (testVendors.includes(',')) {
-		vendors = testVendors.split(',').map((v) => v.trim());
-	} else {
-		vendors = [testVendors];
+for (const vendor of vendors) {
+	if (allVendors.includes(vendor) === false) {
+		throw new Error(`No e2e testing capabilities for vendor "${vendor}".`);
 	}
-
-	for (const vendor of vendors) {
-		if (allVendors.includes(vendor) === false) {
-			throw new Error(`No e2e testing capabilities for vendor "${vendor}".`);
-		}
-	}
-
-	return vendors;
 }
+
+export default vendors;
