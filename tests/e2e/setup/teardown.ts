7e10d2017e2319f40e2e0a8a379ed5d5ad22fefb
Oreille <33065839+Oreilles@users.noreply.github.com>
2022-01-13 22:32:45 +0100

    End-to-end tests refactor (#10968)
    * Refactor e2e tests
    
    * Only install root dependencies for lint step
    
    * Fix dumbest error ever
    
    * Pass process.env too to spawned subprocess
    
    * Suppress npm package installation prompt
    
    * Improve error handling
    
    * Add new compose file for tests
    
    * Avoid port conflict with remoted
    
    * Update docker-compose.yml
    
    * Add test docs
    
    * Use current branch workflow files and simplify skips
    
    * Fix workflow file
    
    * Fix workflow file
    
    * Try adding `.yml` extension to allow reference in `uses`
    
    * Place workflow file in folders to allow reference in `uses`
    
    * Requires more work than expected, reverting
    
    * Update docs to use correct compose file
    
    * Remove comment / unused code
    
    * Run tests from main
    
    Co-authored-by: ian <licitdev@gmail.com>
    Co-authored-by: Jay Cammarano <jay.cammarano@gmail.com>
    Co-authored-by: rijkvanzanten <rijkvanzanten@me.com>

--- tests/e2e/setup/teardown.ts
+++ tests/e2e/setup/teardown.ts
@@ -1,105 +1,29 @@
 /* eslint-disable no-console */
 
 import Listr from 'listr';
-import Dockerode from 'dockerode';
-import { getDBsToTest } from '../get-dbs-to-test';
-import config, { CONTAINER_PERSISTENCE_FILE } from '../config';
+import vendors from '../get-dbs-to-test';
+import config from '../config';
 import { GlobalConfigTsJest } from 'ts-jest/dist/types';
-import { readFileSync, rmSync } from 'fs';
 import global from './global';
-
-const docker = new Dockerode();
 
 if (require.main === module) {
 	teardown(undefined, true);
 }
 
-export default async function teardown(jestConfig?: GlobalConfigTsJest, isAfterWatch = false): Promise<void> {
+export default async function teardown(jestConfig?: GlobalConfigTsJest, _isAfterWatch = false): Promise<void> {
 	if (jestConfig?.watch || jestConfig?.watchAll) return;
-
-	const vendors = getDBsToTest();
-
-	console.log('\n');
 
 	await new Listr([
 		{
-			skip: () => !isAfterWatch,
-			title: 'Restore container info',
-			task: async () => {
-				const containers = JSON.parse(readFileSync(CONTAINER_PERSISTENCE_FILE).toString());
-				if (!(containers.db && containers.directus)) {
-					throw new Error('Could not load saved containers');
-				}
-				const restorePersistedContainer = ({ vendor, id }: { vendor: string; id: string }) => ({
-					vendor,
-					container: docker.getContainer(id),
-				});
-				global.databaseContainers = containers.db.map(restorePersistedContainer);
-				global.directusContainers = containers.directus.map(restorePersistedContainer);
-				rmSync(CONTAINER_PERSISTENCE_FILE);
-			},
-		},
-		{
-			title: 'Stop Docker containers',
+			title: 'Stop Directus servers',
 			task: () => {
 				return new Listr(
 					vendors.map((vendor) => {
 						return {
 							title: config.names[vendor]!,
-							task: () => {
-								return new Listr(
-									[
-										{
-											title: 'Database',
-											task: async () =>
-												await global.databaseContainers
-													.find((containerInfo) => containerInfo.vendor === vendor)
-													?.container.stop(),
-										},
-										{
-											title: 'Directus',
-											task: async () =>
-												await global.directusContainers
-													.find((containerInfo) => containerInfo.vendor === vendor)
-													?.container.stop(),
-										},
-									],
-									{ concurrent: true, exitOnError: false }
-								);
-							},
-						};
-					}),
-					{ concurrent: true, exitOnError: false }
-				);
-			},
-		},
-		{
-			title: 'Remove Docker containers',
-			task: () => {
-				return new Listr(
-					vendors.map((vendor) => {
-						return {
-							title: config.names[vendor]!,
-							task: () => {
-								return new Listr(
-									[
-										{
-											title: 'Database',
-											task: async () =>
-												await global.databaseContainers
-													.find((containerInfo) => containerInfo.vendor === vendor)
-													?.container.remove(),
-										},
-										{
-											title: 'Directus',
-											task: async () =>
-												await global.directusContainers
-													.find((containerInfo) => containerInfo.vendor === vendor)
-													?.container.remove(),
-										},
-									],
-									{ concurrent: true, exitOnError: false }
-								);
+							task: async () => {
+								const directus = global.directus[vendor];
+								directus!.kill();
 							},
 						};
 					}),
