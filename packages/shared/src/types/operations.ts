cddb58da9e67eff3456c1bf0990ee9fee3b57ee0
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2022-01-18 00:42:45 +0100

    Update operation handler return type

--- packages/shared/src/types/operations.ts
+++ packages/shared/src/types/operations.ts
@@ -1,7 +1,7 @@
 export type OperationHandler = (
 	data: Record<string, unknown>,
 	options: Record<string, any>
-) => unknown | void | Promise<unknown> | Promise<void>;
+) => unknown | Promise<unknown> | void;
 
 export interface OperationAppConfig {
 	id: string;
