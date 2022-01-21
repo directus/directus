2853efd556f8b1971442714f936821de9137b62c
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2022-01-21 19:52:39 +0100

    Improve event handler return types

--- packages/shared/src/types/events.ts
+++ packages/shared/src/types/events.ts
@@ -8,7 +8,11 @@ export type EventContext = {
 	accountability: Accountability | null;
 };
 
-export type FilterHandler = (payload: any, meta: Record<string, any>, context: EventContext) => any | Promise<any>;
+export type FilterHandler = (
+	payload: any,
+	meta: Record<string, any>,
+	context: EventContext
+) => unknown | Promise<unknown>;
 export type ActionHandler = (meta: Record<string, any>, context: EventContext) => void;
 export type InitHandler = (meta: Record<string, any>) => void;
-export type ScheduleHandler = () => void;
+export type ScheduleHandler = () => void | Promise<void>;
