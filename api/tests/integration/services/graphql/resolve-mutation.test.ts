01e8196aea7549afa908a69cb23de379c5979410
jaycammarano <jay.cammarano@gmail.com>
2022-01-27 03:28:31 +0100

    itemService being mocker "better" (different)

--- api/tests/integration/services/graphql/resolve-mutation.test.ts
+++ api/tests/integration/services/graphql/resolve-mutation.test.ts
@@ -4,7 +4,7 @@ import { userSchema } from '../../../__test-utils__/schemas';
 import { ResolveMutation } from '../../../../src/services/graphql/resolve-mutation';
 import { info } from '../../../__test-utils__/gql-queries';
 import * as ItemsService from '../../../../src/services/items';
-import * as GetService from '../../../../src/services/graphql/shared/get-service';
+// import * as GetService from '../../../../src/services/graphql/shared/get-service';
 import { Item, AbstractServiceOptions } from '../../../../src/types';
 
 jest.mock('../../../../src/database/index', () => {
@@ -16,18 +16,22 @@ describe('Class ResolveMutation', () => {
 	// mock class ItemsService to see which functions are called and what args they are passed
 
 	describe('resolveMutation()', () => {
-		let get: jest.SpyInstance<ItemsService.ItemsService<Item>, [options: AbstractServiceOptions, collection: string]>;
+		// let get: jest.SpyInstance<ItemsService.ItemsService<Item>, [options: AbstractServiceOptions, collection: string]>;
 		let itemService: jest.SpyInstance<
 			ItemsService.ItemsService<Item>,
 			[collection: string, options: AbstractServiceOptions]
 		>;
 
 		beforeEach(() => {
-			itemService = jest.spyOn(ItemsService, 'ItemsService').mockReturnValue({
-				readByQuery: { readByQuery: true },
-				upsertSingleton: { upsertSingleton: true },
-				readSingleton: { readSingleton: true },
-			} as any);
+			itemService = jest.spyOn(ItemsService, 'ItemsService').mockImplementation(
+				() =>
+					({
+						createOn: { createOne: true },
+						readByQuery: { readByQuery: true },
+						readSingleton: { readSingleton: true },
+						upsertSingleton: { upsertSingleton: true },
+					} as any)
+			);
 		});
 
 		afterEach(() => {
@@ -44,10 +48,12 @@ describe('Class ResolveMutation', () => {
 			expect(itemService.mock.calls[0][0]).toStrictEqual('');
 
 			expect(itemService.mock.results.length).toBe(1);
+			// We want results to contain the response from one of these functions
 			expect(itemService.mock.results).toBe([
 				{
 					type: 'return',
 					value: {
+						createOne: true,
 						readByQuery: true,
 						readSingleton: true,
 						upsertSingleton: true,
