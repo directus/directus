c210fe94eb448daf8f32e3259f6f1af778efafb3
rijkvanzanten <rijkvanzanten@me.com>
2022-02-21 11:51:03 -0500

    Fix theme color

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
 	ga: 'UA-24637628-7',
 	head: [
 		['link', { rel: 'manifest', href: '/manifest.json' }],
-		['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#00C897' }],
+		['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#6644ff' }],
 		['link', { rel: 'shortcut icon', type: 'image/svg+xml', href: '/favicon.svg' }],
 		[
 			'link',
@@ -35,9 +35,9 @@ module.exports = {
 			},
 		],
 		['meta', { name: 'application-name', content: 'Directus Docs' }],
-		['meta', { name: 'theme-color', content: '#00C897' }],
+		['meta', { name: 'theme-color', content: '#6644ff' }],
 		['meta', { name: 'apple-mobile-web-app-title', content: 'Directus Docs' }],
-		['meta', { name: 'msapplication-TileColor', content: '#00C897' }],
+		['meta', { name: 'msapplication-TileColor', content: '#6644ff' }],
 		['meta', { name: 'msapplication-config', content: '/browserconfig.xml' }],
 	],
 	themeConfig: {
