837e1e271c69a840f55a035a47abdebcf6c3a048
rijkvanzanten <rijkvanzanten@me.com>
2022-03-03 15:10:56 -0500

    Tweak and finish + test authenticate

--- api/src/middleware/authenticate.ts
+++ api/src/middleware/authenticate.ts
@@ -1,15 +1,14 @@
 import { Accountability } from '@directus/shared/types';
 import { NextFunction, Request, Response } from 'express';
-import jwt, { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 import { isEqual } from 'lodash';
 import getDatabase from '../database';
 import emitter from '../emitter';
 import env from '../env';
 import { InvalidCredentialsException } from '../exceptions';
-import { DirectusTokenPayload } from '../types';
+import asyncHandler from '../utils/async-handler';
 import { getIPFromReq } from '../utils/get-ip-from-req';
 import isDirectusJWT from '../utils/is-directus-jwt';
-import asyncHandler from '../utils/async-handler';
+import { verifyAccessJWT } from '../utils/jwt';
 
 /**
  * Verify the passed JWT and assign the user ID and role to `req`
@@ -39,7 +38,7 @@ export const handler = async (req: Request, res: Response, next: NextFunction) =
 		}
 	);
 
-	if (isEqual(req.accountability, defaultAccountability) === false) {
+	if (customAccountability && isEqual(customAccountability, defaultAccountability) === false) {
 		req.accountability = customAccountability;
 		return next();
 	}
@@ -48,19 +47,7 @@ export const handler = async (req: Request, res: Response, next: NextFunction) =
 
 	if (req.token) {
 		if (isDirectusJWT(req.token)) {
-			let payload: DirectusTokenPayload;
-
-			try {
-				payload = jwt.verify(req.token, env.SECRET as string, { issuer: 'directus' }) as DirectusTokenPayload;
-			} catch (err: any) {
-				if (err instanceof TokenExpiredError) {
-					throw new InvalidCredentialsException('Token expired.');
-				} else if (err instanceof JsonWebTokenError) {
-					throw new InvalidCredentialsException('Token invalid.');
-				} else {
-					throw err;
-				}
-			}
+			const payload = verifyAccessJWT(req.token, env.SECRET);
 
 			req.accountability.share = payload.share;
 			req.accountability.share_scope = payload.share_scope;
--- api/tests/middleware/authenticate.test.ts
+++ api/tests/middleware/authenticate.test.ts
@@ -1,10 +1,17 @@
 // @ts-nocheck
 
-import '../../src/types/express.d.ts';
-import { handler } from '../../src/middleware/authenticate';
+import jwt from 'jsonwebtoken';
+import getDatabase from '../../src/database';
 import emitter from '../../src/emitter';
+import env from '../../src/env';
+import { InvalidCredentialsException } from '../../src/exceptions';
+import { handler } from '../../src/middleware/authenticate';
+import '../../src/types/express.d.ts';
 
-jest.mock('../../src/emitter');
+jest.mock('../../src/database');
+jest.mock('../../src/env', () => ({
+	SECRET: 'test',
+}));
 
 afterEach(() => {
 	jest.resetAllMocks();
@@ -21,8 +28,7 @@ test('Short-circuits when authenticate filter is used', async () => {
 
 	const customAccountability = { admin: true };
 
-	const mockEmitter = jest.mocked(emitter, true);
-	mockEmitter.emitFilter.mockResolvedValue(customAccountability);
+	jest.spyOn(emitter, 'emitFilter').mockResolvedValue(customAccountability);
 
 	await handler(req, res, next);
 
@@ -39,8 +45,7 @@ test('Uses default public accountability when no token is given', async () => {
 	const res = {};
 	const next = jest.fn();
 
-	const mockEmitter = jest.mocked(emitter, true);
-	mockEmitter.emitFilter.mockImplementation((_, payload) => payload);
+	jest.spyOn(emitter, 'emitFilter').mockImplementation((_, payload) => payload);
 
 	await handler(req, res, next);
 
@@ -52,5 +57,161 @@ test('Uses default public accountability when no token is given', async () => {
 		ip: '127.0.0.1',
 		userAgent: 'fake-user-agent',
 	});
+
+	expect(next).toHaveBeenCalledTimes(1);
+});
+
+test('Sets accountability to payload contents if valid token is passed', async () => {
+	const userID = '3fac3c02-607f-4438-8d6e-6b8b25109b52';
+	const roleID = '38269fc6-6eb6-475a-93cb-479d97f73039';
+	const share = 'ca0ad005-f4ad-4bfe-b428-419ee8784790';
+	const shareScope = {
+		collection: 'articles',
+		item: 15,
+	};
+	const appAccess = true;
+	const adminAccess = false;
+
+	const token = jwt.sign(
+		{
+			id: userID,
+			role: roleID,
+			app_access: appAccess,
+			admin_access: adminAccess,
+			share,
+			share_scope: shareScope,
+		},
+		env.SECRET,
+		{ issuer: 'directus' }
+	);
+
+	const req = {
+		ip: '127.0.0.1',
+		get: jest.fn((string) => (string === 'user-agent' ? 'fake-user-agent' : null)),
+		token,
+	};
+
+	const res = {};
+	const next = jest.fn();
+
+	await handler(req, res, next);
+
+	expect(req.accountability).toEqual({
+		user: userID,
+		role: roleID,
+		app: appAccess,
+		admin: adminAccess,
+		share,
+		share_scope: shareScope,
+		ip: '127.0.0.1',
+		userAgent: 'fake-user-agent',
+	});
+
+	expect(next).toHaveBeenCalledTimes(1);
+
+	// Test with 1/0 instead or true/false
+	next.mockClear();
+
+	req.token = jwt.sign(
+		{
+			id: userID,
+			role: roleID,
+			app_access: 1,
+			admin_access: 0,
+			share,
+			share_scope: shareScope,
+		},
+		env.SECRET,
+		{ issuer: 'directus' }
+	);
+
+	await handler(req, res, next);
+
+	expect(req.accountability).toEqual({
+		user: userID,
+		role: roleID,
+		app: appAccess,
+		admin: adminAccess,
+		share,
+		share_scope: shareScope,
+		ip: '127.0.0.1',
+		userAgent: 'fake-user-agent',
+	});
+
+	expect(next).toHaveBeenCalledTimes(1);
+});
+
+test('Throws InvalidCredentialsException when static token is used, but user does not exist', async () => {
+	jest.mocked(getDatabase).mockReturnValue({
+		select: jest.fn().mockReturnThis(),
+		from: jest.fn().mockReturnThis(),
+		leftJoin: jest.fn().mockReturnThis(),
+		where: jest.fn().mockReturnThis(),
+		first: jest.fn().mockResolvedValue(undefined),
+	});
+
+	const req = {
+		ip: '127.0.0.1',
+		get: jest.fn((string) => (string === 'user-agent' ? 'fake-user-agent' : null)),
+		token: 'static-token',
+	};
+
+	const res = {};
+	const next = jest.fn();
+
+	expect(handler(req, res, next)).rejects.toEqual(new InvalidCredentialsException());
+	expect(next).toHaveBeenCalledTimes(0);
+});
+
+test('Sets accountability to user information when static token is used', async () => {
+	const req = {
+		ip: '127.0.0.1',
+		get: jest.fn((string) => (string === 'user-agent' ? 'fake-user-agent' : null)),
+		token: 'static-token',
+	};
+
+	const res = {};
+	const next = jest.fn();
+
+	const testUser = { id: 'test-id', role: 'test-role', admin_access: true, app_access: false };
+
+	const expectedAccountability = {
+		user: testUser.id,
+		role: testUser.role,
+		app: testUser.app_access,
+		admin: testUser.admin_access,
+		ip: '127.0.0.1',
+		userAgent: 'fake-user-agent',
+	};
+
+	jest.mocked(getDatabase).mockReturnValue({
+		select: jest.fn().mockReturnThis(),
+		from: jest.fn().mockReturnThis(),
+		leftJoin: jest.fn().mockReturnThis(),
+		where: jest.fn().mockReturnThis(),
+		first: jest.fn().mockResolvedValue(testUser),
+	});
+
+	await handler(req, res, next);
+
+	expect(req.accountability).toEqual(expectedAccountability);
+	expect(next).toHaveBeenCalledTimes(1);
+
+	// Test for 0 / 1 instead of false / true
+	next.mockClear();
+	testUser.admin_access = 1;
+	testUser.app_access = 0;
+	await handler(req, res, next);
+	expect(req.accountability).toEqual(expectedAccountability);
+	expect(next).toHaveBeenCalledTimes(1);
+
+	// Test for "1" / "0" instead of true / false
+	next.mockClear();
+	testUser.admin_access = '0';
+	testUser.app_access = '1';
+	expectedAccountability.admin = false;
+	expectedAccountability.app = true;
+	await handler(req, res, next);
+	expect(req.accountability).toEqual(expectedAccountability);
 	expect(next).toHaveBeenCalledTimes(1);
 });
