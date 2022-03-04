f489a5fa6576cd3d4717d2bab378394c825e0be7
rijkvanzanten <rijkvanzanten@me.com>
2022-03-04 16:58:07 -0500

    Fix public view with logo

--- app/src/views/public/public-view.vue
+++ app/src/views/public/public-view.vue
@@ -221,6 +221,10 @@ const logoURL = computed<string | null>(() => {
 		.fallback {
 			width: 100%;
 			height: 100%;
+			position: absolute;
+			left: 0;
+			top: 0;
+			z-index: -1;
 		}
 
 		.foreground {
