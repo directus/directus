a5080758d00643775d081424622784b6524cd353
Donald Ness <donald.ness@gmail.com>
2022-03-08 20:05:14 -0600

    Display fullscreen rich text input below dialog instead of exiting fullscreen

--- app/src/interfaces/input-rich-text-html/useSourceCode.ts
+++ app/src/interfaces/input-rich-text-html/useSourceCode.ts
@@ -23,10 +23,6 @@ export default function useSourceCode(editor: Ref<any>): UsableSourceCode {
 		icon: 'sourcecode',
 		tooltip: i18n.global.t('wysiwyg_options.source_code'),
 		onAction: () => {
-			if (editor.value.plugins.fullscreen.isFullscreen()) {
-				editor.value.execCommand('mceFullScreen');
-			}
-
 			codeDrawerOpen.value = true;
 			code.value = editor.value.getContent();
 		},
