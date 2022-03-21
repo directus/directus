e36db5459d92ec60c763cbc2204cc5d9c9f087d1
ian <licitdev@gmail.com>
2022-03-21 14:51:20 +0800

    Fix CORS origin in CSV format

--- api/src/middleware/cors.ts
+++ api/src/middleware/cors.ts
@@ -1,12 +1,23 @@
 import cors from 'cors';
 import { RequestHandler } from 'express';
 import env from '../env';
+import { toArray } from '@directus/shared/utils';
 
 let corsMiddleware: RequestHandler = (req, res, next) => next();
 
 if (env.CORS_ENABLED === true) {
+	const originWhitelist = env.CORS_ORIGIN ? toArray(env.CORS_ORIGIN) : false;
+
 	corsMiddleware = cors({
-		origin: env.CORS_ORIGIN || true,
+		origin: function (origin, callback) {
+			if (!originWhitelist) {
+				callback(null, true);
+			} else if (originWhitelist.indexOf(origin) !== -1) {
+				callback(null, origin);
+			} else {
+				callback(null, originWhitelist[0]);
+			}
+		},
 		methods: env.CORS_METHODS || 'GET,POST,PATCH,DELETE',
 		allowedHeaders: env.CORS_ALLOWED_HEADERS,
 		exposedHeaders: env.CORS_EXPOSED_HEADERS,
