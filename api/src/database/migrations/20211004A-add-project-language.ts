38ed54af51f6ed0be177483bfa531db0eb39065f
Christian Rendl <cr@mutor.at>
2022-03-22 15:26:28 +0100

    Fix cockroachdb test error in knex migration

--- api/src/database/migrations/20211004A-add-project-language.ts
+++ api/src/database/migrations/20211004A-add-project-language.ts
@@ -1,19 +1,34 @@
 import { Knex } from 'knex';
+import { getHelpers } from '../helpers';
 
 export async function up(knex: Knex): Promise<void> {
+	const helper = getHelpers(knex).schema;
+
 	await knex.schema.alterTable('directus_settings', (table) => {
 		table.string('project_language').notNullable().defaultTo('en-US');
 	});
-	await knex.schema.alterTable('directus_users', (table) => {
-		table.string('language').defaultTo(null).alter();
-	});
+
+	if (helper.isOneOfClients(['cockroachdb'])) {
+		knex.schema.raw('ALTER TABLE directus_users ALTER COLUMN language SET DEFAULT NULL');
+	} else {
+		await knex.schema.alterTable('directus_users', (table) => {
+			table.string('language').nullable().defaultTo(null).alter();
+		});
+	}
 }
 
 export async function down(knex: Knex): Promise<void> {
+	const helper = getHelpers(knex).schema;
+
 	await knex.schema.alterTable('directus_settings', (table) => {
 		table.dropColumn('project_language');
 	});
-	await knex.schema.alterTable('directus_users', (table) => {
-		table.string('language').defaultTo('en-US').alter();
-	});
+
+	if (helper.isOneOfClients(['cockroachdb'])) {
+		knex.schema.raw("ALTER TABLE directus_users ALTER COLUMN language SET DEFAULT 'en-US'");
+	} else {
+		await knex.schema.alterTable('directus_users', (table) => {
+			table.string('language').nullable().defaultTo('en-US');
+		});
+	}
 }
