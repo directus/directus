6edf451e9ecdd403430d24fd93ddd06704efc030
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-03-23 13:22:28 +0800

    Prevent M2M item edits without permission via app

--- app/src/interfaces/list-m2m/use-permissions.ts
+++ app/src/interfaces/list-m2m/use-permissions.ts
@@ -2,10 +2,16 @@ import { usePermissionsStore, useUserStore } from '@/stores';
 import { Collection } from '@directus/shared/types';
 import { computed, Ref, ComputedRef } from 'vue';
 
+type UsablePermissions = {
+	createAllowed: ComputedRef<boolean>;
+	selectAllowed: ComputedRef<boolean>;
+	updateAllowed: ComputedRef<boolean>;
+};
+
 export default function usePermissions(
 	junctionCollection: Ref<Collection>,
 	relationCollection: Ref<Collection>
-): { createAllowed: ComputedRef<boolean>; selectAllowed: ComputedRef<boolean> } {
+): UsablePermissions {
 	const permissionsStore = usePermissionsStore();
 	const userStore = useUserStore();
 
@@ -35,5 +41,20 @@ export default function usePermissions(
 		return hasJunctionPermissions;
 	});
 
-	return { createAllowed, selectAllowed };
+	const updateAllowed = computed(() => {
+		const admin = userStore.currentUser?.role.admin_access === true;
+		if (admin) return true;
+
+		const hasJunctionPermissions = !!permissionsStore.permissions.find(
+			(permission) => permission.action === 'update' && permission.collection === junctionCollection.value.collection
+		);
+
+		const hasRelatedPermissions = !!permissionsStore.permissions.find(
+			(permission) => permission.action === 'create' && permission.collection === relationCollection.value.collection
+		);
+
+		return hasJunctionPermissions && hasRelatedPermissions;
+	});
+
+	return { createAllowed, selectAllowed, updateAllowed };
 }
