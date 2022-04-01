a006d9867bbd4520fbde0a3516203c95d9d2da6b
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-04-01 15:07:48 +0800

    Cast input data for filters

--- api/src/database/helpers/fn/dialects/sqlite.ts
+++ api/src/database/helpers/fn/dialects/sqlite.ts
@@ -3,35 +3,35 @@ import { Knex } from 'knex';
 
 export class FnHelperSQLite extends FnHelper {
 	year(table: string, column: string): Knex.Raw {
-		return this.knex.raw("strftime('%Y', ??.?? / 1000, 'unixepoch')", [table, column]);
+		return this.knex.raw("CAST(strftime('%Y', ??.?? / 1000, 'unixepoch') AS INTEGER)", [table, column]);
 	}
 
 	month(table: string, column: string): Knex.Raw {
-		return this.knex.raw("strftime('%m', ??.?? / 1000, 'unixepoch')", [table, column]);
+		return this.knex.raw("CAST(strftime('%m', ??.?? / 1000, 'unixepoch') AS INTEGER)", [table, column]);
 	}
 
 	week(table: string, column: string): Knex.Raw {
-		return this.knex.raw("strftime('%W', ??.?? / 1000, 'unixepoch')", [table, column]);
+		return this.knex.raw("CAST(strftime('%W', ??.?? / 1000, 'unixepoch') AS INTEGER)", [table, column]);
 	}
 
 	day(table: string, column: string): Knex.Raw {
-		return this.knex.raw("strftime('%d', ??.?? / 1000, 'unixepoch')", [table, column]);
+		return this.knex.raw("CAST(strftime('%d', ??.?? / 1000, 'unixepoch') AS INTEGER)", [table, column]);
 	}
 
 	weekday(table: string, column: string): Knex.Raw {
-		return this.knex.raw("strftime('%w', ??.?? / 1000, 'unixepoch')", [table, column]);
+		return this.knex.raw("CAST(strftime('%w', ??.?? / 1000, 'unixepoch') AS INTEGER)", [table, column]);
 	}
 
 	hour(table: string, column: string): Knex.Raw {
-		return this.knex.raw("strftime('%H', ??.?? / 1000, 'unixepoch')", [table, column]);
+		return this.knex.raw("CAST(strftime('%H', ??.?? / 1000, 'unixepoch') AS INTEGER)", [table, column]);
 	}
 
 	minute(table: string, column: string): Knex.Raw {
-		return this.knex.raw("strftime('%M', ??.?? / 1000, 'unixepoch')", [table, column]);
+		return this.knex.raw("CAST(strftime('%M', ??.?? / 1000, 'unixepoch') AS INTEGER)", [table, column]);
 	}
 
 	second(table: string, column: string): Knex.Raw {
-		return this.knex.raw("strftime('%S', ??.?? / 1000, 'unixepoch')", [table, column]);
+		return this.knex.raw("CAST(strftime('%S', ??.?? / 1000, 'unixepoch') AS INTEGER)", [table, column]);
 	}
 
 	count(table: string, column: string): Knex.Raw<any> {
