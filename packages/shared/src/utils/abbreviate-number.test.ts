adb901eac8764a8c724fa6001df8907ea2db1fa8
jaycammarano <jay.cammarano@gmail.com>
2022-04-18 16:54:05 +0200

    better tests

--- packages/shared/src/utils/abbreviate-number.test.ts
+++ packages/shared/src/utils/abbreviate-number.test.ts
@@ -1,28 +1,34 @@
 import { abbreviateNumber } from './abbreviate-number';
 
 describe('when no unit is given', () => {
-	it('returns a string representation of the number when under 1000', () => {
+	it('when under 1000', () => {
 		expect(abbreviateNumber(0.78, 2)).toBe('0.78');
 	});
-	it('returns a string representation of the number rounded to nearest 1000 unit: k', () => {
+	it('when a number over 1000 is given', () => {
 		expect(abbreviateNumber(7008, 0)).toBe('7K');
 	});
-	it('returns a string representation of the rounded number when negative', () => {
+	it('when negative number is given', () => {
 		expect(abbreviateNumber(-7008, 0)).toBe('-7K');
 	});
 });
 
 describe('when unit M is given', () => {
-	it('returns a string representation of the number rounded to nearest 1000 unit: M', () => {
-		expect(abbreviateNumber(7008, 0, ['M'])).toBe('7M');
+	it('when under 1000', () => {
+		expect(abbreviateNumber(0.78, 2, ['M'])).toBe('0.78');
 	});
-	it('returns a string representation of the rounded number when negative', () => {
+	it('when over 1000', () => {
+		expect(abbreviateNumber(8000, 0, ['M'])).toBe('8M');
+	});
+	it('when negative', () => {
 		expect(abbreviateNumber(-7008, 0, ['M'])).toBe('-7M');
 	});
 });
 
 describe('when multiple units(["M","T"]) are given', () => {
-	it('returns a string representation of the number rounded to nearest 1000 unit: M', () => {
+	it('when under 1000', () => {
+		expect(abbreviateNumber(0.78, 2, ['M', 'T'])).toBe('0.78');
+	});
+	it('when number is over 1000', () => {
 		expect(abbreviateNumber(7008, 0, ['M', 'T'])).toBe('7M');
 	});
 	it('returns a string representation of the rounded number when negative', () => {
