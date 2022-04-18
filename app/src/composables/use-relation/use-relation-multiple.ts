303e192a05de35ec0941ae6d4f85996fd1cb5c72
Nitwel <nitwel@arcor.de>
2022-04-18 19:08:06 +0200

    fix update fetchedItems on every param change (#12832)

--- app/src/composables/use-relation/use-relation-multiple.ts
+++ app/src/composables/use-relation/use-relation-multiple.ts
@@ -57,7 +57,7 @@ export function useRelationMultiple(
 		}
 	});
 
-	watch(previewQuery, updateFetchedItems, { immediate: true });
+	watch([previewQuery, itemId, relation], updateFetchedItems, { immediate: true });
 
 	const { fetchedSelectItems, selected, isItemSelected } = useSelected();
 
