5c99906e4838b78ab0d27b666588146222ad52ed
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-04-20 20:10:02 +0800

    Refresh datetime display value every minute for relative format (#12872)

--- app/src/displays/datetime/datetime.vue
+++ app/src/displays/datetime/datetime.vue
@@ -2,88 +2,93 @@
 	<span class="datetime">{{ displayValue }}</span>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { useI18n } from 'vue-i18n';
-import { defineComponent, ref, watch, PropType, computed } from 'vue';
+import { ref, watch, computed, onMounted, onUnmounted } from 'vue';
 import localizedFormat from '@/utils/localized-format';
 import localizedFormatDistance from '@/utils/localized-format-distance';
 import { parseISO, parse } from 'date-fns';
 
-export default defineComponent({
-	props: {
-		value: {
-			type: String,
-			required: true,
-		},
-		type: {
-			type: String as PropType<'dateTime' | 'time' | 'date' | 'timestamp'>,
-			required: true,
-			validator: (val: string) => ['dateTime', 'date', 'time', 'timestamp'].includes(val),
-		},
-		format: {
-			type: String,
-			default: 'long',
-		},
-		relative: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	setup(props) {
-		const { t } = useI18n();
+interface Props {
+	value: string;
+	type: 'dateTime' | 'date' | 'time' | 'timestamp';
+	format?: string;
+	relative?: boolean;
+}
 
-		const localValue = computed(() => {
-			if (!props.value) return null;
+const props = withDefaults(defineProps<Props>(), {
+	format: 'long',
+	relative: true,
+});
 
-			if (props.type === 'timestamp') {
-				return parseISO(props.value);
-			} else if (props.type === 'dateTime') {
-				return parse(props.value, "yyyy-MM-dd'T'HH:mm:ss", new Date());
-			} else if (props.type === 'date') {
-				return parse(props.value, 'yyyy-MM-dd', new Date());
-			} else if (props.type === 'time') {
-				return parse(props.value, 'HH:mm:ss', new Date());
+const { t } = useI18n();
+
+const displayValue = ref<string | null>(null);
+
+const localValue = computed(() => {
+	if (!props.value) return null;
+
+	if (props.type === 'timestamp') {
+		return parseISO(props.value);
+	} else if (props.type === 'dateTime') {
+		return parse(props.value, "yyyy-MM-dd'T'HH:mm:ss", new Date());
+	} else if (props.type === 'date') {
+		return parse(props.value, 'yyyy-MM-dd', new Date());
+	} else if (props.type === 'time') {
+		return parse(props.value, 'HH:mm:ss', new Date());
+	}
+
+	return null;
+});
+
+const relativeFormat = (value: Date) =>
+	localizedFormatDistance(value, new Date(), {
+		addSuffix: true,
+	});
+
+watch(
+	localValue,
+	async (newValue) => {
+		if (newValue === null) {
+			displayValue.value = null;
+			return;
+		}
+
+		if (props.relative) {
+			displayValue.value = await relativeFormat(newValue);
+		} else {
+			let format;
+			if (props.format === 'long') {
+				format = `${t('date-fns_date')} ${t('date-fns_time')}`;
+				if (props.type === 'date') format = String(t('date-fns_date'));
+				if (props.type === 'time') format = String(t('date-fns_time'));
+			} else if (props.format === 'short') {
+				format = `${t('date-fns_date_short')} ${t('date-fns_time_short')}`;
+				if (props.type === 'date') format = String(t('date-fns_date_short'));
+				if (props.type === 'time') format = String(t('date-fns_time_short'));
+			} else {
+				format = props.format;
 			}
 
-			return null;
-		});
-
-		const displayValue = ref<string | null>(null);
-
-		watch(
-			localValue,
-			async (newValue) => {
-				if (newValue === null) {
-					displayValue.value = null;
-					return;
-				}
-
-				if (props.relative) {
-					displayValue.value = await localizedFormatDistance(newValue, new Date(), {
-						addSuffix: true,
-					});
-				} else {
-					let format;
-					if (props.format === 'long') {
-						format = `${t('date-fns_date')} ${t('date-fns_time')}`;
-						if (props.type === 'date') format = String(t('date-fns_date'));
-						if (props.type === 'time') format = String(t('date-fns_time'));
-					} else if (props.format === 'short') {
-						format = `${t('date-fns_date_short')} ${t('date-fns_time_short')}`;
-						if (props.type === 'date') format = String(t('date-fns_date_short'));
-						if (props.type === 'time') format = String(t('date-fns_time_short'));
-					} else {
-						format = props.format;
-					}
-
-					displayValue.value = await localizedFormat(newValue, format);
-				}
-			},
-			{ immediate: true }
-		);
-
-		return { displayValue };
+			displayValue.value = await localizedFormat(newValue, format);
+		}
 	},
+	{ immediate: true }
+);
+
+let refreshInterval: number | null = null;
+
+onMounted(async () => {
+	if (!props.relative) return;
+
+	refreshInterval = window.setInterval(async () => {
+		if (!localValue.value) return;
+		displayValue.value = await relativeFormat(localValue.value);
+	}, 60000);
+});
+
+onUnmounted(() => {
+	if (refreshInterval) clearInterval(refreshInterval);
 });
 </script>
 
