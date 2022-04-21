d83da9cf41a230720161caa739e2d3d8f6a03715
Gerard <gerardlamo@gmail.com>
2022-04-21 20:07:53 +0200

    Fix linting and logic in options

--- app/src/displays/datetime/index.ts
+++ app/src/displays/datetime/index.ts
@@ -7,8 +7,9 @@ export default defineDisplay({
 	description: '$t:displays.datetime.description',
 	icon: 'query_builder',
 	component: DisplayDateTime,
-	options: ({ field: { meta } }) => {
-		const options = [
+	options: ({ field }) => {
+		const options = field.meta?.options || {};
+		const fields = [
 			{
 				field: 'relative',
 				name: '$t:displays.datetime.relative',
@@ -23,62 +24,68 @@ export default defineDisplay({
 				schema: {
 					default_value: false,
 				},
-			}
+			},
 		];
-		
-		options.push(! meta?.options.relative ? ({
-			field: 'format',
-			name: '$t:displays.datetime.format',
-			type: 'string',
-			meta: {
-				interface: 'select-dropdown',
-				width: 'half',
-				options: {
-					choices: [
-						{ text: '$t:displays.datetime.long', value: 'long' },
-						{ text: '$t:displays.datetime.short', value: 'short' },
-					],
-					allowOther: true,
+
+		if (options.relative) {
+			fields.push({
+				field: 'format',
+				name: '$t:displays.datetime.format',
+				type: 'string',
+				meta: {
+					interface: 'select-dropdown',
+					width: 'half',
+					options: {
+						choices: [
+							{ text: '$t:displays.datetime.long', value: 'long' },
+							{ text: '$t:displays.datetime.short', value: 'short' },
+						],
+						allowOther: true,
+					},
+					note: '$t:displays.datetime.format_note',
 				},
-				note: '$t:displays.datetime.format_note',
-			},
-			schema: {
-				default_value: 'long',
-			},
-		}) : ({
-			field: 'strict',
-			name: 'Strict',
-			type: 'boolean',
-			meta: {
-				width: 'half',
-				interface: 'boolean',
-				options: {
-					label: 'Use strict units',
+				schema: {
+					default_value: 'long',
 				},
-				note: "Removes words like 'almost', 'over', 'less than'",
-			},
-			schema: {
-				default_value: false,
-			},
-		},
-		{
-			field: 'suffix',
-			name: 'Suffix',
-			type: 'boolean',
-			meta: {
-				width: 'half',
-				interface: 'boolean',
-				options: {
-					label: 'Show relative indicator',
+			});
+		} else {
+			fields.push(
+				{
+					field: 'strict',
+					name: 'Strict',
+					type: 'boolean',
+					meta: {
+						width: 'half',
+						interface: 'boolean',
+						options: {
+							label: 'Use strict units',
+						},
+						note: "Removes words like 'almost', 'over', 'less than'",
+					},
+					schema: {
+						default_value: false,
+					},
 				},
-				note: "Uses words like 'in' and 'ago'",
-			},
-			schema: {
-				default_value: true,
-			},
-		});
-		
-		return options
+				{
+					field: 'suffix',
+					name: 'Suffix',
+					type: 'boolean',
+					meta: {
+						width: 'half',
+						interface: 'boolean',
+						options: {
+							label: 'Show relative indicator',
+						},
+						note: "Uses words like 'in' and 'ago'",
+					},
+					schema: {
+						default_value: true,
+					},
+				}
+			);
+		}
+
+		return fields;
 	},
 	types: ['dateTime', 'date', 'time', 'timestamp'],
 });
