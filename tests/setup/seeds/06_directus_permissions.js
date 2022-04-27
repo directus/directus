0188bd00701f7c02057e25edfe3ae81a0aad064d
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-04-27 23:01:25 +0800

    attempt to fix oracle

--- tests/setup/seeds/06_directus_permissions.js
+++ tests/setup/seeds/06_directus_permissions.js
@@ -7,13 +7,13 @@ exports.seed = async function (knex) {
 			role: '214faee7-d6a6-4a4c-b1cd-f9e9bd0b6fb7',
 			collection: 'guests',
 			action: 'read',
-			fields: ['*'],
+			fields: '*',
 		},
 		{
 			role: '214faee7-d6a6-4a4c-b1cd-f9e9bd0b6fb7',
 			collection: 'directus_roles',
 			action: 'read',
-			fields: ['*'],
+			fields: '*',
 		},
 	]);
 };
