4774c1bdd288c7b68c6b51d164a5ef5a5f869e2f
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-04-29 13:33:18 +0800

    default status to "active"

--- api/src/database/migrations/20220102A-add-flows.ts
+++ api/src/database/migrations/20220102A-add-flows.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
 		table.string('name').notNullable();
 		table.string('icon', 30);
 		table.text('note');
-		table.string('status').notNullable().defaultTo('inactive');
+		table.string('status').notNullable().defaultTo('active');
 		table.string('trigger');
 		table.json('options');
 		table.timestamp('date_created').defaultTo(knex.fn.now());
