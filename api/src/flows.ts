f3baefa4e69853667382d1188153eb70af3612f8
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-05-02 20:53:07 +0800

    simplify manual trigger handler

--- api/src/flows.ts
+++ api/src/flows.ts
@@ -108,16 +108,12 @@ class FlowManager {
 				const handler = (data: unknown, context: Record<string, unknown>) => this.executeFlow(flow, data, context);
 
 				this.operationFlowHandlers[flow.id] = handler;
-			} else if (flow.trigger === 'webhook') {
+			} else if (flow.trigger === 'webhook' || flow.trigger === 'manual') {
 				const handler = (data: unknown, context: Record<string, unknown>) => this.executeFlow(flow, data, context);
 
 				const method = flow.options?.method ?? 'GET';
 
 				this.webhookFlowHandlers[`${method}-${flow.id}`] = handler;
-			} else if (flow.trigger === 'manual') {
-				const handler = (data: unknown, context: Record<string, unknown>) => this.executeFlow(flow, data, context);
-
-				this.webhookFlowHandlers[`GET-${flow.id}`] = handler;
 			}
 		}
 	}
