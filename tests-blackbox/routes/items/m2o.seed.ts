bcb0b57ffe0beecd72e13dde4debce19560ca563
ian <licitdev@gmail.com>
2022-05-10 17:36:27 +0800

    Increase seedDB() timeout

--- tests-blackbox/routes/items/m2o.seed.ts
+++ tests-blackbox/routes/items/m2o.seed.ts
@@ -99,151 +99,155 @@ export const seedSchema: TestsSchema = {
 };
 
 export const seedDB = () => {
-	it.each(vendors)('%s', async (vendor) => {
-		try {
-			// Delete existing collections
-			await DeleteCollection(vendor, { collection: collectionCities });
-			await DeleteCollection(vendor, { collection: collectionStates });
-			await DeleteCollection(vendor, { collection: collectionCountries });
+	it.each(vendors)(
+		'%s',
+		async (vendor) => {
+			try {
+				// Delete existing collections
+				await DeleteCollection(vendor, { collection: collectionCities });
+				await DeleteCollection(vendor, { collection: collectionStates });
+				await DeleteCollection(vendor, { collection: collectionCountries });
 
-			// Create countries collection
-			await CreateCollection(vendor, {
-				collection: collectionCountries,
-			});
+				// Create countries collection
+				await CreateCollection(vendor, {
+					collection: collectionCountries,
+				});
 
-			await CreateField(vendor, {
-				collection: collectionCountries,
-				field: 'name',
-				type: 'string',
-			});
+				await CreateField(vendor, {
+					collection: collectionCountries,
+					field: 'name',
+					type: 'string',
+				});
 
-			// Create states collection
-			await CreateCollection(vendor, {
-				collection: collectionStates,
-			});
+				// Create states collection
+				await CreateCollection(vendor, {
+					collection: collectionStates,
+				});
 
-			await CreateField(vendor, {
-				collection: collectionStates,
-				field: 'name',
-				type: 'string',
-			});
+				await CreateField(vendor, {
+					collection: collectionStates,
+					field: 'name',
+					type: 'string',
+				});
 
-			await CreateFieldM2O(vendor, {
-				collection: collectionStates,
-				field: 'country_id',
-				primaryKeyType: 'integer',
-				otherCollection: collectionCountries,
-			});
+				await CreateFieldM2O(vendor, {
+					collection: collectionStates,
+					field: 'country_id',
+					primaryKeyType: 'integer',
+					otherCollection: collectionCountries,
+				});
 
-			// Create cities collection
-			await CreateCollection(vendor, {
-				collection: collectionCities,
-			});
+				// Create cities collection
+				await CreateCollection(vendor, {
+					collection: collectionCities,
+				});
 
-			await CreateField(vendor, {
-				collection: collectionCities,
-				field: 'name',
-				type: 'string',
-			});
+				await CreateField(vendor, {
+					collection: collectionCities,
+					field: 'name',
+					type: 'string',
+				});
 
-			await CreateFieldM2O(vendor, {
-				collection: collectionCities,
-				field: 'state_id',
-				primaryKeyType: 'integer',
-				otherCollection: collectionStates,
-			});
+				await CreateFieldM2O(vendor, {
+					collection: collectionCities,
+					field: 'state_id',
+					primaryKeyType: 'integer',
+					otherCollection: collectionStates,
+				});
 
-			// Create countries
-			await CreateItem(vendor, {
-				collection: collectionCountries,
-				item: [
-					{
-						id: 10000,
-						name: 'United States',
-					},
-					{
-						id: 10001,
-						name: 'Malaysia',
-					},
-				] as Country[],
-			});
+				// Create countries
+				await CreateItem(vendor, {
+					collection: collectionCountries,
+					item: [
+						{
+							id: 10000,
+							name: 'United States',
+						},
+						{
+							id: 10001,
+							name: 'Malaysia',
+						},
+					] as Country[],
+				});
 
-			// Create states
-			await CreateItem(vendor, {
-				collection: collectionStates,
-				item: [
-					{
-						id: 10000,
-						name: 'Washington',
-						country_id: 10000,
-					},
-					{
-						id: 10001,
-						name: 'California',
-						country_id: 10000,
-					},
-					{
-						id: 10002,
-						name: 'Johor',
-						country_id: 10001,
-					},
-					{
-						id: 10003,
-						name: 'Sabah',
-						country_id: 10001,
-					},
-				] as State[],
-			});
+				// Create states
+				await CreateItem(vendor, {
+					collection: collectionStates,
+					item: [
+						{
+							id: 10000,
+							name: 'Washington',
+							country_id: 10000,
+						},
+						{
+							id: 10001,
+							name: 'California',
+							country_id: 10000,
+						},
+						{
+							id: 10002,
+							name: 'Johor',
+							country_id: 10001,
+						},
+						{
+							id: 10003,
+							name: 'Sabah',
+							country_id: 10001,
+						},
+					] as State[],
+				});
 
-			// Create cities
-			await CreateItem(vendor, {
-				collection: collectionCities,
-				item: [
-					{
-						id: 10000,
-						name: 'Seattle',
-						state_id: 10000,
-					},
-					{
-						id: 10001,
-						name: 'Spokane',
-						state_id: 10000,
-					},
-					{
-						id: 10002,
-						name: 'Los Angeles',
-						state_id: 10001,
-					},
-					{
-						id: 10003,
-						name: 'San Francisco',
-						state_id: 10001,
-					},
-					{
-						id: 10004,
-						name: 'Johor Bahru',
-						state_id: 10002,
-					},
-					{
-						id: 10005,
-						name: 'Muar',
-						state_id: 10002,
-					},
-					{
-						id: 10006,
-						name: 'Kota Kinabalu',
-						state_id: 10003,
-					},
-					{
-						id: 10007,
-						name: 'Sandakan',
-						state_id: 10003,
-					},
-				] as City[],
-			});
-			expect(true).toBeTruthy();
-		} catch (error) {
-			expect(error).toBeFalsy();
-		}
-	});
+				// Create cities
+				await CreateItem(vendor, {
+					collection: collectionCities,
+					item: [
+						{
+							id: 10000,
+							name: 'Seattle',
+							state_id: 10000,
+						},
+						{
+							id: 10001,
+							name: 'Spokane',
+							state_id: 10000,
+						},
+						{
+							id: 10002,
+							name: 'Los Angeles',
+							state_id: 10001,
+						},
+						{
+							id: 10003,
+							name: 'San Francisco',
+							state_id: 10001,
+						},
+						{
+							id: 10004,
+							name: 'Johor Bahru',
+							state_id: 10002,
+						},
+						{
+							id: 10005,
+							name: 'Muar',
+							state_id: 10002,
+						},
+						{
+							id: 10006,
+							name: 'Kota Kinabalu',
+							state_id: 10003,
+						},
+						{
+							id: 10007,
+							name: 'Sandakan',
+							state_id: 10003,
+						},
+					] as City[],
+				});
+				expect(true).toBeTruthy();
+			} catch (error) {
+				expect(error).toBeFalsy();
+			}
+		},
+		30000
+	);
 };
