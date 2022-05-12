c3fbdbce99eb994a92aa855bfdd552870d92b015
Rijk van Zanten <rijkvanzanten@me.com>
2022-05-12 15:40:38 -0400

    Add string-export support for related-values and translations display (#13263)
    * Add string preview handler for related values display
    
    * Fix continue clause in for loop
    
    * Add string export for translations display
    
    * Handle null values better
    
    * ... while making sure false/0/'' still is considered a value

--- app/src/displays/related-values/index.ts
+++ app/src/displays/related-values/index.ts
@@ -4,6 +4,9 @@ import { getFieldsFromTemplate } from '@directus/shared/utils';
 import getRelatedCollection from '@/utils/get-related-collection';
 import DisplayRelatedValues from './related-values.vue';
 import { useFieldsStore } from '@/stores';
+import { getDisplay } from '@/displays';
+import { get, set } from 'lodash';
+import { renderPlainStringTemplate } from '@/utils/render-string-template';
 
 type Options = {
 	template: string;
@@ -42,6 +45,54 @@ export default defineDisplay({
 				meta: displayTemplateMeta,
 			},
 		];
+	},
+	handler: async (value, options, { collection, field }) => {
+		if (!field || !collection) return value;
+
+		const relatedCollections = getRelatedCollection(collection, field.field);
+
+		if (!relatedCollections) return value;
+
+		const fieldsStore = useFieldsStore();
+
+		const fieldKeys = getFieldsFromTemplate(options.template);
+
+		const fields = fieldKeys.map((fieldKey) => {
+			return {
+				key: fieldKey,
+				field: fieldsStore.getField(
+					relatedCollections.junctionCollection ?? relatedCollections.relatedCollection,
+					fieldKey
+				),
+			};
+		});
+
+		const stringValues: Record<string, string> = {};
+
+		for (const { key, field } of fields) {
+			const fieldValue = get(value, key);
+
+			if (fieldValue === null || fieldValue === undefined) continue;
+
+			if (!field?.meta?.display) {
+				set(stringValues, key, fieldValue);
+				continue;
+			}
+
+			const display = getDisplay(field.meta.display);
+
+			const stringValue = display?.handler
+				? await display.handler(fieldValue, field?.meta?.display_options ?? {}, {
+						interfaceOptions: field?.meta?.options ?? {},
+						field: field ?? undefined,
+						collection: collection,
+				  })
+				: fieldValue;
+
+			set(stringValues, key, stringValue);
+		}
+
+		return renderPlainStringTemplate(options.template, stringValues);
 	},
 	types: ['alias', 'string', 'uuid', 'integer', 'bigInteger', 'json'],
 	localTypes: ['m2m', 'm2o', 'o2m', 'translations', 'm2a', 'file', 'files'],
