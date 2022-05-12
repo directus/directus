bda7c25819dbf751d06666a10476e07f906b76a5
AustinPhillipTaylor <47870619+AustinPhillipTaylor@users.noreply.github.com>
2022-05-11 20:07:40 -0400

    Added graphql mutation for theme overrides updates

--- api/src/services/graphql.ts
+++ api/src/services/graphql.ts
@@ -1700,6 +1700,7 @@ export class GraphQLService {
 				project_background: { type: GraphQLString },
 				project_note: { type: GraphQLString },
 				custom_css: { type: GraphQLString },
+				theme_overrides: { type: GraphQLJSON },
 			},
 		});
 
@@ -2689,6 +2690,26 @@ export class GraphQLService {
 						});
 						await service.inviteUser(args.email, args.role, args.invite_url || null);
 						return true;
+					},
+				},
+			});
+		}
+
+		if ('directus_settings' in schema.update.collections) {
+			schemaComposer.Mutation.addFields({
+				update_theme_overrides: {
+					type: ReadCollectionTypes['directus_settings'],
+					args: {
+						data: GraphQLJSON,
+					},
+					resolve: async (_, args, { req }) => {
+						const service = new SettingsService({
+							accountability: this.accountability,
+							schema: this.schema,
+						});
+						await service.updateThemeOverrides(args.data, req!.sanitizedQuery);
+						const result = await service.readSingleton(req!.sanitizedQuery);
+						return result;
 					},
 				},
 			});
