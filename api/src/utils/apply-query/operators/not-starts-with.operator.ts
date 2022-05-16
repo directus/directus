6817273d9876e69dcdc3a7288a5ca1a258215663
Bernat Vadell <bvadell@feimsoft.com>
2022-05-16 18:46:56 +0200

    fix: _nstarts_with not applied correctly

--- api/src/utils/apply-query/operators/not-starts-with.operator.ts
+++ api/src/utils/apply-query/operators/not-starts-with.operator.ts
@@ -3,6 +3,6 @@ import { registerOperator } from './operator-register';
 export default registerOperator({
 	operator: '_nstarts_with',
 	apply: ({ query, selectionRaw, compareValue }) => {
-		query.where(selectionRaw, 'like', `${compareValue}%`);
+		query.whereNot(selectionRaw, 'like', `${compareValue}%`);
 	},
 });
