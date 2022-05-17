0b6d35ac902edfeee5fe201e1f12fe4b3c474006
rijkvanzanten <rijkvanzanten@me.com>
2022-05-17 18:10:06 -0400

    [WIP] Start rendering logs drawer from sidebar

--- app/src/modules/settings/routes/flows/components/logs-sidebar-detail.vue
+++ app/src/modules/settings/routes/flows/components/logs-sidebar-detail.vue
@@ -1,7 +1,33 @@
 <template>
-	<sidebar-detail :title="t('logs')" icon="fact_check" :badge="5">
-		{{ revisions }}
+	<sidebar-detail :title="t('logs')" icon="fact_check" :badge="revisionsCount">
+		<v-detail
+			v-for="group in revisionsByDate"
+			:key="group.dateFormatted"
+			:label="group.dateFormatted"
+			class="revisions-date-group"
+			start-open
+		>
+			<div class="scroll-container">
+				<p v-for="revision in group.revisions" :key="revision.id" @click="previewing = revision">
+					{{ getTime(revision.activity.timestamp) }}
+				</p>
+			</div>
+		</v-detail>
 	</sidebar-detail>
+
+	<v-drawer
+		:model-value="!!previewing"
+		:title="previewing?.timestamp"
+		@cancel="previewing = null"
+		@esc="previewing = null"
+	>
+		<div class="content">
+			<div v-for="[key, value] of valuePreviews" :key="key" class="operation-data">
+				<p class="key type-label">{{ key }}</p>
+				<pre class="json selectable">{{ value }}</pre>
+			</div>
+		</div>
+	</v-drawer>
 </template>
 
 <script lang="ts" setup>
@@ -9,6 +35,7 @@ import { useRevisions } from '@/composables/use-revisions';
 import { Action, FlowRaw } from '@directus/shared/types';
 import { computed, ref, toRefs, unref } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { format } from 'date-fns';
 
 const { t } = useI18n();
 
@@ -20,13 +47,43 @@ const props = defineProps<Props>();
 
 const { flow } = toRefs(props);
 
-const { revisions } = useRevisions(
+const { revisionsByDate, revisionsCount } = useRevisions(
 	ref('directus_flows'),
 	computed(() => unref(flow).id),
 	{
 		action: Action.RUN,
 	}
 );
+
+const previewing = ref();
+
+const valuePreviews = computed(() => {
+	if (!unref(previewing)) return [];
+
+	return Object.entries(unref(previewing).data).filter(([key, value]) => key.startsWith('$') === false && value);
+});
+
+function getTime(timestamp: string) {
+	return format(new Date(timestamp), String(t('date-fns_time')));
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content {
+	padding: var(--content-padding);
+}
+
+.operation-data + .operation-data {
+	border-top: var(--border-width) solid var(--border-normal);
+	margin-top: 20px;
+	padding-top: 20px;
+}
+
+.json {
+	background-color: var(--background-subdued);
+	font-family: var(--family-monospace);
+	border-radius: var(--border-radius);
+	padding: 20px;
+	margin-top: 20px;
+}
+</style>
