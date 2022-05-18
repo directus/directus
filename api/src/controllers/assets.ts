e0f396eadbf0b84beec4e974fbe7b2095603f6a2
Jose Varela <joselcvarela@gmail.com>
2022-05-18 21:33:21 +0100

    Make expression lazy instead of greedy Fix CodeQL "Polynomial regular expression used on uncontrolled data"

--- api/src/controllers/assets.ts
+++ api/src/controllers/assets.ts
@@ -140,7 +140,7 @@ router.get(
 
 		if (req.headers.range) {
 			// substring 6 = "bytes="
-			const rangeParts = /bytes=(.*)-(.*)/.exec(req.headers.range);
+			const rangeParts = /bytes=(.*?)-(.*)/.exec(req.headers.range);
 
 			range = {
 				start: rangeParts?.[1] ? Number(rangeParts[1]) : undefined,
