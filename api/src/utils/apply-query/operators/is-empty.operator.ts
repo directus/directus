ba3fcb82478989eed66acbd637ef3c2c88e71dac
ian <licitdev@gmail.com>
2022-05-19 01:38:38 +0800

    Fix compareValue check for _null and _empty type of operators (#13388)

--- api/src/utils/apply-query/operators/is-empty.operator.ts
+++ api/src/utils/apply-query/operators/is-empty.operator.ts
@@ -3,7 +3,7 @@ import { registerOperator } from './operator-register';
 export default registerOperator({
 	operator: '_empty',
 	apply: ({ query, selectionRaw, compareValue }) => {
-		if (compareValue === true) {
+		if (compareValue) {
 			query.where(selectionRaw, '=', '');
 		} else {
 			query.where(selectionRaw, '!=', '');
