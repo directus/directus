747bfef9e780bf7e72c374ee025d1b4cf374fcbd
rijkvanzanten <rijkvanzanten@me.com>
2022-05-19 12:51:40 -0400

    Resolve range undefined check

--- api/src/exceptions/range-not-satisfiable.ts
+++ api/src/exceptions/range-not-satisfiable.ts
@@ -3,7 +3,10 @@ import { Range } from '@directus/drive';
 
 export class RangeNotSatisfiableException extends BaseException {
 	constructor(range?: Range) {
-		const rangeString = range && (range?.start || range?.end) ? `"${range.start ?? ''}-${range.end ?? ''}" ` : '';
+		const rangeString =
+			range && (range?.start !== undefined || range?.end !== undefined)
+				? `"${range.start ?? ''}-${range.end ?? ''}" `
+				: '';
 
 		super(
 			`Range ${rangeString}is invalid or the file's size doesn't match the requested range.`,
