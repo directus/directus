9afe8aae13af9e42356281ce64c0b1caa3631777
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2022-05-20 17:17:13 +0200

    Do not drop non null constraint from column that is nullable

--- api/src/database/migrations/20220429C-drop-non-null-from-ip-of-activity.ts
+++ api/src/database/migrations/20220429C-drop-non-null-from-ip-of-activity.ts
@@ -3,13 +3,11 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_activity', (table) => {
 		table.setNullable('ip');
-		table.setNullable('user_agent');
 	});
 }
 
 export async function down(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_activity', (table) => {
 		table.dropNullable('ip');
-		table.dropNullable('user_agent');
 	});
 }
