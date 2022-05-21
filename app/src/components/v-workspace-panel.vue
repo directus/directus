2b667749250caad58002251a174b397174066146
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2022-05-21 02:45:25 +0200

    Make delete action name generic in v-workspace-panel

--- app/src/components/v-workspace-panel.vue
+++ app/src/components/v-workspace-panel.vue
@@ -50,7 +50,7 @@
 						<v-list-item-icon>
 							<v-icon name="delete" />
 						</v-list-item-icon>
-						<v-list-item-content>{{ t('delete_operation') }}</v-list-item-content>
+						<v-list-item-content>{{ t('delete') }}</v-list-item-content>
 					</v-list-item>
 				</v-list>
 			</v-menu>
