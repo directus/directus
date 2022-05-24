3f4ad0fe87ffc36910a12a67486411e1a47b7060
Brainslug <br41nslug@users.noreply.github.com>
2022-05-24 21:19:52 +0200

    Environment variable with _FILE suffix containing invalid path throws error on start (#13274)
    * opt-in for reading of files from environment variables suffixed with _FILE
    
    * updated docs for new environment variable
    
    * [WIP] allow list
    
    * moved allow list to constants file
    
    * updated whitelist logic for regex matches
    
    * Revert "updated docs for new environment variable"
    
    This reverts commit 584966df38be4bbf1f89487d1e57a0f6116f584b.
    
    * moved allow list back to env.ts to prevent circular dependency

--- api/src/env.ts
+++ api/src/env.ts
@@ -11,6 +11,180 @@ import { requireYAML } from './utils/require-yaml';
 import { toArray } from '@directus/shared/utils';
 import { parseJSON } from './utils/parse-json';
 
+// keeping this here for now to prevent a circular import to constants.ts
+const allowedEnvironmentVars = [
+	// general
+	'CONFIG_PATH',
+	'HOST',
+	'PORT',
+	'PUBLIC_URL',
+	'LOG_LEVEL',
+	'LOG_STYLE',
+	'MAX_PAYLOAD_SIZE',
+	'ROOT_REDIRECT',
+	'SERVE_APP',
+	'GRAPHQL_INTROSPECTION',
+	// server
+	'SERVER_KEEP_ALIVE_TIMEOUT',
+	'SERVER_HEADERS_TIMEOUT',
+	// database
+	'DB_CLIENT',
+	'DB_HOST',
+	'DB_PORT',
+	'DB_DATABASE',
+	'DB_USER',
+	'DB_PASSWORD',
+	'DB_FILENAME',
+	'DB_CONNECTION_STRING',
+	'DB_POOL_.+',
+	'DB_EXCLUDE_TABLES',
+	'DB_CHARSET',
+	'DB_VERSION',
+	// security
+	'KEY',
+	'SECRET',
+	'ACCESS_TOKEN_TTL',
+	'REFRESH_TOKEN_TTL',
+	'REFRESH_TOKEN_COOKIE_DOMAIN',
+	'REFRESH_TOKEN_COOKIE_SECURE',
+	'REFRESH_TOKEN_COOKIE_SAME_SITE',
+	'REFRESH_TOKEN_COOKIE_NAME',
+	'PASSWORD_RESET_URL_ALLOW_LIST',
+	'USER_INVITE_URL_ALLOW_LIST',
+	'IP_TRUST_PROXY',
+	'IP_CUSTOM_HEADER',
+	'ASSETS_CONTENT_SECURITY_POLICY',
+	'IMPORT_IP_DENY_LIST',
+	'CONTENT_SECURITY_POLICY_.+',
+	'HSTS_.+', // covers 'HSTS_ENABLED' too
+	// hashing
+	'HASH_MEMORY_COST',
+	'HASH_LENGTH',
+	'HASH_TIME_COST',
+	'HASH_PARALLELISM',
+	'HASH_TYPE',
+	'HASH_ASSOCIATED_DATA',
+	// cors
+	'CORS_ENABLED',
+	'CORS_ORIGIN',
+	'CORS_METHODS',
+	'CORS_ALLOWED_HEADERS',
+	'CORS_EXPOSED_HEADERS',
+	'CORS_CREDENTIALS',
+	'CORS_MAX_AGE',
+	// rate limiting
+	'RATE_LIMITER_ENABLED',
+	'RATE_LIMITER_POINTS',
+	'RATE_LIMITER_DURATION',
+	'RATE_LIMITER_STORE',
+	'RATE_LIMITER_REDIS',
+	'RATE_LIMITER_REDIS_HOST',
+	'RATE_LIMITER_REDIS_PORT',
+	'RATE_LIMITER_REDIS_PASSWORD',
+	'RATE_LIMITER_MEMCACHE',
+	// cache
+	'CACHE_ENABLED',
+	'CACHE_TTL',
+	'CACHE_CONTROL_S_MAXAGE',
+	'CACHE_AUTO_PURGE',
+	'CACHE_SYSTEM_TTL',
+	'CACHE_SCHEMA',
+	'CACHE_PERMISSIONS',
+	'CACHE_NAMESPACE',
+	'CACHE_STORE',
+	'CACHE_STATUS_HEADER',
+	'CACHE_REDIS',
+	'CACHE_REDIS_HOST',
+	'CACHE_REDIS_PORT',
+	'CACHE_REDIS_PASSWORD',
+	'CACHE_MEMCACHE',
+	// storage
+	'STORAGE_LOCATIONS',
+	'STORAGE_.+_DRIVER',
+	'STORAGE_.+_ROOT',
+	'STORAGE_.+_KEY',
+	'STORAGE_.+_SECRET',
+	'STORAGE_.+_BUCKET',
+	'STORAGE_.+_REGION',
+	'STORAGE_.+_ENDPOINT',
+	'STORAGE_.+_ACL',
+	'STORAGE_.+_CONTAINER_NAME',
+	'STORAGE_.+_ACCOUNT_NAME',
+	'STORAGE_.+_ACCOUNT_KEY',
+	'STORAGE_.+_ENDPOINT',
+	'STORAGE_.+_KEY_FILENAME',
+	'STORAGE_.+_BUCKET',
+	// metadata
+	'FILE_METADATA_ALLOW_LIST',
+	// assets
+	'ASSETS_CACHE_TTL',
+	'ASSETS_TRANSFORM_MAX_CONCURRENT',
+	'ASSETS_TRANSFORM_IMAGE_MAX_DIMENSION',
+	'ASSETS_TRANSFORM_MAX_OPERATIONS',
+	'ASSETS_CONTENT_SECURITY_POLICY',
+	// auth
+	'AUTH_PROVIDERS',
+	'AUTH_DISABLE_DEFAULT',
+	'AUTH_.+_DRIVER',
+	'AUTH_.+_CLIENT_ID',
+	'AUTH_.+_CLIENT_SECRET',
+	'AUTH_.+_SCOPE',
+	'AUTH_.+_AUTHORIZE_URL',
+	'AUTH_.+_ACCESS_URL',
+	'AUTH_.+_PROFILE_URL',
+	'AUTH_.+_IDENTIFIER_KEY',
+	'AUTH_.+_EMAIL_KEY',
+	'AUTH_.+_FIRST_NAME_KEY',
+	'AUTH_.+_LAST_NAME_KEY',
+	'AUTH_.+_ALLOW_PUBLIC_REGISTRATION',
+	'AUTH_.+_DEFAULT_ROLE_ID',
+	'AUTH_.+_ICON',
+	'AUTH_.+_PARAMS',
+	'AUTH_.+_ISSUER_URL',
+	'AUTH_.+_AUTH_REQUIRE_VERIFIED_EMAIL',
+	'AUTH_.+_CLIENT_URL',
+	'AUTH_.+_BIND_DN',
+	'AUTH_.+_BIND_PASSWORD',
+	'AUTH_.+_USER_DN',
+	'AUTH_.+_USER_ATTRIBUTE',
+	'AUTH_.+_USER_SCOPE',
+	'AUTH_.+_MAIL_ATTRIBUTE',
+	'AUTH_.+_FIRST_NAME_ATTRIBUTE',
+	'AUTH_.+_LAST_NAME_ATTRIBUTE',
+	'AUTH_.+_GROUP_DN',
+	'AUTH_.+_GROUP_ATTRIBUTE',
+	'AUTH_.+_GROUP_SCOPE',
+	// extensions
+	'EXTENSIONS_PATH',
+	'EXTENSIONS_AUTO_RELOAD',
+	// emails
+	'EMAIL_FROM',
+	'EMAIL_TRANSPORT',
+	'EMAIL_SENDMAIL_NEW_LINE',
+	'EMAIL_SENDMAIL_PATH',
+	'EMAIL_SMTP_HOST',
+	'EMAIL_SMTP_PORT',
+	'EMAIL_SMTP_USER',
+	'EMAIL_SMTP_PASSWORD',
+	'EMAIL_SMTP_POOL',
+	'EMAIL_SMTP_SECURE',
+	'EMAIL_SMTP_IGNORE_TLS',
+	'EMAIL_MAILGUN_API_KEY',
+	'EMAIL_MAILGUN_DOMAIN',
+	'EMAIL_MAILGUN_HOST',
+	'EMAIL_SES_CREDENTIALS__ACCESS_KEY_ID',
+	'EMAIL_SES_CREDENTIALS__SECRET_ACCESS_KEY',
+	'EMAIL_SES_REGION',
+	// admin account
+	'ADMIN_EMAIL',
+	'ADMIN_PASSWORD',
+	// telemetry
+	'TELEMETRY',
+	// limits & optimization
+	'RELATIONAL_BATCH_SIZE',
+	'EXPORT_BATCH_SIZE',
+].map((name) => new RegExp(`^${name}$`));
+
 const acceptedEnvTypes = ['string', 'number', 'regex', 'array', 'json'];
 
 const defaults: Record<string, any> = {
@@ -224,19 +398,21 @@ function processValues(env: Record<string, any>) {
 	for (let [key, value] of Object.entries(env)) {
 		// If key ends with '_FILE', try to get the value from the file defined in this variable
 		// and store it in the variable with the same name but without '_FILE' at the end
-		let newKey;
+		let newKey: string | undefined;
 		if (key.length > 5 && key.endsWith('_FILE')) {
 			newKey = key.slice(0, -5);
-			if (newKey in env) {
-				throw new Error(
-					`Duplicate environment variable encountered: you can't use "${newKey}" and "${key}" simultaneously.`
-				);
-			}
-			try {
-				value = fs.readFileSync(value, { encoding: 'utf8' });
-				key = newKey;
-			} catch {
-				throw new Error(`Failed to read value from file "${value}", defined in environment variable "${key}".`);
+			if (allowedEnvironmentVars.some((pattern) => pattern.test(newKey as string))) {
+				if (newKey in env) {
+					throw new Error(
+						`Duplicate environment variable encountered: you can't use "${newKey}" and "${key}" simultaneously.`
+					);
+				}
+				try {
+					value = fs.readFileSync(value, { encoding: 'utf8' });
+					key = newKey;
+				} catch {
+					throw new Error(`Failed to read value from file "${value}", defined in environment variable "${key}".`);
+				}
 			}
 		}
 
@@ -263,7 +439,7 @@ function processValues(env: Record<string, any>) {
 					env[key] = tryJSON(value);
 					break;
 				case 'boolean':
-					env[key] = value === 'true' || value === true || value === '1' || value === 1;
+					env[key] = toBoolean(value);
 			}
 			continue;
 		}
@@ -321,4 +497,8 @@ function tryJSON(value: any) {
 	} catch {
 		return value;
 	}
+}
+
+function toBoolean(value: any): boolean {
+	return value === 'true' || value === true || value === '1' || value === 1;
 }
