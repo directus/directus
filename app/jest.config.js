9b2cae634e128daa9d564141f783a88917369978
jaycammarano <jay.cammarano@gmail.com>
2022-05-27 19:40:27 +0000

    added '@' to modules mapper

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+const base = require('../jest.config.js');
+
+require('dotenv').config();
+
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
+	...base,
+	moduleNameMapper: {
+		...base.moduleNameMapper,
+		'@/(.*)$': `${__dirname}/src/$1`,
+	},
 };
