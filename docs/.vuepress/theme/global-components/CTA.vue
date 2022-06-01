8e4c365b6e9baae04e4bf7b1dbbc26a8bfed0350
Eron Powell <erondpowell@gmail.com>
2022-06-01 18:34:31 -0500

    bare min btn outline

---
+++ docs/.vuepress/theme/global-components/CTA.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<div class="border">
+			<h3 class="cta-title">Title</h3>
+			<p class="cta-text">Ready to do XYZ?</p>
+			<button class="cta-button">Click Me!</button>
+		</div>
+	</div>
+</template>
+
+<script>
+// TODO: replace hard text in the h3, p, and button with input params and fallback values
+</script>
+
+<style lang='stylus' scoped>
+
+.border
+	border 1px solid var(--primary)
+	border-radius 6px
+	background-color var(--background-subdued)
+	padding 2%
+
+</style>
