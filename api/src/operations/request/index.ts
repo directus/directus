d1b7d3e66634e1eb8b9135a5e196fc436c9f63ff
Rijk van Zanten <rijkvanzanten@me.com>
2022-06-06 16:02:47 -0400

    Fix formatting of headers in request operation (#13759)
    Fixes #13737

--- api/src/operations/request/index.ts
+++ api/src/operations/request/index.ts
@@ -5,14 +5,19 @@ type Options = {
 	url: string;
 	method: Method;
 	body: Record<string, any> | string | null;
-	headers: Record<string, string>;
+	headers?: { header: string; value: string }[] | null;
 };
 
 export default defineOperationApi<Options>({
 	id: 'request',
 
 	handler: async ({ url, method, body, headers }) => {
-		const result = await axios({ url, method, data: body, headers });
+		const customHeaders = headers?.reduce((acc, { header, value }) => {
+			acc[header] = value;
+			return acc;
+		}, {} as Record<string, string>);
+
+		const result = await axios({ url, method, data: body, headers: customHeaders });
 
 		return { status: result.status, statusText: result.statusText, headers: result.headers, data: result.data };
 	},
