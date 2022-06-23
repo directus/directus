dd00dfbbfd6e9c9a9c38b8878f07fdecf2ef0416
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-06-23 23:15:38 +0800

    Allow admin to import into system collections (#14065)
    * allow admin to import into system collections
    
    * add refresh event after import to relevant pages
    
    * make sure it's limited to admin users

--- api/src/services/import-export.ts
+++ api/src/services/import-export.ts
@@ -39,7 +39,7 @@ export class ImportService {
 	}
 
 	async import(collection: string, mimetype: string, stream: NodeJS.ReadableStream): Promise<void> {
-		if (collection.startsWith('directus_')) throw new ForbiddenException();
+		if (this.accountability?.admin !== true && collection.startsWith('directus_')) throw new ForbiddenException();
 
 		const createPermissions = this.accountability?.permissions?.find(
 			(permission) => permission.collection === collection && permission.action === 'create'
