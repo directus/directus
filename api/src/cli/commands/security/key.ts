f385ecb4423ba4620a12e72e683d672b368bab20
danilopolani <danilo.polani@gmail.com>
2022-07-08 21:20:44 +0200

    return key generation directly in output

--- api/src/cli/commands/security/key.ts
+++ api/src/cli/commands/security/key.ts
@@ -1,8 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export default async function generateKey(): Promise<void> {
-	const key = uuidv4();
-
-	process.stdout.write(`Your new app key: ${key}\n`);
+	process.stdout.write(uuidv4());
 	process.exit(0);
 }
