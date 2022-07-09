0ab9bc1f060e5399d65c262d23c795a6ac7e7c91
rijkvanzanten <rijkvanzanten@me.com>
2022-07-09 14:50:02 +0200

    Remove sdk from jest config

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,6 @@ module.exports = {
 		'@directus/drive(.*)$': `${__dirname}/packages/drive/src/$1`,
 		'@directus/extension-sdk(.*)$': `${__dirname}/packages/extension-sdk/src/$1`,
 		'@directus/schema(.*)$': `${__dirname}/packages/schema/src/$1`,
-		'@directus/sdk(.*)$': `${__dirname}/packages/sdk/src/$1`,
 		'@directus/shared(.*)$': `${__dirname}/packages/shared/src/$1`,
 		'@directus/specs(.*)$': `${__dirname}/packages/specs/$1`,
 	},
