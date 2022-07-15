dee6c9a050a0192ba076ec9c4a6f07f259351b47
Clayton Gulick <claytongulick@gmail.com>
2022-07-15 17:07:59 -0500

    support live reload during development for external extensions

--- api/src/controllers/extensions.ts
+++ api/src/controllers/extensions.ts
@@ -5,6 +5,10 @@ import { getExtensionManager } from '../extensions';
 import { respond } from '../middleware/respond';
 import { depluralize, isAppExtension } from '@directus/shared/utils';
 import { Plural } from '@directus/shared/types';
+import env from '../env';
+import fs from 'fs';
+import util from 'util';
+import path from 'path';
 
 const router = Router();
 
@@ -29,6 +33,23 @@ router.get(
 	}),
 	respond
 );
+
+//support direct serving of external app extensions during development
+if (env.NODE_ENV === 'development') {
+	router.get(
+		'/app/index.js',
+		asyncHandler(async (req, res) => {
+			const readFile = util.promisify(fs.readFile);
+			const externalExtensionsPath = path.join(env.EXTENSIONS_PATH, 'app', 'index.js');
+			const extensionSource = await readFile(externalExtensionsPath, 'utf8');
+
+			res.setHeader('Content-Type', 'application/javascript; charset=UTF-8');
+			res.setHeader('Cache-Control', 'no-store');
+			res.setHeader('Vary', 'Origin, Cache-Control');
+			res.end(extensionSource);
+		})
+	);
+}
 
 router.get(
 	'/:type/index.js',
