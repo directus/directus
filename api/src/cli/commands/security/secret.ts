1780a9dc1a6dec03f28380b6dfa99abffa3abbcc
rijkvanzanten <rijkvanzanten@me.com>
2022-07-20 16:43:52 -0400

    Write secret directly

--- api/src/cli/commands/security/secret.ts
+++ api/src/cli/commands/security/secret.ts
@@ -1,8 +1,6 @@
 import { nanoid } from 'nanoid';
 
 export default async function generateSecret(): Promise<void> {
-	const SECRET = nanoid(32);
-
-	process.stdout.write(`Your new app secret: ${SECRET}\n`);
+	process.stdout.write(nanoid(32));
 	process.exit(0);
 }
