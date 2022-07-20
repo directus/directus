7420ce1a7146ea2a7a79d29e5153c5d25a029ea5
rijkvanzanten <rijkvanzanten@me.com>
2022-07-20 14:42:18 -0400

    Convert v-media to script[setup]

--- app/src/components/v-media/v-media.vue
+++ app/src/components/v-media/v-media.vue
@@ -7,105 +7,96 @@
 			class="video-js vjs-big-play-centered media-player-theme"
 			:style="styleOverrides"
 			v-bind="attrsWithoutSrc"
-		></component>
+		/>
 	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, computed, onMounted } from 'vue';
+<script lang="ts" setup>
+import { ref, computed, onMounted, useAttrs } from 'vue';
 import { omit } from 'lodash';
 import videojs, { VideoJsPlayer, VideoJsPlayerOptions } from 'video.js';
 
+interface Props {
+	src: string;
+	mime: string;
+	options?: VideoJsPlayerOptions;
+	height?: string;
+	width?: string;
+	maxHeight?: string;
+	maxWidth?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	options: () => ({}),
+	height: undefined,
+	width: undefined,
+	maxHeight: undefined,
+	maxWidth: undefined,
+});
+
+defineEmits(['error']);
+const attrs = useAttrs();
+
+const mediaElement = ref<HTMLElement>();
+const mediaPlayer = ref<VideoJsPlayer>();
+
+const defaultOptions = {
+	autoplay: false,
+	controls: true,
+	fluid: true,
+	muted: false,
+	preload: 'none',
+	sources: [
+		{
+			src: props.src,
+			type: props.mime,
+		},
+	],
+	plugins: {},
+};
+
+const type = computed<'video' | 'audio' | null>(() => {
+	if (props.mime === null) return null;
+
+	if (props.mime.startsWith('video')) {
+		return 'video';
+	}
+
+	if (props.mime.startsWith('audio')) {
+		return 'audio';
+	}
+
+	return null;
+});
+
+const videoOptions = computed<VideoJsPlayerOptions>(() => {
+	const mergedOptions = Object.assign({}, defaultOptions, props.options);
+
+	return mergedOptions;
+});
+
+const styleOverrides = computed(() => {
+	return {
+		'--media-player-width': props.width,
+		'--media-player-height': props.height,
+		'--media-player-max-width': props.maxWidth,
+		'--media-player-max-height': props.maxHeight,
+	};
+});
+
+onMounted(() => {
+	if (!mediaElement.value) return;
+	mediaPlayer.value = videojs(mediaElement.value, videoOptions.value);
+});
+
+const attrsWithoutSrc = computed(() => omit(attrs, ['src']));
+</script>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
 export default defineComponent({
 	inheritAttrs: false,
-	props: {
-		src: {
-			type: String,
-			required: true,
-		},
-		mime: {
-			type: String,
-			required: true,
-		},
-		options: {
-			type: Object,
-			default: () => ({}),
-		},
-		height: {
-			type: String,
-			default: undefined,
-		},
-		width: {
-			type: String,
-			default: undefined,
-		},
-		maxHeight: {
-			type: String,
-			default: undefined,
-		},
-		maxWidth: {
-			type: String,
-			default: undefined,
-		},
-	},
-	emits: ['error'],
-	setup(props, { attrs }) {
-		const mediaElement = ref<HTMLElement>();
-		const mediaPlayer = ref<VideoJsPlayer>();
-
-		const defaultOptions = {
-			autoplay: false,
-			controls: true,
-			fluid: true,
-			muted: false,
-			preload: 'none',
-			sources: [
-				{
-					src: props.src,
-					type: props.mime,
-				},
-			],
-			plugins: {},
-		};
-
-		const type = computed<'video' | 'audio' | null>(() => {
-			if (props.mime === null) return null;
-
-			if (props.mime.startsWith('video')) {
-				return 'video';
-			}
-
-			if (props.mime.startsWith('audio')) {
-				return 'audio';
-			}
-
-			return null;
-		});
-
-		const videoOptions = computed<VideoJsPlayerOptions>(() => {
-			const mergedOptions = Object.assign({}, defaultOptions, props.options);
-
-			return mergedOptions;
-		});
-
-		const styleOverrides = computed(() => {
-			return {
-				'--media-player-width': props.width,
-				'--media-player-height': props.height,
-				'--media-player-max-width': props.maxWidth,
-				'--media-player-max-height': props.maxHeight,
-			};
-		});
-
-		onMounted(() => {
-			if (!mediaElement.value) return;
-			mediaPlayer.value = videojs(mediaElement.value, videoOptions.value);
-		});
-
-		const attrsWithoutSrc = computed(() => omit(attrs, ['src']));
-
-		return { mediaElement, type, styleOverrides, attrsWithoutSrc };
-	},
 });
 </script>
 
