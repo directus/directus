3f83a271fc079b59fc4afbeb213885449366963d
rijkvanzanten <rijkvanzanten@me.com>
2022-07-20 12:16:52 -0400

    Use image data based aspect ratio on preview

--- app/src/views/private/components/file-preview/file-preview.vue
+++ app/src/views/private/components/file-preview/file-preview.vue
@@ -53,12 +53,18 @@ const isSVG = computed(() => props.mime.includes('svg'));
 
 const maxHeight = computed(() => Math.min(props.height ?? 528, 528) + 'px');
 const isSmall = computed(() => props.height < 528);
+
+const aspectRatio = computed(() => {
+	if (!props.height || !props.width) return 1;
+	return props.width / props.height;
+});
 </script>
 
 <style lang="scss" scoped>
 .file-preview {
 	position: relative;
 	max-width: calc((var(--form-column-max-width) * 2) + var(--form-horizontal-gap));
+	aspect-ratio: v-bind(aspectRatio);
 
 	img,
 	video,
