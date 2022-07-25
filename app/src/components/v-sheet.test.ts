1d87d2e748a69086bb5ce96f57ebc45391a4d2b7
rijkvanzanten <rijkvanzanten@me.com>
2022-07-25 15:44:48 -0400

    Add whitespace

--- app/src/components/v-sheet.test.ts
+++ app/src/components/v-sheet.test.ts
@@ -5,10 +5,12 @@ import VSheet from './v-sheet.vue';
 
 test('Mount component', () => {
 	expect(VSheet).toBeTruthy();
+
 	const wrapper = mount(VSheet, {
 		slots: {
 			default: 'Slot Content',
 		},
 	});
+
 	expect(wrapper.html()).toMatchSnapshot();
 });
