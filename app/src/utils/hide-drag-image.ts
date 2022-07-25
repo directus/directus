516cbd87eb49a7dbc94643dddc7d7dbbd4a92977
rijkvanzanten <rijkvanzanten@me.com>
2022-07-25 15:15:23 -0400

    Add test coverage for hide-drag-image

--- app/src/utils/hide-drag-image.ts
+++ app/src/utils/hide-drag-image.ts
@@ -1,3 +1,6 @@
+/**
+ * Set the passed data transfer's drag image to an empty image object of 0x0 size
+ */
 export function hideDragImage(dataTransfer: DataTransfer): void {
 	const emptyImg = new Image();
 	dataTransfer.setDragImage(emptyImg, 0, 0);
