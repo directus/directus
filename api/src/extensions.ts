707ba0db21dd9d466f000d63c69d0d167f7601b4
rijkvanzanten <rijkvanzanten@me.com>
2022-07-28 14:58:58 -0400

    Support running prod locally through ./api/cli.js

--- api/src/extensions.ts
+++ api/src/extensions.ts
@@ -310,7 +310,9 @@ class ExtensionManager {
 	}
 
 	private async getSharedDepsMapping(deps: string[]) {
-		const appDir = await fse.readdir(path.join(resolvePackage('@directus/app'), 'dist', 'assets'));
+		const appDir = await fse.readdir(
+			path.join(resolvePackage('@directus/app', require.main?.filename), 'dist', 'assets')
+		);
 
 		const depsMapping: Record<string, string> = {};
 		for (const dep of deps) {
