527478723569768fcfbd39e35a98e3f6c6047106
rijkvanzanten <rijkvanzanten@me.com>
2022-08-03 19:27:55 -0400

    Add compressor function

---
+++ packages/shared/src/utils/compress.test.ts
@@ -0,0 +1,74 @@
+import { describe, test, expect } from 'vitest';
+import { compress, mapToSortedArray, encode, to36 } from './compress';
+
+const plain = {
+	string: 'directus',
+	true: true,
+	false: false,
+	null: null,
+	empty: '',
+	integer: 2022,
+	float: 12.34,
+	undefined: undefined,
+};
+
+const deep = {
+	another: plain,
+	nested: plain,
+	arr: [plain, plain],
+};
+
+const arr = [plain, deep];
+
+describe('compress', () => {
+	test('Compresses plain objects', () => {
+		expect(compress(plain)).toMatchInlineSnapshot(
+			'"string|directus|true|false|null|empty|integer|float|undefined^1K6^12.34^$0|1|2|-1|3|-2|4|-3|5|-4|6|9|7|A|8|-5]"'
+		);
+	});
+
+	test('Compresses deep nested objects', () => {
+		expect(compress(deep)).toMatchInlineSnapshot(
+			'"another|string|directus|true|false|null|empty|integer|float|undefined|nested|arr^1K6^12.34^$0|$1|2|3|-1|4|-2|5|-3|6|-4|7|C|8|D|9|-5]|A|$1|2|3|-1|4|-2|5|-3|6|-4|7|C|8|D|9|-5]|B|@$1|2|3|-1|4|-2|5|-3|6|-4|7|C|8|D|9|-5]|$1|2|3|-1|4|-2|5|-3|6|-4|7|C|8|D|9|-5]]]"'
+		);
+	});
+
+	test('Compresses array input', () => {
+		expect(compress(arr)).toMatchInlineSnapshot(
+			'"string|directus|true|false|null|empty|integer|float|undefined|another|nested|arr^1K6^12.34^@$0|1|2|-1|3|-2|4|-3|5|-4|6|C|7|D|8|-5]|$9|$0|1|2|-1|3|-2|4|-3|5|-4|6|C|7|D|8|-5]|A|$0|1|2|-1|3|-2|4|-3|5|-4|6|C|7|D|8|-5]|B|@$0|1|2|-1|3|-2|4|-3|5|-4|6|C|7|D|8|-5]|$0|1|2|-1|3|-2|4|-3|5|-4|6|C|7|D|8|-5]]]]"'
+		);
+	});
+
+	test('Throws error on non-supported types', () => {
+		expect(() => compress({ method: () => true })).toThrowError();
+	});
+});
+
+describe('mapToSortedArray', () => {
+	test('Returns map as array sorted by map values', () => {
+		expect(
+			mapToSortedArray(
+				new Map([
+					['b', 1],
+					['a', 0],
+					['c', 2],
+				])
+			)
+		).toEqual(['a', 'b', 'c']);
+	});
+});
+
+describe('encode', () => {
+	test('Encodes special characters used in compressed string to URL encoded', () => {
+		const input = 'hello + | ^ % end';
+		expect(encode(input)).toBe('hello+%2B+%7C+%5E+%25+end');
+	});
+});
+
+describe('to36', () => {
+	test('Converts base-10 number to capitalized base36 equivalent', () => {
+		expect(to36(1)).toBe('1');
+		expect(to36(1234)).toBe('YA');
+		expect(to36(10513)).toBe('841');
+	});
+});
---
+++ packages/shared/src/utils/compress.ts
@@ -0,0 +1,202 @@
+const enum Types {
+	NULL = 'null',
+	UNDEFINED = 'undefined',
+	STRING = 'string',
+	INTEGER = 'integer',
+	FLOAT = 'float',
+	BOOLEAN = 'boolean',
+	EMPTY = 'empty',
+}
+
+const enum Tokens {
+	'TRUE' = -1,
+	'FALSE' = -2,
+	'NULL' = -3,
+	'EMPTY' = -4,
+	'UNDEFINED' = -5,
+}
+
+type Token = {
+	type: Types;
+	index: number;
+};
+
+type NestedToken = ['@' | '$', ...(Token | NestedToken)[]];
+
+/**
+ * Compress an object or array to a smaller string representation by dropping the properties.
+ *
+ * @NOTE only works reliably on arrays of objects where each object has the same structure
+ */
+export function compress(obj: Record<string, any> | Record<string, any>[]) {
+	const strings = new Map<string, number>();
+	const integers = new Map<string, number>();
+	const floats = new Map<number, number>();
+
+	const getAst = (part: unknown | unknown[]): Token | NestedToken => {
+		if (part === null) {
+			return {
+				type: Types.NULL,
+				index: Tokens.NULL,
+			};
+		}
+
+		if (part === undefined) {
+			return {
+				type: Types.UNDEFINED,
+				index: Tokens.UNDEFINED,
+			};
+		}
+
+		if (Array.isArray(part)) {
+			return ['@', ...part.map((subPart) => getAst(subPart))];
+		}
+
+		if (typeof part === 'object') {
+			return [
+				'$',
+				...Object.entries(part)
+					.map(([key, value]) => [getAst(key), getAst(value)])
+					.flat(),
+			];
+		}
+
+		if (part === '') {
+			return {
+				type: Types.EMPTY,
+				index: Tokens.EMPTY,
+			};
+		}
+
+		if (typeof part === 'string') {
+			const value = encode(part);
+
+			if (strings.has(value)) {
+				return {
+					type: Types.STRING,
+					index: strings.get(value)!,
+				};
+			}
+
+			const index = strings.size;
+
+			strings.set(value, index);
+
+			return {
+				type: Types.STRING,
+				index,
+			};
+		}
+
+		if (typeof part === 'number' && Number.isInteger(part)) {
+			const value = to36(part);
+
+			if (integers.has(value)) {
+				return {
+					type: Types.INTEGER,
+					index: integers.get(value)!,
+				};
+			}
+
+			const index = integers.size;
+
+			integers.set(value, index);
+
+			return {
+				type: Types.INTEGER,
+				index,
+			};
+		}
+
+		if (typeof part === 'number') {
+			if (floats.has(part)) {
+				return {
+					type: Types.FLOAT,
+					index: floats.get(part)!,
+				};
+			}
+
+			const index = floats.size;
+
+			floats.set(part, index);
+
+			return {
+				type: Types.FLOAT,
+				index,
+			};
+		}
+
+		if (typeof part === 'boolean') {
+			return {
+				type: Types.BOOLEAN,
+				index: part ? Tokens.TRUE : Tokens.FALSE,
+			};
+		}
+
+		throw new Error(`Unexpected argument of type ${typeof part}`);
+	};
+
+	const ast = getAst(obj);
+
+	const getCompressed = (part: Token | NestedToken) => {
+		if (Array.isArray(part)) {
+			let compressed: string = part.shift() as '@' | '$';
+			part.forEach((subPart) => (compressed += getCompressed(subPart as Token) + '|'));
+			if (compressed.endsWith('|')) compressed = compressed.slice(0, -1);
+			return compressed + ']';
+		}
+
+		const { type, index } = part;
+
+		switch (type) {
+			case Types.STRING:
+				return to36(index);
+			case Types.INTEGER:
+				return to36(strings.size + index);
+			case Types.FLOAT:
+				return to36(strings.size + integers.size + index);
+			default:
+				return index;
+		}
+	};
+
+	let compressed = mapToSortedArray(strings).join('|');
+	compressed += '^' + mapToSortedArray(integers).join('|');
+	compressed += '^' + mapToSortedArray(floats).join('|');
+	compressed += '^' + getCompressed(ast);
+
+	return compressed;
+}
+
+export function mapToSortedArray(map: Map<string | number, number>): (string | number)[] {
+	const output: (string | number)[] = [];
+
+	map.forEach((index, value) => {
+		output[index] = value;
+	});
+
+	return output;
+}
+
+export function encode(str: string) {
+	return str.replace(/[+ |^%]/g, (a) => {
+		switch (a) {
+			case ' ':
+				return '+';
+			case '+':
+				return '%2B';
+			case '|':
+				return '%7C';
+			case '^':
+				return '%5E';
+			case '%':
+			default:
+				// The regex matches explicit, so this default shouldn't be hit
+				return '%25';
+		}
+	});
+}
+
+export function to36(num: number): string {
+	return num.toString(36).toUpperCase();
+}
