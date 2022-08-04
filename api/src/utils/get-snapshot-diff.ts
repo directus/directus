11abcd5200ced0b32e9a56788325781332be1c2e
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-08-04 17:49:24 +0800

    prevent updates to autoincrement fields' schema

--- api/src/utils/get-snapshot-diff.ts
+++ api/src/utils/get-snapshot-diff.ts
@@ -39,10 +39,16 @@ export function getSnapshotDiff(current: Snapshot, after: Snapshot): SnapshotDif
 						(afterField) => afterField.collection === currentField.collection && afterField.field === currentField.field
 					);
 
+					const isAutoIncrementPrimaryKey =
+						!!currentField.schema?.is_primary_key && !!currentField.schema?.has_auto_increment;
+
 					return {
 						collection: currentField.collection,
 						field: currentField.field,
-						diff: diff(sanitizeField(currentField), sanitizeField(afterField)),
+						diff: diff(
+							sanitizeField(currentField, isAutoIncrementPrimaryKey),
+							sanitizeField(afterField, isAutoIncrementPrimaryKey)
+						),
 					};
 				}),
 				...after.fields
@@ -116,38 +122,58 @@ export function getSnapshotDiff(current: Snapshot, after: Snapshot): SnapshotDif
 	return diffedSnapshot;
 }
 
+/**
+ * Omit certain database vendor specific collection properties that should not be compared when performing diff
+ *
+ * @param collection collection to sanitize
+ * @returns sanitized collection
+ *
+ * @see {@link https://github.com/knex/knex-schema-inspector/blob/master/lib/types/table.ts}
+ */
+
 function sanitizeCollection(collection: Collection | undefined) {
-	return collection
-		? omit(collection, [
-				// Not supported in SQLite + comment in MSSQL
-				'schema.comment',
-				'schema.schema',
+	const omittedPaths = [
+		// Not supported in SQLite + comment in MSSQL
+		'schema.comment',
+		'schema.schema',
 
-				// MySQL Only
-				'schema.collation',
-				'schema.engine',
+		// MySQL Only
+		'schema.collation',
+		'schema.engine',
 
-				// Postgres Only
-				'schema.owner',
+		// Postgres Only
+		'schema.owner',
 
-				// SQLite Only
-				'schema.sql',
+		// SQLite Only
+		'schema.sql',
 
-				//MSSQL only
-				'schema.catalog',
-		  ])
-		: collection;
+		//MSSQL only
+		'schema.catalog',
+	];
+
+	return collection ? omit(collection, omittedPaths) : collection;
 }
 
-function sanitizeField(field: Field | undefined) {
-	return field
-		? omit(field, [
+/**
+ * Omit certain database vendor specific field properties that should not be compared when performing diff
+ *
+ * @param field field to sanitize
+ * @param sanitizeAllSchema Whether or not the whole field schema should be sanitized. Mainly used to prevent modifying autoincrement fields
+ * @returns sanitized field
+ *
+ * @see {@link https://github.com/knex/knex-schema-inspector/blob/master/lib/types/column.ts}
+ */
+function sanitizeField(field: Field | undefined, sanitizeAllSchema = false) {
+	const omittedPaths = sanitizeAllSchema
+		? ['schema']
+		: [
 				// Not supported in SQLite or MSSQL
 				'schema.comment',
 
 				// Postgres Only
 				'schema.schema',
 				'schema.foreign_key_schema',
-		  ])
-		: field;
+		  ];
+
+	return field ? omit(field, omittedPaths) : field;
 }
