b35ab7973de5174f6ded80acc35d941667ead2c3
Gerard <gerardlamo@gmail.com>
2022-08-08 18:08:44 +0200

    Remove comment

--- packages/shared/src/utils/validate-payload.test.ts
+++ packages/shared/src/utils/validate-payload.test.ts
@@ -1,4 +1,4 @@
-//import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { Filter } from '../../src/types/filter';
 import { validatePayload } from './validate-payload';
 
