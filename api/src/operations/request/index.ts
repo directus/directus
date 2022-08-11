0377763f6d8a900d296f54780ffe1d7efee1d5ad
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-08-11 22:17:31 +0800

    Add Content-Type header when applicable

--- api/src/operations/request/index.ts
+++ api/src/operations/request/index.ts
@@ -1,4 +1,4 @@
-import { defineOperationApi } from '@directus/shared/utils';
+import { defineOperationApi, parseJSON } from '@directus/shared/utils';
 import axios, { Method } from 'axios';
 
 type Options = {
@@ -12,10 +12,15 @@ export default defineOperationApi<Options>({
 	id: 'request',
 
 	handler: async ({ url, method, body, headers }) => {
-		const customHeaders = headers?.reduce((acc, { header, value }) => {
-			acc[header] = value;
-			return acc;
-		}, {} as Record<string, string>);
+		const customHeaders =
+			headers?.reduce((acc, { header, value }) => {
+				acc[header] = value;
+				return acc;
+			}, {} as Record<string, string>) ?? {};
+
+		if (!customHeaders['Content-Type'] && isValidJSON(body)) {
+			customHeaders['Content-Type'] = 'application/json';
+		}
 
 		const shouldEncode = decodeURI(url) === url;
 		const result = await axios({
@@ -26,5 +31,14 @@ export default defineOperationApi<Options>({
 		});
 
 		return { status: result.status, statusText: result.statusText, headers: result.headers, data: result.data };
+
+		function isValidJSON(value: any): boolean {
+			try {
+				parseJSON(value);
+				return true;
+			} catch {
+				return false;
+			}
+		}
 	},
 });
