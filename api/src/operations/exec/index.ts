b344a85e64f88b8e96ec45e612f9ca9c71d0d1c1
rijkvanzanten <rijkvanzanten@me.com>
2022-08-15 13:13:50 -0400

    WIP Remove npx-import experiment

--- api/src/operations/exec/index.ts
+++ api/src/operations/exec/index.ts
@@ -8,16 +8,10 @@ type Options = {
 export default defineOperationApi<Options>({
 	id: 'exec',
 	handler: async ({ code }, { data }) => {
-		// This a very nasty workaround to get around TypeScript auto converting async imports to
-		// require.. npx-import is ESM only, so needs to be required through await import() in CJS
-		const { npxImport } = await (new Function('return import("npx-import")')() as Promise<typeof import('npx-import')>);
-
 		const vm = new NodeVM({
-			sandbox: {
-				npxImport,
-			},
 			eval: false,
 			wasm: false,
+			require: false,
 		});
 
 		let script;
@@ -31,8 +25,7 @@ export default defineOperationApi<Options>({
 
 		try {
 			const fn = await vm.run(script);
-			const res = await fn(data);
-			return res;
+			return await fn(data);
 		} catch (err) {
 			console.log(err);
 			throw new Error(`Couldn't run code.`);
