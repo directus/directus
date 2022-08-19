ea70dc8f9deb84bcbe5a398f60a24109b6aa2eb0
brainslug <tim@brainslug.nl>
2022-08-19 15:02:37 +0200

    added test rendering a text field

--- app/src/components/v-form/v-form.test.ts
+++ app/src/components/v-form/v-form.test.ts
@@ -3,10 +3,7 @@ import { GlobalMountOptions } from '@vue/test-utils/dist/types';
 import { mount } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
 import { createI18n } from 'vue-i18n';
-
-import { generateRouter } from '@/__utils__/router';
 import { Directive, h } from 'vue';
-import { Router } from 'vue-router';
 
 import VForm from './v-form.vue';
 
@@ -37,6 +34,47 @@ test('Mount component', () => {
 		global,
 		props: {
 			fields: [],
+			collection: null,
+			loading: false,
+		},
+		shallow: true,
+	});
+
+	expect(wrapper.html()).toMatchSnapshot();
+});
+
+test('Render text field', () => {
+	// this correctly fails for the issue introduced by https://github.com/directus/directus/pull/15144
+	const wrapper = mount(VForm, {
+		global,
+		props: {
+			fields: [
+				{
+					collection: 'test',
+					field: 'test',
+					type: 'string',
+					meta: {
+						collection: 'test',
+						field: 'test',
+						special: null,
+						interface: 'input',
+						options: null,
+						display: null,
+						display_options: null,
+						readonly: false,
+						hidden: false,
+						sort: null,
+						width: 'full',
+						translations: null,
+						note: null,
+						conditions: null,
+						required: false,
+						group: null,
+						validation: null,
+						validation_message: null,
+					},
+				},
+			],
 			collection: null,
 			loading: false,
 		},
