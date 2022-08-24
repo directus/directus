8a1995dfa053c8b60622f6b58ca9652ca212233d
ian <licitdev@gmail.com>
2022-08-24 15:47:04 +0800

    Increase default stall time to 500ms

--- api/src/env.ts
+++ api/src/env.ts
@@ -199,7 +199,7 @@ const defaults: Record<string, any> = {
 	REFRESH_TOKEN_COOKIE_SAME_SITE: 'lax',
 	REFRESH_TOKEN_COOKIE_NAME: 'directus_refresh_token',
 
-	LOGIN_STALL_TIME: 300,
+	LOGIN_STALL_TIME: 500,
 
 	ROOT_REDIRECT: './admin',
 
