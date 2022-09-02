8969ad166ecf60fa762562cddd79bb37a7323008
Chirica Gheorghe <gheorghi.chirica@crawless.com>
2022-09-02 15:22:32 +0300

    Add global search extension

--- custom_extensions.sh
+++ custom_extensions.sh
@@ -7,15 +7,11 @@ DIRECTUS_DIR="/directus/api"
 
 mkdir -p ${DIRECTUS_DIR}/displays/fields-action
 wget -O ${DIRECTUS_DIR}/displays/fields-action/index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.1.1/display-index.js
-
 mkdir -p ${DIRECTUS_DIR}/interfaces/fields-action
 wget -O ${DIRECTUS_DIR}/interfaces/fields-action/index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.1.1/interface-index.js
 
 # Install https://github.com/u12206050/directus-extension-global-search
 
-#git clone https://github.com/u12206050/directus-extension-global-search /tmp/global-search
-#cd /tmp/global-search
-#npm install
-#npm run build
-#mkdir -p ${DIRECTUS_DIR}/extensions/modules/global-search
-#cp /tmp/global-search/dist/index.js ${DIRECTUS_DIR}/extensions/modules/global-search/index.js
+mkdir -p ${DIRECTUS_DIR}/extensions/modules
+wget -O /tmp/global-search.zip https://github.com/u12206050/directus-extension-global-search/releases/download/1.0.0/global-search.zip
+unzip -d ${DIRECTUS_DIR}/extensions/modules/ /tmp/global-search.zip
