b1446f0542e2b7cf395e6f2c6fdd8585f9426cfa
ksoltani <k.soltani@groupe-projex.fr>
2022-09-02 16:45:53 +0200

    dynamic origin routing - first commit

--- api/src/middleware/cors.ts
+++ api/src/middleware/cors.ts
@@ -6,7 +6,13 @@ let corsMiddleware: RequestHandler = (req, res, next) => next();
 
 if (env.CORS_ENABLED === true) {
 	corsMiddleware = cors({
-		origin: env.CORS_ORIGIN || true,
+		origin: function (origin, callback) {
+			if (!origin || (typeof env.CORS_ORIGIN == 'string' && env.CORS_ORIGIN == origin) || (Array.isArray(env.CORS_ORIGIN) && env.CORS_ORIGIN.indexOf(origin) !== -1)) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
 		methods: env.CORS_METHODS || 'GET,POST,PATCH,DELETE',
 		allowedHeaders: env.CORS_ALLOWED_HEADERS,
 		exposedHeaders: env.CORS_EXPOSED_HEADERS,
