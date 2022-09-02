c9ef544847315ab532d2bff2ffa72cb41daf35a1
Chirica Gheorghe <gheorghi.chirica@crawless.com>
2022-09-02 15:15:05 +0300

    Fix popd

--- custom_extensions.sh
+++ custom_extensions.sh
@@ -6,11 +6,16 @@ DIRECTUS_DIR="/directus/api"
 # Install https://github.com/utomic-media/directus-extension-field-actions
 
 mkdir -p ${DIRECTUS_DIR}/displays/fields-action
-pushd ${DIRECTUS_DIR}/displays/fields-action
-	wget -O index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.1.1/display-index.js
-popd
+wget -O ${DIRECTUS_DIR}/displays/fields-action/index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.1.1/display-index.js
 
 mkdir -p ${DIRECTUS_DIR}/interfaces/fields-action
-pushd ${DIRECTUS_DIR}/interfaces/fields-action
-	wget -O index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.1.1/interface-index.js
-popd
+wget -O ${DIRECTUS_DIR}/interfaces/fields-action/index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.1.1/interface-index.js
+
+# Install https://github.com/u12206050/directus-extension-global-search
+
+#git clone https://github.com/u12206050/directus-extension-global-search /tmp/global-search
+#cd /tmp/global-search
+#npm install
+#npm run build
+#mkdir -p ${DIRECTUS_DIR}/extensions/modules/global-search
+#cp /tmp/global-search/dist/index.js ${DIRECTUS_DIR}/extensions/modules/global-search/index.js
