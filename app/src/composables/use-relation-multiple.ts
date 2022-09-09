d7f5729ce571d24281c09f82fbc04b88a77fa6b9
Nitwel <nitwel@arcor.de>
2022-09-09 16:27:02 +0200

    fix duplicate indexes

--- app/src/composables/use-relation-multiple.ts
+++ app/src/composables/use-relation-multiple.ts
@@ -91,8 +91,16 @@ export function useRelationMultiple(
 		const info = relation.value;
 		if (info?.type === undefined) return [];
 
-		if (info.type === 'o2m') return _value.value.create;
-		return _value.value.create.filter((item) => item[info.reverseJunctionField.field] === undefined);
+		const items = _value.value.create.map((item, index) => {
+			return {
+				...item,
+				$type: 'created',
+				$index: index,
+			} as DisplayItem;
+		})
+
+		if (info.type === 'o2m') return items;
+		return items.filter((item) => item[info.reverseJunctionField.field] === undefined);
 	});
 
 	const displayItems = computed(() => {
@@ -161,13 +169,7 @@ export function useRelationMultiple(
 
 		items.push(
 			...selectedOnPage,
-			...newItems.map((item, index) => {
-				return {
-					...item,
-					$type: 'created',
-					$index: index,
-				} as DisplayItem;
-			})
+			...newItems
 		);
 
 		const sortField = relation.value.sortField;
