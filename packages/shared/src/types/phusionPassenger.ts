d7965b170e4f594787cc0cc927497fc0c27a0cd2
Lukas Schätzle <lukas-schaetzle@web.de>
2022-09-09 21:58:18 +0200

    Declare Passenger locally

--- packages/shared/src/types/phusionPassenger.ts
+++ packages/shared/src/types/phusionPassenger.ts
@@ -1,13 +1,9 @@
 import { EventEmitter } from 'events';
 
-interface PhusionPassenger extends EventEmitter {
+export interface PhusionPassenger extends EventEmitter {
 	options: {
 		passenger_version: string;
 		start_command: string;
 		startup_file: string;
 	};
-}
-
-declare global {
-	const PhusionPassenger: PhusionPassenger | undefined;
 }
--- packages/shared/src/utils/node/get-entry-file.ts
+++ packages/shared/src/utils/node/get-entry-file.ts
@@ -1,3 +1,7 @@
+import { PhusionPassenger } from '../../types';
+
+declare const PhusionPassenger: PhusionPassenger | undefined;
+
 export function getEntryFile(): string | undefined {
 	return PhusionPassenger !== undefined ? PhusionPassenger.options.startup_file : require.main?.filename;
 }
