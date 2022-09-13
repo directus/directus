081e60c3df85086482e6642e1449e22172e28419
Ludovic Sterlin <ludovic.sterlin@hinfact.com>
2022-09-13 20:11:36 +0200

    refactor: run pnpm format on main

--- app/.storybook/fix-actions.js
+++ app/.storybook/fix-actions.js
@@ -1,22 +1,20 @@
 import { action } from '@storybook/addon-actions';
 
 export function fix(args, argTypes, update) {
+	if (args === undefined) args = {};
 
-    if(args === undefined) args = {}
+	for (let type of Object.values(argTypes)) {
+		if (type.table.category !== 'events') continue;
 
-    for(let type of Object.values(argTypes)) {
-        if(type.table.category !== 'events') continue
+		if (type.name.startsWith('update:')) {
+			args[type.name] = (event) => {
+				// update({
+				//     [type.name.substring(7)]: event
+				// })
+				action(type.name)(event);
+			};
+		} else args[type.name] = action(type.name);
+	}
 
-        if(type.name.startsWith('update:')) {
-            args[type.name] = (event) => {
-                // update({
-                //     [type.name.substring(7)]: event
-                // })
-                action(type.name)(event)
-            }
-        } else
-            args[type.name] = action(type.name);
-    }
-
-    return {args}
+	return { args };
 }
