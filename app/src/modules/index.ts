1efdd5a705ad08e6c7fe235246b259052e82d41e
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2022-09-20 17:04:50 +0200

    Remove module getter

--- app/src/modules/index.ts
+++
@@ -1,9 +0,0 @@
-import { shallowRef, Ref } from 'vue';
-import { ModuleConfig } from '@directus/shared/types';
-
-const modulesRaw: Ref<ModuleConfig[]> = shallowRef([]);
-const modules: Ref<ModuleConfig[]> = shallowRef([]);
-
-export function getModules(): { modules: Ref<ModuleConfig[]>; modulesRaw: Ref<ModuleConfig[]> } {
-	return { modules, modulesRaw };
-}
