e602c257d3302099675c17d9a2b01338df4b4b15
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-09-22 21:04:37 +0800

    snapshot joi validation

--- api/src/services/schema.ts
+++ api/src/services/schema.ts
@@ -1,13 +1,57 @@
+import { TYPES } from '@directus/shared/constants';
 import { Accountability } from '@directus/shared/types';
 import { getSimpleHash } from '@directus/shared/utils';
+import Joi from 'joi';
 import { Knex } from 'knex';
 import { version as currentDirectusVersion } from '../../package.json';
+import { ALIAS_TYPES } from '../constants';
 import getDatabase from '../database';
-import { ForbiddenException } from '../exceptions';
+import { ForbiddenException, InvalidPayloadException } from '../exceptions';
 import { AbstractServiceOptions, Snapshot, SnapshotDiff } from '../types';
 import { applySnapshot } from '../utils/apply-snapshot';
 import { getSnapshot } from '../utils/get-snapshot';
 import { getSnapshotDiff } from '../utils/get-snapshot-diff';
+
+const snapshotJoiSchema = Joi.object({
+	version: Joi.number().required(),
+	directus: Joi.string().required(),
+	vendor: Joi.string().valid('mysql', 'postgres', 'cockroachdb', 'sqlite', 'oracle', 'mssql', 'redshift').optional(),
+	collections: Joi.array().items(
+		Joi.object({
+			collection: Joi.string(),
+			meta: Joi.any(),
+			schema: Joi.object({
+				name: Joi.string(),
+			}),
+		})
+	),
+	fields: Joi.array().items(
+		Joi.object({
+			collection: Joi.string(),
+			field: Joi.string(),
+			meta: Joi.any(),
+			schema: Joi.object({
+				default_value: Joi.any(),
+				max_length: [Joi.number(), Joi.string(), Joi.valid(null)],
+				is_nullable: Joi.bool(),
+			})
+				.unknown()
+				.allow(null),
+			type: Joi.string()
+				.valid(...TYPES, ...ALIAS_TYPES)
+				.allow(null),
+		})
+	),
+	relations: Joi.array().items(
+		Joi.object({
+			collection: Joi.string(),
+			field: Joi.string(),
+			meta: Joi.any(),
+			related_collection: Joi.any(),
+			schema: Joi.any(),
+		})
+	),
+});
 
 export class SchemaService {
 	knex: Knex;
@@ -42,6 +86,12 @@ export class SchemaService {
 
 	async diff(snapshot: Snapshot): Promise<SnapshotDiff | null> {
 		if (this.accountability?.admin !== true) throw new ForbiddenException();
+
+		const { error } = snapshotJoiSchema.validate(snapshot);
+
+		if (error) {
+			throw new InvalidPayloadException(error.message);
+		}
 
 		const currentSnapshot = await getSnapshot({ database: this.knex });
 		const diff = getSnapshotDiff(currentSnapshot, snapshot);
