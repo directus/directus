d4e11ec2867af7a40417d6cade247a1c1578cbe9
Nitwel <nitwel@arcor.de>
2022-09-23 10:46:14 +0200

    fix shared imports

--- packages/shared/src/utils/node/index.ts
+++ packages/shared/src/utils/node/index.ts
@@ -2,5 +2,5 @@ export * from './ensure-extension-dirs.js';
 export * from './generate-extensions-entry.js';
 export * from './get-extensions.js';
 export * from './list-folders.js';
-export * from './path-to-relative-url';
+export * from './path-to-relative-url.js';
 export * from './resolve-package.js';
