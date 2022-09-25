1374ac47ec47b4c6323ed6c901a1bb2d7d5f25f9
Chirica Gheorghe <gheorghi.chirica@crawless.com>
2022-09-25 10:20:30 +0300

    Update CI

--- payment_extensions.sh
+++ payment_extensions.sh
@@ -12,8 +12,7 @@ DIRECTUS_SNAPSHOTS="${DIRECTUS_DIR}/snapshots"
 mkdir -p ${DIRECTUS_SNAPSHOTS}
 
 # TODO, fix this download, how to download from a different project Packages Registry??
-#curl --header 'Private-Token: ${CI_JOB_TOKEN}' \
-#	https://gitlab.com/api/v4/projects/39544563/packages/generic/Releases/0.0.1/directus-payment-integration-release.zip
+#curl --header 'Private-Token: ${CI_JOB_TOKEN}' https://gitlab.com/api/v4/projects/39544563/packages/generic/Releases/0.0.1/directus-payment-integration-release.zip
 
 wget https://s3.eu-central-2.wasabisys.com/crwtests/directus-payment-integration-release.zip
 
