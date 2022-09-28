7b9d552ae4357104a836acb2f6c6fd65eabb58de
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-09-28 18:55:50 +0800

    update hash comparison logic for create/delete

--- api/src/services/schema.ts
+++ api/src/services/schema.ts
@@ -120,39 +120,93 @@ export class SchemaService {
 
 		if (payload.hash !== snapshotWithHash.hash) {
 			for (const diffCollection of payload.diff.collections) {
-				if (diffCollection.hash) {
+				const collection = diffCollection.collection;
+
+				if (diffCollection.diff[0]?.kind === 'N') {
+					const existingCollection = snapshotWithHash.collections.find(
+						(c) => c.collection === diffCollection.collection
+					);
+					if (existingCollection) {
+						throw new InvalidPayloadException(
+							`Provided diff is trying to create collection "${collection}" but it already exists. Please regenerate a new diff and try again.`
+						);
+					}
+				} else if (diffCollection.diff[0]?.kind === 'D') {
+					const existingCollection = snapshotWithHash.collections.find(
+						(c) => c.collection === diffCollection.collection
+					);
+					if (!existingCollection) {
+						throw new InvalidPayloadException(
+							`Provided diff is trying to delete collection "${collection}" but it does not exist. Please regenerate a new diff and try again.`
+						);
+					}
+				} else {
 					const matchingCollection = snapshotWithHash.collections.find((c) => c.hash === diffCollection.hash);
 					if (!matchingCollection) {
 						throw new InvalidPayloadException(
-							`Provided diff for collection "${diffCollection.collection}" does not match the current instance's collection, indicating it has changed after this diff was generated. Please regenerate a new diff and try again.`
+							`Provided diff to update collection "${collection}" does not match the current instance's collection, indicating it has changed after this diff was generated. Please regenerate a new diff and try again.`
 						);
 					}
 				}
 			}
 			for (const diffField of payload.diff.fields) {
-				if (diffField.hash) {
+				const field = `${diffField.collection}.${diffField.field}`;
+
+				if (diffField.diff[0]?.kind === 'N') {
+					const existingField = snapshotWithHash.fields.find(
+						(f) => f.collection === diffField.collection && f.field === diffField.field
+					);
+					if (existingField) {
+						throw new InvalidPayloadException(
+							`Provided diff is trying to create field "${field}" but it already exists. Please regenerate a new diff and try again.`
+						);
+					}
+				} else if (diffField.diff[0]?.kind === 'D') {
+					const existingField = snapshotWithHash.fields.find(
+						(f) => f.collection === diffField.collection && f.field === diffField.field
+					);
+					if (!existingField) {
+						throw new InvalidPayloadException(
+							`Provided diff is trying to delete field "${field}" but it does not exist. Please regenerate a new diff and try again.`
+						);
+					}
+				} else {
 					const matchingField = snapshotWithHash.fields.find((f) => f.hash === diffField.hash);
 					if (!matchingField) {
 						throw new InvalidPayloadException(
-							`Provided diff for field "${diffField.collection}.${diffField.field}" does not match the current instance's field, indicating it has changed after this diff was generated. Please regenerate a new diff and try again.`
+							`Provided diff to update field "${field}" does not match the current instance's field, indicating it has changed after this diff was generated. Please regenerate a new diff and try again.`
 						);
 					}
 				}
 			}
 			for (const diffRelation of payload.diff.relations) {
-				if (diffRelation.hash) {
+				let relation = `${diffRelation.collection}.${diffRelation.field}`;
+				if (diffRelation.related_collection) relation += `-> ${diffRelation.related_collection}`;
+
+				if (diffRelation.diff[0]?.kind === 'N') {
+					const existingRelation = snapshotWithHash.relations.find(
+						(r) => r.collection === diffRelation.collection && r.field === diffRelation.field
+					);
+					if (existingRelation) {
+						throw new InvalidPayloadException(
+							`Provided diff is trying to create relation "${relation}" but it already exists. Please regenerate a new diff and try again.`
+						);
+					}
+				} else if (diffRelation.diff[0]?.kind === 'D') {
+					const existingRelation = snapshotWithHash.relations.find(
+						(r) => r.collection === diffRelation.collection && r.field === diffRelation.field
+					);
+					if (!existingRelation) {
+						throw new InvalidPayloadException(
+							`Provided diff is trying to delete relation "${relation}" but it does not exist. Please regenerate a new diff and try again.`
+						);
+					}
+				} else {
 					const matchingRelation = snapshotWithHash.relations.find((r) => r.hash === diffRelation.hash);
 					if (!matchingRelation) {
-						// Related collection doesn't exist for a2o relationship types
-						if (diffRelation.related_collection) {
-							throw new InvalidPayloadException(
-								`Provided diff for relation "${diffRelation.collection}.${diffRelation.field} -> ${diffRelation.related_collection}" does not match the current instance's relation, indicating it has changed after this diff was generated. Please regenerate a new diff and try again.`
-							);
-						} else {
-							throw new InvalidPayloadException(
-								`Provided diff for relation "${diffRelation.collection}.${diffRelation.field}" does not match the current instance's relation, indicating it has changed after this diff was generated. Please regenerate a new diff and try again.`
-							);
-						}
+						throw new InvalidPayloadException(
+							`Provided diff for relation "${relation}" does not match the current instance's relation, indicating it has changed after this diff was generated. Please regenerate a new diff and try again.`
+						);
 					}
 				}
 			}
