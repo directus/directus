1ea86608b5c8682d4d4beaa8196219f735171d78
Jonathan Schneider <jonathan@jott.media>
2022-09-30 15:43:36 +0200

    add mail templates to flow (proposal)

--- api/src/operations/mail/index.ts
+++ api/src/operations/mail/index.ts
@@ -1,24 +1,32 @@
 import { defineOperationApi } from '@directus/shared/utils';
-import { MailService } from '../../services';
+import { MailService, EmailOptions } from '../../services';
 import { md } from '../../utils/md';
 
 type Options = {
-	body: string;
+	body?: string;
+	template?: string;
+	data?: Record<string, any>;
 	to: string;
-	type: 'wysiwyg' | 'markdown';
+	type: 'wysiwyg' | 'markdown' | 'template';
 	subject: string;
 };
 
 export default defineOperationApi<Options>({
 	id: 'mail',
 
-	handler: async ({ body, to, type, subject }, { accountability, database, getSchema }) => {
+	handler: async ({ body, template, data, to, type, subject }, { accountability, database, getSchema }) => {
 		const mailService = new MailService({ schema: await getSchema({ database }), accountability, knex: database });
+		const mailObject: EmailOptions = { to, subject };
 
-		await mailService.send({
-			html: type === 'wysiwyg' ? body : md(body),
-			to,
-			subject,
-		});
+		if (type === 'template') {
+			mailObject.template = {
+				name: template || 'base',
+				data: data || {},
+			};
+		} else {
+			mailObject.html = type === 'wysiwyg' ? body || '' : md(body || '');
+		}
+
+		await mailService.send(mailObject);
 	},
 });
