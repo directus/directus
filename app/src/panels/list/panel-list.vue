4d5037f3c3a8e4829e2df12395ddc941298d804d
brainslug <tim@brainslug.nl>
2022-10-10 14:37:27 +0200

    re-introducing the item drawer for list-items

--- app/src/panels/list/panel-list.vue
+++ app/src/panels/list/panel-list.vue
@@ -2,24 +2,42 @@
 	<div class="list" :class="{ 'has-header': showHeader }">
 		<div>
 			<v-list>
-				<v-list-item v-for="row in data" :key="row[primaryKeyField.field]" class="selectable">
+				<v-list-item
+					v-for="row in data"
+					:key="row[primaryKeyField]"
+					class="selectable"
+					clickable
+					@click="startEditing(row)"
+				>
 					<render-template :item="row" :collection="collection" :template="displayTemplate" />
 					<div class="spacer" />
 				</v-list-item>
 			</v-list>
 		</div>
+		<drawer-item
+			:active="!!currentlyEditing"
+			:collection="collection"
+			:primary-key="currentlyEditing ?? '+'"
+			:edits="editsAtStart"
+			@input="saveEdits"
+			@update:active="cancelEdit"
+		/>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+import api from '@/api';
 import { useFieldsStore } from '@/stores/fields';
-import { computed } from 'vue';
+import { useInsightsStore } from '@/stores/insights';
+import { unexpectedError } from '@/utils/unexpected-error';
 
 const props = withDefaults(
 	defineProps<{
 		showHeader?: boolean;
 		displayTemplate?: string;
 		collection: string;
+		dashboard: string;
 		data?: object;
 	}>(),
 	{
@@ -30,9 +48,30 @@ const props = withDefaults(
 	}
 );
 
-const fieldsStore = useFieldsStore();
+const currentlyEditing = ref<number | string>();
+const editsAtStart = ref<Record<string, any>>();
 
-const primaryKeyField = computed(() => fieldsStore.getPrimaryKeyFieldForCollection(props.collection));
+const fieldsStore = useFieldsStore();
+const insightsStore = useInsightsStore();
+
+const primaryKeyField = computed(() => fieldsStore.getPrimaryKeyFieldForCollection(props.collection)?.field ?? 'id');
+
+function startEditing(item: Record<string, any>) {
+	currentlyEditing.value = item[primaryKeyField.value];
+	editsAtStart.value = item;
+}
+function cancelEdit() {
+	editsAtStart.value = undefined;
+	currentlyEditing.value = undefined;
+}
+async function saveEdits(item: Record<string, any>) {
+	try {
+		await api.patch(`/items/${props.collection}/${currentlyEditing.value}`, item);
+	} catch (err: any) {
+		unexpectedError(err);
+	}
+	await insightsStore.refresh(props.dashboard);
+}
 </script>
 
 <style scoped>
