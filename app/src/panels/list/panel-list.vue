af8eabf2716f96d74e2940567e4c27aea01a6fab
Brainslug <br41nslug@users.noreply.github.com>
2022-10-11 19:40:45 +0200

    Insights dashboard - List panels link to listed collection items. (#15930)
    * extra checks for valid input
    
    * re-introducing the item drawer for list-items
    
    * making list panel drawer optional
    
    * Cleanup for consistency
    
    Co-authored-by: rijkvanzanten <rijkvanzanten@me.com>

--- app/src/panels/list/panel-list.vue
+++ app/src/panels/list/panel-list.vue
@@ -2,37 +2,82 @@
 	<div class="list" :class="{ 'has-header': showHeader }">
 		<div>
 			<v-list>
-				<v-list-item v-for="row in data" :key="row[primaryKeyField.field]" class="selectable">
+				<v-list-item
+					v-for="row in data"
+					:key="row[primaryKeyField]"
+					class="selectable"
+					:clickable="linkToItem === true"
+					@click="startEditing(row)"
+				>
 					<render-template :item="row" :collection="collection" :template="displayTemplate" />
 					<div class="spacer" />
 				</v-list-item>
 			</v-list>
 		</div>
+		<drawer-item
+			:active="!!currentlyEditing"
+			:collection="collection"
+			:primary-key="currentlyEditing ?? '+'"
+			:edits="editsAtStart"
+			@input="saveEdits"
+			@update:active="cancelEdit"
+		/>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+import api from '@/api';
 import { useFieldsStore } from '@/stores/fields';
-import { computed } from 'vue';
+import { useInsightsStore } from '@/stores/insights';
+import { unexpectedError } from '@/utils/unexpected-error';
 
 const props = withDefaults(
 	defineProps<{
 		showHeader?: boolean;
 		displayTemplate?: string;
+		linkToItem?: boolean;
 		collection: string;
+		dashboard: string;
 		data?: object;
 	}>(),
 	{
 		showHeader: false,
 		displayTemplate: '',
+		linkToItem: false,
 		sortDirection: 'desc',
 		data: () => ({}),
 	}
 );
 
-const fieldsStore = useFieldsStore();
+const currentlyEditing = ref<number | string>();
+const editsAtStart = ref<Record<string, any>>();
 
-const primaryKeyField = computed(() => fieldsStore.getPrimaryKeyFieldForCollection(props.collection));
+const fieldsStore = useFieldsStore();
+const insightsStore = useInsightsStore();
+
+const primaryKeyField = computed(() => fieldsStore.getPrimaryKeyFieldForCollection(props.collection)?.field ?? 'id');
+
+function startEditing(item: Record<string, any>) {
+	if (!props.linkToItem) return;
+	currentlyEditing.value = item[primaryKeyField.value];
+	editsAtStart.value = item;
+}
+
+function cancelEdit() {
+	editsAtStart.value = undefined;
+	currentlyEditing.value = undefined;
+}
+
+async function saveEdits(item: Record<string, any>) {
+	try {
+		await api.patch(`/items/${props.collection}/${currentlyEditing.value}`, item);
+	} catch (err: any) {
+		unexpectedError(err);
+	}
+
+	await insightsStore.refresh(props.dashboard);
+}
 </script>
 
 <style scoped>
