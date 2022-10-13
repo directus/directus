7365834505b4e0745d4fda71b4728c3e1d4061a8
Ahmed Meftah <contact@ahmedmeftah.com>
2022-10-13 01:32:53 +0100

    Create ScanFolder component to send request to backend

---
+++ app/src/modules/files/components/scan-folder.vue
@@ -0,0 +1,77 @@
+<template>
+	<v-dialog v-model="dialogActive" @esc="dialogActive = false">
+		<template #activator="{ on }">
+			<v-button
+				v-tooltip.bottom="disabled ? t('not_allowed') : t('check_files_integrity')"
+				rounded
+				icon
+				secondary
+				:disabled="disabled"
+				@click="on"
+			>
+				<v-icon name="sync_problem" outline />
+			</v-button>
+		</template>
+
+		<v-card>
+			<v-card-title>{{ t('delete_are_you_sure') }}</v-card-title>
+			<v-card-actions>
+				<v-button secondary @click="dialogActive = false">{{ t('cancel') }}</v-button>
+				<v-button :loading="checking" @click="checkFilesIntegrity">
+					{{ t('check') }}
+				</v-button>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script lang="ts">
+import { useI18n } from 'vue-i18n';
+import { defineComponent, ref } from 'vue';
+import { useFolders } from '@/composables/use-folders';
+import api from '@/api';
+import { unexpectedError } from '@/utils/unexpected-error';
+
+export default defineComponent({
+	props: {
+		parent: {
+			type: String,
+			default: null,
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	setup(props) {
+		const { t } = useI18n();
+
+		const dialogActive = ref(false);
+		const checking = ref(false);
+
+		const { fetchFolders } = useFolders();
+
+		return { t, checkFilesIntegrity, dialogActive, checking };
+
+		async function checkFilesIntegrity() {
+			checking.value = true;
+
+			try {
+				await api.get(`/folders/scan`, {
+					params: {
+						parent: props.parent,
+					},
+				});
+
+				await fetchFolders();
+
+				dialogActive.value = false;
+			} catch (err: any) {
+				unexpectedError(err);
+			} finally {
+				checking.value = false;
+			}
+		}
+	},
+});
+</script>
