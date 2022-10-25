9ecf285dba9794a62a37f8ecc86597de39f8fc2e
Chirica Gheorghe <gheorghi.chirica@crawless.com>
2022-10-25 19:01:48 +0300

    Add startup script

--- start_up.sh
+++ start_up.sh
@@ -4,6 +4,6 @@ DIRECTUS_DIR="/directus/api"
 DIRECTUS_EXTENSIONS="${DIRECTUS_DIR}/extensions"
 DIRECTUS_MIGRATIONS="${DIRECTUS_EXTENSIONS}/migrations"
 
-mv "${DIRECTUS_MIGRATIONS}" "tmp_${DIRECTUS_MIGRATIONS}"
+mv "${DIRECTUS_MIGRATIONS}" "${DIRECTUS_EXTENSIONS}/tmp_migrations"
 
 node ./cli.js bootstrap && for snapshots_file in ./snapshots/*.yaml; do echo $snapshots_file; node ./cli.js schema apply --yes $snapshots_file || true; done && node ./dist/start.js;
