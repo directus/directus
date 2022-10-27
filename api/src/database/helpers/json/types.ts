83d87782cac1b8c0aa296345fd8a7681cafc01c8
brainslug <tim@brainslug.nl>
2022-10-27 10:36:18 +0200

    start compatibility checking

--- api/src/database/helpers/json/types.ts
+++ api/src/database/helpers/json/types.ts
@@ -2,9 +2,12 @@ import { DatabaseHelper } from '../types';
 import { Knex } from 'knex';
 // import { JsonFieldNode } from '../../../types';
 
+const JSON_FEATURES = ['JSON_QUERYING', 'JSON_WILDCARDS', 'JSON_FILTERING'] as const;
+type SupportedJsonFeature = typeof JSON_FEATURES[number];
+
 export abstract class JsonHelper extends DatabaseHelper {
-	supported(): boolean | Promise<boolean> {
-		return true;
+	async supported(): Promise<SupportedJsonFeature[]> {
+		return ['JSON_QUERYING'];
 	}
 	// abstract jsonQuery(table: string, query: Knex.QueryBuilder, fields: JsonFieldNode[]): Knex.QueryBuilder;
 	abstract jsonColumn(table: string, column: string, path: string, alias: string): Knex.Raw;
