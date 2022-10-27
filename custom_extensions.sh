0c91b7fd6b96320eca7d61a162e4419b690940f5
Chirica Gheorghe <gheorghi.chirica@crawless.com>
2022-10-27 10:53:34 +0300

    Update dockerfile

--- custom_extensions.sh
+++ custom_extensions.sh
@@ -33,7 +33,7 @@ wget -O ${DIRECTUS_INTERFACES}/fields-masked/index.js https://github.com/dimitro
 
 # Install crawless custom extensions
 
-curl --header "Private-Token: ${GITLAB_PIPELINE_TOKEN}" -LO ${CI_API_V4_URL}/projects/39544563/packages/generic/Releases/0.0.1/directus-custom-extensions-release.zip
+curl --header "Private-Token: ${GITLAB_PIPELINE_TOKEN}" -LO ${CI_API_V4_URL}/projects/40500377/packages/generic/Releases/0.0.1/directus-custom-extensions-release.zip
 
 unzip directus-custom-extensions-release.zip
 
