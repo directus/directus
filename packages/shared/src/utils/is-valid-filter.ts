4cb43101b249126c97a9985e24ffeee662bbb1b6
brainslug <tim@brainslug.nl>
2022-10-31 13:06:51 +0100

    fixed util and added tests

--- packages/shared/src/utils/is-valid-filter.ts
+++ packages/shared/src/utils/is-valid-filter.ts
@@ -3,5 +3,7 @@ import { VALID_FILTER_OPERATORS } from '../constants';
 export function isValidFilter(name: string): boolean {
 	if (!name.startsWith('_')) return false;
 	const filterName = name.substring(1);
-	return filterName in VALID_FILTER_OPERATORS;
+	// strange typing to allow for array.includes() on readonly without errors
+	const filters = VALID_FILTER_OPERATORS as unknown as string[];
+	return filters.includes(filterName);
 }
