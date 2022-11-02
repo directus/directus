bff16f9c830566d99693983305de1791043bdb8c
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-11-02 14:20:18 +0800

    re-enable html warn and minor tweaks

--- app/src/lang/translations.test.ts
+++ app/src/lang/translations.test.ts
@@ -4,6 +4,7 @@ import { merge } from 'lodash';
 import { createI18n } from 'vue-i18n';
 import availableLanguages from './available-languages.yaml';
 
+const locales = Object.keys(availableLanguages).sort();
 const consoleErrorSpy = vi.spyOn(console, 'error');
 
 afterEach(() => {
@@ -15,18 +16,18 @@ afterEach(() => {
  * Example input: { a: { b: 'b', c: 'c' }, d: 'd' }
  * Example output: { 'a.b': 'b', 'a.c': 'c', d: 'd' }
  */
-function flatten(item: Record<string, any> | string, path: string[] = []): any {
+function flatten(item: Record<string, any> | string, path: string[] = []): Record<string, any> {
 	if (typeof item === 'string') return { [path.join('.')]: item };
 	return Object.entries(item).reduce((acc, [key, value]) => merge(acc, flatten(value, [...path, key])), {});
 }
 
-describe.each(Object.keys(availableLanguages).sort())('Locale %s', async (lang) => {
-	const i18n = createI18n({ locale: lang, warnHtmlMessage: false });
+describe.each(locales)('Locale %s', async (locale) => {
+	const i18n = createI18n({ locale: locale });
 
-	const { default: translations } = await import(`./translations/${lang}.yaml`);
-	i18n.global.mergeLocaleMessage(lang, translations);
+	const { default: translations } = await import(`./translations/${locale}.yaml`);
+	i18n.global.mergeLocaleMessage(locale, translations);
 
-	const messages = flatten((i18n.global.messages as Record<string, any>)[lang]);
+	const messages = flatten((i18n.global.messages as Record<string, any>)[locale]);
 	const translationKeys = Object.keys(messages);
 
 	test.skipIf(translationKeys.length === 0).each(translationKeys)('%s', (key) => {
