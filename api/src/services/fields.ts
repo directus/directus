adff255d917730e3b542b6542cecda7b4daff163
ian <licitdev@gmail.com>
2022-11-09 07:49:49 +0800

    Skip checking of virtual alias fields (#16320)
    * Default virtual fields as alias type
    
    * Make the condition more verbose instead of defaulting
    
    * Remove unnecessary optional chaining
    
    * Add tests
    
    * Revert to alias defaulting in #d06d62
    
    * Shift condition up

--- api/src/services/fields.ts
+++ api/src/services/fields.ts
@@ -362,10 +362,10 @@ export class FieldsService {
 				: null;
 
 			if (
+				hookAdjustedField.type &&
 				(hookAdjustedField.type === 'alias' ||
 					this.schema.collections[collection].fields[field.field]?.type === 'alias') &&
-				hookAdjustedField.type &&
-				hookAdjustedField.type !== this.schema.collections[collection].fields[field.field]?.type
+				hookAdjustedField.type !== (this.schema.collections[collection].fields[field.field]?.type ?? 'alias')
 			) {
 				throw new InvalidPayloadException('Alias type cannot be changed');
 			}
