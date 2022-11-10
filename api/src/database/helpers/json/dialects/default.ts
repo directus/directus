cc3b654c02c704eb6b637068365a8d963d695983
brainslug <tim@brainslug.nl>
2022-11-10 13:39:15 +0100

    merge fallback into default helper

--- api/src/database/helpers/json/dialects/default.ts
+++ api/src/database/helpers/json/dialects/default.ts
@@ -2,12 +2,24 @@ import { Item } from '@directus/shared/types';
 import { parseJSON } from '@directus/shared/utils';
 import { Knex } from 'knex';
 import { JsonHelper } from '../types';
+import { JSONPath } from 'jsonpath-plus';
+import { JsonFieldNode } from '../../../../types';
+import { transformFilterJsonPath } from './postgres';
+import { getOperation } from '../../../../utils/apply-query';
+
+const jsonPathPlusOptions = {
+	preventEval: true,
+};
 
 /**
- * The default handler only passes the data to the Knex jsonExtract function
+ * The default handler holds shared functions and fallback features
  */
 export class JsonHelperDefault extends JsonHelper {
 	preProcess(dbQuery: Knex.QueryBuilder, table: string): void {
+		this.preProcessKnex(dbQuery, table);
+		// what do we default to?
+	}
+	preProcessKnex(dbQuery: Knex.QueryBuilder, table: string): void {
 		dbQuery
 			.select(
 				this.nodes.map((node) => {
@@ -16,8 +28,20 @@ export class JsonHelperDefault extends JsonHelper {
 			)
 			.from(table);
 	}
+	preProcessFallback(dbQuery: Knex.QueryBuilder, table: string): void {
+		dbQuery
+			.select(
+				this.nodes.map((node) => {
+					return this.knex.raw('??.?? as ??', [table, node.name, node.fieldKey]);
+				})
+			)
+			.from(table);
+	}
 	postProcess(items: Item[]): void {
-		if (this.nodes.length === 0) return;
+		// what do we default to?
+		this.postProcessParseJSON(items);
+	}
+	postProcessParseJSON(items: Item[]): void {
 		const keys = this.nodes.map(({ fieldKey }) => fieldKey);
 		for (const item of items) {
 			for (const jsonAlias of keys) {
@@ -30,5 +54,36 @@ export class JsonHelperDefault extends JsonHelper {
 				}
 			}
 		}
+	}
+	postProcessFallback(items: Item[]): void {
+		for (const item of items) {
+			for (const jsonNode of this.nodes) {
+				this.postProcessJsonPath(item, jsonNode);
+			}
+		}
+	}
+	protected postProcessJsonPath(item: Item, node: JsonFieldNode) {
+		try {
+			const data = typeof item[node.fieldKey] === 'string' ? parseJSON(item[node.fieldKey]) : item[node.fieldKey];
+			const jsonPath = Object.keys(node.query).length === 0 ? node.jsonPath : this.buildFilterPath(node);
+			item[node.fieldKey] = JSONPath({
+				...jsonPathPlusOptions,
+				path: jsonPath,
+				json: data,
+			});
+		} catch (e) {
+			item[node.fieldKey] = null;
+		}
+	}
+	protected buildFilterPath(node: JsonFieldNode) {
+		if (!node.query?.filter) return node.jsonPath;
+
+		const conditions = [];
+		for (const [jsonPath, value] of Object.entries(node.query?.filter)) {
+			const { operator: filterOperator, value: filterValue } = getOperation(jsonPath, value);
+			conditions.push(transformFilterJsonPath(jsonPath, filterOperator, filterValue));
+		}
+
+		return `${node.jsonPath}[?(${conditions.join(' && ')})]`;
 	}
 }
