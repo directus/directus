69522e7549bd13204e4d7bce6c667b6c040a353e
Nitwel <nitwel@arcor.de>
2022-11-16 11:59:59 +0100

    improve start/pause button

--- app/src/layouts/graph/actions.vue
+++ app/src/layouts/graph/actions.vue
@@ -1,18 +1,18 @@
 <template>
-	<div>
-        <v-icon v-if="props.running" name="pause" @click="modelRunning = false" />
-        <v-icon v-else name="play_arrow" @click="modelRunning = true" />
-    </div>
+	<v-button icon rounded secondary @click="modelRunning = !modelRunning">
+		<v-icon v-if="props.running" name="pause" />
+		<v-icon v-else name="play_arrow" />
+	</v-button>
 </template>
 
 <script setup lang="ts">
 import { useSync } from '@directus/shared/composables';
 
 interface Props {
-    running: boolean;
+	running: boolean;
 }
 
-const props = defineProps<Props>()
+const props = defineProps<Props>();
 
 const emit = defineEmits(['update:running']);
 
@@ -20,25 +20,7 @@ const modelRunning = useSync(props, 'running', emit);
 </script>
 
 <style lang="scss" scoped>
-.item-count {
-	position: relative;
-	display: none;
-	margin: 0 8px;
-	color: var(--foreground-subdued);
-	white-space: nowrap;
-
-	@media (min-width: 600px) {
-		display: inline;
-	}
-}
-
-.fade-enter-active,
-.fade-leave-active {
-	transition: opacity var(--medium) var(--transition);
-}
-
-.fade-enter-from,
-.fade-leave-to {
-	opacity: 0;
+.v-button {
+	margin-right: 8px;
 }
 </style>
