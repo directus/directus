5e6f9c37f17a9b385898f7c9d021e946b3efcfa6
rijkvanzanten <rijkvanzanten@me.com>
2022-11-17 16:42:15 -0500

    Finish tests for send-request

--- api/src/utils/send-request.test.ts
+++ api/src/utils/send-request.test.ts
@@ -50,3 +50,81 @@ test('Defaults to environment variables for timeout/redirects', async () => {
 		maxRedirects: 2,
 	});
 });
+
+test('Allows setting timeout/max-redirects through options', async () => {
+	vi.mocked(getEnv).mockReturnValueOnce({
+		REQUEST_TIMEOUT: 1000,
+		REQUEST_MAX_REDIRECTS: 1,
+	});
+	vi.mocked(axios.request).mockResolvedValue({});
+
+	await sendRequest({
+		method: 'POST',
+		url: 'localhost:1234',
+		data: { status: 'active' },
+		options: { maxRedirects: 2, timeout: 1500 },
+	});
+
+	expect(axios.request).toHaveBeenCalledWith({
+		url: 'localhost:1234',
+		method: 'POST',
+		data: { status: 'active' },
+		headers: {},
+		proxy: undefined,
+		timeout: 1500,
+		maxRedirects: 2,
+	});
+});
+
+test('Passes proxy options from getProxyOptions', async () => {
+	vi.mocked(getProxyOptions).mockReturnValueOnce({ host: 'localhost', port: 1234, protocol: 'http' });
+	vi.mocked(getEnv).mockReturnValueOnce({
+		REQUEST_TIMEOUT: 1000,
+		REQUEST_MAX_REDIRECTS: 1,
+	});
+	vi.mocked(axios.request).mockResolvedValue({});
+
+	await sendRequest({
+		method: 'POST',
+		url: 'localhost:1234',
+		data: { status: 'active' },
+		options: { maxRedirects: 2, timeout: 1500 },
+	});
+
+	expect(axios.request).toHaveBeenCalledWith({
+		url: 'localhost:1234',
+		method: 'POST',
+		data: { status: 'active' },
+		headers: {},
+		proxy: { host: 'localhost', port: 1234, protocol: 'http' },
+		timeout: 1500,
+		maxRedirects: 2,
+	});
+});
+
+test('Returns axios result', async () => {
+	vi.mocked(getEnv).mockReturnValueOnce({});
+	vi.mocked(axios.request).mockResolvedValue({
+		status: 200,
+		data: {
+			test: true,
+		},
+		statusText: 'OK',
+		headers: {
+			'X-Status': 'OK',
+		},
+	});
+
+	const result = await sendRequest({ method: 'POST', url: 'localhost:1234', data: { status: 'active' } });
+
+	expect(result).toStrictEqual({
+		status: 200,
+		data: {
+			test: true,
+		},
+		statusText: 'OK',
+		headers: {
+			'X-Status': 'OK',
+		},
+	});
+});
