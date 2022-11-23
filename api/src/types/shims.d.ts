c12f03604806588007656265f28ae2e0926043d6
Pascal Jufer <pascal-jufer@bluewin.ch>
2022-11-23 05:36:32 +0100

    Remove outdated type deps & update concurrently

--- api/src/types/shims.d.ts
+++ api/src/types/shims.d.ts
@@ -2,12 +2,3 @@ declare module 'grant' {
 	const grant: any;
 	export default grant;
 }
-
-declare module 'pino-http' {
-	import PinoHttp from '@types/pino-http';
-	const pinoHttp: PinoHttp;
-	export default pinoHttp;
-	export const stdSerializers: {
-		req: (req: any) => Record<string, any>;
-	};
-}
