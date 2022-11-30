c1fc555ae2cf365c045eb4c4dc64686757d2cc37
rijkvanzanten <rijkvanzanten@me.com>
2022-11-30 14:00:33 -0500

    Add tests for register drivers

---
+++ api/src/storage/register-drivers.test.ts
@@ -0,0 +1,64 @@
+import type { StorageManager, LocalFileSystemStorage } from '@directus/drive';
+import { expect, test, vi, afterEach } from 'vitest';
+import { registerDrivers } from './register-drivers';
+import { getEnv } from '../env';
+import { getStorageDriver } from './get-storage-driver';
+
+vi.mock('./get-storage-driver');
+vi.mock('../env');
+
+afterEach(() => {
+	vi.clearAllMocks();
+});
+
+test('Ignores STORAGE variables that are not driver', () => {
+	vi.mocked(getEnv).mockReturnValueOnce({
+		STORAGE_SOMETHING: 'x',
+	});
+
+	const mockStorageManager = {
+		registerDriver: vi.fn(),
+	} as unknown as StorageManager;
+
+	registerDrivers(mockStorageManager);
+
+	expect(mockStorageManager.registerDriver).toHaveBeenCalledTimes(0);
+});
+
+test('Ignores DRIVER variables that do not have the STORAGE prefix', () => {
+	vi.mocked(getEnv).mockReturnValueOnce({
+		SOMETHING_DRIVER: 'x',
+	});
+
+	const mockStorageManager = {
+		registerDriver: vi.fn(),
+	} as unknown as StorageManager;
+
+	registerDrivers(mockStorageManager);
+
+	expect(mockStorageManager.registerDriver).toHaveBeenCalledTimes(0);
+});
+
+test('Registers all drivers on the passed manager', () => {
+	vi.mocked(getEnv).mockReturnValueOnce({
+		STORAGE_A_DRIVER: 'x',
+		STORAGE_Y_DRIVER: 'y',
+	});
+
+	const mockStorageManager = {
+		registerDriver: vi.fn(),
+	} as unknown as StorageManager;
+
+	const mockDriverX = {} as typeof LocalFileSystemStorage;
+	const mockDriverY = {} as typeof LocalFileSystemStorage;
+
+	vi.mocked(getStorageDriver).mockImplementation((driver) => {
+		if (driver === 'x') return mockDriverX;
+		return mockDriverY;
+	});
+
+	registerDrivers(mockStorageManager);
+
+	expect(mockStorageManager.registerDriver).toHaveBeenCalledWith('x', mockDriverX);
+	expect(mockStorageManager.registerDriver).toHaveBeenCalledWith('y', mockDriverY);
+});
