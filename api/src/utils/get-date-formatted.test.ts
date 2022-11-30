94aa49a493645d9db2aaf3609544969559d0ca38
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2022-11-30 23:40:23 +0800

    fix timezone tests to account for daylight saving

--- api/src/utils/get-date-formatted.test.ts
+++ api/src/utils/get-date-formatted.test.ts
@@ -10,19 +10,26 @@ afterEach(() => {
 	vi.useRealTimers();
 });
 
-test.each([
-	{ utc: '2023-02-03T01:23:45.678Z', unixTimestamp: 1675387425678, expected: '20230203-12345' },
-	{ utc: '2023-06-12T01:23:45.678Z', unixTimestamp: 1686533025678, expected: '20230612-12345' },
-	{ utc: '2025-10-20T01:23:45.678Z', unixTimestamp: 1760923425678, expected: '20251020-12345' },
-	{ utc: '2050-11-11T12:34:56.789Z', unixTimestamp: 2551782896789, expected: '20501111-123456' },
-])('should format "$unixTimestamp" into "$expected"', ({ unixTimestamp, expected }) => {
-	// account for timezone difference depending on the machine where this test is ran
-	const now = new Date();
-	const timezoneOffset = now.getTimezoneOffset() * 60 * 1000;
-	const targetSystemTime = new Date(unixTimestamp).valueOf() + timezoneOffset;
+function getUtcDateForString(date: string) {
+	const now = new Date(date);
 
-	// set to the unix timestamp that is not offset by local timezone
-	vi.setSystemTime(targetSystemTime);
+	// account for timezone difference depending on the machine where this test is ran
+	const timezoneOffsetInMinutes = now.getTimezoneOffset();
+	const timezoneOffsetInMilliseconds = timezoneOffsetInMinutes * 60 * 1000;
+	const nowUTC = new Date(now.valueOf() + timezoneOffsetInMilliseconds);
+
+	return nowUTC;
+}
+
+test.each([
+	{ utc: '2023-01-01T01:23:45.678Z', expected: '20230101-12345' },
+	{ utc: '2023-01-11T01:23:45.678Z', expected: '20230111-12345' },
+	{ utc: '2023-11-01T01:23:45.678Z', expected: '20231101-12345' },
+	{ utc: '2023-11-11T12:34:56.789Z', expected: '20231111-123456' },
+])('should format $utc into "$expected"', ({ utc, expected }) => {
+	const nowUTC = getUtcDateForString(utc);
+
+	vi.setSystemTime(nowUTC);
 
 	expect(getDateFormatted()).toBe(expected);
 });
