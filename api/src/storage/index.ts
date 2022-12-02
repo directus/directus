d0de88fc5ce0424dc7e03c5be7d4209a9dfa3d19
rijkvanzanten <rijkvanzanten@me.com>
2022-12-02 09:48:00 -0500

    Use dynamic imports

--- api/src/storage/index.ts
+++ api/src/storage/index.ts
@@ -7,8 +7,8 @@ export const _cache: { storage: any | null } = {
 export const getStorage = async () => {
 	if (_cache.storage) return _cache.storage;
 
-	const { StorageManager } = await eval(`import('@directus/storage')`);
-	const { DriverLocal } = await eval(`import('@directus/storage-driver-local')`);
+	const { StorageManager } = await import('@directus/storage');
+	const { DriverLocal } = await import('@directus/storage-driver-local');
 
 	validateEnv(['STORAGE_LOCATIONS']);
 
@@ -17,6 +17,8 @@ export const getStorage = async () => {
 	// registerDrivers(_cache._storage);
 
 	_cache.storage = new StorageManager();
+
+	// @TODO replace with dynamic calls
 	_cache.storage.registerDriver('local', DriverLocal);
 	_cache.storage.registerLocation('local', {
 		driver: 'local',
