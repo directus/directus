da948f995a3102dacb67ada968077a91afcf3aeb
Jose Varela <joselcvarela@gmail.com>
2022-12-04 12:26:58 +0000

    Adds ability to verify JWT with meaningful errors

--- api/src/utils/jwt.ts
+++ api/src/utils/jwt.ts
@@ -2,13 +2,14 @@ import jwt, { JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 import { DirectusTokenPayload } from '../types';
 import { InvalidTokenException, ServiceUnavailableException, TokenExpiredException } from '../exceptions';
 
-export function verifyAccessJWT(token: string, secret: string): DirectusTokenPayload {
+export function verifyJWT(token: string, secret: string): Record<string, any> {
 	let payload;
 
 	try {
-		payload = jwt.verify(token, secret, {
+		const { iat, iss, ...rest } = jwt.verify(token, secret, {
 			issuer: 'directus',
 		}) as Record<string, any>;
+		payload = rest;
 	} catch (err) {
 		if (err instanceof TokenExpiredError) {
 			throw new TokenExpiredException();
@@ -19,7 +20,11 @@ export function verifyAccessJWT(token: string, secret: string): DirectusTokenPay
 		}
 	}
 
-	const { id, role, app_access, admin_access, share, share_scope } = payload;
+	return payload;
+}
+
+export function verifyAccessJWT(token: string, secret: string): DirectusTokenPayload {
+	const { id, role, app_access, admin_access, share, share_scope } = verifyJWT(token, secret);
 
 	if (role === undefined || app_access === undefined || admin_access === undefined) {
 		throw new InvalidTokenException('Invalid token payload.');
