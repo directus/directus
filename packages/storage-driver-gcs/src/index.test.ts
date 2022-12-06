a8229aea835d2225bfe0673216b8bc8286fcf90a
rijkvanzanten <rijkvanzanten@me.com>
2022-12-06 15:22:31 -0500

    Start on gcs

---
+++ packages/storage-driver-gcs/src/index.test.ts
@@ -0,0 +1,63 @@
+import { expect, describe, test, vi, afterEach } from 'vitest';
+import { DriverGCS } from './index.js';
+import { normalizePath } from '@directus/shared/utils';
+import { isReadableStream } from '@directus/shared/utils/node';
+import { join } from 'node:path';
+import type { Readable } from 'node:stream';
+import { Storage } from '@google-cloud/storage';
+
+vi.mock('@directus/shared/utils/node');
+vi.mock('@directus/shared/utils');
+vi.mock('@google-cloud/storage');
+vi.mock('node:path');
+
+describe('#constructor', () => {
+	test('Defaults root path to empty string', () => {
+		const driver = new DriverGCS({
+			bucket: 'test-bucket',
+		});
+
+		expect(driver['root']).toBe('');
+	});
+
+	test('Normalizes config path when root is given', () => {
+		const testRoot = '/root/';
+		const mockRoot = 'root/';
+
+		vi.mocked(normalizePath).mockReturnValue(mockRoot);
+
+		new DriverGCS({
+			bucket: 'test-bucket',
+			root: testRoot,
+		});
+
+		expect(normalizePath).toHaveBeenCalledWith(testRoot, { removeLeading: true });
+	});
+
+	test('Instantiates Storage object with config options', () => {
+		new DriverGCS({
+			bucket: 'test-bucket',
+			apiEndpoint: 'test-endpoint',
+		});
+
+		expect(Storage).toHaveBeenCalledWith({ apiEndpoint: 'test-endpoint' });
+	});
+
+	test('Creates bucket access instance', () => {
+		const mockBucket = {};
+
+		const mockStorage = {
+			bucket: vi.fn().mockReturnValue(mockBucket),
+		} as unknown as Storage;
+
+		vi.mocked(Storage).mockReturnValueOnce(mockStorage);
+
+		const driver = new DriverGCS({
+			bucket: 'test-bucket',
+			apiEndpoint: 'test-endpoint',
+		});
+
+		expect(mockStorage.bucket).toHaveBeenCalledWith('test-bucket');
+		expect(driver['bucket']).toBe(mockBucket);
+	});
+});
---
+++ packages/storage-driver-gcs/src/index.ts
@@ -0,0 +1,50 @@
+import { normalizePath } from '@directus/shared/utils';
+import { isReadableStream } from '@directus/shared/utils/node';
+import type { Driver, Range } from '@directus/storage';
+import { Storage } from '@google-cloud/storage';
+import type { StorageOptions, Bucket } from '@google-cloud/storage';
+import { join } from 'node:path';
+import type { Readable } from 'node:stream';
+
+export type DriverGCSConfig = {
+	root?: string;
+	bucket: string;
+} & StorageOptions;
+
+export class DriverGCS implements Driver {
+	private root: string;
+	private bucket: Bucket;
+
+	constructor(config: DriverGCSConfig) {
+		const { bucket, root, ...storageOptions } = config;
+
+		this.root = root ? normalizePath(root, { removeLeading: true }) : '';
+
+		const storage = new Storage(storageOptions);
+		this.bucket = storage.bucket(bucket);
+	}
+
+	private fullPath(filepath: string) {
+		return normalizePath(join(this.root, filepath));
+	}
+
+	async getStream(filepath: string, range?: Range) {}
+
+	async getBuffer(filepath: string) {}
+
+	async getStat(filepath: string) {}
+
+	async exists(filepath: string) {}
+
+	async move(src: string, dest: string) {}
+
+	async copy(src: string, dest: string) {}
+
+	async put(filepath: string, content: Buffer | NodeJS.ReadableStream | string, type = 'application/octet-stream') {}
+
+	async delete(filepath: string) {}
+
+	async *list(prefix = '') {}
+}
+
+export default DriverGCS;
