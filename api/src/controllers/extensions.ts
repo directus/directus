54a1f03086d0339d40caaf7072468b579c76ac3d
Nitwel <mail@nitwel.de>
2022-12-17 15:53:07 +0100

    update endpoints

--- api/src/controllers/extensions.ts
+++ api/src/controllers/extensions.ts
@@ -5,7 +5,7 @@ import { getExtensionManager } from '../extensions';
 import { respond } from '../middleware/respond';
 import { depluralize, isIn } from '@directus/shared/utils';
 import { Plural } from '@directus/shared/types';
-import { EXTENSION_PACKAGE_TYPES } from '@directus/shared/constants';
+import { EXTENSION_TYPES } from '@directus/shared/constants';
 import ms from 'ms';
 import env from '../env';
 import { getCacheControlHeader } from '../utils/get-cache-headers';
@@ -32,7 +32,7 @@ router.get(
 	asyncHandler(async (req, res, next) => {
 		const type = depluralize(req.params.type as Plural<string>);
 
-		if (!isIn(type, EXTENSION_PACKAGE_TYPES)) {
+		if (!isIn(type, EXTENSION_TYPES)) {
 			throw new RouteNotFoundException(req.path);
 		}
 
@@ -80,6 +80,8 @@ router.post('/', asyncHandler(async (req, res, next) => {
 	}),
 	respond
 );
+
+router.delete('/:name', asyncHandler(async (req, res, next) => {}));
 
 router.get(
 	'/sources/index.js',
