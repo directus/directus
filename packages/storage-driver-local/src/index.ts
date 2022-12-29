d6e1152f90e6853bceaa148f289462d6b7526682
Roger Stringer <roger@datamcfly.com>
2022-12-29 06:42:00 -0800

    remove writeStream.on error listener

--- packages/storage-driver-local/src/index.ts
+++ packages/storage-driver-local/src/index.ts
@@ -79,16 +79,12 @@ export class DriverLocal implements Driver {
 			const fullPath = this.fullPath(filepath);
 			await this.ensureDir(dirname(fullPath));
 			const writeStream = createWriteStream(fullPath);
-			writeStream.on('error', (error) => {
-				throw error;
-			});
 			content.on('close', () => {
 				writeStream.end();
 				writeStream.on('finish', () => {
 					writeStream.close();
 				});
 			});
-
 			await pipeline(content, writeStream);
 		} catch (error) {
 			throw new Error(`Failed writing file "${filepath}": ${error}`);
