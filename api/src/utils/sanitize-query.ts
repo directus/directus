71fba5de69c6b7c8d0fe9a1dacd31db0f2a0f31e
Brainslug <br41nslug@users.noreply.github.com>
2022-12-30 12:18:32 +0100

    Added check for integers
    Co-authored-by: ian <licitdev@gmail.com>

--- api/src/utils/sanitize-query.ts
+++ api/src/utils/sanitize-query.ts
@@ -129,7 +129,7 @@ function sanitizeFilter(rawFilter: any, accountability: Accountability | null) {
 function sanitizeLimit(rawLimit: any) {
 	if (rawLimit === undefined || rawLimit === null) return null;
 	const limit = Number(rawLimit);
-	if (limit < -1) return null; // don't accept below -1
+	if (!Number.isInteger(limit) || limit < -1) return null; // don't accept non-integers or below -1
 	return limit;
 }
 
