2f728785e289e14919b936ad4f3a8c1669272397
Nitwel <mail@nitwel.de>
2023-01-03 11:37:49 +0100

    readd endpoint

--- api/src/controllers/extensions.ts
+++ api/src/controllers/extensions.ts
@@ -5,8 +5,34 @@ import { getExtensionManager } from '../extensions';
 import ms from 'ms';
 import env from '../env';
 import { getCacheControlHeader } from '../utils/get-cache-headers';
+import { respond } from '../middleware/respond';
+import { depluralize, isIn } from '@directus/shared/utils';
+import { Plural } from '@directus/shared/types';
+import { EXTENSION_TYPES } from '@directus/shared/constants';
 
 const router = Router();
+
+router.get(
+	'/:type',
+	asyncHandler(async (req, res, next) => {
+		const type = depluralize(req.params.type as Plural<string>);
+
+		if (!isIn(type, EXTENSION_TYPES)) {
+			throw new RouteNotFoundException(req.path);
+		}
+
+		const extensionManager = getExtensionManager();
+
+		const extensions = extensionManager.getExtensionsList(type);
+
+		res.locals.payload = {
+			data: extensions,
+		};
+
+		return next();
+	}),
+	respond
+);
 
 router.get(
 	'/sources/index.js',
