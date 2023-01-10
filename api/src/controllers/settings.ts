e532b9dc66c589838142932d77eb40844b81d0c6
Brainslug <tim@brainslug.nl>
2023-01-10 14:39:17 +0100

    removed backend endpoint implementation

--- api/src/controllers/settings.ts
+++ api/src/controllers/settings.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { ForbiddenException } from '../exceptions';
 import { respond } from '../middleware/respond';
 import useCollection from '../middleware/use-collection';
-import { SettingsService, ItemsService } from '../services';
+import { SettingsService } from '../services';
 import asyncHandler from '../utils/async-handler';
 
 const router = express.Router();
@@ -43,38 +43,6 @@ router.patch(
 			throw error;
 		}
 
-		return next();
-	}),
-	respond
-);
-
-router.get(
-	'/translation-strings',
-	asyncHandler(async (req, res, next) => {
-		const service = new SettingsService({
-			accountability: req.accountability,
-			schema: req.schema,
-		});
-		const records = await service.readSingleton({
-			fields: ['translations'],
-			alias: {
-				translations: 'json(translation_strings2$[*])',
-			},
-			deep: {
-				translations: {
-					_filter: {
-						'$.lang': { _eq: 'nl-NL' },
-					},
-				},
-			},
-		});
-		const strings = records.translations.reduce((a: Record<string, any>, c: any) => {
-			if (!a[c.key]) a[c.key] = {};
-			a[c.key][c.lang] = c.value;
-			return a;
-		}, {} as Record<string, any>);
-		const translation_strings = Object.entries(strings).map(([key, translations]) => ({ key, translations }));
-		res.locals.payload = { data: { translation_strings } };
 		return next();
 	}),
 	respond
