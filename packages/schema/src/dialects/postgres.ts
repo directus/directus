3e40254361251ea8b990c232d9e5c6f065e445c7
Arnaud Leymet <arnaud@codename.co>
2023-01-16 00:15:25 +0100

    :sparkles: Postgres support for views

--- packages/schema/src/dialects/postgres.ts
+++ packages/schema/src/dialects/postgres.ts
@@ -1,4 +1,4 @@
-import KnexPostgres, { parseDefaultValue } from 'knex-schema-inspector/dist/dialects/postgres';
+import KnexPostgres, { parseDefaultValue } from './knex-schema-inspector-postgres';
 import { Column } from 'knex-schema-inspector/dist/types/column';
 import { SchemaOverview } from '../types/overview';
 import { SchemaInspector } from '../types/schema';
@@ -29,28 +29,92 @@ export default class Postgres extends KnexPostgres implements SchemaInspector {
 			// cannot have primary keys so they cannot be used)
 			this.knex.raw(
 				`
-        SELECT c.table_name
-          , c.column_name
-          , c.column_default as default_value
-          , c.data_type
-			 		, c.character_maximum_length as max_length
-          , c.is_generated = 'ALWAYS' is_generated
-          , CASE WHEN c.is_identity = 'YES' THEN true ELSE false END is_identity
-          , CASE WHEN c.is_nullable = 'YES' THEN true ELSE false END is_nullable
-        FROM
-          information_schema.columns c
-        LEFT JOIN information_schema.tables t
-          ON c.table_name = t.table_name
-        WHERE
-          t.table_type = 'BASE TABLE'
-          AND c.table_schema IN (${bindings});
+        WITH tables_views_and_foreign_tables AS (
+          SELECT c.table_name
+            , c.column_name
+            , c.column_default as default_value
+            , c.data_type
+            , c.character_maximum_length as max_length
+            , c.is_generated = 'ALWAYS' is_generated
+            , CASE WHEN c.is_identity = 'YES' THEN true ELSE false END is_identity
+            , CASE WHEN c.is_nullable = 'YES' THEN true ELSE false END is_nullable
+          FROM
+            information_schema.columns c
+          LEFT JOIN information_schema.tables t
+            ON c.table_name = t.table_name
+          WHERE
+            -- t.table_type = 'BASE TABLE'
+            c.table_schema IN (${bindings})
+        ),
+
+        materialized_views AS (
+          WITH inner_materialized_views AS (
+            SELECT (current_database())::information_schema.sql_identifier AS table_catalog,
+              (nc.nspname)::information_schema.sql_identifier AS table_schema,
+              (c.relname)::information_schema.sql_identifier AS table_name,
+              (
+                CASE
+                  WHEN ((pg_relation_is_updatable((c.oid)::regclass, FALSE) & 20) = 20) THEN 'YES'::TEXT
+                  ELSE 'NO'::TEXT
+                END)::information_schema.yes_or_no AS is_updatable,
+              (
+                CASE
+                  WHEN ((pg_relation_is_updatable((c.oid)::regclass, FALSE) & 8) = 8) THEN 'YES'::TEXT
+                  ELSE 'NO'::TEXT
+                END)::information_schema.yes_or_no AS is_insertable_into
+              FROM pg_namespace nc,
+                pg_class c
+              WHERE (
+                (
+                  ((c.relnamespace = nc.oid) AND (c.relkind = 'm'::"char")) AND (NOT pg_is_other_temp_schema(nc.oid))
+                ) AND (
+                  (pg_has_role(c.relowner, 'USAGE'::TEXT) OR has_table_privilege(c.oid, 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER'::TEXT))
+                  OR has_any_column_privilege(c.oid, 'SELECT, INSERT, UPDATE, REFERENCES'::TEXT)
+                )
+              )
+          ),
+
+          attrs AS (
+            SELECT t.relname AS table_name,
+                   a.attname AS column_name,
+                   pg_catalog.format_type(a.atttypid, a.atttypmod) AS data_type,
+                   NOT a.attnotnull AS is_nullable
+            FROM pg_attribute a
+              JOIN pg_class t on a.attrelid = t.oid
+              JOIN pg_namespace s on t.relnamespace = s.oid
+            WHERE a.attnum > 0
+              AND NOT a.attisdropped
+			  -- TODO: handle double bindings:
+              -- AND s.nspname = (${'' /*bindings*/})
+            ORDER BY a.attnum
+          )
+
+          SELECT
+            attrs.table_name,
+            attrs.column_name,
+            NULL AS default_value,
+            attrs.data_type,
+            NULL::INTEGER AS max_length,
+            FALSE AS is_generated,
+            FALSE AS is_identity,
+            attrs.is_nullable
+          FROM inner_materialized_views mv
+          LEFT JOIN attrs
+            ON attrs.table_name = mv.table_name
+        )
+
+        SELECT * FROM tables_views_and_foreign_tables
+        UNION
+        SELECT * FROM materialized_views;
       `,
 				this.explodedSchema
 			),
 
 			this.knex.raw(
 				`
-        SELECT relname as table_name
+      WITH tables AS (
+        SELECT 'table' as type
+          , relname as table_name
           , pg_attribute.attname as column_name
         FROM pg_index
           , pg_class
@@ -65,6 +129,23 @@ export default class Postgres extends KnexPostgres implements SchemaInspector {
           AND indisprimary
           AND indnatts = 1
 			 AND relkind != 'S'
+      ),
+      views AS (
+        WITH summary AS (
+          SELECT 'view' AS type
+            , c.table_name
+            , column_name
+            , ordinal_position AS rank
+          FROM information_schema.columns c
+          LEFT JOIN information_schema.tables t
+            ON c.table_name = t.table_name
+          WHERE t.table_type = 'VIEW'
+        )
+        SELECT type, table_name, column_name
+        FROM summary
+        WHERE rank = 1
+      )
+      SELECT * FROM tables
       `,
 				this.explodedSchema
 			),
@@ -93,7 +174,6 @@ export default class Postgres extends KnexPostgres implements SchemaInspector {
 				FROM geometries g
 				JOIN information_schema.tables t
 					ON g.f_table_name = t.table_name
-					AND t.table_type = 'BASE TABLE'
 				WHERE f_table_schema in (${bindings})
 				`,
 				this.explodedSchema
@@ -112,7 +192,8 @@ export default class Postgres extends KnexPostgres implements SchemaInspector {
 			}
 
 			if (column.table_name in overview === false) {
-				overview[column.table_name] = { columns: {}, primary: <any>undefined };
+				// TODO: The id column should be defaulted to for non-basic tables for the time being
+				overview[column.table_name] = { columns: {}, primary: 'id' };
 			}
 			if (['point', 'polygon'].includes(column.data_type)) {
 				column.data_type = 'unknown';
@@ -121,6 +202,7 @@ export default class Postgres extends KnexPostgres implements SchemaInspector {
 		}
 
 		for (const { table_name, column_name } of primaryKeys) {
+			overview[table_name] ??= { columns: {}, primary: <any>undefined };
 			overview[table_name].primary = column_name;
 		}
 		for (const { table_name, column_name, data_type } of geometryColumns) {
