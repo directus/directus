aa2e3870e5710e04e24c16bb659db719bc800305
Gerard Lamusse <gerardlamo@gmail.com>
2023-01-19 16:32:22 +0100

    Update description of split command

--- api/src/cli/index.ts
+++ api/src/cli/index.ts
@@ -92,7 +92,7 @@ export async function createCli(): Promise<Command> {
 		.description('Create a new Schema Snapshot')
 		.option('-y, --yes', `Assume "yes" as answer to all prompts and run non-interactively`, false)
 		.addOption(new Option('--format <format>', 'JSON or YAML format').choices(['json', 'yaml']).default('yaml'))
-		.addOption(new Option('-s, --split', 'Export as a single schema file'))
+		.addOption(new Option('-s, --split', 'Split up the schema into individual files for each collection'))
 		.argument('[path]', 'Path to snapshot file')
 		.action(snapshot);
 
