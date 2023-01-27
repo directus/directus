dcf3102961aceecc06b8d0794918be20096ef763
Brainslug <tim@brainslug.nl>
2023-01-27 16:57:04 +0100

    added tests for the validate query function

---
+++ api/src/utils/validate-query.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test, vi } from 'vitest';
+import { validateQuery } from './validate-query';
+
+vi.mock('../env', async () => {
+	const actual = (await vi.importActual('../env')) as { default: Record<string, any> };
+	const MOCK_ENV = {
+		...actual.default,
+		MAX_QUERY_LIMIT: 100,
+	};
+	return {
+		default: MOCK_ENV,
+		getEnv: () => MOCK_ENV,
+	};
+});
+
+describe('max limit', () => {
+	test.each([-1, 1, 25])('should accept number %i', (limit) => {
+		expect(() => validateQuery({ limit })).not.toThrowError('limit');
+	});
+
+	test('should error with 101', () => {
+		expect(() => validateQuery({ limit: 101 })).toThrowError('limit');
+	});
+});
