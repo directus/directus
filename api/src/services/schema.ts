72170492785f0d061aa11173bf1ccd45cd02fcb3
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-01-30 14:57:40 +0100

    Extract diff & snapshot validation into own tested utils

--- api/src/services/schema.ts
+++ api/src/services/schema.ts
@@ -1,103 +1,14 @@
-import { TYPES } from '@directus/shared/constants';
 import { Accountability } from '@directus/shared/types';
-import Joi from 'joi';
 import { Knex } from 'knex';
-import { version as currentDirectusVersion } from '../../package.json';
-import { ALIAS_TYPES } from '../constants';
-import getDatabase, { getDatabaseClient } from '../database';
-import { ForbiddenException, InvalidPayloadException } from '../exceptions';
-import {
-	AbstractServiceOptions,
-	DatabaseClients,
-	DiffKind,
-	Snapshot,
-	SnapshotDiff,
-	SnapshotDiffWithHash,
-	SnapshotWithHash,
-} from '../types';
+import getDatabase from '../database';
+import { ForbiddenException } from '../exceptions';
+import { AbstractServiceOptions, Snapshot, SnapshotDiff, SnapshotDiffWithHash, SnapshotWithHash } from '../types';
 import { applyDiff } from '../utils/apply-diff';
 import { getSnapshot } from '../utils/get-snapshot';
 import { getSnapshotDiff } from '../utils/get-snapshot-diff';
 import { getVersionedHash } from '../utils/get-versioned-hash';
-
-const snapshotJoiSchema = Joi.object({
-	version: Joi.number().valid(1).required(),
-	directus: Joi.string().required(),
-	vendor: Joi.string()
-		.valid(...DatabaseClients)
-		.optional(),
-	collections: Joi.array().items(
-		Joi.object({
-			collection: Joi.string(),
-			meta: Joi.any(),
-			schema: Joi.object({
-				name: Joi.string(),
-			}),
-		})
-	),
-	fields: Joi.array().items(
-		Joi.object({
-			collection: Joi.string(),
-			field: Joi.string(),
-			meta: Joi.any(),
-			schema: Joi.object({
-				default_value: Joi.any(),
-				max_length: [Joi.number(), Joi.string(), Joi.valid(null)],
-				is_nullable: Joi.bool(),
-			})
-				.unknown()
-				.allow(null),
-			type: Joi.string()
-				.valid(...TYPES, ...ALIAS_TYPES)
-				.allow(null),
-		})
-	),
-	relations: Joi.array().items(
-		Joi.object({
-			collection: Joi.string(),
-			field: Joi.string(),
-			meta: Joi.any(),
-			related_collection: Joi.any(),
-			schema: Joi.any(),
-		})
-	),
-});
-
-const deepDiffSchema = Joi.object({
-	kind: Joi.string().valid(DiffKind.NEW, DiffKind.DELETE, DiffKind.EDIT, DiffKind.ARRAY).required(),
-	path: Joi.array().items(Joi.string()),
-	lhs: Joi.object().when('kind', { is: [DiffKind.DELETE, DiffKind.EDIT], then: Joi.required() }),
-	rhs: Joi.object().when('kind', { is: [DiffKind.NEW, DiffKind.EDIT], then: Joi.required() }),
-	index: Joi.number().when('kind', { is: DiffKind.ARRAY, then: Joi.required() }),
-	item: Joi.link('/').when('kind', { is: DiffKind.ARRAY, then: Joi.required() }),
-});
-
-const applyJoiSchema = Joi.object({
-	hash: Joi.string().required(),
-	diff: Joi.object({
-		collections: Joi.array().items(
-			Joi.object({
-				collection: Joi.string(),
-				diff: Joi.array().items(deepDiffSchema),
-			})
-		),
-		fields: Joi.array().items(
-			Joi.object({
-				collection: Joi.string(),
-				field: Joi.string(),
-				diff: Joi.array().items(deepDiffSchema),
-			})
-		),
-		relations: Joi.array().items(
-			Joi.object({
-				collection: Joi.string(),
-				field: Joi.string(),
-				related_collection: Joi.any(),
-				diff: Joi.array().items(deepDiffSchema),
-			})
-		),
-	}).required(),
-});
+import { validateApplyDiff } from '../utils/validate-diff';
+import { validateSnapshot } from '../utils/validate-snapshot';
 
 export class SchemaService {
 	knex: Knex;
@@ -119,104 +30,10 @@ export class SchemaService {
 	async apply(payload: SnapshotDiffWithHash): Promise<void> {
 		if (this.accountability?.admin !== true) throw new ForbiddenException();
 
-		const { error } = applyJoiSchema.validate(payload);
-
-		if (error) throw new InvalidPayloadException(error.message);
-
 		const currentSnapshot = await this.snapshot();
-
 		const snapshotWithHash = this.getHashedSnapshot(currentSnapshot);
 
-		if (
-			payload.diff.collections.length === 0 &&
-			payload.diff.fields.length === 0 &&
-			payload.diff.relations.length === 0
-		) {
-			return;
-		}
-
-		if (payload.hash !== snapshotWithHash.hash) {
-			for (const diffCollection of payload.diff.collections) {
-				const collection = diffCollection.collection;
-
-				if (diffCollection.diff[0]?.kind === DiffKind.NEW) {
-					const existingCollection = snapshotWithHash.collections.find(
-						(c) => c.collection === diffCollection.collection
-					);
-
-					if (existingCollection) {
-						throw new InvalidPayloadException(
-							`Provided diff is trying to create collection "${collection}" but it already exists. Please generate a new diff and try again.`
-						);
-					}
-				} else if (diffCollection.diff[0]?.kind === DiffKind.DELETE) {
-					const existingCollection = snapshotWithHash.collections.find(
-						(c) => c.collection === diffCollection.collection
-					);
-
-					if (!existingCollection) {
-						throw new InvalidPayloadException(
-							`Provided diff is trying to delete collection "${collection}" but it does not exist. Please generate a new diff and try again.`
-						);
-					}
-				}
-			}
-			for (const diffField of payload.diff.fields) {
-				const field = `${diffField.collection}.${diffField.field}`;
-
-				if (diffField.diff[0]?.kind === DiffKind.NEW) {
-					const existingField = snapshotWithHash.fields.find(
-						(f) => f.collection === diffField.collection && f.field === diffField.field
-					);
-
-					if (existingField) {
-						throw new InvalidPayloadException(
-							`Provided diff is trying to create field "${field}" but it already exists. Please generate a new diff and try again.`
-						);
-					}
-				} else if (diffField.diff[0]?.kind === DiffKind.DELETE) {
-					const existingField = snapshotWithHash.fields.find(
-						(f) => f.collection === diffField.collection && f.field === diffField.field
-					);
-
-					if (!existingField) {
-						throw new InvalidPayloadException(
-							`Provided diff is trying to delete field "${field}" but it does not exist. Please generate a new diff and try again.`
-						);
-					}
-				}
-			}
-			for (const diffRelation of payload.diff.relations) {
-				let relation = `${diffRelation.collection}.${diffRelation.field}`;
-				if (diffRelation.related_collection) relation += `-> ${diffRelation.related_collection}`;
-
-				if (diffRelation.diff[0]?.kind === DiffKind.NEW) {
-					const existingRelation = snapshotWithHash.relations.find(
-						(r) => r.collection === diffRelation.collection && r.field === diffRelation.field
-					);
-
-					if (existingRelation) {
-						throw new InvalidPayloadException(
-							`Provided diff is trying to create relation "${relation}" but it already exists. Please generate a new diff and try again.`
-						);
-					}
-				} else if (diffRelation.diff[0]?.kind === DiffKind.DELETE) {
-					const existingRelation = snapshotWithHash.relations.find(
-						(r) => r.collection === diffRelation.collection && r.field === diffRelation.field
-					);
-
-					if (!existingRelation) {
-						throw new InvalidPayloadException(
-							`Provided diff is trying to delete relation "${relation}" but it does not exist. Please generate a new diff and try again.`
-						);
-					}
-				}
-			}
-
-			throw new InvalidPayloadException(
-				`Provided hash does not match the current instance's schema hash, indicating the schema has changed after this diff was generated. Please generate a new diff and try again.`
-			);
-		}
+		if (!validateApplyDiff(payload, snapshotWithHash)) return;
 
 		await applyDiff(currentSnapshot, payload.diff, { database: this.knex });
 	}
@@ -227,31 +44,7 @@ export class SchemaService {
 	): Promise<SnapshotDiff | null> {
 		if (this.accountability?.admin !== true) throw new ForbiddenException();
 
-		if (!options?.force) {
-			if (snapshot.directus !== currentDirectusVersion) {
-				throw new InvalidPayloadException(
-					`Provided snapshot's directus version ${snapshot.directus} does not match the current instance's version ${currentDirectusVersion}. You can bypass this check by passing the "force" query parameter.`
-				);
-			}
-
-			if (!snapshot.vendor) {
-				throw new InvalidPayloadException(
-					'Provided snapshot does not contain the "vendor" property. You can bypass this check by passing the "force" query parameter.'
-				);
-			}
-
-			const currentVendor = getDatabaseClient();
-
-			if (snapshot.vendor !== currentVendor) {
-				throw new InvalidPayloadException(
-					`Provided snapshot's vendor ${snapshot.vendor} does not match the current instance's vendor ${currentVendor}. You can bypass this check by passing the "force" query parameter.`
-				);
-			}
-		}
-
-		const { error } = snapshotJoiSchema.validate(snapshot);
-
-		if (error) throw new InvalidPayloadException(error.message);
+		validateSnapshot(snapshot, options?.force);
 
 		const currentSnapshot = options?.currentSnapshot ?? (await getSnapshot({ database: this.knex }));
 		const diff = getSnapshotDiff(currentSnapshot, snapshot);
