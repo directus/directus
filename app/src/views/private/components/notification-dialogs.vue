8dd08f0e02c7303b768c197f57eb62ddc7825b1a
Brainslug <tim@brainslug.nl>
2023-02-14 12:42:58 +0100

    reverted missed App commit in the rebase

--- app/src/views/private/components/notification-dialogs.vue
+++ app/src/views/private/components/notification-dialogs.vue
@@ -49,6 +49,8 @@ export default defineComponent({
 ### Project details
 Directus Version: ${parsedInfo.value?.directus.version}
 Environment: ${import.meta.env.MODE}
+OS: ${parsedInfo.value?.os.type} ${parsedInfo.value?.os.version}
+Node: ${parsedInfo.value?.node.version}
 
 ### Error
 
