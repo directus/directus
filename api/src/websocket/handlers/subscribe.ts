9b8e8546f874b4315866a13eb4b2e7b638cc2add
Brainslug <br41nslug@users.noreply.github.com>
2023-02-27 17:27:30 +0100

    Implementing horizontal scaling options for Websockets (#17223)
    * implemented event sharing using the messenger
    
    * removed excessive logs
    
    * fixed handlers
    
    * cleaned some outdated code and added comments

--- api/src/websocket/handlers/subscribe.ts
+++ api/src/websocket/handlers/subscribe.ts
@@ -3,21 +3,58 @@ import { ItemsService } from '../../services/items';
 import type { Subscription, WebSocketClient } from '../types';
 import emitter from '../../emitter';
 import logger from '../../logger';
-import { fmtMessage } from '../utils/message';
+import { fmtMessage, trimUpper } from '../utils/message';
 import { refreshAccountability } from '../authenticate';
 import { MetaService } from '../../services';
 import { sanitizeQuery } from '../../utils/sanitize-query';
 import { handleWebsocketException, WebSocketException } from '../exceptions';
 import type { Accountability, SchemaOverview } from '@directus/shared/types';
 import { WebSocketSubscribeMessage } from '../messages';
+import { getMessenger, Messenger } from '../../messenger';
+import { z } from 'zod';
 
+//  move this to types!
+const WebSocketBaseEvent = z
+	.object({
+		action: z.enum(['create', 'update', 'delete']),
+		collection: z.string(),
+		payload: z.record(z.any()).optional(),
+	})
+	.passthrough();
+const WebSocketCreateEvent = WebSocketBaseEvent.extend({
+	action: z.literal('create'),
+	key: z.union([z.string(), z.number()]),
+});
+type WebSocketCreateEvent = z.infer<typeof WebSocketCreateEvent>;
+const WebSocketUpdateEvent = WebSocketBaseEvent.extend({
+	action: z.literal('update'),
+	keys: z.array(z.union([z.string(), z.number()])),
+});
+type WebSocketUpdateEvent = z.infer<typeof WebSocketUpdateEvent>;
+const WebSocketDeleteEvent = WebSocketBaseEvent.extend({
+	action: z.literal('delete'),
+	keys: z.array(z.union([z.string(), z.number()])),
+});
+type WebSocketDeleteEvent = z.infer<typeof WebSocketDeleteEvent>;
+type WebSocketEvent = WebSocketCreateEvent | WebSocketUpdateEvent | WebSocketDeleteEvent;
+
+/**
+ * Handler responsible for subscriptions
+ */
 export class SubscribeHandler {
+	// storage of subscriptions per collection
 	subscriptions: Record<string, Set<Subscription>>;
-
+	// internal message bus
+	protected messenger: Messenger;
+	/**
+	 * Initialize the handler
+	 */
 	constructor() {
 		this.subscriptions = {};
+		this.messenger = getMessenger();
 		this.bindWebsocket();
 		this.bindModules([
+			// move this to constants (or maybe make it configurable)
 			'items',
 			'activity',
 			'collections',
@@ -34,34 +71,60 @@ export class SubscribeHandler {
 			'webhooks',
 		]);
 	}
+	/**
+	 * Hook into websocket client lifecycle events
+	 */
 	bindWebsocket() {
+		// listen to incoming messages on the connected websockets
 		emitter.onAction('websocket.message', ({ client, message }) => {
+			if (!['SUBSCRIBE', 'UNSUBSCRIBE'].includes(trimUpper(message?.type))) return;
 			try {
 				this.onMessage(client, WebSocketSubscribeMessage.parse(message));
 			} catch (error) {
 				handleWebsocketException(client, error, 'subscribe');
 			}
 		});
+		// unsubscribe when a connection drops
 		emitter.onAction('websocket.error', ({ client }) => this.unsubscribe(client));
 		emitter.onAction('websocket.close', ({ client }) => this.unsubscribe(client));
 	}
+	/**
+	 * Hook into the Directus system evens by registering action hooks for each module
+	 * on all mutation events `<module>.create`/`<module>.update`/`<module>.delete`
+	 * @param modules List of modules to register action hooks for
+	 */
 	bindModules(modules: string[]) {
-		const bindAction = (event: string, mutator?: (args: any) => any) => {
+		const bindAction = (event: string, mutator?: (args: any) => Record<string, any>) => {
 			emitter.onAction(event, async (args: any) => {
-				const message = mutator ? mutator(args) : {};
-				message.action = event.split('.').pop();
-				message.collection = args.collection;
-				message.payload = args.payload;
-				logger.debug(`[ WS ] event ${event} ` /*- ${JSON.stringify(message)}`*/);
-				this.dispatch(message.collection, message);
+				// build the event object when the action hook fires
+				const message: Partial<WebSocketEvent> = mutator ? mutator(args) : {};
+				message.action = event.split('.').pop() as 'create' | 'update' | 'delete';
+				message.collection = args.collection as string;
+				message.payload = (args.payload ?? {}) as Record<string, any>;
+				// push the event through the Redis pub/sub
+				logger.debug(`[ WS ] event ${event} - ${JSON.stringify(message)}`);
+				this.messenger.publish('websocket.event', message as Record<string, any>);
 			});
 		};
 		for (const module of modules) {
 			bindAction(module + '.create', ({ key }: any) => ({ key }));
 			bindAction(module + '.update', ({ keys }: any) => ({ keys }));
-			bindAction(module + '.delete');
+			bindAction(module + '.delete', ({ keys }: any) => ({ keys }));
 		}
+		// listen to the Redis pub/sub and dispatch
+		this.messenger.subscribe('websocket.event', (message: Record<string, any>) => {
+			try {
+				this.dispatch(message as WebSocketEvent);
+			} catch (err) {
+				// dont error on an invalid event from the messenger
+				// logger.error('messenger error - ' + JSON.stringify(err, null, 2));
+			}
+		});
 	}
+	/**
+	 * Register a subscription
+	 * @param subscription
+	 */
 	subscribe(subscription: Subscription) {
 		const { collection } = subscription;
 		if (!this.subscriptions[collection]) {
@@ -69,12 +132,15 @@ export class SubscribeHandler {
 		}
 		this.subscriptions[collection]?.add(subscription);
 	}
+	/**
+	 * Remove a subscription
+	 * @param subscription
+	 */
 	unsubscribe(client: WebSocketClient, uid?: string) {
 		if (uid !== undefined) {
 			const subscription = this.getSubscription(uid);
 			if (subscription && subscription.client === client) {
 				this.subscriptions[subscription.collection]?.delete(subscription);
-				this.dispatch(subscription.collection, { action: 'focus' });
 			} else {
 				// logger.warn(`Couldn't find subscription with UID="${uid}" for current user`);
 			}
@@ -86,42 +152,34 @@ export class SubscribeHandler {
 					if (!subscription) continue;
 					if (subscription.client === client && (!uid || subscription.uid === uid)) {
 						this.subscriptions[key]?.delete(subscription);
-						this.dispatch(subscription.collection, { action: 'focus' });
 					}
 				}
 			}
 		}
 	}
-	async dispatch(collection: string, data: Record<string, any>) {
-		const subscriptions = this.subscriptions[collection] ?? new Set();
+	/**
+	 * Dispatch event to subscriptions
+	 */
+	async dispatch(event: WebSocketEvent) {
+		const subscriptions = this.subscriptions[event.collection] ?? new Set();
 		for (const subscription of subscriptions) {
 			const { client } = subscription;
-			if (data['action'] === 'focus' && !subscription.status) {
-				continue; // skip focus updates if not applicable
-			}
-			if (
-				data['action'] === 'status' &&
-				'item' in subscription &&
-				!(subscription.collection === 'directus_users' && subscription.status)
-			) {
-				continue; // skip status updates if not applicable
-			}
 			try {
-				const accountability = await refreshAccountability(client.accountability);
-				client.accountability = accountability;
+				client.accountability = await refreshAccountability(client.accountability);
 				const schema = await getSchema();
 				const result =
 					'item' in subscription
-						? await this.getSinglePayload(subscription, accountability, schema, data['action'])
-						: await this.getMultiPayload(subscription, accountability, schema, data['action']);
+						? await this.getSinglePayload(subscription, client.accountability, schema, event.action)
+						: await this.getMultiPayload(subscription, client.accountability, schema, event.action);
 				client.send(fmtMessage('subscription', result, subscription.uid));
 			} catch (err) {
-				// console.error(err);
 				handleWebsocketException(client, err, 'subscribe');
-				// logger.debug(`[WS REST] ERROR ${JSON.stringify(err)}`);
 			}
 		}
 	}
+	/**
+	 * Handle incoming (un)subscribe requests
+	 */
 	async onMessage(client: WebSocketClient, message: WebSocketSubscribeMessage) {
 		if (message.type === 'SUBSCRIBE') {
 			logger.debug(`[WS REST] SubscribeHandler ${JSON.stringify(message)}`);
@@ -159,9 +217,8 @@ export class SubscribeHandler {
 				}
 				// if no errors were thrown register the subscription
 				this.subscribe(subscription);
-				this.dispatch(subscription.collection, { action: 'focus' });
-				if (!subscription.status || !('item' in subscription) || subscription.collection !== 'directus_users') {
-					// prevent double events for init and focus
+				if (!('item' in subscription)) {
+					// prevent double events for init
 					client.send(fmtMessage('subscription', data, subscription.uid));
 				}
 			} catch (err) {
