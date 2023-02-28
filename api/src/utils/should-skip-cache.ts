372bd76b27678afa20bb47a44cb4e6657b4951ed
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2023-02-28 17:20:13 +0800

    skip cache for requests from data studio

--- api/src/utils/should-skip-cache.ts
+++ api/src/utils/should-skip-cache.ts
@@ -1,5 +1,6 @@
 import { Request } from 'express';
 import { getEnv } from '../env';
+import { Url } from './url';
 
 /**
  * Whether to skip caching for the current request
@@ -11,6 +12,10 @@ export function shouldSkipCache(req: Request): boolean {
 	const env = getEnv();
 
 	if (env.CACHE_SKIP_ALLOWED === false) return false;
+
+	// always skip cache for requests coming from the data studio based on Referer header
+	const adminUrl = new Url(env.PUBLIC_URL).addPath('admin').toString();
+	if (req.get('Referer')?.startsWith(adminUrl)) return true;
 
 	return req.get('cache-control')?.includes('no-store') ?? false;
 }
