d2d520c2ab2f5144f0e8ca2a4b4cd59002552a3c
Chirica Gheorghe <gheorghi.chirica@crawless.com>
2023-03-02 23:21:21 +0200

    Add colab extension

--- crawless_colab_extensions.sh
+++ crawless_colab_extensions.sh
@@ -5,6 +5,7 @@ DIRECTUS_EXTENSIONS="${DIRECTUS_DIR}/extensions"
 DIRECTUS_ENDPOINTS="${DIRECTUS_EXTENSIONS}/endpoints"
 DIRECTUS_HOOKS="${DIRECTUS_EXTENSIONS}/hooks"
 DIRECTUS_MODULES="${DIRECTUS_EXTENSIONS}/modules"
+DIRECTUS_INTERFACES="${DIRECTUS_EXTENSIONS}/interfaces"
 
 curl --header "Private-Token: ${GITLAB_PIPELINE_TOKEN}" -LO ${CI_API_V4_URL}/projects/40500377/packages/generic/Releases/0.0.1/directus-custom-extensions-release.zip
 
@@ -13,3 +14,4 @@ unzip directus-custom-extensions-release.zip
 ls -la ./directus-custom-extensions-release
 
 cp -r ./directus-custom-extensions-release/collab-hook ${DIRECTUS_HOOKS}
+cp -r ./directus-custom-extensions-release/filter-ext ${DIRECTUS_INTERFACES}
