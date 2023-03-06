4006d78805f6a2f06031ca40cc6342b0bfffe14c
Chirica Gheorghe <gheorghi.chirica@crawless.com>
2023-03-06 19:10:21 +0200

    Add colab-ext migrations

--- crawless_colab_extensions.sh
+++ crawless_colab_extensions.sh
@@ -2,10 +2,10 @@
 
 DIRECTUS_DIR="/directus/api"
 DIRECTUS_EXTENSIONS="${DIRECTUS_DIR}/extensions"
-DIRECTUS_ENDPOINTS="${DIRECTUS_EXTENSIONS}/endpoints"
 DIRECTUS_HOOKS="${DIRECTUS_EXTENSIONS}/hooks"
-DIRECTUS_MODULES="${DIRECTUS_EXTENSIONS}/modules"
 DIRECTUS_INTERFACES="${DIRECTUS_EXTENSIONS}/interfaces"
+
+mkdir -p ${DIRECTUS_EXTENSIONS}/migrations
 
 curl --header "Private-Token: ${GITLAB_PIPELINE_TOKEN}" -LO ${CI_API_V4_URL}/projects/40500377/packages/generic/Releases/0.0.1/directus-custom-extensions-release.zip
 
@@ -15,3 +15,4 @@ ls -la ./directus-custom-extensions-release
 
 cp -r ./directus-custom-extensions-release/collab-hook ${DIRECTUS_HOOKS}
 cp -r ./directus-custom-extensions-release/filter-ext ${DIRECTUS_INTERFACES}
+cp -r ./directus-custom-extensions-release/collab-hook/migrations/* ${DIRECTUS_EXTENSIONS}/migrations
