0fe0b28b8a97f8be04a2a7542b81667b4963a7cf
Nitwel <mail@nitwel.de>
2023-03-08 14:45:07 +0100

    Fix deleting data on reupload

--- api/src/services/files.ts
+++ api/src/services/files.ts
@@ -21,6 +21,7 @@ import { ItemsService } from './items';
 
 // @ts-ignore
 import formatTitle from '@directus/format-title';
+import { getSchema } from '../utils/get-schema';
 
 export class FilesService extends ItemsService {
 	constructor(options: AbstractServiceOptions) {
@@ -38,7 +39,18 @@ export class FilesService extends ItemsService {
 	): Promise<PrimaryKey> {
 		const storage = await getStorage();
 
-		const payload = clone(data);
+		let payload = clone(data);
+
+		if (primaryKey !== undefined) {
+			// Include existing data in the payload, so we don't overwrite user set values
+			const schema = await getSchema();
+			const fieldsToExclude = ['id', 'filename_disk', 'type', 'uploaded_by', 'uploaded_on', 'modified_by', 'modified_on', 'charset', 'filesize', 'width', 'height', 'duration', 'embed', 'metadata']
+			const fieldsToKeep = Object.keys(schema.collections[this.collection].fields).filter((field) => fieldsToExclude.includes(field) === false)
+
+			const existingData = (await this.knex.select(fieldsToKeep).from('directus_files').where({ id: primaryKey }).first()) ?? {};
+
+			payload = {...existingData, ...payload}
+		}
 
 		if ('folder' in payload === false) {
 			const settings = await this.knex.select('storage_default_folder').from('directus_settings').first();
