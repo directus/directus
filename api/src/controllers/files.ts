7ab00b6c52204b6c53fbe0df132d67fb422704d4
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-03-16 00:31:51 +0100

    Remove unused import of 'File'

--- api/src/controllers/files.ts
+++ api/src/controllers/files.ts
@@ -9,7 +9,7 @@ import { respond } from '../middleware/respond';
 import useCollection from '../middleware/use-collection';
 import { validateBatch } from '../middleware/validate-batch';
 import { FilesService, MetaService } from '../services';
-import { File, PrimaryKey } from '../types';
+import { PrimaryKey } from '../types';
 import asyncHandler from '../utils/async-handler';
 
 // @ts-ignore
