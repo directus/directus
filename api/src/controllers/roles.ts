3ff37b0f328bbf33eb95572057a393a1278e2f03
ian <licitdev@gmail.com>
2023-03-15 22:25:12 +0800

    Sanitize query in updateByQuery and deleteByQuery (#17649)
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>

--- api/src/controllers/roles.ts
+++ api/src/controllers/roles.ts
@@ -6,6 +6,7 @@ import { validateBatch } from '../middleware/validate-batch';
 import { MetaService, RolesService } from '../services';
 import { PrimaryKey } from '../types';
 import asyncHandler from '../utils/async-handler';
+import { sanitizeQuery } from '../utils/sanitize-query';
 
 const router = express.Router();
 
@@ -103,7 +104,8 @@ router.patch(
 		} else if (req.body.keys) {
 			keys = await service.updateMany(req.body.keys, req.body.data);
 		} else {
-			keys = await service.updateByQuery(req.body.query, req.body.data);
+			const sanitizedQuery = sanitizeQuery(req.body.query, req.accountability);
+			keys = await service.updateByQuery(sanitizedQuery, req.body.data);
 		}
 
 		try {
@@ -162,7 +164,8 @@ router.delete(
 		} else if (req.body.keys) {
 			await service.deleteMany(req.body.keys);
 		} else {
-			await service.deleteByQuery(req.body.query);
+			const sanitizedQuery = sanitizeQuery(req.body.query, req.accountability);
+			await service.deleteByQuery(sanitizedQuery);
 		}
 
 		return next();
