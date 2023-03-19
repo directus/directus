c9595d0838d235b713cfa79def90a4b734c0a748
ian <licitdev@gmail.com>
2023-03-20 02:56:57 +0800

    Spam more requests over longer period

--- tests-blackbox/routes/assets/concurrency.test.ts
+++ tests-blackbox/routes/assets/concurrency.test.ts
@@ -24,53 +24,62 @@ describe('/assets', () => {
 					'%s',
 					async (vendor) => {
 						// Setup
+						const totalRounds = 5;
 						const count = 100;
-						const secondsTotal = 3;
+						const secondsTotal = 2;
 						const msDelay = 100;
+						const timeout = 30000;
 						const insertResponse = await request(getUrl(vendor))
 							.post('/files')
 							.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
 							.field('storage', storage)
 							.attach('file', createReadStream(imageFilePath));
-						const requests = [];
 
 						// Action
-						for (let round = 0; round < secondsTotal * 1000; round += msDelay) {
-							const requestsInner = [];
+						async function spamRequests() {
+							const requests = [];
 
-							for (let i = 0; i < count; i++) {
-								requestsInner.push(
-									request(getUrl(vendor))
-										.get(`/assets/${insertResponse.body.data.id}?cache-buster=${uuid()}`)
-										.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
-										.timeout(15000)
-								);
+							for (let duration = 0; duration < secondsTotal * 1000; duration += msDelay) {
+								const requestsInner = [];
+
+								for (let i = 0; i < count; i++) {
+									requestsInner.push(
+										request(getUrl(vendor))
+											.get(`/assets/${insertResponse.body.data.id}?cache-buster=${uuid()}`)
+											.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+											.timeout(timeout)
+									);
+								}
+
+								requests.push(Promise.all(requestsInner));
+								await sleep(msDelay);
 							}
 
-							requests.push(Promise.all(requestsInner));
-							await sleep(msDelay);
+							return requests;
 						}
 
-						let responses;
+						for (let i = 0; i < totalRounds; i++) {
+							let responses;
 
-						try {
-							responses = await Promise.all(requests);
-						} catch (error) {
-							expect(error).toBeUndefined();
-							return;
-						}
+							try {
+								responses = await Promise.all(await spamRequests());
+							} catch (error) {
+								expect(error).toBeUndefined();
+								return;
+							}
 
-						// Assert
-						for (const round of responses) {
-							for (const response of round) {
-								expect(response.statusCode).toBe(200);
-								expect(response.headers['content-type']).toBe(imageFile.type);
-								expect(response.headers['content-length']).toBe(imageFile.filesize);
-								expect(Buffer.compare(response.body, await readFileSync(imageFilePath))).toBe(0);
+							// Assert
+							for (const round of responses) {
+								for (const response of round) {
+									expect(response.statusCode).toBe(200);
+									expect(response.headers['content-type']).toBe(imageFile.type);
+									expect(response.headers['content-length']).toBe(imageFile.filesize);
+									expect(Buffer.compare(response.body, await readFileSync(imageFilePath))).toBe(0);
+								}
 							}
 						}
 					},
-					180000
+					300000
 				);
 			});
 		});
