103bda58f12479f0275f6719f5d4ff64ac66008f
rijkvanzanten <rijkvanzanten@me.com>
2023-03-25 13:32:35 -0400

    Fix specs

--- packages/specs/index.js
+++ packages/specs/index.js
@@ -1,2 +1,7 @@
 import { readFileSync } from 'node:fs';
-export const spec = JSON.stringify(readFileSync('./dist/openapi.json'));
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const spec = JSON.stringify(readFileSync(resolve(__dirname, './dist/openapi.json')));
