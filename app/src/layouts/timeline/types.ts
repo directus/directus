7ecb7cb586ffc08ca8408401651d154b5674f086
Nitwel <mail@nitwel.de>
2023-03-29 15:38:57 +0200

    run linter

--- app/src/layouts/timeline/types.ts
+++ app/src/layouts/timeline/types.ts
@@ -1,36 +1,36 @@
 export type LayoutOptions = {
-	dateField: string,
-	timeField?: string,
-	title: string,
-	userField: string
+	dateField: string;
+	timeField?: string;
+	title: string;
+	userField: string;
 };
 
 export type Day = {
-	year: number,
-	month: number,
-	day: number,
-	event_count: number,
-}
+	year: number;
+	month: number;
+	day: number;
+	event_count: number;
+};
 
 export type Event = {
-	id: string | number,
-	title: string,
-	time?: Date,
-	item: any,
+	id: string | number;
+	title: string;
+	time?: Date;
+	item: any;
 	user?: {
-		id: string,
-		first_name: string,
-		last_name: string,
-		image?: string,
+		id: string;
+		first_name: string;
+		last_name: string;
+		image?: string;
 		avatar?: {
-			id: string,
-			storage: string,
-			filename_disk: string,
-			type: string,
-			modified_on: string
-		}
-	}
-}
+			id: string;
+			storage: string;
+			filename_disk: string;
+			type: string;
+			modified_on: string;
+		};
+	};
+};
 
 export type LayoutQuery = {
 	fields?: string[];
