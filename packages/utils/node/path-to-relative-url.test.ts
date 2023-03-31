09c5f740de55e239bf64952976b9f3c39d77b394
rijkvanzanten <rijkvanzanten@me.com>
2023-03-31 16:00:46 -0400

    Fix test

--- packages/utils/node/path-to-relative-url.test.ts
+++ packages/utils/node/path-to-relative-url.test.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { describe, expect, it } from 'vitest';
 import { pathToRelativeUrl } from './path-to-relative-url.js';
+import { dirname } from 'node:path/posix';
+import { fileURLToPath } from 'node:url';
 
 describe('pathToRelativeUrl', () => {
 	const filePath = path.resolve(path.join('foo', 'bar.txt'));
@@ -12,8 +14,8 @@ describe('pathToRelativeUrl', () => {
 	});
 
 	it('returns a URL relative to the given path if it is passed as the second argument', async () => {
-		const relativeUrl = pathToRelativeUrl(filePath, __dirname);
+		const relativeUrl = pathToRelativeUrl(filePath, dirname(fileURLToPath(import.meta.url)));
 
-		expect(relativeUrl).toBe('../../../foo/bar.txt');
+		expect(relativeUrl).toBe('../foo/bar.txt');
 	});
 });
