e22ec9dd4c02a7850b8e3807ba53c05ca0278bb9
Hannes Küttner <kuettner.hannes@gmail.com>
2023-04-01 12:40:17 +0200

    Add directus_presets default icon migration

---
+++ api/src/database/migrations/20230401A-update-default-value-bookmark-icon.ts
@@ -0,0 +1,22 @@
+import type { Knex } from 'knex';
+import { getHelpers } from '../helpers';
+
+export async function up(knex: Knex): Promise<void> {
+	const helper = getHelpers(knex).schema;
+
+	await helper.changeToType('directus_presets', 'icon', 'string', {
+		nullable: true,
+		default: 'bookmark',
+		length: 30,
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	const helper = getHelpers(knex).schema;
+
+	await helper.changeToType('directus_presets', 'icon', 'string', {
+		nullable: true,
+		default: 'bookmark_outline',
+		length: 30,
+	});
+}
