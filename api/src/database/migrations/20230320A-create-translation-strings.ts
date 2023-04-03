e8dd46361b44fb3fa5abf30f06e7235f064051ab
Brainslug <tim@brainslug.nl>
2023-04-03 12:25:47 +0200

    fixed migration

--- api/src/database/migrations/20230320A-create-translation-strings.ts
+++ api/src/database/migrations/20230320A-create-translation-strings.ts
@@ -4,7 +4,7 @@ import { set } from 'lodash';
 type NewTranslationString = {
 	key: string;
 	value: string;
-	lang: string;
+	language: string;
 };
 
 type OldTranslationString = {
@@ -15,8 +15,8 @@ type OldTranslationString = {
 function transformStringsNewFormat(oldStrings: OldTranslationString[]): NewTranslationString[] {
 	return oldStrings.reduce<NewTranslationString[]>((result, item) => {
 		if (!item.key || !item.translations) return result;
-		for (const [lang, value] of Object.entries(item.translations)) {
-			result.push({ key: item.key, lang, value });
+		for (const [language, value] of Object.entries(item.translations)) {
+			result.push({ key: item.key, language, value });
 		}
 		return result;
 	}, []);
@@ -24,44 +24,46 @@ function transformStringsNewFormat(oldStrings: OldTranslationString[]): NewTrans
 
 function transformStringsOldFormat(newStrings: NewTranslationString[]): OldTranslationString[] {
 	const keyCache: Record<string, Record<string, string>> = {};
-	for (const { key, lang, value } of newStrings) {
-		if (key in keyCache) {
-			set(keyCache, [key, lang], value);
-		}
+	for (const { key, language, value } of newStrings) {
+		set(keyCache, [key, language], value);
 	}
 	return Object.entries(keyCache).map(([key, translations]) => ({ key, translations } as OldTranslationString));
 }
 
 export async function up(knex: Knex): Promise<void> {
-	const data = await knex.select('translation_strings', 'id').from('directus_settings').first();
+	await knex.schema.createTable('directus_translation_strings', (table) => {
+		table.increments('id', { primaryKey: true });
+		table.string('language').notNullable();
+		table.string('key').notNullable();
+		table.string('value').notNullable();
+	});
 
+	const data = await knex.select('translation_strings', 'id').from('directus_settings').first();
 	if (data?.translation_strings && data?.id) {
 		const parsedTranslationStrings =
 			typeof data.translation_strings === 'string' ? JSON.parse(data.translation_strings) : data.translation_strings;
 
 		const newTranslationStrings = transformStringsNewFormat(parsedTranslationStrings);
 
-		await knex('directus_settings')
-			.where({ id: data.id })
-			.update({
-				translation_strings: JSON.stringify(newTranslationStrings),
-			});
+		for (const item of newTranslationStrings) {
+			await knex('directus_translation_strings').insert(item);
+		}
 	}
 }
 
 export async function down(knex: Knex): Promise<void> {
-	const data = await knex.select('translation_strings', 'id').from('directus_settings').first();
+	const data = await knex.select('language', 'key', 'value').from('directus_translation_strings');
+	const settingsId = await knex.select('id').from('directus_settings').first();
 
-	if (data?.translation_strings && data?.id) {
-		const parsedTranslationStrings =
-			typeof data.translation_strings === 'string' ? JSON.parse(data.translation_strings) : data.translation_strings;
-
-		const oldTranslationStrings = transformStringsOldFormat(parsedTranslationStrings);
+	if (settingsId?.id && data) {
+		const oldTranslationStrings = transformStringsOldFormat(data);
 
 		await knex('directus_settings')
-			.where({ id: data.id })
+			.where({ id: settingsId.id })
 			.update({
 				translation_strings: JSON.stringify(oldTranslationStrings),
 			});
 	}
+
+	await knex.schema.dropTable('directus_translation_strings');
 }
