cc80b7a44b355a06892b6502374c3b75a7100d06
Brainslug <tim@brainslug.nl>
2023-04-05 13:36:13 +0200

    added missing import

--- api/src/utils/sanitize-query.test.ts
+++ api/src/utils/sanitize-query.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi, beforeEach } from 'vitest';
+import { describe, expect, test, vi, beforeEach, afterEach } from 'vitest';
 import { refreshEnv } from '../env.js';
 import { sanitizeQuery } from './sanitize-query.js';
 
