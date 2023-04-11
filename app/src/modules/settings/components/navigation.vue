89e2ba6a3299d3ebc6d950b1555fefd9c97ebbb4
rijkvanzanten <rijkvanzanten@me.com>
2023-04-11 14:59:46 -0400

    Simplify simplify simplify

--- app/src/modules/settings/components/navigation.vue
+++ app/src/modules/settings/components/navigation.vue
@@ -30,7 +30,6 @@ import { useI18n } from 'vue-i18n';
 import { defineComponent, computed } from 'vue';
 import { version } from '../../../../package.json';
 import { useProjectInfo } from '../composables/use-project-info';
-import { getGitHubIssueLink } from '@/utils/get-github-issue-link';
 
 export default defineComponent({
 	setup() {
@@ -81,12 +80,12 @@ export default defineComponent({
 				{
 					icon: 'bug_report',
 					name: t('report_bug'),
-					href: getGitHubIssueLink(projectInfo),
+					href: 'https://github.com/directus/directus/issues/new?template=bug_report.yml',
 				},
 				{
 					icon: 'new_releases',
 					name: t('request_feature'),
-					href: 'https://github.com/directus/directus/discussions/new',
+					href: 'https://github.com/directus/directus/discussions/new?category=feature-requests',
 				},
 			];
 		});
--- app/src/views/private/components/notification-dialogs.vue
+++ app/src/views/private/components/notification-dialogs.vue
@@ -10,7 +10,7 @@
 				</v-card-text>
 				<v-card-actions>
 					<v-button v-if="notification.type === 'error' && admin && notification.code === 'UNKNOWN'" secondary>
-						<a target="_blank" :href="getReportLink(notification)">
+						<a target="_blank" href="https://github.com/directus/directus/issues/new?template=bug_report.yml">
 							{{ t('report_error') }}
 						</a>
 					</v-button>
@@ -22,40 +22,21 @@
 </template>
 
 <script lang="ts">
-import { useI18n } from 'vue-i18n';
-import { defineComponent, computed } from 'vue';
 import { useNotificationsStore } from '@/stores/notifications';
 import { useUserStore } from '@/stores/user';
-import { getGitHubIssueLink } from '@/utils/get-github-issue-link';
-import { Snackbar } from '@/types/notifications';
-import { useProjectInfo } from '@/modules/settings/composables/use-project-info';
+import { computed, defineComponent } from 'vue';
+import { useI18n } from 'vue-i18n';
 
 export default defineComponent({
 	setup() {
 		const { t } = useI18n();
-
-		const { parsedInfo: projectInfo } = useProjectInfo();
 
 		const notificationsStore = useNotificationsStore();
 		const userStore = useUserStore();
 
 		const notifications = computed(() => notificationsStore.dialogs);
 
-		return { t, notifications, admin: userStore.isAdmin, done, getReportLink };
-
-		function getReportLink(notification: Snackbar) {
-			const errorLines = [
-				`**Title:** ${notification.title || 'none'}`,
-				`**Message:** ${notification.text || 'none'}`,
-				'<details><summary><b>Stack Trace</b></summary>',
-				'<pre lang="json">',
-				JSON.stringify(notification.error, Object.getOwnPropertyNames(notification.error), 2),
-				'</pre>',
-				'</details>',
-			];
-
-			return getGitHubIssueLink(projectInfo, { errors: errorLines.join('\n') });
-		}
+		return { t, notifications, admin: userStore.isAdmin, done };
 
 		function done(id: string) {
 			notificationsStore.remove(id);
