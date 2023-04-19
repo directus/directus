80361e7d37dcd792c17c90f794026fd83ecf902a
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2023-04-19 19:31:10 +0200

    Remove extension config import workaround

--- packages/extensions-sdk/src/cli/commands/helpers/load-config.ts
+++ packages/extensions-sdk/src/cli/commands/helpers/load-config.ts
@@ -6,14 +6,12 @@ import type { Config } from '../../types.js';
 
 export const CONFIG_FILE_NAMES = ['extension.config.js', 'extension.config.mjs', 'extension.config.cjs'];
 
-// This is needed to work around Typescript always transpiling import() to require() for CommonJS targets.
-const _import = new Function('url', 'return import(url)');
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default async function loadConfig(): Promise<Config> {
 	for (const fileName of CONFIG_FILE_NAMES) {
 		if (await fse.pathExists(fileName)) {
-			const configFile = await _import(pathToRelativeUrl(path.resolve(fileName), __dirname));
+			const configFile = await import(pathToRelativeUrl(path.resolve(fileName), __dirname));
 
 			return configFile.default;
 		}
