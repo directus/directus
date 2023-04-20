2e5f063df10fd844b94a75b97da6f34e3a06460b
Andrii Trubai <trubay.andrey@gmail.com>
2023-04-20 16:12:48 +0300

    Add possibility of sorting items in tabular view of o2m interface (#15733)
    * Add possibility of sorting items in tabular view of o2m interface
    
    * CLA signing
    
    * in between commit
    
    * allow for resizing on m2m and use existing sort fn
    
    ---------
    
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>
    Co-authored-by: Azri Kahar <42867097+azrikahar@users.noreply.github.com>
    Co-authored-by: Rijk van Zanten <rijkvanzanten@me.com>
    Co-authored-by: Nitwel <mail@nitwel.de>

--- app/src/interfaces/list-o2m/list-o2m.vue
+++ app/src/interfaces/list-o2m/list-o2m.vue
@@ -50,10 +50,13 @@
 				v-model:headers="headers"
 				:class="{ 'no-last-border': totalItemCount <= 10 }"
 				:loading="loading"
+				:show-manual-sort="allowDrag"
+				:manual-sort-key="relationInfo?.sortField"
 				:items="displayItems"
 				:row-height="tableRowHeight"
 				show-resize
 				@click:row="editRow"
+				@update:items="sortItems"
 			>
 				<template v-for="header in headers" :key="header.value" #[`item.${header.value}`]="{ item }">
 					<render-template
