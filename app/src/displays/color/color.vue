779afbc1c48ebd27a9bad09746f0aeefc9759f94
Hannes Küttner <kuettner.hannes@gmail.com>
2023-04-21 22:00:36 +0200

    Migrate displays/colors (and remove unused displayTemplate)

--- app/src/displays/color/color.vue
+++ app/src/displays/color/color.vue
@@ -5,53 +5,44 @@
 	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed } from 'vue';
+<script lang="ts" setup>
+import { computed } from 'vue';
 import Color from 'color';
 import { isHex } from '@/utils/is-hex';
 
-export default defineComponent({
-	props: {
-		value: {
-			type: String,
-			default: null,
-		},
-		defaultColor: {
-			type: String,
-			default: '#B0BEC5',
-			validator: (value: string) => value === null || isHex(value) || value.startsWith('var(--'),
-		},
+const props = defineProps({
+	value: {
+		type: String as () => string | null,
+		default: null,
 	},
-	setup(props) {
-		const displayValue = computed(() => {
-			return props.value;
-		});
-
-		const styles = computed(() => {
-			const defaultColor = props.defaultColor?.startsWith('var(') ? getVar(props.defaultColor) : props.defaultColor;
-			const value = props.value?.startsWith('var(') ? getVar(props.value) : props.value;
-
-			const style: Record<string, any> = { 'background-color': defaultColor };
-
-			if (value !== null) style['background-color'] = value;
-
-			const pageColorString = getVar('var(--background-page)');
-
-			const pageColorRGB = Color(pageColorString);
-			const colorRGB = value === null ? Color(defaultColor) : Color(value);
-
-			if (colorRGB.contrast(pageColorRGB) < 1.1) style['border'] = '1px solid var(--border-normal)';
-
-			return style;
-		});
-
-		return { displayValue, styles };
-
-		function getVar(cssVar: string) {
-			return getComputedStyle(document.body).getPropertyValue(cssVar.slice(4, -1)).trim();
-		}
+	defaultColor: {
+		type: String,
+		default: '#B0BEC5',
+		validator: (value: string) => value === null || isHex(value) || value.startsWith('var(--'),
 	},
 });
+
+const styles = computed(() => {
+	const defaultColor = props.defaultColor?.startsWith('var(') ? getVar(props.defaultColor) : props.defaultColor;
+	const value = props.value?.startsWith('var(') ? getVar(props.value) : props.value;
+
+	const style: Record<string, any> = { 'background-color': defaultColor };
+
+	if (value !== null) style['background-color'] = value;
+
+	const pageColorString = getVar('var(--background-page)');
+
+	const pageColorRGB = Color(pageColorString);
+	const colorRGB = value === null ? Color(defaultColor) : Color(value);
+
+	if (colorRGB.contrast(pageColorRGB) < 1.1) style['border'] = '1px solid var(--border-normal)';
+
+	return style;
+});
+
+function getVar(cssVar: string) {
+	return getComputedStyle(document.body).getPropertyValue(cssVar.slice(4, -1)).trim();
+}
 </script>
 
 <style lang="scss" scoped>
