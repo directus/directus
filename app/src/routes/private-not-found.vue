f634df361c9a28b31e522301176e760b6d377ce6
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-04-23 19:57:08 +0200

    routes/private-not-found

--- app/src/routes/private-not-found.vue
+++ app/src/routes/private-not-found.vue
@@ -8,16 +8,10 @@
 	</private-view>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { useI18n } from 'vue-i18n';
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-	setup() {
-		const { t } = useI18n();
-		return { t };
-	},
-});
+const { t } = useI18n();
 </script>
 
 <style lang="scss" scoped>
