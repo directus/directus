48fb2ea1702469d5dfa370372ea4b813d38e49a0
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-04-24 10:16:36 +0200

    interfaces/presentation-divider/presentation-divider

--- app/src/interfaces/presentation-divider/presentation-divider.vue
+++ app/src/interfaces/presentation-divider/presentation-divider.vue
@@ -13,29 +13,13 @@
 	</v-divider>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-	props: {
-		color: {
-			type: String,
-			default: null,
-		},
-		icon: {
-			type: String,
-			default: null,
-		},
-		title: {
-			type: String,
-			default: null,
-		},
-		inlineTitle: {
-			type: Boolean,
-			default: false,
-		},
-	},
-});
+<script setup lang="ts">
+defineProps<{
+	color?: string;
+	icon?: string;
+	title?: string;
+	inlineTitle?: boolean;
+}>();
 </script>
 
 <style lang="scss" scoped>
