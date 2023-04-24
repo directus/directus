2a66e5fcde74780046eee8c39aaff74e4d5791c8
ian <licitdev@gmail.com>
2023-04-25 01:47:32 +0800

    Update tests

--- tests/blackbox/routes/flows/webhook.test.ts
+++ tests/blackbox/routes/flows/webhook.test.ts
@@ -47,77 +47,176 @@ describe('/flows', () => {
 	});
 
 	describe('Webhook Trigger', () => {
-		it.each(vendors)('%s', async (vendor) => {
-			// Setup
-			const env = envs[vendor];
+		describe('cacheEnabled works for GET', () => {
+			it.each(vendors)('%s', async (vendor) => {
+				// Setup
+				const env = envs[vendor];
 
-			const payloadFlowCreate = {
-				name: 'webhook flow',
-				icon: 'bolt',
-				color: null,
-				description: null,
-				status: 'active',
-				accountability: null,
-				trigger: 'webhook',
-				options: {},
-			};
+				const payloadFlowCreate = {
+					name: 'webhook flow',
+					icon: 'bolt',
+					color: null,
+					description: null,
+					status: 'active',
+					accountability: null,
+					trigger: 'webhook',
+					options: {},
+				};
 
-			const payloadOperationCreate = {
-				position_x: 19,
-				position_y: 1,
-				name: 'Get epoch milliseconds',
-				key: 'op_exev',
-				type: 'exec',
-				options: { code: 'module.exports = async function() { return { epoch: Date.now() }; }' },
-			};
+				const payloadOperationCreate = {
+					position_x: 19,
+					position_y: 1,
+					name: 'Get epoch milliseconds',
+					key: 'op_exev',
+					type: 'exec',
+					options: { code: 'module.exports = async function() { return { epoch: Date.now() }; }' },
+				};
 
-			const flowId = (
+				const flowId = (
+					await request(getUrl(vendor, env))
+						.post('/flows')
+						.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+						.query({ fields: ['id'] })
+						.send(payloadFlowCreate)
+				).body.data.id;
+
+				const flowCacheEnabledId = (
+					await request(getUrl(vendor, env))
+						.post('/flows')
+						.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+						.query({ fields: ['id'] })
+						.send({
+							...payloadFlowCreate,
+							name: 'webhook flow cache disabled',
+							options: { ...payloadFlowCreate.options, cacheEnabled: true },
+						})
+				).body.data.id;
+
+				const flowCacheDisabledId = (
+					await request(getUrl(vendor, env))
+						.post('/flows')
+						.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+						.query({ fields: ['id'] })
+						.send({
+							...payloadFlowCreate,
+							name: 'webhook flow cache enabled',
+							options: { ...payloadFlowCreate.options, cacheEnabled: false },
+						})
+				).body.data.id;
+
 				await request(getUrl(vendor, env))
-					.post('/flows')
+					.patch(`/flows/${flowId}`)
 					.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
-					.query({ fields: ['id'] })
-					.send(payloadFlowCreate)
-			).body.data.id;
+					.send({ operation: { ...payloadOperationCreate, flow: flowId } });
 
-			const flowCacheDisabledId = (
 				await request(getUrl(vendor, env))
-					.post('/flows')
+					.patch(`/flows/${flowCacheEnabledId}`)
 					.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
-					.query({ fields: ['id'] })
-					.send({
-						...payloadFlowCreate,
-						name: 'webhook flow cacheEnabled set to false',
-						options: { cacheEnabled: false },
-					})
-			).body.data.id;
+					.send({ operation: { ...payloadOperationCreate, flow: flowCacheEnabledId } });
 
-			await request(getUrl(vendor, env))
-				.patch(`/flows/${flowId}`)
-				.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
-				.send({ operation: { ...payloadOperationCreate, flow: flowId } });
+				await request(getUrl(vendor, env))
+					.patch(`/flows/${flowCacheDisabledId}`)
+					.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+					.send({ operation: { ...payloadOperationCreate, flow: flowCacheDisabledId } });
 
-			await request(getUrl(vendor, env))
-				.patch(`/flows/${flowCacheDisabledId}`)
-				.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
-				.send({ operation: { ...payloadOperationCreate, flow: flowCacheDisabledId } });
+				// Action
+				const responseDefault = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowId}`);
+				const responseCacheEnabled = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowCacheEnabledId}`);
+				const responseCacheDisabled = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowCacheDisabledId}`);
 
-			// Action
-			const response = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowId}`);
-			const responseCacheDisabled = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowCacheDisabledId}`);
+				await sleep(100);
 
-			await sleep(100);
+				const responseDefault2 = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowId}`);
+				const responseCacheEnabled2 = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowCacheEnabledId}`);
+				const responseCacheDisabled2 = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowCacheDisabledId}`);
 
-			const response2 = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowId}`);
-			const responseCacheDisabled2 = await request(getUrl(vendor, env)).get(`/flows/trigger/${flowCacheDisabledId}`);
+				// Assert
+				expect(responseDefault.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseCacheEnabled.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseCacheDisabled.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseDefault2.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseCacheEnabled.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseCacheDisabled2.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
 
-			// Assert
-			expect(response.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
-			expect(responseCacheDisabled.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
-			expect(response2.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
-			expect(responseCacheDisabled2.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseDefault.body).toEqual(responseDefault2.body);
+				expect(responseCacheEnabled.body).toEqual(responseCacheEnabled2.body);
+				expect(responseCacheDisabled.body).not.toEqual(responseCacheDisabled2.body);
+			});
+		});
 
-			expect(response.body).toEqual(response2.body);
-			expect(responseCacheDisabled.body).not.toEqual(responseCacheDisabled2.body);
+		describe('ignores cacheEnabled for POST', () => {
+			it.each(vendors)('%s', async (vendor) => {
+				// Setup
+				const env = envs[vendor];
+
+				const payloadFlowCreate = {
+					name: 'POST webhook flow',
+					icon: 'bolt',
+					color: null,
+					description: null,
+					status: 'active',
+					accountability: null,
+					trigger: 'webhook',
+					options: { method: 'POST' },
+				};
+
+				const payloadOperationCreate = {
+					position_x: 19,
+					position_y: 1,
+					name: 'Get epoch milliseconds',
+					key: 'op_exev',
+					type: 'exec',
+					options: { code: 'module.exports = async function() { return { epoch: Date.now() }; }' },
+				};
+
+				const flowId = (
+					await request(getUrl(vendor, env))
+						.post('/flows')
+						.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+						.query({ fields: ['id'] })
+						.send(payloadFlowCreate)
+				).body.data.id;
+
+				const flowCacheEnabledId = (
+					await request(getUrl(vendor, env))
+						.post('/flows')
+						.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+						.query({ fields: ['id'] })
+						.send({
+							...payloadFlowCreate,
+							name: 'POST webhook flow cache enabled',
+							options: { ...payloadFlowCreate.options, cacheEnabled: false },
+						})
+				).body.data.id;
+
+				await request(getUrl(vendor, env))
+					.patch(`/flows/${flowId}`)
+					.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+					.send({ operation: { ...payloadOperationCreate, flow: flowId } });
+
+				await request(getUrl(vendor, env))
+					.patch(`/flows/${flowCacheEnabledId}`)
+					.set('Authorization', `Bearer ${common.USER.ADMIN.TOKEN}`)
+					.send({ operation: { ...payloadOperationCreate, flow: flowCacheEnabledId } });
+
+				// Action
+				const responseDefault = await request(getUrl(vendor, env)).post(`/flows/trigger/${flowId}`);
+				const responseCacheEnabled = await request(getUrl(vendor, env)).post(`/flows/trigger/${flowCacheEnabledId}`);
+
+				await sleep(100);
+
+				const responseDefault2 = await request(getUrl(vendor, env)).post(`/flows/trigger/${flowId}`);
+				const responseCacheEnabled2 = await request(getUrl(vendor, env)).post(`/flows/trigger/${flowCacheEnabledId}`);
+
+				// Assert
+				expect(responseDefault.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseCacheEnabled.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseDefault2.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+				expect(responseCacheEnabled2.body).toEqual(expect.objectContaining({ epoch: expect.any(Number) }));
+
+				expect(responseDefault.body).not.toEqual(responseDefault2.body);
+				expect(responseCacheEnabled.body).not.toEqual(responseCacheEnabled2.body);
+			});
 		});
 	});
 });
