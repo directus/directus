871e7eb95419bcb6b9e1d6028141a3e35b7e27c5
Nitwel <mail@nitwel.de>
2023-04-24 16:33:28 +0200

    Improve Permissions in relational Interfaces (#16373)
    * improve permissions check in m2m relations
    
    * improve m2o relations
    
    * More improvements
    
    * finish relational permission rework
    
    * fix field level permissions
    
    * add tests to useRelationPermissions
    
    * remove unused dependency
    
    * fix rename
    
    * fix permissions for translations
    
    * fix disabled check
    
    * ran linter and disabled "vue/one-component-per-file" for use-relation-permissions test
    
    ---------
    
    Co-authored-by: Azri Kahar <42867097+azrikahar@users.noreply.github.com>
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>
    Co-authored-by: rijkvanzanten <rijkvanzanten@me.com>
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>
    Co-authored-by: Brainslug <tim@brainslug.nl>

--- app/src/interfaces/select-dropdown-m2o/select-dropdown-m2o.vue
+++ app/src/interfaces/select-dropdown-m2o/select-dropdown-m2o.vue
@@ -78,7 +78,6 @@
 <script setup lang="ts">
 import { RelationQuerySingle, useRelationSingle } from '@/composables/use-relation-single';
 import { useRelationM2O } from '@/composables/use-relation-m2o';
-import { usePermissionsStore } from '@/stores/permissions';
 import { useCollectionsStore } from '@/stores/collections';
 import { adjustFieldsForDisplays } from '@/utils/adjust-fields-for-displays';
 import { parseFilter } from '@/utils/parse-filter';
@@ -90,6 +89,7 @@ import { get } from 'lodash';
 import { render } from 'micromustache';
 import { computed, inject, ref, toRefs } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { useRelationPermissionsM2O } from '@/composables/use-relation-permissions';
 
 const props = withDefaults(
 	defineProps<{
@@ -171,6 +171,7 @@ const query = computed<RelationQuerySingle>(() => ({
 }));
 
 const { update, remove, displayItem, loading } = useRelationSingle(value, query, relationInfo);
+const { createAllowed, updateAllowed } = useRelationPermissionsM2O(relationInfo);
 
 const currentPrimaryKey = computed<string | number>(() => {
 	if (!displayItem.value || !props.value || !relationInfo.value) return '+';
@@ -228,18 +229,6 @@ function onSelection(selection: (number | string)[]) {
 
 	selectModalActive.value = false;
 }
-
-const { hasPermission } = usePermissionsStore();
-
-const createAllowed = computed(() => {
-	if (!relationInfo.value) return false;
-	return hasPermission(relationInfo.value.relatedCollection.collection, 'create');
-});
-
-const updateAllowed = computed(() => {
-	if (!relationInfo.value) return false;
-	return hasPermission(relationInfo.value.relatedCollection.collection, 'update');
-});
 </script>
 
 <style lang="scss" scoped>
