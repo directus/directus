bdbcc776be5cac4fd7d8958a135411d86c2e252a
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-04-24 02:01:25 +0200

    modules/settings/routes/data-model/field-detail/field-detail-advanced/field-detail-advanced-tabs

--- app/src/modules/settings/routes/data-model/field-detail/field-detail-advanced/field-detail-advanced-tabs.vue
+++ app/src/modules/settings/routes/data-model/field-detail/field-detail-advanced/field-detail-advanced-tabs.vue
@@ -6,87 +6,80 @@
 	</v-tabs>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType, computed } from 'vue';
-import { useI18n } from 'vue-i18n';
+<script setup lang="ts">
 import { useSync } from '@directus/composables';
-import { useFieldDetailStore } from '../store';
 import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { useFieldDetailStore } from '../store';
 
-export default defineComponent({
-	props: {
-		currentTab: {
-			type: Array as PropType<string[]>,
-			required: true,
+const props = defineProps<{
+	currentTab: string[];
+}>();
+
+const emit = defineEmits(['update:currentTab']);
+
+const fieldDetail = useFieldDetailStore();
+
+const { localType } = storeToRefs(fieldDetail);
+
+const currentTabSync = useSync(props, 'currentTab', emit);
+
+const { t } = useI18n();
+
+const tabs = computed(() => {
+	const tabs = [
+		{
+			text: t('schema'),
+			value: 'schema',
 		},
-	},
-	emits: ['update:currentTab'],
-	setup(props, { emit }) {
-		const fieldDetail = useFieldDetailStore();
+		{
+			text: t('field', 1),
+			value: 'field',
+		},
+		{
+			text: t('interface_label'),
+			value: 'interface',
+		},
+	];
 
-		const { localType } = storeToRefs(fieldDetail);
-
-		const currentTabSync = useSync(props, 'currentTab', emit);
-
-		const { t } = useI18n();
-
-		const tabs = computed(() => {
-			const tabs = [
-				{
-					text: t('schema'),
-					value: 'schema',
-				},
-				{
-					text: t('field', 1),
-					value: 'field',
-				},
-				{
-					text: t('interface_label'),
-					value: 'interface',
-				},
-			];
-
-			if (localType.value !== 'presentation' && localType.value !== 'group') {
-				tabs.push({
-					text: t('display'),
-					value: 'display',
-				});
-			}
-
-			if (['o2m', 'm2o', 'm2m', 'm2a', 'files', 'file'].includes(localType.value)) {
-				tabs.splice(1, 0, {
-					text: t('relationship'),
-					value: 'relationship',
-				});
-			}
-
-			if (localType.value === 'translations') {
-				tabs.splice(
-					1,
-					0,
-					...[
-						{
-							text: t('translations'),
-							value: 'relationship',
-						},
-					]
-				);
-			}
-
-			tabs.push({
-				text: t('validation'),
-				value: 'validation',
-			});
-
-			tabs.push({
-				text: t('conditions'),
-				value: 'conditions',
-			});
-
-			return tabs;
+	if (localType.value !== 'presentation' && localType.value !== 'group') {
+		tabs.push({
+			text: t('display'),
+			value: 'display',
 		});
+	}
 
-		return { tabs, currentTabSync };
-	},
+	if (['o2m', 'm2o', 'm2m', 'm2a', 'files', 'file'].includes(localType.value)) {
+		tabs.splice(1, 0, {
+			text: t('relationship'),
+			value: 'relationship',
+		});
+	}
+
+	if (localType.value === 'translations') {
+		tabs.splice(
+			1,
+			0,
+			...[
+				{
+					text: t('translations'),
+					value: 'relationship',
+				},
+			]
+		);
+	}
+
+	tabs.push({
+		text: t('validation'),
+		value: 'validation',
+	});
+
+	tabs.push({
+		text: t('conditions'),
+		value: 'conditions',
+	});
+
+	return tabs;
 });
 </script>
