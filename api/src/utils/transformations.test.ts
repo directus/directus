dd67756ccd0f3d61233b7e934d9b4b20faa51781
Brainslug <tim@brainslug.nl>
2023-04-25 16:02:05 +0200

    extra tests for resolvePreset

--- api/src/utils/transformations.test.ts
+++ api/src/utils/transformations.test.ts
@@ -3,6 +3,28 @@ import { resolvePreset } from './transformations.js';
 import type { File } from '../types/files.js';
 import type { TransformationParams } from '../types/assets.js';
 
+const inputFile: File = {
+	id: '43a15f67-84a7-4e07-880d-e46a9f33c542',
+	storage: 'local',
+	filename_disk: 'test',
+	filename_download: 'test',
+	title: null,
+	type: null,
+	folder: null,
+	uploaded_by: null,
+	uploaded_on: new Date(),
+	charset: null,
+	filesize: 123,
+	width: null,
+	height: null,
+	duration: null,
+	embed: null,
+	description: null,
+	location: null,
+	tags: null,
+	metadata: null,
+};
+
 describe('resolvePreset', () => {
 	test('Prevent input mutation #18301', () => {
 		const inputData: TransformationParams = {
@@ -17,30 +39,7 @@ describe('resolvePreset', () => {
 						fit: 'cover',
 					},
 				],
-				['toFormat', 'jpg', {}],
 			],
-		};
-
-		const inputFile: File = {
-			id: '43a15f67-84a7-4e07-880d-e46a9f33c542',
-			storage: 'local',
-			filename_disk: 'test',
-			filename_download: 'test',
-			title: null,
-			type: null,
-			folder: null,
-			uploaded_by: null,
-			uploaded_on: new Date(),
-			charset: null,
-			filesize: 123,
-			width: null,
-			height: null,
-			duration: null,
-			embed: null,
-			description: null,
-			location: null,
-			tags: null,
-			metadata: null,
 		};
 
 		resolvePreset(inputData, inputFile);
@@ -54,7 +53,61 @@ describe('resolvePreset', () => {
 					fit: 'cover',
 				},
 			],
-			['toFormat', 'jpg', {}],
+		]);
+	});
+
+	test('Add toFormat transformation', () => {
+		const inputData: TransformationParams = {
+			key: 'system-small-cover',
+			format: 'jpg',
+			quality: 80,
+			transforms: [
+				[
+					'resize',
+					{
+						width: 64,
+						height: 64,
+						fit: 'cover',
+					},
+				],
+			],
+		};
+
+		const output = resolvePreset(inputData, inputFile);
+
+		expect(output).toStrictEqual([
+			[
+				'resize',
+				{
+					width: 64,
+					height: 64,
+					fit: 'cover',
+				},
+			],
+			['toFormat', 'jpg', { quality: 80 }],
+		]);
+	});
+
+	test('Add resize transformation', () => {
+		const inputData: TransformationParams = {
+			key: 'system-small-cover',
+			width: 64,
+			height: 64,
+			fit: 'cover',
+		};
+
+		const output = resolvePreset(inputData, inputFile);
+
+		expect(output).toStrictEqual([
+			[
+				'resize',
+				{
+					width: 64,
+					height: 64,
+					fit: 'cover',
+					withoutEnlargement: undefined,
+				},
+			],
 		]);
 	});
 });
