bc5efd18fd6a8bc842cbb4eef3efe27657dc0c91
rijkvanzanten <rijkvanzanten@me.com>
2023-05-02 17:54:58 -0400

    Use defineComponent in options script

--- app/src/interfaces/input-hash/input-hash.vue
+++ app/src/interfaces/input-hash/input-hash.vue
@@ -15,9 +15,11 @@
 </template>
 
 <script lang="ts">
-export default {
+import { defineComponent } from 'vue';
+
+export default defineComponent({
 	inheritAttrs: false,
-};
+});
 </script>
 
 <script setup lang="ts">
