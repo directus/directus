3dbcc4df034a9a84c38961142e70091f7ef48912
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2023-05-09 22:26:25 +0800

    tweak and add comment in monoThemeGenerator

--- app/src/panels/pie-chart/color-generator.ts
+++ app/src/panels/pie-chart/color-generator.ts
@@ -1,22 +1,28 @@
 import chroma from 'chroma-js';
 
-export const monoThemeGenerator = (baseColor: string, length: number): string[] => {
+export function monoThemeGenerator(baseColor: string, length: number): string[] {
 	const theme: string[] = [baseColor];
 	const lab = chroma(baseColor).get('lab') as unknown as [number, number, number];
 
-	let newL = lab[0] - 15;
+	// Lighten or darken the next color in the theme
+	const getUpdatedLightness = (lightness: number, index: number) =>
+		lightness < 0 ? lab[0] + index + 20 : lightness - (index + 10);
 
+	let newLightness = lab[0] - 15;
+
+	// Starts at 1 because the 0-indexed first color in the theme array is the base color
 	for (let index = 1; index < length; index++) {
-		newL = getUpdatedLightness(newL, index);
-		let proposedColor = chroma.lab(newL, lab[1], lab[2]).hex();
+		newLightness = getUpdatedLightness(newLightness, index);
+		let proposedColor = chroma.lab(newLightness, lab[1], lab[2]).hex();
 
 		let round = 0;
 
+		// Regenerate a new color if it already exists and retry whenever it is still the same color until a set amount of time
 		while (theme.includes(proposedColor)) {
-			newL = getUpdatedLightness(newL, index);
+			newLightness = getUpdatedLightness(newLightness, index);
 
-			if (newL > 0 && newL < 255) {
-				proposedColor = chroma.lab(newL, lab[1], lab[2]).hex();
+			if (newLightness > 0 && newLightness < 255) {
+				proposedColor = chroma.lab(newLightness, lab[1], lab[2]).hex();
 			}
 
 			round++;
@@ -30,8 +36,4 @@ export const monoThemeGenerator = (baseColor: string, length: number): string[] 
 	}
 
 	return theme;
-
-	function getUpdatedLightness(lightness: number, index: number) {
-		return lightness < 0 ? lab[0] + index + 20 : lightness - (index + 10);
-	}
-};
+}
