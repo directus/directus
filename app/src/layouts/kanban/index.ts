5a47d73c7c7edc357827618347d20ef658ac96c5
ian <licitdev@gmail.com>
2023-05-15 19:00:48 +0800

    Fix invalid field types in grouping for kanban layout

--- app/src/layouts/kanban/index.ts
+++ app/src/layouts/kanban/index.ts
@@ -72,7 +72,11 @@ export default defineLayout<LayoutOptions, LayoutQuery>({
 				}
 			},
 			group: (field) => {
-				if (field.meta?.options && Object.keys(field.meta?.options).includes('choices')) {
+				if (
+					field.meta?.options &&
+					Object.keys(field.meta?.options).includes('choices') &&
+					['string', 'integer', 'float', 'bigInteger'].includes(field.type)
+				) {
 					return Object.keys(field.meta?.options).includes('choices');
 				}
 
