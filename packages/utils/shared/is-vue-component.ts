b601f08573c58d315dcd0785e497fd8e2693c65b
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-05-16 12:02:02 +0200

    Depend on `typeof` instead of `instanceof` to check Vue comp
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>

--- packages/utils/shared/is-vue-component.ts
+++ packages/utils/shared/is-vue-component.ts
@@ -6,5 +6,5 @@ export function isVueComponent(input: unknown): input is Component {
 
 	// A Vue component usually provides a 'setup' and/or 'render' function
 	// (unfortunately there is no more accurate way to find out, but this should be enough for most cases)
-	return input['setup'] instanceof Function || input['render'] instanceof Function;
+	return typeof input['setup'] === "function" || typeof input['render'] === "function";
 }
