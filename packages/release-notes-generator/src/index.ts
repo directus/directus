dfa6ff79b5ab9bfa865d6d8ddf41a75ca44969fd
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-05-25 17:02:53 +0200

    Simplify again, split up into new util & add test

--- packages/release-notes-generator/src/index.ts
+++ packages/release-notes-generator/src/index.ts
@@ -1,35 +1,11 @@
-import type { ChangelogFunctions, GetDependencyReleaseLine, GetReleaseLine } from '@changesets/types';
 import { appendFileSync } from 'node:fs';
 import { MAIN_PACKAGE } from './constants';
-import type { Changesets } from './types';
 import { generateMarkdown } from './utils/generate-markdown';
 import { getInfo } from './utils/get-info';
+import { processReleaseLines } from './utils/process-release-lines';
 import { processPackages } from './utils/process-packages';
 
-const summaryRegex = /(?:::: info\n(?<=^::: info\n)\n*([\s\S]*)(?<!\n)\n*$(?=\n:::$)\n:::$)?\n*([\s\S]*)/m;
-
-const changesets: Changesets = new Map();
-
-const getReleaseLine: GetReleaseLine = async (changeset) => {
-	const { id, summary, ...rest } = changeset;
-
-	if (changesets.has(id)) {
-		return '';
-	}
-
-	const match = summary.match(summaryRegex);
-	const info = match?.[1];
-	const finalSummary = match?.[2] || '';
-
-	changesets.set(id, { summary: finalSummary, info, ...rest });
-
-	return '';
-};
-
-const getDependencyReleaseLine: GetDependencyReleaseLine = async () => {
-	// Cannot be used since there's no way to get the affected dependency
-	return '';
-};
+const { defaultChangelogFunctions, changesets } = processReleaseLines();
 
 process.on('beforeExit', async () => {
 	await run();
@@ -75,10 +51,5 @@ function earlyExit(): never {
 	process.stdout.write('No (processable) changesets found: Skipping generation of release notes\n');
 	process.exit();
 }
-
-const defaultChangelogFunctions: ChangelogFunctions = {
-	getReleaseLine,
-	getDependencyReleaseLine,
-};
 
 export default defaultChangelogFunctions;
---
+++ packages/release-notes-generator/src/utils/process-release-lines.ts
@@ -0,0 +1,36 @@
+import type { ChangelogFunctions, GetDependencyReleaseLine, GetReleaseLine } from '@changesets/types';
+import type { Changesets } from '../types';
+
+const summaryRegex = /(?:::: info\n+([\s\S]*)(?<!\n)\n+:::$\n*)?([\s\S]*)/m;
+
+export function processReleaseLines(): { defaultChangelogFunctions: ChangelogFunctions; changesets: Changesets } {
+	const changesets: Changesets = new Map();
+
+	const getReleaseLine: GetReleaseLine = async (changeset) => {
+		const { id, summary, ...rest } = changeset;
+
+		if (changesets.has(id)) {
+			return '';
+		}
+
+		const match = summary.match(summaryRegex);
+		const info = match?.[1];
+		const finalSummary = match?.[2] || '';
+
+		changesets.set(id, { summary: finalSummary, info, ...rest });
+
+		return '';
+	};
+
+	const getDependencyReleaseLine: GetDependencyReleaseLine = async () => {
+		// Cannot be used since there's no way to get the affected dependency
+		return '';
+	};
+
+	const defaultChangelogFunctions = {
+		getReleaseLine,
+		getDependencyReleaseLine,
+	};
+
+	return { defaultChangelogFunctions, changesets };
+}
