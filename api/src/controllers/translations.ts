490270d498993f4841de9d9904f49efa0f8cf857
rijkvanzanten <rijkvanzanten@me.com>
2023-05-25 16:54:29 -0400

    Use regular controller

--- api/src/controllers/translations.ts
+++ api/src/controllers/translations.ts
@@ -1,12 +1,56 @@
 import express from 'express';
 import { ForbiddenException } from '../exceptions/index.js';
 import { respond } from '../middleware/respond.js';
+import useCollection from '../middleware/use-collection.js';
 import { validateBatch } from '../middleware/validate-batch.js';
-import { TranslationsService } from '../services/index.js';
+import { TranslationsService } from '../services/translations.js';
+import { MetaService } from '../services/meta.js';
+import type { PrimaryKey } from '../types/index.js';
 import asyncHandler from '../utils/async-handler.js';
-import type { PrimaryKey } from '@directus/types';
+import { sanitizeQuery } from '../utils/sanitize-query.js';
 
 const router = express.Router();
+
+router.use(useCollection('directus_translations'));
+
+router.post(
+	'/',
+	asyncHandler(async (req, res, next) => {
+		const service = new TranslationsService({
+			accountability: req.accountability,
+			schema: req.schema,
+		});
+
+		const savedKeys: PrimaryKey[] = [];
+
+		if (Array.isArray(req.body)) {
+			const keys = await service.createMany(req.body);
+			savedKeys.push(...keys);
+		} else {
+			const primaryKey = await service.createOne(req.body);
+			savedKeys.push(primaryKey);
+		}
+
+		try {
+			if (Array.isArray(req.body)) {
+				const records = await service.readMany(savedKeys, req.sanitizedQuery);
+				res.locals['payload'] = { data: records };
+			} else {
+				const record = await service.readOne(savedKeys[0]!, req.sanitizedQuery);
+				res.locals['payload'] = { data: record };
+			}
+		} catch (error: any) {
+			if (error instanceof ForbiddenException) {
+				return next();
+			}
+
+			throw error;
+		}
+
+		return next();
+	}),
+	respond
+);
 
 const readHandler = asyncHandler(async (req, res, next) => {
 	const service = new TranslationsService({
@@ -14,78 +58,140 @@ const readHandler = asyncHandler(async (req, res, next) => {
 		schema: req.schema,
 	});
 
-	const result = await service.readByQuery(req.sanitizedQuery);
-
-	res.locals['payload'] = {
-		data: result,
-	};
-
-	return next();
-});
-
-router.search('/', validateBatch('read'), readHandler, respond);
-router.get('/', readHandler, respond);
-
-const upsertHandler = asyncHandler(async (req, res, next) => {
-	const service = new TranslationsService({
+	const metaService = new MetaService({
 		accountability: req.accountability,
 		schema: req.schema,
 	});
 
-	try {
-		let savedKeys: PrimaryKey[] = [];
+	let result;
 
-		const existingKeys: PrimaryKey[] = (
-			await service.readByQuery({
-				fields: ['id'],
-				filter: {
-					key: { _eq: req.params['key']! },
-				},
-			})
-		).map(({ id }) => id);
-
-		if (Array.isArray(req.body)) {
-			savedKeys = await service.upsertMany(req.body);
-			const result = await service.readMany(savedKeys, req.sanitizedQuery);
-			res.locals['payload'] = { data: result || null };
-		} else {
-			const savedKey = await service.upsertOne(req.body);
-			savedKeys.push(savedKey);
-			const result = await service.readOne(savedKey, req.sanitizedQuery);
-			res.locals['payload'] = { data: result || null };
-		}
-
-		const deleteKeys = existingKeys.filter((id) => !savedKeys.includes(id));
-		if (deleteKeys.length > 0) await service.deleteMany(deleteKeys);
-	} catch (error: any) {
-		if (error instanceof ForbiddenException) {
-			return next();
-		}
-
-		throw error;
+	if (req.singleton) {
+		result = await service.readSingleton(req.sanitizedQuery);
+	} else if (req.body.keys) {
+		result = await service.readMany(req.body.keys, req.sanitizedQuery);
+	} else {
+		result = await service.readByQuery(req.sanitizedQuery);
 	}
 
+	const meta = await metaService.getMetaForQuery('directus_translations', req.sanitizedQuery);
+
+	res.locals['payload'] = { data: result, meta };
 	return next();
 });
 
-router.post('/:key', upsertHandler, respond);
-router.patch('/:key', upsertHandler, respond);
+router.get('/', validateBatch('read'), readHandler, respond);
+router.search('/', validateBatch('read'), readHandler, respond);
+
+router.get(
+	'/:pk',
+	asyncHandler(async (req, res, next) => {
+		const service = new TranslationsService({
+			accountability: req.accountability,
+			schema: req.schema,
+		});
+
+		const record = await service.readOne(req.params['pk']!, req.sanitizedQuery);
+
+		res.locals['payload'] = { data: record || null };
+		return next();
+	}),
+	respond
+);
+
+router.patch(
+	'/',
+	validateBatch('update'),
+	asyncHandler(async (req, res, next) => {
+		const service = new TranslationsService({
+			accountability: req.accountability,
+			schema: req.schema,
+		});
+
+		let keys: PrimaryKey[] = [];
+
+		if (Array.isArray(req.body)) {
+			keys = await service.updateBatch(req.body);
+		} else if (req.body.keys) {
+			keys = await service.updateMany(req.body.keys, req.body.data);
+		} else {
+			const sanitizedQuery = sanitizeQuery(req.body.query, req.accountability);
+			keys = await service.updateByQuery(sanitizedQuery, req.body.data);
+		}
+
+		try {
+			const result = await service.readMany(keys, req.sanitizedQuery);
+			res.locals['payload'] = { data: result || null };
+		} catch (error: any) {
+			if (error instanceof ForbiddenException) {
+				return next();
+			}
+
+			throw error;
+		}
+
+		return next();
+	}),
+	respond
+);
+
+router.patch(
+	'/:pk',
+	asyncHandler(async (req, res, next) => {
+		const service = new TranslationsService({
+			accountability: req.accountability,
+			schema: req.schema,
+		});
+
+		const primaryKey = await service.updateOne(req.params['pk']!, req.body);
+
+		try {
+			const record = await service.readOne(primaryKey, req.sanitizedQuery);
+			res.locals['payload'] = { data: record || null };
+		} catch (error: any) {
+			if (error instanceof ForbiddenException) {
+				return next();
+			}
+
+			throw error;
+		}
+
+		return next();
+	}),
+	respond
+);
 
 router.delete(
-	'/:key',
+	'/',
+	validateBatch('delete'),
 	asyncHandler(async (req, _res, next) => {
 		const service = new TranslationsService({
 			accountability: req.accountability,
 			schema: req.schema,
 		});
 
-		await service.deleteByQuery({
-			filter: {
-				key: {
-					_eq: req.params['key']!,
-				},
-			},
+		if (Array.isArray(req.body)) {
+			await service.deleteMany(req.body);
+		} else if (req.body.keys) {
+			await service.deleteMany(req.body.keys);
+		} else {
+			const sanitizedQuery = sanitizeQuery(req.body.query, req.accountability);
+			await service.deleteByQuery(sanitizedQuery);
+		}
+
+		return next();
+	}),
+	respond
+);
+
+router.delete(
+	'/:pk',
+	asyncHandler(async (req, _res, next) => {
+		const service = new TranslationsService({
+			accountability: req.accountability,
+			schema: req.schema,
 		});
+
+		await service.deleteOne(req.params['pk']!);
 
 		return next();
 	}),
