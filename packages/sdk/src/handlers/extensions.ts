abfb1f9623a254b9415ebb6848d468f102662a71
Brainslug <tim@brainslug.nl>
2023-06-02 17:47:20 +0200

    uncommented the extension handler (to be reworked in a follow-up)

--- packages/sdk/src/handlers/extensions.ts
+++ packages/sdk/src/handlers/extensions.ts
@@ -2,66 +2,62 @@
  * Settings handler
  */
 
-/** !!! Rework this one !!! */
+import { Transport, TransportRequestOptions, TransportResponse } from '../transport';
 
-// import { Transport, TransportRequestOptions, TransportResponse } from '../transport';
+export class ExtensionEndpoint {
+	private name: string;
+	private transport: Transport;
 
-export {};
+	constructor(transport: Transport, name: string) {
+		this.name = name;
+		this.transport = transport;
+	}
 
-// export class ExtensionEndpoint extends Transport {
-// 	private name: string;
-// 	private transport: Transport;
+	private endpoint(path: string) {
+		if (path.startsWith('/')) {
+			path = path.substr(1);
+		}
 
-// 	constructor(transport: Transport, name: string) {
-// 		this.name = name;
-// 		this.transport = transport;
-// 	}
+		return `/custom/${this.name}/${path}`;
+	}
 
-// 	private endpoint(path: string) {
-// 		if (path.startsWith('/')) {
-// 			path = path.substr(1);
-// 		}
+	get<T = any>(path: string, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
+		return this.transport.get(this.endpoint(path), options);
+	}
 
-// 		return `/custom/${this.name}/${path}`;
-// 	}
+	head<T = any>(path: string, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
+		return this.transport.head(this.endpoint(path), options);
+	}
 
-// 	get<T = any>(path: string, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
-// 		return this.transport.get(this.endpoint(path), options);
-// 	}
+	options<T = any>(path: string, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
+		return this.transport.options(this.endpoint(path), options);
+	}
 
-// 	head<T = any>(path: string, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
-// 		return this.transport.head(this.endpoint(path), options);
-// 	}
+	delete<T = any, P = any>(path: string, data?: P, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
+		return this.transport.delete(this.endpoint(path), data, options);
+	}
 
-// 	options<T = any>(path: string, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
-// 		return this.transport.options(this.endpoint(path), options);
-// 	}
+	post<T = any, P = any>(path: string, data?: P, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
+		return this.transport.post(this.endpoint(path), data, options);
+	}
 
-// 	delete<T = any, P = any>(path: string, data?: P, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
-// 		return this.transport.delete(this.endpoint(path), data, options);
-// 	}
+	put<T = any, P = any>(path: string, data?: P, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
+		return this.transport.put(this.endpoint(path), data, options);
+	}
 
-// 	post<T = any, P = any>(path: string, data?: P, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
-// 		return this.transport.post(this.endpoint(path), data, options);
-// 	}
+	patch<T = any, P = any>(path: string, data?: P, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
+		return this.transport.patch(this.endpoint(path), data, options);
+	}
+}
 
-// 	put<T = any, P = any>(path: string, data?: P, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
-// 		return this.transport.put(this.endpoint(path), data, options);
-// 	}
+export class ExtensionHandler {
+	private transport: Transport;
 
-// 	patch<T = any, P = any>(path: string, data?: P, options?: TransportRequestOptions): Promise<TransportResponse<T>> {
-// 		return this.transport.patch(this.endpoint(path), data, options);
-// 	}
-// }
+	constructor(transport: Transport) {
+		this.transport = transport;
+	}
 
-// export class ExtensionHandler {
-// 	private transport: Transport;
-
-// 	constructor(transport: Transport) {
-// 		this.transport = transport;
-// 	}
-
-// 	endpoint(name: string): ExtensionEndpoint {
-// 		return new ExtensionEndpoint(this.transport, name);
-// 	}
-// }
+	endpoint(name: string): ExtensionEndpoint {
+		return new ExtensionEndpoint(this.transport, name);
+	}
+}
