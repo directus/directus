f8bbcfc1270a74e63a0bef43e4e5ee2ac5a25406
rijkvanzanten <rijkvanzanten@me.com>
2023-06-02 16:24:19 -0400

    Finish user menu

--- app/src/views/private/components/module-navigation/module-navigation-header-user-menu.vue
+++ app/src/views/private/components/module-navigation/module-navigation-header-user-menu.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import { addTokenToURL } from '@/api';
 import { useUserStore } from '@/stores/user';
+import { useNotificationsStore } from '@/stores/notifications';
 import { getRootPath } from '@/utils/get-root-path';
 import { storeToRefs } from 'pinia';
 import { Ref, computed, ref, unref } from 'vue';
 import { useAppStore } from '@/stores/app';
 import { User } from '@directus/types';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 
 const appStore = useAppStore();
 const userStore = useUserStore();
+const notificationsStore = useNotificationsStore();
 
 const { notificationsDrawerOpen } = storeToRefs(appStore);
 const { currentUser, fullName } = storeToRefs(userStore);
+const { unread } = storeToRefs(notificationsStore);
 
 const avatarURL = computed<string | null>(() => {
 	const user = unref(currentUser);
@@ -29,34 +35,53 @@ const userProfileLink = computed<string>(() => {
 </script>
 
 <template>
-	<v-menu placement="bottom-start" show-arrow>
-		<template #activator="{ toggle }">
-			<v-avatar circle :class="{ 'no-avatar': !avatarURL }" x-small @click="toggle">
-				<img
-					v-if="avatarURL && !avatarError"
-					:src="avatarURL"
-					:alt="fullName ?? undefined"
-					class="avatar-image"
-					@error="avatarError = $event"
-				/>
-				<v-icon v-else name="account_circle" />
-			</v-avatar>
-		</template>
+	<div class="module-navigation-header-user-menu">
+		<v-menu placement="bottom-start" show-arrow>
+			<template #activator="{ toggle }">
+				<v-badge :value="unread" class="badge" :disabled="unread == 0">
+					<v-avatar circle x-small @click="toggle" class="avatar">
+						<img
+							v-if="avatarURL && !avatarError"
+							:src="avatarURL"
+							:alt="fullName ?? undefined"
+							class="avatar-image"
+							@error="avatarError = $event"
+						/>
+						<v-icon v-else name="account_circle" />
+					</v-avatar>
+				</v-badge>
+			</template>
 
-		<v-list>
-			<v-list-item clickable :to="userProfileLink">
-				<v-list-item-icon><v-icon name="account_circle" /></v-list-item-icon>
-				<v-list-item-content>Show Profile</v-list-item-content>
-			</v-list-item>
-			<v-list-item clickable @click="notificationsDrawerOpen = true">
-				<v-list-item-icon><v-icon name="notifications" /></v-list-item-icon>
-				<v-list-item-content>Notifications</v-list-item-content>
-			</v-list-item>
-			<v-divider />
-			<v-list-item clickable to="/logout">
-				<v-list-item-icon><v-icon name="logout" /></v-list-item-icon>
-				<v-list-item-content>Log out</v-list-item-content>
-			</v-list-item>
-		</v-list>
-	</v-menu>
+			<v-list>
+				<v-list-item clickable :to="userProfileLink">
+					<v-list-item-icon><v-icon name="account_circle" /></v-list-item-icon>
+					<v-list-item-content>{{ t('show_profile') }}</v-list-item-content>
+				</v-list-item>
+				<v-list-item clickable @click="notificationsDrawerOpen = true">
+					<v-list-item-icon><v-icon name="notifications" /></v-list-item-icon>
+					<v-list-item-content>{{ t('notifications') }}</v-list-item-content>
+				</v-list-item>
+				<v-divider />
+				<v-list-item clickable to="/logout">
+					<v-list-item-icon><v-icon name="logout" /></v-list-item-icon>
+					<v-list-item-content>{{ t('sign_out') }}</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-menu>
+	</div>
 </template>
+
+<style scoped>
+.module-navigation-header-user-menu {
+	margin-right: 4px;
+	cursor: pointer;
+}
+
+.avatar:hover {
+	outline: 2px solid var(--border-normal);
+}
+
+.badge {
+	--v-badge-background-color: var(--primary);
+}
+</style>
