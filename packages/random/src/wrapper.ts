df38f387a82cf6f67a240a3cf335c191e4ae85e5
Jan Arends <jan.arends@mailbox.org>
2023-06-13 12:55:38 +0200

    fix docs typo
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>

--- packages/random/src/wrapper.ts
+++ packages/random/src/wrapper.ts
@@ -2,7 +2,7 @@ import { randomAlpha } from './alpha.js';
 import { randomInteger } from './integer.js';
 
 /**
- * A wrapper to generate a random identifiers.
+ * A wrapper to generate a random identifier.
  *
  * @returns A random identifier with 3 to 25 characters.
  */
