987c839b65099a343ef26688f3cb65af0631268e
rijkvanzanten <rijkvanzanten@me.com>
2023-06-14 11:58:20 -0400

    Allow optional reason for invalid config

--- api/src/errors/invalid-config.ts
+++ api/src/errors/invalid-config.ts
@@ -2,6 +2,7 @@ import { createError } from '@directus/errors';
 
 export interface InvalidConfigErrorExtensions {
 	provider: string;
+	reason?: string;
 }
 
 export const InvalidConfigError = createError<InvalidConfigErrorExtensions>('INVALID_CONFIG', 'Invalid config.', 503);
