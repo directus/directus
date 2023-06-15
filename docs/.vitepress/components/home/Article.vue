767ee30d605b90ae1da9844c5483b663ef498c6f
Esther Agbaje <folasadeagbaje@gmail.com>
2023-06-15 13:19:20 +0100

    docs: fetch featured articles

--- docs/.vitepress/components/home/Article.vue
+++ docs/.vitepress/components/home/Article.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="vp-docs">
 		<a :href="url">
-			<img :src="img" alt="" class="img-size" />
+			<img :src="img" alt="" class="article-img" />
 			<p class="tag">{{ tag }}</p>
-			<h3 class="sub-headline">{{ title }}</h3>
+			<h3 class="sub-headline article-heading">{{ title }}</h3>
 			<div class="m-10 gray">
 				<span>{{ author }}</span>
 				&nbsp;
@@ -30,12 +30,17 @@ export default {
 </script>
 
 <style scoped>
-.img-size {
+.article-heading {
+	font-weight: 600;
+}
+.article-img {
 	width: 100%;
 	object-fit: contain;
+	border-radius: 8px;
 }
 .tag {
 	background: #f0ecff;
+	text-transform: capitalize;
 	border-radius: 6px;
 	color: #6644ff;
 	display: inline-block;
