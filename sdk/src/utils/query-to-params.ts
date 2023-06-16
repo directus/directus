707ac309989efa11131afbf415120d0f4728a462
Brainslug <tim@brainslug.nl>
2023-06-16 16:36:37 +0200

    recursively join fields in params

--- sdk/src/utils/query-to-params.ts
+++ sdk/src/utils/query-to-params.ts
@@ -6,8 +6,27 @@ export const queryToParams = <Schema extends object, Item extends object>(
 	const params: Record<string, string> = {};
 
 	if (query.fields) {
-		/** @todo expand for nested fields use */
-		params['fields'] = query.fields.join(',');
+		type FieldItem = (typeof query.fields)[number];
+
+		const walkFields = (value: FieldItem, chain: string[] = []): string => {
+			if (typeof value === 'object') {
+				const result = [];
+
+				for (const key in value) {
+					const fieldList = value[key as keyof typeof value] ?? [];
+
+					for (const item of fieldList) {
+						result.push(walkFields(item as FieldItem, [...chain, key]));
+					}
+				}
+
+				return result.join('.');
+			}
+
+			return [...chain, String(value)].join('.');
+		};
+
+		params['fields'] = query.fields.flatMap((value) => walkFields(value)).join(',');
 	}
 
 	return params;
