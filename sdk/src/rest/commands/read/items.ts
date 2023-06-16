7c1253248a27adc0fa0c3e1f31996bde086a6a37
Brainslug <tim@brainslug.nl>
2023-06-16 16:49:24 +0200

    removing old unused code

--- sdk/src/rest/commands/read/items.ts
+++ sdk/src/rest/commands/read/items.ts
@@ -1,19 +1,18 @@
-import type { Query } from '../../../types/query.js';
-import type { RestCommand } from '../../types.js';
-import { queryToParams } from '../../utils/query-to-params.js';
+import type { Query, RestCommand } from '../../../types/index.js';
+import { queryToParams } from '../../../utils/query-to-params.js';
 
 export interface ReadItemsInput<Schema extends object, Item extends object> {
 	query?: Query<Schema, Item>;
 }
 
-// export type ReadItemsOutput<
-// 	Schema extends object,
-// 	Input extends ReadItemsInput<Schema>
-// > = Schema[Input['collection']][];
-
 export const readItems =
-	<Schema extends object, TCollection extends keyof Schema, Item extends Schema[TCollection], Output extends Partial<Item>>(
-		collection: TCollection,
+	<
+		Schema extends object,
+		Collection extends keyof Schema,
+		Item extends Schema[Collection],
+		Output extends Partial<Item>
+	>(
+		collection: Collection,
 		query: Query<Schema, Item> = {}
 	): RestCommand<Query<Schema, Item>, Output[], Schema> =>
 	() => {
