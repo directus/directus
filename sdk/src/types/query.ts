b8100fd8df1b3df85177ac8634fd4f6c59411915
Brainslug <tim@brainslug.nl>
2023-06-16 13:04:06 +0200

    split up query nesting logic

--- sdk/src/types/query.ts
+++ sdk/src/types/query.ts
@@ -11,8 +11,8 @@ export type QueryFields<Schema extends object, Item extends object> = (
 )[];
 
 export type QueryFieldsNested<Schema extends object, Item extends object> = {
-	[Key in keyof Pick<Item, RelationalFields<Schema, Item>>]: QueryFields<
-		Schema,
-		Extract<Item[Key] extends any[] ? Item[Key][number] : Item[Key], ItemType<Schema>>
-	>;
+	[Key in keyof Pick<Item, RelationalFields<Schema, Item>>]: QueryFields<Schema, ExtractItem<Schema, Item[Key]>>;
 };
+
+type ExtractItem<Schema extends object, Item extends object> = Extract<UnpackList<Item>, ItemType<Schema>>;
+type UnpackList<Item extends object> = Item extends any[] ? Item[number] : Item;
