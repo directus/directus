6ea9859a180feeb462c9806c9bf3da8cb8c95f55
Kevin Lewis <kvn@lws.io>
2023-06-19 11:09:16 +0000

    Removed authenticated request

--- docs/index.data.js
+++ docs/index.data.js
@@ -1,13 +1,9 @@
 import fetch from 'node-fetch';
 
-const env = {
-	url: 'https://example.com',
-	token: 'abcdef',
-};
+const url = 'https://marketing.directus.app'
 
 const fields = [
 	'featured.*.title',
-	'featured.*.main',
 	'featured.*.category',
 	'featured.*.publish_date',
 	'featured.*.user_created.first_name',
@@ -16,12 +12,7 @@ const fields = [
 ];
 
 async function getArticles() {
-	const response = await fetch(`${env.url}/items/blog?fields[]=${fields}`, {
-		headers: {
-			Authorization: `Bearer ${env.token}`,
-		},
-	});
-
+	const response = await fetch(`${url}/items/blog?fields[]=${fields}`);
 	return response.json();
 }
 
@@ -37,7 +28,7 @@ export default {
 				category,
 				publish_date,
 				author: `${user_created.first_name} ${user_created.last_name}`,
-				image: `${env.url}/assets/${image.filename_disk}`,
+				image: `${url}/assets/${image.filename_disk}`,
 				slug: `https://directus.io/blog/${article.blog_id.main}`,
 			};
 		});
