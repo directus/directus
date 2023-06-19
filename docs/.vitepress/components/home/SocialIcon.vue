05c12f07050d52fc2e4b0a4b7d4f56826bf21d32
Esther Agbaje <folasadeagbaje@gmail.com>
2023-06-19 13:39:44 +0100

    feat: create social icon component

---
+++ docs/.vitepress/components/home/SocialIcon.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import LinkedIn from './icons/LinkedIn.vue';
+import Docker from './icons/Docker.vue';
+import NPM from './icons/NPM.vue';
+import YouTube from './icons/YouTube.vue';
+import Discord from './icons/Discord.vue';
+import Twitter from './icons/Twitter.vue';
+import GitHub2 from './icons/GitHub2.vue';
+import Divider from '../Divider.vue';
+
+const iconsMap = {
+	linkedin: LinkedIn,
+	docker: Docker,
+	npm: NPM,
+	youtube: YouTube,
+	discord: Discord,
+	twitter: Twitter,
+	github: GitHub2,
+};
+
+const props = defineProps<{
+	url: string;
+	icon: keyof typeof iconsMap;
+}>();
+
+const Comp = iconsMap[props.icon];
+</script>
+
+<template>
+	<a :href="url">
+		<Comp />
+	</a>
+</template>
