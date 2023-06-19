51802b5f6212f7dd75e1a7050c7568efc9bc26e9
Esther Agbaje <folasadeagbaje@gmail.com>
2023-06-19 18:21:33 +0100

    chore: update icons

--- docs/.vitepress/components/home/icons/GitHub2.vue
+++ docs/.vitepress/components/home/icons/GitHub2.vue
@@ -1,5 +1,5 @@
 <template>
-	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<path
 			fill-rule="evenodd"
 			clip-rule="evenodd"
