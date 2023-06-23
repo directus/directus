ce3d3d6f2196a5c8dfede639766771ab59e29b44
Brainslug <tim@brainslug.nl>
2023-06-23 16:01:16 +0200

    added create items commands

---
+++ sdk/src/rest/commands/create/items.ts
@@ -0,0 +1,35 @@
+import type { ApplyQueryFields, Query } from '../../../types/index.js';
+import type { RestCommand } from '../../types.js';
+import { queryToParams } from '../../utils/query-to-params.js';
+
+export type CreateItemsOutput<
+	Schema extends object,
+	Collection extends keyof Schema,
+	TQuery extends Query<Schema, Schema[Collection]>
+> = ApplyQueryFields<Schema, Schema[Collection], TQuery['fields']>[];
+
+export const createItems =
+	<
+		Schema extends object,
+		Collection extends keyof Schema,
+		TQuery extends Query<Schema, Schema[Collection]>,
+		Item = Schema[Collection]
+	>(
+		collection: Collection,
+		items: Partial<Item>[],
+		query?: TQuery
+	): RestCommand<CreateItemsOutput<Schema, Collection, TQuery>, Schema> =>
+	() => {
+		const _collection = String(collection);
+
+		if (_collection.startsWith('directus_')) {
+			throw new Error('Cannot use readItems for core collections');
+		}
+
+		return {
+			path: `/items/${_collection}`,
+			params: queryToParams(query ?? {}),
+			body: JSON.stringify(items),
+			method: 'POST',
+		};
+	};
