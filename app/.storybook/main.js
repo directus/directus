2938df62a1bb96baa2c962c8c1298f53a8d16f9a
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-06-24 19:25:19 +0200

    Format files with Prettier

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,29 +1,33 @@
 const path = require('path');
-const {
-  mergeConfig
-} = require('vite');
+const { mergeConfig } = require('vite');
 module.exports = {
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      resolve: {
-        dedupe: ['@storybook/client-api'],
-        alias: [{
-          find: '@',
-          replacement: path.resolve(__dirname, '..', 'src')
-        }
-        // { find: 'json2csv', replacement: 'json2csv/dist/json2csv.umd.js' },
-        ]
-      }
-    });
-  },
+	async viteFinal(config) {
+		return mergeConfig(config, {
+			resolve: {
+				dedupe: ['@storybook/client-api'],
+				alias: [
+					{
+						find: '@',
+						replacement: path.resolve(__dirname, '..', 'src'),
+					},
+					// { find: 'json2csv', replacement: 'json2csv/dist/json2csv.umd.js' },
+				],
+			},
+		});
+	},
 
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-actions', '@storybook/addon-mdx-gfm'],
-  framework: {
-    name: '@storybook/vue3-vite',
-    options: {}
-  },
-  docs: {
-    autodocs: true
-  }
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-actions',
+		'@storybook/addon-mdx-gfm',
+	],
+	framework: {
+		name: '@storybook/vue3-vite',
+		options: {},
+	},
+	docs: {
+		autodocs: true,
+	},
 };
