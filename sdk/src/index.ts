76ef5fbb298e0bc04a3cbb4c6eeed06f0ba1cfac
rijkvanzanten <rijkvanzanten@me.com>
2023-06-27 15:21:28 -0400

    Tweak exports

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,6 +1,2 @@
-export * from './auth/index.js';
 export * from './client.js';
-export * from './graphql/index.js';
-export * from './realtime/index.js';
-export * from './rest/index.js';
 export * from './types/index.js';
