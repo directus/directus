c9c7c353e3c7baaf42e1d16a5949d647f6714fe0
Brainslug <tim@brainslug.nl>
2023-06-29 14:29:45 +0200

    add all exports to the root

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,2 +1,7 @@
 export * from './client.js';
 export * from './types/index.js';
+
+export * from './auth/index.js';
+export * from './graphql/index.js';
+export * from './realtime/index.js';
+export * from './rest/index.js';
