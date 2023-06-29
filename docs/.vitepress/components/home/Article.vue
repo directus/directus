43dcadbf1772c07e4b63e884a89e86e65c201ab8
rijkvanzanten <rijkvanzanten@me.com>
2023-06-29 16:03:26 +0200

    Add hover state to article card

--- docs/.vitepress/components/home/Article.vue
+++ docs/.vitepress/components/home/Article.vue
@@ -13,9 +13,11 @@ defineProps<{
 </script>
 
 <template>
-	<div class="vp-docs">
+	<div class="article">
 		<a :href="url">
-			<img :src="img" alt="" class="article-img" />
+			<div class="article-img">
+				<img :src="img" alt="" />
+			</div>
 			<Tag :tag="tag" />
 			<h3 class="sub-headline article-heading">{{ title }}</h3>
 			<p class="m-6 text-muted">{{ desc }}</p>
@@ -34,12 +36,31 @@ defineProps<{
 .article-heading {
 	font-weight: 600;
 }
+
 .article-img {
 	width: 100%;
-	object-fit: contain;
 	border-radius: 8px;
+	aspect-ratio: 16 / 9;
+	overflow: hidden;
 }
+
+.article-img img {
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	object-position: center center;
+	transition: scale 150ms ease-out;
+}
+
 .gray {
 	color: var(--vp-c-gray);
+}
+
+.article:hover .article-heading {
+	text-decoration: underline;
+}
+
+.article:hover .article-img img {
+	scale: 1.05;
 }
 </style>
