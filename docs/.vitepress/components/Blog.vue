0ef2ea547c2e66d06bb672d3635d06835133c0c7
bryantgillespie <hey@bryantgillespie.com>
2023-06-30 14:59:28 -0400

    refactor blog index page

--- docs/.vitepress/components/Blog.vue
+++ docs/.vitepress/components/Blog.vue
@@ -1,397 +1,171 @@
+<template>
+	<div class="">
+		<section class="hero paddingBox">
+			<div class="heroPattern">
+				<Pattern />
+			</div>
+			<div class="sectionContainer flex">
+				<div class="heroContent sectionPaddingHero">
+					<h1>Developer Blog</h1>
+					<p>Read project tutorials, tips & tricks, and best practices from the Directus team and community.</p>
+					<div class="heroButtons buttonGroup">
+						<Button href="/blog/guest-author" primary>Become a Guest Author</Button>
+					</div>
+				</div>
+				<div class="heroToggler">
+					<Badge>Featured Article</Badge>
+					<Article
+						v-for="article in [data.blog.articles[0]]"
+						:key="article.id"
+						:title="article.title"
+						:desc="article.description"
+						:author="article.user_created.first_name + ' ' + article.user_created.last_name"
+						:date="getFriendlyDate(article.publish_date)"
+						:url="`/blog/${article.id}`"
+						:img="`https://marketing.directus.app/assets/${article.image}?key=card`"
+					/>
+				</div>
+			</div>
+		</section>
+		<section class="page">
+			<div class="sectionContainer">
+				<Badge>All Articles</Badge>
+				<div class="grid2">
+					<Article
+						v-for="article in data.blog.articles"
+						:key="article.id"
+						:title="article.title"
+						:desc="article.description"
+						:author="article.user_created.first_name + ' ' + article.user_created.last_name"
+						:date="getFriendlyDate(article.publish_date)"
+						:url="`/blog/${article.id}`"
+						:img="`https://marketing.directus.app/assets/${article.image}?key=card`"
+					/>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
+
 <script setup lang="ts">
 import { data } from '../data/blog.data.js';
 import { getFriendlyDate } from '../utils/time.js';
-import { truncateString } from '../utils/strings';
+import Pattern from './home/Pattern.vue';
+import Badge from './Badge.vue';
 </script>
-<template>
-	<section class="page">
-		<div class="container big-stack">
-			<a href="/blog/guest-author" style="text-decoration:underline;">Guest author program</a>
-			<a v-for="article in data.blog.articles" :key="article.id" :href="`/blog/${article.id}`" class="card">
-				<div class="image">
-					<img :src="`https://marketing.directus.app/assets/${article.image}`" alt="" loading="lazy" />
-				</div>
-				<div class="heading">
-					<h2>{{ article.title }}</h2>
-					<p>{{ truncateString(article.summary, 200) }}</p>
-					<span class="meta">
-						{{ article.user_created.first_name }} {{ article.user_created.last_name }} •
-						{{ getFriendlyDate(article.publish_date) }}
-					</span>
-				</div>
-			</a>
-		</div>
-	</section>
-</template>
 
-<style lang="scss" scoped>
+<style scoped>
 .page {
-	padding: 1rem;
+	padding: 24px;
+}
+.paddingBox {
+	padding: 0 32px;
 }
 
-#guides {
-	.three-up {
-		li {
-			a {
-				img {
-					aspect-ratio: 16/9;
-				}
-			}
-		}
-	}
-	@media only screen and (max-width: 880px) {
-		margin-top: 160px !important;
-	}
+.sectionContainer {
+	max-width: calc(var(--vp-layout-max-width) - 64px);
 }
 
-.article-hero {
+.sectionPaddingHero {
+	padding-block: 108px;
+}
+
+.hero {
+	/* background: var(--vp-docs-section-bg);
+	color: white; */
+	position: relative;
+	overflow: hidden;
+	border-radius: 12px;
+}
+
+.heroPattern {
+	position: absolute;
+	top: 40%;
+	left: 50%;
+	translate: -50% -50%;
+	pointer-events: none;
+}
+
+.heroPattern svg {
+	width: 1600px;
+	height: 1000px;
+	opacity: 0.5;
+}
+
+.heroContent {
+	max-width: 580px;
+	position: relative;
+}
+
+.heroContent h1 {
+	font-size: 60px;
+	font-weight: 700;
+	line-height: 1;
+	margin: 20px 0;
+}
+
+.heroContent p {
+	max-inline-size: 50ch;
+	margin: 20px 0;
+}
+
+.heroButtons {
+	margin: 48px 0;
+}
+
+.heroToggler {
+	border-radius: 12px;
+	width: 100%;
+	max-width: 400px;
+	border: 1px solid rgba(255, 255, 255, 0.1);
+	box-shadow: 0 0 100px 24px rgba(255, 255, 255, 0.3);
+	backdrop-filter: blur(4px);
+	margin-left: 20px;
+}
+
+.flex {
 	display: flex;
-	height: auto;
-	max-height: none;
-	padding-top: 110px;
-	.container {
+	align-items: center;
+	justify-content: space-between;
+}
+
+.grid2 {
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	gap: 32px;
+}
+
+.buttonGroup {
+	display: inline-flex;
+	gap: 32px;
+}
+
+@media only screen and (max-width: 1200px) {
+	.heroToggler {
+		display: none;
+	}
+}
+
+@media only screen and (max-width: 768px) {
+	.hero {
+		min-height: unset;
+	}
+	.flex {
+		flex-direction: column;
 		align-items: stretch;
-		// align-items: flex-start;
-		.one-up {
-			display: flex;
-			align-items: flex-end;
-			position: relative;
-			width: calc(100% - 40px - ((100% - 80px) / 3));
-			background-color: white;
-			border-radius: '16px';
-			overflow: hidden;
-			&:hover {
-				.content h2 {
-					text-decoration: underline;
-					text-decoration-color: rgba(white, 0.5);
-					text-decoration-thickness: 2px;
-					text-underline-offset: 3px;
-				}
-			}
-			.content {
-				position: relative;
-				padding: 40px;
-				z-index: 3;
-				&::before {
-					content: '';
-					position: absolute;
-					top: 0;
-					left: -15px;
-					right: -15px;
-					bottom: -30px;
-					background-color: gray;
-					opacity: 0.5;
-					transform: rotate(-4deg);
-					z-index: -1;
-				}
-				span {
-					background: white;
-					padding: 0.25em 0.75em 0.35em;
-					border-radius: 8px;
-					font-weight: 600;
-				}
-				h2 {
-					color: white;
-					text-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
-					line-height: 1.25em;
-					margin-top: 20px;
-					margin-bottom: 20px;
-					transition: text-decoration-color var(--fast) var(--transition);
-					text-decoration-color: rgba(white, 0);
-					font-weight: 600;
-				}
-				small {
-					color: white;
-					text-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
-					opacity: 0.8;
-				}
-			}
-			img {
-				position: absolute;
-				top: 0;
-				left: 0;
-				width: 100%;
-				height: 100%;
-				object-fit: cover;
-				z-index: 1;
-			}
-		}
-		.stacked {
-			list-style-type: none;
-			padding: 0;
-			width: calc((100% - 80px) / 3);
-			a {
-				display: flex;
-				align-content: center;
-				align-items: center;
-				position: relative;
-				width: 100%;
-				max-width: 600px;
-				overflow: hidden;
-				padding: 1.5em 0;
-				&:not(:first-of-type) {
-					border-top: 1px solid gray;
-				}
-				&:hover {
-					h2 {
-						text-decoration: underline;
-						text-decoration-color: gray;
-						text-decoration-thickness: 2px;
-						text-underline-offset: 2px;
-					}
-				}
-				h2 {
-					font-weight: 600;
-					font-size: 1em;
-					margin-bottom: 0.5rem;
-					line-height: 1.4em;
-					transition: text-decoration-color var(--fast) var(--transition);
-					text-decoration-color: rgba(gray, 0);
-				}
-				span.meta {
-					color: #7f858d;
-				}
-			}
-		}
 	}
-	@media only screen and (max-width: 880px) {
-		padding-top: 80px;
-		.container {
-			.one-up {
-				width: 100%;
-				margin-bottom: 20px;
-				.content {
-					margin-top: 160px;
-				}
-			}
-			.stacked {
-				list-style-type: none;
-				padding: 0;
-				width: 100%;
-				a {
-					max-width: none;
-					img {
-						width: 68px;
-						height: 68px;
-						min-width: 68px;
-						min-height: 68px;
-						margin-right: 20px;
-					}
-				}
-			}
-		}
-	}
-}
 
-.three-up {
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: flex-start;
-	padding: 0;
-	margin: 0 auto 0;
-	list-style: none;
-	width: 100%;
-	&.one-row {
-		li {
-			@media only screen and (max-width: 880px) {
-				&:nth-child(n + 3) {
-					display: none;
-				}
-			}
-		}
+	.grid2,
+	.grid3 {
+		grid-template-columns: 1fr;
 	}
-	li {
-		width: 100%;
-		max-width: calc((100% - 80px) / 3);
-		align-items: flex-start;
-		margin: 0;
-		margin-right: 40px;
-		&:nth-child(3n) {
-			margin-right: 0;
-		}
-		a {
-			&:hover {
-				b {
-					text-decoration: underline;
-					text-decoration-color: gray;
-					text-decoration-thickness: 2px;
-					text-underline-offset: 2px;
-				}
-			}
-			img {
-				width: 100%;
-				height: auto;
-				aspect-ratio: 16/9;
-				object-fit: cover;
-				border-radius: 12px;
-				margin-bottom: 10px;
-			}
-			.bold.primary {
-				display: flex;
-				margin-top: 0.75em;
-				span {
-					background: #f0ecff;
-					padding: 0.25em 0.75em;
-					border-radius: 8px;
-					font-weight: 600;
-				}
-			}
-			h2 {
-				font-weight: 600;
-				font-size: 1.25em;
-				margin-top: 1rem;
-				margin-bottom: 1rem;
-				line-height: 1.4em;
-				transition: text-decoration-color var(--fast) var(--transition);
-				text-decoration-color: rgba(gray, 0);
-			}
-			span.meta {
-				color: #7f858d;
-			}
-		}
-		@media only screen and (max-width: 1000px) {
-			.bold.primary span {
-				font-size: 0.75em;
-			}
-			a h2 {
-				font-size: 20px;
-			}
-		}
-		@media only screen and (max-width: 880px) {
-			max-width: calc((100% - 40px) / 2);
-			&:nth-child(3n) {
-				margin-right: 40px;
-			}
-			&:nth-child(2n) {
-				margin-right: 0;
-			}
-		}
-		@media only screen and (max-width: 600px) {
-			max-width: 100%;
-			margin-right: 0 !important;
-			margin-bottom: 40px;
 
-			&:nth-child(n + 3) {
-				display: list-item;
-			}
+	.heroContent h1 {
+		font-size: 42px;
+	}
 
-			a {
-				small.bold {
-					font-size: 16px;
-					line-height: 23px;
-				}
-				b {
-					font-size: 24px;
-					line-height: 24px;
-					font-weight: 800;
-				}
-			}
-		}
-	}
-}
-
-.big-stack a {
-	display: flex;
-	width: 100%;
-	&:not(:first-of-type) {
-		margin-top: 40px;
-	}
-	&:hover {
-		.heading h2 {
-			text-decoration: underline;
-			text-decoration-color: gray;
-			text-decoration-thickness: 2px;
-			text-underline-offset: 3px;
-		}
-	}
-	.image {
-		width: calc((100% - 40px) / 2);
-		aspect-ratio: 16/9;
-		margin-right: 40px;
-		img {
-			border-radius: 12px;
-			display: block;
-			width: 100%;
-			object-fit: cover;
-		}
-	}
-	.heading {
-		width: calc((100% - 40px) / 2);
-		.bold.primary {
-			display: flex;
-			span {
-				background: #f0ecff;
-				padding: 0.25em 0.75em;
-				border-radius: 8px;
-				font-weight: 600;
-			}
-		}
-		h2 {
-			transition: text-decoration-color var(--fast) var(--transition);
-			text-decoration-color: rgba(gray, 0);
-			font-weight: 600;
-			font-size: 1.5em;
-			margin-top: 1rem;
-			margin-bottom: 1rem;
-			line-height: 1.4em;
-		}
-		.summary {
-			width: 100%;
-			max-width: none;
-			display: -webkit-box;
-			// max-height: 90px;
-			-webkit-line-clamp: 3;
-			-webkit-box-orient: vertical;
-			overflow: hidden;
-		}
-		span.meta {
-			color: #7f858d;
-		}
-		button {
-			margin-top: 30px;
-		}
-	}
-	@media only screen and (max-width: 1000px) {
-		.heading {
-			.bold.primary {
-				display: flex;
-				span {
-					font-size: 0.75em;
-				}
-			}
-			h2 {
-				font-size: 20px;
-				margin-top: 0.75em;
-				margin-bottom: 0.75rem;
-			}
-			.meta {
-				font-size: 16px;
-			}
-		}
-	}
-	@media only screen and (max-width: 600px) {
-		flex-wrap: wrap;
-		&:not(:first-of-type) {
-			margin-top: 60px;
-		}
-		.image {
-			width: 100%;
-			margin-right: 0;
-			margin-bottom: 10px;
-		}
-
-		.heading {
-			width: 100%;
-			max-width: none;
-			.bold.primary {
-				margin-top: 0.25em;
-			}
-			h2 {
-				font-size: 24px;
-				margin-top: 0.5em;
-				margin-bottom: 0.25em;
-			}
-		}
+	.sectionPaddingHero {
+		padding-block: 32px;
 	}
 }
 </style>
