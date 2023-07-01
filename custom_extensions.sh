05aaf312819f4c1a5f19660af008a7d02f41d1d4
Chirica Gheorghe <gheorghi.chirica@crawless.com>
2023-07-01 16:06:16 +0300

    Remove extensions

--- custom_extensions.sh
+++ custom_extensions.sh
@@ -12,24 +12,24 @@ DIRECTUS_HOOKS="${DIRECTUS_EXTENSIONS}/hooks"
 
 mkdir -p ${DIRECTUS_EXTENSIONS}/migrations
 
-# Install https://github.com/utomic-media/directus-extension-field-actions
-mkdir -p ${DIRECTUS_DISPLAYS}/fields-action
-wget -O ${DIRECTUS_DISPLAYS}/fields-action/index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.2.0/display-index.js
-mkdir -p ${DIRECTUS_INTERFACES}/fields-action
-wget -O ${DIRECTUS_INTERFACES}/fields-action/index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.2.0/interface-index.js
+## Install https://github.com/utomic-media/directus-extension-field-actions
+#mkdir -p ${DIRECTUS_DISPLAYS}/fields-action
+#wget -O ${DIRECTUS_DISPLAYS}/fields-action/index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.2.0/display-index.js
+#mkdir -p ${DIRECTUS_INTERFACES}/fields-action
+#wget -O ${DIRECTUS_INTERFACES}/fields-action/index.js https://github.com/utomic-media/directus-extension-field-actions/releases/download/1.2.0/interface-index.js
+#
+## Install https://github.com/u12206050/directus-extension-global-search
+#mkdir -p ${DIRECTUS_MODULES}
+##wget -O /tmp/global-search.zip https://github.com/u12206050/directus-extension-global-search/releases/download/1.0.0/global-search.zip
+##unzip -d ${DIRECTUS_MODULES}/ /tmp/global-search.zip
 
-# Install https://github.com/u12206050/directus-extension-global-search
-mkdir -p ${DIRECTUS_MODULES}
-#wget -O /tmp/global-search.zip https://github.com/u12206050/directus-extension-global-search/releases/download/1.0.0/global-search.zip
-#unzip -d ${DIRECTUS_MODULES}/ /tmp/global-search.zip
-
-# Install https://github.com/u12206050/directus-extension-api-viewer-module
-mkdir -p ${DIRECTUS_MODULES}/api-viewer
-wget -O ${DIRECTUS_MODULES}/api-viewer/index.js https://github.com/u12206050/directus-extension-api-viewer-module/releases/download/1.1.1/index.js
+## Install https://github.com/u12206050/directus-extension-api-viewer-module
+#mkdir -p ${DIRECTUS_MODULES}/api-viewer
+#wget -O ${DIRECTUS_MODULES}/api-viewer/index.js https://github.com/u12206050/directus-extension-api-viewer-module/releases/download/1.1.1/index.js
 
 # Install https://github.com/dimitrov-adrian/directus-extension-masked-interface
-mkdir -p ${DIRECTUS_INTERFACES}/fields-masked
-wget -O ${DIRECTUS_INTERFACES}/fields-masked/index.js https://github.com/dimitrov-adrian/directus-extension-masked-interface/releases/download/v1.1.0/index.js
+#mkdir -p ${DIRECTUS_INTERFACES}/fields-masked
+#wget -O ${DIRECTUS_INTERFACES}/fields-masked/index.js https://github.com/dimitrov-adrian/directus-extension-masked-interface/releases/download/v1.1.0/index.js
 
 # Install crawless custom extensions
 # Download from project: https://gitlab.com/crawless/directus-custom-extensions
