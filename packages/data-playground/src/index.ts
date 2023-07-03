3ba89eafcc407f775402934b655f2a210abc4f79
rijkvanzanten <rijkvanzanten@me.com>
2023-07-03 11:48:43 +0200

    Add logging

--- packages/data-playground/src/index.ts
+++ packages/data-playground/src/index.ts
@@ -1,13 +1,8 @@
 import DataDriverPostgres from '../../data-driver-postgres/src/index.js';
 import type { AbstractQuery } from '../../data/src/index.js';
 import { DataEngine } from '../../data/src/index.js';
-import { streamToString } from './stream-to-string.js';
-
-console.log('Creating engine...');
 
 const engine = new DataEngine();
-
-console.log('Registering Postgres...');
 
 await engine.registerStore('postgres', new DataDriverPostgres({
 	connectionString: 'postgresql://postgres:secret@localhost:5100/data'
@@ -29,14 +24,10 @@ const query: AbstractQuery = {
 	]
 }
 
-console.log('Executing query...');
-
 const dataStream = await engine.query(query);
-const output = await streamToString(dataStream);
 
-console.log();
-console.log(output);
-console.log();
+dataStream.on('data', (data) => {
+	console.log(data);
+});
 
-console.log('Destroying...');
 await engine.destroy();
