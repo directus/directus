ee93052fa04e844a6a5c67633bba766bb86afb15
Jan Arends <jan.arends@mailbox.org>
2023-07-04 09:18:36 +0200

    function conversion in pg driver

--- packages/data-driver-postgres/src/utils/condition-string.ts
+++ packages/data-driver-postgres/src/utils/condition-string.ts
@@ -6,9 +6,16 @@ export const conditionString = (where: AbstractSqlQueryConditionNode | AbstractS
 	if (where.type === 'condition') {
 		const target = wrapColumn(where.target.table, where.target.column);
 
-		const comparison = getComparison(where.operation, where.compareTo, where.negate);
+		if (where.target.type === 'primitive') {
+			const comparison = getComparison(where.operation, where.compareTo, where.negate);
+			return `${target} ${comparison}`;
+		}
 
-		return `${target} ${comparison}`;
+		if (where.target.type === 'fn') {
+			return extractDateTime(where.target.fn, target);
+		}
+
+		throw new Error(`Unsupported target type: ${JSON.stringify(where.target)}`);
 	} else {
 		const logicalGroup = where.childNodes
 			.map((childNode) =>
@@ -19,5 +26,40 @@ export const conditionString = (where: AbstractSqlQueryConditionNode | AbstractS
 			.join(where.operator === 'and' ? ' AND ' : ' OR ');
 
 		return where.negate ? `NOT (${logicalGroup})` : logicalGroup;
+	}
+};
+
+/**
+ * @todo Check datatype of the column. If timestamp then add "AT TIME ZONE 'UTC'" to the result string
+ * @todo Probably add count support
+ *
+ * @param fn - The function to use
+ * @param column - The column which will be used as the argument for the function
+ * @returns - EXTRACT(xy FROM ...)
+ */
+export const extractDateTime = (fn: string, column: string): string => {
+	function getFnString(fn: string) {
+		return `EXTRACT(${fn} FROM ${column})`;
+	}
+
+	switch (fn) {
+		case 'year':
+			return getFnString('YEAR');
+		case 'month':
+			return getFnString('MONTH');
+		case 'week':
+			return getFnString('WEEK');
+		case 'day':
+			return getFnString('DAY');
+		case 'dow':
+			return getFnString('DOW');
+		case 'hour':
+			return getFnString('HOUR');
+		case 'minute':
+			return getFnString('MINUTE');
+		case 'second':
+			return getFnString('SECOND');
+		default:
+			throw new Error(`Function ${fn} is not supported.`);
 	}
 };
