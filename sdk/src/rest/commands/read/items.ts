5019478aebddcd436c8baf87222c5c94e084d459
Brainslug <tim@brainslug.nl>
2023-07-05 13:26:33 +0200

    merged related commands into the same file

--- sdk/src/rest/commands/read/items.ts
+++ sdk/src/rest/commands/read/items.ts
@@ -1,12 +1,13 @@
+import type { PrimaryKey } from '@directus/types';
 import type { ApplyQueryFields, CollectionType, Query, RegularCollections } from '../../../types/index.js';
 import type { RestCommand } from '../../types.js';
 import { queryToParams } from '../../utils/query-to-params.js';
 
-export type ReadItemsOutput<
+export type ReadItemOutput<
 	Schema extends object,
 	Collection extends RegularCollections<Schema>,
 	TQuery extends Query<Schema, Schema[Collection]>
-> = ApplyQueryFields<Schema, CollectionType<Schema, Collection>, TQuery['fields']>[];
+> = ApplyQueryFields<Schema, CollectionType<Schema, Collection>, TQuery['fields']>;
 
 /**
  * List all items that exist in Directus.
@@ -24,7 +25,7 @@ export const readItems =
 	>(
 		collection: Collection,
 		query?: TQuery
-	): RestCommand<ReadItemsOutput<Schema, Collection, TQuery>, Schema> =>
+	): RestCommand<ReadItemOutput<Schema, Collection, TQuery>[], Schema> =>
 	() => {
 		const _collection = String(collection);
 
@@ -34,6 +35,39 @@ export const readItems =
 
 		return {
 			path: `/items/${_collection}`,
+			params: queryToParams(query ?? {}),
+			method: 'GET',
+		};
+	};
+
+/**
+ * Get an item that exists in Directus.
+ *
+ * @param collection The collection of the item
+ * @param key The primary key of the item
+ * @param query The query parameters
+ *
+ * @returns Returns an item object if a valid primary key was provided.
+ */
+export const readItem =
+	<
+		Schema extends object,
+		Collection extends RegularCollections<Schema>,
+		TQuery extends Query<Schema, Schema[Collection]>
+	>(
+		collection: Collection,
+		key: PrimaryKey,
+		query?: TQuery
+	): RestCommand<ReadItemOutput<Schema, Collection, TQuery>, Schema> =>
+	() => {
+		const _collection = String(collection);
+
+		if (_collection.startsWith('directus_')) {
+			throw new Error('Cannot use readItem for core collections');
+		}
+
+		return {
+			path: `/items/${_collection}/${key}`,
 			params: queryToParams(query ?? {}),
 			method: 'GET',
 		};
--- sdk/src/rest/commands/read/users.ts
+++ sdk/src/rest/commands/read/users.ts
@@ -1,14 +1,15 @@
+import type { PrimaryKey } from '@directus/types';
 import type { DirectusUser } from '../../../schema/user.js';
 import type { ApplyQueryFields, Query } from '../../../types/index.js';
 import type { RestCommand } from '../../types.js';
 import { queryToParams } from '../../utils/query-to-params.js';
 
-export type ReadUsersOutput<
+export type ReadUserOutput<
 	Schema extends object,
 	Collection extends keyof Schema | string,
 	TQuery extends Query<Schema, Item>,
     Item = Collection extends keyof Schema ? Schema[Collection] : DirectusUser
-> = ApplyQueryFields<Schema, Item, TQuery['fields']>[];
+> = ApplyQueryFields<Schema, Item, TQuery['fields']>;
 
 /**
  * List all users that exist in Directus.
@@ -24,9 +25,53 @@ export const readUsers =
         Collection extends keyof Schema | string = "directus_users",
 	>(
 		query?: TQuery
-	): RestCommand<ReadUsersOutput<Schema, Collection, TQuery>, Schema> =>
+	): RestCommand<ReadUserOutput<Schema, Collection, TQuery>[], Schema> =>
 	() => ({
         path: `/users`,
         params: queryToParams(query ?? {}),
         method: 'GET',
     });
+
+/**
+ * List an existing user by primary key.
+ *
+ * @param key The primary key of the user
+ * @param query The query parameters
+ *
+ * @returns Returns the requested user object.
+ */
+export const readUser =
+	<
+		Schema extends object,
+		TQuery extends Query<Schema, Collection extends keyof Schema ? Schema[Collection] : DirectusUser>,
+        Collection extends keyof Schema | string = "directus_users",
+	>(
+		key: PrimaryKey,
+		query?: TQuery
+	): RestCommand<ReadUserOutput<Schema, Collection, TQuery>, Schema> =>
+	() => ({
+        path: `/users/${key}`,
+        params: queryToParams(query ?? {}),
+        method: 'GET',
+	});
+
+/**
+ * Retrieve the currently authenticated user.
+ *
+ * @param query The query parameters
+ *
+ * @returns Returns the user object for the currently authenticated user.
+ */
+export const readMe =
+	<
+		Schema extends object,
+		TQuery extends Query<Schema, Collection extends keyof Schema ? Schema[Collection] : DirectusUser>,
+        Collection extends keyof Schema | string = "directus_users",
+	>(
+		query?: TQuery
+	): RestCommand<ReadUserOutput<Schema, Collection, TQuery>, Schema> =>
+	() => ({
+        path: `/users/me`,
+        params: queryToParams(query ?? {}),
+        method: 'GET',
+	});
