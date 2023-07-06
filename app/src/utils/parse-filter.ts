e7275b4e40aeccc34540023fc64c638afb961574
Gerard <gerardlamo@gmail.com>
2023-07-06 11:04:32 +0200

    Fix permissions using dynamic user or role fields
    Dynamic variables were not fetched, so not being parsed and checked

--- app/src/utils/parse-filter.ts
+++ app/src/utils/parse-filter.ts
@@ -1,17 +1,20 @@
 import { useUserStore } from '@/stores/user';
-import { Accountability, Filter } from '@directus/types';
+import { Accountability, Filter, Role, User } from '@directus/types';
 import { parseFilter as parseFilterShared } from '@directus/utils';
 
 export function parseFilter(filter: Filter | null): Filter {
-	const userStore = useUserStore();
+	const { currentUser } = useUserStore();
 
-	if (!userStore.currentUser) return filter ?? {};
-	if (!('id' in userStore.currentUser)) return filter ?? {};
+	if (!currentUser) return filter ?? {};
+	if (!('id' in currentUser)) return filter ?? {};
 
 	const accountability: Accountability = {
-		role: userStore.currentUser.role.id,
-		user: userStore.currentUser.id,
+		role: currentUser.role.id,
+		user: currentUser.id,
 	};
 
-	return parseFilterShared(filter, accountability) ?? {};
+	return parseFilterShared(filter, accountability, {
+		$CURRENT_ROLE: currentUser.role as Role,
+		$CURRENT_USER: currentUser as User,
+	}) ?? {};
 }
