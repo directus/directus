c38173cad508843140985aacb590aeeb6475c42d
bryantgillespie <hey@bryantgillespie.com>
2023-07-07 16:54:17 -0400

    flex article card meta to align across grid

--- docs/.vitepress/components/Article.vue
+++ docs/.vitepress/components/Article.vue
@@ -4,14 +4,16 @@
 			<div class="image">
 				<img :src="img" alt="" loading="lazy" />
 			</div>
-			<h3 class="heading">{{ title }}</h3>
-			<p class="description">{{ desc }}</p>
-			<div v-if="author" class="author">
-				<span>{{ author }}</span>
-				&nbsp;
-				<span>•</span>
-				&nbsp;
-				<span>{{ date }}</span>
+			<div class="content">
+				<h3 class="heading">{{ title }}</h3>
+				<p v-if="desc" class="description">{{ desc }}</p>
+				<div v-if="author" class="author">
+					<span>{{ author }}</span>
+					&nbsp;
+					<span>•</span>
+					&nbsp;
+					<span class="date">{{ date }}</span>
+				</div>
 			</div>
 		</a>
 	</div>
@@ -30,9 +32,23 @@ defineProps<{
 </script>
 
 <style scoped>
-.heading {
-	font-weight: 600;
-	margin-top: 12px;
+.article {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+}
+
+.article a {
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+}
+
+.content {
+	flex-grow: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 }
 
 .image {
@@ -48,6 +64,11 @@ defineProps<{
 	object-fit: cover;
 	object-position: center center;
 	transition: scale 150ms ease-out;
+}
+
+.heading {
+	font-weight: 600;
+	margin-top: 12px;
 }
 
 .description {
