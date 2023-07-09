0fffb2a9def0583aca903d503d78fa2e9f4a8443
Jan Arends <jan.arends@mailbox.org>
2023-07-09 10:15:06 +0200

    extracted variable in test

--- packages/data-driver-postgres/src/utils/get-comparison.test.ts
+++ packages/data-driver-postgres/src/utils/get-comparison.test.ts
@@ -4,44 +4,47 @@ import { randomInteger } from '@directus/random';
 import { getComparison } from './get-comparison.js';
 
 let compareTo: ValueNode;
+let value: number;
 
 beforeEach(() => {
 	compareTo = {
 		type: 'value',
 		parameterIndexes: [randomInteger(1, 10)],
 	};
+
+	value = compareTo.parameterIndexes[0]! + 1;
 });
 
 test('eq', () => {
-	expect(getComparison('eq', compareTo)).toStrictEqual(`= $${compareTo.parameterIndexes[0]! + 1}`);
+	expect(getComparison('eq', compareTo)).toStrictEqual(`= $${value}`);
 });
 
 test('gt', () => {
-	expect(getComparison('gt', compareTo)).toStrictEqual(`> $${compareTo.parameterIndexes[0]! + 1}`);
+	expect(getComparison('gt', compareTo)).toStrictEqual(`> $${value}`);
 });
 
 test('gte', () => {
-	expect(getComparison('gte', compareTo)).toStrictEqual(`>= $${compareTo.parameterIndexes[0]! + 1}`);
+	expect(getComparison('gte', compareTo)).toStrictEqual(`>= $${value}`);
 });
 
 test('lt', () => {
-	expect(getComparison('lt', compareTo)).toStrictEqual(`< $${compareTo.parameterIndexes[0]! + 1}`);
+	expect(getComparison('lt', compareTo)).toStrictEqual(`< $${value}`);
 });
 
 test('lte', () => {
-	expect(getComparison('lte', compareTo)).toStrictEqual(`<= $${compareTo.parameterIndexes[0]! + 1}`);
+	expect(getComparison('lte', compareTo)).toStrictEqual(`<= $${value}`);
 });
 
 test('contains', () => {
-	expect(getComparison('contains', compareTo)).toStrictEqual(`LIKE '%$${compareTo.parameterIndexes[0]! + 1}%'`);
+	expect(getComparison('contains', compareTo)).toStrictEqual(`LIKE '%$${value}%'`);
 });
 
 test('starts_with', () => {
-	expect(getComparison('starts_with', compareTo)).toStrictEqual(`LIKE '$${compareTo.parameterIndexes[0]! + 1}%'`);
+	expect(getComparison('starts_with', compareTo)).toStrictEqual(`LIKE '$${value}%'`);
 });
 
 test('ends_with', () => {
-	expect(getComparison('ends_with', compareTo)).toStrictEqual(`LIKE '%$${compareTo.parameterIndexes[0]! + 1}'`);
+	expect(getComparison('ends_with', compareTo)).toStrictEqual(`LIKE '%$${value}'`);
 });
 
 test.skip('in', () => {
@@ -50,7 +53,5 @@ test.skip('in', () => {
 		parameterIndexes: [randomInteger(1, 10), randomInteger(1, 10)],
 	};
 
-	expect(getComparison('in', compareTo)).toStrictEqual(
-		`IN ($${compareTo.parameterIndexes[0]! + 1}, $${compareTo.parameterIndexes[1]! + 1})`
-	);
+	expect(getComparison('in', compareTo)).toStrictEqual(`IN ($${value}, $${compareTo.parameterIndexes[1]! + 1})`);
 });
