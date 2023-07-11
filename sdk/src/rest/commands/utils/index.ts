8d8f2f16808b24247328af9dd4352b019f9fa80e
Brainslug <tim@brainslug.nl>
2023-07-11 17:22:49 +0200

    added most utility endpoints

--- sdk/src/rest/commands/utils/index.ts
+++ sdk/src/rest/commands/utils/index.ts
@@ -1,2 +1,7 @@
+export * from './cache.js';
+export * from './export.js';
 export * from './flows.js';
+export * from './hash.js';
+export * from './import.js';
 export * from './operations.js';
+export * from './sort.js';
