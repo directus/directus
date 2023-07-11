3ecdfc324577d9ab4dae1efca426e76e2c824995
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-07-11 16:16:09 +0200

    Finish parse-preset test

--- app/src/utils/parse-preset.test.ts
+++ app/src/utils/parse-preset.test.ts
@@ -1,17 +1,22 @@
 import { createTestingPinia } from '@pinia/testing';
 import { setActivePinia } from 'pinia';
 import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { parsePreset } from './parse-preset';
 
 beforeEach(() => {
 	setActivePinia(
 		createTestingPinia({
 			createSpy: vi.fn,
 			initialState: {
-				currentUser: {
-					first_name: 'john',
-					role: {
+				userStore: {
+					currentUser: {
 						id: '1',
-						name: 'admin',
+						role: {
+							id: '1',
+							name: 'admin',
+						},
+						// Custom nested user field
+						org: { name: 'Directus' },
 					},
 				},
 			},
@@ -19,21 +24,8 @@ beforeEach(() => {
 	);
 });
 
-import { useUserStore } from '@/stores/user';
-import { parsePreset } from './parse-preset';
-
 describe('parse-preset', () => {
-
 	it('Should return preset with populated variables', () => {
-		const userStore = useUserStore();
-		expect(userStore.currentUser).toHaveProperty('first_name', 'john');
-
-		userStore.currentUser = Object.assign({}, userStore.currentUser, {
-			org: {
-				name: 'Directus',
-			}
-		});
-
 		const preset = {
 			role: '$CURRENT_ROLE',
 			orgName: '$CURRENT_USER.org.name',
