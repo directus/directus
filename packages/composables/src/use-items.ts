60cb8b527932a555b87df1b51d69bc00f364c3a4
Gerard Lamusse <gerardlamo@gmail.com>
2023-07-24 21:37:46 +0200

    Add support for deep in useItems (#18713)
    * Add support for deep in useItems
    
    * Add support for computedRef on deep
    
    * Add changeset
    
    ---------
    
    Co-authored-by: rijkvanzanten <rijkvanzanten@me.com>

--- packages/composables/src/use-items.ts
+++ packages/composables/src/use-items.ts
@@ -33,13 +33,14 @@ export type ComputedQuery = {
 	search: Ref<Query['search']> | ComputedRef<Query['search']> | WritableComputedRef<Query['search']>;
 	filter: Ref<Query['filter']> | ComputedRef<Query['filter']> | WritableComputedRef<Query['filter']>;
 	page: Ref<Query['page']> | WritableComputedRef<Query['page']>;
+	deep?: Ref<Query['deep']> | ComputedRef<Query['deep']> | WritableComputedRef<Query['deep']>;
 };
 
 export function useItems(collection: Ref<string | null>, query: ComputedQuery): UsableItems {
 	const api = useApi();
 	const { primaryKeyField } = useCollection(collection);
 
-	const { fields, alias, limit, sort, search, filter, page } = query;
+	const { fields, alias, limit, sort, search, filter, page, deep } = query;
 
 	const endpoint = computed(() => {
 		if (!collection.value) return null;
@@ -70,7 +71,7 @@ export function useItems(collection: Ref<string | null>, query: ComputedQuery): 
 	const fetchItems = throttle(getItems, 500);
 
 	watch(
-		[collection, limit, sort, search, filter, fields, page],
+		[collection, limit, sort, search, filter, fields, page, deep],
 		async (after, before) => {
 			if (isEqual(after, before)) return;
 
@@ -163,6 +164,7 @@ export function useItems(collection: Ref<string | null>, query: ComputedQuery): 
 					page: unref(page),
 					search: unref(search),
 					filter: unref(filter),
+					deep: unref(deep),
 				},
 				signal: existingRequests.items.signal,
 			});
