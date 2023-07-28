ff921faad590dda319a2628ab3274be90269d48b
Tema Smirnov <git.tema@smirnov.one>
2023-07-28 05:03:34 +0300

    feat(docs): made docs to work via https on localhost
    Added Vite plugin: `vite-plugin-mkcert`

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitepress';
+import VitePluginMkCert from 'vite-plugin-mkcert';
 import { sharedConfig } from './shared';
 import { config as enUS } from './locales/en-US';
 
 export default defineConfig({
 	...sharedConfig,
+
+	vite: {
+		plugins: [
+			// Docs will work on localhost via https
+			// @ts-ignore
+			VitePluginMkCert()
+		]
+	},
 
 	locales: {
 		root: {
