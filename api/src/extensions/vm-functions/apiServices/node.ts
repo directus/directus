c4b9b40aa90a65ac6ecf25e4ed037e9931154805
Nitwel <mail@nitwel.de>
2023-08-08 12:30:09 +0200

    add more services

--- api/src/extensions/vm-functions/apiServices/node.ts
+++ api/src/extensions/vm-functions/apiServices/node.ts
@@ -2,13 +2,39 @@ import type { Context } from "isolated-vm";
 import type { ApiExtensionInfo } from "../../vm.js";
 import { VMFunction } from "../vm-function.js";
 import { createRequire } from "node:module";
-import { ItemsService } from "../../../services/items.js";
+import {
+	ItemsService,
+	AssetsService,
+	ActivityService,
+	CollectionsService,
+	DashboardsService,
+	FieldsService,
+	FilesService,
+	ImportService,
+	ExportService,
+	NotificationsService,
+	OperationsService,
+	PanelsService,
+	PermissionsService,
+	PresetsService,
+	RelationsService,
+	RevisionsService,
+	RolesService,
+	ServerService,
+	SettingsService,
+	SharesService,
+	TranslationsService,
+	UsersService,
+	WebhooksService,
+	WebSocketService
+} from "../../../services/index.js";
 import { getSchema } from "../../../utils/get-schema.js";
 
 
 const require = createRequire(import.meta.url);
 const ivm = require('isolated-vm')
 
+type ApiServices = 'items' | 'assets' | 'activity' | 'collections' | 'dashboards' | 'fields' | 'files' | 'import' | 'export' | 'notifications' | 'operations' | 'panels' | 'permissions' | 'presets' | 'relations' | 'revisions' | 'roles' | 'server' | 'settings' | 'shares' | 'translations' | 'users' | 'webhooks' | 'websocket'
 
 export class ApiServiceVMFunction extends VMFunction {
 	constructor() {
@@ -19,33 +45,79 @@ export class ApiServiceVMFunction extends VMFunction {
 
 		const schema = await getSchema()
 
-		context.evalClosure(this.vmCode, [
+		await context.evalClosure(this.vmCode, [
 			ivm,
-			new ivm.Reference(function (type: 'items', collection: string) {
+			new ivm.Reference(function (type: ApiServices, ...args: any[]) {
 				console.log("Api Service: ", type)
 
-				if (type === 'items') {
-					const itemsService = new ItemsService(collection, {
-						schema: schema
-					})
+				switch (type) {
+					case 'items':
+						return createReference(new ItemsService(args[0], { schema }))
+					case 'assets':
+						return createReference(new AssetsService({ schema }))
+					case 'activity':
+						return createReference(new ActivityService({ schema }))
+					case 'collections':
+						return createReference(new CollectionsService({ schema }))
+					case 'dashboards':
+						return createReference(new DashboardsService({ schema }))
+					case 'fields':
+						return createReference(new FieldsService({ schema }))
+					case 'files':
+						return createReference(new FilesService({ schema }))
+					case 'import':
+						return createReference(new ImportService({ schema }))
+					case 'export':
+						return createReference(new ExportService({ schema }))
+					case 'notifications':
+						return createReference(new NotificationsService({ schema }))
+					case 'operations':
+						return createReference(new OperationsService({ schema }))
+					case 'panels':
+						return createReference(new PanelsService({ schema }))
+					case 'permissions':
+						return createReference(new PermissionsService({ schema }))
+					case 'presets':
+						return createReference(new PresetsService({ schema }))
+					case 'relations':
+						return createReference(new RelationsService({ schema }))
+					case 'revisions':
+						return createReference(new RevisionsService({ schema }))
+					case 'roles':
+						return createReference(new RolesService({ schema }))
+					case 'server':
+						return createReference(new ServerService({ schema }))
+					case 'settings':
+						return createReference(new SettingsService({ schema }))
+					case 'shares':
+						return createReference(new SharesService({ schema }))
+					case 'translations':
+						return createReference(new TranslationsService({ schema }))
+					case 'users':
+						return createReference(new UsersService({ schema }))
+					case 'webhooks':
+						return createReference(new WebhooksService({ schema }))
+					case 'websocket':
+						return createReference(new WebSocketService())
 
-					return new ivm.Reference(function (resolve: any, reject: any, prop: string, args: any[]) {
 
-						if (prop in itemsService === false) {
-							reject.apply(null, [new ivm.ExternalCopy(new Error('Method does not exist on service')).copyInto()])
-							return
-						}
+				})
+		])
 
-						itemsService[prop](...args).then((result) => {
-							resolve.apply(null, [new ivm.ExternalCopy(result).copyInto()])
-						}).catch((err) => {
-							reject.apply(null, [new ivm.ExternalCopy(err).copyInto()])
-						})
-					})
+		function createReference(service: any) {
+			return new ivm.Reference(function (resolve: any, reject: any, prop: string, args: any[]) {
+
+				if (prop in service === false) {
+					reject.apply(null, [new ivm.ExternalCopy(new Error('Method does not exist on service')).copyInto()])
+					return
 				}
 
-
+				service[prop](...args).then((result) => {
+					resolve.apply(null, [new ivm.ExternalCopy(result).copyInto()])
+				}).catch((err) => {
+					reject.apply(null, [new ivm.ExternalCopy(err).copyInto()])
+				})
 			})
-		])
+		}
 	}
 }
--- api/src/extensions/vm-functions/apiServices/vm.js
+++ api/src/extensions/vm-functions/apiServices/vm.js
@@ -1,31 +1,284 @@
 const ivm = $0
-const requestService = $1
+const databaseService = $1
 
 class ItemsService {
-
 	constructor(collection) {
-		const service = requestService.applySync(null, [
+		const service = databaseService.applySync(null, [
 			'items',
 			collection
 		])
-
-		return new Proxy({}, {
-			get: (target, prop) => {
-				return (...args) => {
-					console.log('args', args, prop, service)
-					return new Promise((resolve, reject) => {
-
-						service.apply(null, [
-							new ivm.Reference(resolve),
-							new ivm.Reference(reject),
-							prop,
-							new ivm.ExternalCopy(args).copyInto()
-						])
-					})
-				}
-			}
-		})
+		return createProxy(service)
 	}
 }
 
+class ActivityService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'activity',
+		])
+		return createProxy(service)
+	}
+}
+
+class AssetsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'assets',
+		])
+		return createProxy(service)
+	}
+}
+
+class CollectionsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'collections',
+		])
+		return createProxy(service)
+	}
+}
+
+class DashboardsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'dashboards',
+		])
+		return createProxy(service)
+	}
+}
+
+class FieldsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'fields',
+		])
+		return createProxy(service)
+	}
+}
+
+class FilesService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'files',
+		])
+		return createProxy(service)
+	}
+}
+
+class FlowsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'flows',
+		])
+		return createProxy(service)
+	}
+}
+
+class FolderService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'folder',
+		])
+		return createProxy(service)
+	}
+}
+
+class ImportService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'import',
+		])
+		return createProxy(service)
+	}
+}
+
+class ExportService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'export',
+		])
+		return createProxy(service)
+	}
+}
+
+class NotificationsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'notifications',
+		])
+		return createProxy(service)
+	}
+}
+
+class OperationsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'operations',
+		])
+		return createProxy(service)
+	}
+}
+
+class PanelsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'panels',
+		])
+		return createProxy(service)
+	}
+}
+
+class PermissionsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'permissions',
+		])
+		return createProxy(service)
+	}
+}
+
+class PresetsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'presets',
+		])
+		return createProxy(service)
+	}
+}
+
+class RelationsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'relations',
+		])
+		return createProxy(service)
+	}
+}
+
+class RevisionsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'revisions',
+		])
+		return createProxy(service)
+	}
+}
+
+class RolesService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'roles',
+		])
+		return createProxy(service)
+	}
+}
+
+class ServerService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'server',
+		])
+		return createProxy(service)
+	}
+}
+
+class SettingsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'settings',
+		])
+		return createProxy(service)
+	}
+}
+
+class SharesService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'shares',
+		])
+		return createProxy(service)
+	}
+}
+
+class TranslationsService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'translations',
+		])
+		return createProxy(service)
+	}
+}
+
+class UsersService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'users',
+		])
+		return createProxy(service)
+	}
+}
+
+class WebhooksService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'webhooks',
+		])
+		return createProxy(service)
+	}
+}
+
+class WebSocketService {
+	constructor() {
+		const service = databaseService.applySync(null, [
+			'websocket',
+		])
+		return createProxy(service)
+	}
+}
+
+
+function createProxy(service) {
+	return new Proxy({}, {
+		get: (_, prop) => {
+			return (...args) => {
+				console.log('args', args, prop, service)
+				return new Promise((resolve, reject) => {
+
+					service.apply(null, [
+						new ivm.Reference(resolve),
+						new ivm.Reference(reject),
+						prop,
+						new ivm.ExternalCopy(args).copyInto()
+					])
+				})
+			}
+		}
+	})
+}
+
 globalThis.ItemsService = ItemsService
+globalThis.ActivityService = ActivityService
+globalThis.AssetsService = AssetsService
+globalThis.CollectionsService = CollectionsService
+globalThis.DashboardsService = DashboardsService
+globalThis.FieldsService = FieldsService
+globalThis.FilesService = FilesService
+globalThis.FlowsService = FlowsService
+globalThis.FolderService = FolderService
+globalThis.ImportService = ImportService
+globalThis.ExportService = ExportService
+globalThis.NotificationsService = NotificationsService
+globalThis.OperationsService = OperationsService
+globalThis.PanelsService = PanelsService
+globalThis.PermissionsService = PermissionsService
+globalThis.PresetsService = PresetsService
+globalThis.RelationsService = RelationsService
+globalThis.RevisionsService = RevisionsService
+globalThis.RolesService = RolesService
+globalThis.ServerService = ServerService
+globalThis.SettingsService = SettingsService
+globalThis.SharesService = SharesService
+globalThis.TranslationsService = TranslationsService
+globalThis.UsersService = UsersService
+globalThis.WebhooksService = WebhooksService
+globalThis.WebSocketService = WebSocketService
