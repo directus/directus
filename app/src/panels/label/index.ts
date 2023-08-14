5115f16c9269689c7c892edec2369a39d77deb77
Connor <12714889+ConnorSimply@users.noreply.github.com>
2023-08-14 17:10:19 -0500

    Start on Label Formatting Options and Resizing

--- app/src/panels/label/index.ts
+++ app/src/panels/label/index.ts
@@ -7,29 +7,202 @@ export default definePanel({
 	description: '$t:panels.label.description',
 	icon: 'title',
 	component: PanelLabel,
-	options: [
-		{
-			field: 'text',
-			name: '$t:label',
-			type: 'string',
-			meta: {
-				interface: 'input',
-				width: 'half',
-			},
-		},
-		{
-			field: 'color',
-			name: '$t:color',
-			type: 'string',
-			meta: {
-				interface: 'select-color',
-				width: 'half',
-				options: {
-					placeholder: '$t:automatic',
+	options: ({ options }) => {
+		if(!options) options = {};
+		return [
+			{
+				field: 'text',
+				name: '$t:label',
+				type: 'string',
+				meta: {
+					interface: options?.whiteSpace == 'nowrap' ? 'input' : 'input-multiline',
 				},
 			},
-		},
-	],
-	minWidth: 8,
-	minHeight: 6,
+			{
+				field: 'whiteSpace',
+				type: 'string',
+				name: '$t:text_wrap',
+				meta: {
+					width: 'half',
+					interface: 'select-dropdown',
+					options: {
+						choices: [
+							{
+								text: 'Normal',
+								value: 'normal',
+							},
+							{
+								text: 'No Wrap',
+								value: 'nowrap',
+							},
+							{
+								divider: true,
+							},
+							{
+								text: 'Preserve',
+								value: 'pre',
+							},
+							{
+								text: 'Preserve Wrap',
+								value: 'pre-wrap',
+							},
+							{
+								text: 'Preserve Line',
+								value: 'pre-line',
+							},
+							{
+								text: 'Break Spaces',
+								value: 'break-spaces',
+							},
+						],
+					},
+				},
+			},
+			{
+				field: 'styleDivider',
+				type: 'alias',
+				meta: {
+					interface: 'presentation-divider',
+					options: {
+						icon: 'style',
+						title: 'Style & Format',
+					},
+					special: ['alias', 'no-data'],
+				},
+			},
+			{
+				field: 'color',
+				name: '$t:color',
+				type: 'string',
+				meta: {
+					interface: 'select-color',
+					width: 'half',
+					options: {
+						placeholder: '$t:automatic',
+					},
+				},
+			},
+			{
+				field: 'textAlign',
+				type: 'string',
+				name: '$t:text_align',
+				meta: {
+					width: 'half',
+					interface: 'select-dropdown',
+					options: {
+						choices: [
+							{
+								text: 'Left',
+								value: 'left',
+							},
+							{
+								text: 'Center',
+								value: 'center',
+							},
+							{
+								text: 'Right',
+								value: 'right',
+							},
+							{
+								text: 'Justify',
+								value: 'justify',
+							},
+						],
+					},
+				},
+			},
+			{
+				field: 'fontWeight',
+				type: 'string',
+				name: '$t:font_weight',
+				meta: {
+					width: 'half',
+					interface: 'select-dropdown',
+					options: {
+						choices: [
+							{
+								text: 'Thin',
+								value: 100
+							},
+							{
+								text: 'Extra Light',
+								value: 200
+							},
+							{
+								text: 'Light',
+								value: 300
+							},
+							{
+								text: 'Normal',
+								value: 400
+							},
+							{
+								text: 'Medium',
+								value: 500
+							},
+							{
+								text: 'Semi Bold',
+								value: 600
+							},
+							{
+								text: 'Bold',
+								value: 700
+							},
+							{
+								text: 'Extra Bold',
+								value: 800
+							},
+							{
+								text: 'Black',
+								value: 900
+							},
+						],
+					},
+				},
+			},
+			{
+				field: 'fontStyle',
+				type: 'string',
+				name: '$t:font_style',
+				meta: {
+					width: 'half',
+					interface: 'select-dropdown',
+					options: {
+						choices: [
+							{
+								text: 'Normal',
+								value: 'normal',
+							},
+							{
+								text: 'Italic',
+								value: 'italic',
+							},
+							{
+								text: 'Oblique',
+								value: 'oblique',
+							},
+						],
+					},
+				},
+			},
+			{
+				field: 'font',
+				type: 'string',
+				name: '$t:font',
+				meta: {
+					width: 'half',
+					interface: 'select-dropdown',
+					options: {
+						choices: [
+							{ text: '$t:displays.formatted-value.font_sans_serif', value: 'sans-serif' },
+							{ text: '$t:displays.formatted-value.font_serif', value: 'serif' },
+							{ text: '$t:displays.formatted-value.font_monospace', value: 'monospace' },
+						],
+					},
+				},
+			},
+		];
+	},
+	minWidth: 6,
+	minHeight: 1,
 });
--- app/src/panels/label/panel-label.vue
+++ app/src/panels/label/panel-label.vue
@@ -1,22 +1,96 @@
 <template>
-	<div class="label type-title selectable" :class="{ 'has-header': showHeader }" :style="{ color: color }">
-		{{ text }}
+	<div
+		ref="labelContainer"
+		class="label type-title selectable"
+		:class="[font, { 'has-header': showHeader }]"
+		:style="{ color: color }"
+	>
+		<p ref="labelText" :style="{ whiteSpace: whiteSpace, fontWeight: fontWeight, textAlign: textAlign, fontStyle: fontStyle }">
+			{{ text }}
+		</p>
 	</div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, onBeforeUnmount, onUpdated, ref } from 'vue';
+
 withDefaults(
 	defineProps<{
 		showHeader?: boolean;
 		text?: string;
+		whiteSpace?: string | undefined;
 		color?: string | undefined;
+		textAlign?: string | undefined;
+		fontWeight?: number | undefined;
+		fontStyle?: string | undefined;
+		font?: 'sans-serif' | 'serif' | 'monospace';
 	}>(),
 	{
 		showHeader: false,
 		text: '',
 		color: undefined,
+		whiteSpace: 'normal',
+		textAlign: 'center',
+		fontWeight: 400,
+		fontStyle: undefined,
+		font: 'sans-serif',
 	}
 );
+
+const labelContainer = ref<HTMLDivElement | null>(null);
+const labelText = ref<HTMLParagraphElement | null>(null);
+let resizeObserver: ResizeObserver | null = null;
+
+onMounted(() => {
+	const container = labelContainer.value;
+
+	if (container) {
+		adjustFontSize(container);
+
+		// Create a ResizeObserver to watch for changes in the container's dimensions
+		resizeObserver = new ResizeObserver(() => {
+			adjustFontSize(container);
+		});
+
+		resizeObserver.observe(container);
+	}
+});
+
+onUpdated(() => {
+	const container = labelContainer.value;
+
+	if (container) {
+		adjustFontSize(container);
+	}
+});
+
+onBeforeUnmount(() => {
+	// Disconnect the ResizeObserver when the component is unmounted
+	if (resizeObserver) {
+		resizeObserver.disconnect();
+		resizeObserver = null;
+	}
+});
+
+function adjustFontSize(container: HTMLDivElement) {
+	const text = labelText.value;
+	if (!container || !text) return;
+	let fontSize = 128; // starting font size in px
+	let padding = Math.max(5, fontSize * 0.3);
+
+	container.style.fontSize = `${fontSize}px`;
+
+	// Decrease the font size until the text fits the container
+	while (
+		text &&
+		(text.offsetHeight > container.offsetHeight - padding * 2 || text.offsetWidth > container.offsetWidth - padding * 2)
+	) {
+		fontSize -= 1;
+		padding = Math.max(5, fontSize * 0.3)
+		container.style.fontSize = `${fontSize}px`;
+		container.style.padding = `${padding}px`;
+	}
+}
 </script>
 
 <script lang="ts">
@@ -27,16 +101,29 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .label {
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	width: 100%;
 	height: 100%;
-	font-weight: 800;
-	font-size: 42px;
-	line-height: 52px;
+	color: var(--color-text);
+	font-weight: 500;
+	line-height: 1.2;
+
+	&.sans-serif {
+		font-family: var(--family-sans-serif);
+	}
+
+	&.serif {
+		font-family: var(--family-serif);
+	}
+
+	&.monospace {
+		font-family: var(--family-monospace);
+	}
+
 }
 
 .label.has-header {
