7ad1aae7f065f65e3a233b62f54b741bebd0223a
Nitwel <mail@nitwel.de>
2023-08-15 12:42:55 +0200

    fix for readAll

--- api/src/services/fields.ts
+++ api/src/services/fields.ts
@@ -81,17 +81,9 @@ export class FieldsService {
 			fields.push(...systemFieldRows);
 		}
 
-		let fieldsInfo = await this.knex.select('*').from('directus_fields').where({ collection })
-
-		if (fieldsInfo) {
-			fieldsInfo = (await this.payloadService.processValues('read', fieldsInfo)) as FieldMeta[];
-		}
-
-		fieldsInfo = fieldsInfo || systemFieldRows.find((fieldMeta) => fieldMeta.collection === collection);
-
 		const columns = (await this.schemaInspector.columnInfo(collection)).map((column) => ({
 			...column,
-			default_value: getDefaultValue(column, fieldsInfo.find((field) => field.field === column.name)),
+			default_value: getDefaultValue(column, fields.find((field) => field.collection === column.table && field.field === column.name)),
 		}));
 
 		const columnsWithSystem = columns.map((column) => {
