271ff9f25c46200acb16bf01cec5b0a9ab1e1868
Jan Arends <jan.arends@mailbox.org>
2023-08-17 14:27:50 +0200

    added index file in sql utils

--- packages/data-sql/src/index.ts
+++ packages/data-sql/src/index.ts
@@ -1,4 +1,3 @@
 export * from './converter/index.js';
 export * from './types/index.js';
-export { expand } from './utils/expand.js';
-export { convertNumericOperators } from './utils/numeric-operator-conversion.js';
+export * from './utils/index.js';
---
+++ packages/data-sql/src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './numeric-operator-conversion.js';
+export * from './expand.js';
