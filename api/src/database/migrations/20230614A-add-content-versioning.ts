f5738e3ae58ed90c0b0ab590341213458114e14a
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2023-08-22 16:53:56 +0800

    remove icon and color from branches

--- api/src/database/migrations/20230614A-add-content-versioning.ts
+++ api/src/database/migrations/20230614A-add-content-versioning.ts
@@ -6,8 +6,6 @@ export async function up(knex: Knex): Promise<void> {
 		table.string('name').notNullable();
 		table.string('collection', 64).references('collection').inTable('directus_collections').onDelete('CASCADE');
 		table.string('item');
-		table.string('icon', 30);
-		table.string('color').nullable();
 		table.string('hash').notNullable();
 		table.timestamp('date_created').defaultTo(knex.fn.now());
 		table.uuid('user_created').references('id').inTable('directus_users').onDelete('SET NULL');
