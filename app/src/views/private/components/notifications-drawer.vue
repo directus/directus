624a57f7eab1a61b4500352159b8d54c2b55d446
Nitwel <mail@nitwel.de>
2023-08-23 14:30:45 +0200

    improve notifications view

--- app/src/views/private/components/notifications-drawer.vue
+++ app/src/views/private/components/notifications-drawer.vue
@@ -39,33 +39,68 @@
 			{{ t('no_notifications_copy') }}
 		</v-info>
 
-		<v-table
-			v-else
-			v-model="selection"
-			v-model:headers="tableHeaders"
-			show-select
-			:loading="loading"
-			:items="notifications"
-			item-key="id"
-			@click:row="onRowClick"
-		></v-table>
+		<v-list v-else-if="loading" class="notifications">
+			<v-skeleton-loader v-for="i in 10" :key="i" />
+		</v-list>
+
+		<v-list v-else class="notifications">
+			<v-list-item
+				v-for="notification in notifications"
+				:key="notification.id"
+				block
+				:clickable="notification.message"
+				@click="toggleNotification(notification.id)"
+			>
+				<div class="header">
+					<v-checkbox
+						:model-value="selection.includes(notification.id)"
+						@update:model-value="toggleSelected(notification.id)"
+					/>
+					<div v-tooltip="notification.subject" class="title">{{ notification.subject }}</div>
+					<div class="spacer" />
+					<div class="time">{{ notification.time }}</div>
+					<v-icon
+						v-if="notification.to"
+						v-tooltip="t('goto_collection_content')"
+						clickable
+						name="open_in_new"
+						@click="onLinkClick(notification.to)"
+					/>
+					<v-icon
+						v-if="notification.message"
+						clickable
+						:name="openNotifications.includes(notification.id) ? 'expand_less' : 'expand_more'"
+					/>
+				</div>
+				<div
+					v-if="openNotifications.includes(notification.id) && notification.message"
+					v-md="notification.message"
+					class="message"
+					@click.stop
+				/>
+			</v-list-item>
+		</v-list>
 	</v-drawer>
 </template>
 
 <script setup lang="ts">
 import api from '@/api';
-import { Header as TableHeader } from '@/components/v-table/types';
 import { useCollectionsStore } from '@/stores/collections';
 import { useNotificationsStore } from '@/stores/notifications';
 import { useUserStore } from '@/stores/user';
 import { localizedFormatDistance } from '@/utils/localized-format-distance';
 import { useAppStore } from '@directus/stores';
-import { Item, Notification } from '@directus/types';
+import { Notification } from '@directus/types';
 import { parseISO } from 'date-fns';
 import { storeToRefs } from 'pinia';
 import { ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
+
+type LocalNotification = Notification & {
+	time: string;
+	to?: string;
+};
 
 const { t } = useI18n();
 const appStore = useAppStore();
@@ -75,32 +110,30 @@ const collectionsStore = useCollectionsStore();
 
 const router = useRouter();
 
-const notifications = ref<Notification[]>([]);
+const notifications = ref<LocalNotification[]>([]);
 const loading = ref(false);
 const error = ref(null);
-const selection = ref([]);
+const selection = ref<string[]>([]);
 const tab = ref(['inbox']);
+const openNotifications = ref<string[]>([]);
+
+function toggleSelected(id: string) {
+	if (selection.value.includes(id)) {
+		selection.value.splice(selection.value.indexOf(id), 1);
+	} else {
+		selection.value.push(id);
+	}
+}
+
+function toggleNotification(id: string) {
+	if (openNotifications.value.includes(id)) {
+		openNotifications.value.splice(openNotifications.value.indexOf(id), 1);
+	} else {
+		openNotifications.value.push(id);
+	}
+}
 
 const { notificationsDrawerOpen } = storeToRefs(appStore);
-
-const tableHeaders = ref<TableHeader[]>([
-	{
-		text: t('subject'),
-		value: 'subject',
-		sortable: false,
-		width: 300,
-		align: 'left',
-		description: null,
-	},
-	{
-		text: t('timestamp'),
-		value: 'timestampDistance',
-		sortable: false,
-		width: 180,
-		align: 'left',
-		description: null,
-	},
-]);
 
 fetchNotifications();
 
@@ -126,7 +159,7 @@ async function fetchNotifications() {
 						},
 					],
 				},
-				fields: ['id', 'subject', 'collection', 'item', 'timestamp'],
+				fields: ['id', 'subject', 'message', 'collection', 'item', 'timestamp'],
 				sort: ['-timestamp'],
 			},
 		});
@@ -135,18 +168,29 @@ async function fetchNotifications() {
 
 		const notificationsRaw = response.data.data as Notification[];
 
-		const notificationsWithRelative: (Notification & { timestampDistance: string })[] = [];
+		notifications.value = notificationsRaw.map((notification) => {
+			let to: string | undefined = undefined;
 
-		for (const notification of notificationsRaw) {
-			notificationsWithRelative.push({
+			if (notification.collection) {
+				const collection = collectionsStore.getCollection(notification.collection);
+
+				if (collection?.meta?.singleton) {
+					to = `/content/${notification.collection}`;
+				} else {
+					to = `/content/${notification.collection}/${notification.item}`;
+				}
+			} else if (String(notification.item).startsWith('/')) {
+				to = String(notification.item);
+			}
+
+			return {
 				...notification,
-				timestampDistance: localizedFormatDistance(parseISO(notification.timestamp), new Date(), {
+				time: localizedFormatDistance(parseISO(notification.timestamp), new Date(), {
 					addSuffix: true,
 				}),
-			});
-		}
-
-		notifications.value = notificationsWithRelative;
+				to,
+			};
+		});
 	} catch (err: any) {
 		error.value = err;
 	} finally {
@@ -156,7 +200,7 @@ async function fetchNotifications() {
 
 async function toggleArchive() {
 	await api.patch('/notifications', {
-		keys: selection.value.map(({ id }) => id),
+		keys: selection.value,
 		data: {
 			status: tab.value[0] === 'inbox' ? 'archived' : 'inbox',
 		},
@@ -167,18 +211,8 @@ async function toggleArchive() {
 	selection.value = [];
 }
 
-function onRowClick({ item }: { item: Item; event: PointerEvent }) {
-	if (item.collection) {
-		const collection = collectionsStore.getCollection(item.collection);
-
-		if (collection?.meta?.singleton) {
-			router.push(`/content/${item.collection}`);
-		} else {
-			router.push(`/content/${item.collection}/${item.item}`);
-		}
-	} else if (String(item.item).startsWith('/')) {
-		router.push(item.item);
-	}
+function onLinkClick(to: string) {
+	router.push(to);
 
 	notificationsDrawerOpen.value = false;
 }
@@ -194,6 +228,56 @@ function onRowClick({ item }: { item: Item; event: PointerEvent }) {
 
 		tr {
 			margin-right: var(--content-padding);
+		}
+	}
+}
+
+.notifications {
+	padding: 0px var(--content-padding) var(--content-padding-bottom) var(--content-padding);
+
+	.v-skeleton-loader {
+		margin-bottom: 8px;
+	}
+
+	.v-list-item {
+		&.block {
+			height: unset;
+			min-height: var(--input-height);
+			flex-flow: wrap;
+			padding: calc(18px) var(--input-padding) calc(10px) var(--input-padding);
+		}
+
+		.header {
+			width: 100%;
+			display: flex;
+			gap: 8px;
+			margin-bottom: 8px;
+
+			.title {
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				max-width: 55%;
+			}
+			.time {
+				color: var(--foreground-subdued);
+			}
+		}
+
+		.spacer {
+			flex-grow: 1;
+		}
+
+		.message {
+			width: 100%;
+			cursor: default;
+
+			&:deep(blockquote) {
+				background-color: var(--background-normal);
+				padding: 4px 8px;
+				margin: 4px 0;
+				border-radius: var(--border-radius);
+			}
 		}
 	}
 }
