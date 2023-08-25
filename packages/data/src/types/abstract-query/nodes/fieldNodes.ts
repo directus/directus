968be7e2f283929d1129eac5dde0f761d557cc17
Jan Arends <jan.arends@mailbox.org>
2023-08-25 15:44:47 +0200

    formatter

--- packages/data/src/types/abstract-query/nodes/fieldNodes.ts
+++ packages/data/src/types/abstract-query/nodes/fieldNodes.ts
@@ -2,4 +2,7 @@ import type { AbstractQueryFieldNodePrimitive } from './primitive.js';
 import type { AbstractQueryFieldNodeFn } from './function.js';
 import type { AbstractQueryFieldNodeRelatedJoinAny } from './related.js';
 
-export type AbstractQueryFieldNode = AbstractQueryFieldNodePrimitive | AbstractQueryFieldNodeFn | AbstractQueryFieldNodeRelatedJoinAny;
+export type AbstractQueryFieldNode =
+	| AbstractQueryFieldNodePrimitive
+	| AbstractQueryFieldNodeFn
+	| AbstractQueryFieldNodeRelatedJoinAny;
