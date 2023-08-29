6aa2cd528c98eef389a9f0444f230e7a80cab5f4
Jan Arends <jan.arends@mailbox.org>
2023-08-29 11:47:30 +0200

    type fix

--- packages/data/src/types/abstract-query/nodes/fieldNodes.ts
+++ packages/data/src/types/abstract-query/nodes/fieldNodes.ts
@@ -1,8 +1,8 @@
 import type { AbstractQueryFieldNodePrimitive } from './primitive.js';
 import type { AbstractQueryFieldNodeFn } from './function.js';
-import type { AbstractQueryFieldNodeRelatedJoinAny } from './related.js';
+import type { AbstractQueryFieldNodeRelated } from './related.js';
 
 export type AbstractQueryFieldNode =
 	| AbstractQueryFieldNodePrimitive
 	| AbstractQueryFieldNodeFn
-	| AbstractQueryFieldNodeRelatedJoinAny;
+	| AbstractQueryFieldNodeRelated;
