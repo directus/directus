d634fc989c4aed68bc6fb3e7903c541710ec3dda
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-09-07 14:04:31 +0200

    Attempt to make "require shares fields" migration more reliable

--- api/src/database/migrations/20230721A-require-shares-fields.ts
+++ api/src/database/migrations/20230721A-require-shares-fields.ts
@@ -1,37 +1,62 @@
+import { createInspector } from '@directus/schema';
 import type { Knex } from 'knex';
+import logger from '../../logger.js';
 
 export async function up(knex: Knex): Promise<void> {
+	await dropConstraint(knex);
+
 	await knex.schema.alterTable('directus_shares', (table) => {
-		if (knex.client.constructor.name === 'Client_MySQL') {
-			// Temporary drop foreign key constraint, see https://github.com/directus/directus/issues/19399
-			table.dropForeign('collection', 'directus_shares_collection_foreign');
-		}
-
 		table.dropNullable('collection');
-
-		if (knex.client.constructor.name === 'Client_MySQL') {
-			// Recreate foreign key constraint, from 20211211A-add-shares.ts
-			table.foreign('collection').references('directus_collections.collection').onDelete('CASCADE');
-		}
-
 		table.dropNullable('item');
 	});
+
+	await recreateConstraint(knex);
 }
 
 export async function down(knex: Knex): Promise<void> {
+	await dropConstraint(knex);
+
 	await knex.schema.alterTable('directus_shares', (table) => {
-		if (knex.client.constructor.name === 'Client_MySQL') {
-			// Temporary drop foreign key constraint, see https://github.com/directus/directus/issues/19399
-			table.dropForeign('collection', 'directus_shares_collection_foreign');
-		}
-
 		table.setNullable('collection');
-
-		if (knex.client.constructor.name === 'Client_MySQL') {
-			// Recreate foreign key constraint, from 20211211A-add-shares.ts
-			table.foreign('collection').references('directus_collections.collection').onDelete('CASCADE');
-		}
-
 		table.setNullable('item');
 	});
+
+	await recreateConstraint(knex);
+}
+
+/**
+ * Temporary drop foreign key constraint for MySQL instances, see https://github.com/directus/directus/issues/19399
+ */
+async function dropConstraint(knex: Knex) {
+	if (knex.client.constructor.name === 'Client_MySQL') {
+		const inspector = createInspector(knex);
+
+		const foreignKeys = await inspector.foreignKeys('directus_shares');
+		const collectionForeignKeys = foreignKeys.filter((fk) => fk.column === 'collection');
+		const constraintName = collectionForeignKeys[0]?.constraint_name;
+
+		if (constraintName && collectionForeignKeys.length === 1) {
+			await knex.schema.alterTable('directus_shares', (table) => {
+				table.dropForeign('collection', constraintName);
+			});
+		} else {
+			logger.warn(
+				`Unexpected number of foreign key constraints on 'directus_shares.collection':`,
+				collectionForeignKeys
+			);
+		}
+	}
+}
+
+/**
+ * Recreate foreign key constraint for MySQL instances, from 20211211A-add-shares.ts
+ */
+async function recreateConstraint(knex: Knex) {
+	if (knex.client.constructor.name === 'Client_MySQL') {
+		return knex.schema.alterTable('directus_shares', async (table) => {
+			if (knex.client.constructor.name === 'Client_MySQL') {
+				table.foreign('collection').references('directus_collections.collection').onDelete('CASCADE');
+			}
+		});
+	}
 }
