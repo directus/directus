58a941449f92cfbb1755a55ffc361d2cdc6c0ea7
albsa <albertsamsir@hotmail.com>
2023-09-26 00:16:00 +0200

    Add subject option to the password reset e-mail

--- api/src/controllers/auth.ts
+++ api/src/controllers/auth.ts
@@ -169,7 +169,7 @@ router.post(
 		const service = new UsersService({ accountability, schema: req.schema });
 
 		try {
-			await service.requestPasswordReset(req.body.email, req.body.reset_url || null);
+			await service.requestPasswordReset(req.body.email, req.body.reset_url || null, req.body.subject || null);
 			return next();
 		} catch (err: any) {
 			if (isDirectusError(err, ErrorCode.InvalidPayload)) {
