68c83cc31f224b16e88fb57207b988342d0d1b7b
Brainslug <tim@brainslug.nl>
2023-09-26 13:15:46 +0200

    Fixed role type on core user schema

--- sdk/src/schema/user.ts
+++ sdk/src/schema/user.ts
@@ -1,4 +1,4 @@
-import type { MergeCoreCollection } from '../index.js';
+import type { DirectusRole, MergeCoreCollection } from '../index.js';
 import type { DirectusFile } from './file.js';
 
 /**
@@ -22,7 +22,7 @@ export type DirectusUser<Schema extends object> = MergeCoreCollection<
 		theme: string | null;
 		tfa_secret: string | null;
 		status: string;
-		role: string | null;
+		role: DirectusRole<Schema> | string | null;
 		token: string | null;
 		last_access: 'datetime' | null;
 		last_page: string | null;
