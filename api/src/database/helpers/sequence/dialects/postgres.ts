16118cc6ff0595fb92ac5013572851e78e8f95d2
Jan Arends <jan.arends@mailbox.org>
2023-09-27 11:16:57 +0200

    fix

--- api/src/database/helpers/sequence/dialects/postgres.ts
+++ api/src/database/helpers/sequence/dialects/postgres.ts
@@ -8,7 +8,7 @@ export class AutoIncrementHelperPostgres extends AutoSequenceHelper {
 	 */
 	override async resetAutoIncrementSequence(table: string, column: string): Promise<Knex.Raw | void> {
 		return await this.knex.raw(
-			`WITH sequence_infos AS (SELECT pg_get_serial_sequence('"${table}"', '${column}') AS seq_name, MAX("${column}") as max_val FROM "${table})" SELECT SETVAL(seq_name, max_val) FROM sequence_infos;`
+			`WITH sequence_infos AS (SELECT pg_get_serial_sequence('"${table}"', '${column}') AS seq_name, MAX("${column}") as max_val FROM "${table}") SELECT SETVAL(seq_name, max_val) FROM sequence_infos;`
 		);
 	}
 }
