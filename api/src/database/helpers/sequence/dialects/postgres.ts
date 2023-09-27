6f76d513163fb3d249fc0833809cc59cd1aa9eee
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-09-27 13:01:01 +0200

    Update api/src/database/helpers/sequence/dialects/postgres.ts

--- api/src/database/helpers/sequence/dialects/postgres.ts
+++ api/src/database/helpers/sequence/dialects/postgres.ts
@@ -6,7 +6,7 @@ export class AutoIncrementHelperPostgres extends AutoSequenceHelper {
 	 * Resets the auto increment sequence based on the max value of the PK column.
 	 * The sequence name is determined using a sub query.
 	 *
-	 * The table name value for getting the sequence name needed to be escaped explicitly,
+	 * The table name value for getting the sequence name needs to be escaped explicitly,
 	 * otherwise PostgreSQL would throw an error for capitalized table names saying "relation x does not exist".
 	 */
 	override async resetAutoIncrementSequence(table: string, column: string): Promise<Knex.Raw | void> {
