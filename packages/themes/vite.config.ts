237e653d8ae0c4941d799044bdddb3f5cb8b120e
Rijk van Zanten <rijkvanzanten@me.com>
2023-09-28 15:24:56 -0400

    Generate json from vite plugin

--- packages/themes/vite.config.ts
+++ packages/themes/vite.config.ts
@@ -3,9 +3,22 @@ import { resolve } from 'node:path';
 import { nodeExternals } from 'rollup-plugin-node-externals';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { writeFile } from 'node:fs/promises';
 
 export default defineConfig({
-	plugins: [{ ...nodeExternals(), enforce: 'pre' }, vue(), dts()],
+	plugins: [
+		{ ...nodeExternals(), enforce: 'pre' },
+		vue(),
+		dts(),
+		{
+			name: 'generate-json-schema',
+			closeBundle: async () => {
+				const { ThemeSchema } = await import('./dist/index.js');
+				const jsonSchema = JSON.stringify(ThemeSchema, null, '\t');
+				await writeFile('./schema.json', jsonSchema);
+			},
+		},
+	],
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
