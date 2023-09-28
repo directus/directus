a2c0675666768e5a91d932f5c725c6dae7a07cb3
Rijk van Zanten <rijkvanzanten@me.com>
2023-09-28 16:36:44 -0400

    Use comments for embed insertion

--- api/src/app.ts
+++ api/src/app.ts
@@ -216,8 +216,8 @@ export default async function createApp(): Promise<express.Application> {
 
 		const htmlWithVars = html
 			.replace(/<base \/>/, `<base href="${adminUrl.toString({ rootRelative: true })}/" />`)
-			.replace(/<embed-head \/>/, embeds.head)
-			.replace(/<embed-body \/>/, embeds.body);
+			.replace('<!-- directus-embed-head -->', embeds.head)
+			.replace('<!-- directus-embed-body -->', embeds.body);
 
 		const sendHtml = (_req: Request, res: Response) => {
 			res.setHeader('Cache-Control', 'no-cache');
