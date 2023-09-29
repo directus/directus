bac0d978fc7622bddc76379190ecdced039dd710
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-09-29 15:44:46 +0200

    add fields forms to onboarding and stop using separate files

--- app/src/routes/onboarding/onboarding.vue
+++ app/src/routes/onboarding/onboarding.vue
@@ -4,8 +4,9 @@
 		<div class="nav">
 			<!-- Directus Logo -->
 			<div class="logo-container">
-				<div class="logo" :class="{ running: isLoading }"></div>
-				<h1 class="logo-type">directus</h1>
+				<div class="logo-wrapper">
+					<div class="logo" :class="{ running: isLoading }"></div>
+				</div>
 			</div>
 
 			<v-progress-linear :value="progressPercent" rounded :indeterminate="isLoading"></v-progress-linear>
@@ -13,12 +14,36 @@
 
 		<!-- Content -->
 		<div class="onboarding-slides">
-			<!-- Test First Slide -->
 			<Transition name="dialog" mode="out-in">
-				<component :is="slides[0]" v-if="currentSlideIndex === 0"></component>
-				<component :is="slides[1]" v-else-if="currentSlideIndex === 1"></component>
-				<component :is="slides[2]" v-else-if="currentSlideIndex === 2"></component>
-				<component :is="LastSlide" v-else-if="currentSlideIndex === 3"></component>
+				<!-- Welcome Slide -->
+				<div class="intro-text" v-if="currentSlideIndex === 0">
+					<h1 class="type-title">{{ t('onboarding.welcome.title') }}</h1>
+					<p class="type-text">{{ t('onboarding.welcome.text') }}</p>
+				</div>
+
+				<!-- Project Slide -->
+				<div class="slide" v-else-if="currentSlideIndex === 1">
+					<div class="intro-text">
+						<h1 class="type-title">{{ t('onboarding.project.title') }}</h1>
+						<p class="type-text">{{ t('onboarding.project.text') }}</p>
+					</div>
+					<v-form v-model="payload.body.project" :fields="projectFields" :autofocus="true" />
+				</div>
+
+				<!-- User Slide -->
+				<div class="slide" v-else-if="currentSlideIndex === 2">
+					<div class="intro-text">
+						<h1 class="type-title">{{ t('onboarding.user.title') }}</h1>
+						<p class="type-text">{{ t('onboarding.user.text') }}</p>
+					</div>
+					<v-form v-model="payload.body.user" :fields="userFields" :autofocus="true" :inline="true" />
+				</div>
+
+				<!-- Last Slide -->
+				<div class="intro-text" v-else-if="currentSlideIndex === 3">
+					<h1 class="type-title">{{ t('onboarding.loading.title') }}</h1>
+					<p class="type-text">{{ t('onboarding.loading.text') }}</p>
+				</div>
 			</Transition>
 		</div>
 
@@ -48,19 +73,36 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { Field } from '@directus/types';
+import { Ref, computed, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
-import LastSlide from './slides/last-slide.vue';
+import VForm from '../../components/v-form/v-form.vue';
 import ProjectSlide from './slides/project.vue';
 import UserSlide from './slides/user.vue';
 import WelcomeSlide from './slides/welcome.vue';
 
+type OnboardingPayload = {
+	version: 1;
+	body: {
+		user?: {};
+		project?: {};
+	};
+};
+
 const { t } = useI18n();
 const router = useRouter();
 
-const currentSlideIndex = ref(0);
+const payload: Ref<OnboardingPayload> = ref({
+	version: 1,
+	body: {
+		user: {},
+		project: {},
+	},
+});
+
 const slides = [WelcomeSlide, ProjectSlide, UserSlide];
+const currentSlideIndex = ref(0);
 const slideCount = slides.length;
 const progressPercent = computed(() => (currentSlideIndex.value / slideCount) * 100);
 const isFirstSlide = computed(() => currentSlideIndex.value === 0);
@@ -70,6 +112,7 @@ const isLoading = ref(false);
 function prevSlide() {
 	currentSlideIndex.value = Math.max(currentSlideIndex.value - 1, 0);
 }
+
 async function skipSlide() {
 	if (isLastSlide.value) {
 		isLoading.value = true;
@@ -77,8 +120,9 @@ async function skipSlide() {
 			router.replace('/content');
 		}, 3000);
 	}
-	currentSlideIndex.value += 1;
+	currentSlideIndex.value = Math.min(currentSlideIndex.value + 1, slideCount);
 }
+
 async function nextSlide() {
 	if (isLastSlide.value) {
 		isLoading.value = true;
@@ -86,8 +130,501 @@ async function nextSlide() {
 			router.replace('/content');
 		}, 3000);
 	}
-	currentSlideIndex.value += 1;
+	currentSlideIndex.value = Math.min(currentSlideIndex.value + 1, slideCount);
 }
+
+const userFields: Field[] = [
+	{
+		collection: 'onboarding',
+		name: t('fields.directus_users.first_name'),
+		field: 'first_name',
+		type: 'string',
+		schema: {
+			name: 'first_name',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'character varying',
+			is_nullable: false,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: 255,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 17,
+			collection: 'onboarding',
+			field: 'first_name',
+			special: null,
+			interface: 'input',
+			options: { placeholder: t('fields.directus_users.first_name'), trim: true },
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 2,
+			width: 'half',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+	{
+		collection: 'onboarding',
+		name: t('fields.directus_users.last_name'),
+		field: 'last_name',
+		type: 'string',
+		schema: {
+			name: 'last_name',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'character varying',
+			is_nullable: false,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: 255,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 18,
+			collection: 'onboarding',
+			field: 'last_name',
+			special: null,
+			interface: 'input',
+			options: { placeholder: t('fields.directus_users.last_name'), trim: true },
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 3,
+			width: 'half',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+	{
+		collection: 'onboarding',
+		name: t('fields.directus_users.email'),
+		field: 'email',
+		type: 'string',
+		schema: {
+			name: 'email',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'character varying',
+			is_nullable: false,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: 255,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 19,
+			collection: 'onboarding',
+			field: 'email',
+			special: null,
+			interface: 'input',
+			options: { placeholder: t('fields.directus_users.email'), trim: true },
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 4,
+			width: 'half',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: { _and: [{ email: { _regex: '.+@.+\\..+' } }] },
+			validation_message: "t('validationError.email')",
+		},
+	},
+	{
+		collection: 'onboarding',
+		name: t('todo'),
+		field: 'wants_emails',
+		type: 'boolean',
+		schema: {
+			name: 'wants_emails',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'boolean',
+			is_nullable: false,
+			generation_expression: null,
+			default_value: false,
+			is_generated: false,
+			max_length: null,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 20,
+			collection: 'onboarding',
+			field: 'wants_emails',
+			special: ['cast-boolean'],
+			interface: 'boolean',
+			options: {
+				label: 'yes',
+			},
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 5,
+			width: 'half',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+	{
+		collection: 'onboarding',
+		name: t('todo'),
+		field: 'questions_user',
+		type: 'string',
+		schema: {
+			name: 'questions_user',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'character varying',
+			is_nullable: true,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: 255,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 25,
+			collection: 'onboarding',
+			field: 'questions_user',
+			special: null,
+			interface: 'select-radio',
+			options: {
+				choices: [
+					{ text: 'Frontend', value: 'frontend' },
+					{ text: 'Backend', value: 'backend' },
+					{ text: 'Fullstack', value: 'fullstack' },
+					{ text: 'SQL queries & basic coding', value: 'sql queries and basic coding' },
+					{ text: 'Non technical', value: 'non technical' },
+				],
+			},
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 6,
+			width: 'full',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+];
+
+const projectFields: Field[] = [
+	{
+		collection: 'onboarding',
+		name: t('fields.directus_settings.project_name'),
+		field: 'project_name',
+		type: 'string',
+		schema: {
+			name: 'project_name',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'character varying',
+			is_nullable: false,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: 255,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 27,
+			collection: 'onboarding',
+			field: 'project_name',
+			special: null,
+			interface: 'input',
+			options: { placeholder: t('fields.directus_settings.project_name'), trim: true },
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 8,
+			width: 'half',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+	{
+		collection: 'onboarding',
+		name: t('fields.directus_settings.project_url'),
+		field: 'public_url',
+		type: 'string',
+		schema: {
+			name: 'public_url',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'character varying',
+			is_nullable: false,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: 255,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 28,
+			collection: 'onboarding',
+			field: 'public_url',
+			special: null,
+			interface: 'input',
+			options: { placeholder: t('fields.directus_settings.project_url'), trim: true },
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 9,
+			width: 'half',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+	{
+		collection: 'onboarding',
+		name: t('fields.directus_settings.project_color'),
+		field: 'brand_color',
+		type: 'string',
+		schema: {
+			name: 'brand_color',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'character varying',
+			is_nullable: false,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: 255,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 29,
+			collection: 'onboarding',
+			field: 'brand_color',
+			special: null,
+			interface: 'select-color',
+			options: { placeholder: t('fields.directus_settings.project_color'), trim: true },
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 11,
+			width: 'half',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+	{
+		collection: 'onboarding',
+		name: t('fields.directus_settings.project_logo'),
+		field: 'project_logo',
+		type: 'uuid',
+		schema: {
+			name: 'project_logo',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'uuid',
+			is_nullable: true,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: null,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: 'public',
+			foreign_key_table: 'directus_files',
+			foreign_key_column: 'id',
+		},
+		meta: {
+			id: 30,
+			collection: 'onboarding',
+			field: 'project_logo',
+			special: ['file'],
+			interface: 'file-image',
+			options: { crop: false },
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 10,
+			width: 'half',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+	{
+		collection: 'onboarding',
+		name: t('todo'),
+		field: 'questions_project',
+		type: 'string',
+		schema: {
+			name: 'questions_project',
+			table: 'onboarding',
+			schema: 'public',
+			data_type: 'character varying',
+			is_nullable: true,
+			generation_expression: null,
+			default_value: null,
+			is_generated: false,
+			max_length: 255,
+			comment: null,
+			numeric_precision: null,
+			numeric_scale: null,
+			is_unique: false,
+			is_primary_key: false,
+			has_auto_increment: false,
+			foreign_key_schema: null,
+			foreign_key_table: null,
+			foreign_key_column: null,
+		},
+		meta: {
+			id: 31,
+			collection: 'onboarding',
+			field: 'questions_project',
+			special: null,
+			interface: 'select-radio',
+			options: {
+				choices: [
+					{ text: 'Personal project', value: 'personal project' },
+					{ text: 'Work project', value: 'work project' },
+					// Attention! Small hack: Make one option so long that <select-radio> renders with grid-1
+					{ text: 'Just exploring            ', value: 'just exploring' },
+				],
+			},
+			display: null,
+			display_options: null,
+			readonly: false,
+			hidden: false,
+			sort: 12,
+			width: 'full',
+			translations: null,
+			note: null,
+			conditions: null,
+			required: false,
+			group: null,
+			validation: null,
+			validation_message: null,
+		},
+	},
+];
 </script>
 
 <style scoped>
@@ -143,16 +680,26 @@ async function nextSlide() {
 }
 
 .onboarding-slides {
-	/* min-height: 300px; */
 	width: 100%;
 }
 
-.logo {
-	width: 80px;
+.logo-wrapper {
+	padding: 8px 12px;
+	border-radius: var(--border-radius-outline);
+	width: 64px;
 	height: 64px;
+	background-color: var(--purple);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+.logo {
+	width: 40px;
+	height: 32px;
+	transform: translateY(4px); /* To center the animated rabbit because the sprite has too much height */
 	background-image: url('@/assets/sprite.svg');
 	background-position: 0% 0%;
-	background-size: 1200px 64px;
+	background-size: 600px 32px;
 }
 
 .logo-container {
@@ -176,5 +723,18 @@ async function nextSlide() {
 	100% {
 		background-position: 100%;
 	}
+}
+
+.intro-text {
+	margin-bottom: 64px;
+}
+
+/* Inherits from global class */
+.type-title {
+	margin-bottom: 8px;
+}
+/* Inherits from global class */
+.type-text {
+	max-width: 600px;
 }
 </style>
