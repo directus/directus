f85243e2b3121a9b40df9e23a124419d2d3d36e9
Brainslug <tim@brainslug.nl>
2023-10-04 21:21:53 +0200

    made the fetch type a lot broader

--- sdk/src/types/globals.ts
+++ sdk/src/types/globals.ts
@@ -1,6 +1,7 @@
-export type FetchInterface =
-	| ((input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response | unknown>)
-	| ((input: RequestInfo, init?: RequestInit | undefined) => Promise<Response | unknown>);
+// had to take the "| any" way out because supporting each and every "fetch" alternative type
+// is not feasable because all have different custom properties we cannot account for and the
+// "unknown" return type is checked at runtime by the extractData function
+export type FetchInterface = (input: string | any, init?: RequestInit | any) => Promise<unknown>;
 
 export type UrlInterface = typeof URL;
 
