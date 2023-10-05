30a1805c409233eea02750517dcb444b4bec546d
Brainslug <tim@brainslug.nl>
2023-10-05 12:29:15 +0200

    updated comment

--- sdk/src/types/globals.ts
+++ sdk/src/types/globals.ts
@@ -1,6 +1,4 @@
-// had to take the "| any" way out because supporting each and every "fetch" alternative type
-// is not feasable because all have different custom properties we cannot account for and the
-// "unknown" return type is checked at runtime by the extractData function
+// using "| any" to ensure compatibility with various "Fetch" alternative function signatures
 export type FetchInterface = (input: string | any, init?: RequestInit | any) => Promise<unknown>;
 
 export type UrlInterface = typeof URL;
