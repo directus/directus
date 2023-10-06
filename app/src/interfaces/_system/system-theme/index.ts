3fb99c36e451a00da0221a809b74714a9bada51c
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-06 16:16:01 -0400

    Fix import

--- app/src/interfaces/_system/system-theme/index.ts
+++ app/src/interfaces/_system/system-theme/index.ts
@@ -1,4 +1,4 @@
-import { defineInterface } from '@directus/utils';
+import { defineInterface } from '@directus/extensions';
 import InterfaceSystemTheme from './system-theme.vue';
 
 export default defineInterface({
