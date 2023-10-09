1adaf08bfb556b290c9d8a9ba1f19414f6047fc6
Brainslug <tim@brainslug.nl>
2023-10-09 20:13:24 +0200

    fixed the output typing

--- sdk/src/types/aggregate.ts
+++ sdk/src/types/aggregate.ts
@@ -46,7 +46,7 @@ export type AggregationTypes = {
  * Aggregation parameters
  */
 export type AggregateRecord<Fields = string> = {
-	[Func in keyof AggregationTypes]?: Fields | (AggregationTypes[Func]['wildcard'] extends never ? never : '*');
+	[Func in keyof AggregationTypes]?: Fields | Fields[] | (AggregationTypes[Func]['wildcard'] extends never ? never : '*');
 };
 
 /**
@@ -96,11 +96,17 @@ export type AggregationOutput<
 		: never
 	: object) & {
 	[Func in keyof Options['aggregate']]: Func extends keyof AggregationTypes
-		? Options['aggregate'][Func] extends string
-			? Options['aggregate'][Func] extends '*'
-				? AggregationTypes[Func]['output']
-				: { [Field in Options['aggregate'][Func]]: AggregationTypes[Func]['output'] }
-			: never
+		? Options['aggregate'][Func] extends string[]
+			? {
+				[Field in UnpackList<Options['aggregate'][Func]>]: Field extends '*'
+					? AggregationTypes[Func]['output']
+					: { [SubField in Field]: AggregationTypes[Func]['output'] }[Field]
+			}
+			: Options['aggregate'][Func] extends string
+				? Options['aggregate'][Func] extends '*'
+					? AggregationTypes[Func]['output']
+					: { [SubField in Options['aggregate'][Func]]: AggregationTypes[Func]['output'] }[Options['aggregate'][Func]]
+				: never
 		: never;
 })[];
 
