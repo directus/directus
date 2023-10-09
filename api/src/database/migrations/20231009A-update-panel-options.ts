f3d0bd2d16a5860107cc6bb1ae61a0eb4f0c74d6
Connor <12714889+ConnorSimply@users.noreply.github.com>
2023-10-09 17:32:30 -0500

    Fix Prettier

--- api/src/database/migrations/20231009A-update-panel-options.ts
+++ api/src/database/migrations/20231009A-update-panel-options.ts
@@ -1,83 +1,87 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-    const panels = await knex('directus_panels').where('type', '=', 'metric').select();
+	const panels = await knex('directus_panels').where('type', '=', 'metric').select();
 
-    for (const panel of panels) {
-        let options = panel.options;
+	for (const panel of panels) {
+		let options = panel.options;
 
-        // Check if the options are stringified and parse them
-        const wasStringified = typeof options === 'string';
-        if (wasStringified) {
-            options = JSON.parse(options);
-        }
+		// Check if the options are stringified and parse them
+		const wasStringified = typeof options === 'string';
+		if (wasStringified) {
+			options = JSON.parse(options);
+		}
 
-        let needsUpdate = false;
+		let needsUpdate = false;
 
-        // Check and update abbreviate -> notation
-        if (options && options.abbreviate === true) {
-            options.notation = 'compact';
-            delete options.abbreviate;
-            needsUpdate = true;
-        }
+		// Check and update abbreviate -> notation
+		if (options && options.abbreviate === true) {
+			options.notation = 'compact';
+			delete options.abbreviate;
+			needsUpdate = true;
+		}
 
-        // Check and update decimals -> minimumFractionDigits and maximumFractionDigits
-        if (options && typeof options.decimals === 'number') {
-            options.minimumFractionDigits = options.decimals;
-            options.maximumFractionDigits = options.decimals;
-            delete options.decimals;
-            needsUpdate = true;
-        }
+		// Check and update decimals -> minimumFractionDigits and maximumFractionDigits
+		if (options && typeof options.decimals === 'number') {
+			options.minimumFractionDigits = options.decimals;
+			options.maximumFractionDigits = options.decimals;
+			delete options.decimals;
+			needsUpdate = true;
+		}
 
-        // Convert the options back to string if they were stringified initially
-        if (wasStringified) {
-            options = JSON.stringify(options);
-        }
+		// Convert the options back to string if they were stringified initially
+		if (wasStringified) {
+			options = JSON.stringify(options);
+		}
 
-        // Update the row with modified options if necessary
-        if (needsUpdate) {
-            await knex('directus_panels').update({ options }).where('id', panel.id);
-        }
-    }
+		// Update the row with modified options if necessary
+		if (needsUpdate) {
+			await knex('directus_panels').update({ options }).where('id', panel.id);
+		}
+	}
 }
 
 export async function down(knex: Knex): Promise<void> {
-    const panels = await knex('directus_panels').where('type', '=', 'metric').select();
+	const panels = await knex('directus_panels').where('type', '=', 'metric').select();
 
-    for (const panel of panels) {
-        let options = panel.options;
+	for (const panel of panels) {
+		let options = panel.options;
 
-        // Check if the options are stringified and parse them
-        const wasStringified = typeof options === 'string';
-        if (wasStringified) {
-            options = JSON.parse(options);
-        }
+		// Check if the options are stringified and parse them
+		const wasStringified = typeof options === 'string';
+		if (wasStringified) {
+			options = JSON.parse(options);
+		}
 
-        let needsUpdate = false;
+		let needsUpdate = false;
 
-        // Revert notation -> abbreviate
-        if (options && options.notation === 'compact') {
-            options.abbreviate = true;
-            delete options.notation;
-            needsUpdate = true;
-        }
+		// Revert notation -> abbreviate
+		if (options && options.notation === 'compact') {
+			options.abbreviate = true;
+			delete options.notation;
+			needsUpdate = true;
+		}
 
-        // Revert minimumFractionDigits and maximumFractionDigits -> decimals
-        if (options && typeof options.minimumFractionDigits === 'number' && options.minimumFractionDigits === options.maximumFractionDigits) {
-            options.decimals = options.minimumFractionDigits;
-            delete options.minimumFractionDigits;
-            delete options.maximumFractionDigits;
-            needsUpdate = true;
-        }
+		// Revert minimumFractionDigits and maximumFractionDigits -> decimals
+		if (
+			options &&
+			typeof options.minimumFractionDigits === 'number' &&
+			options.minimumFractionDigits === options.maximumFractionDigits
+		) {
+			options.decimals = options.minimumFractionDigits;
+			delete options.minimumFractionDigits;
+			delete options.maximumFractionDigits;
+			needsUpdate = true;
+		}
 
-        // Convert the options back to string if they were stringified initially
-        if (wasStringified) {
-            options = JSON.stringify(options);
-        }
+		// Convert the options back to string if they were stringified initially
+		if (wasStringified) {
+			options = JSON.stringify(options);
+		}
 
-        // Update the row with reverted options if necessary
-        if (needsUpdate) {
-            await knex('directus_panels').update({ options }).where('id', panel.id);
-        }
-    }
+		// Update the row with reverted options if necessary
+		if (needsUpdate) {
+			await knex('directus_panels').update({ options }).where('id', panel.id);
+		}
+	}
 }
