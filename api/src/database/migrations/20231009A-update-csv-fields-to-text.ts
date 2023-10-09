4142b0e237ea7a7a546559b4c31c9524d5cf0782
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-10-09 17:27:04 +0200

    linter says helloooo

--- api/src/database/migrations/20231009A-update-csv-fields-to-text.ts
+++ api/src/database/migrations/20231009A-update-csv-fields-to-text.ts
@@ -4,8 +4,10 @@ import { getHelpers } from '../helpers/index.js';
 export async function up(knex: Knex): Promise<void> {
 	const helper = getHelpers(knex).schema;
 	const csvFields = await knex.select('collection', 'field').from('directus_fields').where('special', '=', 'cast-csv');
+
 	for (const entry of csvFields) {
 		const { defaultValue, nullable } = await knex(entry.collection).columnInfo(entry.field);
+
 		await helper.changeToType(entry.collection, entry.field, 'text', {
 			default: defaultValue,
 			nullable,
@@ -16,8 +18,10 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
 	const helper = getHelpers(knex).schema;
 	const csvFields = await knex.select('collection', 'field').from('directus_fields').where('special', '=', 'cast-csv');
+
 	for (const entry of csvFields) {
 		const { defaultValue, nullable } = await knex(entry.collection).columnInfo(entry.field);
+
 		await helper.changeToType(entry.collection, entry.field, 'string', {
 			default: defaultValue,
 			nullable,
