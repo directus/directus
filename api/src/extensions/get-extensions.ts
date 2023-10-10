9b4c09b096ecb95e013100e07956b8c9cbff1773
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-10 14:09:15 -0400

    Prevent duplicate extensions from being loaded

--- api/src/extensions/get-extensions.ts
+++ api/src/extensions/get-extensions.ts
@@ -1,10 +1,19 @@
+import type { Extension } from '@directus/extensions';
 import { getLocalExtensions, getPackageExtensions, resolvePackageExtensions } from '@directus/extensions/node';
 import env from '../env.js';
 
 export const getExtensions = async () => {
-	const packageExtensions = await getPackageExtensions(env['PACKAGE_FILE_LOCATION']);
-	const localPackageExtensions = await resolvePackageExtensions(env['EXTENSIONS_PATH']);
 	const localExtensions = await getLocalExtensions(env['EXTENSIONS_PATH']);
+
+	const loadedNames = localExtensions.map(({ name }) => name);
+
+	const filterDuplicates = ({ name }: Extension) => loadedNames.includes(name) === false;
+
+	const localPackageExtensions = (await resolvePackageExtensions(env['EXTENSIONS_PATH'])).filter(filterDuplicates);
+
+	loadedNames.push(...localPackageExtensions.map(({ name }) => name));
+
+	const packageExtensions = (await getPackageExtensions(env['PACKAGE_FILE_LOCATION'])).filter(filterDuplicates);
 
 	return [...packageExtensions, ...localPackageExtensions, ...localExtensions];
 };
