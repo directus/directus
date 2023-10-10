edf9a525fc42ffedd7288559624fd5871034336f
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-10 14:59:47 -0400

    Add commented out columns
    For part 2!

--- api/src/database/migrations/20231010A-add-extensions.ts
+++ api/src/database/migrations/20231010A-add-extensions.ts
@@ -4,6 +4,8 @@ export async function up(knex: Knex): Promise<void> {
 	await knex.schema.createTable('directus_extensions', (table) => {
 		table.string('name').primary().notNullable();
 		table.boolean('enabled').defaultTo(true).notNullable();
+		// table.json('permissions');
+		// table.json('options');
 	});
 }
 
