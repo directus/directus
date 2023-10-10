ae0466b94f434a904af30fb655a1dc95645dda6b
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-10 15:00:12 -0400

    Normalize settings into extension info

--- api/src/extensions/normalize-extension-info.ts
+++ api/src/extensions/normalize-extension-info.ts
@@ -1,28 +1,34 @@
-import type { BundleExtension, Extension, ExtensionInfo, NestedExtensionType } from '@directus/extensions';
+import type { Extension, ExtensionInfo, ExtensionSettings, NormalizedBundleExtension } from '@directus/extensions';
+import { omit } from 'lodash-es';
 
 /**
  * Creates a standardized extension info object for a given extension
  */
-export const normalizeExtensionInfo = (extension: Extension): ExtensionInfo => {
+export const normalizeExtensionInfo = (extension: Extension, settings: ExtensionSettings[]): ExtensionInfo => {
 	const extensionInfo: ExtensionInfo = {
 		name: extension.name,
 		type: extension.type,
 		local: extension.local,
 		entries: [],
+		settings: omit(
+			settings.find(({ name }) => extension.name === name),
+			'name'
+		),
 	};
 
 	if (extension.host) extensionInfo.host = extension.host;
 	if (extension.version) extensionInfo.version = extension.version;
 
 	if (extension.type === 'bundle') {
-		const bundleExtensionInfo: Omit<BundleExtension, 'entrypoint' | 'path'> = {
-			name: extensionInfo.name,
+		const bundleExtensionInfo: NormalizedBundleExtension = {
 			type: 'bundle',
+			name: extensionInfo.name,
 			local: extensionInfo.local,
 			entries: extension.entries.map((entry) => ({
 				name: entry.name,
 				type: entry.type,
-			})) as { name: ExtensionInfo['name']; type: NestedExtensionType }[],
+				settings: omit(settings.find(({ name }) => `${extension.name}/${entry.name}` === name)!, 'name'),
+			})),
 		};
 
 		return bundleExtensionInfo;
