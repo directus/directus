31fea0d4bf097535edcc5ccf39e1617233840f53
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-10 14:59:31 -0400

    Add extension settings type

--- packages/extensions/shared/types/info.ts
+++ packages/extensions/shared/types/info.ts
@@ -1,7 +1,30 @@
 import type { ApiExtension, AppExtension, BundleExtension, HybridExtension } from './extension-types.js';
 
+export type NormalizedAppExtension = Omit<AppExtension, 'entrypoint' | 'path'> & {
+	settings: Omit<ExtensionSettings, 'name'>;
+};
+
+export type NormalizedApiExtension = Omit<ApiExtension, 'entrypoint' | 'path'> & {
+	settings: Omit<ExtensionSettings, 'name'>;
+};
+
+export type NormalizedHybridExtension = Omit<HybridExtension, 'entrypoint' | 'path'> & {
+	settings: Omit<ExtensionSettings, 'name'>;
+};
+
+export type NormalizedBundleExtension = Omit<BundleExtension, 'entrypoint' | 'path'> & {
+	entries: (BundleExtension['entries'][number] & { settings: Omit<ExtensionSettings, 'name'> })[];
+};
+
 export type ExtensionInfo =
-	| Omit<AppExtension, 'entrypoint' | 'path'>
-	| Omit<ApiExtension, 'entrypoint' | 'path'>
-	| Omit<HybridExtension, 'entrypoint' | 'path'>
-	| Omit<BundleExtension, 'entrypoint' | 'path'>;
+	| NormalizedApiExtension
+	| NormalizedAppExtension
+	| NormalizedBundleExtension
+	| NormalizedHybridExtension;
+
+export interface ExtensionSettings {
+	name: string;
+	enabled: boolean;
+	// options: Record<string, unknown> | null;
+	// permissions: Record<string, unknown> | null;
+}
