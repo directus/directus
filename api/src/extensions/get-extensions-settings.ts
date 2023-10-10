466a69189c4b8b995025df63e92d533dd6dba1b0
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-10 14:59:38 -0400

    Add settings getter with db sync

---
+++ api/src/extensions/get-extensions-settings.ts
@@ -0,0 +1,46 @@
+import type { Extension, ExtensionSettings } from '@directus/extensions';
+import { difference } from 'lodash-es';
+import getDatabase from '../database/index.js';
+
+/**
+ * Loads stored settings for all extensions. Creates empty new rows in extensions tables for
+ * extensions that don't have settings yet, and remove any settings for extensions that are no
+ * longer installed.
+ */
+export const getExtensionsSettings = async (extensions: Extension[]) => {
+	/**
+	 * @TODO bundles
+	 */
+
+	const database = getDatabase();
+
+	const settings = await database.select<ExtensionSettings[]>('*').from('directus_extensions');
+
+	const extensionNames = extensions
+		.map((extension) => {
+			if (extension.type === 'bundle') {
+				return extension.entries.map((entry) => `${extension.name}/${entry.name}`);
+			}
+
+			return extension.name;
+		})
+		.flat();
+
+	const extensionSettingNames = settings.map(({ name }) => name);
+
+	const missing = difference(extensionNames, extensionSettingNames);
+
+	if (missing.length > 0) {
+		const missingRows = missing.map((name) => ({ name, enabled: true, options: null, permissions: null }));
+		await database.insert(missingRows).into('directus_extensions');
+		settings.push(...missingRows);
+	}
+
+	/**
+	 * Silently ignore settings for extensions that have been manually removed from the extensions
+	 * folder. Having them automatically synced feels dangerous, as it's a destructive action. In the
+	 * edge case you'd deploy / start with the extensions folder misconfigured, it would remove all
+	 * previous options on startup, without an option to undo.
+	 */
+	return settings.filter(({ name }) => extensionNames.includes(name));
+};
