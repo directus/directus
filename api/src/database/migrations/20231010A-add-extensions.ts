9cb14bc793935e5528775efeffc1ba9fe4f59dc8
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-12 18:03:44 -0400

    Remove commented fields

--- api/src/database/migrations/20231010A-add-extensions.ts
+++ api/src/database/migrations/20231010A-add-extensions.ts
@@ -4,8 +4,6 @@ export async function up(knex: Knex): Promise<void> {
 	await knex.schema.createTable('directus_extensions', (table) => {
 		table.string('name').primary().notNullable();
 		table.boolean('enabled').defaultTo(true).notNullable();
-		// table.json('permissions');
-		// table.json('options');
 	});
 }
 
