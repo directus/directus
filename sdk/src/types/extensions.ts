63dfbe3d4da5141cbef702724a0adbcdc725dd25
Brainslug <br41nslug@users.noreply.github.com>
2023-10-13 16:40:26 +0200

    Update sdk/src/types/extensions.ts
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>

--- sdk/src/types/extensions.ts
+++ sdk/src/types/extensions.ts
@@ -2,12 +2,12 @@
 export type ExtensionTypes = "interface" | "display" | "layout" | "module" | "panel" | "hook" | "endpoint" | "operation" | "bundle";
 
 /**
- * Copied from @directus/extensions
  * The API output structure used when engaging with the /extensions endpoints
+ * (Copied from `@directus/extensions`)
  */
 export interface ExtensionItem {
 	name: string;
 	bundle: string | null;
 	schema: { type: ExtensionTypes, local: boolean } | null;
-	meta: { enabled: boolean; /* options, permissions? */ };
+	meta: { enabled: boolean };
 }
