391dad2d957b49b18a8db8c643a13ff1e72ca005
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-10-16 14:01:59 +0200

    Don't require hash for GQL

--- api/src/database/migrations/20230823A-add-content-versioning.ts
+++ api/src/database/migrations/20230823A-add-content-versioning.ts
@@ -14,7 +14,10 @@ export async function up(knex: Knex): Promise<void> {
 			.onDelete('CASCADE');
 
 		table.string('item').notNullable();
-		table.string('hash').notNullable();
+
+		// Hash is managed on API side
+		table.string('hash');
+
 		table.timestamp('date_created').defaultTo(knex.fn.now());
 		table.timestamp('date_updated').defaultTo(knex.fn.now());
 		table.uuid('user_created').references('id').inTable('directus_users').onDelete('SET NULL');
