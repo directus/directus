56e079a60abc383d0a12ff36db77a6f00f8ce0e0
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-10-21 01:34:54 +0200

    Initialize isolated-vm for Vitest

--- api/globalSetup.js
+++ api/globalSetup.js
@@ -3,5 +3,6 @@
 // This is to ensure shared libraries remain loaded in memory until after all threads are complete.
 // Without this, the following error may occur: Module did not self-register
 import 'sharp';
+import 'isolated-vm';
 
 export default function () {}
