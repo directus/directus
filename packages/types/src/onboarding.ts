f12ae7593b6e00b79435a5d59701b9dcdc3f29aa
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-10-20 15:51:35 +0200

    change keys of payload type to required

--- packages/types/src/onboarding.ts
+++ packages/types/src/onboarding.ts
@@ -1,12 +1,12 @@
 export type OnboardingPayload = {
 	version: 1;
 	body: {
-		user?: {
+		user: {
 			email: string | null;
 			wants_emails: boolean | null;
 			primary_skillset: string | null;
 		};
-		project?: {
+		project: {
 			name: string | null;
 			url: string | null;
 			type: string | null;
