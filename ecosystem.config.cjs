18482d86602d698d0bc42056abd76881aca89e7f
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-24 12:14:22 -0400

    Update ecosystem.config.cjs
    Co-authored-by: Wryn (yage) Wagner <yage@yage.io>

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,17 +12,17 @@ module.exports = [
 		args: ['start'],
 
 		// General
-		instances: process.env.PM2_INSTANCES ?? 'max',
+		instances: process.env.PM2_INSTANCES ?? '1',
 		exec_mode: process.env.PM2_EXEC_MODE ?? 'cluster',
-		max_memory_restart: process.env.PM2_MAX_MEMORY_RESTART ?? '1G',
+		max_memory_restart: process.env.PM2_MAX_MEMORY_RESTART ?? '',
 
 		// Control flow
-		min_uptime: process.env.PM2_MIN_UPTIME ?? 1000,
-		listen_timeout: process.env.PM2_LISTEN_TIMEOUT ?? 8000,
-		kill_timeout: process.env.PM2_KILL_TIMEOUT ?? 10000,
+		min_uptime: process.env.PM2_MIN_UPTIME ?? '',
+		listen_timeout: process.env.PM2_LISTEN_TIMEOUT ?? '',
+		kill_timeout: process.env.PM2_KILL_TIMEOUT ?? '',
 		wait_ready: true,
-		max_restarts: process.env.PM2_MAX_RESTARTS ?? 5,
+		max_restarts: process.env.PM2_MAX_RESTARTS ?? '',
 		restart_delay: process.env.PM2_RESTART_DELAY ?? 0,
-		autorestart: process.env.PM2_AUTO_RESTART ?? true,
+		autorestart: process.env.PM2_AUTO_RESTART ?? false,
 	},
 ];
