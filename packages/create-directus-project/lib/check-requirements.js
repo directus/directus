d8a1b39d1a0081dc142996a59b5e382e08baa524
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-24 11:21:15 -0400

    Require node 18+
    Fixes #20165

--- packages/create-directus-project/lib/check-requirements.js
+++ packages/create-directus-project/lib/check-requirements.js
@@ -6,9 +6,9 @@ export default function checkRequirements() {
 	const nodeVersion = process.versions.node;
 	const major = +nodeVersion.split('.')[0];
 
-	if (major < 12) {
+	if (major < 18) {
 		console.error(`You are running ${chalk.red(`Node ${nodeVersion}`)}.`);
-		console.error(`Directus requires ${chalk.green(`Node 12`)} and up.`);
+		console.error(`Directus requires ${chalk.green(`Node 18`)} and up.`);
 		console.error('Please update your Node version and try again.');
 		process.exit(1);
 	}
