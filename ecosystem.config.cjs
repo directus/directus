0e19941bd4d424b63900d9b1178b9f0c98f1d0f9
Rijk van Zanten <rijkvanzanten@me.com>
2023-10-25 14:19:46 -0400

    Default to undefined instead of empty string

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -14,14 +14,14 @@ module.exports = [
 		// General
 		instances: process.env.PM2_INSTANCES ?? '1',
 		exec_mode: process.env.PM2_EXEC_MODE ?? 'cluster',
-		max_memory_restart: process.env.PM2_MAX_MEMORY_RESTART ?? '',
+		max_memory_restart: process.env.PM2_MAX_MEMORY_RESTART,
 
 		// Control flow
-		min_uptime: process.env.PM2_MIN_UPTIME ?? '',
-		listen_timeout: process.env.PM2_LISTEN_TIMEOUT ?? '',
-		kill_timeout: process.env.PM2_KILL_TIMEOUT ?? '',
+		min_uptime: process.env.PM2_MIN_UPTIME,
+		listen_timeout: process.env.PM2_LISTEN_TIMEOUT,
+		kill_timeout: process.env.PM2_KILL_TIMEOUT,
 		wait_ready: true,
-		max_restarts: process.env.PM2_MAX_RESTARTS ?? '',
+		max_restarts: process.env.PM2_MAX_RESTARTS,
 		restart_delay: process.env.PM2_RESTART_DELAY ?? 0,
 		autorestart: process.env.PM2_AUTO_RESTART ?? false,
 	},
