a61c8c79af069b174c6f3d4dc822a7806e73472a
Connor <12714889+ConnorSimply@users.noreply.github.com>
2023-10-26 02:33:52 -0500

    Update Bug Report & Feature Request Links to Use Templates (#20197)

---
+++ .changeset/friendly-students-march.md
@@ -0,0 +1,5 @@
+---
+"@directus/app": patch
+---
+
+Updated Bug Report and Feature Request Links in Settings to Use Templates
--- app/src/modules/settings/components/navigation.vue
+++ app/src/modules/settings/components/navigation.vue
@@ -62,12 +62,12 @@ const externalItems = computed(() => {
 		{
 			icon: 'bug_report',
 			name: t('report_bug'),
-			href: 'https://github.com/directus/directus/issues/new',
+			href: 'https://github.com/directus/directus/issues/new?template=bug_report.yml',
 		},
 		{
 			icon: 'new_releases',
 			name: t('request_feature'),
-			href: 'https://github.com/directus/directus/discussions/new',
+			href: 'https://github.com/directus/directus/discussions/new?category=feature-requests',
 		},
 	];
 });
