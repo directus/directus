8eaa8a7e43ff0a80356485641833fff3ba7199a1
Jan Arends <jan.arends@mailbox.org>
2023-10-27 16:43:43 +0200

    docs

--- packages/data/src/types/abstract-query/fields/nested/relational.ts
+++ packages/data/src/types/abstract-query/fields/nested/relational.ts
@@ -45,6 +45,10 @@ export interface AbstractQueryFieldNodeRelationalOneToAny {
 
 /**
  * Used to build a relational query for m2o and o2m relations.
+ *
+ * A record may be identified across multiple field values (e.g. via a composite primary key in SQL).
+ * Hence the field(s) are stored in an array.
+ *
  * @example
  * ```
  * const functionNode = {
@@ -54,20 +58,38 @@ export interface AbstractQueryFieldNodeRelationalOneToAny {
  * 	foreign: {
  * 		store: 'mongodb',
  * 		collection: 'some-collection',
+ * 		fields: ['author_id'],
  *    },
  * };
  * ```
  */
 export interface AbstractQueryFieldNodeRelationalJoinMany {
-	/** the fields of the current collection which have the relational value to an external collection or item */
+	/**
+	 * The field names which identify an item in the...
+	 *
+	 * m2o: MANY collection. It has the references stored to the ONE item (the foreign key in SQL).
+	 * o2m: ONE collection. It has the values stored which identifies an item (the primary key in SQL).
+	 **/
 	local: {
 		fields: AtLeastOneElement<string>;
 	};
 
-	/** the external collection or item which should be pulled/joined/merged into the current collection */
+	/**
+	 * m2o: information of the ONE collection
+	 * o2m: information of the MANY collection
+	 **/
 	foreign: {
+		/** In the future this collection can also be stored in a different datastore. */
 		store: string;
+
 		collection: string;
+
+		/**
+		 * The field names which identify an item in the...
+		 *
+		 * m2o: ONE collection.
+		 * o2m: MANY collection.
+		 **/
 		fields: AtLeastOneElement<string>;
 	};
 }
