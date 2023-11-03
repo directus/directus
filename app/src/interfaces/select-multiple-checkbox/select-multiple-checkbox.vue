853e33f4bed950fb7b56379678541d7555c508de
Rijk van Zanten <rijkvanzanten@me.com>
2023-11-03 17:40:41 -0400

    Use var defaults for v-radio

--- app/src/interfaces/select-multiple-checkbox/select-multiple-checkbox.vue
+++ app/src/interfaces/select-multiple-checkbox/select-multiple-checkbox.vue
@@ -215,11 +215,11 @@ const { otherValues, addOtherValue, setOtherValue } = useCustomSelectionMultiple
 	}
 
 	&.active {
-		--v-icon-color: var(--v-radio-color);
+		--v-icon-color: var(--v-radio-color, var(--theme--primary));
 
 		position: relative;
 		background-color: transparent;
-		border-color: var(--v-radio-color);
+		border-color: var(--v-radio-color, var(--theme--primary));
 
 		&::before {
 			position: absolute;
@@ -227,7 +227,7 @@ const { otherValues, addOtherValue, setOtherValue } = useCustomSelectionMultiple
 			left: 0;
 			width: 100%;
 			height: 100%;
-			background-color: var(--v-radio-color);
+			background-color: var(--v-radio-color, var(--theme--primary));
 			opacity: 0.1;
 			content: '';
 			pointer-events: none;
