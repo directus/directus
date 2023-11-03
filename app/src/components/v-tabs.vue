04f0f8355ae0bf2c737a9d4d7b3c7b5da77ec915
Rijk van Zanten <rijkvanzanten@me.com>
2023-11-03 16:50:46 -0400

    Remove unused v-tabs var

--- app/src/components/v-tabs.vue
+++ app/src/components/v-tabs.vue
@@ -47,10 +47,6 @@ function update(newSelection: readonly (string | number)[]) {
 </template>
 
 <style scoped>
-:global(body) {
-	--v-tabs-underline-color: var(--theme--foreground);
-}
-
 .v-tabs.horizontal {
 	position: relative;
 	display: inline-flex;
