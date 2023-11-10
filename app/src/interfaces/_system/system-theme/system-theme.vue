1e655922084a4a7c22bb15d5cb73f5ec8c816c4b
Rijk van Zanten <rijkvanzanten@me.com>
2023-11-10 16:04:20 -0500

    WIP add theme selector interface

--- app/src/interfaces/_system/system-theme/system-theme.vue
+++ app/src/interfaces/_system/system-theme/system-theme.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useThemeStore } from '@directus/themes';
 import { computed } from 'vue';
-import { useI18n } from 'vue-i18n';
+import ThemePreview from './theme-preview.vue';
 
 defineEmits<{
 	input: [string | null];
@@ -17,17 +17,70 @@ const themeStore = useThemeStore();
 
 const items = computed(() => themeStore.themes[props.appearance].map((theme) => theme.name));
 
-const { t } = useI18n();
-
 const valueWithDefault = computed(() => props.value ?? themeStore.themes[props.appearance][0]?.name ?? null);
 </script>
 
 <template>
-	<v-select
-		:model-value="valueWithDefault"
-		:disabled="disabled"
-		:items="items"
-		:placeholder="t('select_a_theme')"
-		@update:model-value="$emit('input', $event)"
-	/>
+	<div class="interface-system-theme">
+		<button
+			v-for="theme of items"
+			:key="theme"
+			:class="{ active: theme === valueWithDefault }"
+			class="theme"
+			@click="$emit('input', theme)"
+		>
+			<ThemePreview :dark-mode="appearance === 'dark'" :theme="theme" />
+
+			<div class="label">
+				<v-icon :name="theme === valueWithDefault ? 'radio_button_checked' : 'radio_button_unchecked'" />
+				{{ theme }}
+			</div>
+		</button>
+	</div>
 </template>
+
+<style scoped lang="scss">
+.interface-system-theme {
+	display: grid;
+	grid-template-columns: repeat(auto-fit, 240px);
+	gap: 20px;
+}
+
+.theme {
+	background-color: var(--theme--form--field--input--background);
+	padding: 10px;
+	border-width: var(--theme--border-width);
+	border-style: solid;
+	border-color: var(--theme--form--field--input--border-color);
+	border-radius: var(--theme--border-radius);
+	color: var(--theme--form--field--input--foreground);
+	box-shadow: var(--theme--form--field--input--box-shadow);
+	transition-duration: var(--fast);
+	transition-property: background-color, border-color, box-shadow;
+	transition-timing-function: var(--ease-out);
+	text-align: left;
+
+	--v-icon-color: var(--theme--primary);
+
+	.label {
+		display: flex;
+		align-items: center;
+
+		.v-icon {
+			margin-right: 4px;
+		}
+	}
+
+	&:hover {
+		background-color: var(--theme--form--field--input--background);
+		border-color: var(--v-input-border-color-hover, var(--theme--form--field--input--border-color-hover));
+		box-shadow: var(--theme--form--field--input--box-shadow-hover);
+	}
+
+	&.active {
+		background-color: var(--theme--form--field--input--background);
+		border-color: var(--theme--form--field--input--border-color-focus);
+		box-shadow: var(--theme--form--field--input--box-shadow-focus);
+	}
+}
+</style>
---
+++ app/src/interfaces/_system/system-theme/theme-preview.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { useThemeStore } from '@directus/themes';
+import { computed } from 'vue';
+
+const props = defineProps<{
+	darkMode: boolean;
+	theme: string;
+}>();
+
+const themeStore = useThemeStore();
+
+const theme = computed(() => {
+	const appearance = props.darkMode ? 'dark' : 'light';
+
+	const theme = themeStore.themes[appearance].find((theme) => theme.name === props.theme);
+
+	return theme ?? null;
+});
+
+const rules = computed(() => {
+	return theme.value?.rules ?? null;
+});
+</script>
+
+<template>
+	<svg class="theme-preview" width="210" height="142" viewBox="0 0 210 142" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect x="1" y="1" width="208" height="140" rx="4" fill="white" />
+		<rect x="57" y="1" width="136" height="16" fill="white" />
+		<path d="M17 1H57V141H17V1Z" fill="#F0F4F9" />
+		<path d="M17 1H57V17H17V1Z" fill="#E4EAF1" />
+		<path
+			d="M1.5 5C1.5 3.067 3.067 1.5 5 1.5H16.5V140.5H5C3.067 140.5 1.5 138.933 1.5 137V5Z"
+			fill="#18222F"
+			stroke="#18222F"
+		/>
+		<rect x="21" y="21" width="24" height="8" rx="4" fill="#E4EAF1" />
+		<rect x="63.5" y="45.5" width="59" height="11" rx="3.5" fill="white" stroke="#6644FF" />
+		<rect x="127.5" y="45.5" width="59" height="11" rx="3.5" fill="white" stroke="#F0F4F9" />
+		<rect x="63" y="33" width="24" height="8" rx="4" fill="#172940" fill-opacity="0.2" />
+		<rect x="63.5" y="81.5" width="123" height="35" rx="3.5" fill="white" stroke="#F0F4F9" />
+		<rect x="127" y="33" width="24" height="8" rx="4" fill="#172940" fill-opacity="0.2" />
+		<rect x="63" y="69" width="32" height="8" rx="4" fill="#172940" fill-opacity="0.2" />
+		<rect x="77" y="5" width="32" height="8" rx="4" fill="#172940" fill-opacity="0.2" />
+		<rect x="21" y="5" width="32" height="8" rx="4" fill="#172940" fill-opacity="0.2" />
+		<rect x="21" y="33" width="32" height="8" rx="4" fill="#E4EAF1" />
+		<rect x="21" y="69" width="32" height="8" rx="4" fill="#E4EAF1" />
+		<rect x="21" y="45" width="24" height="8" rx="4" fill="#E4EAF1" />
+		<rect x="21" y="57" width="24" height="8" rx="4" fill="#E4EAF1" />
+		<rect x="21" y="81" width="24" height="8" rx="4" fill="#E4EAF1" />
+		<path d="M1 5C1 2.79086 2.79086 1 5 1H17V17H1V5Z" fill="#6644FF" />
+		<path
+			d="M6.3442 14.0021L5.74 13.7204L8.31642 9.77268L6.2017 8.78657C5.67 8.53864 6.21363 8.24044 6.23416 8.22795C7.65597 7.21381 9.78606 5.7061 12.6301 3.69273L13.2343 3.97448L10.6579 7.92217L12.7786 8.9111C13.0203 9.02379 13.0997 9.20058 12.8343 9.42257C8.50651 12.4726 6.3442 14.0021 6.3442 14.0021Z"
+			fill="white"
+		/>
+		<rect x="1" y="17" width="16" height="16" fill="#F0F4F9" />
+		<rect x="5" y="21" width="8" height="8" rx="4" fill="#18222F" />
+		<rect x="5" y="37" width="8" height="8" rx="4" fill="#8196B1" />
+		<rect x="5" y="53" width="8" height="8" rx="4" fill="#8196B1" />
+		<rect x="5" y="69" width="8" height="8" rx="4" fill="#8196B1" />
+		<rect x="63" y="5" width="8" height="8" rx="4" fill="#F0F4F9" />
+		<rect x="179" y="5" width="8" height="8" rx="4" fill="#6644FF" />
+		<rect x="165" y="5" width="8" height="8" rx="4" fill="#F0F4F9" />
+		<path d="M193 1H205C207.209 1 209 2.79086 209 5V137C209 139.209 207.209 141 205 141H193V1Z" fill="#F0F4F9" />
+		<path d="M193 1H205C207.209 1 209 2.79086 209 5V17H193V1Z" fill="#E4EAF1" />
+		<path d="M193 17H209V33H193V17Z" fill="#E4EAF1" />
+		<path d="M193 33H209V49H193V33Z" fill="#E4EAF1" />
+		<rect x="197" y="5" width="8" height="8" rx="4" fill="#172940" fill-opacity="0.2" />
+		<rect x="197" y="21" width="8" height="8" rx="4" fill="#172940" fill-opacity="0.2" />
+		<rect x="197" y="37" width="8" height="8" rx="4" fill="#172940" fill-opacity="0.2" />
+		<rect x="0.5" y="0.5" width="209" height="141" rx="4.5" stroke="#F0F4F9" />
+		<rect opacity="0.5" x="57" y="17" width="136" height="1" fill="#F0F4F9" />
+		<rect opacity="0.15" x="57" y="18" width="136" height="1" fill="#F0F4F9" />
+	</svg>
+</template>
