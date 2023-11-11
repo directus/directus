9de0e1cd52ed927d0af495438ad246ce360f3b18
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-11-11 01:23:10 +0100

    Use isObject helper

--- packages/utils/shared/parse-filter.ts
+++ packages/utils/shared/parse-filter.ts
@@ -5,6 +5,7 @@ import { adjustDate } from './adjust-date.js';
 import { deepMap } from './deep-map.js';
 import { get } from './get-with-arrays.js';
 import { isDynamicVariable } from './is-dynamic-variable.js';
+import { isObject } from './is-object.js';
 import { parseJSON } from './parse-json.js';
 import { toArray } from './to-array.js';
 
@@ -103,9 +104,9 @@ function parseFilterEntry(
 		return { [key]: value.map((filter: Filter) => parseFilterRecursive(filter, accountability, context)) };
 	} else if (['_in', '_nin', '_between', '_nbetween'].includes(String(key))) {
 		// When array indices are above 20 (default value),
-		// the query parser parses them as an key-value pair object instead of an array,
+		// the query parser (qs) parses them as a key-value pair object instead of an array,
 		// so we will need to convert them back to an array
-		const val = typeof value === 'object' && !Array.isArray(value) ? Object.values(value) : value;
+		const val = isObject(value) ? Object.values(value) : value;
 		return { [key]: toArray(val).flatMap((value) => parseFilterValue(value, accountability, context)) } as Filter;
 	} else if (['_intersects', '_nintersects', '_intersects_bbox', '_nintersects_bbox'].includes(String(key))) {
 		// Geometry filters always expect to operate against a GeoJSON object. Parse the
