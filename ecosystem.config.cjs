6a56fbe1305583993f0619e35a9a664616a2fc9a
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2023-11-13 15:08:16 +0100

    typecast pm2 env vars

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,17 +12,17 @@ module.exports = [
 		args: ['start'],
 
 		// General
-		instances: process.env.PM2_INSTANCES ?? '1',
+		instances: parseInt(process.env.PM2_INSTANCES) || 1,
 		exec_mode: process.env.PM2_EXEC_MODE ?? 'cluster',
 		max_memory_restart: process.env.PM2_MAX_MEMORY_RESTART,
 
 		// Control flow
-		min_uptime: process.env.PM2_MIN_UPTIME,
-		listen_timeout: process.env.PM2_LISTEN_TIMEOUT,
-		kill_timeout: process.env.PM2_KILL_TIMEOUT,
+		min_uptime: parseInt(process.env.PM2_MIN_UPTIME) || undefined,
+		listen_timeout: parseInt(process.env.PM2_LISTEN_TIMEOUT) || undefined,
+		kill_timeout: parseInt(process.env.PM2_KILL_TIMEOUT) || undefined,
 		wait_ready: true,
-		max_restarts: process.env.PM2_MAX_RESTARTS,
-		restart_delay: process.env.PM2_RESTART_DELAY ?? 0,
-		autorestart: process.env.PM2_AUTO_RESTART ?? false,
+		max_restarts: parseInt(process.env.PM2_MAX_RESTARTS) || undefined,
+		restart_delay: parseInt(process.env.PM2_RESTART_DELAY) || 0,
+		autorestart: ['1', 'true'].includes(process.env.PM2_AUTO_RESTART?.toLowerCase()),
 	},
 ];
