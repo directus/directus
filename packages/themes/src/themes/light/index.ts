ba50f78096b6ccc9dbf94b46315608a07046f73b
Rijk van Zanten <rijkvanzanten@me.com>
2023-11-13 17:47:22 -0500

    Add color match, start on minimal

--- packages/themes/src/themes/light/index.ts
+++ packages/themes/src/themes/light/index.ts
@@ -1,2 +1,3 @@
 export { theme as directusDefault } from './directus-default.js';
-export { theme as directusOrganic } from './directus-organic.js';
+export { theme as directusColorMatch } from './directus-color-match.js';
+export { theme as directusMinimal } from './directus-minimal.js';
