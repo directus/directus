d332777597e95c0c8cb693ab432cd9ef7d769272
Kevin Lewis <kvn@lws.io>
2023-11-15 12:30:59 +0000

    Add newsletter to bottom on mobile

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -34,6 +34,7 @@ const path = computed(() => route.path);
 		</template>
 		<template #doc-footer-before>
 			<Feedback :url="path" :title="title" />
+			<Newsletter class="newsletter mobile" />
 			<Meta v-if="contributors" id="contributors" title-left="Contributors">
 				<template #left>
 					<div class="contributors">{{ contributors }}</div>
@@ -48,11 +49,22 @@ const path = computed(() => route.path);
 	margin-top: 2em;	
 }
 
+.newsletter.mobile {
+	display: none;
+}
+
 #contributors {
 	margin-bottom: 2em;
 }
 
 .contributors {
 	font-weight: 700;
+}
+
+@media screen and (max-width: 1280px) {
+		.newsletter.mobile {
+		display: block;
+		margin-bottom: 2em;
+	}
 }
 </style>
