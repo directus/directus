7bd24d603eeddffc226e447ac76c1dc2c880e814
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2023-11-18 01:22:01 +0800

    fix default values for system-theme-overrides

--- app/src/interfaces/_system/system-theme-overrides/system-theme-overrides.vue
+++ app/src/interfaces/_system/system-theme-overrides/system-theme-overrides.vue
@@ -2,6 +2,7 @@
 import { useThemeConfiguration } from '@/composables/use-theme-configuration';
 import { Theme, useTheme } from '@directus/themes';
 import { clone, get, isEmpty, setWith, unset } from 'lodash';
+import { computed } from 'vue';
 import SystemThemeOverridesGroup from './system-theme-overrides-group.vue';
 import type { SetValueFn } from './types.js';
 
@@ -11,6 +12,7 @@ defineOptions({
 
 const props = defineProps<{
 	value: Record<string, unknown> | null;
+	appearance: 'dark' | 'light';
 }>();
 
 const emit = defineEmits<{
@@ -37,7 +39,9 @@ const set: SetValueFn = (path: string[], value: string) => {
 	}
 };
 
-const { darkMode, themeDark, themeLight } = useThemeConfiguration();
+const { themeDark, themeLight } = useThemeConfiguration();
+
+const darkMode = computed(() => props.appearance === 'dark');
 
 const { theme } = useTheme(darkMode, themeLight, themeDark, {}, {});
 </script>
