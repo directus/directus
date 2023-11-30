247b423e758215239ff3764cabf76ff2a66ad9c6
Rijk van Zanten <rijkvanzanten@me.com>
2023-11-29 19:06:10 -0500

    Add redis bus to createBus

--- packages/memory/src/bus/lib/create.ts
+++ packages/memory/src/bus/lib/create.ts
@@ -1,19 +1,15 @@
 import type { BusConfig } from '../types/config.js';
 import { BusLocal } from './local.js';
-// import { BusRedis } from "./redis.js"
+import { BusRedis } from './redis.js';
 
 export const createBus = (config: BusConfig) => {
 	if (config.type === 'local') {
 		return new BusLocal(config);
 	}
 
-	// if (config.type === 'redis') {
-	// 	return new BusRedis(config);
-	// }
-
-	// if (config.type === 'multi') {
-	// 	return new BusMulti(config);
-	// }
+	if (config.type === 'redis') {
+		return new BusRedis(config);
+	}
 
 	throw new Error(`Invalid configuration: Type does not exist.`);
 };
