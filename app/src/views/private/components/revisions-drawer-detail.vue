6b1f22fc23d49dcae9261b8e51df92111004c873
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-11-30 17:59:50 +0100

    Make version in revisions-drawer-detail optional

--- app/src/views/private/components/revisions-drawer-detail.vue
+++ app/src/views/private/components/revisions-drawer-detail.vue
@@ -10,7 +10,7 @@ import RevisionsDrawer from './revisions-drawer.vue';
 const props = defineProps<{
 	collection: string;
 	primaryKey: string | number;
-	version: ContentVersion | null;
+	version?: ContentVersion | null;
 }>();
 
 defineEmits(['revert']);
