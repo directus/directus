064b01a100daa1681eb1f652ca5033453758c492
Jan Arends <jan.arends@mailbox.org>
2023-12-04 18:41:30 +0100

    deleted test
    Co-authored-by: Nicola Krumschmidt <nickrum@users.noreply.github.com>

--- packages/data-sql/src/query-converter/fields/create-nested-one-from-any.ts
+++
@@ -1,7 +0,0 @@
-import type { AbstractQueryFieldNodeNestedUnionOne } from '@directus/data';
-import type { AbstractSqlNestedOneFromAny } from '../../types/abstract-sql.js';
-
-export function getNestedOneFromAny(field: AbstractQueryFieldNodeNestedUnionOne): AbstractSqlNestedOneFromAny {
-	// create sub query (function)
-	// use create-nested-many.ts functionss
-}
