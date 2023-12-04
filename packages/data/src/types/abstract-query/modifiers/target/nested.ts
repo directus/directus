2fa00ab7e06e726fde78d0dfcc89bb691ae61371
Jan Arends <jan.arends@mailbox.org>
2023-12-04 12:22:55 +0100

    added relational any option to nested one

--- packages/data/src/types/abstract-query/modifiers/target/nested.ts
+++ packages/data/src/types/abstract-query/modifiers/target/nested.ts
@@ -1,11 +1,18 @@
-import type { AbstractQueryFieldNodeNestedRelationalMany } from '../../common/nested/relational.js';
+import type {
+	AbstractQueryFieldNodeNestedRelationalAny,
+	AbstractQueryFieldNodeNestedRelationalMany,
+} from '../../common/nested/relational.js';
 import type { AbstractQueryTarget } from '../target.js';
 
+/**
+ * Node to query data from a m2o or a2o relation.
+ */
 export interface AbstractQueryTargetNestedOne {
 	type: 'nested-one-target';
 
 	/* From the related collection the user can pick primitives, apply a function or add another nested node */
 	field: AbstractQueryTarget;
 
-	meta: AbstractQueryFieldNodeNestedRelationalMany; // AbstractQueryFieldNodeNestedObjectOne | AbstractQueryFieldNodeNestedJsonOne
+	/* A nested one can either be from a m2o or an a2o relationship */
+	meta: AbstractQueryFieldNodeNestedRelationalMany | AbstractQueryFieldNodeNestedRelationalAny; // AbstractQueryFieldNodeNestedObjectOne | AbstractQueryFieldNodeNestedJsonOne
 }
