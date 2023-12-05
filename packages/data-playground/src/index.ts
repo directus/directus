2bf3341aa8c1671bd5f050a8b6f86dc75de0d17d
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2023-12-05 19:25:43 +0100

    Add double nested o2m example to playground

--- packages/data-playground/src/index.ts
+++ packages/data-playground/src/index.ts
@@ -100,36 +100,186 @@ const endWithConditionQuery: AbstractQuery = {
 
 const o2mQuery: AbstractQuery = {
 	store: 'postgres',
-	collection: 'authors',
+	collection: 'film',
 	fields: [
 		{
 			type: 'primitive',
-			field: 'name',
-			alias: 'fistName',
+			field: 'title',
+			alias: 'title',
+		},
+		{
+			type: 'primitive',
+			field: 'film_id',
+			alias: 'film_id',
 		},
 		{
 			type: 'nested-single-many',
 			fields: [
 				{
 					type: 'primitive',
-					field: 'title',
-					alias: 'as',
+					field: 'actor_id',
+					alias: 'actor_id',
+				},
+				{
+					type: 'primitive',
+					field: 'film_id',
+					alias: 'film_id',
+				},
+				{
+					type: 'primitive',
+					field: 'last_update',
+					alias: 'last_update',
+				},
+				{
+					type: 'nested-single-one',
+					fields: [
+						{
+							type: 'primitive',
+							field: 'first_name',
+							alias: 'first_name',
+						},
+						{
+							type: 'primitive',
+							field: 'last_name',
+							alias: 'last_name',
+						},
+					],
+					alias: 'actor',
+					nesting: {
+						type: 'relational-many',
+						local: { fields: ['actor_id'] },
+						foreign: {
+							store: 'postgres',
+							collection: 'actor',
+							fields: ['actor_id'],
+						},
+					},
 				},
 			],
-			alias: 'sd',
+			alias: 'film_actor',
 			nesting: {
 				type: 'relational-many',
-				local: { fields: ['id'] },
+				local: { fields: ['film_id'] },
 				foreign: {
 					store: 'postgres',
-					collection: 'articles',
-					fields: ['author'],
+					collection: 'film_actor',
+					fields: ['film_id'],
 				},
 			},
 			modifiers: {},
 		},
 	],
-	modifiers: {},
+	modifiers: {
+		limit: {
+			type: 'limit',
+			value: 4,
+		},
+	},
+};
+
+const o2mTimesTwoQuery: AbstractQuery = {
+	store: 'postgres',
+	collection: 'store',
+	fields: [
+		{
+			type: 'primitive',
+			field: 'store_id',
+			alias: 'store_id',
+		},
+		{
+			type: 'primitive',
+			field: 'last_update',
+			alias: 'last_update',
+		},
+		{
+			type: 'nested-single-many',
+			fields: [
+				{
+					type: 'primitive',
+					field: 'customer_id',
+					alias: 'customer_id',
+				},
+				{
+					type: 'primitive',
+					field: 'store_id',
+					alias: 'store_id',
+				},
+				{
+					type: 'primitive',
+					field: 'first_name',
+					alias: 'first_name',
+				},
+				{
+					type: 'primitive',
+					field: 'last_name',
+					alias: 'last_name',
+				},
+				{
+					type: 'nested-single-many',
+					fields: [
+						{
+							type: 'primitive',
+							field: 'payment_id',
+							alias: 'payment_id',
+						},
+						{
+							type: 'primitive',
+							field: 'customer_id',
+							alias: 'customer_id',
+						},
+						{
+							type: 'primitive',
+							field: 'amount',
+							alias: 'amount',
+						},
+						{
+							type: 'primitive',
+							field: 'payment_date',
+							alias: 'payment_date',
+						},
+					],
+					alias: 'payment',
+					nesting: {
+						type: 'relational-many',
+						local: { fields: ['customer_id'] },
+						foreign: {
+							store: 'postgres',
+							collection: 'payment',
+							fields: ['customer_id'],
+						},
+					},
+					modifiers: {
+						limit: {
+							type: 'limit',
+							value: 2,
+						},
+					},
+				},
+			],
+			alias: 'customer',
+			nesting: {
+				type: 'relational-many',
+				local: { fields: ['store_id'] },
+				foreign: {
+					store: 'postgres',
+					collection: 'customer',
+					fields: ['store_id'],
+				},
+			},
+			modifiers: {
+				limit: {
+					type: 'limit',
+					value: 2,
+				},
+			},
+		},
+	],
+	modifiers: {
+		limit: {
+			type: 'limit',
+			value: 2,
+		},
+	},
 };
 
 const engine = new DataEngine();
@@ -138,22 +288,20 @@ const engine = new DataEngine();
 	await engine.registerStore(
 		'postgres',
 		new DataDriverPostgres({
-			connectionString: 'postgresql://postgres:moinmoin@localhost:5432/increment_test',
+			connectionString: 'postgresql://postgres:password@localhost:5432/postgres',
 		}),
 	);
 
-	const dataStream = await engine.query(o2mQuery);
+	const dataStream = await engine.query(o2mTimesTwoQuery);
 	const chunks = [];
-	console.log(1);
+	console.log('START');
 
 	for await (const record of dataStream) {
-		console.log(record);
+		console.dir(record, { depth: null });
 		chunks.push(record);
 	}
 
-	console.log('hallo');
-	console.log(chunks.length);
-	console.log(JSON.stringify(chunks, null, 2));
+	console.log(`Length: ${chunks.length}`);
 
 	await engine.destroy();
 })();
