a7fb0f04418e6cbca98b01939c923c1373f36afb
Pascal Jufer <pascal-jufer@bluewin.ch>
2023-12-06 02:51:54 +0100

    Storybook is Histoire

---
+++ app/src/components/v-text-overflow.story.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import VTextOverflow from './v-text-overflow.vue';
+
+function initState() {
+	return {
+		text: 'This text should not wrap to a new line!',
+		placement: 'bottom',
+	};
+}
+</script>
+
+<template>
+	<Story title="VTextOverflow" :init-state="initState">
+		<template #default="{ state: { _hPropState, _hPropDefs, $data: _, ...state } }">
+			<v-text-overflow v-bind="state" />
+		</template>
+	</Story>
+</template>
