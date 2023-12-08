2880f5d008fe571a123c9c6c245bf50f1c5b1988
Azri Kahar <42867097+azrikahar@users.noreply.github.com>
2023-12-08 22:12:59 +0800

    add migration file

---
+++ api/src/database/migrations/20231208A-add-indexes.ts
@@ -0,0 +1,30 @@
+import type { Knex } from 'knex';
+import { getHelpers } from '../helpers/index.js';
+import logger from '../../logger.js';
+
+export async function up(knex: Knex): Promise<void> {
+	const helper = getHelpers(knex).schema;
+	const isRedshift = helper.isOneOfClients(['redshift']);
+
+	if (isRedshift) {
+		logger.warn('Skipping migration on Redshift as it does not allow creating an index');
+		return;
+	}
+
+	await knex.schema.alterTable('directus_revisions', (table) => {
+		table.index(['item']);
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	const helper = getHelpers(knex).schema;
+	const isRedshift = helper.isOneOfClients(['redshift']);
+
+	if (isRedshift) {
+		return;
+	}
+
+	await knex.schema.alterTable('directus_revisions', (table) => {
+		table.dropIndex(['item']);
+	});
+}
