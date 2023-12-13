b0f5baa394c03c05384918fa50be177e9fdc4afc
Rijk van Zanten <rijkvanzanten@me.com>
2023-12-13 11:29:22 -0500

    Add `@directus/memory` package (#20514)
    * Add analytics table
    
    * Setup memory package boilerplate
    
    * Bootstrap library
    
    * [WIP] setup serialize/deserialize for data
    
    * Finish compress/serialize utils
    
    * Add get/set for local
    
    * Add increments method
    
    * Add delete + comments
    
    * Add pub/sub methods
    
    * Remove erroneous files
    
    * Add has, setMax
    
    * Restructure to 3 subclasses
    
    * WIP add Redis cache
    
    * Add set-max
    
    * Add cacheMulti
    
    * Add tests for redis cache
    
    * Don't compress small values
    
    * Only decompress values that are compressed
    
    * Add test for arr-to-buf
    
    * Add tests for multi-stage cache
    
    * Add test for create
    
    * Finish redis based messenger bus
    
    * Add redis bus to createBus
    
    * Resolve lock file conflict
    
    * Align dev deps versions
    
    * Format files
    
    * Update packages/memory/package.json
    
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>
    
    * It's okay, ESLint
    
    * Add local rate limiter
    
    * Add redis limiter
    
    * Add simple readme
    
    * Add changeset
    
    * 'key value' -> 'key-value'
    
    * Sort package.json
    
    * Remove leftovers
    
    * Move timer calls to corresponding test
    
    * Add KV asbraction based on previous "cache"
    
    * Update cache to use kv
    
    * Add more specific console logs
    
    * Only define setMax if not exists
    
    * Run formatter
    
    * Use parseJSON util
    
    * Fix typo in class docs
    
    * Install directus/utils
    
    * Default to Map if LRU config isn't set
    
    * Ignore errors thrown in handlers
    
    * Create process-id util
    
    * Use pubsub in multi-stage handler
    
    * WIP Update tests
    
    * Update package lock
    
    * Fix formatting errors
    
    * Fix timestamp in test
    
    * Uno mas
    
    * Fix mock order
    
    ---------
    
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>

---
+++ packages/memory/src/bus/lib/create.test.ts
@@ -0,0 +1,30 @@
+import { expect, test, vi } from 'vitest';
+import type { BusConfig } from '../index.js';
+import { createBus } from './create.js';
+import { BusLocal } from './local.js';
+import { BusRedis } from './redis.js';
+
+vi.mock('./local.js');
+vi.mock('./redis.js');
+
+test('Creates local cache for type local', () => {
+	const config = { type: 'local' } as unknown as BusConfig;
+
+	const cache = createBus(config);
+	expect(BusLocal).toHaveBeenCalledWith(config);
+	expect(cache).toBeInstanceOf(BusLocal);
+});
+
+test('Creates redis cache for type redis', () => {
+	const config = { type: 'redis' } as unknown as BusConfig;
+
+	const cache = createBus(config);
+	expect(BusRedis).toHaveBeenCalledWith(config);
+	expect(cache).toBeInstanceOf(BusRedis);
+});
+
+test('Throws an error for unknown types', () => {
+	const config = { type: 'WRONG' } as unknown as BusConfig;
+
+	expect(() => createBus(config)).toThrowError();
+});
