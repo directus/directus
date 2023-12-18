e5eb65438e3bbd2b54cbff71d2412747897df46a
wasimTQ <wasimthoufiq21@gmail.com>
2023-12-18 18:29:42 +0530

    handle bigInteger field min and max values in the interface instead of the global v-form

--- app/src/components/v-form/v-form.vue
+++ app/src/components/v-form/v-form.vue
@@ -15,7 +15,6 @@ import type { FormField as TFormField } from './types';
 import { getFormFields } from './utils/get-form-fields';
 import { updateFieldWidths } from './utils/update-field-widths';
 import ValidationErrors from './validation-errors.vue';
-import { useFieldDetailStore } from '@/modules/settings/routes/data-model/field-detail/store';
 
 type FieldValues = {
 	[field: string]: any;
@@ -162,21 +161,8 @@ function useForm() {
 		return fields;
 	});
 
-	const fieldDetailStore = useFieldDetailStore();
-
 	const fieldsMap = computed<Record<string, TFormField | undefined>>(() => {
-		const fieldsObject = Object.fromEntries(fieldsWithConditions.value.map((field) => [field.field, field]));
-
-		// Set the fields of Maximum and Minimum values on the interface of a table field
-		// to accept bigInteger values if the field itself is a bigInteger
-		if (fieldsObject['min'] && fieldsObject['max']) {
-			if (fieldDetailStore.field.type && fieldDetailStore.field.type == 'bigInteger') {
-				fieldsObject['min'].type = 'bigInteger';
-				fieldsObject['max'].type = 'bigInteger';
-			}
-		}
-
-		return fieldsObject;
+		return Object.fromEntries(fieldsWithConditions.value.map((field) => [field.field, field]));
 	});
 
 	const fieldNames = computed(() => {
