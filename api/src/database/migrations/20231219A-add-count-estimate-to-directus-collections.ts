f7156204841f7d85872c187895858645942c4e3c
wasimTQ <wasimthoufiq21@gmail.com>
2023-12-19 22:48:30 +0530

    add migration to add count_estimate on collections table

---
+++ api/src/database/migrations/20231219A-add-count-estimate-to-directus-collections.ts
@@ -0,0 +1,13 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_collections', (table) => {
+		table.integer('count_estimate').defaultTo(0);
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_collections', (table) => {
+		table.dropColumn('count_estimate');
+	});
+}
