e46c928588a8087e9d8636570c638ab8077ca5da
Rijk van Zanten <rijkvanzanten@me.com>
2023-12-20 15:33:24 -0500

    Add tests for init

--- api/src/telemetry/lib/init-telemetry.ts
+++ api/src/telemetry/lib/init-telemetry.ts
@@ -3,24 +3,30 @@ import { useEnv } from '../../env.js';
 import { scheduleSynchronizedJob } from '../../utils/schedule.js';
 import { track } from './track.js';
 
+export const jobCallback = () => track();
+
 /**
  * Initialize the telemetry tracking. Will generate a report on start, and set a schedule to report
  * every 6 hours
+ *
+ * @returns Whether or not telemetry has been initialized
  */
 export const initTelemetry = async () => {
 	const env = useEnv();
-	const { lockCache } = getCache();
 
-	if (env['TELEMETRY'] === false) return;
+	if (env['TELEMETRY'] === false) return false;
+
+	scheduleSynchronizedJob('telemetry', '0 */6 * * *', jobCallback);
+
+	const { lockCache } = getCache();
 
 	if (!(await lockCache.get('telemetry-lock'))) {
 		await lockCache.set('telemetry-lock', true, 30000);
 
 		track({ wait: false });
 
-		// Don't flush the lock. We want to debounce these calls in the 30s window we're setting
-		// when the first container starts the track on startup
+		// Don't flush the lock. We want to debounce these calls across containers on startup
 	}
 
-	scheduleSynchronizedJob('telemetry', '0 */6 * * *', () => track());
+	return true;
 };
--- api/src/telemetry/utils/get-random-wait-time.ts
+++ api/src/telemetry/utils/get-random-wait-time.ts
@@ -1,5 +1,5 @@
 /**
- * Returns randomized value between 0 and 1.8e+6 intended to be used as the randomized wait for
+ * Returns randomized value between 0 and 1.8e+6 (30min in ms) intended to be used as the randomized wait for
  * telemetry tracking
  */
 export const getRandomWaitTime = () => Math.floor(Math.random() * 1.8e+6);
