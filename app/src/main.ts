884d932610b386e054d347ac70a2f0c56427de3f
Rijk van Zanten <rijkvanzanten@me.com>
2023-12-21 13:32:36 -0500

    Fix import order

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,9 +1,11 @@
 /* eslint-disable no-console */
 
+// Note: the import order is important. Vue has to come first. (Unhead will break if that's first)
+import { createApp } from 'vue';
+
 import { getVueComponentName } from '@/utils/get-vue-component-name';
 import { createHead } from '@unhead/vue';
 import { createPinia } from 'pinia';
-import { createApp } from 'vue';
 import App from './app.vue';
 import { registerComponents } from './components/register';
 import { DIRECTUS_LOGO } from './constants';
