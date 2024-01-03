ceb8e7dd6299cb31985e1bf15293ff8b54949ea7
Mahendra <22556323+mahendraHegde@users.noreply.github.com>
2024-01-03 15:14:13 -0300

    use bigint notations to declare last constant

--- api/src/services/graphql/types/bigint.ts
+++ api/src/services/graphql/types/bigint.ts
@@ -41,9 +41,8 @@ function parseNumberValue(input: string) {
 		// If the input is not a safe integer, its a big int, so return it as string,
 		// because currently string is the best way to handle big int due to knex limitations and JSON.stringify not able to serialise bigInt
 
-		//its needed to use hardcoded string instead of  2**63-1 directly as it will first convert to number hence loose the precise value
-		const minVal = BigInt('-9223372036854775808'); //-1*(2**63)
-		const maxVal = BigInt('9223372036854775807'); //(2**63-1)
+		const minVal = -1n * 2n ** 63n;
+		const maxVal = 2n ** 63n - 1n;
 
 		const bigIntInput = BigInt(input);
 
