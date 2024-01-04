a351dad4d893365b5280c34877f648ca517819df
Rijk van Zanten <rijkvanzanten@me.com>
2024-01-04 17:03:33 -0500

    Add tests for get-config-path

--- packages/env/src/utils/get-config-path.ts
+++ packages/env/src/utils/get-config-path.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'node:path';
-import { DEFAULTS } from '../constants/defaults';
+import { DEFAULTS } from '../constants/defaults.js';
 
 /**
  * Get the file location on the local filesystem of the config file
