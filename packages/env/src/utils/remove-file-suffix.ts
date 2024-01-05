409002c7ffff6264a22e5cac1a8acea198ad51a2
Rijk van Zanten <rijkvanzanten@me.com>
2024-01-05 15:47:27 -0500

    Add test for file suffix / try-json

--- packages/env/src/utils/remove-file-suffix.ts
+++ packages/env/src/utils/remove-file-suffix.ts
@@ -1,4 +1,4 @@
 /**
- * Convert a FILE property back to the config option key name
+ * Convert a _FILE property back to the config option key name
  */
 export const removeFileSuffix = (key: string) => key.slice(0, -5);
