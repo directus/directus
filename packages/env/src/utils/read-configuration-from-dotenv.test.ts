e48ed17cd6bba8062d64b3a8dfef8ecd8b1f20b0
Rijk van Zanten <rijkvanzanten@me.com>
2024-01-05 14:40:54 -0500

    Add test for read-config-from-dotenv

---
+++ packages/env/src/utils/read-configuration-from-dotenv.test.ts
@@ -0,0 +1,27 @@
+import { parse } from 'dotenv';
+import { readFileSync } from 'node:fs';
+import { afterEach, expect, test, vi } from 'vitest';
+import { readConfigurationFromDotEnv } from './read-configuration-from-dotenv.js';
+
+vi.mock('dotenv');
+vi.mock('node:fs');
+
+afterEach(() => {
+	vi.clearAllMocks();
+});
+
+test('Reads file contents of path synchronously', () => {
+	readConfigurationFromDotEnv('./test/path');
+	expect(readFileSync).toHaveBeenCalledWith('./test/path');
+});
+
+test('Parses file contents with dotenv', () => {
+	vi.mocked(readFileSync).mockReturnValue('dotenv-file-contents');
+	readConfigurationFromDotEnv('./test/path');
+	expect(parse).toHaveBeenCalledWith('dotenv-file-contents');
+});
+
+test('Returns parsed dotenv config', () => {
+	vi.mocked(parse).mockReturnValue({ hello: 'world' });
+	expect(readConfigurationFromDotEnv('./test/path')).toEqual({ hello: 'world' });
+});
