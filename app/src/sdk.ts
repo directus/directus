3277f9a39c810047f3263635bacef7f90b98e3ff
Brainslug <tim@brainslug.nl>
2024-01-09 19:35:57 +0100

    Removed fallback onRequest handler

--- app/src/sdk.ts
+++ app/src/sdk.ts
@@ -1,7 +1,6 @@
 import { authentication, createDirectus, rest } from '@directus/sdk';
 import type { AuthenticationClient, DirectusClient, RestClient } from '@directus/sdk';
 import { getPublicURL } from './utils/get-root-path';
-import api from './api';
 
 type SdkClient<Schema extends object = any> = DirectusClient<Schema> &
 	AuthenticationClient<Schema> &
@@ -9,23 +8,6 @@ type SdkClient<Schema extends object = any> = DirectusClient<Schema> &
 
 const sdk: SdkClient = createDirectus(getPublicURL())
 	.with(authentication('cookie', { credentials: 'include', msRefreshBeforeExpires: 10_000 }))
-	.with(
-		rest({
-			credentials: 'include',
-			onRequest: (req) => {
-				if (!Array.isArray(req.headers) && req.headers instanceof Headers === false) {
-					if (!req.headers) req.headers = {};
-					// share the token managed by axios for now
-					const authHeader = api.defaults.headers.common['Authorization'];
-
-					if (authHeader) {
-						req.headers['Authorization'] = authHeader.toString();
-					}
-				}
-
-				return req;
-			},
-		}),
-	);
+	.with(rest({ credentials: 'include' }));
 
 export default sdk;
