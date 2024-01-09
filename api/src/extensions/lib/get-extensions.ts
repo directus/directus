5fc1c538a48f936a1c9a4b85021a1b96ad2c2999
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-01-09 02:50:18 +0100

    `extensions` -> `localExtensions`

--- api/src/extensions/lib/get-extensions.ts
+++ api/src/extensions/lib/get-extensions.ts
@@ -6,14 +6,15 @@ import { getExtensionsPath } from './get-extensions-path.js';
 export const getExtensions = async () => {
 	const env = useEnv();
 	const logger = useLogger();
-	const extensions = await resolveExtensions(getExtensionsPath());
-	const extensionNames = extensions.map(({ name }) => name);
 
-	/** Extensions that are listed in the root package.json dependencies */
+	const localExtensions = await resolveExtensions(getExtensionsPath());
+	const localExtensionNames = localExtensions.map(({ name }) => name);
+
+	/** Extensions that are listed as dependencies in the root package.json */
 	const dependencyExtensions = await resolveDependencyExtensions(env['PACKAGE_FILE_LOCATION'] as string);
 
 	const dedupedDependencyExtensions = dependencyExtensions.filter((ext) => {
-		if (extensionNames.includes(ext.name)) {
+		if (localExtensionNames.includes(ext.name)) {
 			logger.warn(`Node module extension "${ext.name}" already exists within the extensions folder.`);
 			return false;
 		}
@@ -21,5 +22,5 @@ export const getExtensions = async () => {
 		return true;
 	});
 
-	return [...extensions, ...dedupedDependencyExtensions];
+	return [...localExtensions, ...dedupedDependencyExtensions];
 };
