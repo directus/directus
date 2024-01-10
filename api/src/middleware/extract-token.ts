207ab933bd57e0ead642d346e744b084bf94bac1
Brainslug <tim@brainslug.nl>
2024-01-10 17:07:39 +0100

    use env vars

--- api/src/middleware/extract-token.ts
+++ api/src/middleware/extract-token.ts
@@ -9,21 +9,25 @@
 
 import type { RequestHandler } from 'express';
 import { InvalidPayloadError } from '@directus/errors';
+import { useEnv } from '@directus/env';
 
 const extractToken: RequestHandler = (req, _res, next) => {
+	const env = useEnv();
+	const cookie_name = env['ACCESS_TOKEN_COOKIE_NAME'] as string;
+
 	let token: string | null = null;
 
 	if (req.query && req.query['access_token']) {
 		token = req.query['access_token'] as string;
 	}
 
-	if (req.cookies && req.cookies['access_token']) {
+	if (req.cookies && req.cookies[cookie_name]) {
 		if (token !== null) {
 			// RFC6750 compliance:
 			throw new InvalidPayloadError({ reason: 'The request uses more than one method for including an access token, or is otherwise malformed' });
 		}
 
-		token = req.cookies['access_token'];
+		token = req.cookies[cookie_name];
 	}
 
 	if (req.headers && req.headers.authorization) {
