80f28a87203809ce55b5e252f43941e860e76896
Brainslug <tim@brainslug.nl>
2024-01-11 15:20:33 +0100

    added basic tests for is-url-allowed

---
+++ api/src/utils/is-url-allowed.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from 'vitest';
+import isUrlAllowed from './is-url-allowed.js';
+
+test('isUrlAllowed should block different paths', () => {
+	const checkUrl = 'http://example.com/test1'
+	const allowedUrls = ['http://example.com/test2', 'http://example.com/test3', 'http://example.com/']
+
+	expect(isUrlAllowed(checkUrl, allowedUrls)).toBe(false);
+});
+
+test('isUrlAllowed should block different domains', () => {
+	const checkUrl = 'http://directus.io/'
+	const allowedUrls = ['http://example.com/', 'http://directus.chat']
+
+	expect(isUrlAllowed(checkUrl, allowedUrls)).toBe(false);
+});
+
+test('isUrlAllowed accepts varying protocols', () => {
+	const checkUrl = 'http://example.com/'
+	const allowedUrls = ['ftp://example.com/', 'https://example.com/']
+
+	expect(isUrlAllowed(checkUrl, allowedUrls)).toBe(true);
+});
