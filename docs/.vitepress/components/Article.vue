9c27bf5409fd352a5df23d2f086affd4a04334ed
Kevin Lewis <kvn@lws.io>
2024-01-17 20:49:35 +0100

    Remove alt from Article component, always empty.

--- docs/.vitepress/components/Article.vue
+++ docs/.vitepress/components/Article.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 defineProps<{
-	title?: string;
+	title: string;
 	tag?: string;
 	url: string;
 	img: string;
 	author?: string;
 	date?: string;
 	desc?: string;
-	alt?: string;
 }>();
 </script>
 
@@ -15,7 +14,7 @@ defineProps<{
 	<div class="article">
 		<a :href="url">
 			<div class="image">
-				<img :src="img" :alt="alt || ''" loading="lazy" />
+				<img :src="img" alt="" loading="lazy" />
 			</div>
 			<div class="content">
 				<h3 class="heading">{{ title }}</h3>
