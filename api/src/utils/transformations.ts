062c8f23f602f682aebcb0c015e43155d3ff6715
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2024-01-22 18:35:06 +0100

    Add focal point support for images (#20768)
    * add visual feedback for invalid value
    
    * add focal point MVP
    
    * Revert "add visual feedback for invalid value"
    
    This reverts commit 1df186834273261ac1a6f0dea40be7e246e2601f.
    
    Accidently committed some local testing stuff. Pls disregard! :)
    
    * fix wrong cropping
    
    * fix text for new cropping, import correct type
    
    * fix saving
    
    * place initial focal point to saved value or center, display different cancel text
    
    * split up tooltips
    
    * honor rotations & flips when saving focal point
    
    * apply custom cropper styles for focal mode
    
    * Create loud-crews-fix.md
    
    * add test and only crop when covering with fixed dimensions to preserve default behaviour
    
    * linter gods pls forgive me
    
    * replace json field with two int fields
    
    * add focal point to sdk
    
    * fix transformation for the two new db columns
    
    * update test for new columns, add new tests
    
    * wip: saving now differentiates between only img data and focal point and only enable saving if there are changes
    
    but this is not optimal. would be better to check beforehand if we can collapse
    to requests to one. Now its bad because
    one request might succeed and the other fails.
    
    * refactor image editor change persistence
    
    now we save it in one request!
    
    * Update loud-crews-fix.md
    
    * add `focal_point_x` and `focal_point_y` to possible asset transformations
    
    * fix assigning localdragmode upon cropper init
    
    * reuse fetched fields in type
    
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>
    
    * update file type
    
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>
    
    * update changeset
    
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>
    
    * improve type for `ASSET_TRANSFORM_QUERY_KEYS`
    
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Trying out the batch change feature from github for the first time. Lets see.
    
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>
    
    * rename `persistChanges` to `saveImage`
    
    * Add docs for focal points (#20959)
    
    * Add user guide
    
    * Added to API Reference
    
    * Prettier
    
    * Spellchecker
    
    * default null
    
    Co-authored-by: Daniel Biegler <DanielBiegler@users.noreply.github.com>
    
    * from -> around
    
    Co-authored-by: Daniel Biegler <DanielBiegler@users.noreply.github.com>
    
    * from -> around
    
    ---------
    
    Co-authored-by: Daniel Biegler <DanielBiegler@users.noreply.github.com>
    
    * add changeset for docs
    
    * run prettier lets goooooooo
    
    * move & show focal point fields and add divider
    
    ---------
    
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>
    Co-authored-by: Kevin Lewis <kvn@lws.io>

--- api/src/utils/transformations.ts
+++ api/src/utils/transformations.ts
@@ -1,4 +1,6 @@
 import type { File } from '@directus/types';
+import { clamp } from 'lodash-es';
+import type { Region } from 'sharp';
 import type { Transformation, TransformationFormat, TransformationSet } from '../types/index.js';
 
 export function resolvePreset({ transformationParams, acceptFormat }: TransformationSet, file: File): Transformation[] {
@@ -14,18 +16,66 @@ export function resolvePreset({ transformationParams, acceptFormat }: Transforma
 		]);
 	}
 
-	if (transformationParams.width || transformationParams.height) {
-		transforms.push([
-			'resize',
-			{
-				width: transformationParams.width ? Number(transformationParams.width) : undefined,
-				height: transformationParams.height ? Number(transformationParams.height) : undefined,
-				fit: transformationParams.fit,
-				withoutEnlargement: transformationParams.withoutEnlargement
-					? Boolean(transformationParams.withoutEnlargement)
-					: undefined,
-			},
-		]);
+	if ((transformationParams.width || transformationParams.height) && file.width && file.height) {
+		const toWidth = transformationParams.width ? Number(transformationParams.width) : undefined;
+		const toHeight = transformationParams.height ? Number(transformationParams.height) : undefined;
+
+		const toFocalPointX = transformationParams.focal_point_x
+			? Number(transformationParams.focal_point_x)
+			: file.focal_point_x;
+
+		const toFocalPointY = transformationParams.focal_point_y
+			? Number(transformationParams.focal_point_y)
+			: file.focal_point_y;
+
+		/*
+		 * Focal point cropping only works with a fixed size (width x height) when `cover`ing,
+		 * since the other modes show the whole image. Sharp by default also simply scales up/down
+		 * when only supplied with one dimension, so we **must** check, else we break existing behaviour.
+		 * See: https://sharp.pixelplumbing.com/api-resize#resize
+		 * Also only crop to focal point when explicitly defined so that users can still `cover` with
+		 * other parameters like `position` and `gravity` - Else fall back to regular behaviour
+		 */
+		if (
+			(transformationParams.fit === undefined || transformationParams.fit === 'cover') &&
+			toWidth &&
+			toHeight &&
+			toFocalPointX !== null &&
+			toFocalPointY !== null
+		) {
+			const transformArgs = getResizeArguments(
+				{ w: file.width, h: file.height },
+				{ w: toWidth, h: toHeight },
+				{ x: toFocalPointX, y: toFocalPointY },
+			);
+
+			transforms.push(
+				[
+					'resize',
+					{
+						width: transformArgs.width,
+						height: transformArgs.height,
+						fit: transformationParams.fit,
+						withoutEnlargement: transformationParams.withoutEnlargement
+							? Boolean(transformationParams.withoutEnlargement)
+							: undefined,
+					},
+				],
+				['extract', transformArgs.region],
+			);
+		} else {
+			transforms.push([
+				'resize',
+				{
+					width: toWidth,
+					height: toHeight,
+					fit: transformationParams.fit,
+					withoutEnlargement: transformationParams.withoutEnlargement
+						? Boolean(transformationParams.withoutEnlargement)
+						: undefined,
+				},
+			]);
+		}
 	}
 
 	return transforms;
@@ -62,4 +112,73 @@ export function maybeExtractFormat(transforms: Transformation[]): string | undef
 	const toFormats = transforms.filter((t) => t[0] === 'toFormat');
 	const lastToFormat = toFormats[toFormats.length - 1];
 	return lastToFormat ? lastToFormat[1]?.toString() : undefined;
+}
+
+type Dimensions = { w: number; h: number };
+type FocalPoint = { x: number; y: number };
+
+/**
+ * Resize an image but keep it centered on the focal point.
+ * Based on the method outlined in https://github.com/lovell/sharp/issues/1198#issuecomment-384591756
+ */
+function getResizeArguments(
+	original: Dimensions,
+	target: Dimensions,
+	focalPoint?: FocalPoint | null,
+): { width: number; height: number; region: Region } {
+	const { width, height, factor } = getIntermediateDimensions(original, target);
+
+	const region = getExtractionRegion(factor, focalPoint ?? { x: original.w / 2, y: original.h / 2 }, target, {
+		w: width,
+		h: height,
+	});
+
+	return { width, height, region };
+}
+
+/**
+ * Calculates the dimensions of the intermediate (resized) image.
+ */
+function getIntermediateDimensions(
+	original: Dimensions,
+	target: Dimensions,
+): { width: number; height: number; factor: number } {
+	const hRatio = original.h / target.h;
+	const wRatio = original.w / target.w;
+
+	let factor: number;
+	let width: number;
+	let height: number;
+
+	if (hRatio < wRatio) {
+		factor = hRatio;
+		height = Math.round(target.h);
+		width = Math.round(original.w / factor);
+	} else {
+		factor = wRatio;
+		width = Math.round(target.w);
+		height = Math.round(original.h / factor);
+	}
+
+	return { width, height, factor };
+}
+
+/**
+ * Calculates the Region to extract from the intermediate image.
+ */
+function getExtractionRegion(
+	factor: number,
+	focalPoint: FocalPoint,
+	target: Dimensions,
+	intermediate: Dimensions,
+): Region {
+	const newXCenter = focalPoint.x / factor;
+	const newYCenter = focalPoint.y / factor;
+
+	return {
+		left: clamp(Math.round(newXCenter - target.w / 2), 0, intermediate.w - target.w),
+		top: clamp(Math.round(newYCenter - target.h / 2), 0, intermediate.h - target.h),
+		width: target.w,
+		height: target.h,
+	};
 }
