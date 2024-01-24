da73a1faccdc7d3f500a9f125434efa24ad10bbf
Jan Arends <jan.arends@mailbox.org>
2024-01-24 14:27:46 +0100

    test for conversion

---
+++ packages/data-sql/src/query-converter/fields/nodes/json-select.test.ts
@@ -0,0 +1,71 @@
+import { expect, test } from 'vitest';
+import { convertJson } from './json-select.js';
+import type { AbstractQueryFieldNodeNestedSingleOne } from '@directus/data';
+import { randomInteger } from '@directus/random';
+import type { AbstractSqlQuerySelectJsonNode } from '../../../index.js';
+
+test('json select', () => {
+	const tableIndex = randomInteger(0, 100);
+	const columnIndex = randomInteger(0, 100);
+
+	/**
+	 * Assuming the json value in the database looks like this:
+	 *
+	 * [
+	 *	{
+	 *		author: {
+	 *			name: {
+	 *				first: 'John',
+	 *				last: 'Doe',
+	 *			},
+	 *			age: 42,
+	 *		},
+	 *	},
+	 * ];
+	 */
+
+	const jsonField: AbstractQueryFieldNodeNestedSingleOne = {
+		type: 'nested-single-one',
+		fields: [
+			{
+				type: 'nested-single-one',
+				nesting: {
+					type: 'object-many',
+					fieldName: 'name',
+				},
+				fields: [
+					{
+						type: 'primitive',
+						field: 'first',
+						alias: 'derErste',
+					},
+				],
+				alias: 'derName',
+			},
+			{
+				type: 'primitive',
+				field: 'age',
+				alias: 'age',
+			},
+		],
+		alias: 'derAuthor',
+		nesting: {
+			type: 'object-many',
+			fieldName: 'author',
+		},
+	};
+
+	const result = convertJson(jsonField, tableIndex, columnIndex);
+
+	const expected: AbstractSqlQuerySelectJsonNode = {
+		type: 'json',
+		tableIndex,
+		columnIndex: 0,
+		path: [
+			['author', 'name', 'first'],
+			['author', 'age'],
+		],
+	};
+
+	expect(result).toBe(expected);
+});
