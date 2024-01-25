526727ab0f1e695d45628f79b3ec1d89919c2993
Gerard <gerardlamo@gmail.com>
2024-01-25 11:44:31 +0100

    Remove unused merge

--- api/src/utils/apply-diff.ts
+++ api/src/utils/apply-diff.ts
@@ -2,7 +2,7 @@ import type { Field, Relation, SchemaOverview } from '@directus/types';
 import type { Diff, DiffDeleted, DiffNew } from 'deep-diff';
 import deepDiff from 'deep-diff';
 import type { Knex } from 'knex';
-import { cloneDeep, merge, set } from 'lodash-es';
+import { cloneDeep, set } from 'lodash-es';
 import { flushCaches } from '../cache.js';
 import { getHelpers } from '../database/helpers/index.js';
 import getDatabase from '../database/index.js';
