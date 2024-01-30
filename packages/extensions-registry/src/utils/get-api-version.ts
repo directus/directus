14186a32c590182d2856d593122d48aa47987d78
Rijk van Zanten <rijkvanzanten@me.com>
2024-01-30 16:35:22 -0500

    Add version utility

--- packages/extensions-registry/src/utils/get-api-version.ts
+++ packages/extensions-registry/src/utils/get-api-version.ts
@@ -1,12 +1,28 @@
-import { DEFAULT_REGISTRY } from "../constants.js"
 import ky from 'ky';
+import { DEFAULT_REGISTRY } from '../constants.js';
+import { RegistryVersionResponse } from '../schemas/registry-version-response.js';
 
 export interface GetApiVersionOptions {
 	registry?: string;
 }
 
+export const _cache: Record<string, string> = {};
+
 export const getApiVersion = async (options?: GetApiVersionOptions) => {
 	const registry = options?.registry ?? DEFAULT_REGISTRY;
 
+	if (registry in _cache) {
+		return _cache[registry];
+	}
 
-}
+	const response = await ky.get(new URL('/version', registry)).json();
+
+	const { version } = await RegistryVersionResponse.parseAsync(response);
+
+	_cache[registry] = version;
+
+	// Invalidate the memoization at least every 6 hours
+	setTimeout(() => delete _cache[registry], 6 * 60 * 60 * 1000);
+
+	return _cache[registry];
+};
