668c24a419f416decc8a676786f48f5379e020b9
Rijk van Zanten <rijkvanzanten@me.com>
2024-01-31 14:27:54 -0500

    Finish tests for extensions-registry package

--- packages/extensions-registry/src/utils/get-api-version.ts
+++ packages/extensions-registry/src/utils/get-api-version.ts
@@ -6,23 +6,23 @@ export interface GetApiVersionOptions {
 	registry?: string;
 }
 
-export const _cache: Record<string, string> = {};
+export const _cache: Map<string, string> = new Map();
 
 export const getApiVersion = async (options?: GetApiVersionOptions) => {
 	const registry = options?.registry ?? DEFAULT_REGISTRY;
 
-	if (registry in _cache) {
-		return _cache[registry];
+	if (_cache.has(registry)) {
+		return _cache.get(registry)!;
 	}
 
 	const response = await ky.get(new URL('/version', registry)).json();
 
 	const { version } = await RegistryVersionResponse.parseAsync(response);
 
-	_cache[registry] = version;
+	_cache.set(registry, version);
 
 	// Invalidate the memoization at least every 6 hours
-	setTimeout(() => delete _cache[registry], 6 * 60 * 60 * 1000);
+	setTimeout(() => _cache.delete(registry), 6 * 60 * 60 * 1000);
 
-	return _cache[registry];
+	return _cache.get(registry)!;
 };
