7204aa2ec91f2d1218ee607171073fba238012e8
Rijk van Zanten <rijkvanzanten@me.com>
2024-02-04 11:39:37 -0500

    Download to tmp path instead of extensions

--- api/src/extensions/lib/installation/manager.ts
+++ api/src/extensions/lib/installation/manager.ts
@@ -1,13 +1,11 @@
 import { useEnv } from '@directus/env';
-import { ForbiddenError } from '@directus/errors';
+import { ForbiddenError, ServiceUnavailableError } from '@directus/errors';
 import { EXTENSION_PKG_KEY, ExtensionManifest } from '@directus/extensions';
 import { download, type DownloadOptions } from '@directus/extensions-registry';
 import DriverLocal from '@directus/storage-driver-local';
 import { move } from 'fs-extra';
-import { createWriteStream } from 'node:fs';
 import { mkdir, readFile, rm } from 'node:fs/promises';
 import { Readable } from 'node:stream';
-import { pipeline } from 'node:stream/promises';
 import Queue from 'p-queue';
 import { join } from 'path';
 import type { ReadableStream } from 'stream/web';
@@ -15,20 +13,19 @@ import tar from 'tar';
 import { useLogger } from '../../../logger.js';
 import { getStorage } from '../../../storage/index.js';
 import { getExtensionsPath } from '../get-extensions-path.js';
-import { ServiceUnavailableError } from '@directus/errors';
 
 const env = useEnv();
 
 export class InstallationManager {
 	extensionPath = getExtensionsPath();
-	driverLocal: DriverLocal = new DriverLocal({ root: this.extensionPath });
+
 	async install(versionId: string) {
 		const logger = useLogger();
 
-		const downloadPath = join('directus_install_temp', versionId);
+		const tempDir = join(env['TEMP_PATH'] as string, 'marketplace', versionId);
 
-		//temp dir inside extension folder to download and extract the tar
-		const tempDir = join(this.extensionPath, downloadPath);
+		const tmpStorage = new DriverLocal({ root: tempDir });
+
 		await mkdir(tempDir, { recursive: true });
 
 		const options: DownloadOptions = {};
@@ -44,11 +41,10 @@ export class InstallationManager {
 		}
 
 		const tarStream = Readable.fromWeb(tarReadableStream as ReadableStream);
+		const tarPath = join(tempDir, `bin.tar.tgz`);
 
 		try {
-			const tarPath = join(tempDir, `bin.tar.tgz`);
-			const writeStream = createWriteStream(tarPath, { encoding: 'binary' });
-			await pipeline(tarStream, writeStream);
+			await tmpStorage.write('bin.tar.tgz', tarStream);
 
 			await tar.extract({
 				file: tarPath,
@@ -56,7 +52,7 @@ export class InstallationManager {
 			});
 
 			const packageFile = JSON.parse(await readFile(join(tempDir, 'package', 'package.json'), { encoding: 'utf-8' }));
-			const extensionManifest = ExtensionManifest.parse(packageFile);
+			const extensionManifest = await ExtensionManifest.parseAsync(packageFile);
 
 			if (!extensionManifest[EXTENSION_PKG_KEY]?.type) {
 				throw new Error(`Extension type not found in package.json`);
@@ -66,13 +62,12 @@ export class InstallationManager {
 				// Upload the extension into the configured extensions location
 				const storage = await getStorage();
 				const remoteDisk = storage.location(env['EXTENSIONS_LOCATION'] as string);
-				const prefix = join(downloadPath, 'package');
 
 				const queue = new Queue({ concurrency: 1000 });
 
-				for await (const filepath of this.driverLocal.list(prefix)) {
-					const readStream = await this.driverLocal.read(filepath);
-					const remotePath = join(env['EXTENSIONS_PATH'] as string, versionId, filepath.split(prefix)[1]!);
+				for await (const filepath of tmpStorage.list()) {
+					const readStream = await tmpStorage.read(filepath);
+					const remotePath = join(env['EXTENSIONS_PATH'] as string, versionId, filepath);
 					queue.add(() => remoteDisk.write(remotePath, readStream));
 				}
 
@@ -90,7 +85,6 @@ export class InstallationManager {
 				{ cause: err },
 			);
 		} finally {
-			// Remove the temporary dir no matter what
 			await rm(tempDir, { recursive: true });
 		}
 	}
