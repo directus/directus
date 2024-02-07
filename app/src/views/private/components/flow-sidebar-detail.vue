4495d0e1c757633f93c8ac0efafb5a744eed845f
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2024-02-07 13:15:05 -0500

    do not hide manual flow dialog on item selection drawer

--- app/src/views/private/components/flow-sidebar-detail.vue
+++ app/src/views/private/components/flow-sidebar-detail.vue
@@ -181,7 +181,7 @@ const runManualFlow = async (flowId: string) => {
 		</div>
 
 		<v-dialog :model-value="!!confirmRunFlow" @esc="resetConfirm">
-			<v-card>
+			<v-card class="manual-flow-card">
 				<template v-if="confirmDetails">
 					<v-card-title>{{ confirmDetails.description ?? t('run_flow_confirm') }}</v-card-title>
 
