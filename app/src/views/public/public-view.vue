7fd0d74268d7fec041f75f398aeef4bcedbe5126
Rijk van Zanten <rijkvanzanten@me.com>
2024-02-08 16:41:53 -0500

    Fix css layering

--- app/src/views/public/public-view.vue
+++ app/src/views/public/public-view.vue
@@ -187,6 +187,10 @@ const logoURL = computed<string | null>(() => {
 			width: 100%;
 			height: 100%;
 			object-fit: cover;
+			position: absolute;
+			z-index: -1;
+			top: 0;
+			left: 0;
 		}
 
 		.fallback {
