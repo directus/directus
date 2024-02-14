d41c86ab5db9b40122e4b5a067912b2941b3f79b
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-02-14 11:28:33 +0100

    Update Node.js version check in create-directus-project

--- packages/create-directus-project/lib/check-requirements.js
+++ packages/create-directus-project/lib/check-requirements.js
@@ -3,13 +3,14 @@
 import chalk from 'chalk';
 
 export default function checkRequirements() {
-	const nodeVersion = process.versions.node;
-	const major = +nodeVersion.split('.')[0];
+	const nodeVersion = process.versions.node.split('.');
+	const major = +nodeVersion[0];
+	const minor = +nodeVersion[1];
 
-	if (major < 18) {
+	if (major < 18 || minor < 17) {
 		console.error(`You are running ${chalk.red(`Node ${nodeVersion}`)}.`);
-		console.error(`Directus requires ${chalk.green(`Node 18`)} and up.`);
-		console.error('Please update your Node version and try again.');
+		console.error(`Directus requires ${chalk.green(`Node.js 18.17`)} and up.`);
+		console.error('Please update your Node.js version and try again.');
 		process.exit(1);
 	}
 }
