d6e03422e0f558603ad03fc44b8426e851ad9c1a
Brainslug <tim@brainslug.nl>
2024-02-29 16:22:03 +0100

    Removed obsolete v-image code

--- app/src/components/v-image.vue
+++ app/src/components/v-image.vue
@@ -4,14 +4,6 @@ import { useAttrs } from 'vue';
 const attrs = useAttrs();
 </script>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-	inheritAttrs: false,
-});
-</script>
-
 <template>
 	<img v-bind="attrs" />
 </template>
