2a68d4edc82b208636862e9213ffff7f71c271ea
Rijk van Zanten <rijkvanzanten@me.com>
2024-03-07 16:55:16 -0500

    Use lock-based strategy rather than primary process

--- api/src/extensions/lib/sync-extensions.ts
+++ api/src/extensions/lib/sync-extensions.ts
@@ -7,42 +7,43 @@ import { dirname, join, relative, resolve, sep } from 'node:path';
 import { pipeline } from 'node:stream/promises';
 import Queue from 'p-queue';
 import { useBus } from '../../bus/index.js';
+import { useLock } from '../../lock/index.js';
 import { useLogger } from '../../logger.js';
 import { getStorage } from '../../storage/index.js';
 import { getExtensionsPath } from './get-extensions-path.js';
 import { SyncStatus, getSyncStatus, setSyncStatus } from './sync-status.js';
 
 export const syncExtensions = async (options?: { force: boolean }): Promise<void> => {
+	const lock = useLock();
+	const messenger = useBus();
+	const env = useEnv();
+	const logger = useLogger();
+
 	if (!options?.force) {
 		const isDone = (await getSyncStatus()) === SyncStatus.DONE;
 		if (isDone) return;
 	}
 
-	const env = useEnv();
-	const logger = useLogger();
+	const machineId = await mid.machineId();
+	const machineKey = `extensions-sync/${machineId}`;
 
-	const extensionsPath = getExtensionsPath();
-	const storageExtensionsPath = env['EXTENSIONS_PATH'] as string;
+	const processId = await lock.increment(machineKey);
 
-	const messenger = useBus();
+	const currentProcessShouldHandleSync = processId === 1;
 
-	const isPrimaryProcess =
-		String(process.env['NODE_APP_INSTANCE']) === '0' || process.env['NODE_APP_INSTANCE'] === undefined;
-
-	const id = await mid.machineId();
-
-	const message = `extensions-sync/${id}`;
-
-	if (isPrimaryProcess === false) {
+	if (currentProcessShouldHandleSync === false) {
 		logger.trace('Extensions already being synced to this machine from another process.');
 
 		/**
 		 * Wait until the process that called the lock publishes a message that the syncing is complete
 		 */
 		return new Promise((resolve) => {
-			messenger.subscribe(message, () => resolve());
+			messenger.subscribe(machineKey, () => resolve());
 		});
 	}
+
+	const extensionsPath = getExtensionsPath();
+	const storageExtensionsPath = env['EXTENSIONS_PATH'] as string;
 
 	if (await exists(extensionsPath)) {
 		// In case the FS still contains the cached extensions from a previous invocation. We have to
@@ -80,7 +81,7 @@ export const syncExtensions = async (options?: { force: boolean }): Promise<void
 	}
 
 	await queue.onIdle();
-
 	await setSyncStatus(SyncStatus.DONE);
-	messenger.publish(message, { ready: true });
+	await lock.delete(machineKey);
+	messenger.publish(machineKey, { ready: true });
 };
