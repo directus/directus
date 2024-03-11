e8b0c9e4b18d67b282a12f480c8556172b254249
Brainslug <tim@brainslug.nl>
2024-03-11 11:57:54 +0100

    prettier

--- sdk/vitest.config.ts
+++ sdk/vitest.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		typecheck :{
+		typecheck: {
 			only: true,
 			enabled: true,
 			include: ['tests/**/*.test.ts'],
