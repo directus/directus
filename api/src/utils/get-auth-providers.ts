9c73d0ede688a406c85ecc831ab72b2eed882280
Brainslug <tim@brainslug.nl>
2024-03-14 18:16:16 +0100

    cleaned up the code

--- api/src/utils/get-auth-providers.ts
+++ api/src/utils/get-auth-providers.ts
@@ -8,39 +8,31 @@ interface AuthProvider {
 	label?: string;
 }
 
-export function getAuthProviders(): AuthProvider[] {
+export function getAuthProviders({ sessionOnly } = { sessionOnly: false }): AuthProvider[] {
 	const env = useEnv();
 
-	return toArray(env['AUTH_PROVIDERS'] as string)
-		.filter((provider) => provider && env[`AUTH_${provider.toUpperCase()}_DRIVER`])
-		.map((provider) => ({
-			name: provider,
-			label: env[`AUTH_${provider.toUpperCase()}_LABEL`] as string,
-			driver: env[`AUTH_${provider.toUpperCase()}_DRIVER`] as string,
-			icon: env[`AUTH_${provider.toUpperCase()}_ICON`] as string,
-		}));
-}
+	let providers = toArray(env['AUTH_PROVIDERS'] as string)
+		.filter((provider) => provider && env[`AUTH_${provider.toUpperCase()}_DRIVER`]);
 
-export function getSessionAuthProviders(): AuthProvider[] {
-	const env = useEnv();
-
-	return toArray(env['AUTH_PROVIDERS'] as string)
-		.filter((provider) => {
-			if (!provider) return false;
+	if (sessionOnly) {
+		providers = providers.filter((provider) => {
 			const driver = env[`AUTH_${provider.toUpperCase()}_DRIVER`] as string;
-			if (!driver) return false;
 
+			// only the following 3 drivers require a mode selection
 			if (['oauth2', 'openid', 'saml'].includes(driver)) {
-				const mode = env[`AUTH_${provider.toUpperCase()}_MODE`] as string;
+				const mode = env[`AUTH_${provider.toUpperCase()}_MODE`] as string | undefined;
+				// if mode is not defined it defaults to session
 				return !mode || mode === 'session';
 			}
 
 			return true;
-		})
-		.map((provider) => ({
-			name: provider,
-			label: env[`AUTH_${provider.toUpperCase()}_LABEL`] as string,
-			driver: env[`AUTH_${provider.toUpperCase()}_DRIVER`] as string,
-			icon: env[`AUTH_${provider.toUpperCase()}_ICON`] as string,
-		}));
+		});
+	}
+
+	return providers.map((provider) => ({
+		name: provider,
+		label: env[`AUTH_${provider.toUpperCase()}_LABEL`] as string,
+		driver: env[`AUTH_${provider.toUpperCase()}_DRIVER`] as string,
+		icon: env[`AUTH_${provider.toUpperCase()}_ICON`] as string,
+	}));
 }
