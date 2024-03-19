3f6e8dc4a179ce180c355d71dccfcc6e8fc8019a
Brainslug <tim@brainslug.nl>
2024-03-19 18:39:19 +0100

    stop over-engineering

--- app/src/idle.ts
+++ app/src/idle.ts
@@ -1,72 +1,64 @@
 import { throttle } from 'lodash';
 import emitter, { Events } from './events';
 
+const events = ['pointermove', 'pointerdown', 'keydown'];
+
 export const time = 5 * 60 * 1000; // 5 min in ms
 
-class IdleTracker {
-	readonly events: string[];
-	timeout: number  | null;
-	visible: boolean;
-	idle: boolean;
-	throttledEvents: () => any;
+let timeout: number | null;
 
-	constructor() {
-		this.events = ['pointermove', 'pointerdown', 'keydown'];
-		this.timeout = null;
-		this.visible = true;
-		this.idle = false;
-		this.throttledEvents = throttle(this.onIdleEvents.bind(this), 500);
+let visible = true;
+let idle = false;
+
+const throttledOnIdleEvents = throttle(onIdleEvents, 500);
+
+export function startIdleTracking(): void {
+	document.addEventListener('visibilitychange', onVisibilityChange);
+
+	for (const event of events) {
+		document.addEventListener(event, throttledOnIdleEvents);
 	}
 
-	startTracking() {
-		document.addEventListener('visibilitychange', this.onVisibilityChange);
+	resetTimeout();
+}
 
-		for (const event of this.events) {
-			document.addEventListener(event, this.throttledEvents);
-		}
+export function stopIdleTracking(): void {
+	document.removeEventListener('visibilitychange', onVisibilityChange);
 
-		this.resetTimeout();
+	for (const event of events) {
+		document.removeEventListener(event, throttledOnIdleEvents);
+	}
+}
+
+function onIdleEvents() {
+	if (idle === true) {
+		idle = false;
+		emitter.emit(Events.tabActive);
 	}
 
-	stopTracking() {
-		document.removeEventListener('visibilitychange', this.onVisibilityChange);
+	resetTimeout();
+}
 
-		for (const event of this.events) {
-			document.removeEventListener(event, this.throttledEvents);
-		}
+function onVisibilityChange() {
+	if (document.visibilityState === 'hidden' && visible === true) {
+		visible = false;
+		emitter.emit(Events.tabIdle);
 	}
 
-	onIdleEvents() {
-		if (this.idle === true) {
-			this.idle = false;
-			this.visible = true;
-			emitter.emit(Events.tabActive);
-		}
+	if (document.visibilityState === 'visible' && visible === false) {
+		visible = true;
+		emitter.emit(Events.tabActive);
+	}
+}
 
-		this.resetTimeout();
+function resetTimeout() {
+	if (timeout) {
+		window.clearTimeout(timeout);
+		timeout = null;
 	}
 
-	resetTimeout() {
-		if (this.timeout) {
-			window.clearTimeout(this.timeout);
-			this.timeout = null;
-		}
-
-		this.timeout = window.setTimeout(() => {
-			this.idle = true;
-			emitter.emit(Events.tabIdle);
-		}, time);
-	}
-
-	onVisibilityChange() {
-		if (document.visibilityState === 'hidden' && this.visible === true) {
-			this.visible = false;
-			emitter.emit(Events.tabHidden);
-		}
-
-		if (document.visibilityState === 'visible' && this.visible === false) {
-			this.visible = true;
-			emitter.emit(Events.tabActive);
-		}
-	}
+	timeout = window.setTimeout(() => {
+		idle = true;
+		emitter.emit(Events.tabIdle);
+	}, time);
 }
