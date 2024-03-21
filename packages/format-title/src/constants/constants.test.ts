04a424608d2f3daa68121e5864240c7442c8d8e2
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-03-21 11:25:43 +0100

    Simplify duplicates test

--- packages/format-title/src/constants/constants.test.ts
+++ packages/format-title/src/constants/constants.test.ts
@@ -2,23 +2,9 @@ import { expect, test } from 'vitest';
 import acronyms from '../constants/acronyms.js';
 import specialCase from '../constants/special-case.js';
 
-const lists: string[][] = [acronyms, specialCase];
+test('No duplicates across acronyms and special cases', () => {
+	const constants = [...acronyms, ...specialCase];
+	const duplicates = constants.filter((item, index) => constants.indexOf(item) !== index);
 
-test('Words should not be duplicated in lists', () => {
-	const words: string[] = [];
-	let hasDuplicate = false;
-
-	for (const list of lists) {
-		for (const word of list) {
-			if (words.includes(word)) {
-				// eslint-disable-next-line no-console
-				console.log(`Duplicated word: "${word}"`);
-				hasDuplicate = true;
-			} else {
-				words.push(word);
-			}
-		}
-	}
-
-	expect(hasDuplicate).toBe(false);
+	expect(duplicates).toEqual([]);
 });
