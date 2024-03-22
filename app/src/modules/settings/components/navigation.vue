8ce013bfad2b337a86c1c3d8606ba87acf081373
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2024-03-22 17:20:01 +0100

    remove isvisible field
    no longer used

--- app/src/modules/settings/components/navigation.vue
+++ app/src/modules/settings/components/navigation.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { DEFAULT_REPORT_BUG_URL, DEFAULT_REPORT_FEATURE_URL } from '@/constants.js';
-import { useFeatureFlagStore } from '@/stores/feature-flags';
 import { useServerStore } from '@/stores/server';
 import { useSettingsStore } from '@/stores/settings';
 import { storeToRefs } from 'pinia';
@@ -8,7 +7,6 @@ import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 type Link = {
-	isVisible: boolean;
 	icon: string;
 	name: string;
 	to?: string;
@@ -19,30 +17,20 @@ type Link = {
 const { t } = useI18n();
 const { info } = storeToRefs(useServerStore());
 const { settings } = storeToRefs(useSettingsStore());
-const { showWebhooks } = storeToRefs(useFeatureFlagStore());
 
 const links = computed<Link[][]>(() => [
 	[
 		{
-			isVisible: true,
 			icon: 'database',
 			name: t('settings_data_model'),
 			to: `/settings/data-model`,
 		},
 		{
-			isVisible: true,
 			icon: 'admin_panel_settings',
 			name: t('settings_permissions'),
 			to: `/settings/roles`,
 		},
 		{
-			isVisible: showWebhooks.value,
-			icon: 'anchor',
-			name: t('settings_webhooks'),
-			to: `/settings/webhooks`,
-		},
-		{
-			isVisible: true,
 			icon: 'bolt',
 			name: t('settings_flows'),
 			to: `/settings/flows`,
@@ -50,25 +38,21 @@ const links = computed<Link[][]>(() => [
 	],
 	[
 		{
-			isVisible: true,
 			icon: 'tune',
 			name: t('settings_project'),
 			to: `/settings/project`,
 		},
 		{
-			isVisible: true,
 			icon: 'palette',
 			name: t('settings_appearance'),
 			to: `/settings/appearance`,
 		},
 		{
-			isVisible: true,
 			icon: 'bookmark',
 			name: t('settings_presets'),
 			to: `/settings/presets`,
 		},
 		{
-			isVisible: true,
 			icon: 'translate',
 			name: t('settings_translations'),
 			to: `/settings/translations`,
@@ -76,14 +60,12 @@ const links = computed<Link[][]>(() => [
 	],
 	[
 		{
-			isVisible: true,
 			icon: 'storefront',
 			name: t('marketplace'),
 			to: '/settings/marketplace',
 			chip: t('beta'),
 		},
 		{
-			isVisible: true,
 			icon: 'category',
 			name: t('extensions'),
 			to: '/settings/extensions',
@@ -91,13 +73,11 @@ const links = computed<Link[][]>(() => [
 	],
 	[
 		{
-			isVisible: true,
 			icon: 'bug_report',
 			name: t('report_bug'),
 			href: settings.value?.report_bug_url ?? DEFAULT_REPORT_BUG_URL,
 		},
 		{
-			isVisible: true,
 			icon: 'new_releases',
 			name: t('request_feature'),
 			href: settings.value?.report_feature_url ?? DEFAULT_REPORT_FEATURE_URL,
@@ -109,17 +89,15 @@ const links = computed<Link[][]>(() => [
 <template>
 	<v-list nav>
 		<template v-for="(group, index) in links">
-			<template v-for="item in group" :key="item.to ?? item.href">
-				<v-list-item v-if="item.isVisible" :to="item.to" :href="item.href">
-					<v-list-item-icon><v-icon :name="item.icon" /></v-list-item-icon>
-					<v-list-item-content>
-						<span class="label">
-							<v-text-overflow class="label" :text="item.name" />
-							<v-chip v-if="item.chip" class="chip" outlined x-small>{{ item.chip }}</v-chip>
-						</span>
-					</v-list-item-content>
-				</v-list-item>
-			</template>
+			<v-list-item v-for="item in group" :key="item.to ?? item.href" :to="item.to" :href="item.href">
+				<v-list-item-icon><v-icon :name="item.icon" /></v-list-item-icon>
+				<v-list-item-content>
+					<span class="label">
+						<v-text-overflow class="label" :text="item.name" />
+						<v-chip v-if="item.chip" class="chip" outlined x-small>{{ item.chip }}</v-chip>
+					</span>
+				</v-list-item-content>
+			</v-list-item>
 
 			<v-divider v-if="index !== links.length - 1" :key="index" />
 		</template>
