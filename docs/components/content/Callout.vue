61528899367b932641b12a189f5c4d96cded6fd2
Kevin Lewis <kvn@lws.io>
2024-04-05 19:03:21 +0200

    Create a project

--- docs/components/content/Callout.vue
+++ docs/components/content/Callout.vue
@@ -1,25 +1,46 @@
 <script setup>
-defineProps({
+const props = defineProps({
 	title: String,
+	url: String,
+	toggleable: Boolean,
 	type: {
 		type: String,
 		default: 'info'	// warning | link | user-guide | dev-docs | api-ref | tutorials
-	}
+	},
 })
+
+const componentType = props.url ? 'a' : 'div'
 </script>
 
 <template>
-  <div>
-	<p>{{ type }} <b>{{ title }}</b></p>
-    <ContentSlot :use="$slots.default" />
-  </div>
+	<details v-if="toggleable">
+		<summary>{{ title }}</summary>
+		<ContentSlot :use="$slots.default" />
+	</details>
+	<component :is="componentType" v-else :href="url" class="callout">
+		<p>{{ type }} <b>{{ title }}</b></p>
+		<ContentSlot :use="$slots.default" />
+	</component>
 </template>
 
 <style scoped>
-div {
+.callout {
+	display: block;
 	background: #f8fafc;
 	border: 1px solid #e2e8f0;
 	padding: 1px 1rem;
 	border-radius: 8px;
+	text-decoration: none;
+	color: inherit;
+}
+
+a.callout {
+	border-style: dashed;
+}
+
+a.callout:hover {
+	border-style: solid;
+	border-color: #6644ff;
+	cursor: pointer;
 }
 </style>
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+	content: {
+	}
 })
