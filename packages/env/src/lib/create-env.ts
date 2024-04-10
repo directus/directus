e5041d8cab3dc9abd5a4e21812307dcbeaeb304d
Jose Varela <joselcvarela@gmail.com>
2024-04-10 23:12:42 +0100

    Allow cast file or default to string

--- packages/env/src/lib/create-env.ts
+++ packages/env/src/lib/create-env.ts
@@ -9,6 +9,7 @@ import { readConfigurationFromProcess } from '../utils/read-configuration-from-p
 import { removeFileSuffix } from '../utils/remove-file-suffix.js';
 import { cast } from './cast.js';
 import { readConfigurationFromFile } from './read-configuration-from-file.js';
+import { getCastFlag } from '../utils/has-cast-prefix.js';
 
 export const createEnv = (): Env => {
 	const baseConfiguration = readConfigurationFromProcess();
@@ -25,7 +26,9 @@ export const createEnv = (): Env => {
 	for (let [key, value] of Object.entries(rawConfiguration)) {
 		if (isFileKey(key) && isDirectusVariable(key) && typeof value === 'string') {
 			try {
-				value = readFileSync(value, { encoding: 'utf8' });
+				const castFlag = getCastFlag(value);
+				const castPrefix = (castFlag ? castFlag : 'string') + ':';
+				value = castPrefix + readFileSync(castFlag ? value.replace(castPrefix, '') : value, { encoding: 'utf8' });
 				key = removeFileSuffix(key);
 			} catch {
 				throw new Error(`Failed to read value from file "${value}", defined in environment variable "${key}".`);
