b92d03bd35bbc40d70aee43bb5b579b5321d9fa4
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-04-19 23:12:07 +0200

    Inline error code const

--- api/src/utils/transaction.ts
+++ api/src/utils/transaction.ts
@@ -3,16 +3,6 @@ import { getDatabaseClient } from '../database/index.js';
 import { useLogger } from '../logger.js';
 
 /**
- * This error code indicates that the transaction failed due to another
- * concurrent or recent transaction attempting to write to the same data.
- * This can usually be solved by restarting the transaction on client-side
- * after a short delay, so that it is executed against the latest state.
- *
- * @link https://www.cockroachlabs.com/docs/stable/transaction-retry-error-reference
- */
-const COCKROACH_RETRY_ERROR_CODE = '40001';
-
-/**
  * Execute the given handler within the current transaction or a newly created one
  * if the current knex state isn't a transaction yet.
  *
@@ -27,6 +17,16 @@ export const transaction = async <T = unknown>(knex: Knex, handler: (knex: Knex)
 			return await knex.transaction((trx) => handler(trx));
 		} catch (error: any) {
 			const client = getDatabaseClient(knex);
+
+			/**
+			 * This error code indicates that the transaction failed due to another
+			 * concurrent or recent transaction attempting to write to the same data.
+			 * This can usually be solved by restarting the transaction on client-side
+			 * after a short delay, so that it is executed against the latest state.
+			 *
+			 * @link https://www.cockroachlabs.com/docs/stable/transaction-retry-error-reference
+			 */
+			const COCKROACH_RETRY_ERROR_CODE = '40001';
 
 			if (client !== 'cockroachdb' || error?.code !== COCKROACH_RETRY_ERROR_CODE) throw error;
 
