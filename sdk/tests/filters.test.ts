323a02c4d5d0c50cbb9ee0a7a91bd58d72e1970c
Brainslug <tim@brainslug.nl>
2024-04-22 18:22:07 +0200

    prettier

--- sdk/tests/filters.test.ts
+++ sdk/tests/filters.test.ts
@@ -3,48 +3,61 @@ import { createDirectus, readItems, rest } from '../src/index.js';
 import type { TestSchema } from './schema.js';
 
 describe('Test QueryFilters', () => {
-
 	test('issue #20633 resolving _and/_or filters', () => {
 		const client = createDirectus<TestSchema>('http://localhost:8055').with(rest());
 
-		const withConditional = () => client.request(
-			readItems('collection_a', {
-			  fields: ['id', 'string_field', {
-				m2o: ['id']
-			  }],
-			  filter: {
-				_and: [{ id: { _eq: 1 } }]
-			  },
-		}));
-
-		const withoutConditional = () => client.request(
+		const withConditional = () =>
+			client.request(
 				readItems('collection_a', {
-				  fields: ['id', 'string_field', {
-					m2o: ['id']
-				  }],
-				  filter: { id: { _eq: 1 } },
-			}));
+					fields: [
+						'id',
+						'string_field',
+						{
+							m2o: ['id'],
+						},
+					],
+					filter: {
+						_and: [{ id: { _eq: 1 } }],
+					},
+				}),
+			);
 
+		const withoutConditional = () =>
+			client.request(
+				readItems('collection_a', {
+					fields: [
+						'id',
+						'string_field',
+						{
+							m2o: ['id'],
+						},
+					],
+					filter: { id: { _eq: 1 } },
+				}),
+			);
 
 		// make sure the output types are identical since filtering should not affect output
 		type TypeWithConditional = Awaited<ReturnType<typeof withConditional>>;
 		type TypeWithoutConditional = Awaited<ReturnType<typeof withoutConditional>>;
 
-		const resultA: TypeWithConditional = [{
-			id: 1,
-			string_field: 'string',
-			m2o: { id: 1 }
-		}];
+		const resultA: TypeWithConditional = [
+			{
+				id: 1,
+				string_field: 'string',
+				m2o: { id: 1 },
+			},
+		];
 
 		expectTypeOf(resultA).toEqualTypeOf<TypeWithoutConditional>();
 
-		const resultB: TypeWithoutConditional = [{
-			id: 1,
-			string_field: 'string',
-			m2o: { id: 1 }
-		}];
+		const resultB: TypeWithoutConditional = [
+			{
+				id: 1,
+				string_field: 'string',
+				m2o: { id: 1 },
+			},
+		];
 
 		expectTypeOf(resultB).toEqualTypeOf<TypeWithConditional>();
-	})
-})
-
+	});
+});
