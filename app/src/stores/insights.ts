04680845d788a004c3eda2885c202eda44f7b57b
Brainslug <tim@brainslug.nl>
2024-04-22 21:44:59 +0200

    resolved insights input types

--- app/src/stores/insights.ts
+++ app/src/stores/insights.ts
@@ -5,17 +5,17 @@ import { Dashboard } from '@/types/insights';
 import { fetchAll } from '@/utils/fetch-all';
 import { queryToGqlString } from '@/utils/query-to-gql-string';
 import { unexpectedError } from '@/utils/unexpected-error';
-import type { Panel } from '@directus/extensions';
-import { createPanels, deletePanels } from '@directus/sdk';
+import { DirectusPanel, createPanels, deletePanels, updatePanelsBatch } from '@directus/sdk';
 import { isSystemCollection } from '@directus/system-data';
 import type { Item } from '@directus/types';
 import { applyOptionsData, getSimpleHash, toArray } from '@directus/utils';
-import { AxiosResponse } from 'axios';
 import escapeStringRegexp from 'escape-string-regexp';
 import { assign, clone, get, isUndefined, mapKeys, omit, omitBy, pull, uniq } from 'lodash';
 import { nanoid } from 'nanoid';
 import { acceptHMRUpdate, defineStore } from 'pinia';
 import { computed, reactive, ref, unref } from 'vue';
+
+type Panel = DirectusPanel<any>;
 
 export type CreatePanel = Partial<Panel> &
 	Pick<Panel, 'id' | 'width' | 'height' | 'position_x' | 'position_y' | 'type' | 'options'>;
@@ -187,8 +187,8 @@ export const useInsightsStore = defineStore('insightsStore', () => {
 	function emptyRelations() {
 		return unref(panels)
 			.filter(({ type }) => type === 'relational-variable')
-			.filter(({ options }) => get(unref(variables), options.field) == undefined)
-			.map(({ options }) => options.field)
+			.filter(({ options }) => get(unref(variables), options?.field) == undefined)
+			.map(({ options }) => options?.field)
 			.filter((fieldName) => typeof fieldName === 'string' && fieldName.length > 0);
 	}
 
@@ -332,7 +332,7 @@ export const useInsightsStore = defineStore('insightsStore', () => {
 		panelEdits = omitBy(panelEdits, isUndefined);
 
 		const isNew = id.startsWith('_');
-		const arr = isNew ? edits.create : edits.update;
+		const arr: Partial<Panel>[] = isNew ? edits.create : edits.update;
 
 		/**
 		 * Check what the currently used data query is, so we can compare it to the new query later to
@@ -427,20 +427,12 @@ export const useInsightsStore = defineStore('insightsStore', () => {
 
 			if (edits.create) {
 				// Created edits might come with a temporary ID for editing. Make sure to submit to API without temp ID
-				//TODO fix type
-				const panels = edits.create.map((create) => omit(create, 'id')) as Parameters<typeof createPanels>[0];
+				const panels = edits.create.map((create) => omit(create, 'id'));
 				requests.push(sdk.request(createPanels(panels)));
 			}
 
 			if (edits.update) {
-				// TODO update updatePanels signature
-				requests.push(
-					sdk.request(() => ({
-						path: '/panels',
-						method: 'PATCH',
-						body: JSON.stringify(edits.update),
-					})),
-				);
+				requests.push(sdk.request(updatePanelsBatch(edits.update)));
 			}
 
 			if (edits.delete) {
