be9bdbedbace7b8076e8a6b1a5db30de2e82d460
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2024-04-23 13:30:03 +0200

    redo how emails will be filtered

--- api/src/database/migrations/20240422A-public-registration.ts
+++ api/src/database/migrations/20240422A-public-registration.ts
@@ -6,19 +6,19 @@ const COLUMN_ROLES_ID = `${TABLE_ROLES}.id`;
 const TABLE_SETTINGS = 'directus_settings';
 const NEW_COLUMN_IS_REGISTRATION_ENABLED = 'is_public_registration_enabled';
 const NEW_COLUMN_ROLE = 'public_registration_role';
-const NEW_COLUMN_VALIDATE_EMAIL = 'require_public_registration_email_validation';
+const NEW_COLUMN_EMAIL_FILTER = 'public_registration_email_filter';
 
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable(TABLE_SETTINGS, (table) => {
 		table.boolean(NEW_COLUMN_IS_REGISTRATION_ENABLED).notNullable().defaultTo(false);
 		table.uuid(NEW_COLUMN_ROLE).nullable();
 		table.foreign(NEW_COLUMN_ROLE).references(COLUMN_ROLES_ID).onDelete('SET NULL');
-		table.boolean(NEW_COLUMN_VALIDATE_EMAIL).notNullable().defaultTo(true);
+		table.json(NEW_COLUMN_EMAIL_FILTER).nullable();
 	});
 }
 
 export async function down(knex: Knex): Promise<void> {
 	await knex.schema.alterTable(TABLE_SETTINGS, (table) => {
-		table.dropColumns(NEW_COLUMN_IS_REGISTRATION_ENABLED, NEW_COLUMN_ROLE, NEW_COLUMN_VALIDATE_EMAIL);
+		table.dropColumns(NEW_COLUMN_IS_REGISTRATION_ENABLED, NEW_COLUMN_ROLE, NEW_COLUMN_EMAIL_FILTER);
 	});
 }
