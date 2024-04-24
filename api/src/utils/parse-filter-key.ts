0e6e3554eac98db5efb5c32045cedde4cf793c91
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-04-24 17:56:41 +0200

    Perf optimization by defining regex outside of fn
    Also using this opportunity to explain the regex in comment
    
    Co-authored-by: Hannes Küttner <4376726+hanneskuettner@users.noreply.github.com>

--- api/src/utils/parse-filter-key.ts
+++ api/src/utils/parse-filter-key.ts
@@ -1,8 +1,17 @@
 /**
+ * Result for keys with a function (e.g. `year(date_created)`):
+ * - Group 1: Function (`year`)
+ * - Group 3: Field (`date_created`)
+ *
+ * If group 3 is undefined, it is a key without a function.
+ */
+const FILTER_KEY_REGEX = /^([^()]+)(\(([^)]+)\))?/;
+
+/**
  * Strip the function declarations from a list of fields
  */
 export function parseFilterKey(key: string) {
-	const match = key.match(/^([^()]+)(\(([^)]+)\))?/);
+	const match = key.match(FILTER_KEY_REGEX);
 
 	const fieldNameWithFunction = match?.[3]?.trim();
 	const fieldName = fieldNameWithFunction || key.trim();
