d297f281900f4a7308306581aa4187154f6897df
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-04-25 14:43:46 +0200

    Inject attr at beginning
    Just in case for `<span mi*/>` which would result into `<span mi*/ translate="no">`

--- docs/.vitepress/lib/markdown-plugins/material-icons-no-translate.ts
+++ docs/.vitepress/lib/markdown-plugins/material-icons-no-translate.ts
@@ -4,11 +4,9 @@ import iterator from 'markdown-it-for-inline';
 const MATERIAL_ICON_RE = /<span(.*\bmi\b.*)>/;
 
 export function useMaterialIconsNoTranslate(md: any) {
-	md.use(iterator, 'mi_no_translate', 'html_inline', function (tokens: any, idx: number) {
-		const m = tokens[idx].content.match(MATERIAL_ICON_RE);
+	md.use(iterator, 'mi_no_translate', 'html_inline', (tokens: any, idx: number) => {
+		const match = tokens[idx].content.match(MATERIAL_ICON_RE);
 
-		if (m) {
-			tokens[idx].content = `<span${m[1]} translate="no">`;
-		}
+		if (match) tokens[idx].content = `<span translate="no"${match[1]}>`;
 	});
 }
