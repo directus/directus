436ad63e0ab368b2e4935d7e6795c2d49a7b0d4d
Hannes Küttner <4376726+hanneskuettner@users.noreply.github.com>
2024-04-25 15:14:04 +0200

    Fix SQL injection possibility
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>

--- api/src/database/helpers/number/dialects/postgres.ts
+++ api/src/database/helpers/number/dialects/postgres.ts
@@ -3,6 +3,6 @@ import { NumberSearchHelper } from '../types.js';
 
 export class NumberSearchHelperPostgres extends NumberSearchHelper {
 	override orWhere(dbQuery: Knex.QueryBuilder, collection: string, name: string, value: number): Knex.QueryBuilder {
-		return dbQuery.orWhereRaw(`??.?? = ${value}`, [collection, name]);
+		return dbQuery.orWhereRaw(`??.?? = ?`, [collection, name, value]);
 	}
 }
