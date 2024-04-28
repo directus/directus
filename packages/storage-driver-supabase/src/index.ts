be53cf4f3eafe6bf1139bd1120328a4e8adeb8b9
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-04-29 00:41:23 +0200

    Align comments, use neutral wording

--- packages/storage-driver-supabase/src/index.ts
+++ packages/storage-driver-supabase/src/index.ts
@@ -10,7 +10,7 @@ export type DriverSupabaseConfig = {
 	bucket: string;
 	serviceRole: string;
 	projectId?: string;
-	// Allows a custom Supabase endpoint incase self-hosting
+	/** Allows a custom Supabase endpoint for self-hosting */
 	endpoint?: string;
 	root?: string;
 };
@@ -150,27 +150,27 @@ export class DriverSupabase implements Driver {
 		let itemCount = 0;
 
 		/*
-			The Supabase API only returns the directories and files directly within the queried location
-			(called prefix in their API), matching the search query. Directories can be identified by the id being null.
-
-			Since we are unsure if the last part of the prefix param is a directory, a file or part of a directory or file name
-			we first need to query with the largest common denominator (the parent directory part of the prefix)
-			and filter the results with the remaining part of the query.
-
-			This can lead to these outcomes:
-			1. The full prefix is a file and is split up into parentdir/filename, the API is queried with
-			   { prefix: parentdir, search: filename } and returns one result { name: filename, id: "..." }.
-			   We then yield the filename.
-			2. The full prefix is a directory and is split up into parentdir/dir, the API is queried with
-			   { prefix: parentdir, search: dir } and returns one result { name: dir, id: null } and importantly, not the
-			   contents of the directory. We then need to recursively list the contents of the directory by calling this
-			   function with the prefix and a trailing "/" as an indicator that we are interested in the directory contents.
-			   2.1. The prefix ends with a "/", the API is queried with { prefix: prefix, search: "" }
-			   		 and returns all files and directories within the prefix, which are yielded and recursively listed.
-			3. Special case of 1. and 2.: The prefix is part of a filename/directory name and could result in more than one result
-			   that all match the search query. The API is queried with { prefix: parentdir, search: part of filename }
-			   and returns all files and directories in parentdir that match the search query. Filenames are yielded and
-			   directories are recursively listed.
+		 *	The Supabase API only returns the directories and files directly within the queried location
+		 *	(called prefix in their API) matching the search query. Directories can be identified by the id being null.
+		 *
+		 *	Since it's unknown whether the last part of the prefix param is a directory, a file or part of a directory or file name,
+		 *	the first query will be the largest common denominator (the parent directory part of the prefix)
+		 *	and the results then filtered with the remaining part of the query.
+		 *
+		 *	This can lead to the following outcomes:
+		 *	1. The full prefix is a file and is split up into parentdir/filename, the API is queried with
+		 *	   { prefix: parentdir, search: filename } and returns one result { name: filename, id: "..." }.
+		 *	   The filename is yielded in that case.
+		 *	2. The full prefix is a directory and is split up into parentdir/dir, the API is queried with
+		 *	   { prefix: parentdir, search: dir } and returns one result { name: dir, id: null } and importantly, not the
+		 *	   contents of the directory. Then  the contents of the directory must be listed recursively by calling this
+		 *	   function with the prefix and a trailing "/".
+		 *	   2.1. The prefix ends with a "/", the API is queried with { prefix: prefix, search: "" }
+		 *	        and returns all files and directories within the prefix, which are yielded and recursively listed.
+		 *	3. Special case of 1. and 2.: The prefix is part of a filename/directory name and could result in more than one result
+		 *	   that all match the search query. The API is queried with { prefix: parentdir, search: part of filename }
+		 *	   and returns all files and directories in parentdir that match the search query. Filenames are yielded and
+		 *	   directories are recursively listed.
 		 */
 		const isDirectory = prefix.endsWith('/');
 		const prefixDirectory = isDirectory ? prefix : dirname(prefix);
@@ -191,15 +191,15 @@ export class DriverSupabase implements Driver {
 			offset += itemCount;
 
 			for (const item of data) {
-				// Since the API only returns the filename the file path is constructed by joining the prefix with the item name.
+				// Since the API only returns the filename, the file path is constructed by joining the prefix with the item name
 				const filePath = normalizePath(join(prefixDirectory, item.name));
 
 				if (item.id !== null) {
-					// remove the root from the path and yield the filename
+					// Remove the root from the path and yield the filename
 					yield filePath.substring(this.config.root ? this.config.root.length + 1 : 0);
 				} else {
-					// this is a directory, recursively list it
-					yield* this.listGenerator(filePath + '/');
+					// This is a directory, recursively list it
+					yield* this.listGenerator(`${filePath}/`);
 				}
 			}
 		} while (itemCount === limit);
