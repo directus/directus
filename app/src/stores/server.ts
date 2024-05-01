6de32b0b384b9ba206fad45ce3163aab9bbe37f1
Jose Varela <joselcvarela@gmail.com>
2024-05-01 06:34:02 +0100

    Improve rate limit calculation

--- app/src/stores/server.ts
+++ app/src/stores/server.ts
@@ -114,7 +114,9 @@ export const useServerStore = defineStore('serverStore', () => {
 				await replaceQueue();
 			} else {
 				const { duration, points } = serverInfoResponse.data.data.rateLimit;
-				await replaceQueue({ intervalCap: points - 10, interval: duration * 1000, carryoverConcurrencyCount: true });
+				const intervalCap = 1;
+				const interval = Math.ceil((duration * 1000) / points);
+				await replaceQueue({ intervalCap, interval, carryoverConcurrencyCount: true, concurrency: 1 });
 			}
 		}
 	};
