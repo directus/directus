c50176af434feebc418c3e1d61bfd275e1033ef2
Brainslug <tim@brainslug.nl>
2024-05-01 15:16:19 +0200

    updated imports

--- api/src/utils/verify-session-jwt.ts
+++ api/src/utils/verify-session-jwt.ts
@@ -1,6 +1,6 @@
-import getDatabase from "../database";
+import getDatabase from '../database/index.js';
 import { InvalidTokenError } from '@directus/errors';
-import type { DirectusTokenPayload } from "../types";
+import type { DirectusTokenPayload } from '../types/index.js';
 
 export async function verifySessionJWT(payload: DirectusTokenPayload) {
 	const database = getDatabase();
@@ -9,8 +9,8 @@ export async function verifySessionJWT(payload: DirectusTokenPayload) {
 		.select('token', 'user', 'expires', 'ip')
 		.from('directus_sessions')
 		.where({
-			'token': payload['session'],
-			'user': payload['id'],
+			token: payload['session'],
+			user: payload['id'],
 		})
 		.andWhere('expires', '>=', new Date())
 		.first();
