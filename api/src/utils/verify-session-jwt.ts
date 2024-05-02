e3453643bd6b17449d1150ad1a24ae9bb471f6ab
Brainslug <br41nslug@users.noreply.github.com>
2024-05-02 12:16:03 +0200

    Update api/src/utils/verify-session-jwt.ts
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>

--- api/src/utils/verify-session-jwt.ts
+++ api/src/utils/verify-session-jwt.ts
@@ -6,7 +6,7 @@ export async function verifySessionJWT(payload: DirectusTokenPayload) {
 	const database = getDatabase();
 
 	const session = await database
-		.select('token', 'user', 'expires', 'ip')
+		.select(1)
 		.from('directus_sessions')
 		.where({
 			token: payload['session'],
