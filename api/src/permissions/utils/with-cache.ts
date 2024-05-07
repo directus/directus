34624b200b8a7c55dd747342adede820329d4d35
Rijk van Zanten <rijkvanzanten@me.com>
2024-05-07 14:02:48 -0400

    Add with-cache memoize util

---
+++ api/src/permissions/utils/with-cache.ts
@@ -0,0 +1,23 @@
+import { getSimpleHash } from '@directus/utils';
+import { useCache } from '../cache.js';
+
+export function withCache<F extends (...args: any[]) => any>(namespace: string, handler: F) {
+	const cache = useCache();
+
+	const fn = async (...args: any[]) => {
+		const key = namespace + '-' + getSimpleHash(JSON.stringify(args[0])); // todo make deterministic
+		const cached = await cache.get(key);
+
+		if (cached) {
+			return cached;
+		}
+
+		const res = await handler(...args);
+
+		cache.set(key, res);
+
+		return res;
+	};
+
+	return fn as F;
+}
