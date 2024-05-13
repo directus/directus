99368ee23bb8886d3676f864bcf77a154389279c
ian <licitdev@gmail.com>
2024-05-13 16:26:54 +0800

    Allow specifying of reason for limit exceeded error

--- packages/errors/src/errors/limit-exceeded.ts
+++ packages/errors/src/errors/limit-exceeded.ts
@@ -1,3 +1,14 @@
 import { createError, ErrorCode } from '../index.js';
 
-export const LimitExceededError = createError(ErrorCode.LimitExceeded, 'Limit exceeded.', 403);
+export interface LimitExceededErrorExtensions {
+	message: string;
+}
+
+export const messageConstructor = (extensions?: LimitExceededErrorExtensions) =>
+	`${extensions?.message ? extensions.message : 'Limit exceeded.'}`;
+
+export const LimitExceededError = createError<LimitExceededErrorExtensions>(
+	ErrorCode.LimitExceeded,
+	messageConstructor,
+	403,
+);
