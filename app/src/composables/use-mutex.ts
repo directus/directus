5f6e47ae63c3bab1dca731ccd12448d30b610b0b
ian <licitdev@gmail.com>
2024-05-14 00:15:50 +0800

    Ensure mutex is released when callback errors

--- app/src/composables/use-mutex.ts
+++ app/src/composables/use-mutex.ts
@@ -22,27 +22,33 @@ export function useMutex(key: MutexKey, expiresMs: number) {
 
 	async function localStorageLock(callback: (lock?: Lock | null) => Promise<any>) {
 		let retries = 0;
+		let hasAcquiredMutex = false;
 
-		do {
-			const mutex = localStorage.getItem(internalKey);
+		try {
+			do {
+				const mutex = localStorage.getItem(internalKey);
 
-			if (!mutex || Number(mutex) > Date.now() + expiresMs) {
-				// set lock
-				localStorage.setItem(internalKey, String(Date.now() + expiresMs));
+				if (!mutex || Number(mutex) > Date.now() + expiresMs) {
+					// set lock
+					localStorage.setItem(internalKey, String(Date.now() + expiresMs));
+					hasAcquiredMutex = true;
 
-				// do logic
-				await callback(null);
+					// do logic
+					await callback(null);
 
+					break;
+				}
+
+				await sleep(timeout);
+				retries += 1;
+			} while (retries < maxRetries);
+			// throw error when hitting max retries?
+		} finally {
+			if (hasAcquiredMutex) {
 				// release lock
 				localStorage.removeItem(internalKey);
-
-				break;
 			}
-
-			await sleep(timeout);
-			retries += 1;
-		} while (retries < maxRetries);
-		// throw error when hitting max retries?
+		}
 	}
 
 	return {
