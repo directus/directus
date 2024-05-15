6c0cae62c004698d696312225f25fd0c44013077
Brainslug <tim@brainslug.nl>
2024-05-15 11:58:23 +0200

    use the correct TTL variable for the database session

--- api/src/services/authentication.ts
+++ api/src/services/authentication.ts
@@ -358,7 +358,7 @@ export class AuthenticationService {
 		}
 
 		const newRefreshToken = nanoid(64);
-		const refreshTokenExpiration = new Date(Date.now() + getMilliseconds(env['REFRESH_TOKEN_TTL'], 0));
+		const refreshTokenExpiration = new Date(Date.now() + getMilliseconds(env[options?.session ? 'SESSION_COOKIE_TTL' : 'REFRESH_TOKEN_TTL'], 0));
 
 		const tokenPayload: DirectusTokenPayload = {
 			id: record.user_id,
