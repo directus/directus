07b3e3e779ca355fdf377a2a5b95c649359c1dcb
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2024-05-22 19:41:58 +0200

    add missing type annotation

--- api/src/permissions/modules/fetch-global-access/fetch-global-access.ts
+++ api/src/permissions/modules/fetch-global-access/fetch-global-access.ts
@@ -1,13 +1,14 @@
 import type { Knex } from 'knex';
 import { fetchGlobalAccessForRoles } from './lib/fetch-global-access-for-roles.js';
 import { fetchGlobalAccessForUser } from './lib/fetch-global-access-for-user.js';
+import type { GlobalAccess } from './types.js';
 
 /**
  * Fetch the global access (eg admin/app access) rules for the given roles, or roles+user combination
  *
  * Will fetch roles and user info separately so they can be cached and reused individually
  */
-export async function fetchGlobalAccess(knex: Knex, roles: string[], user?: string) {
+export async function fetchGlobalAccess(knex: Knex, roles: string[], user?: string): Promise<GlobalAccess> {
 	let { app, admin } = await fetchGlobalAccessForRoles(knex, roles);
 
 	if (user !== undefined) {
