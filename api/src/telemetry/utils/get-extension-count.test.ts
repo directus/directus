3121cc905325f97d88ed426f817309f604711e94
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2024-05-28 17:41:45 +0200

    change to test the subtraction instead of implementation

--- api/src/telemetry/utils/get-extension-count.test.ts
+++ api/src/telemetry/utils/get-extension-count.test.ts
@@ -1,31 +1,30 @@
 import { Knex } from 'knex';
 import { afterEach, beforeEach, expect, test, vi } from 'vitest';
-import { getExtensionCount } from './get-extension-count.js';
+import { getExtensionCount, type ExtensionCount } from './get-extension-count.js';
 
-let mockResult: { count: string }[];
-let mockFilteredBundleResult: { count: string }[];
+let mockQueryEnabled: { count?: string | number } | undefined;
+let mockQueryBundles: { count?: string | number } | undefined;
+const mockCountEnabled = 4;
+const mockCountBundles = 2;
 let mockDb: Knex;
 
 beforeEach(() => {
-	mockResult = [
-		{
-			count: '15',
-		},
-	];
+	mockQueryEnabled = {
+		count: mockCountEnabled,
+	};
 
-	mockFilteredBundleResult = [
-		{
-			count: '2',
-		},
-	];
+	mockQueryBundles = {
+		count: mockCountBundles,
+	};
 
 	mockDb = {
 		count: vi.fn().mockReturnThis(),
+		where: vi.fn().mockReturnThis(),
+		whereIn: vi.fn().mockReturnThis(),
+		andWhere: vi.fn().mockReturnThis(),
 		distinct: vi.fn().mockReturnThis(),
 		from: vi.fn().mockReturnThis(),
-		where: vi.fn().mockResolvedValue(mockResult),
-		whereIn: vi.fn().mockReturnThis(),
-		andWhere: vi.fn().mockResolvedValue(mockFilteredBundleResult),
+		first: vi.fn().mockResolvedValueOnce(mockQueryEnabled).mockResolvedValueOnce(mockQueryBundles),
 	} as unknown as Knex;
 });
 
@@ -33,22 +32,8 @@ afterEach(() => {
 	vi.clearAllMocks();
 });
 
-test('Fetches extension count from the database', async () => {
+test('Subtracts bundle parents from total extensions', async () => {
 	const result = await getExtensionCount(mockDb);
 
-	expect(mockDb.count).toHaveBeenCalledWith('*', { as: 'count' });
-	expect(mockDb.from).toHaveBeenCalledWith('directus_extensions');
-	expect(mockDb.where).toHaveBeenCalledWith('enabled', '=', true);
-	expect(mockDb.distinct).toHaveBeenCalledWith('bundle');
-	expect(mockDb.whereIn).toHaveBeenCalledWith('id', expect.anything());
-	expect(mockDb.andWhere).toHaveBeenCalledWith('enabled', '=', true);
-
-	expect(mockDb.count).toBeCalledTimes(2);
-	expect(mockDb.from).toBeCalledTimes(3);
-	expect(mockDb.where).toBeCalledTimes(1);
-	expect(mockDb.distinct).toBeCalledTimes(1);
-	expect(mockDb.whereIn).toBeCalledTimes(1);
-	expect(mockDb.andWhere).toBeCalledTimes(1);
-
-	expect(result).toEqual({ activeTotal: 15, activeBundles: 2 });
+	expect(result).toEqual({ totalEnabled: mockCountEnabled - mockCountBundles } satisfies ExtensionCount);
 });
