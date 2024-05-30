83d0e8b1377b5ce9197275a3c8c02b0bd42ed122
Hannes Küttner <kuettner.hannes@gmail.com>
2024-05-30 22:00:36 +0200

    Update validateCollectionAccess test

--- api/src/permissions/modules/validate-access/lib/validate-collection-access.test.ts
+++ api/src/permissions/modules/validate-access/lib/validate-collection-access.test.ts
@@ -1,52 +1,63 @@
 import type { Accountability, Permission } from '@directus/types';
 import { beforeEach, expect, test, vi } from 'vitest';
-import type { AccessService } from '../../../../services/access.js';
-import type { PermissionsService } from '../../../../services/index.js';
 import { fetchPermissions } from '../../../lib/fetch-permissions.js';
+import { fetchPolicies } from '../../../lib/fetch-policies.js';
+import type { Context } from '../../../types.js';
 import { validateCollectionAccess } from './validate-collection-access.js';
 
 vi.mock('../../../lib/fetch-permissions.js');
 vi.mock('../../../lib/fetch-policies.js');
 
-let accessService: AccessService;
-let permissionsService: PermissionsService;
-
 beforeEach(() => {
 	vi.clearAllMocks();
 
-	accessService = {
-		readByQuery: vi.fn().mockResolvedValue([]),
-	} as unknown as AccessService;
-
-	permissionsService = {
-		readByQuery: vi.fn().mockResolvedValue([]),
-	} as unknown as PermissionsService;
+	vi.mocked(fetchPolicies).mockResolvedValue([]);
 });
 
 test('Returns false if permissions is an empty array', async () => {
 	vi.mocked(fetchPermissions).mockResolvedValue([]);
 
+	const accountability = {} as unknown as Accountability;
+
 	const res = await validateCollectionAccess(
-		accessService,
-		permissionsService,
-		{} as unknown as Accountability,
-		'read',
-		'collection-a',
+		{ accountability, action: 'read', collection: 'collection-a' },
+		{} as unknown as Context,
 	);
 
 	expect(res).toBe(false);
+
+	expect(fetchPolicies).toHaveBeenCalledWith(accountability, {});
+
+	expect(fetchPermissions).toHaveBeenCalledWith(
+		{
+			action: 'read',
+			policies: [],
+			collections: ['collection-a'],
+		},
+		{},
+	);
 });
 
 test('Returns true if permissions exist', async () => {
 	vi.mocked(fetchPermissions).mockResolvedValue([{} as unknown as Permission]);
 
+	const accountability = {} as unknown as Accountability;
+
 	const res = await validateCollectionAccess(
-		accessService,
-		permissionsService,
-		{} as unknown as Accountability,
-		'read',
-		'collection-a',
+		{ accountability, action: 'read', collection: 'collection-a' },
+		{} as unknown as Context,
 	);
 
 	expect(res).toBe(true);
+
+	expect(fetchPolicies).toHaveBeenCalledWith(accountability, {});
+
+	expect(fetchPermissions).toHaveBeenCalledWith(
+		{
+			action: 'read',
+			policies: [],
+			collections: ['collection-a'],
+		},
+		{},
+	);
 });
