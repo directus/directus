708eeb561da99e6f0efc8e6cbb94f90c33e621dd
ian <licitdev@gmail.com>
2024-05-31 14:41:22 +0800

    Update extension count test

--- api/src/telemetry/utils/get-extension-count.test.ts
+++ api/src/telemetry/utils/get-extension-count.test.ts
@@ -1,39 +1,91 @@
+import { randomUUID } from 'crypto';
 import { Knex } from 'knex';
 import { afterEach, beforeEach, expect, test, vi } from 'vitest';
+import { ExtensionsService } from '../../services/extensions.js';
 import { getExtensionCount, type ExtensionCount } from './get-extension-count.js';
 
-let mockQueryEnabled: { count?: string | number } | undefined;
-let mockQueryBundles: { count?: string | number } | undefined;
-const mockCountEnabled = 4;
-const mockCountBundles = 2;
+vi.mock('../../utils/get-schema.js');
+vi.mock('../../services/extensions.js');
+
 let mockDb: Knex;
 
+function generateParentBundleExtension(enabled: boolean) {
+	return {
+		id: randomUUID(),
+		meta: {
+			enabled,
+		},
+		schema: {
+			type: 'bundle',
+		},
+	};
+}
+
+function generateNestedBundleExtension(enabled: boolean) {
+	const bundle = randomUUID();
+
+	return {
+		id: randomUUID(),
+		bundle,
+		meta: {
+			enabled,
+			bundle,
+		},
+		schema: {
+			type: 'endpoint',
+		},
+	};
+}
+
+function generateExtension(enabled: boolean) {
+	return {
+		id: randomUUID(),
+		meta: {
+			enabled,
+		},
+		schema: {
+			type: 'endpoint',
+		},
+	};
+}
+
+function generateMissingExtension(enabled: boolean) {
+	return {
+		id: randomUUID(),
+		meta: {
+			enabled,
+		},
+		schema: null,
+	};
+}
+
 beforeEach(() => {
-	mockQueryEnabled = {
-		count: mockCountEnabled,
-	};
-
-	mockQueryBundles = {
-		count: mockCountBundles,
-	};
-
-	mockDb = {
-		count: vi.fn().mockReturnThis(),
-		where: vi.fn().mockReturnThis(),
-		whereIn: vi.fn().mockReturnThis(),
-		andWhere: vi.fn().mockReturnThis(),
-		distinct: vi.fn().mockReturnThis(),
-		from: vi.fn().mockReturnThis(),
-		first: vi.fn().mockResolvedValueOnce(mockQueryEnabled).mockResolvedValueOnce(mockQueryBundles),
-	} as unknown as Knex;
+	vi.mocked(ExtensionsService.prototype.readAll).mockResolvedValue([
+		// Normal extensions
+		generateExtension(true),
+		generateExtension(true),
+		generateExtension(false),
+		// Parent bundle extension
+		generateParentBundleExtension(true),
+		generateParentBundleExtension(true),
+		generateParentBundleExtension(false),
+		// Extensions within a bundle
+		generateNestedBundleExtension(true),
+		generateNestedBundleExtension(true),
+		generateNestedBundleExtension(false),
+		// Missing extensions
+		generateMissingExtension(true),
+		generateMissingExtension(true),
+		generateMissingExtension(false),
+	] as any);
 });
 
 afterEach(() => {
 	vi.clearAllMocks();
 });
 
-test('Subtracts bundle parents from total extensions', async () => {
+test('Calculates count of enabled extensions', async () => {
 	const result = await getExtensionCount(mockDb);
 
-	expect(result).toEqual({ totalEnabled: mockCountEnabled - mockCountBundles } satisfies ExtensionCount);
+	expect(result).toEqual({ totalEnabled: 4 } satisfies ExtensionCount);
 });
