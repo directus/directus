89c656236f302f43e39e1b246118939715551f44
Nicola Krumschmidt <nicola.krumschmidt@freenet.de>
2024-06-05 16:26:55 +0200

    Directly await schema id query

--- packages/schema/src/dialects/mssql.ts
+++ packages/schema/src/dialects/mssql.ts
@@ -275,9 +275,8 @@ export default class MSSQL implements SchemaInspector {
 	async columnInfo(table?: string, column?: string) {
 		const dbName = this.knex.client.database();
 
-		const schemaIdQuery = this.knex.select('schema_id').from('sys.schemas').where({ name: this.schema }).first();
+		const schemaIdResult = await this.knex.select('schema_id').from('sys.schemas').where({ name: this.schema }).first();
 
-		const schemaIdResult = await schemaIdQuery;
 		const schemaId = schemaIdResult.schema_id;
 
 		if (!schemaId) {
