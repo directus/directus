f7a72028e639390b60c6687130643460b3a3932d
Hannes Küttner <kuettner.hannes@gmail.com>
2024-06-12 19:57:21 +0200

    Add validation against non-existent fields and collections to `validatePath`

--- api/src/permissions/modules/process-ast/utils/validate-path.ts
+++ api/src/permissions/modules/process-ast/utils/validate-path.ts
@@ -1,14 +1,19 @@
 import { ForbiddenError } from '@directus/errors';
-import type { Permission } from '@directus/types';
+import type { Permission, SchemaOverview } from '@directus/types';
 
-// TODO add validation against schema object for non-existing fields and collections
-
-export function validatePath(path: string, permissions: Permission[], collection: string, fields: Set<string>) {
+export function validatePath(
+	path: string,
+	permissions: Permission[],
+	collection: string,
+	fields: Set<string>,
+	schema: SchemaOverview,
+) {
 	const pathSuffix = path === '' ? 'root' : `"${path}"`;
 
 	const permissionsForCollection = permissions.filter((permission) => permission.collection === collection);
+	const collectionInfo = schema.collections[collection];
 
-	if (permissionsForCollection.length === 0) {
+	if (collectionInfo === undefined || permissionsForCollection.length === 0) {
 		throw new ForbiddenError({
 			reason: `You don't have permission to access collection "${collection}" or it does not exist. Queried in ${pathSuffix}.`,
 		});
@@ -26,17 +31,20 @@ export function validatePath(path: string, permissions: Permission[], collection
 			allowedFields.add(field);
 
 			if (field === '*') {
-				// Exit early if all fields are allowed
-				return;
+				break;
 			}
 		}
 	}
 
 	const requestedFields = Array.from(fields);
-	const forbiddenFields = requestedFields.filter((field) => allowedFields.has(field) === false);
+	const nonExistentFields = requestedFields.filter((field) => collectionInfo.fields[field] === undefined);
 
-	if (forbiddenFields.length > 0) {
-		const fieldStr = forbiddenFields.map((field) => `"${field}"`).join(', ');
+	const forbiddenFields = allowedFields.has('*')
+		? []
+		: requestedFields.filter((field) => allowedFields.has(field) === false);
+
+	if (forbiddenFields.length > 0 || nonExistentFields.length > 0) {
+		const fieldStr = [...forbiddenFields, ...nonExistentFields].map((field) => `"${field}"`).join(', ');
 
 		throw new ForbiddenError({
 			reason:
