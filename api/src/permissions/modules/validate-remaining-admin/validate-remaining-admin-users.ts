8426f5df82bdd6d902749dbe247787dd68a73da2
Hannes Küttner <kuettner.hannes@gmail.com>
2024-06-13 09:10:55 +0200

    Clean up option type

--- api/src/permissions/modules/validate-remaining-admin/validate-remaining-admin-users.ts
+++ api/src/permissions/modules/validate-remaining-admin/validate-remaining-admin-users.ts
@@ -1,14 +1,9 @@
-import type { PrimaryKey } from '@directus/types';
-import { fetchUserCount } from '../../../utils/fetch-user-count/fetch-user-count.js';
+import { fetchUserCount, type FetchUserCountOptions } from '../../../utils/fetch-user-count/fetch-user-count.js';
 import type { Context } from '../../types.js';
 import { validateRemainingAdminCount } from './validate-remaining-admin-count.js';
 
-export interface ValidateRemainingAdminUsersOptions {
-	excludeAccessRows?: PrimaryKey[];
-	excludePolicies?: PrimaryKey[];
-	excludeUsers?: PrimaryKey[];
-	excludeRoles?: PrimaryKey[];
-}
+export interface ValidateRemainingAdminUsersOptions
+	extends Pick<FetchUserCountOptions, 'excludeAccessRows' | 'excludePolicies' | 'excludeUsers' | 'excludeRoles'> {}
 
 export async function validateRemainingAdminUsers(options: ValidateRemainingAdminUsersOptions, context: Context) {
 	const { admin } = await fetchUserCount({
