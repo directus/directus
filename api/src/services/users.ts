09313b6a634bbf48f2e8b2adc52f66153c7a6d96
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-06-14 16:41:45 +0200

    Ensure service integrity (#22738)
    Co-authored-by: Rijk van Zanten <rijkvanzanten@me.com>
    Co-authored-by: ian <licitdev@gmail.com>

--- api/src/services/users.ts
+++ api/src/services/users.ts
@@ -1,11 +1,11 @@
 import { useEnv } from '@directus/env';
 import { ForbiddenError, InvalidPayloadError, RecordNotUniqueError, UnprocessableContentError } from '@directus/errors';
-import type { Item, PrimaryKey, Query, RegisterUserInput, User } from '@directus/types';
+import type { Item, PrimaryKey, RegisterUserInput, User } from '@directus/types';
 import { getSimpleHash, toArray, toBoolean, validatePayload } from '@directus/utils';
 import { FailedValidationError, joiValidationErrorItemToErrorExtensions } from '@directus/validation';
 import Joi from 'joi';
 import jwt from 'jsonwebtoken';
-import { cloneDeep, isEmpty, mergeWith } from 'lodash-es';
+import { isEmpty, mergeWith } from 'lodash-es';
 import { performance } from 'perf_hooks';
 import getDatabase from '../database/index.js';
 import { useLogger } from '../logger.js';
@@ -19,7 +19,6 @@ import { getSecret } from '../utils/get-secret.js';
 import isUrlAllowed from '../utils/is-url-allowed.js';
 import { verifyJWT } from '../utils/jwt.js';
 import { stall } from '../utils/stall.js';
-import { transaction } from '../utils/transaction.js';
 import { Url } from '../utils/url.js';
 import { ItemsService } from './items.js';
 import { MailService } from './mail/index.js';
@@ -196,8 +195,43 @@ export class UsersService extends ItemsService {
 	 * Create a new user
 	 */
 	override async createOne(data: Partial<Item>, opts?: MutationOptions): Promise<PrimaryKey> {
-		const result = await this.createMany([data], opts);
-		return result[0]!;
+		try {
+			if (data['email']) {
+				this.validateEmail(data['email']);
+				await this.checkUniqueEmails([data['email']]);
+			}
+
+			if (data['password']) {
+				await this.checkPasswordPolicy([data['password']]);
+			}
+
+			if (shouldCheckUserLimits() && data['role']) {
+				const increasedCounts: AccessTypeCount = {
+					admin: 0,
+					app: 0,
+					api: 0,
+				};
+
+				if (typeof data['role'] === 'object') {
+					if ('admin_access' in data['role'] && data['role']['admin_access'] === true) {
+						increasedCounts.admin++;
+					} else if ('app_access' in data['role'] && data['role']['app_access'] === true) {
+						increasedCounts.app++;
+					} else {
+						increasedCounts.api++;
+					}
+				} else {
+					const existingRoleCounts = await getRoleCountsByRoles(this.knex, [data['role']]);
+					mergeWith(increasedCounts, existingRoleCounts, (x, y) => x + y);
+				}
+
+				await checkIncreasedUserLimits(this.knex, increasedCounts);
+			}
+		} catch (err: any) {
+			(opts || (opts = {})).preMutationError = err;
+		}
+
+		return await super.createOne(data, opts);
 	}
 
 	/**
@@ -252,45 +286,6 @@ export class UsersService extends ItemsService {
 		}
 
 		return await super.createMany(data, opts);
-	}
-
-	/**
-	 * Update many users by query
-	 */
-	override async updateByQuery(query: Query, data: Partial<Item>, opts?: MutationOptions): Promise<PrimaryKey[]> {
-		const keys = await this.getKeysByQuery(query);
-		return keys.length ? await this.updateMany(keys, data, opts) : [];
-	}
-
-	/**
-	 * Update a single user by primary key
-	 */
-	override async updateOne(key: PrimaryKey, data: Partial<Item>, opts?: MutationOptions): Promise<PrimaryKey> {
-		await this.updateMany([key], data, opts);
-		return key;
-	}
-
-	override async updateBatch(data: Partial<Item>[], opts: MutationOptions = {}): Promise<PrimaryKey[]> {
-		if (!opts.mutationTracker) opts.mutationTracker = this.createMutationTracker();
-
-		const primaryKeyField = this.schema.collections[this.collection]!.primary;
-
-		const keys: PrimaryKey[] = [];
-
-		await transaction(this.knex, async (trx) => {
-			const service = new UsersService({
-				accountability: this.accountability,
-				knex: trx,
-				schema: this.schema,
-			});
-
-			for (const item of data) {
-				if (!item[primaryKeyField]) throw new InvalidPayloadError({ reason: `User in update misses primary key` });
-				keys.push(await service.updateOne(item[primaryKeyField]!, item, opts));
-			}
-		});
-
-		return keys;
 	}
 
 	/**
@@ -403,14 +398,6 @@ export class UsersService extends ItemsService {
 	}
 
 	/**
-	 * Delete a single user by primary key
-	 */
-	override async deleteOne(key: PrimaryKey, opts?: MutationOptions): Promise<PrimaryKey> {
-		await this.deleteMany([key], opts);
-		return key;
-	}
-
-	/**
 	 * Delete multiple users by primary key
 	 */
 	override async deleteMany(keys: PrimaryKey[], opts?: MutationOptions): Promise<PrimaryKey[]> {
@@ -426,25 +413,6 @@ export class UsersService extends ItemsService {
 
 		await super.deleteMany(keys, opts);
 		return keys;
-	}
-
-	override async deleteByQuery(query: Query, opts?: MutationOptions): Promise<PrimaryKey[]> {
-		const primaryKeyField = this.schema.collections[this.collection]!.primary;
-		const readQuery = cloneDeep(query);
-		readQuery.fields = [primaryKeyField];
-
-		// Not authenticated:
-		const itemsService = new ItemsService(this.collection, {
-			knex: this.knex,
-			schema: this.schema,
-		});
-
-		const itemsToDelete = await itemsService.readByQuery(readQuery);
-		const keys: PrimaryKey[] = itemsToDelete.map((item: Item) => item[primaryKeyField]);
-
-		if (keys.length === 0) return [];
-
-		return await this.deleteMany(keys, opts);
 	}
 
 	async inviteUser(email: string | string[], role: string, url: string | null, subject?: string | null): Promise<void> {
