3e7c198daf9d11ca5946595d5fcae627c28f2af2
Hannes Küttner <4376726+hanneskuettner@users.noreply.github.com>
2024-06-14 17:27:22 +0200

    Update api/src/permissions/modules/validate-remaining-admin/validate-remaining-admin-count.ts
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>

--- api/src/permissions/modules/validate-remaining-admin/validate-remaining-admin-count.ts
+++ api/src/permissions/modules/validate-remaining-admin/validate-remaining-admin-count.ts
@@ -3,7 +3,7 @@ import { UnprocessableContentError } from '@directus/errors';
 export function validateRemainingAdminCount(count: number) {
 	if (count <= 0) {
 		throw new UnprocessableContentError({
-			reason: `Doing this would remove the last admin user from the system`,
+			reason: `Cannot remove the last admin user from the system`,
 		});
 	}
 }
