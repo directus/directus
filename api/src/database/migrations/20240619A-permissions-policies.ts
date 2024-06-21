0c6bc8f28781da5f403ab908b7b474d44ead6c55
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-06-21 09:17:03 +0200

    Fix policies migration for public role permissions (#22811)
    Co-authored-by: Hannes Küttner <4376726+hanneskuettner@users.noreply.github.com>

--- api/src/database/migrations/20240619A-permissions-policies.ts
+++ api/src/database/migrations/20240619A-permissions-policies.ts
@@ -69,7 +69,7 @@ export async function up(knex: Knex) {
 	// Link permissions to policies instead of roles
 
 	await knex.schema.alterTable('directus_permissions', (table) => {
-		table.uuid('policy').notNullable().references('directus_policies.id').onDelete('CASCADE');
+		table.uuid('policy').references('directus_policies.id').onDelete('CASCADE');
 		// Drop the foreign key constraint here in order to update `null` role to public policy ID
 		table.dropForeign('role');
 	});
@@ -86,6 +86,7 @@ export async function up(knex: Knex) {
 
 	await knex.schema.alterTable('directus_permissions', (table) => {
 		table.dropColumns('role');
+		table.dropNullable('policy');
 	});
 
 	/////////////////////////////////////////////////////////////////////////////////////////////////
