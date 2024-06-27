8c8521cf2617bc5c2b09c89f61fcc4685c8741b5
Rijk van Zanten <rijkvanzanten@me.com>
2024-06-27 10:51:05 -0400

    Only set default filename disk if none is provided (#22848)

--- api/src/services/files.ts
+++ api/src/services/files.ts
@@ -86,7 +86,7 @@ export class FilesService extends ItemsService {
 			path.extname(payload.filename_download!) || (payload.type && '.' + extension(payload.type)) || '';
 
 		// The filename_disk is the FINAL filename on disk
-		payload.filename_disk = primaryKey + (fileExtension || '');
+		payload.filename_disk ||= primaryKey + (fileExtension || '');
 
 		// Temp filename is used for replacements
 		const tempFilenameDisk = 'temp_' + payload.filename_disk;
