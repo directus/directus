cbb0c4f54175e27f829740193fdad2d4e55768db
Aprilia <80976002+useEffects@users.noreply.github.com>
2024-07-03 18:26:33 +0530

    Prioritize access_token in searchparam over access_token in the cookies when using "strict" mode for websocket authentication (#22888)
    * changes
    
    * prioritize query token if present
    
    * prioritize cookie over handshake
    
    * Create moody-bees-pay.md
    
    * Update moody-bees-pay.md
    
    ---------
    
    Co-authored-by: Brainslug <tim@brainslug.nl>
    Co-authored-by: Brainslug <br41nslug@users.noreply.github.com>

--- api/src/websocket/controllers/base.ts
+++ api/src/websocket/controllers/base.ts
@@ -137,14 +137,14 @@ export default abstract class SocketController {
 		const context: UpgradeContext = { request, socket, head };
 		const sessionCookieName = env['SESSION_COOKIE_NAME'] as string;
 
-		if (cookies[sessionCookieName]) {
-			const token = cookies[sessionCookieName] as string;
+		if (this.authentication.mode === 'strict' || query['access_token']) {
+			const token = query['access_token'] as string;
 			await this.handleTokenUpgrade(context, token);
 			return;
 		}
 
-		if (this.authentication.mode === 'strict') {
-			const token = query['access_token'] as string;
+		if (cookies[sessionCookieName]) {
+			const token = cookies[sessionCookieName] as string;
 			await this.handleTokenUpgrade(context, token);
 			return;
 		}
