8398d50e51783729ad3cb3ca034387353be03155
Brainslug <tim@brainslug.nl>
2024-07-11 16:44:20 +0200

    fixed import

--- api/src/services/comments.ts
+++ api/src/services/comments.ts
@@ -2,7 +2,7 @@ import { useEnv } from '@directus/env';
 import { ErrorCode, InvalidPayloadError, isDirectusError } from '@directus/errors';
 import type { Accountability, Comment, PrimaryKey } from '@directus/types';
 import { uniq } from 'lodash-es';
-import { useLogger } from '../logger.js';
+import { useLogger } from '../logger/index.js';
 import type { AbstractServiceOptions, MutationOptions } from '../types/index.js';
 import { getPermissions } from '../utils/get-permissions.js';
 import { isValidUuid } from '../utils/is-valid-uuid.js';
