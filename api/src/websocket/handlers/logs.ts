7ff05831b1132341301bdb9564af1997a5029f8b
Brainslug <tim@brainslug.nl>
2024-07-15 18:02:31 +0200

    simplify logging handler

--- api/src/websocket/handlers/logs.ts
+++ api/src/websocket/handlers/logs.ts
@@ -1,17 +1,12 @@
-import { ErrorCode, ServiceUnavailableError } from '@directus/errors';
+import { ServiceUnavailableError } from '@directus/errors';
 import type { Bus } from '@directus/memory';
 import { useBus } from '../../bus/index.js';
-import emitter from '../../emitter.js';
 import { getLogsController, LogsController } from '../controllers/index.js';
-import { WebSocketError, handleWebSocketError } from '../errors.js';
-import { WebSocketLogsMessage } from '../messages.js';
-import type { LogsSubscription, WebSocketClient } from '../types.js';
-import { fmtMessage, getMessageType } from '../utils/message.js';
+import { fmtMessage } from '../utils/message.js';
 
 export class LogsHandler {
 	controller: LogsController;
 	messenger: Bus;
-	subscriptions: Set<LogsSubscription>;
 
 	constructor(controller?: LogsController) {
 		controller = controller ?? getLogsController();
@@ -22,86 +17,11 @@ export class LogsHandler {
 
 		this.controller = controller;
 		this.messenger = useBus();
-		this.subscriptions = new Set();
-
-		this.bindWebSocket();
 
 		this.messenger.subscribe('logs', (message: Record<string, any>) => {
-			this.subscriptions.forEach((subscription) => subscription.client.send(fmtMessage('logs', { data: message })));
+			// broadcast streaming log
+			const logMsg = fmtMessage('logs', { data: message });
+			this.controller.clients.forEach((client) => client.send(logMsg));
 		});
-	}
-
-	/**
-	 * Hook into websocket client lifecycle events
-	 */
-	bindWebSocket() {
-		// listen to incoming messages on the connected websockets
-		emitter.onAction('websocket.message', ({ client, message }) => {
-			if (!['subscribe_logs', 'unsubscribe_logs'].includes(getMessageType(message))) return;
-
-			try {
-				const parsedMessage = WebSocketLogsMessage.parse(message);
-
-				this.onMessage(client, parsedMessage).catch((error) => {
-					// this catch is required because the async onMessage function is not awaited
-					handleWebSocketError(client, error, 'logs');
-				});
-			} catch (error) {
-				handleWebSocketError(client, error, 'logs');
-			}
-		});
-
-		// unsubscribe when a connection drops
-		emitter.onAction('websocket.error', ({ client }) => this.unsubscribe(client));
-		emitter.onAction('websocket.close', ({ client }) => this.unsubscribe(client));
-	}
-
-	/**
-	 * Register a logs subscription
-	 * @param subscription
-	 */
-	subscribe(subscription: LogsSubscription) {
-		this.subscriptions.add(subscription);
-	}
-
-	/**
-	 * Remove a logs subscription
-	 * @param client WebSocketClient
-	 */
-	unsubscribe(client: WebSocketClient) {
-		for (const subscription of this.subscriptions) {
-			if (subscription.client === client) {
-				this.subscriptions.delete(subscription);
-			}
-		}
-	}
-
-	/**
-	 * Handle incoming (un)subscribe requests
-	 */
-	async onMessage(client: WebSocketClient, message: WebSocketLogsMessage) {
-		const accountability = client.accountability;
-
-		if (!accountability?.admin) {
-			throw new WebSocketError('logs', ErrorCode.Forbidden, `You don't have permission to access this.`);
-		}
-
-		if (message.type === 'subscribe_logs') {
-			try {
-				this.subscribe({ client });
-
-				client.send(fmtMessage('logs', { event: 'subscribe_logs' }));
-			} catch (err) {
-				handleWebSocketError(client, err, 'subscribe_logs');
-			}
-		} else if (message.type === 'unsubscribe_logs') {
-			try {
-				this.unsubscribe(client);
-
-				client.send(fmtMessage('logs', { event: 'unsubscribe_logs' }));
-			} catch (err) {
-				handleWebSocketError(client, err, 'unsubscribe_logs');
-			}
-		}
 	}
 }
