81791b86d1131a83a1a6c35442704226ef76162b
Brainslug <tim@brainslug.nl>
2024-07-15 18:03:24 +0200

    Added shutdown callback

--- api/src/server.ts
+++ api/src/server.ts
@@ -19,6 +19,7 @@ import {
 	createLogsController,
 	createSubscriptionController,
 	createWebSocketController,
+	getLogsController,
 	getSubscriptionController,
 	getWebSocketController,
 } from './websocket/controllers/index.js';
@@ -129,6 +130,7 @@ export async function createServer(): Promise<http.Server> {
 	async function onSignal() {
 		getSubscriptionController()?.terminate();
 		getWebSocketController()?.terminate();
+		getLogsController()?.terminate();
 
 		const database = getDatabase();
 		await database.destroy();
