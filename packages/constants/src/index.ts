9ad4cdbb11c63467d315e4d399320e39d6958ac9
ian <licitdev@gmail.com>
2024-07-16 00:08:15 +0800

    Expose allowed log levels

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -2,5 +2,6 @@ export * from './activity.js';
 export * from './fields.js';
 export * from './files.js';
 export * from './injection.js';
-export * from './regex.js';
+export * from './logs.js';
 export * from './number.js';
+export * from './regex.js';
---
+++ packages/constants/src/logs.ts
@@ -0,0 +1,8 @@
+export const LOG_LEVELS = {
+	trace: 10,
+	debug: 20,
+	info: 30,
+	warn: 40,
+	error: 50,
+	fatal: 60,
+} as const;
