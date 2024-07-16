a024ea8c4e15c8b21a58839d6488a5571ddb9600
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-07-16 13:56:29 +0200

    Enhance type casting

--- packages/schema/src/dialects/mssql.ts
+++ packages/schema/src/dialects/mssql.ts
@@ -283,7 +283,7 @@ export default class MSSQL implements SchemaInspector {
 			throw new Error(`Schema '${this.schema}' not found.`);
 		}
 
-		const dbResult: RawColumn[] | RawColumn = await this.knex.transaction(async (trx) => {
+		const dbResult = await this.knex.transaction<RawColumn[] | RawColumn>(async (trx) => {
 			await trx.raw(`IF OBJECT_ID('tempdb..##IndexInfo') IS NOT NULL DROP TABLE #IndexInfo;`);
 
 			await trx.raw(`
@@ -379,9 +379,7 @@ export default class MSSQL implements SchemaInspector {
 			return rawColumnToColumn(dbResult as RawColumn);
 		}
 
-		const records = dbResult as RawColumn[];
-
-		return records.map(rawColumnToColumn);
+		return (dbResult as RawColumn[]).map(rawColumnToColumn);
 	}
 
 	/**
