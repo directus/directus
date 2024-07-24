156bfcb5245f577acae1a4c8e86be8f5441e635b
Pascal Jufer <pascal-jufer@bluewin.ch>
2024-07-24 16:43:44 +0200

    Fix "Update Files Date Fields" migration for MySQL (#23094)
    * Fix "Update Files Date Fields" migration for MySQL
    
    * Remove unnecessary await

--- api/src/database/migrations/20240716A-update-files-date-fields.ts
+++ api/src/database/migrations/20240716A-update-files-date-fields.ts
@@ -1,9 +1,20 @@
 import type { Knex } from 'knex';
+import { getHelpers } from '../helpers/index.js';
 
 export async function up(knex: Knex): Promise<void> {
-	await knex.schema.alterTable('directus_files', (table) => {
-		table.renameColumn('uploaded_on', 'created_on');
-	});
+	const helper = getHelpers(knex).schema;
+	const isMysql = helper.isOneOfClients(['mysql']);
+
+	if (isMysql) {
+		// Knex creates invalid statement on MySQL, see https://github.com/knex/knex/issues/1888
+		await knex.schema.raw(
+			'ALTER TABLE `directus_files` CHANGE `uploaded_on` `created_on` TIMESTAMP NOT NULL DEFAULT current_timestamp();',
+		);
+	} else {
+		await knex.schema.alterTable('directus_files', (table) => {
+			table.renameColumn('uploaded_on', 'created_on');
+		});
+	}
 
 	await knex.schema.alterTable('directus_files', (table) => {
 		table.timestamp('uploaded_on');
@@ -17,7 +28,16 @@ export async function down(knex: Knex): Promise<void> {
 		table.dropColumn('uploaded_on');
 	});
 
-	await knex.schema.alterTable('directus_files', (table) => {
-		table.renameColumn('created_on', 'uploaded_on');
-	});
+	const helper = getHelpers(knex).schema;
+	const isMysql = helper.isOneOfClients(['mysql']);
+
+	if (isMysql) {
+		await knex.schema.raw(
+			'ALTER TABLE `directus_files` CHANGE `created_on` `uploaded_on` TIMESTAMP NOT NULL DEFAULT current_timestamp();',
+		);
+	} else {
+		await knex.schema.alterTable('directus_files', (table) => {
+			table.renameColumn('created_on', 'uploaded_on');
+		});
+	}
 }
