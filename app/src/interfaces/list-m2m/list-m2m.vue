0c092deedc6706d515e2ae8b2c3e6cd8f5c5c8cc
SP12893678 <36910625+SP12893678@users.noreply.github.com>
2024-08-08 01:41:50 +0800

    Remove extra div insdie ul element (#23116)

--- app/src/interfaces/list-m2m/list-m2m.vue
+++ app/src/interfaces/list-m2m/list-m2m.vue
@@ -522,13 +522,14 @@ const { createAllowed, updateAllowed, deleteAllowed, selectAllowed } = useRelati
 				/>
 			</template>
 
-			<v-list v-else>
+			<template v-else>
 				<v-notice v-if="displayItems.length === 0">
 					{{ t('no_items') }}
 				</v-notice>
 
 				<draggable
 					:model-value="displayItems"
+					tag="v-list"
 					item-key="id"
 					handle=".drag-handle"
 					:disabled="!allowDrag"
@@ -572,7 +573,7 @@ const { createAllowed, updateAllowed, deleteAllowed, selectAllowed } = useRelati
 						</v-list-item>
 					</template>
 				</draggable>
-			</v-list>
+			</template>
 
 			<div class="actions" :class="layout">
 				<template v-if="layout === LAYOUTS.TABLE">
