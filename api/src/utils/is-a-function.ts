fb8d863b24ddbf56798407c6edeabc08230a4f0c
Jan Arends <jan.arends@mailbox.org>
2024-08-14 19:13:35 +0200

    added comment

--- api/src/utils/is-a-function.ts
+++ api/src/utils/is-a-function.ts
@@ -1,5 +1,14 @@
 /**
- * Based on the patterns found in the 'uuid' and 'uuid-validate' npm packages, both of which are MIT licensed.
+ * Checks if a string looks like a function call.
+ *
+ * It basically returns true, if the string
+ * -  starts with a word character or an underscore
+ * -  followed by zero or more word characters, digits or underscores
+ * -  followed by an opening parenthesis
+ * -  followed by zero or more characters that are not a closing parenthesis or an opening parenthesis for nested function calls
+ * -  followed by a closing parenthesis
+ *
+ * Spaces are also allowed between the function name and the opening parenthesis, as well as between the closing parenthesis and the arguments.
  */
 export function valueIsAFunction(value: string): boolean {
 	const regex = /(?:^)([a-zA-Z_][a-zA-Z0-9_]*)\s*\(\s*(?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*\s*\)/g;
