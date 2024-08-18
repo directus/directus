28c5970c265e8b2397db075a3437e8627b0825b3
ian <licitdev@gmail.com>
2024-08-18 22:40:06 +0800

    Simplify migration and update date

--- api/src/database/migrations/20240818A-separate-comments.ts
+++ api/src/database/migrations/20240818A-separate-comments.ts
@@ -1,7 +1,6 @@
 import { Action } from '@directus/constants';
 import { processChunk } from '@directus/utils';
 import type { Knex } from 'knex';
-import { randomUUID } from 'node:crypto';
 
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.createTable('directus_comments', (table) => {
@@ -22,38 +21,6 @@ export async function up(knex: Knex): Promise<void> {
 		table.uuid('user_created').references('id').inTable('directus_users').onDelete('SET NULL');
 		// Cannot have two constraints from/to the same table, handled on API side
 		table.uuid('user_updated').references('id').inTable('directus_users');
-	});
-
-	const comments = await knex
-		.select('id', 'collection', 'item', 'comment', 'user', 'timestamp')
-		.from('directus_activity')
-		.whereNotNull('comment')
-		.orderBy('id');
-
-	await processChunk(comments, 100, async (commentChunk) => {
-		for (const comment of commentChunk) {
-			await knex.transaction(async (trx) => {
-				const newCommentId = randomUUID();
-
-				await trx('directus_comments').insert({
-					id: newCommentId,
-					collection: comment.collection,
-					item: comment.item,
-					comment: comment.comment,
-					user_created: comment.user,
-					date_created: comment.timestamp,
-				});
-
-				await trx('directus_activity')
-					.update({
-						action: Action.CREATE,
-						collection: 'directus_comments',
-						item: newCommentId,
-						comment: null,
-					})
-					.where('id', '=', comment.id);
-			});
-		}
 	});
 }
 
@@ -80,35 +47,16 @@ export async function down(knex: Knex): Promise<void> {
 						comment: comment.comment,
 					})
 					.where('id', '=', migratedRecords[0].id);
-
-				continue;
+			} else {
+				await knex('directus_activity').insert({
+					action: Action.COMMENT,
+					collection: comment.collection,
+					item: comment.item,
+					comment: comment.comment,
+					user: comment.user_created,
+					timestamp: comment.date_created,
+				});
 			}
-
-			const unmigratedRecords = await knex('directus_activity')
-				.select('id')
-				.where('collection', '=', comment.collection)
-				.andWhere('item', '=', comment.item)
-				.andWhere('action', '=', Action.COMMENT)
-				.limit(1);
-
-			if (unmigratedRecords[0]) {
-				await knex('directus_activity')
-					.update({
-						comment: comment.comment,
-					})
-					.where('id', '=', unmigratedRecords[0].id);
-
-				continue;
-			}
-
-			await knex('directus_activity').insert({
-				action: Action.COMMENT,
-				collection: comment.collection,
-				item: comment.item,
-				comment: comment.comment,
-				user: comment.user_created,
-				timestamp: comment.date_created,
-			});
 		}
 	});
 
