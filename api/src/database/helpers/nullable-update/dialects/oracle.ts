abbf07e7950015387bf22ba56a5b77901d99c69d
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2024-08-30 16:49:31 +0200

    update comment, autoformat

--- api/src/database/helpers/nullable-update/dialects/oracle.ts
+++ api/src/database/helpers/nullable-update/dialects/oracle.ts
@@ -1,12 +1,12 @@
-import type { Knex } from 'knex';
-import { NullableFieldUpdateHelper } from '../types.js';
 import type { Column } from '@directus/schema';
 import type { Field, RawField } from '@directus/types';
+import type { Knex } from 'knex';
+import { NullableFieldUpdateHelper } from '../types.js';
 
 /**
- * Oracle fail when overwriting the nullable option with same value.
+ * Oracle throws an error when overwriting the nullable option with same value.
  * Therefore we need to check if the nullable option has changed and only then apply it.
- * The default value can be set regardless auf the previous value.  @TODO double check
+ * The default value can be set regardless of the previous value.
  */
 export class NullableFieldUpdateHelperOracle extends NullableFieldUpdateHelper {
 	override updateNullableValue(column: Knex.ColumnBuilder, field: RawField | Field, existing: Column): void {
