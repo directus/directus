53664d81a8ed31f5ec2c0c296366ff4f05460c51
Shane Brass <shanebrass1618@gmail.com>
2024-09-03 15:28:38 -0600

    Removing unused import

--- packages/extensions-sdk/src/cli/types.ts
+++ packages/extensions-sdk/src/cli/types.ts
@@ -1,5 +1,5 @@
 import type { EXTENSION_LANGUAGES } from '@directus/extensions';
-import type { Plugin, RollupOptions, OutputOptions as RollupOutputOptions, RollupLog } from 'rollup';
+import type { Plugin, RollupOptions, OutputOptions as RollupOutputOptions } from 'rollup';
 
 export type Language = (typeof EXTENSION_LANGUAGES)[number];
 export type LanguageShort = 'js' | 'ts';
