50198aa1eed70efa586cbf9331ca142c44d9f319
Shane <shanebrass1618@gmail.com>
2024-10-15 07:09:18 -0600

    SDK: Throw errors when present in JSON responses (#22666)
    Co-authored-by: Rijk van Zanten <rijkvanzanten@me.com>
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>

---
+++ .changeset/strange-kangaroos-roll.md
@@ -0,0 +1,5 @@
+---
+'@directus/sdk': patch
+---
+
+Ensured failing GraphQL requests are properly handled by throwing an error and exposing `errors` and `data` information
--- sdk/src/utils/extract-data.ts
+++ sdk/src/utils/extract-data.ts
@@ -11,8 +11,11 @@ export async function extractData(response: unknown) {
 
 		if (type?.startsWith('application/json') || type?.startsWith('application/health+json')) {
 			const result = await response.json();
-			if (!response.ok) throw result;
+
+			if (!response.ok || 'errors' in result) throw result;
+
 			if ('data' in result) return result.data;
+
 			return result;
 		}
 
@@ -27,5 +30,10 @@ export async function extractData(response: unknown) {
 	}
 
 	// exception for alternatives like ofetch that don't return the Response object
-	return 'data' in response ? response.data : response;
+
+	if ('errors' in response) throw response;
+
+	if ('data' in response) return response.data;
+
+	return response;
 }
