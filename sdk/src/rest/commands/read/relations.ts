75080fa20b8a5dd27964862fd163fa8c275e11b9
Brainslug <br41nslug@users.noreply.github.com>
2024-10-18 19:29:23 +0200

    Fix SDK relation type (#23877)
    Co-authored-by: Pascal Jufer <pascal-jufer@bluewin.ch>

--- sdk/src/rest/commands/read/relations.ts
+++ sdk/src/rest/commands/read/relations.ts
@@ -1,18 +1,13 @@
 import type { DirectusRelation } from '../../../schema/relation.js';
-import type { ApplyQueryFields, Query } from '../../../types/index.js';
+import type { ApplyQueryFields } from '../../../types/index.js';
 import { throwIfEmpty } from '../../utils/index.js';
 import type { RestCommand } from '../../types.js';
 
-export type ReadRelationOutput<Schema, Item extends object = DirectusRelation<Schema>> = ApplyQueryFields<
-	Schema,
-	Item,
-	'*'
->;
+export type ReadRelationOutput<Schema> = ApplyQueryFields<Schema, DirectusRelation<Schema>, '*'>;
 
 /**
  * List all Relations that exist in Directus.
- * @param query The query parameters
- * @returns An array of up to limit Relation objects. If no items are available, data will be an empty array.
+ * @returns An array of Relation objects. If no items are available, data will be an empty array.
  */
 export const readRelations =
 	<Schema>(): RestCommand<ReadRelationOutput<Schema>[], Schema> =>
@@ -22,31 +17,30 @@ export const readRelations =
 	});
 
 /**
- * List an existing Relation by primary key.
+ * List all Relations of a collection.
  * @param collection The collection
- * @returns Returns a Relation object if a valid primary key was provided.
- * @throws Will throw if collection is empty
+ * @returns Returns an array of Relation objects if a valid collection name was provided.
  */
 export const readRelationByCollection =
-	<Schema>(collection: DirectusRelation<Schema>['collection']): RestCommand<ReadRelationOutput<Schema>, Schema> =>
+	<Schema>(collection: DirectusRelation<Schema>['collection']): RestCommand<ReadRelationOutput<Schema>[], Schema> =>
 	() => ({
 		path: `/relations/${collection}`,
 		method: 'GET',
 	});
 
 /**
- * List an existing Relation by primary key.
+ * List an existing Relation by collection and field name.
  * @param collection The collection
  * @param field The field
- * @returns Returns a Relation object if a valid primary key was provided.
+ * @returns Returns a Relation object if a valid collection and field name was provided.
  * @throws Will throw if collection is empty
  * @throws Will throw if field is empty
  */
 export const readRelation =
-	<Schema, const TQuery extends Query<Schema, DirectusRelation<Schema>>>(
+	<Schema>(
 		collection: DirectusRelation<Schema>['collection'],
 		field: DirectusRelation<Schema>['field'],
-	): RestCommand<ReadRelationOutput<Schema, TQuery>, Schema> =>
+	): RestCommand<ReadRelationOutput<Schema>, Schema> =>
 	() => {
 		throwIfEmpty(collection, 'Collection cannot be empty');
 		throwIfEmpty(field, 'Field cannot be empty');
