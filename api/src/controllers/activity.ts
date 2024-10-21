70dda8438134c4b4452aad6acc7955e54758234d
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2024-10-21 09:03:17 -0400

    remove remaining comment code in activity

--- api/src/controllers/activity.ts
+++ api/src/controllers/activity.ts
@@ -3,7 +3,6 @@ import { respond } from '../middleware/respond.js';
 import useCollection from '../middleware/use-collection.js';
 import { validateBatch } from '../middleware/validate-batch.js';
 import { ActivityService } from '../services/activity.js';
-import { CommentsService } from '../services/comments.js';
 import { MetaService } from '../services/meta.js';
 import asyncHandler from '../utils/async-handler.js';
 
@@ -23,40 +22,16 @@ const readHandler = asyncHandler(async (req, res, next) => {
 	});
 
 	let result;
-	let isComment;
 
 	if (req.singleton) {
 		result = await service.readSingleton(req.sanitizedQuery);
 	} else if (req.body.keys) {
 		result = await service.readMany(req.body.keys, req.sanitizedQuery);
 	} else {
-		const sanitizedFilter = req.sanitizedQuery.filter;
-
-		if (
-			sanitizedFilter &&
-			'_and' in sanitizedFilter &&
-			Array.isArray(sanitizedFilter['_and']) &&
-			sanitizedFilter['_and'].find(
-				(andItem) => 'action' in andItem && '_eq' in andItem['action'] && andItem['action']['_eq'] === 'comment',
-			)
-		) {
-			const commentsService = new CommentsService({
-				accountability: req.accountability,
-				schema: req.schema,
-				serviceOrigin: 'activity',
-			});
-
-			result = await commentsService.readByQuery(req.sanitizedQuery);
-			isComment = true;
-		} else {
-			result = await service.readByQuery(req.sanitizedQuery);
-		}
+		result = await service.readByQuery(req.sanitizedQuery);
 	}
 
-	const meta = await metaService.getMetaForQuery(
-		isComment ? 'directus_comments' : 'directus_activity',
-		req.sanitizedQuery,
-	);
+	const meta = await metaService.getMetaForQuery('directus_activity', req.sanitizedQuery);
 
 	res.locals['payload'] = {
 		data: result,
