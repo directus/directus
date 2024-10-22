3517a19f55f0bef79dd4e8ecaed80ca84041966a
Hannes Küttner <kuettner.hannes@gmail.com>
2024-10-22 16:55:18 +0200

    Mock out `fetchPermittedAstRootFields` instead of `runAst` since the internals of `validateItemAccess` changed

--- api/src/permissions/modules/validate-access/lib/validate-item-access.test.ts
+++ api/src/permissions/modules/validate-access/lib/validate-item-access.test.ts
@@ -1,13 +1,14 @@
 import type { Accountability, SchemaOverview } from '@directus/types';
 import { beforeEach, expect, test, vi } from 'vitest';
 import { getAstFromQuery } from '../../../../database/get-ast-from-query/get-ast-from-query.js';
-import { runAst } from '../../../../database/run-ast/run-ast.js';
+import { fetchPermittedAstRootFields } from '../../../../database/run-ast/modules/fetch-permitted-ast-root-fields.js';
 import type { AST } from '../../../../types/ast.js';
 import type { Context } from '../../../types.js';
 import { processAst } from '../../process-ast/process-ast.js';
 import { validateItemAccess } from './validate-item-access.js';
 
 vi.mock('../../../../database/get-ast-from-query/get-ast-from-query.js');
+vi.mock('../../../../database/run-ast/modules/fetch-permitted-ast-root-fields.js');
 vi.mock('../../../../database/run-ast/run-ast.js');
 vi.mock('../../process-ast/process-ast.js');
 
@@ -32,7 +33,7 @@ test('Queries the database', async () => {
 	const ast = { query: {} } as unknown as AST;
 
 	vi.mocked(getAstFromQuery).mockResolvedValue(ast);
-	vi.mocked(runAst).mockResolvedValue([]);
+	vi.mocked(fetchPermittedAstRootFields).mockResolvedValue([]);
 
 	await expect(
 		validateItemAccess({ accountability: acc, action: 'read', collection: 'collection-a', primaryKeys: [1] }, {
@@ -63,7 +64,7 @@ test('Queries the database', async () => {
 		{ schema },
 	);
 
-	expect(runAst).toHaveBeenCalledWith(
+	expect(fetchPermittedAstRootFields).toHaveBeenCalledWith(
 		{
 			query: {
 				filter: {
@@ -73,9 +74,11 @@ test('Queries the database', async () => {
 				},
 			},
 		},
-		schema,
-		acc,
-		{ knex: undefined },
+		{
+			schema,
+			accountability: acc,
+			knex: undefined,
+		},
 	);
 });
 
@@ -83,7 +86,7 @@ test('Returns false if no items are returned', async () => {
 	const schema = { collections: { 'collection-a': { primary: 'field-a' } } } as unknown as SchemaOverview;
 	const acc = {} as unknown as Accountability;
 
-	vi.mocked(runAst).mockResolvedValue([]);
+	vi.mocked(fetchPermittedAstRootFields).mockResolvedValue([]);
 
 	await expect(
 		validateItemAccess({ accountability: acc, action: 'read', collection: 'collection-a', primaryKeys: [1] }, {
@@ -96,7 +99,7 @@ test('Returns true the number of returned items matches the number of requested 
 	const schema = { collections: { 'collection-a': { primary: 'field-a' } } } as unknown as SchemaOverview;
 	const acc = {} as unknown as Accountability;
 
-	vi.mocked(runAst).mockResolvedValue([{}, {}]);
+	vi.mocked(fetchPermittedAstRootFields).mockResolvedValue([{}, {}]);
 
 	await expect(
 		validateItemAccess({ accountability: acc, action: 'read', collection: 'collection-a', primaryKeys: [1, 2] }, {
