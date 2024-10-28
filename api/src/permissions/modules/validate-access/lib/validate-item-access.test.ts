2f855ee139cd8bae4097f1ae072aaa6c21690999
Hannes Küttner <kuettner.hannes@gmail.com>
2024-10-28 15:41:06 +0100

    Fix test

--- api/src/permissions/modules/validate-access/lib/validate-item-access.test.ts
+++ api/src/permissions/modules/validate-access/lib/validate-item-access.test.ts
@@ -1,8 +1,6 @@
 import type { Accountability, SchemaOverview } from '@directus/types';
 import { beforeEach, expect, test, vi } from 'vitest';
-import { getAstFromQuery } from '../../../../database/get-ast-from-query/get-ast-from-query.js';
 import { fetchPermittedAstRootFields } from '../../../../database/run-ast/modules/fetch-permitted-ast-root-fields.js';
-import type { AST } from '../../../../types/ast.js';
 import type { Context } from '../../../types.js';
 import { processAst } from '../../process-ast/process-ast.js';
 import { validateItemAccess } from './validate-item-access.js';
@@ -30,9 +28,7 @@ test('Throws error when primary key does not exist in given collection', async (
 test('Queries the database', async () => {
 	const schema = { collections: { 'collection-a': { primary: 'field-a' } } } as unknown as SchemaOverview;
 	const acc = {} as unknown as Accountability;
-	const ast = { query: {} } as unknown as AST;
 
-	vi.mocked(getAstFromQuery).mockResolvedValue(ast);
 	vi.mocked(fetchPermittedAstRootFields).mockResolvedValue([]);
 
 	await expect(
@@ -41,38 +37,36 @@ test('Queries the database', async () => {
 		} as Context),
 	).resolves.toBe(false);
 
-	expect(getAstFromQuery).toHaveBeenCalledWith(
-		{
-			accountability: acc,
-			action: 'read',
-			collection: 'collection-a',
-			query: {
-				fields: [],
-				limit: 1,
-			},
-		},
-		{ schema } as Context,
-	);
-
 	expect(processAst).toHaveBeenCalledWith(
 		{
 			accountability: acc,
 			action: 'read',
 			collection: 'collection-a',
 			primaryKeys: [1],
-			ast,
+			ast: {
+				type: 'root',
+				name: 'collection-a',
+				query: expect.objectContaining({ limit: 1 }),
+				children: [],
+				cases: [],
+			},
 		},
 		{ schema },
 	);
 
 	expect(fetchPermittedAstRootFields).toHaveBeenCalledWith(
 		{
+			type: 'root',
+			name: 'collection-a',
+			children: [],
+			cases: [],
 			query: {
 				filter: {
 					'field-a': {
 						_in: [1],
 					},
 				},
+				limit: 1,
 			},
 		},
 		{
