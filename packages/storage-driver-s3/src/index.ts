29bc3e7affcc5a02a23c6e2fd4cf9e05723ec14b
ian <licitdev@gmail.com>
2024-11-20 14:24:55 +0800

    Fix linting

--- packages/storage-driver-s3/src/index.ts
+++ packages/storage-driver-s3/src/index.ts
@@ -284,12 +284,12 @@ export class DriverS3 implements TusDriver {
 			...(context.metadata?.['contentType']
 				? {
 						ContentType: context.metadata['contentType'],
-					}
+				  }
 				: {}),
 			...(context.metadata?.['cacheControl']
 				? {
 						CacheControl: context.metadata['cacheControl'],
-					}
+				  }
 				: {}),
 		});
 
