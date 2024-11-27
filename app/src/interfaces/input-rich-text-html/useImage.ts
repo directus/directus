040585653230e1adf9fdc63b6ba0e61dfe83f786
Matthew Rumery <mrumery@lucidchart.com>
2024-11-27 11:20:42 -0700

    chore: remove unused imports

--- app/src/interfaces/input-rich-text-html/useImage.ts
+++ app/src/interfaces/input-rich-text-html/useImage.ts
@@ -1,8 +1,6 @@
 import { i18n } from '@/lang';
 import { addQueryToPath } from '@/utils/add-query-to-path';
 import { getPublicURL } from '@/utils/get-root-path';
-import { readableMimeType } from '@/utils/readable-mime-type';
-import mime from 'mime/lite';
 import { Ref, ref, watch } from 'vue';
 import { SettingsStorageAssetPreset, File } from '@directus/types';
 
