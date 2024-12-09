82366e0d3b19f284a636c1bb6c591f0c7e3d1058
Alex Gaillard <alex@directus.io>
2024-12-09 16:48:03 -0500

    Uppercase shortcut key

--- app/src/interfaces/input-rich-text-html/input-rich-text-html.vue
+++ app/src/interfaces/input-rich-text-html/input-rich-text-html.vue
@@ -260,7 +260,7 @@ function setupContentWatcher() {
 function setup(editor: any) {
 	editorRef.value = editor;
 
-	const linkShortcut = 'meta+k';
+	const linkShortcut = 'meta+K';
 
 	editor.ui.registry.addToggleButton('customImage', imageButton);
 	editor.ui.registry.addToggleButton('customMedia', mediaButton);
