be86221588361fe1b7720ab45530a9f0dbfff77f
Brainslug <tim@brainslug.nl>
2024-12-12 14:19:57 +0100

    Added the access table schema

---
+++ sdk/src/schema/access.ts
@@ -0,0 +1,15 @@
+import type { MergeCoreCollection } from '../index.js';
+import type { DirectusRole } from './role.js';
+import type { DirectusUser } from './user.js';
+
+export type DirectusAccess<Schema = any> = MergeCoreCollection<
+	Schema,
+	'directus_access',
+	{
+		id: string; // uuid
+		role: string | DirectusRole<Schema>;
+		user: string | DirectusUser<Schema>;
+		policy: string | DirectusRole<Schema>;
+		sort: number;
+	}
+>;
