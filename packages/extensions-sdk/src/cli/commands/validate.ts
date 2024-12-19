a6d4734799e6bdccd509bccda89ebd303332775f
Mike Elsmore <mike@directus.io>
2024-12-19 16:14:37 +0000

    First pass Extensions SDK CLI validate command

---
+++ packages/extensions-sdk/src/cli/commands/validate.ts
@@ -0,0 +1,102 @@
+import { EXTENSION_PKG_KEY, EXTENSION_TYPES } from '@directus/extensions';
+import chalk from 'chalk';
+import fse from 'fs-extra';
+import ora from 'ora';
+import { log } from '../utils/logger.js';
+
+export default async function validate(): Promise<void> {
+  const spinner = ora(chalk.bold('Validating Directus extension...')).start();
+
+  try {
+    await Promise.all([checkBuiltCode(), checkReadMe(), checkLicense(), checkDirectusConfig()]);
+
+    spinner.succeed(chalk.bold('Extension is valid'));
+  } catch (error) {
+    spinner.fail(chalk.bold('Failed validation'));
+    log(String(error), 'error');
+  }
+}
+
+async function checkBuiltCode() {
+  const spinner = ora('Check for /dist').start();
+
+  if (!(await fse.pathExists(`${process.cwd()}/dist`))) {
+    spinner.fail();
+    return Promise.reject('No /dist directory');
+  }
+
+  spinner.succeed('Valid /dist');
+  return true;
+}
+
+async function checkReadMe() {
+  const spinner = ora('Check for README').start();
+
+  if (!(await fse.pathExists(`${process.cwd()}/README.md`))) {
+    spinner.fail();
+    return Promise.reject('No README.md');
+  }
+
+  spinner.succeed('Valid README');
+  return true;
+}
+
+async function checkLicense() {
+  const spinner = ora('Check for LICENSE').start();
+
+  if (!(await fse.pathExists(`${process.cwd()}/LICENSE`))) {
+    spinner.fail();
+    return Promise.reject('No LICENSE file');
+  }
+
+  spinner.succeed('Valid LICENSE');
+  return true;
+}
+
+async function checkDirectusConfig() {
+  const spinner = ora(`Checking ${EXTENSION_PKG_KEY} Object`).start();
+
+  spinner.text = 'Checking package file exists';
+
+  if (!(await fse.pathExists(`${process.cwd()}/package.json`))) {
+    spinner.fail();
+    return Promise.reject('No package.json');
+  }
+
+  const packageFile = await fse.readJson(`${process.cwd()}/package.json`);
+
+  spinner.text = `Checking ${EXTENSION_PKG_KEY} is present`;
+
+  if (!packageFile[EXTENSION_PKG_KEY]) {
+    spinner.fail();
+    return Promise.reject('Directus object not found');
+  }
+
+  const { type, path, host } = packageFile[EXTENSION_PKG_KEY];
+
+  spinner.text = `Checking extension type`;
+
+  if (!EXTENSION_TYPES.includes(type)) {
+    spinner.fail();
+    return Promise.reject(`Invalid Directus Extension Type: ${type}`);
+  }
+
+  spinner.text = `Checking extension path`;
+
+  if (!(await fse.pathExists(`${process.cwd()}/${path}`))) {
+    spinner.fail();
+    return Promise.reject(`Extension path ${path} invalid`);
+  }
+
+  spinner.text = 'Checking for valid Directus host version';
+
+  const regex = new RegExp(/^\^?[1-9]\d*(\.[1-9]\d*)*/);
+
+  if (!regex.test(host)) {
+    spinner.fail();
+    return Promise.reject(`${host} not a valid Directus version`);
+  }
+
+  spinner.succeed(`Valid ${EXTENSION_PKG_KEY} Object`);
+  return true;
+}
--- packages/extensions-sdk/src/cli/run.ts
+++ packages/extensions-sdk/src/cli/run.ts
@@ -3,6 +3,7 @@ import add from './commands/add.js';
 import build from './commands/build.js';
 import create from './commands/create.js';
 import link from './commands/link.js';
+import validate from './commands/validate.js';
 import getSdkVersion from './utils/get-sdk-version.js';
 
 const program = new Command();
@@ -11,36 +12,41 @@ program.name('directus-extension').usage('[command] [options]');
 program.version(getSdkVersion(), '-v, --version');
 
 program
-	.command('create')
-	.arguments('<type> <name>')
-	.description('Scaffold a new Directus extension')
-	.option('--no-install', 'skip dependency installation after creating extension')
-	.option('-l, --language <language>', 'specify the language to use')
-	.action(create);
+  .command('create')
+  .arguments('<type> <name>')
+  .description('Scaffold a new Directus extension')
+  .option('--no-install', 'skip dependency installation after creating extension')
+  .option('-l, --language <language>', 'specify the language to use')
+  .action(create);
 
 program
-	.command('add')
-	.description('Add entries to an existing Directus extension')
-	.option('--no-install', 'skip dependency (re)installation after adding extension')
-	.action(add);
+  .command('add')
+  .description('Add entries to an existing Directus extension')
+  .option('--no-install', 'skip dependency (re)installation after adding extension')
+  .action(add);
 
 program
-	.command('build')
-	.description('Bundle a Directus extension to a single entrypoint')
-	.option('-t, --type <type>', 'specify the extension type instead of reading from package manifest')
-	.option('-i, --input <file>', 'specify the entrypoint instead of reading from package manifest')
-	.option('-o, --output <file>', 'specify the output file instead of reading from package manifest')
-	.option('-l, --language <language>', '[DEPRECATED]')
-	.option('-f, --force', '[DEPRECATED]')
-	.option('-w, --watch', 'watch and rebuild on changes')
-	.option('--no-minify', 'disable minification')
-	.option('--sourcemap', 'include source maps in output')
-	.action(build);
+  .command('build')
+  .description('Bundle a Directus extension to a single entrypoint')
+  .option('-t, --type <type>', 'specify the extension type instead of reading from package manifest')
+  .option('-i, --input <file>', 'specify the entrypoint instead of reading from package manifest')
+  .option('-o, --output <file>', 'specify the output file instead of reading from package manifest')
+  .option('-l, --language <language>', '[DEPRECATED]')
+  .option('-f, --force', '[DEPRECATED]')
+  .option('-w, --watch', 'watch and rebuild on changes')
+  .option('--no-minify', 'disable minification')
+  .option('--sourcemap', 'include source maps in output')
+  .action(build);
 
 program
-	.command('link')
-	.description('Creates a symlink to the extension in the Directus extensions folder')
-	.argument('<path>', 'path to the extension folder of directus')
-	.action(link);
+  .command('link')
+  .description('Creates a symlink to the extension in the Directus extensions folder')
+  .argument('<path>', 'path to the extension folder of directus')
+  .action(link);
+
+program
+  .command('validate')
+  .description('Check that the extension matches the Directus extensions requirements')
+  .action(validate);
 
 program.parse(process.argv);
