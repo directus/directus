c57ee086b59911aee5291bd21502a89b00568e20
Mike Elsmore <mike@directus.io>
2024-12-20 16:02:43 +0000

    Correct format to prettier rules

--- packages/extensions-sdk/src/cli/commands/validate.ts
+++ packages/extensions-sdk/src/cli/commands/validate.ts
@@ -5,98 +5,98 @@ import ora from 'ora';
 import { log } from '../utils/logger.js';
 
 export default async function validate(): Promise<void> {
-  const spinner = ora(chalk.bold('Validating Directus extension...')).start();
+	const spinner = ora(chalk.bold('Validating Directus extension...')).start();
 
-  try {
-    await Promise.all([checkBuiltCode(), checkReadMe(), checkLicense(), checkDirectusConfig()]);
+	try {
+		await Promise.all([checkBuiltCode(), checkReadMe(), checkLicense(), checkDirectusConfig()]);
 
-    spinner.succeed(chalk.bold('Extension is valid'));
-  } catch (error) {
-    spinner.fail(chalk.bold('Failed validation'));
-    log(String(error), 'error');
-  }
+		spinner.succeed(chalk.bold('Extension is valid'));
+	} catch (error) {
+		spinner.fail(chalk.bold('Failed validation'));
+		log(String(error), 'error');
+	}
 }
 
 async function checkBuiltCode() {
-  const spinner = ora('Check for /dist').start();
+	const spinner = ora('Check for /dist').start();
 
-  if (!(await fse.pathExists(`${process.cwd()}/dist`))) {
-    spinner.fail();
-    return Promise.reject('No /dist directory');
-  }
+	if (!(await fse.pathExists(`${process.cwd()}/dist`))) {
+		spinner.fail();
+		return Promise.reject('No /dist directory');
+	}
 
-  spinner.succeed('Valid /dist');
-  return true;
+	spinner.succeed('Valid /dist');
+	return true;
 }
 
 async function checkReadMe() {
-  const spinner = ora('Check for README').start();
+	const spinner = ora('Check for README').start();
 
-  if (!(await fse.pathExists(`${process.cwd()}/README.md`))) {
-    spinner.fail();
-    return Promise.reject('No README.md');
-  }
+	if (!(await fse.pathExists(`${process.cwd()}/README.md`))) {
+		spinner.fail();
+		return Promise.reject('No README.md');
+	}
 
-  spinner.succeed('Valid README');
-  return true;
+	spinner.succeed('Valid README');
+	return true;
 }
 
 async function checkLicense() {
-  const spinner = ora('Check for LICENSE').start();
+	const spinner = ora('Check for LICENSE').start();
 
-  if (!(await fse.pathExists(`${process.cwd()}/LICENSE`))) {
-    spinner.fail();
-    return Promise.reject('No LICENSE file');
-  }
+	if (!(await fse.pathExists(`${process.cwd()}/LICENSE`))) {
+		spinner.fail();
+		return Promise.reject('No LICENSE file');
+	}
 
-  spinner.succeed('Valid LICENSE');
-  return true;
+	spinner.succeed('Valid LICENSE');
+	return true;
 }
 
 async function checkDirectusConfig() {
-  const spinner = ora(`Checking ${EXTENSION_PKG_KEY} Object`).start();
+	const spinner = ora(`Checking ${EXTENSION_PKG_KEY} Object`).start();
 
-  spinner.text = 'Checking package file exists';
+	spinner.text = 'Checking package file exists';
 
-  if (!(await fse.pathExists(`${process.cwd()}/package.json`))) {
-    spinner.fail();
-    return Promise.reject('No package.json');
-  }
+	if (!(await fse.pathExists(`${process.cwd()}/package.json`))) {
+		spinner.fail();
+		return Promise.reject('No package.json');
+	}
 
-  const packageFile = await fse.readJson(`${process.cwd()}/package.json`);
+	const packageFile = await fse.readJson(`${process.cwd()}/package.json`);
 
-  spinner.text = `Checking ${EXTENSION_PKG_KEY} is present`;
+	spinner.text = `Checking ${EXTENSION_PKG_KEY} is present`;
 
-  if (!packageFile[EXTENSION_PKG_KEY]) {
-    spinner.fail();
-    return Promise.reject('Directus object not found');
-  }
+	if (!packageFile[EXTENSION_PKG_KEY]) {
+		spinner.fail();
+		return Promise.reject('Directus object not found');
+	}
 
-  const { type, path, host } = packageFile[EXTENSION_PKG_KEY];
+	const { type, path, host } = packageFile[EXTENSION_PKG_KEY];
 
-  spinner.text = `Checking extension type`;
+	spinner.text = `Checking extension type`;
 
-  if (!EXTENSION_TYPES.includes(type)) {
-    spinner.fail();
-    return Promise.reject(`Invalid Directus Extension Type: ${type}`);
-  }
+	if (!EXTENSION_TYPES.includes(type)) {
+		spinner.fail();
+		return Promise.reject(`Invalid Directus Extension Type: ${type}`);
+	}
 
-  spinner.text = `Checking extension path`;
+	spinner.text = `Checking extension path`;
 
-  if (!(await fse.pathExists(`${process.cwd()}/${path}`))) {
-    spinner.fail();
-    return Promise.reject(`Extension path ${path} invalid`);
-  }
+	if (!(await fse.pathExists(`${process.cwd()}/${path}`))) {
+		spinner.fail();
+		return Promise.reject(`Extension path ${path} invalid`);
+	}
 
-  spinner.text = 'Checking for valid Directus host version';
+	spinner.text = 'Checking for valid Directus host version';
 
-  const regex = new RegExp(/^\^?[1-9]\d*(\.[1-9]\d*)*/);
+	const regex = new RegExp(/^\^?[1-9]\d*(\.[1-9]\d*)*/);
 
-  if (!regex.test(host)) {
-    spinner.fail();
-    return Promise.reject(`${host} not a valid Directus version`);
-  }
+	if (!regex.test(host)) {
+		spinner.fail();
+		return Promise.reject(`${host} not a valid Directus version`);
+	}
 
-  spinner.succeed(`Valid ${EXTENSION_PKG_KEY} Object`);
-  return true;
+	spinner.succeed(`Valid ${EXTENSION_PKG_KEY} Object`);
+	return true;
 }
