7d2b0ffffe265049196e9aadb52f310db3e38cfa
ian <licitdev@gmail.com>
2024-12-25 02:13:03 +0800

    Patch blackbox tests

--- tests/blackbox/setup/seeds/01_delete_existing_data.js
+++ tests/blackbox/setup/seeds/01_delete_existing_data.js
@@ -3,6 +3,9 @@ export async function seed(knex) {
 		await knex('directus_collections').del();
 		await knex('directus_relations').del();
 		await knex('directus_roles').del();
+		await knex('directus_permissions').del();
+		await knex('directus_policies').del();
+		await knex('directus_access').del();
 		await knex('directus_users').del();
 	}
 }
