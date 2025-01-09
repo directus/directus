ef64d0f404bbdd8219ca0047f1580c7acb0a471b
Connor Winston <connor@winston.guru>
2025-01-09 12:53:53 -0600

    Fix Template Rendering (#24232)
    * Fix Label Display Not Listing
    
    * Fix Formatted Array List not Including Spaces
    
    * Fix User Display not being Formatted Correctly
    
    * Update Template Rendered to Render Nested Field Displays
    
    * Remove Console Log
    
    * Fix Formatting
    
    * Create nasty-rockets-give.md
    
    * Fix Error Handling
    
    * Update app/src/views/private/components/render-template.vue
    
    * Fix No Component Arrayed Values, Fix Default Displays
    
    * Update app/src/displays/formatted-value/formatted-value.vue
    
    * Fix Prettier
    
    ---------
    
    Co-authored-by: Alex Gaillard <alex@directus.io>
    Co-authored-by: formfcw <dev@formfcw.com>

--- app/src/views/private/components/render-template.vue
+++ app/src/views/private/components/render-template.vue
@@ -4,7 +4,7 @@ import { useFieldsStore } from '@/stores/fields';
 import { getDefaultDisplayForType } from '@/utils/get-default-display-for-type';
 import { translate } from '@/utils/translate-literal';
 import { Field } from '@directus/types';
-import { get } from 'lodash';
+import { get, flatMap } from 'lodash';
 import { computed, ref } from 'vue';
 
 const props = withDefaults(
@@ -27,140 +27,121 @@ const templateEl = ref<HTMLElement>();
 
 const regex = /({{.*?}})/g;
 
+const getNestedValues = (data: any, path: string) => {
+	const pathParts = path.split('.');
+	let currentData = data;
+
+	for (const part of pathParts) {
+		if (Array.isArray(currentData)) {
+			currentData = flatMap(currentData.map((item) => get(item, part)));
+		} else {
+			currentData = get(currentData, part);
+		}
+	}
+
+	return Array.isArray(currentData) ? currentData : [currentData];
+};
+
 const parts = computed(() =>
 	props.template
 		.split(regex)
 		.filter((p) => p)
 		.map((part) => {
-			if (part.startsWith('{{') === false) return part;
+			if (part.startsWith('{{') === false) return [part];
 
 			const fieldKey = part.replace(/{{/g, '').replace(/}}/g, '').trim();
-			const fieldKeyBefore = fieldKey.split('.').slice(0, -1).join('.');
-			const fieldKeyAfter = fieldKey.split('.').slice(-1)[0];
 
-			// Try getting the value from the item, return some question marks if it doesn't exist
-			const value = get(props.item, fieldKeyBefore);
+			// Try getting the value from the item
+			const value = getNestedValues(props.item, fieldKey);
 
-			return Array.isArray(value) ? handleArray(fieldKeyBefore, fieldKeyAfter) : handleObject(fieldKey);
+			let field: Field | null = props.fields?.find((field) => field.field === fieldKey) ?? null;
+
+			if (props.collection) {
+				field = fieldsStore.getField(props.collection, fieldKey);
+			}
+
+			if (!field) return value;
+
+			const component = field?.meta?.display || getDefaultDisplayForType(field.type);
+			const options = field?.meta?.display_options;
+
+			// No need to render the empty display overhead in this case
+			if (component === 'raw') return value;
+
+			const displayInfo = useExtension(
+				'display',
+				computed(() => component ?? null),
+			);
+
+			if (!displayInfo.value) return value;
+
+			if (component && ['related-values', 'formatted-value'].includes(component)) {
+				// These displays natively support rendering arrays of values
+				return [
+					{
+						component,
+						options,
+						value: value,
+						interface: field.meta?.interface,
+						interfaceOptions: field.meta?.options,
+						type: field.type,
+						collection: field.collection,
+						field: field.field,
+					},
+				];
+			} else if (component) {
+				return value.map((v) => {
+					return {
+						component: component,
+						options: field.meta?.display_options,
+						value: v,
+						interface: field.meta?.interface,
+						interfaceOptions: field.meta?.options,
+						type: field.type,
+						collection: field.collection,
+						field: field.field,
+					};
+				});
+			} else {
+				return value;
+			}
 		})
 		.map((p) => p ?? null),
 );
-
-function handleArray(fieldKeyBefore: string, fieldKeyAfter: string) {
-	const value = get(props.item, fieldKeyBefore);
-
-	let field: Field | null = props.fields?.find((field) => field.field === fieldKeyBefore) ?? null;
-
-	if (props.collection) {
-		field = fieldsStore.getField(props.collection, fieldKeyBefore);
-	}
-
-	if (value === undefined) return null;
-
-	if (!field) return value;
-
-	const displayInfo = useExtension(
-		'display',
-		computed(() => field?.meta?.display ?? null),
-	);
-
-	let component = field.meta?.display;
-	let options = field.meta?.display_options;
-
-	if (!displayInfo.value) {
-		component = 'related-values';
-		options = { template: `{{${fieldKeyAfter}}}` };
-	}
-
-	return {
-		component,
-		options,
-		value: value,
-		interface: field.meta?.interface,
-		interfaceOptions: field.meta?.options,
-		type: field.type,
-		collection: field.collection,
-		field: field.field,
-	};
-}
-
-function handleObject(fieldKey: string) {
-	let field: Field | null = props.fields?.find((field) => field.field === fieldKey) ?? null;
-
-	if (props.collection) {
-		field = fieldsStore.getField(props.collection, fieldKey);
-	}
-
-	/**
-	 * This is for cases where you are rendering a display template directly on
-	 * directus_files. The $thumbnail fields doesn't exist, but instead renders a
-	 * thumbnail based on the other fields in the file info. In that case, the value
-	 * should be the whole related file object, not just the fake "thumbnail" field. By
-	 * stripping out the thumbnail part in the field key path, the rest of the function
-	 * will extract the value correctly.
-	 */
-	if (field && field.collection === 'directus_files' && field.field === '$thumbnail') {
-		fieldKey = fieldKey
-			.split('.')
-			.filter((part) => part !== '$thumbnail')
-			.join('.');
-	}
-
-	const value = fieldKey ? get(props.item, fieldKey) : props.item;
-
-	if (value === undefined) return null;
-
-	if (!field) return value;
-
-	const display = field?.meta?.display || getDefaultDisplayForType(field.type);
-
-	// No need to render the empty display overhead in this case
-	if (display === 'raw') return value;
-
-	const displayInfo = useExtension(
-		'display',
-		computed(() => field?.meta?.display ?? null),
-	);
-
-	// If used display doesn't exist in the current project, return raw value
-	if (!displayInfo.value) return value;
-
-	return {
-		component: field.meta?.display,
-		options: field.meta?.display_options,
-		value: value,
-		interface: field.meta?.interface,
-		interfaceOptions: field.meta?.options,
-		type: field.type,
-		collection: field.collection,
-		field: field.field,
-	};
-}
 </script>
 
 <template>
 	<div ref="templateEl" class="render-template">
 		<span class="vertical-aligner" />
 		<template v-for="(part, index) in parts" :key="index">
-			<value-null v-if="part === null || (typeof part === 'object' && part.value === null)" />
-			<v-error-boundary v-else-if="typeof part === 'object' && part.component" :name="`display-${part.component}`">
-				<component
-					:is="`display-${part.component}`"
-					v-bind="part.options"
-					:value="part.value"
-					:interface="part.interface"
-					:interface-options="part.interfaceOptions"
-					:type="part.type"
-					:collection="part.collection"
-					:field="part.field"
-				/>
-
-				<template #fallback>
-					<span>{{ part.value }}</span>
-				</template>
-			</v-error-boundary>
-			<span v-else-if="typeof part === 'string'" :dir="direction">{{ translate(part) }}</span>
-			<span v-else>{{ part }}</span>
+			<template v-for="(subPart, subIndex) in part" :key="subIndex">
+				<v-error-boundary>
+					<value-null
+						v-if="
+							subPart === null ||
+							(typeof subPart === 'object' && (subPart.value === null || subPart.value === undefined))
+						"
+					/>
+					<template v-else-if="subPart?.component">
+						<component
+							:is="`display-${subPart.component}`"
+							v-bind="subPart.options"
+							:value="subPart.value"
+							:interface="subPart.interface"
+							:interface-options="subPart.interfaceOptions"
+							:type="subPart.type"
+							:collection="subPart.collection"
+							:field="subPart.field"
+						/>
+						<span>&nbsp;</span>
+					</template>
+					<span v-else-if="typeof subPart === 'string'" :dir="direction">{{ translate(subPart) }}</span>
+					<span v-else>{{ subPart }}</span>
+					<template #fallback>
+						<span>{{ subPart?.value || subPart }}</span>
+					</template>
+				</v-error-boundary>
+			</template>
 		</template>
 	</div>
 </template>
