fb99da67367ef47d63aa4cc913e8baf8c04d4291
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2025-01-13 15:46:24 -0500

    add o2m tests

--- api/src/database/run-ast/utils/merge-with-parent-items.test.ts
+++ api/src/database/run-ast/utils/merge-with-parent-items.test.ts
@@ -125,3 +125,142 @@ describe('m2o', () => {
 		});
 	});
 });
+
+describe('o2m', () => {
+	const schema = {
+		collections: {
+			collection_a: {
+				primary: 'id',
+			},
+		},
+	} as unknown as SchemaOverview;
+
+	const nestedNode = {
+		type: 'o2m',
+		fieldKey: 'nodes',
+		relation: {
+			field: 'parentPK',
+			related_collection: 'collection_a',
+		},
+		query: {},
+	} as NestedCollectionNode;
+
+	test('should return [] relation node when no access to parent primary key', () => {
+		const nestedItem: Item[] = [
+			{
+				id: 'nestedPK1',
+				first_name: 'Lorem',
+				last_name: 'Ipsum',
+				parentPK: 1,
+			},
+		];
+
+		const parentItem: Item[] = [{ id: null }];
+
+		const items = mergeWithParentItems(schema, nestedItem, parentItem, nestedNode, true);
+
+		expect(items).toEqual([{ id: null, nodes: [] }]);
+	});
+
+	test('should return [] relation node when parentPK is null', () => {
+		const nestedItem: Item[] = [
+			{
+				id: 'nestedPK1',
+				first_name: 'Lorem',
+				last_name: 'Ipsum',
+				parentPK: null,
+			},
+		];
+
+		const parentItem: Item[] = [{ id: 1 }];
+
+		const items = mergeWithParentItems(schema, nestedItem, parentItem, nestedNode, true);
+
+		expect(items).toEqual([
+			{
+				id: 1,
+				nodes: [],
+			},
+		]);
+	});
+
+	test('should return [] relation node when parentKey is not present', () => {
+		const nestedItem: Item[] = [
+			{
+				id: 'nestedPK1',
+				first_name: 'Lorem',
+				last_name: 'Ipsum',
+				parentPK: 1,
+			},
+		];
+
+		const parentItem: Item[] = [{ id: 2 }];
+
+		const items = mergeWithParentItems(schema, nestedItem, parentItem, nestedNode, true);
+
+		expect(items).toEqual([
+			{
+				id: 2,
+				nodes: [],
+			},
+		]);
+	});
+
+	test('should return relation node when keys have type mismatch', () => {
+		const nestedItem: Item[] = [
+			{
+				id: '1234',
+				first_name: 'Lorem',
+				last_name: 'Ipsum',
+				parentPK: 1,
+			},
+		];
+
+		const parentItem: Item[] = [{ id: 1 }];
+
+		const items = mergeWithParentItems(schema, nestedItem, parentItem, nestedNode, true);
+
+		expect(items).toEqual([
+			{
+				id: 1,
+				nodes: [
+					{
+						id: '1234',
+						first_name: 'Lorem',
+						last_name: 'Ipsum',
+						parentPK: 1,
+					},
+				],
+			},
+		]);
+	});
+
+	test('should return relation node', () => {
+		const nestedItem: Item[] = [
+			{
+				id: 1,
+				first_name: 'Lorem',
+				last_name: 'Ipsum',
+				parentPK: 1,
+			},
+		];
+
+		const parentItem: Item[] = [{ id: 1 }];
+
+		const items = mergeWithParentItems(schema, nestedItem, parentItem, nestedNode, true);
+
+		expect(items).toEqual([
+			{
+				id: 1,
+				nodes: [
+					{
+						id: 1,
+						first_name: 'Lorem',
+						last_name: 'Ipsum',
+						parentPK: 1,
+					},
+				],
+			},
+		]);
+	});
+});
