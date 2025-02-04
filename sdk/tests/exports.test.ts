55c11ad39f5e5cbddee8a6b5ec155a1c850662e7
WoLfulus <wolfulus@gmail.com>
2025-02-04 17:02:32 -0300

    Format with prettier

--- sdk/tests/exports.test.ts
+++ sdk/tests/exports.test.ts
@@ -1,7 +1,7 @@
-import { glob, readFile } from "node:fs/promises";
-import { join, resolve } from "node:path";
+import { glob, readFile } from 'node:fs/promises';
+import { join, resolve } from 'node:path';
 
-import { assert, describe, test } from "vitest";
+import { assert, describe, test } from 'vitest';
 
 // Or enable "ESNext" in tsconfig.json#compilerOptions.lib
 declare global {
@@ -11,11 +11,11 @@ declare global {
 }
 
 function getPackageRoot() {
-	return resolve(join(import.meta.dirname, ".."));
+	return resolve(join(import.meta.dirname, '..'));
 }
 
 async function getNonIndexFileNamesIn(dir: string) {
-	const iterator = glob("*.ts", {
+	const iterator = glob('*.ts', {
 		withFileTypes: true,
 		cwd: join(getPackageRoot(), dir),
 	});
@@ -23,43 +23,35 @@ async function getNonIndexFileNamesIn(dir: string) {
 	const files = await Array.fromAsync(iterator);
 	return files
 		.filter((file) => file.isFile())
-		.filter((file) => file.name != "index.ts")
-		.map((file) => file.name.replace(/\.ts$/, ""));
+		.filter((file) => file.name != 'index.ts')
+		.map((file) => file.name.replace(/\.ts$/, ''));
 }
 
 async function getFileLines(file: string) {
-	const contents = await readFile(join(getPackageRoot(), file), "utf-8");
-	return contents.split("\n");
+	const contents = await readFile(join(getPackageRoot(), file), 'utf-8');
+	return contents.split('\n');
 }
 
 function isExportingAllFrom(file: string, lines: string[]) {
-	return lines.includes(`export * from './${file}.js';`) ||
-		lines.includes(`export type * from './${file}.js';`);
+	return lines.includes(`export * from './${file}.js';`) || lines.includes(`export type * from './${file}.js';`);
 }
 
-describe("Test Exported Types", () => {
-	test("all schema files must be exported", async () => {
-		const index = await getFileLines("src/schema/index.ts");
-		const files = await getNonIndexFileNamesIn("src/schema");
+describe('Test Exported Types', () => {
+	test('all schema files must be exported', async () => {
+		const index = await getFileLines('src/schema/index.ts');
+		const files = await getNonIndexFileNamesIn('src/schema');
 
 		for (const file of files) {
-			assert(
-				isExportingAllFrom(file, index),
-				`SDK schema file "${file}" is not exported`,
-			);
+			assert(isExportingAllFrom(file, index), `SDK schema file "${file}" is not exported`);
 		}
 	});
 
-
-	test("all type file must be exported", async () => {
-		const index = await getFileLines("src/types/index.ts");
-		const files = await getNonIndexFileNamesIn("src/types");
+	test('all type file must be exported', async () => {
+		const index = await getFileLines('src/types/index.ts');
+		const files = await getNonIndexFileNamesIn('src/types');
 
 		for (const file of files) {
-			assert(
-				isExportingAllFrom(file, index),
-				`SDK type file "${file}" is not exported`,
-			);
+			assert(isExportingAllFrom(file, index), `SDK type file "${file}" is not exported`);
 		}
 	});
 });
