7643c518f3a8f90b16b0278f078831120bdfe5ca
WoLfulus <wolfulus@gmail.com>
2025-02-04 16:50:30 -0300

    Export unexported SDK types

--- sdk/src/schema/index.ts
+++ sdk/src/schema/index.ts
@@ -2,6 +2,7 @@ export * from './access.js';
 export * from './activity.js';
 export * from './collection.js';
 export * from './core.js';
+export * from './comment.js';
 export * from './dashboard.js';
 export * from './extension.js';
 export * from './field.js';
