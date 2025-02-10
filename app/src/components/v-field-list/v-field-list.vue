46f9b360649716e081a2a3c5c51abc4a3b0f2ca1
formfcw <dev@formfcw.com>
2025-02-10 08:53:21 +0100

    show filter search field if list contains collapsible items or more than 10

--- app/src/components/v-field-list/v-field-list.vue
+++ app/src/components/v-field-list/v-field-list.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useFieldTree, type FieldNode } from '@/composables/use-field-tree';
 import { useFieldsStore } from '@/stores/fields';
+import { useRelationsStore } from '@/stores/relations';
 import { Field } from '@directus/types';
 import { debounce, isNil } from 'lodash';
 import { computed, ref, toRefs, unref, watch } from 'vue';
@@ -32,12 +33,26 @@ const props = withDefaults(
 const emit = defineEmits(['add']);
 
 const fieldsStore = useFieldsStore();
+const relationsStore = useRelationsStore();
 
 const { collection, includeRelations } = toRefs(props);
 
-const fieldsCount = computed(() => fieldsStore.getFieldsForCollection(collection.value)?.length ?? 0);
-
 const search = ref('');
+
+const showSearch = computed(() => {
+	const fields = fieldsStore.getFieldsForCollection(collection.value);
+	if (!fields.length) return false;
+
+	if (fields?.length > 10) return true;
+
+	const anyGroupExists = fields.some((field) => field.meta?.group !== null);
+	if (anyGroupExists) return true;
+
+	const anyRelationExists = relationsStore.getRelationsForCollection(collection.value)?.length;
+	if (anyRelationExists) return true;
+
+	return false;
+});
 
 const { treeList: treeListOriginal, loadFieldRelations, refresh } = useFieldTree(collection, undefined, filter);
 
@@ -118,7 +133,7 @@ function filter(field: Field, parent?: FieldNode): boolean {
 <template>
 	<v-list :mandatory="false" @toggle="loadFieldRelations($event.value)">
 		<slot name="prepend" />
-		<v-list-item v-if="fieldsCount > 10">
+		<v-list-item v-if="showSearch">
 			<v-list-item-content>
 				<v-input v-model="search" autofocus small :placeholder="t('search')" @click.stop>
 					<template #append>
