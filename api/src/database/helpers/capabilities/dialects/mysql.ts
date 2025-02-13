c82ecb9dd7133131157ad3158d77d6c99f603ceb
Hannes Küttner <kuettner.hannes@gmail.com>
2025-02-13 19:38:33 +0100

    chore: add reference to MySQL docs

--- api/src/database/helpers/capabilities/dialects/mysql.ts
+++ api/src/database/helpers/capabilities/dialects/mysql.ts
@@ -2,6 +2,7 @@ import { CapabilitiesHelper } from '../types.js';
 
 export class CapabilitiesHelperMySQL extends CapabilitiesHelper {
 	override supportsAliasInGroupBy(): boolean {
+		// Supported in MySQL https://dev.mysql.com/doc/refman/8.4/en/group-by-handling.html
 		return true;
 	}
 }
