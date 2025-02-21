55d493d9ae95c964994f5e2c298d1b87e2d625e6
lucasiscovici <luluisco@yahoo.fr>
2025-02-21 17:17:14 +0100

    Update openid.ts

--- api/src/auth/drivers/openid.ts
+++ api/src/auth/drivers/openid.ts
@@ -185,11 +185,13 @@ export class OpenIDAuthDriver extends LocalAuthDriver {
 			const additionalOptions={}
 			const checks = { code_verifier: payload['codeVerifier'], state: codeChallenge, nonce: codeChallenge }
 			const requiresClientCredentials = env[`AUTH_${providerName.toUpperCase()}_ACCESS_TOKEN_REQUIRES_CLIENT_CREDENTIALS`] ?? false
+			
 			if(requiresClientCredentials){
 				additionalOptions.exchangeBody={
 					client_id: client.client_id,
 					client_secret: client.client_secret
 				}
+				
 				delete checks["code_verifier"]
 				delete checks["nonce"]
 			}
