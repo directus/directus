5932e354e1009be83b390e2142cc04a2a8b0b312
Jan Arends <jan.arends@mailbox.org>
2025-02-26 10:16:21 -0600

    update file name

--- tests/blackbox/setup/sequential-tests.ts
+++ tests/blackbox/setup/sequential-tests.ts
@@ -32,7 +32,7 @@ export const sequentialTestsList: Record<'db' | 'common', SequentialTestsList> =
 		only: [
 			'/tests/db/seed-database.test.ts',
 			'/common/common.test.ts',
-			'/tests/db/routes/items/no-relation-II.test.ts',
+			'/tests/db/routes/items/case-when.test.ts',
 		],
 	},
 };
