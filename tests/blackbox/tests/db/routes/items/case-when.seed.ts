4b62843bf7512d6df58fcd5f5a90ef72793cf83e
Jan Arends <jan.arends@mailbox.org>
2025-03-01 10:55:47 +0100

    debugging

--- tests/blackbox/tests/db/routes/items/case-when.seed.ts
+++ tests/blackbox/tests/db/routes/items/case-when.seed.ts
@@ -30,59 +30,71 @@ export const seedDBStructure = () => {
 			try {
 				await DeleteCollection(vendor, { collection });
 
-				// await CreateCollection(vendor, {
-				// 	collection,
-				// 	primaryKeyType: 'integer',
-				// });
+				let res = await CreateCollection(vendor, {
+					collection,
+					primaryKeyType: 'integer',
+				});
 
-				const collectionResponse = await request(getUrl(vendor))
-					.post(`/collections/${collection}`)
-					.set('Authorization', `Bearer ${USER.TESTS_FLOW.TOKEN}`)
-					.send({
-						collection: 'articles_case_when',
-						fields: [
-							{
-								field: 'id',
-								type: 'integer',
-								meta: { hidden: true, interface: 'input', readonly: true },
-								schema: { is_primary_key: true, has_auto_increment: true },
-							},
-							{
-								field: 'user_created',
-								type: 'uuid',
-								meta: {
-									special: ['user-created'],
-									interface: 'select-dropdown-m2o',
-									options: { template: '{{avatar}} {{first_name}} {{last_name}}' },
-									display: 'user',
-									readonly: true,
-									hidden: true,
-									width: 'half',
-								},
-								schema: {},
-							},
-							{
-								field: 'date_created',
-								type: 'timestamp',
-								meta: {
-									special: ['date-created'],
-									interface: 'datetime',
-									readonly: true,
-									hidden: true,
-									width: 'half',
-									display: 'datetime',
-									display_options: { relative: true },
-								},
-								schema: {},
-							},
-						],
-						schema: {},
-						meta: { singleton: false },
-					});
-
-				if (!collectionResponse.ok) {
-					throw new Error('Could not create collection', collectionResponse.body);
+				if (!res.ok) {
+					throw new Error('Could not create collection', res.body);
 				}
+
+				await CreateField(vendor, {
+					collection,
+					field: 'user_created',
+					type: 'uuid',
+				});
+
+				await CreateField(vendor, {
+					collection,
+					field: 'date_created',
+					type: 'timestamp',
+				});
+
+				// const collectionResponse = await request(getUrl(vendor))
+				// 	.post(`/collections/${collection}`)
+				// 	.set('Authorization', `Bearer ${USER.TESTS_FLOW.TOKEN}`)
+				// 	.send({
+				// 		collection: 'articles_case_when',
+				// 		fields: [
+				// 			{
+				// 				field: 'id',
+				// 				type: 'integer',
+				// 				meta: { hidden: true, interface: 'input', readonly: true },
+				// 				schema: { is_primary_key: true, has_auto_increment: true },
+				// 			},
+				// 			{
+				// 				field: 'user_created',
+				// 				type: 'uuid',
+				// 				meta: {
+				// 					special: ['user-created'],
+				// 					interface: 'select-dropdown-m2o',
+				// 					options: { template: '{{avatar}} {{first_name}} {{last_name}}' },
+				// 					display: 'user',
+				// 					readonly: true,
+				// 					hidden: true,
+				// 					width: 'half',
+				// 				},
+				// 				schema: {},
+				// 			},
+				// 			{
+				// 				field: 'date_created',
+				// 				type: 'timestamp',
+				// 				meta: {
+				// 					special: ['date-created'],
+				// 					interface: 'datetime',
+				// 					readonly: true,
+				// 					hidden: true,
+				// 					width: 'half',
+				// 					display: 'datetime',
+				// 					display_options: { relative: true },
+				// 				},
+				// 				schema: {},
+				// 			},
+				// 		],
+				// 		schema: {},
+				// 		meta: { singleton: false },
+				// 	});
 
 				const relationsResponse = await request(getUrl(vendor))
 					.post(`/relations`)
@@ -97,6 +109,12 @@ export const seedDBStructure = () => {
 				if (!relationsResponse.ok) {
 					throw new Error('Could not create relation', relationsResponse.body);
 				}
+
+				const collectionResponse = await request(getUrl(vendor))
+					.get(`/collections`)
+					.set('Authorization', `Bearer ${USER.ADMIN.TOKEN}`);
+
+				console.log(collectionResponse.body);
 
 				console.log('schema is setup');
 
