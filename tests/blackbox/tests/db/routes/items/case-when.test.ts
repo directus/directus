dcdceb8bf938a06acb5fa674d887cc7a0ca3f4ce
Jan Arends <jan.arends@mailbox.org>
2025-03-03 11:09:59 +0100

    Revert "refactoring"
    This reverts commit 7c3e60c8ced1a15d1f5982c621eec7977b1e5ec2.

--- tests/blackbox/tests/db/routes/items/case-when.test.ts
+++ tests/blackbox/tests/db/routes/items/case-when.test.ts
@@ -1,85 +1,15 @@
 import { getUrl } from '@common/config';
 import vendors, { type Vendor } from '@common/get-dbs-to-test';
 import request from 'supertest';
-import { beforeAll, describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { collection, seedDBValues } from './case-when.seed';
 import { USER } from '@common/variables';
 
-const userToken = 'pp2KIAA3mGdgqngRVDuegxNuVj7gM-es';
-let newUserId: null | string = null;
-
-beforeAll(async () => {
-	it.each(vendors)(
-		'%s',
-		async (vendor) => {
-			// create a new user with a new policy and new permissions
-			const userResponse = await request(getUrl(vendor))
-				.post(`/users`)
-				.set('Authorization', `Bearer ${USER.ADMIN.TOKEN}`)
-				.send({
-					first_name: 'john',
-					email: 'j@mail.com',
-					password: '12345',
-					token: 'pp2KIAA3mGdgqngRVDuegxNuVj7gM-es',
-					policies: {
-						create: [
-							{
-								policy: {
-									name: 'sample',
-									permissions: {
-										create: [
-											{
-												policy: '+',
-												permissions: {
-													_and: [
-														{
-															user_created: {
-																id: {
-																	_eq: '$CURRENT_USER',
-																},
-															},
-														},
-													],
-												},
-												validation: null,
-												fields: ['id', 'user_created', 'date_created'],
-												presets: null,
-												collection: 'articles_case_when',
-												action: 'read',
-											},
-											{
-												policy: '+',
-												permissions: null,
-												validation: null,
-												fields: ['*'],
-												presets: null,
-												collection: 'articles_case_when',
-												action: 'create',
-											},
-										],
-										update: [],
-										delete: [],
-									},
-									app_access: true,
-								},
-							},
-						],
-						update: [],
-						delete: [],
-					},
-				});
-
-			if (!userResponse.ok) {
-				throw new Error('Could not create user', userResponse.body);
-			}
-
-			newUserId = userResponse.body.data.id;
-
-			await seedDBValues(vendor, userToken);
-		},
-		300_000,
-	);
-});
+// beforeAll(async () => {
+// 	console.log('seed db');
+// 	seedResult = await seedDBValues();
+// 	console.log('seeded db', seedResult.isSeeded);
+// }, 300_000);
 
 // test('Seed Database Values', () => {
 // 	if (seedResult) {
@@ -90,52 +20,106 @@ beforeAll(async () => {
 // 	}
 // });
 
-describe('Admin can query both articles', async () => {
+describe('retrieves items with filters', async () => {
+	const userToken = 'pp2KIAA3mGdgqngRVDuegxNuVj7gM-es';
+
 	it.each(vendors)('%s', async (vendor) => {
+		const userResponse = await request(getUrl(vendor))
+			.post(`/users`)
+			.set('Authorization', `Bearer ${USER.ADMIN.TOKEN}`)
+			.send({
+				first_name: 'john',
+				email: 'j@mail.com',
+				password: '12345',
+				token: 'pp2KIAA3mGdgqngRVDuegxNuVj7gM-es',
+				policies: {
+					create: [
+						{
+							policy: {
+								name: 'sample',
+								permissions: {
+									create: [
+										{
+											policy: '+',
+											permissions: {
+												_and: [
+													{
+														user_created: {
+															id: {
+																_eq: '$CURRENT_USER',
+															},
+														},
+													},
+												],
+											},
+											validation: null,
+											fields: ['id', 'user_created', 'date_created'],
+											presets: null,
+											collection: 'articles_case_when',
+											action: 'read',
+										},
+										{
+											policy: '+',
+											permissions: null,
+											validation: null,
+											fields: ['*'],
+											presets: null,
+											collection: 'articles_case_when',
+											action: 'create',
+										},
+									],
+									update: [],
+									delete: [],
+								},
+								app_access: true,
+							},
+						},
+					],
+					update: [],
+					delete: [],
+				},
+			});
+
+		if (!userResponse.ok) {
+			throw new Error('Could not create user', userResponse.body);
+		}
+
+		await seedDBValues(vendor, userToken);
+
+		// Admin can query both articles
 		const response = await request(getUrl(vendor))
 			.get(`/items/${collection}`)
 			.set('Authorization', `Bearer ${USER.ADMIN.TOKEN}`);
 
 		expect(response.statusCode).toEqual(200);
 		expect(response.body.data.length).toBe(2);
-	});
-});
 
-describe('Editor can only query articles created by them', async () => {
-	it.each(vendors)('%s', async (vendor) => {
+		// Editor can only query articles created by them
 		const response1 = await request(getUrl(vendor))
 			.get(`/items/${collection}`)
 			.set('Authorization', `Bearer ${userToken}`);
 
 		expect(response1.statusCode).toEqual(200);
 		expect(response1.body.data.length).toBe(2);
-	});
-});
 
-// The query for which the deduplication was implemented
-describe('A groupBy query with aggregation', async () => {
-	it.each(vendors)('%s', async (vendor) => {
+		// A groupBy query with aggregation
+		// The query for which the deduplication was implemented
 		const groupByWithAggregationResponse = await request(getUrl(vendor))
 			.get(`/items/${collection}?groupBy=day(date_created)&aggregate[count]=*`)
 			.set('Authorization', `Bearer ${userToken}`);
 
 		expect(groupByWithAggregationResponse.statusCode).toEqual(200);
 		expect(groupByWithAggregationResponse.body.data.length).toBe(1);
-
 		// It would be nice to also check the actual result, but the result is not deterministic.
 		// Depending on the database, it returns either an integer or a string.
 		// expect(groupByWithAggregationResponse.body.data[0].count).toBe(2);
-	});
-});
 
-// A query which produces a case when statement in the final query
-// where the fields to compare, have different data types:
-// user_created is a string, whereas directus_users.id is a of type UUID.
-describe('A groupBy query with aggregation', async () => {
-	it.each(vendors)('%s', async (vendor) => {
+		// A query which produces a case when statement in the final query
+		// where the fields to compare, have different data types:
+		// user_created is a string, whereas directus_users.id is a of type UUID.
 		const comparisonResult = await request(getUrl(vendor))
 			.get(
-				`/comments?filter[_and][0][collection][_eq]=directus_users&filter[_and][1][item][_eq]=${newUserId}&aggregate[count]=id`,
+				`/comments?filter[_and][0][collection][_eq]=directus_users&filter[_and][1][item][_eq]=${userResponse.body.data.id}&aggregate[count]=id`,
 			)
 			.set('Authorization', `Bearer ${userToken}`);
 
