2f9878626ceb23f4a6ef04af440e1c576a3bb32e
Jan Arends <jan.arends@mailbox.org>
2025-03-04 09:19:48 +0100

    Additional integration test for case when queries  (#24640)
    * added seed
    
    * rename
    
    * start of test
    
    * add permissions
    
    * add permission to policy in seed
    
    * activate test
    
    * finalize test
    
    * refactoring
    
    * Revert "refactoring"
    
    This reverts commit 7c3e60c8ced1a15d1f5982c621eec7977b1e5ec2.
    
    * clean up
    
    * removed unneeded type
    
    * added one expectation
    
    * Touchups
    
    * Remove unneeded seedDBValues
    
    * Use collection var
    
    * Optimize import
    
    * enabled expect again
    
    * fix
    
    ---------
    
    Co-authored-by: ian <licitdev@gmail.com>

--- tests/blackbox/common/functions.ts
+++ tests/blackbox/common/functions.ts
@@ -658,13 +658,14 @@ export async function CreateFieldM2A(vendor: Vendor, options: OptionsCreateField
 export type OptionsCreateItem = {
 	collection: string;
 	item: any;
+	token?: string;
 };
 
 export async function CreateItem(vendor: Vendor, options: OptionsCreateItem) {
 	// Action
 	const response = await request(getUrl(vendor))
 		.post(`/items/${options.collection}`)
-		.set('Authorization', `Bearer ${USER.TESTS_FLOW.TOKEN}`)
+		.set('Authorization', `Bearer ${options.token ?? USER.TESTS_FLOW.TOKEN}`)
 		.send(options.item);
 
 	return response.body.data;
