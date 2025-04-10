2fdcd0d7901fd6b7d2e21ce9c47e671b79f19077
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2025-04-10 02:21:05 -0400

    Fix `applyOptionsData` errors not appearing in the flow log (#25025)
    * dont skip logs for `applyOptionsData` errors
    
    * add changeset
    
    * Update api/src/flows.ts
    
    Co-authored-by: ian <licitdev@gmail.com>
    
    * Update api/src/flows.ts
    
    Co-authored-by: ian <licitdev@gmail.com>
    
    ---------
    
    Co-authored-by: ian <licitdev@gmail.com>

--- api/src/flows.ts
+++ api/src/flows.ts
@@ -430,9 +430,11 @@ class FlowManager {
 
 		const handler = this.operations.get(operation.type)!;
 
-		const options = applyOptionsData(operation.options, keyedData);
+		let options = operation.options;
 
 		try {
+			options = applyOptionsData(options, keyedData);
+
 			let result = await handler(options, {
 				services,
 				env: useEnv(),
