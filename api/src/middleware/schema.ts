9e512d0a331ad5ca1f8363a249b29d1bbb0b79c8
Nitwel <mail@nitwel.de>
2025-04-16 18:14:49 +0200

    Update schema.ts

--- api/src/middleware/schema.ts
+++ api/src/middleware/schema.ts
@@ -4,7 +4,6 @@ import { getSchema } from '../utils/get-schema.js';
 
 const schema: RequestHandler = asyncHandler(async (req, _res, next) => {
 	req.schema = await getSchema();
-
 	return next();
 });
 
