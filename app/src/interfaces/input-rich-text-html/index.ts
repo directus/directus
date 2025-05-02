67cf2429ef4a0f581010f2ee4364105eb9d0b462
abdallah-awwad <abdallahhawwad@gmail.com>
2025-05-02 20:52:08 +0300

    Rearrange options

--- app/src/interfaces/input-rich-text-html/index.ts
+++ app/src/interfaces/input-rich-text-html/index.ts
@@ -181,6 +181,10 @@ export default defineInterface({
 								text: '$t:wysiwyg_options.blockquote',
 							},
 							{
+								value: 'inlineCode',
+								text: '$t:wysiwyg_options.codeblock',
+							},
+							{
 								value: 'customLink',
 								text: '$t:wysiwyg_options.link',
 							},
@@ -205,14 +209,6 @@ export default defineInterface({
 								text: '$t:wysiwyg_options.hr',
 							},
 							{
-								value: 'inlineCode',
-								text: '$t:wysiwyg_options.codeblock',
-							},
-							{
-								value: 'code',
-								text: '$t:wysiwyg_options.source_code',
-							},
-							{
 								value: 'fullscreen',
 								text: '$t:wysiwyg_options.fullscreen',
 							},
@@ -223,6 +219,10 @@ export default defineInterface({
 							{
 								value: 'ltr rtl',
 								text: '$t:wysiwyg_options.directionality',
+							},
+							{
+								value: 'code',
+								text: '$t:wysiwyg_options.source_code',
 							},
 						],
 					},
