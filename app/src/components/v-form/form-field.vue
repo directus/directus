5d403947bbdc1b3f6c6dd66f170730c402aff902
Rob Luton <rob.luton@gmail.com>
2025-05-05 08:58:41 -0500

    change form-field data-id attribute to data-primaryKey

--- app/src/components/v-form/form-field.vue
+++ app/src/components/v-form/form-field.vue
@@ -147,7 +147,7 @@ function useComputedValues() {
 
 <template>
 	<div
-		:data-id="field.meta?.id"
+		:data-primary-key="primaryKey"
 		:data-collection="field.collection"
 		:data-field="field.field"
 		class="field"
