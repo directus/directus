57a0ac8232f1e8d8900716006480567adadb193f
Nitwel <mail@nitwel.de>
2025-05-24 10:21:04 +0200

    fix imports

--- api/src/permissions/utils/merge-permissions.ts
+++ api/src/permissions/utils/merge-permissions.ts
@@ -1,5 +1,5 @@
 import type { LogicalFilterAND, LogicalFilterOR, Permission } from '@directus/types';
-import { flatten, intersection, isEqual, merge, omit, uniq } from 'lodash-es';
+import { flatten, isEqual, merge, omit } from 'lodash-es';
 import { mergeFields } from './merge-fields.js';
 
 // Adapted from https://github.com/directus/directus/blob/141b8adbf4dd8e06530a7929f34e3fc68a522053/api/src/utils/merge-permissions.ts#L4
