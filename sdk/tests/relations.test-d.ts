84cea91d44c1abe715531be973e80693c0ea0f50
Brainslug <tim@brainslug.nl>
2025-07-09 20:09:16 +0200

    add more relations to tests

--- sdk/tests/relations.test-d.ts
+++ sdk/tests/relations.test-d.ts
@@ -1,24 +1,30 @@
 import { describe, expectTypeOf, test } from 'vitest';
-import { createDirectus, readItems, rest } from '../src/index.js';
-import type { CollectionA, CollectionB, TestSchema } from './schema.js';
+import { createDirectus, readItems, rest, type Prettify } from '../src/index.js';
+import type { CollectionA, CollectionAB_Any, CollectionAB_Many, CollectionB, CollectionC, TestSchema } from './schema.js';
 
-describe('Test Relational return typing', () => {
+describe('Test relational return typing (issue #23545)', () => {
 
-    test('Flat relational ID #23545', () => {
+    test('Flat relational field return type', () => {
         const client = createDirectus<TestSchema>('https://directus.example.com').with(rest());
 
-        const _itemFlatRelation = () => client.request(readItems('collection_a', {
-            fields: ['m2o']
+        const _itemFlatRelations = () => client.request(readItems('collection_a', {
+            fields: ['m2o', 'o2m', 'm2m', 'm2a']
         }));
 
-        type Item = Awaited<ReturnType<typeof _itemFlatRelation>>[0];
+        type Item = Awaited<ReturnType<typeof _itemFlatRelations>>[0];
         
         const result: Item = {
-            m2o: 123
+            m2o: 123,
+            o2m: [123],
+            m2m: [123],
+            m2a: [123],
         };
 
         type Expected = {
-            m2o: Exclude<CollectionA['m2o'], CollectionB>; // number
+            m2o: Exclude<CollectionA['m2o'], CollectionB>;
+            o2m: Exclude<CollectionA['o2m'], CollectionC[]>;
+            m2m: Exclude<CollectionA['m2m'], CollectionAB_Many[]>;
+            m2a: Exclude<CollectionA['m2a'], CollectionAB_Any[]>;
         };
 
         expectTypeOf(result).toEqualTypeOf<Expected>();
@@ -27,22 +33,37 @@ describe('Test Relational return typing', () => {
     test('Nested relational ID #23545', () => {
         const client = createDirectus<TestSchema>('https://directus.example.com').with(rest());
 
-        const _itemNestedRelation = () => client.request(readItems('collection_a', {
-            fields: [
-                { 'm2o': ['id'] }
-            ]
+        const _itemNestedRelations = () => client.request(readItems('collection_a', {
+            fields: [{
+                'm2o': ['id'],
+                'o2m': ['id'],
+                'm2m': ['id'],
+                'm2a': ['id'],
+            }],
         }));
 
-        type Item = Awaited<ReturnType<typeof _itemNestedRelation>>[0];
+        type Item = Awaited<ReturnType<typeof _itemNestedRelations>>[0];
 
         const result: Item = {
             m2o: {
-                id: 123
-            }
+                id: 123,
+            },
+            o2m: [{
+                id: 123,
+            }],
+            m2m: [{
+                id: 123,
+            }],
+            m2a: [{
+                id: 123,
+            }],
         };
 
         type Expected = {
-            m2o: Pick<CollectionB, "id">
+            m2o: Pick<CollectionB, "id">;
+            o2m: Pick<CollectionC, "id">[] | null;
+            m2m: Pick<CollectionAB_Many, "id">[] | null;
+            m2a: Pick<CollectionAB_Any, "id">[] | null;
         };
 
         expectTypeOf(result).toEqualTypeOf<Expected>();
