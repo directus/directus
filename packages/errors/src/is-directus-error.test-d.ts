d57bab4a60fa7144b17d794d0f7bec4a5eb36d1a
Matthew Rumery <matt.superman@gmail.com>
2025-07-15 15:29:08 -0600

    refactor: adjust import for @directus/errors

--- packages/errors/src/is-directus-error.test-d.ts
+++ packages/errors/src/is-directus-error.test-d.ts
@@ -1,25 +1,25 @@
 import { expectTypeOf, test } from 'vitest';
+import type { DirectusExtensionsError } from '@directus/types';
 import { ErrorCode } from './codes.js';
-import { type DirectusError } from './create-error.js';
 import { ContainsNullValuesError, type ContainsNullValuesErrorExtensions } from './errors/contains-null-values.js';
 import { ContentTooLargeError } from './errors/content-too-large.js';
 import { isDirectusError } from './is-directus-error.js';
 
 test('Guards input as DirectusError', () => {
-	expectTypeOf(isDirectusError).guards.toEqualTypeOf<DirectusError<unknown>>();
+	expectTypeOf(isDirectusError).guards.toEqualTypeOf<DirectusExtensionsError<unknown>>();
 });
 
 test('Returns specific type when provided code for built-in error', () => {
 	const contentTooLargeError = new ContentTooLargeError();
 
 	if (isDirectusError(contentTooLargeError, ErrorCode.ContentTooLarge)) {
-		expectTypeOf(contentTooLargeError).toEqualTypeOf<DirectusError<never>>();
+		expectTypeOf(contentTooLargeError).toEqualTypeOf<DirectusExtensionsError<never>>();
 	}
 
 	const containsNullValuesError = new ContainsNullValuesError({ collection: 'sample', field: 'sample' });
 
 	if (isDirectusError(containsNullValuesError, ErrorCode.ContainsNullValues)) {
-		expectTypeOf(containsNullValuesError).toEqualTypeOf<DirectusError<ContainsNullValuesErrorExtensions>>();
+		expectTypeOf(containsNullValuesError).toEqualTypeOf<DirectusExtensionsError<ContainsNullValuesErrorExtensions>>();
 	}
 });
 
@@ -27,7 +27,7 @@ test('Returns unknown when provided code is not a built-in error', () => {
 	const error = { name: 'DirectusError', code: 'CustomError' };
 
 	if (isDirectusError(error, error.code)) {
-		expectTypeOf(error).toEqualTypeOf<DirectusError<unknown>>();
+		expectTypeOf(error).toEqualTypeOf<DirectusExtensionsError<unknown>>();
 	}
 });
 
@@ -37,6 +37,6 @@ test('Allows to pass custom extensions type', () => {
 	type CustomDirectusErrorExtensions = { custom: string };
 
 	if (isDirectusError<CustomDirectusErrorExtensions>(error)) {
-		expectTypeOf(error).toEqualTypeOf<DirectusError<CustomDirectusErrorExtensions>>();
+		expectTypeOf(error).toEqualTypeOf<DirectusExtensionsError<CustomDirectusErrorExtensions>>();
 	}
 });
