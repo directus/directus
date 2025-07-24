a33560c3322aa6623daca842101cf4af84569abc
Rijk van Zanten <rijkvanzanten@me.com>
2025-07-24 16:51:33 -0400

    Make text-direction not nullable

--- api/src/database/migrations/20250718A-add-direction.ts
+++ api/src/database/migrations/20250718A-add-direction.ts
@@ -2,7 +2,7 @@ import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_users', (table) => {
-		table.string('text_direction').defaultTo('auto');
+		table.string('text_direction').defaultTo('auto').notNullable();
 	});
 }
 
