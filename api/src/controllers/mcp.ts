504d028feccd63e76bfa54c5fb0e9d2fa68eb41b
Brainslug <tim@brainslug.nl>
2025-07-31 23:58:29 +0200

    await the handleREquest function

--- api/src/controllers/mcp.ts
+++ api/src/controllers/mcp.ts
@@ -1,18 +1,19 @@
 import { Router } from 'express';
 import { DirectusMCP } from '../mcp/index.js';
+import asyncHandler from '../utils/async-handler.js';
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/', asyncHandler(async (req, res) => {
 	const mcp = new DirectusMCP();
 
-	mcp.handleRequest(req, res);
-});
+	await mcp.handleRequest(req, res);
+}));
 
-router.post('/', (req, res) => {
+router.post('/', asyncHandler(async (req, res) => {
 	const mcp = new DirectusMCP();
 
-	mcp.handleRequest(req, res);
-});
+	await mcp.handleRequest(req, res);
+}));
 
 export default router;
