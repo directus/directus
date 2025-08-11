61d8d4ba7a7a565ceae58f2ae011a296fa8bacda
Nitwel <mail@nitwel.de>
2025-08-11 14:23:27 +0200

    fix tests

--- tests/e2e/tests/query/fields.test.ts
+++ tests/e2e/tests/query/fields.test.ts
@@ -8,11 +8,11 @@ test('ABC', async () => {
 
 	const snapshot = await readFile(join(import.meta.dirname, 'fields.snapshot.json'), { encoding: 'utf8' });
 	const diff = await directus.request(schemaDiff(JSON.parse(snapshot), true));
-	await directus.request(schemaApply(diff));
+	if (diff) await directus.request(schemaApply(diff));
 
 	const result = await directus.request(
 		createItem('fields', {
-			big_integer: 1321931,
+			big_integer: '1321931',
 			boolean: false,
 			date: '2025-08-11',
 			date_time: '2025-08-11T13:24:35',
@@ -28,17 +28,18 @@ test('ABC', async () => {
 	);
 
 	expect(result).toMatchObject({
-		big_integer: 1321931,
 		boolean: false,
 		date: '2025-08-11',
 		date_time: '2025-08-11T13:24:35',
-		decimal: '48832.00000',
 		float: 2,
 		integer: 1,
 		string: 'hello',
 		text: 'lorem ipsum',
 		time: '11:10:59',
-		timestamp: '2025-08-11T11:25:30.000Z',
 		uuid: '1bc5500a-762e-420c-baff-6359ea42c36b',
 	});
+
+	expect(String(result.timestamp).startsWith('2025-08-11T11:25:30'));
+	expect(Number(result.big_integer)).toEqual(1321931);
+	expect(Number(result.decimal)).toEqual(48832);
 });
