49c1d59bb6e1116f4870bd45e4a8c5ea5205c155
Nitwel <mail@nitwel.de>
2025-08-11 14:10:00 +0200

    update tests

--- tests/e2e/setup.ts
+++ tests/e2e/setup.ts
@@ -1,10 +1,10 @@
 import type { TestProject } from 'vitest/node';
-import { sandbox, StopSandbox } from '@directus/sandbox';
+import { Database, sandbox, StopSandbox } from '@directus/sandbox';
 
 let sb: StopSandbox | undefined;
 
 export async function setup(_project: TestProject) {
-	sb = await sandbox('maria', { extras: { maildev: true } });
+	sb = await sandbox((process.env['DATABASE'] as Database) ?? 'postgres', { extras: { maildev: true } });
 }
 
 export async function teardown(_project: TestProject) {
