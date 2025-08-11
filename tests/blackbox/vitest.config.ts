106393180d55f18e2ea08b5a64ebc8bce0022b0b
Nitwel <mail@nitwel.de>
2025-08-11 13:40:24 +0200

    restructure and github workflow

--- tests/blackbox/vitest.config.ts
+++ tests/blackbox/vitest.config.ts
@@ -1,7 +1,20 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
+import Sequencer from './setup/sequencer';
 
 export default defineConfig({
+	plugins: [tsconfigPaths()],
 	test: {
-		globalSetup: './setup.ts',
+		poolOptions: {
+			forks: {
+				minForks: 1,
+				maxForks: 6,
+			},
+		},
+		environment: './setup/environment.ts',
+		sequence: {
+			sequencer: Sequencer,
+		},
+		testTimeout: 15_000,
 	},
 });
