4b0baa51a7143fbe73a74f1b282d4d88adfa8d54
Nitwel <mail@nitwel.de>
2025-08-16 18:41:13 +0200

    fix setup

--- tests/e2e/setup/setup-all.ts
+++ tests/e2e/setup/setup-all.ts
@@ -1,26 +1,46 @@
 import type { TestProject } from 'vitest/node';
 import { sandboxes, StopSandbox } from '@directus/sandbox';
+import { createDirectus, staticToken, schemaDiff, schemaApply, rest } from '@directus/sdk';
+import { Schema } from './schema';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 let sb: StopSandbox | undefined;
 
 export async function setup(_project: TestProject) {
 	if (process.env['ALL'] !== 'true') return;
 
+	const ports: number[] = [];
+
 	const dbs = (['maria', 'cockroachdb', 'mssql', 'mysql', 'oracle', 'postgres', 'sqlite'] as const).map(
-		(database, index) => ({
-			database,
-			options: {
-				prefix: database,
-				port: String(8000 + index * 100),
-				dockerBasePort: String(8000 + index * 100 + 1),
-			},
-		}),
+		(database, index) => {
+			const port = 8000 + index * 100;
+			ports.push(port);
+			return {
+				database,
+				options: {
+					prefix: database,
+					port: String(port),
+					dockerBasePort: String(port + 1),
+				},
+			};
+		},
 	);
 
 	sb = await sandboxes(dbs, {
 		build: true,
 		watch: true,
 	});
+
+	const snapshot = JSON.parse(await readFile(join(import.meta.dirname, 'schema.json'), { encoding: 'utf8' }));
+
+	await Promise.all(
+		ports.map(async (port) => {
+			const api = createDirectus<Schema>(`http://localhost:${port}`).with(rest()).with(staticToken('admin'));
+			const diff = await api.request(schemaDiff(snapshot, true));
+			await api.request(schemaApply(diff));
+		}),
+	);
 }
 
 export async function teardown(_project: TestProject) {
--- tests/e2e/setup/setup-one.ts
+++ tests/e2e/setup/setup-one.ts
@@ -8,20 +8,20 @@ import { readFile } from 'fs/promises';
 let sb: StopSandbox | undefined;
 
 export async function setup(project: TestProject) {
+	if (process.env['ALL'] === 'true') return;
+
 	const dev = Boolean(project.config.env['DEV']);
 	const watch = Boolean(project.config.env['WATCH']);
 	const database = project.config.env['DATABASE'] as Database;
 	const port = project.config.env['PORT'];
 
-	if (process.env['ALL'] !== 'true') {
-		sb = await sandbox(database, {
-			port,
-			dev: true,
-			watch: true,
-			prefix: database,
-			dockerBasePort: String(Number(project.config.env['PORT']) + 1),
-		});
-	}
+	sb = await sandbox(database, {
+		port,
+		dev: true,
+		watch: true,
+		prefix: database,
+		dockerBasePort: String(Number(project.config.env['PORT']) + 1),
+	});
 
 	const api = createDirectus<Schema>(`http://localhost:${port}`).with(rest()).with(staticToken('admin'));
 
