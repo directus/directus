9cdd4826af329d41b37c1f394fcfd48f6e574340
Brainslug <tim@brainslug.nl>
2025-08-20 17:45:15 +0200

    prettier

--- api/src/mailer.test.ts
+++ api/src/mailer.test.ts
@@ -14,72 +14,72 @@ const mockGetConfigFromEnv = vi.fn();
 vi.mocked(await import('./utils/get-config-from-env.js')).getConfigFromEnv = mockGetConfigFromEnv;
 
 describe('getMailer', () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-    // Reset the module to clear any cached transporter
-    vi.resetModules();
-  });
+	beforeEach(() => {
+		vi.clearAllMocks();
+		// Reset the module to clear any cached transporter
+		vi.resetModules();
+	});
 
-  afterEach(() => {
-    vi.clearAllMocks();
-  });
+	afterEach(() => {
+		vi.clearAllMocks();
+	});
 
-  test('should not throw when creating SES transport', () => {
-    mockUseEnv.mockReturnValue({
-      EMAIL_TRANSPORT: 'ses'
-    });
-    
-    mockGetConfigFromEnv.mockReturnValue({
-        region: 'us-east-1',
-        credentials: {
-            accessKeyId: 'access',
-            secretAccessKey: 'secret',
-        }
-    });
+	test('should not throw when creating SES transport', () => {
+		mockUseEnv.mockReturnValue({
+			EMAIL_TRANSPORT: 'ses',
+		});
 
-    expect(() => getMailer()).not.toThrow();
-  });
+		mockGetConfigFromEnv.mockReturnValue({
+			region: 'us-east-1',
+			credentials: {
+				accessKeyId: 'access',
+				secretAccessKey: 'secret',
+			},
+		});
 
-  test('should not throw when creating sendmail transport', () => {
-    mockUseEnv.mockReturnValue({
-      EMAIL_TRANSPORT: 'sendmail'
-    });
+		expect(() => getMailer()).not.toThrow();
+	});
 
-    mockGetConfigFromEnv.mockReturnValue({
-        newLine: 'unix',
-        path: '/usr/sbin/sendmail',
-    });
+	test('should not throw when creating sendmail transport', () => {
+		mockUseEnv.mockReturnValue({
+			EMAIL_TRANSPORT: 'sendmail',
+		});
 
-    expect(() => getMailer()).not.toThrow();
-  });
+		mockGetConfigFromEnv.mockReturnValue({
+			newLine: 'unix',
+			path: '/usr/sbin/sendmail',
+		});
 
-  test('should not throw when creating SMTP transport', () => {
-    mockUseEnv.mockReturnValue({
-      EMAIL_TRANSPORT: 'smtp'
-    });
-    
-    mockGetConfigFromEnv.mockReturnValue({
-        host: '0.0.0.0',
-        port: '123',
-        user: 'me',
-        password: 'safe',
-        name: 'test',
-    });
+		expect(() => getMailer()).not.toThrow();
+	});
 
-    expect(() => getMailer()).not.toThrow();
-  });
+	test('should not throw when creating SMTP transport', () => {
+		mockUseEnv.mockReturnValue({
+			EMAIL_TRANSPORT: 'smtp',
+		});
 
-  test('should not throw when creating Mailgun transport', () => {
-    mockUseEnv.mockReturnValue({
-      EMAIL_TRANSPORT: 'mailgun'
-    });
+		mockGetConfigFromEnv.mockReturnValue({
+			host: '0.0.0.0',
+			port: '123',
+			user: 'me',
+			password: 'safe',
+			name: 'test',
+		});
 
-    mockGetConfigFromEnv.mockReturnValue({
-        apiKey: 'test',
-        domain: 'test',
-        host: 'api.mailgun.net',
-    });
+		expect(() => getMailer()).not.toThrow();
+	});
 
-    expect(() => getMailer()).not.toThrow();
-  });
+	test('should not throw when creating Mailgun transport', () => {
+		mockUseEnv.mockReturnValue({
+			EMAIL_TRANSPORT: 'mailgun',
+		});
+
+		mockGetConfigFromEnv.mockReturnValue({
+			apiKey: 'test',
+			domain: 'test',
+			host: 'api.mailgun.net',
+		});
+
+		expect(() => getMailer()).not.toThrow();
+	});
 });
