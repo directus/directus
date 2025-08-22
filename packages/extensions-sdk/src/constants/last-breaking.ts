621eff43611d0e63ef1b09b5b571abd84aef70fb
ian <licitdev@gmail.com>
2025-08-22 23:37:39 +0800

    Redo random changes

--- packages/extensions-sdk/src/constants/last-breaking.ts
+++ packages/extensions-sdk/src/constants/last-breaking.ts
@@ -2,4 +2,4 @@
  * Last release with known breaking changes to the Extensions SDK and it's output.
  * This will be used as the default value for the `host` manifest field in created extensions
  */
-export const LAST_BREAKING_RELEASE = '10.10.0';
+export const LAST_BREAKING_RELEASE = '11.10.0';
