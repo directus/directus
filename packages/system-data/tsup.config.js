2ee16414831e598e33212c883bd4afc07483b7f0
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2025-08-25 11:27:31 -0400

    revert tsup to tsdown changes

--- packages/system-data/tsup.config.js
+++ packages/system-data/tsup.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'tsdown';
-import yaml from 'unplugin-yaml/rolldown';
+import { defineConfig } from 'tsup';
+import { YAMLPlugin } from 'esbuild-yaml';
 
 const env = process.env.NODE_ENV;
 
 export default defineConfig({
-	plugins: [yaml()],
-	entry: 'src/index.ts',
+	esbuildPlugins: [YAMLPlugin()],
+	entry: ['src/index.ts'],
 	format: ['cjs', 'esm'],
 	target: 'es2020',
 	minify: env === 'production',
 	watch: env === 'development',
+	clean: true,
 	dts: true,
 });
