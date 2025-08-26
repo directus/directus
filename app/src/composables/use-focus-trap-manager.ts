8346b6f0cde6039adb542c7e9e9f0cbe2a2db582
Florian C. Wachmann <dev@formfcw.com>
2025-08-26 20:58:11 +0200

    Fix focus trap in TinyMCE dialogs (#25746)
    * fix focus trap issue in TinyMCE
    
    * add changeset
    
    ---------
    
    Co-authored-by: Alex Gaillard <alex@directus.io>

---
+++ app/src/composables/use-focus-trap-manager.ts
@@ -0,0 +1,31 @@
+import type { UseFocusTrapReturn as FocusTrap } from '@vueuse/integrations/useFocusTrap';
+import { ref, provide, inject } from 'vue';
+
+const nestedValidationSymbol = 'focusTrapManager';
+
+export function useFocusTrapManager() {
+	const trap = ref<FocusTrap>();
+
+	provide(nestedValidationSymbol, { pauseFocusTrap, unpauseFocusTrap });
+
+	return { addFocusTrap };
+
+	function addFocusTrap(focusTrap: FocusTrap) {
+		trap.value = focusTrap;
+	}
+
+	function pauseFocusTrap() {
+		trap.value?.pause();
+	}
+
+	function unpauseFocusTrap() {
+		trap.value?.unpause();
+	}
+}
+
+export function useInjectFocusTrapManager() {
+	return inject(nestedValidationSymbol, {
+		pauseFocusTrap: () => {},
+		unpauseFocusTrap: () => {},
+	});
+}
