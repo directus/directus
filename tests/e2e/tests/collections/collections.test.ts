f61f2c8500863e7064f8090421167d897036f104
Nitwel <mail@nitwel.de>
2025-09-02 12:42:00 +0200

    test collection crud

--- tests/e2e/tests/collections/collections.test.ts
+++ tests/e2e/tests/collections/collections.test.ts
@@ -1,20 +1,83 @@
-import { createDirectus, staticToken, rest, createCollection } from '@directus/sdk';
+import {
+	createDirectus,
+	staticToken,
+	rest,
+	createCollection,
+	readCollection,
+	updateCollection,
+	deleteCollection,
+} from '@directus/sdk';
 
-import { test } from 'vitest';
-import { Schema } from '../fields/relational/schema';
+import { expect, test } from 'vitest';
+import { randomUUID } from 'node:crypto';
 
-const api = createDirectus<Schema>(`http://localhost:${process.env['PORT']}`).with(rest()).with(staticToken('admin'));
+const api = createDirectus(`http://localhost:${process.env['PORT']}`).with(rest()).with(staticToken('admin'));
+
+const collectionName = `collections_${randomUUID()}`;
 
 test('creating a collection', async () => {
-	await api.request(
+	const create = await api.request(
 		createCollection({
-			collection: 'test',
+			collection: collectionName,
 			fields: [
 				{
 					field: 'id',
 					type: 'integer',
+					meta: { hidden: true, interface: 'input', readonly: true },
+					schema: { is_primary_key: true, has_auto_increment: true },
 				},
 			],
+			schema: {},
+			meta: { singleton: false },
 		}),
 	);
+
+	expect(create).toBeDefined();
+
+	const read = await api.request(readCollection(collectionName));
+
+	expect(read).toMatchObject({
+		collection: collectionName,
+		meta: {
+			accountability: 'all',
+			archive_app_filter: true,
+			archive_field: null,
+			archive_value: null,
+			collapse: 'open',
+			collection: collectionName,
+			color: null,
+			display_template: null,
+			group: null,
+			hidden: false,
+			icon: null,
+			item_duplication_fields: null,
+			note: null,
+			preview_url: null,
+			singleton: false,
+			sort: null,
+			sort_field: null,
+			translations: null,
+			unarchive_value: null,
+			versioning: false,
+		},
+		schema: {
+			comment: null,
+			name: collectionName,
+			schema: 'public',
+		},
+	});
+
+	const update = await api.request(
+		updateCollection(collectionName, {
+			meta: {
+				note: 'Updated',
+			},
+		}),
+	);
+
+	expect(update).toBeDefined();
+
+	const del = await api.request(deleteCollection(collectionName));
+
+	expect(del).toBeNull();
 });
