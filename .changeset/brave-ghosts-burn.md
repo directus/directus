b53619b250719da0428147311e784ca786da14d2
zjpiazza <zjpiazza@gmail.com>
2025-09-08 13:59:41 -0500

    Fix failing checks
    * Add changeset
    * Format
    * Lint

---
+++ .changeset/brave-ghosts-burn.md
@@ -0,0 +1,9 @@
+---
+'@directus/extensions': minor
+'@directus/constants': minor
+'@directus/types': minor
+'@directus/api': minor
+'@directus/app': minor
+---
+
+Add "editor" extension type support
--- app/src/views/private/components/editor-sidebar-detail.vue
+++ app/src/views/private/components/editor-sidebar-detail.vue
@@ -29,10 +29,7 @@ const selectedEditor = useExtension('editor', editor);
 const currentEditor = computed(() => selectedEditor.value);
 
 // Add a "Default" option to allow clearing the custom editor
-const editorOptions = computed(() => [
-	{ id: null, name: t('default'), icon: 'edit' },
-	...editors.value
-]);
+const editorOptions = computed(() => [{ id: null, name: t('default'), icon: 'edit' }, ...editors.value]);
 </script>
 
 <template>
@@ -40,13 +37,7 @@ const editorOptions = computed(() => [
 		<div class="editor-options">
 			<div class="field">
 				<div class="type-label">{{ t('editor') }}</div>
-				<v-select 
-					v-model="editor" 
-					:items="editorOptions" 
-					item-text="name" 
-					item-value="id" 
-					item-icon="icon"
-				>
+				<v-select v-model="editor" :items="editorOptions" item-text="name" item-value="id" item-icon="icon">
 					<template v-if="currentEditor?.icon" #prepend>
 						<v-icon :name="currentEditor.icon" />
 					</template>
@@ -64,11 +55,11 @@ const editorOptions = computed(() => [
 	--v-input-font-family: var(--theme--font-family-monospace);
 
 	.field {
-		margin-bottom: 12px;
+		margin-block-end: 12px;
 	}
 
 	.type-label {
-		margin-bottom: 4px;
+		margin-block-end: 4px;
 	}
 }
 </style>
