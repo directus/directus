7ffc23ed429d755b99d18ff48aa5aeefd9d02fe9
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2025-09-12 14:19:18 -0400

    prefix all health tests with `directus-`

--- api/src/services/server.ts
+++ api/src/services/server.ts
@@ -284,8 +284,8 @@ export class ServerService {
 			const startTime = performance.now();
 
 			try {
-				await cache!.set(`health-${checkID}`, true, 5);
-				await cache!.delete(`health-${checkID}`);
+				await cache!.set(`directus-health-${checkID}`, true, 5);
+				await cache!.delete(`directus-health-${checkID}`);
 			} catch (err: any) {
 				checks['cache:responseTime']![0]!.status = 'error';
 				checks['cache:responseTime']![0]!.output = err;
@@ -324,8 +324,8 @@ export class ServerService {
 			const startTime = performance.now();
 
 			try {
-				await rateLimiter.consume(`health-${checkID}`, 1);
-				await rateLimiter.delete(`health-${checkID}`);
+				await rateLimiter.consume(`directus-health-${checkID}`, 1);
+				await rateLimiter.delete(`directus-health-${checkID}`);
 			} catch (err: any) {
 				checks['rateLimiter:responseTime']![0]!.status = 'error';
 				checks['rateLimiter:responseTime']![0]!.output = err;
@@ -366,8 +366,8 @@ export class ServerService {
 			const startTime = performance.now();
 
 			try {
-				await rateLimiterGlobal.consume(`health-${checkID}`, 1);
-				await rateLimiterGlobal.delete(`health-${checkID}`);
+				await rateLimiterGlobal.consume(`directus-health-${checkID}`, 1);
+				await rateLimiterGlobal.delete(`directus-health-${checkID}`);
 			} catch (err: any) {
 				checks['rateLimiterGlobal:responseTime']![0]!.status = 'error';
 				checks['rateLimiterGlobal:responseTime']![0]!.output = err;
@@ -409,7 +409,7 @@ export class ServerService {
 				const startTime = performance.now();
 
 				try {
-					await disk.write('health-file', Readable.from([checkID]));
+					await disk.write('directus-health-file', Readable.from([checkID]));
 				} catch (err: any) {
 					checks[`storage:${location}:responseTime`]![0]!.status = 'error';
 					checks[`storage:${location}:responseTime`]![0]!.output = err;
