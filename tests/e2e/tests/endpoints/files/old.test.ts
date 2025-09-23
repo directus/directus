bcb81de931a04bc592b00735628d99689197f854
Nitwel <mail@nitwel.de>
2025-09-23 16:24:29 +0200

    add old files tests

---
+++ tests/e2e/tests/endpoints/files/old.test.ts
@@ -0,0 +1,65 @@
+import { describe, expect, test } from 'vitest';
+
+describe('/files', () => {
+	describe('POST /', () => {
+		describe('when only request body is provided without any multipart files', () => {
+			test('returns created file when required properties are included', async () => {
+				// Setup
+				const payload = {
+					title: 'Test File',
+					storage: 'local',
+					filename_download: 'test_file',
+					type: 'application/octet-stream',
+				};
+
+				const response = await fetch(`http://localhost:${process.env['PORT']}/files`, {
+					method: 'POST',
+					body: JSON.stringify(payload),
+					headers: {
+						['Content-Type']: 'application/json',
+						Authorization: 'Bearer admin',
+					},
+				});
+
+				// Assert
+				expect(response.status).toBe(200);
+
+				expect(await response.json()).toMatchObject({
+					data: {
+						title: payload.title,
+						storage: payload.storage,
+						filename_download: payload.filename_download,
+					},
+				});
+			});
+
+			test('returns code: FAILED_VALIDATION when required property "storage" is not included', async () => {
+				// Setup
+				const payload = { title: 'Test File', filename_download: 'test_file', type: 'application/octet-stream' };
+
+				const response = await fetch(`http://localhost:${process.env['PORT']}/files`, {
+					method: 'POST',
+					body: JSON.stringify(payload),
+					headers: {
+						['Content-Type']: 'application/json',
+						Authorization: 'Bearer admin',
+					},
+				});
+
+				// Assert
+				expect(response.status).toBe(400);
+
+				expect(await response.json()).toMatchObject({
+					errors: [
+						{
+							message: 'Validation failed for field "storage". Value is required.',
+							extensions: {
+								code: 'FAILED_VALIDATION',
+							},
+						},
+					],
+				});
+			});
+		});
+	});
+});
