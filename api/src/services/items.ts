2f66b676ce7283ba014630a98866e54a737e7438
Nitwel <mail@nitwel.de>
2025-10-06 15:12:05 +0200

    simplify id fetching

--- api/src/services/items.ts
+++ api/src/services/items.ts
@@ -1183,7 +1183,7 @@ export class ItemsService<Item extends AnyItem = AnyItem, Collection extends str
 
 		if (query.version) {
 			const primaryKeyField = this.schema.collections[this.collection]!.primary;
-			const key = (await this.readByQuery({ limit: 1, fields: [primaryKeyField] }))[0]?.[primaryKeyField];
+			const key = (await this.knex.select(primaryKeyField).from(this.collection).limit(1).first())[primaryKeyField];
 			record = await handleVersion(this, key, query, opts);
 		} else {
 			record = (await this.readByQuery(query, opts))[0];
