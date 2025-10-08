f8112bd54deaad424f657726d32168ae256f307c
Brainslug <tim@brainslug.nl>
2025-10-08 17:25:15 +0200

    fixed extended query type and made query parsing edge cases more explicit

--- sdk/src/rest/utils/query-to-params.ts
+++ sdk/src/rest/utils/query-to-params.ts
@@ -1,9 +1,9 @@
 import type { AggregationTypes, GroupByFields, Query } from '../../types/index.js';
 
 type ExtendedQuery<Schema, Item> = Query<Schema, Item> & {
-	aggregate?: Record<keyof AggregationTypes, string>;
+	aggregate?: Partial<Record<keyof AggregationTypes, string>>;
 	groupBy?: (string | GroupByFields<Schema, Item>)[];
-};
+} & Record<string, unknown>;
 
 export const formatFields = (fields: (string | Record<string, any>)[]) => {
 	type FieldItem = (typeof fields)[number];
@@ -68,54 +68,93 @@ export const queryToParams = <Schema = any, Item = Record<string, unknown>>(
 ): Record<string, string> => {
 	const params: Record<string, string> = {};
 
-	if (Array.isArray(query.fields) && query.fields.length > 0) {
-		params['fields'] = formatFields(query.fields).join(',');
+	if (query.fields) {
+		if (Array.isArray(query.fields) && query.fields.length > 0) {
+			params['fields'] = formatFields(query.fields).join(',');
+		}
+
+		// backwards JS compatibility for `fields: "id,name"`
+		if (typeof query.fields === 'string') {
+			params['fields'] = query.fields;
+		}
 	}
 
-	if (query.filter && Object.keys(query.filter).length > 0) {
+	if (query.filter && typeof query.filter === 'object' && Object.keys(query.filter).length > 0) {
 		params['filter'] = JSON.stringify(query.filter);
 	}
 
-	if (query.search) {
-		// covers both empty string and undefined
+	if (query.search && typeof query.search === 'string' && query.search.length > 0) {
 		params['search'] = query.search;
 	}
 
-	if ('sort' in query && query.sort) {
-		// covers empty array and undefined
-		params['sort'] = typeof query.sort === 'string' ? query.sort : query.sort.join(',');
+	if (query.sort) {
+		if (Array.isArray(query.sort) && query.sort.length > 0) {
+			params['sort'] = query.sort.join(',');
+		}
+
+		if (typeof query.sort === 'string' && query.sort.length > 0) {
+			params['sort'] = query.sort;
+		}
 	}
 
-	if (typeof query.limit === 'number' && query.limit >= -1) {
-		params['limit'] = String(query.limit);
+	if ('limit' in query) {
+		if (typeof query.limit === 'number' && query.limit >= -1) {
+			params['limit'] = String(query.limit);
+		}
+
+		// backwards JS compatibility for string limits
+		if (typeof query.limit === 'string' && Number(query.limit) >= -1) {
+			params['limit'] = query.limit;
+		}
 	}
 
-	if (typeof query.offset === 'number' && query.offset >= 0) {
-		params['offset'] = String(query.offset);
+	if ('offset' in query) {
+		if (typeof query.offset === 'number' && query.offset >= 0) {
+			params['offset'] = String(query.offset);
+		}
+
+		// backwards JS compatibility for string offsets
+		if (typeof query.offset === 'string' && Number(query.offset) >= 0) {
+			params['offset'] = query.offset;
+		}
 	}
 
-	if (typeof query.page === 'number' && query.page >= 1) {
-		params['page'] = String(query.page);
+	if ('page' in query) {
+		if (typeof query.page === 'number' && query.page >= 1) {
+			params['page'] = String(query.page);
+		}
+
+		// backwards JS compatibility for string pages
+		if (typeof query.page === 'string' && Number(query.page) >= 1) {
+			params['page'] = query.page;
+		}
 	}
 
-	if (query.deep && Object.keys(query.deep).length > 0) {
+	if (query.deep && typeof query.deep === 'object' && Object.keys(query.deep).length > 0) {
 		params['deep'] = JSON.stringify(query.deep);
 	}
 
-	if (query.alias && Object.keys(query.alias).length > 0) {
+	if (query.alias && typeof query.alias === 'object' && Object.keys(query.alias).length > 0) {
 		params['alias'] = JSON.stringify(query.alias);
 	}
 
-	if (query.aggregate && Object.keys(query.aggregate).length > 0) {
+	if (query.aggregate && typeof query.aggregate === 'object' && Object.keys(query.aggregate).length > 0) {
 		params['aggregate'] = JSON.stringify(query.aggregate);
 	}
 
-	if (query.groupBy && query.groupBy.length > 0) {
-		params['groupBy'] = query.groupBy.join(',');
+	if (query.groupBy) {
+		if (Array.isArray(query.groupBy) && query.groupBy.length > 0) {
+			params['groupBy'] = query.groupBy.join(',');
+		}
+
+		// backwards JS compatibility for string pages
+		if (typeof query.groupBy === 'string' && String(query.groupBy).length > 0) {
+			params['groupBy'] = query.groupBy;
+		}
 	}
 
 	for (const [key, value] of Object.entries(query)) {
-		if (key in knownQueryKeys) continue;
+		if (key in params || knownQueryKeys.includes(key)) continue;
 
 		if (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean') {
 			params[key] = String(value);
