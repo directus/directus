71cb9563c34085f7e0d46ea48c471de7c18ed235
ian <licitdev@gmail.com>
2025-10-11 00:11:19 +0800

    Fix formatting

--- api/src/services/mail/rate-limiter.ts
+++ api/src/services/mail/rate-limiter.ts
@@ -1,21 +1,21 @@
-import { useEnv } from "@directus/env";
-import { RateLimiterQueue } from "rate-limiter-flexible";
-import { createRateLimiter } from "../../rate-limiter.js";
+import { useEnv } from '@directus/env';
+import { RateLimiterQueue } from 'rate-limiter-flexible';
+import { createRateLimiter } from '../../rate-limiter.js';
 
 let emailQueue: RateLimiterQueue | undefined;
 
 const env = useEnv();
 
 if (Boolean(env['EMAIL_LIMITER_ENABLED']) === true) {
-    const rateLimiter = createRateLimiter('EMAIL_LIMITER');
+	const rateLimiter = createRateLimiter('EMAIL_LIMITER');
 
-    emailQueue = new RateLimiterQueue(rateLimiter, {
-        maxQueueSize: Number(env['EMAIL_LIMITER_QUEUE_SIZE']),
-    });
+	emailQueue = new RateLimiterQueue(rateLimiter, {
+		maxQueueSize: Number(env['EMAIL_LIMITER_QUEUE_SIZE']),
+	});
 }
 
 export async function emailRateLimiter() {
-    if (emailQueue) {
-        await emailQueue.removeTokens(1);
-    }
+	if (emailQueue) {
+		await emailQueue.removeTokens(1);
+	}
 }
