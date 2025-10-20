3912d48adf94c45a697a7816942710d5cd2e2e59
Brainslug <tim@brainslug.nl>
2025-10-20 21:28:25 +0200

    initial mail rate limiter tests

---
+++ api/src/services/mail/rate-limiter.test.ts
@@ -0,0 +1,127 @@
+import { describe, test, expect, vi, beforeEach } from 'vitest';
+import { useEnv } from '@directus/env';
+
+vi.mock('@directus/env', () => ({
+	useEnv: vi.fn().mockReturnValue({}),
+}));
+
+// importing it here once so the import time does not skew the timing of the tests
+// commenting this line out will add about 700ms to the first test of the file
+await import('./rate-limiter.js');
+
+describe('Email Rate Limiter', () => {
+	beforeEach(async () => {
+		vi.clearAllMocks();
+		vi.resetModules();
+	});
+
+	describe('useEmailRateLimiterQueue', () => {
+		test('should resolve to undefined when rate limiter is not enabled', async () => {
+			vi.mocked(useEnv).mockReturnValue({
+				EMAIL_LIMITER_ENABLED: 'false',
+			});
+
+			// dynamic import because useEnv is accessed in the file root
+			const { useEmailRateLimiterQueue } = await import('./rate-limiter.js');
+			
+			await expect(useEmailRateLimiterQueue()).resolves.toBeUndefined();
+		});
+		
+		test('should be able to consume all points without error', async () => {
+			vi.mocked(useEnv).mockReturnValue({
+				EMAIL_LIMITER_ENABLED: 'true',
+				EMAIL_LIMITER_POINTS: 3,
+				EMAIL_LIMITER_DURATION: 10,
+				EMAIL_LIMITER_QUEUE_SIZE: 0,
+			});
+
+			// dynamic import because useEnv is accessed in the file root
+			const { useEmailRateLimiterQueue } = await import('./rate-limiter.js');
+
+			// consume 3 points
+			await expect(Promise.all([
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+			])).resolves.not.toThrowError();
+		});
+		
+		test('should throw an error after all points have been consumed', async () => {
+			vi.mocked(useEnv).mockReturnValue({
+				EMAIL_LIMITER_ENABLED: 'true',
+				EMAIL_LIMITER_POINTS: 3,
+				EMAIL_LIMITER_DURATION: 10,
+				EMAIL_LIMITER_QUEUE_SIZE: 0,
+			});
+
+			// dynamic import because useEnv is accessed in the file root
+			const { useEmailRateLimiterQueue } = await import('./rate-limiter.js');
+
+			// consume 4 points
+			await expect(Promise.all([
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+			])).rejects.toThrowError(/^Email sending limit exceeded\./);
+		});
+		
+		test('should be able to fill the queue without error', async () => {
+			vi.mocked(useEnv).mockReturnValue({
+				EMAIL_LIMITER_ENABLED: 'true',
+				EMAIL_LIMITER_POINTS: 1,
+				EMAIL_LIMITER_DURATION: 0.02, // 20ms keep this low for test speed
+				EMAIL_LIMITER_QUEUE_SIZE: 2,
+			});
+
+			// dynamic import because useEnv is accessed in the file root
+			const { useEmailRateLimiterQueue } = await import('./rate-limiter.js');
+
+			// consume 1 point and fill the queue
+			await expect(Promise.all([
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+			])).resolves.not.toThrowError();
+		});
+		
+		test('should throw an error after the queue is full', async () => {
+			vi.mocked(useEnv).mockReturnValue({
+				EMAIL_LIMITER_ENABLED: 'true',
+				EMAIL_LIMITER_POINTS: 1,
+				EMAIL_LIMITER_DURATION: 1,
+				EMAIL_LIMITER_QUEUE_SIZE: 2,
+			});
+
+			// dynamic import because useEnv is accessed in the file root
+			const { useEmailRateLimiterQueue } = await import('./rate-limiter.js');
+
+			// consume 1 point and overflow the queue
+			await expect(Promise.all([
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+			])).rejects.toThrowError(/^Email sending limit exceeded\./);
+		});
+		
+		test('should include a custom message in the error', async () => {
+			vi.mocked(useEnv).mockReturnValue({
+				EMAIL_LIMITER_ENABLED: 'true',
+				EMAIL_LIMITER_POINTS: 1,
+				EMAIL_LIMITER_DURATION: 1,
+				EMAIL_LIMITER_QUEUE_SIZE: 0,
+				EMAIL_LIMITER_ERROR_MESSAGE: 'My custom message.'
+			});
+
+			// dynamic import because useEnv is accessed in the file root
+			const { useEmailRateLimiterQueue } = await import('./rate-limiter.js');
+
+			// consume 2 points
+			await expect(Promise.all([
+				useEmailRateLimiterQueue(),
+				useEmailRateLimiterQueue(),
+			])).rejects.toThrowError(/^Email sending limit exceeded\..* My custom message\./);
+		});
+	});
+});
