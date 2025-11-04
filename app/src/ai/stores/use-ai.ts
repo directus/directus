59b743b2301f543c7c7ad5e9e9253ebb1e31c0a5
bryantgillespie <hey@bryantgillespie.com>
2025-11-04 15:26:40 -0500

    fix whitespace

--- app/src/ai/stores/use-ai.ts
+++ app/src/ai/stores/use-ai.ts
@@ -1,6 +1,6 @@
 import { useLocalStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
-import {  computed } from 'vue';
+import { computed } from 'vue';
 import { DefaultChatTransport, type UIMessage } from 'ai';
 import { AI_MODELS } from '../models';
 import { Chat } from '@ai-sdk/vue';
@@ -10,7 +10,6 @@ export const useAiStore = defineStore('ai-store', () => {
 
 	const currentProvider = computed(() => selectedModel.value?.split('/')[0] ?? 'openai');
 	const currentModel = computed(() => selectedModel.value?.split('/')[1] ?? 'gpt-5');
-
 
 	const chat = new Chat<UIMessage>({
 		transport: new DefaultChatTransport({
@@ -33,7 +32,6 @@ export const useAiStore = defineStore('ai-store', () => {
 	function addMessage(message: string) {
 		chat.sendMessage({ text: message });
 	}
-
 
 	return {
 		currentProvider,
