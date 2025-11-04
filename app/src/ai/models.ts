c62905fcddac0122bfcf367c719d11512d5ac4f0
bryantgillespie <hey@bryantgillespie.com>
2025-11-04 15:00:02 -0500

    update models to long form (again)

--- app/src/ai/models.ts
+++ app/src/ai/models.ts
@@ -1,9 +1,9 @@
 export const AI_MODELS = [
-	'gpt-5-mini',
-	'gpt-5-nano',
-	'gpt-5',
-	'gpt-5-pro',
-	'claude-haiku-4-5',
-	'claude-sonnet-4-5',
-	'claude-opus-4-1',
+	'openai/gpt-5-mini',
+	'openai/gpt-5-nano',
+	'openai/gpt-5',
+	'openai/gpt-5-pro',
+	'anthropic/claude-haiku-4-5',
+	'anthropic/claude-sonnet-4-5',
+	'anthropic/claude-opus-4-1',
 ] as const;
