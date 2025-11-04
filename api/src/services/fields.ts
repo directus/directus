86251d31b27afb715be6667eeb54650f2a4a6e63
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2025-11-04 13:48:49 -0500

    fix oracle querying

--- api/src/services/fields.ts
+++ api/src/services/fields.ts
@@ -844,7 +844,8 @@ export class FieldsService {
 				const permissionRows: { id: number; collection: string; fields: string }[] = await trx
 					.select('id', 'collection', 'fields')
 					.from('directus_permissions')
-					.whereRaw('collection = ? AND fields LIKE ?', [collection, '%' + field + '%']);
+					.where('collection', collection)
+					.andWhereLike('fields', '%' + field + '%');
 
 				if (permissionRows.length > 0) {
 					for (const permissionRow of permissionRows) {
