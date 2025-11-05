1b6fcab3ab7a695a90d0ad914ecabc8fa33a318b
Brainslug <tim@brainslug.nl>
2025-11-05 17:14:30 +0100

    prettier

--- api/src/database/migrations/20251027A-add-provider-access.ts
+++ api/src/database/migrations/20251027A-add-provider-access.ts
@@ -1,35 +1,35 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-		const rowsLimit = 100;
-		let hasMore = true;
-		
-		while (hasMore) {
-			const policies = await knex('directus_permissions')
-				.select('directus_permissions.id', 'directus_permissions.fields')
-				.join('directus_policies', 'directus_policies.id', 'directus_permissions.policy')
-				.where('directus_policies.app_access', '=', 1)
-				.andWhere('directus_permissions.collection', '=', 'directus_users')
-				.andWhere('directus_permissions.action', '=', 'read')
-				.andWhereNot('directus_permissions.fields', 'LIKE', '*')
-				.andWhereNot('directus_permissions.fields', 'LIKE', '%provider%')
-				.limit(rowsLimit);
+	const rowsLimit = 100;
+	let hasMore = true;
 
-			if (policies.length === 0) {
-				hasMore = false;
-				break;
-			}
+	while (hasMore) {
+		const policies = await knex('directus_permissions')
+			.select('directus_permissions.id', 'directus_permissions.fields')
+			.join('directus_policies', 'directus_policies.id', 'directus_permissions.policy')
+			.where('directus_policies.app_access', '=', 1)
+			.andWhere('directus_permissions.collection', '=', 'directus_users')
+			.andWhere('directus_permissions.action', '=', 'read')
+			.andWhereNot('directus_permissions.fields', 'LIKE', '*')
+			.andWhereNot('directus_permissions.fields', 'LIKE', '%provider%')
+			.limit(rowsLimit);
 
-			for (const policy of policies) {
-				if (policy.id && typeof policy.fields === 'string') {
-					await knex('directus_permissions')
-						.update({
-							fields: policy.fields + ',provider',
-						})
-						.where('id', '=', policy.id);
-				}
+		if (policies.length === 0) {
+			hasMore = false;
+			break;
+		}
+
+		for (const policy of policies) {
+			if (policy.id && typeof policy.fields === 'string') {
+				await knex('directus_permissions')
+					.update({
+						fields: policy.fields + ',provider',
+					})
+					.where('id', '=', policy.id);
 			}
 		}
+	}
 }
 
 export async function down(_knex: Knex): Promise<void> {
