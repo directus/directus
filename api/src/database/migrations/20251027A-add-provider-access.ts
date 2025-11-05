86629548bf61d89a7d58c6c58f36505834ca834b
Brainslug <tim@brainslug.nl>
2025-11-05 16:08:05 +0100

    more precise query

--- api/src/database/migrations/20251027A-add-provider-access.ts
+++ api/src/database/migrations/20251027A-add-provider-access.ts
@@ -1,23 +1,24 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-	const policies = await knex('directus_permissions')
-		.select('directus_permissions.id', 'directus_permissions.fields')
-		.join('directus_policies', 'directus_policies.id', 'directus_permissions.policy')
-		.where('directus_policies.app_access', '=', 1)
-		.andWhere('directus_permissions.collection', '=', 'directus_users')
-		.andWhere('directus_permissions.action', '=', 'read')
-		.andWhere('directus_permissions.fields', '!=', '*');
-	
-	// there are definitely better ways of doing this
-	for (const policy of policies) {
-		if (typeof policy.fields === 'string' && policy.fields.length > 0 && !policy.fields.includes('provider')) {
-			await knex('directus_permissions').update({
-				fields: policy.fields + ',provider'
-			}).where('id', '=', policy.id);
-		}
-	}
+		const policies = await knex('directus_permissions')
+			.select('directus_permissions.id', 'directus_permissions.fields')
+			.join('directus_policies', 'directus_policies.id', 'directus_permissions.policy')
+			.where('directus_policies.app_access', '=', 1)
+			.andWhere('directus_permissions.collection', '=', 'directus_users')
+			.andWhere('directus_permissions.action', '=', 'read')
+			.andWhereNot('directus_permissions.fields', 'LIKE', '%provider%')
+			.andWhereNot('directus_permissions.fields', '=', '*');
 
+		for (const policy of policies) {
+			if (typeof policy.fields === 'string') {
+				await knex('directus_permissions')
+					.update({
+						fields: policy.fields + ',provider',
+					})
+					.where('id', '=', policy.id);
+			}
+		}
 }
 
 export async function down(_knex: Knex): Promise<void> {
