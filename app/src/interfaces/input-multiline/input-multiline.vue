f9d56646d11cd89611706af911e7db392412d7c0
Rob Luton <rob.luton@gmail.com>
2025-11-06 11:24:02 -0600

    add non-editable prop and class to input-multiline interface

--- app/src/interfaces/input-multiline/input-multiline.vue
+++ app/src/interfaces/input-multiline/input-multiline.vue
@@ -6,6 +6,7 @@ const props = withDefaults(
 		value: string | null;
 		clear?: boolean;
 		disabled?: boolean;
+		nonEditable?: boolean;
 		placeholder?: string;
 		trim?: boolean;
 		font?: 'sans-serif' | 'serif' | 'monospace';
@@ -47,7 +48,7 @@ const percentageRemaining = computed(() => {
 		:model-value="value"
 		:nullable="!clear"
 		:disabled="disabled"
-		:class="font"
+		:class="[font, { 'non-editable': nonEditable }]"
 		:dir="direction"
 		@update:model-value="$emit('input', $event)"
 	>
