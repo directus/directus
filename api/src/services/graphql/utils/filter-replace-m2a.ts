46b144aabe06ef87df21eba51907fa3112701939
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2025-11-07 15:34:08 -0500

    account for deep alias

--- api/src/services/graphql/utils/filter-replace-m2a.ts
+++ api/src/services/graphql/utils/filter-replace-m2a.ts
@@ -1,4 +1,4 @@
-import type { Filter, NestedDeepQuery, Query, SchemaOverview } from '@directus/types';
+import type { DeepQuery, Filter, NestedDeepQuery, Query, SchemaOverview } from '@directus/types';
 import { getRelation } from '@directus/utils';
 import { getRelationType } from '../../../utils/get-relation-type.js';
 
@@ -23,21 +23,21 @@ export function filterReplaceM2A(
 		const type = relation ? getRelationType({ relation, collection, field }) : null;
 
 		if (type === 'o2m' && relation) {
-			filter[key] = filterReplaceM2A(filter[key], relation.collection, schema);
+			filter[key] = filterReplaceM2A(filter[key], relation.collection, schema, options);
 		} else if (type === 'm2o' && relation) {
-			filter[key] = filterReplaceM2A(filter[key], relation.related_collection!, schema);
+			filter[key] = filterReplaceM2A(filter[key], relation.related_collection!, schema, options);
 		} else if (
 			type === 'a2o' &&
 			relation &&
 			any_collection &&
 			relation.meta?.one_allowed_collections?.includes(any_collection)
 		) {
-			filter[`${field}:${any_collection}`] = filterReplaceM2A(filter[key], any_collection, schema);
+			filter[`${field}:${any_collection}`] = filterReplaceM2A(filter[key], any_collection, schema, options);
 			delete filter[key];
 		} else if (Array.isArray(filter[key])) {
-			filter[key] = filter[key].map((item) => filterReplaceM2A(item, collection, schema));
+			filter[key] = filter[key].map((item) => filterReplaceM2A(item, collection, schema, options));
 		} else if (typeof filter[key] === 'object') {
-			filter[key] = filterReplaceM2A(filter[key], collection, schema);
+			filter[key] = filterReplaceM2A(filter[key], collection, schema, options);
 		}
 	}
 
@@ -60,7 +60,7 @@ export function filterReplaceM2ADeep(
 
 			if (!field) continue;
 
-			field = options?.aliasMap?.[field] ?? field;
+			field = options?.aliasMap?.[field] || (deep as DeepQuery)._alias?.[field] || field;
 
 			const relation = getRelation(schema.relations, collection, field);
 
@@ -69,17 +69,17 @@ export function filterReplaceM2ADeep(
 			const type = getRelationType({ relation, collection, field });
 
 			if (type === 'o2m') {
-				deep[key] = filterReplaceM2ADeep(deep[key], relation.collection, schema, options);
+				deep[key] = filterReplaceM2ADeep(deep[key], relation.collection, schema);
 			} else if (type === 'm2o') {
-				deep[key] = filterReplaceM2ADeep(deep[key], relation.related_collection!, schema, options);
+				deep[key] = filterReplaceM2ADeep(deep[key], relation.related_collection!, schema);
 			} else if (type === 'a2o' && any_collection && relation.meta?.one_allowed_collections?.includes(any_collection)) {
-				deep[`${field}:${any_collection}`] = filterReplaceM2ADeep(deep[key], any_collection, schema, options);
+				deep[`${field}:${any_collection}`] = filterReplaceM2ADeep(deep[key], any_collection, schema);
 				delete deep[key];
 			}
 		}
 
 		if (key === '_filter') {
-			deep[key] = filterReplaceM2A(deep[key], collection, schema, options);
+			deep[key] = filterReplaceM2A(deep[key], collection, schema);
 		}
 	}
 
