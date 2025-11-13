c2414b0cc39936fa7dec5e098ac8cec5b155e244
Brainslug <tim@brainslug.nl>
2025-11-13 20:09:24 +0100

    move status utils into the sync folder

---
+++ api/src/extensions/lib/sync/status.ts
@@ -0,0 +1,34 @@
+/**
+ * Utility functions to write a `.status` file on the filesystem to indicate active synchronization 
+ */
+import { join } from 'node:path';
+import { exists } from 'fs-extra';
+import { writeFile, rm } from 'node:fs/promises';
+import { getExtensionsPath } from '../get-extensions-path.js';
+
+export const SyncStatus = {
+	SYNCING: 'SYNCING',
+	IDLE: 'IDLE',
+} as const;
+
+export type SyncStatus = keyof typeof SyncStatus;
+
+export async function getSyncStatus(): Promise<SyncStatus> {
+    const statusFilePath = join(getExtensionsPath(), '.status');
+
+    if (await exists(statusFilePath)) {
+        return SyncStatus.SYNCING;
+    }
+
+    return SyncStatus.IDLE;
+}
+
+export async function setSyncStatus(status: SyncStatus) {
+    const statusFilePath = join(getExtensionsPath(), '.status');
+
+    if (status === SyncStatus.SYNCING) {
+        await writeFile(statusFilePath, '');
+    } else {
+        await rm(statusFilePath);
+    }
+}
