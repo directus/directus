a84fa28b28ec75eaeea6fdcaf34432c27d9f02c2
Rijk van Zanten <rijkvanzanten@me.com>
2025-11-18 18:17:58 -0500

    Run formatter

--- app/src/ai/components/ai-magic-button.vue
+++ app/src/ai/components/ai-magic-button.vue
@@ -6,23 +6,33 @@ const { animate = false } = defineProps<{ animate?: boolean }>();
 const leftAnimating = ref(false);
 const rightAnimating = ref(false);
 
-watch(() => animate, (newAnimate) => {
-	if (newAnimate) {
-		leftAnimating.value = true;
-		rightAnimating.value = true;
-	}
-});
+watch(
+	() => animate,
+	(newAnimate) => {
+		if (newAnimate) {
+			leftAnimating.value = true;
+			rightAnimating.value = true;
+		}
+	},
+);
 
 const onAnimationIteration = (star: 'left' | 'right') => {
 	if (animate === false) {
 		if (star === 'left') leftAnimating.value = false;
 		if (star === 'right') rightAnimating.value = false;
 	}
-}
+};
 </script>
 
 <template>
-	<svg class="ai-magic-button" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<svg
+		class="ai-magic-button"
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
 		<path class="left" :class="{ animate: leftAnimating }" @animationiteration="onAnimationIteration('left')" />
 		<path class="right" :class="{ animate: rightAnimating }" @animationiteration="onAnimationIteration('right')" />
 	</svg>
@@ -32,7 +42,8 @@ const onAnimationIteration = (star: 'left' | 'right') => {
 .ai-magic-button {
 	overflow: visible;
 
-	.left, .right {
+	.left,
+	.right {
 		fill: currentColor;
 		transform-origin: center center;
 		animation-duration: 1s;
@@ -47,13 +58,17 @@ const onAnimationIteration = (star: 'left' | 'right') => {
 	}
 
 	.left {
-		d: path("M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z");
+		d: path(
+			'M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z'
+		);
 		translate: -2px -1px;
 		animation-name: sparkle-left;
 	}
 
 	.right {
-		d: path("M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z");
+		d: path(
+			'M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z'
+		);
 		translate: 6px 5px;
 		animation-name: sparkle-right;
 	}
@@ -63,21 +78,27 @@ const onAnimationIteration = (star: 'left' | 'right') => {
 	0% {
 		opacity: 1;
 		translate: -2px -1px;
-		d: path("M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z");
+		d: path(
+			'M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z'
+		);
 		scale: 1;
 	}
 
 	33% {
 		opacity: 0;
 		translate: -2px -8px;
-		scale: .8;
-		d: path("M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z");
+		scale: 0.8;
+		d: path(
+			'M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z'
+		);
 	}
 
 	33.1% {
 		opacity: 0;
 		translate: -2px 8px;
-		d: path("M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z");
+		d: path(
+			'M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z'
+		);
 	}
 
 	66% {
@@ -87,7 +108,9 @@ const onAnimationIteration = (star: 'left' | 'right') => {
 	100% {
 		opacity: 1;
 		translate: -2px -1px;
-		d: path("M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z");
+		d: path(
+			'M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z'
+		);
 	}
 }
 
@@ -95,31 +118,39 @@ const onAnimationIteration = (star: 'left' | 'right') => {
 	0% {
 		opacity: 1;
 		translate: 6px 5px;
-		d: path("M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z");
+		d: path(
+			'M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z'
+		);
 	}
 
 	50% {
 		translate: 6px -1px;
-		d: path("M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z");
+		d: path(
+			'M12 15.175L13 13L15.175 12L13 11L12 8.825L11 11L8.825 12L11 13L12 15.175ZM12 20L9.5 14.5L4 12L9.5 9.5L12 4L14.5 9.5L20 12L14.5 14.5L12 20Z'
+		);
 	}
 
 	75% {
 		opacity: 0;
 		translate: 6px -8px;
-		scale: .8;
+		scale: 0.8;
 	}
 
 	75.1% {
 		opacity: 0;
 		translate: 6px 8px;
-		d: path("M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z");
+		d: path(
+			'M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z'
+		);
 		scale: 0.8;
 	}
 
 	100% {
 		opacity: 1;
 		translate: 6px 5px;
-		d: path("M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z");
+		d: path(
+			'M12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12L12 12ZM12 16L10.75 13.25L8 12L10.75 10.75L12 8L13.25 10.75L16 12L13.25 13.25L12 16Z'
+		);
 		scale: 1;
 	}
 }
