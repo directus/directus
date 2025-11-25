3b8a94a2245837845e209869bc3d03e44a7b33cf
bryantgillespie <hey@bryantgillespie.com>
2025-11-24 21:38:29 -0500

    cleaner compueted

--- app/src/modules/content/routes/item.vue
+++ app/src/modules/content/routes/item.vue
@@ -264,12 +264,12 @@ const livePreviewSizeStorage = useLocalStorage<number>('live-preview-size', 50);
 const breakpoints = useBreakpoints(breakpointsTailwind);
 const isMobile = breakpoints.smaller('sm');
 
-const livePreviewActive = computed(() => {
-	if (!collectionInfo.value?.meta?.preview_url) return false;
-	if (unref(isNew)) return false;
-
-	return livePreviewMode.value === 'split';
-});
+const livePreviewActive = computed(
+	() =>
+		!!collectionInfo.value?.meta?.preview_url &&
+		!unref(isNew) &&
+		livePreviewMode.value === 'split',
+);
 
 const livePreviewCollapsed = computed({
 	get() {
