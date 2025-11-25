04bc137714a875e2e79cd7aa1615e5b3cd4b2411
bryantgillespie <hey@bryantgillespie.com>
2025-11-24 19:55:45 -0500

    use container queries for form

--- app/src/components/v-form/v-form.vue
+++ app/src/components/v-form/v-form.vue
@@ -487,6 +487,7 @@ function getComparisonIndicatorClasses(field: TFormField, isGroup = false) {
 @use '@/styles/mixins';
 
 .v-form {
+	container-type: inline-size;
 	@include mixins.form-grid;
 
 	.first-visible-field :deep(.presentation-divider) {
