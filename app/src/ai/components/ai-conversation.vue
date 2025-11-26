7ff7e1238641cb4ba6f6b48401f0b7a4a1185e82
Rijk van Zanten <rijkvanzanten@me.com>
2025-11-26 18:28:33 -0500

    Nudge nudge

--- app/src/ai/components/ai-conversation.vue
+++ app/src/ai/components/ai-conversation.vue
@@ -138,7 +138,6 @@ function scrollToBottom(behavior: ScrollBehavior = 'smooth') {
 
 .messages-container {
 	position: relative;
-	padding-inline: 8px;
 	flex: 1;
 	overflow-y: auto;
 	min-block-size: 0;
