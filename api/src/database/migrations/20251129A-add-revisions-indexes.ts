16635b76241e1d538a22244d31325b50c890df5a
Hakan ENSEP <ensep@users.noreply.github.com>
2025-11-28 23:27:17 +0300

    Fix formatting with Prettier

--- api/src/database/migrations/20251129A-add-revisions-indexes.ts
+++ api/src/database/migrations/20251129A-add-revisions-indexes.ts
@@ -5,44 +5,44 @@ import { getHelpers } from '../helpers/index.js';
 import { getDatabaseClient } from '../index.js';
 
 const REVISION_INDEXES = [
-    { collection: 'directus_revisions', field: 'item', ignore: [] },
-    { collection: 'directus_revisions', field: 'collection', ignore: [] },
-    { collection: 'directus_revisions', field: 'version', ignore: [] },
-    { collection: 'directus_revisions', field: 'parent', ignore: ["mysql"] },
+	{ collection: 'directus_revisions', field: 'item', ignore: [] },
+	{ collection: 'directus_revisions', field: 'collection', ignore: [] },
+	{ collection: 'directus_revisions', field: 'version', ignore: [] },
+	{ collection: 'directus_revisions', field: 'parent', ignore: ['mysql'] },
 ];
 
 export async function up(knex: Knex): Promise<void> {
-    const client = getDatabaseClient(knex);
-    const helpers = getHelpers(knex);
-    const schema = await getSchema();
-    const service = new FieldsService({ knex, schema });
+	const client = getDatabaseClient(knex);
+	const helpers = getHelpers(knex);
+	const schema = await getSchema();
+	const service = new FieldsService({ knex, schema });
 
-    for (const { collection, field, ignore } of REVISION_INDEXES) {
-        if (ignore.includes(client)) continue;
+	for (const { collection, field, ignore } of REVISION_INDEXES) {
+		if (ignore.includes(client)) continue;
 
-        const existingColumn = await service.columnInfo(collection, field);
+		const existingColumn = await service.columnInfo(collection, field);
 
-        if (!existingColumn.is_indexed) {
-            await helpers.schema.createIndex(collection, field, { attemptConcurrentIndex: true });
-        }
-    }
+		if (!existingColumn.is_indexed) {
+			await helpers.schema.createIndex(collection, field, { attemptConcurrentIndex: true });
+		}
+	}
 }
 
 export async function down(knex: Knex): Promise<void> {
-    const client = getDatabaseClient(knex);
-    const helpers = getHelpers(knex);
-    const schema = await getSchema();
-    const service = new FieldsService({ knex, schema });
+	const client = getDatabaseClient(knex);
+	const helpers = getHelpers(knex);
+	const schema = await getSchema();
+	const service = new FieldsService({ knex, schema });
 
-    for (const { collection, field, ignore } of REVISION_INDEXES) {
-        if (ignore.includes(client)) continue;
+	for (const { collection, field, ignore } of REVISION_INDEXES) {
+		if (ignore.includes(client)) continue;
 
-        const existingColumn = await service.columnInfo(collection, field);
+		const existingColumn = await service.columnInfo(collection, field);
 
-        if (existingColumn.is_indexed) {
-            await knex.schema.alterTable(collection, async (table) => {
-                table.dropIndex([field], helpers.schema.generateIndexName('index', collection, field));
-            });
-        }
-    }
+		if (existingColumn.is_indexed) {
+			await knex.schema.alterTable(collection, async (table) => {
+				table.dropIndex([field], helpers.schema.generateIndexName('index', collection, field));
+			});
+		}
+	}
 }
