c393b4a61e5d62a4b5b85c8f744f2b895ae00e7f
bryantgillespie <hey@bryantgillespie.com>
2025-12-08 18:02:32 -0500

    close inline nav on route change

--- app/src/views/private/private-view/stores/nav-bar.ts
+++ app/src/views/private/private-view/stores/nav-bar.ts
@@ -1,9 +1,22 @@
-import { useLocalStorage, createEventHook } from '@vueuse/core';
+import { useLocalStorage, createEventHook, useBreakpoints } from '@vueuse/core';
 import { defineStore } from 'pinia';
+import { watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { BREAKPOINTS } from '@/constants';
 
 export const useNavBarStore = defineStore('nav-bar-store', () => {
 	const collapsed = useLocalStorage('nav-bar-collapsed', false);
 	const size = useLocalStorage('nav-bar-size', 250);
+
+	const route = useRoute();
+	const breakpoints = useBreakpoints(BREAKPOINTS);
+
+	watch(
+		() => route.fullPath,
+		() => {
+			if (breakpoints.smaller('lg').value) collapse();
+		},
+	);
 
 	const collapseHook = createEventHook();
 	const expandHook = createEventHook();
