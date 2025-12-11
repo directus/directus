da5acc2a884836846231726dc9342ad2ad05d634
Rijk van Zanten <rijkvanzanten@me.com>
2025-12-11 15:49:07 -0500

    Create reusable header bar action button comp

---
+++ app/src/views/private/private-view/components/private-view-header-bar-action-button.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import VButton from '@/components/v-button.vue';
+import VIcon from '@/components/v-icon/v-icon.vue';
+
+defineProps<{
+	icon: string;
+	disabled?: boolean;
+	loading?: boolean;
+	secondary?: boolean;
+	outlined?: boolean;
+	to?: string;
+}>();
+
+defineEmits<{
+	click: [];
+}>();
+</script>
+
+<template>
+	<VButton
+		:disabled
+		:loading
+		:secondary
+		:outlined
+		:to
+		icon
+		rounded
+		small
+		@click="$emit('click')"
+	>
+		<VIcon :name="icon" small />
+	</VButton>
+</template>
