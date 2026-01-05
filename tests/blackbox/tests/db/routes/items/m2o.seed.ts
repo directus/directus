718b6bc9187631b825e724f653030c3547921477
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2026-01-05 15:55:32 -0500

    revert import re-order

--- tests/blackbox/tests/db/routes/items/m2o.seed.ts
+++ tests/blackbox/tests/db/routes/items/m2o.seed.ts
@@ -3,9 +3,9 @@ import vendors from '@common/get-dbs-to-test';
 import { SeedFunctions } from '@common/seed-functions';
 import type { PrimaryKeyType } from '@common/types';
 import { PRIMARY_KEY_TYPES } from '@common/variables';
+import type { CachedTestsSchema, TestsSchema, TestsSchemaVendorValues } from '../../query/filter';
 import { set } from 'lodash-es';
 import { expect, it } from 'vitest';
-import type { CachedTestsSchema, TestsSchema, TestsSchemaVendorValues } from '../../query/filter';
 import { getTestsAllTypesSchema, seedAllFieldTypesStructure, seedAllFieldTypesValues } from './seed-all-field-types';
 import { seedRelationalFields } from './seed-relational-fields';
 
