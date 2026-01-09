c3e20037668546976c07fade7198682918a5269b
Rijk van Zanten <rijkvanzanten@me.com>
2026-01-09 18:33:52 -0500

    Cache /ai/object route

--- api/src/middleware/respond.ts
+++ api/src/middleware/respond.ts
@@ -27,11 +27,35 @@ export const respond: RequestHandler = asyncHandler(async (req, res) => {
 		if (maxSize !== null) exceedsMaxSize = valueSize > maxSize;
 	}
 
+	const isGetRequest = req.method.toLowerCase() === 'get';
+
+	// Routes that should be cached, even though they aren't get requests
+	const cachedPostRoutes = ['/graphql', '/ai/object'];
+	const isCachedNonGetRoute = cachedPostRoutes.some((route) => req.originalUrl?.startsWith(route));
+
+	console.log([
+		(isGetRequest || isCachedNonGetRoute),
+		req.originalUrl?.startsWith('/auth') === false,
+		env['CACHE_ENABLED'] === true,
+		!!cache,
+		!req.sanitizedQuery.export,
+		res.locals['cache'] !== false,
+		exceedsMaxSize === false,
+		(await permissionsCacheable(
+			req.collection,
+			{
+				knex: getDatabase(),
+				schema: req.schema,
+			},
+			req.accountability,
+		))
+	]);
+
 	if (
-		(req.method.toLowerCase() === 'get' || req.originalUrl?.startsWith('/graphql')) &&
+		(isGetRequest || isCachedNonGetRoute) &&
 		req.originalUrl?.startsWith('/auth') === false &&
 		env['CACHE_ENABLED'] === true &&
-		cache &&
+		!!cache &&
 		!req.sanitizedQuery.export &&
 		res.locals['cache'] !== false &&
 		exceedsMaxSize === false &&
