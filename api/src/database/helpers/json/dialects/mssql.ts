0047b19096903d150311fe622733cb9f39a97502
Brainslug <tim@brainslug.nl>
2026-01-15 22:00:18 +0100

    added mssql support detection

--- api/src/database/helpers/json/dialects/mssql.ts
+++ api/src/database/helpers/json/dialects/mssql.ts
@@ -1,5 +1,14 @@
-import { JsonHelper } from "../types.js";
+import { JsonHelper } from '../types.js';
 
 export class JsonHelperMSSQL extends JsonHelper {
-
+	override async supported() {
+		// JSON functions were introduced in SQL Server 2016 (version 13.0)
+		// Check if JSON_VALUE function is supported
+		try {
+			await this.knex.raw(`SELECT JSON_VALUE('{"name":"test"}', '$.name') as result`);
+			return true;
+		} catch {
+			return false;
+		}
+	}
 }
