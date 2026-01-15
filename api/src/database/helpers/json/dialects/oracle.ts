fcd5c8de0e06ece1691d2144d6956fdeac071031
Brainslug <tim@brainslug.nl>
2026-01-15 22:20:51 +0100

    added oracle support detection

--- api/src/database/helpers/json/dialects/oracle.ts
+++ api/src/database/helpers/json/dialects/oracle.ts
@@ -1,5 +1,14 @@
-import { JsonHelper } from "../types.js";
+import { JsonHelper } from '../types.js';
 
 export class JsonHelperOracle extends JsonHelper {
-
+	override async supported() {
+		// JSON functions were introduced in Oracle Database 12c Release 1 (12.1)
+		// Check if JSON_VALUE function is supported
+		try {
+			await this.knex.raw(`SELECT JSON_VALUE('{"name":"test"}', '$.name') FROM DUAL`);
+			return true;
+		} catch {
+			return false;
+		}
+	}
 }
