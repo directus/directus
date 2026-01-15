a580ceb76b2cb5ff9eeb675f7132b11a900b99aa
Brainslug <tim@brainslug.nl>
2026-01-15 21:31:21 +0100

    added cockroachdb support detection

--- api/src/database/helpers/json/dialects/crdb.ts
+++ api/src/database/helpers/json/dialects/crdb.ts
@@ -1,5 +1,14 @@
-import { JsonHelper } from "../types.js";
+import { JsonHelper } from '../types.js';
 
 export class JsonHelperCockroachdb extends JsonHelper {
+	override async supported() {
+		// Check if the "jsonb_extract_path" function is supported
+		// But CockroachDB has supported JSONB natively since v1.0 so we may just be able to "return true" here
+		const res = await this.knex
+			.select('function')
+			.from('crdb_internal.builtin_functions')
+			.where({ function: 'jsonb_extract_path' });
 
+		return res.length > 0;
+	}
 }
