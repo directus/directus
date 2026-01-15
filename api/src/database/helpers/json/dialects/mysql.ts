0411b9b135db2f81bce313bf4a9a2e10ddc7b393
Brainslug <tim@brainslug.nl>
2026-01-15 22:09:23 +0100

    added mysql support detection

--- api/src/database/helpers/json/dialects/mysql.ts
+++ api/src/database/helpers/json/dialects/mysql.ts
@@ -1,5 +1,14 @@
 import { JsonHelper } from "../types.js";
 
 export class JsonHelperMySQL extends JsonHelper {
-
+	override async supported() {
+		// JSON functions were introduced in MySQL 5.7.8 and MariaDB 10.2.7, we support MySQL 8+ and MariaDB 10.2+
+		// Check if JSON_EXTRACT function is supported
+		try {
+			await this.knex.raw(`SELECT JSON_EXTRACT('{"name":"test"}', '$.name') as result`);
+			return true;
+		} catch {
+			return false;
+		}
+	}
 }
