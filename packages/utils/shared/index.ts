136fc32be534d165fe0bf04fbdb93dc22d906ea3
ian <licitdev@gmail.com>
2026-01-16 22:17:10 +0800

    Cleanup removed fn

--- packages/utils/shared/index.ts
+++ packages/utils/shared/index.ts
@@ -39,5 +39,4 @@ export * from './to-array.js';
 export * from './to-boolean.js';
 export * from './validate-payload.js';
 export * from './deep-map-filter.js';
-export * from './generate-filter-validator.js';
 export * from './get-relation-info.js';
