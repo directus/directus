a63a868f42d6fcfe79b6930fe4c5311fbc3db460
formfcw <dev@formfcw.com>
2026-02-17 17:01:41 +0100

    feat: add version parameter support to getSettingsUrls function

--- app/src/modules/visual/utils/get-settings-urls.ts
+++ app/src/modules/visual/utils/get-settings-urls.ts
@@ -1,8 +1,14 @@
 import { normalizeUrl } from './normalize-url';
 import { useSettingsStore } from '@/stores/settings';
+import { renderPlainStringTemplate } from '@/utils/render-string-template';
 
-export function getSettingsUrls() {
+export function getSettingsUrls(version = 'main') {
 	const { settings } = useSettingsStore();
-	const settingsUrls = settings?.visual_editor_urls?.map((item) => item.url).filter(Boolean) || [];
+
+	const settingsUrls =
+		settings?.visual_editor_urls
+			?.map((item) => renderPlainStringTemplate(item.url, { $version: version }) ?? item.url)
+			.filter(Boolean) || [];
+
 	return settingsUrls.map(normalizeUrl).filter(Boolean);
 }
