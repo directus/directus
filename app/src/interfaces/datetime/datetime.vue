9bcbc3e8421672919759510ea4748ad3fafd5c57
alvarosabu <alvaro.saburido@gmail.com>
2026-02-23 15:54:15 +0100

    Use useTemplateRef deactivate instead

--- app/src/interfaces/datetime/datetime.vue
+++ app/src/interfaces/datetime/datetime.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { isValid } from 'date-fns';
-import { computed, ref } from 'vue';
+import { computed, ref, useTemplateRef } from 'vue';
 import UseDatetime, { type Props as UseDatetimeProps } from '@/components/use-datetime.vue';
 import VDatePicker from '@/components/v-date-picker/v-date-picker.vue';
 import VIcon from '@/components/v-icon/v-icon.vue';
@@ -28,6 +28,7 @@ const emit = defineEmits<{
 	(e: 'input', value: string | null): void;
 }>();
 
+const dateTimeMenu = useTemplateRef('dateTimeMenu');
 const menuActive = ref(false);
 
 const isValidValue = computed(() => (props.value ? isValid(parseDate(props.value, props.type)) : false));
@@ -39,7 +40,7 @@ function unsetValue(e: any) {
 }
 
 function closeDatePicker() {
-	menuActive.value = false;
+	dateTimeMenu.value?.deactivate();
 }
 </script>
 
