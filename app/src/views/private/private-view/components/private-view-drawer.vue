b2604f5d714d96df83d6428299fa3f7d1e2e123e
HZooly <torzuoli.hugo@gmail.com>
2026-03-02 18:07:41 +0100

    reintroduce eager prop to VDialog

--- app/src/views/private/private-view/components/private-view-drawer.vue
+++ app/src/views/private/private-view/components/private-view-drawer.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
-import TransitionDialog from '@/components/transition/dialog.vue';
-import VOverlay from '@/components/v-overlay.vue';
+import VDialog from '@/components/v-dialog.vue';
 
 const collapsed = defineModel<boolean>('collapsed');
 
@@ -8,38 +7,17 @@ defineProps<{ placement: 'left' | 'right' }>();
 </script>
 
 <template>
-	<Teleport to="#dialog-outlet">
-		<TransitionDialog>
-			<div v-show="!collapsed" class="container" :class="placement">
-				<VOverlay active absolute @click="collapsed = true" />
-				<div class="panel">
-					<slot />
-				</div>
-			</div>
-		</TransitionDialog>
-	</Teleport>
+	<VDialog :model-value="!collapsed" :placement eager @update:model-value="collapsed = !$event">
+		<div class="panel">
+			<slot />
+		</div>
+	</VDialog>
 </template>
 
 <style scoped>
-.container {
-	position: fixed;
-	inset: 0;
-	z-index: 500;
-	display: flex;
-	--v-overlay-z-index: 1;
-}
-
-.container.right {
-	justify-content: flex-end;
-}
-
 .panel {
-	display: flex;
 	block-size: 100%;
 	inline-size: 280px;
 	max-inline-size: 100%;
-	overflow: hidden;
-	z-index: 2;
-	box-shadow: 0 4px 12px rgb(38 50 56 / 0.1);
 }
 </style>
