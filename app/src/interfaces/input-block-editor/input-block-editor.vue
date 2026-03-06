798e2605890c511f3071c52d13bab059eb084fbb
formfcw <dev@formfcw.com>
2026-03-06 13:34:55 +0100

    format

--- app/src/interfaces/input-block-editor/input-block-editor.vue
+++ app/src/interfaces/input-block-editor/input-block-editor.vue
@@ -223,7 +223,8 @@ const menuActive = computed(() => fileHandler.value !== null);
 
 .input-block-editor .editor {
 	border-radius: var(--theme--border-radius);
-	padding: var(--theme--form--field--input--padding) max(1.8125rem, calc(var(--theme--form--field--input--padding) + 0.875rem));
+	padding: var(--theme--form--field--input--padding)
+		max(1.8125rem, calc(var(--theme--form--field--input--padding) + 0.875rem));
 }
 
 .disabled {
