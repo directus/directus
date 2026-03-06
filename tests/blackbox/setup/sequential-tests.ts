8cfe57c7766528f9ab1006f5b3391234c3fc1cf3
Nitwel <mail@nitwel.de>
2026-03-06 15:56:07 +0100

    ups?

--- tests/blackbox/setup/sequential-tests.ts
+++ tests/blackbox/setup/sequential-tests.ts
@@ -31,7 +31,10 @@ export const sequentialTestsList: Record<'db' | 'common', SequentialTestsList> =
 			'/tests/db/routes/collections/schema-cache.test.ts',
 		],
 		// If specified, only run these tests sequentially
-		// only: ['/tests/db/seed-database.test.ts', '/common/common.test.ts'],
+		only: [
+			// '/tests/db/seed-database.test.ts',
+			// '/common/common.test.ts'
+		],
 	},
 };
 
