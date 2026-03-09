0a02c3e770447b040432b1fa3bbf09cd0249d1da
Nitwel <mail@nitwel.de>
2026-03-09 13:36:03 +0100

    add tracking of date_created/updated to directus_users

---
+++ api/src/database/migrations/20260309A-user-created-updated-tracking.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_users', (table) => {
+		table.timestamp('date_created').defaultTo(knex.fn.now());
+		table.timestamp('date_updated').defaultTo(knex.fn.now());
+	});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable('directus_deployment_runs', (table) => {
+		table.dropColumn('date_created');
+		table.dropColumn('date_updated');
+	});
+}
