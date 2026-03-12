46bc91c9f1b3f56a8e88a0c30935946221e1e2d1
bryantgillespie <hey@bryantgillespie.com>
2026-03-12 12:00:57 -0400

    add type to regex

--- packages/constants/src/regex.ts
+++ packages/constants/src/regex.ts
@@ -1,2 +1,2 @@
 export const REGEX_BETWEEN_PARENS: RegExp = /\(([^()]+)\)/;
-export const REGEX_DB_SAFE_IDENTIFIER = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
+export const REGEX_DB_SAFE_IDENTIFIER: RegExp = /^[a-zA-Z_][a-zA-Z0-9_]*$/;
