ea9f079a545748f4919ca63cbccc52ae3e53c315
formfcw <dev@formfcw.com>
2026-04-10 21:21:31 +0200

    refactor: wrap prepend slot in a div for improved structure

--- app/src/views/private/private-view/components/private-view-header-bar-actions.vue
+++ app/src/views/private/private-view/components/private-view-header-bar-actions.vue
@@ -1,12 +1,24 @@
 <template>
-	<slot name="prepend" />
+	<div class="prepend">
+		<slot name="prepend" />
+	</div>
+
 	<div class="actions">
 		<slot />
 	</div>
+
 	<slot name="append" />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.prepend {
+	margin-inline-end: 0.625rem;
+
+	&:empty {
+		display: contents;
+	}
+}
+
 .actions {
 	position: relative;
 	display: flex;
