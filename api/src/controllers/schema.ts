7d745c6a25fda03049140a9005625f4ffbbca50b
Nitwel <mail@nitwel.de>
2026-04-16 19:48:18 +0200

    Update api/src/controllers/schema.ts
    Co-authored-by: judda <44623501+ComfortablyCoding@users.noreply.github.com>

--- api/src/controllers/schema.ts
+++ api/src/controllers/schema.ts
@@ -122,7 +122,7 @@ router.post(
 	asyncHandler(async (req, res, next) => {
 		const service = new SchemaService({ accountability: req.accountability });
 		const diff: SnapshotDiffWithHash = res.locals['upload'];
-		await service.apply(diff, { force: 'force' in req.query });
+   	await service.apply(diff, { force: toBoolean(req.query.force) });
 		return next();
 	}),
 	respond,
