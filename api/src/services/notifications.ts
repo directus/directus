e9e7c8c3c812d85ee8fbf3d73f6db7fd272671ae
Anise <cameron.casebolt17@gmail.com>
2026-04-20 14:21:55 -0700

    file contained 7 levels of indentation fixed with early returns,making MailService.send use variables and allowed collapsed soem liens into 1

--- api/src/services/notifications.ts
+++ api/src/services/notifications.ts
@@ -19,61 +19,47 @@ export class NotificationsService extends ItemsService {
 
 	override async createOne(data: Partial<Notification>, opts?: MutationOptions): Promise<PrimaryKey> {
 		const response = await super.createOne(data, opts);
-
 		await this.sendEmail(data);
-
 		return response;
 	}
 
 	async sendEmail(data: Partial<Notification>) {
-		if (data.recipient) {
-			const usersService = new UsersService({ schema: this.schema, knex: this.knex });
+		if (!data.recipient) return;
 
-			const user = await usersService.readOne(data.recipient, {
-				fields: ['id', 'email', 'email_notifications', 'role'],
+		const usersService = new UsersService({ schema: this.schema, knex: this.knex });
+
+		const user = await usersService.readOne(data.recipient, {
+			fields: ['id', 'email', 'email_notifications', 'role'],
+		});
+
+		if (!user['email'] || user['email_notifications'] !== true) return;
+
+		const manageUserAccountUrl = new Url(env['PUBLIC_URL'] as string)
+			.addPath('admin', 'users', user['id'])
+			.toString();
+
+		const html = data.message ? md(data.message) : '';
+
+		const roles = await fetchRolesTree(user['role'], { knex: this.knex });
+
+		const { app: app_access } = await fetchGlobalAccess(
+			{ user: user['id'], roles, ip: null },
+			{ knex: this.knex },
+		);
+
+		const mailService = new MailService({
+			schema: this.schema,
+			knex: this.knex,
+			accountability: this.accountability,
+		});
+
+		const template = { name: 'base', data: app_access ? { url: manageUserAccountUrl, html } : { html } };
+		const defaultTemplateData = await mailService.getDefaultTemplateData();
+
+		mailService
+			.send({ template, to: user['email'], subject: data.subject }, { defaultTemplateData })
+			.catch((error) => {
+				logger.error(error, `Could not send notification via mail`);
 			});
-
-			if (user['email'] && user['email_notifications'] === true) {
-				const manageUserAccountUrl = new Url(env['PUBLIC_URL'] as string)
-					.addPath('admin', 'users', user['id'])
-					.toString();
-
-				const html = data.message ? md(data.message) : '';
-				const roles = await fetchRolesTree(user['role'], { knex: this.knex });
-
-				const { app: app_access } = await fetchGlobalAccess(
-					{
-						user: user['id'],
-						roles,
-						ip: null,
-					},
-					{ knex: this.knex },
-				);
-
-				const mailService = new MailService({
-					schema: this.schema,
-					knex: this.knex,
-					accountability: this.accountability,
-				});
-
-				mailService
-					.send(
-						{
-							template: {
-								name: 'base',
-								data: app_access ? { url: manageUserAccountUrl, html } : { html },
-							},
-							to: user['email'],
-							subject: data.subject,
-						},
-						{
-							defaultTemplateData: await mailService.getDefaultTemplateData(),
-						},
-					)
-					.catch((error) => {
-						logger.error(error, `Could not send notification via mail`);
-					});
-			}
-		}
 	}
 }
