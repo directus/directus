b488e706d0a243644613cf325a26befa97b6ebbe
formfcw <dev@formfcw.com>
2026-04-22 23:08:20 +0200

    remove test website

--- packages/visual-editing/test-website/app/components/ui/popover/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Popover } from './Popover.vue';
-export { default as PopoverContent } from './PopoverContent.vue';
-export { default as PopoverTrigger } from './PopoverTrigger.vue';
