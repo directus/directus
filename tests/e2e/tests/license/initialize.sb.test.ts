e0f88d7b4edd8ed3a6a2f9ddd730c6159222c52a
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2026-05-07 19:46:25 -0400

    tests(mls): add initialization case B

--- tests/e2e/tests/license/initialize.sb.test.ts
+++ tests/e2e/tests/license/initialize.sb.test.ts
@@ -6,7 +6,7 @@ import { database } from '@utils/constants.js';
 import { describe, expect } from 'vitest';
 import { createLicense } from '../../../mock-license-server/dist/index.js';
 
-describe('env LICENSE_KEY (Case D)', async () => {
+describe('new env LICENSE_KEY (Case D)', async () => {
 	const license = createLicense({ meta: { name: 'env-license-key' } });
 	const devMode = process.env['NODE_ENV'] === 'development';
 
@@ -46,13 +46,134 @@ describe('env LICENSE_KEY (Case D)', async () => {
 	// Confirm License Meta
 	expect(info.source).toBe('env');
 	expect(info.status).toBe('active');
-	expect(info.name).toBe('env-license-key');
+	expect(info.name).toBe(license.meta.name);
 
 	// Confirm DB sync
 	const settings = await directus.knex!('directus_settings').select('license_key', 'license_token').first();
 
-	expect(settings?.license_key).toBeTruthy();
-	expect(settings?.license_token).toBeTruthy();
+	expect(settings?.license_key).toBeDefined();
+	expect(settings?.license_token).toBeDefined();
+
+	await directus.stop();
+});
+
+describe('env LICENSE_KEY changed (Case B)', async () => {
+	const port = 56885; //LICENSE-TODO: Make this dynamic
+	const license = createLicense({ key: 'D1HH5-N7XMX-V2P45-H457R-2MRT6', meta: { name: 'env-license-key-og' } });
+	const changedLicense = createLicense({ meta: { name: 'env-license-key-changed' } });
+
+	const originalEncryptedLicenseKey =
+		'1||scrypt||16384||8||1||Sh9S/mToJLytYhIgMuShhg==||/3UW2wcm3mCwEe4I||oIzoT+eGrwhZBcj+fjzsA7MtnDeOiW5AwCqddxg=||Ht7L4tOHrGw7pwsCgyz7mw==';
+
+	const devMode = process.env['NODE_ENV'] === 'development';
+
+	const directus = await sandbox(database, {
+		dev: devMode,
+		port,
+		watch: devMode,
+		prefix: database,
+		env: {
+			CACHE_SCHEMA: 'false',
+			DB_FILENAME: `directus_test_${randomUUID()}.db`,
+			LICENSE_KEY: changedLicense.key,
+			SECRET: '7f96fdc0-cc0f-4359-b0db-d6d40d6b9982',
+		},
+		docker: {
+			keep: devMode,
+		},
+		extras: {
+			license: true,
+		},
+		cache: false,
+		knex: true,
+		hooks: {
+			beforeApi: async ({ env, knex }) => {
+				// create key now that the license server is up
+				await fetch(`http://localhost:${env.LICENSE_PORT}/admin/license`, {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify(license),
+				});
+
+				await fetch(`http://localhost:${env.LICENSE_PORT}/admin/license`, {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify(changedLicense),
+				});
+
+				const settings = await knex!.select('id', 'project_id').from('directus_settings').limit(1).first();
+
+				// simulate activation for original license
+				await fetch(`http://localhost:${env.LICENSE_PORT}/api/licenses/activate`, {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						license_key: license.key,
+						project_id: settings.project_id,
+						public_url: `http://127.0.0.1:${port}`,
+					}),
+				});
+
+				await knex?.('directus_settings')
+					.update({ license_key: originalEncryptedLicenseKey })
+					.where({ id: settings.id });
+			},
+		},
+	});
+
+	const api = createDirectus<any>(`http://localhost:${directus.apis[0].port}`).with(rest()).with(staticToken('admin'));
+
+	const info: LicenseInfoOutput = await api.request(readLicense());
+
+	// Confirm License Meta
+	expect(info.source).toBe('env');
+	expect(info.status).toBe('active');
+	expect(info.name).toBe(changedLicense.meta.name);
+
+	// Confirm DB sync
+	const sync = await directus.knex!('directus_settings').select('license_key', 'license_token').first();
+
+	expect(sync?.license_key).toBeDefined();
+	expect(sync?.license_key).not.toEqual(originalEncryptedLicenseKey);
+	expect(sync?.license_token).toBeDefined();
+
+	await directus.stop();
+});
+
+describe('env LICENSE_KEY unchanged refreshes (Case C)', async () => {
+	const license = createLicense({ meta: { name: 'env-license-key-unchanged' } });
+	const devMode = process.env['NODE_ENV'] === 'development';
+
+	const directus = await sandbox(database, {
+		dev: devMode,
+		watch: devMode,
+		prefix: database,
+		env: {
+			CACHE_SCHEMA: 'false',
+			DB_FILENAME: `directus_test_${randomUUID()}.db`,
+			LICENSE_KEY: license.key,
+		},
+		docker: {
+			keep: devMode,
+		},
+		extras: {
+			license: true,
+		},
+		cache: false,
+		knex: true,
+		hooks: {
+			beforeApi: async ({ env, knex }) => {
+				// create key now that the license server is up
+				await fetch(`http://localhost:${env.LICENSE_PORT}/admin/license`, {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify(license),
+				});
+			},
+		},
+	});
+
+	// TBD
 
 	await directus.stop();
 });
