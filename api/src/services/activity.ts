c49af9cb95099b2d52dba0c9cfc14efd8222d926
Brainslug <tim@brainslug.nl>
2026-05-08 20:28:21 +0200

    fix activity history filter and added basic query cache

--- api/src/services/activity.ts
+++ api/src/services/activity.ts
@@ -3,6 +3,8 @@ import { getHistoryFilterQuery } from '../utils/get-history-filter-query.js';
 import { ItemsService } from './items.js';
 
 export class ActivityService extends ItemsService {
+	private queryCache = new WeakMap<Query, Query>();
+
 	constructor(options: AbstractServiceOptions) {
 		super('directus_activity', options);
 	}
@@ -12,12 +14,26 @@ export class ActivityService extends ItemsService {
 			return super.readByQuery(query, opts);
 		}
 
-		const historyQuery = getHistoryFilterQuery(query, 'revision_historical_timeframe', (sinceDate) => ({
-			timestamp: {
-				_gte: sinceDate.toISOString(),
-			},
-		}));
+		const historyQuery = this.getLimitedHistoryQuery(query);
 
 		return super.readByQuery(historyQuery, opts);
+	}
+
+	getLimitedHistoryQuery(query: Query) {
+		let cachedQuery = this.queryCache.get(query);
+
+		if (!cachedQuery) {
+			cachedQuery = getHistoryFilterQuery(query, 'activity_historical_timeframe', (sinceDate) => ({
+				activity: {
+					timestamp: {
+						_gte: sinceDate.toISOString(),
+					},
+				},
+			}));
+
+			this.queryCache.set(query, cachedQuery);
+		}
+
+		return cachedQuery;
 	}
 }
