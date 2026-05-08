9c447690c5bb541294a4b245bc5411444eedb44f
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2026-05-08 19:54:56 -0400

    utilize entitlement getActiveSeats for resolution

--- api/src/license/entitlements/lib/seats.ts
+++ api/src/license/entitlements/lib/seats.ts
@@ -1,10 +1,130 @@
+import type { LicensePendingResolutionLimitSeats } from '@directus/license';
+import { toBoolean } from '@directus/utils';
 import getDatabase from '../../../database/index.js';
-import { UsersService } from '../../../services/index.js';
+import { AccessService, UsersService } from '../../../services/index.js';
+import { fetchAccessRoles } from '../../../utils/fetch-user-count/fetch-access-roles.js';
 import { fetchUserCount } from '../../../utils/fetch-user-count/fetch-user-count.js';
 import { getSchema } from '../../../utils/get-schema.js';
 
-export async function getActiveSeats() {
-	// license-TODO use simplified "fetchUserCount"
+export async function getActiveSeats(ctx?: {
+	adminId: string;
+}): Promise<LicensePendingResolutionLimitSeats['candidates'] | void> {
+	// license-TODO refactor
+	if (!ctx?.adminId) return;
+
+	const schema = await getSchema();
+
+	const accessService = new AccessService({ schema });
+
+	const accessRows = await accessService.readByQuery({
+		fields: ['role', 'user.id', 'user.status', 'user.role', 'policy.app_access', 'policy.admin_access'],
+	});
+
+	const adminRoles = new Set<string>();
+	const appRoles = new Set<string>();
+	const adminUsers = new Set<string>();
+	const appUsers = new Set<string>();
+
+	for (const accessRow of accessRows) {
+		const isAdmin = toBoolean(accessRow['policy']?.['admin_access']);
+		const isApp = !isAdmin && toBoolean(accessRow['policy']?.['app_access']);
+
+		if (!isAdmin && !isApp) continue;
+
+		if (accessRow['user'] && accessRow['user'].status === 'active') {
+			if (isAdmin) {
+				adminUsers.add(accessRow['user'].id);
+			} else if (adminUsers.has(accessRow['user'].id) === false && adminRoles.has(accessRow['user']?.role) === false) {
+				appUsers.add(accessRow['user'].id);
+			}
+		}
+
+		if (accessRow['role']) {
+			if (isAdmin) {
+				adminRoles.add(accessRow['role']);
+			} else {
+				appRoles.add(accessRow['role']);
+			}
+		}
+	}
+
+	const { adminRoles: allAdminRoles, appRoles: allAppRoles } = await fetchAccessRoles(
+		{
+			adminRoles,
+			appRoles,
+		},
+		{ knex: await getDatabase() },
+	);
+
+	const usersService = new UsersService({ schema });
+
+	const adminCandidates = await usersService.readByQuery({
+		fields: ['id', 'first_name', 'last_name', 'avatar'],
+		filter: {
+			_and: [
+				{
+					id: {
+						_neq: ctx.adminId,
+					},
+				},
+				{
+					_or: [
+						{
+							id: {
+								_in: Array.from(adminUsers),
+							},
+						},
+						{
+							role: {
+								_in: Array.from(allAdminRoles),
+							},
+						},
+					],
+				},
+				{
+					status: {
+						_eq: 'active',
+					},
+				},
+			],
+		},
+	});
+
+	const appCandidates = await usersService.readByQuery({
+		fields: ['id', 'first_name', 'last_name', 'avatar'],
+		filter: {
+			_and: [
+				{
+					id: {
+						_neq: ctx.adminId,
+					},
+				},
+				{
+					_or: [
+						{
+							id: {
+								_in: Array.from(appUsers),
+								_nin: Array.from(adminUsers),
+							},
+						},
+						{
+							role: {
+								_in: Array.from(allAppRoles),
+								_nin: Array.from(allAdminRoles),
+							},
+						},
+					],
+				},
+				{
+					status: {
+						_eq: 'active',
+					},
+				},
+			],
+		},
+	});
+
+	return [...appCandidates, ...adminCandidates.map((admin) => ({ ...admin, admin: true }))] as any;
 }
 
 export async function countActiveSeats() {
