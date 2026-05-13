94241c226c815ed7695c04e175eebde2b8b0ff11
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2026-05-13 16:59:54 -0400

    improve lock handling

--- api/src/middleware/is-locked.ts
+++ api/src/middleware/is-locked.ts
@@ -1,21 +1,21 @@
-import { ServiceUnavailableError } from '@directus/errors';
+import { ResourceRestrictedError } from '@directus/errors';
 import type { NextFunction, Request, Response } from 'express';
+import type { RequestHandler } from 'express-serve-static-core';
 import { getLicenseManager } from '../license/manager.js';
 import asyncHandler from '../utils/async-handler.js';
 
 /**
  * Throws an error if the license is in a locked state
  */
-export const handler = async (_req: Request, _res: Response, next: NextFunction) => {
-	if (await getLicenseManager().isLocked()) {
-		// LICENSE-TODO: Convert to a dedicated PROJECT_LOCKED?
-		throw new ServiceUnavailableError({
-			reason: 'License is in a locked state and must be resolved',
-			service: 'license',
-		});
-	}
+export const handler = (resource: string): RequestHandler =>
+	asyncHandler(async (_req: Request, _res: Response, next: NextFunction) => {
+		if (await getLicenseManager().isLocked()) {
+			throw new ResourceRestrictedError({
+				category: resource,
+			});
+		}
 
-	return next();
-};
+		return next();
+	});
 
-export default asyncHandler(handler);
+export default handler;
