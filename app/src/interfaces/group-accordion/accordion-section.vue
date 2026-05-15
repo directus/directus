2de20519dcf924478da66a8b5ae2df6b6b232be0
formfcw <dev@formfcw.com>
2026-05-15 13:07:21 +0200

    fix positioning of edit dot in accordion group interface

--- app/src/interfaces/group-accordion/accordion-section.vue
+++ app/src/interfaces/group-accordion/accordion-section.vue
@@ -212,8 +212,9 @@ function useComparisonIndicator() {
 
 	.edit-dot {
 		position: absolute;
-		inset-block-start: 0.8125rem;
 		inset-inline-start: -0.375rem;
+		inset-block-start: 50%;
+		transform: translateY(-50%);
 		display: block;
 		inline-size: 0.25rem;
 		block-size: 0.25rem;
