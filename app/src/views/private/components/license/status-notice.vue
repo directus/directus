2a014bac390af5b5fe4c743c744b5e91c54dab1a
Rob Luton <rob.luton@gmail.com>
2026-05-27 11:55:41 -0500

    update status notice message

--- app/src/views/private/components/license/status-notice.vue
+++ app/src/views/private/components/license/status-notice.vue
@@ -2,16 +2,21 @@
 import { DIRECTUS_DOMAIN } from '@directus/constants';
 import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
-import { useI18n } from 'vue-i18n';
 import { I18nT } from 'vue-i18n';
 import VNotice from '@/components/v-notice.vue';
 import { GRACE_DANGER_THRESHOLD_DAYS, useLicenseStore } from '@/stores/license';
 import { useServerStore } from '@/stores/server';
 import { useUserStore } from '@/stores/user';
 
-const { t } = useI18n();
 const serverStore = useServerStore();
-const { gracePeriodDaysRemaining, isLocked, isCoreGrace } = storeToRefs(useLicenseStore());
+const { gracePeriodDaysRemaining, isLocked, isCoreGrace, graceDeadline } = storeToRefs(useLicenseStore());
+
+const formattedCoreGraceDate = computed(() =>
+	graceDeadline.value
+		? new Intl.DateTimeFormat(undefined, { month: 'long', day: 'numeric', year: 'numeric' }).format(graceDeadline.value)
+		: '',
+);
+
 const { isAdmin } = storeToRefs(useUserStore());
 
 const show = computed(
@@ -28,6 +33,8 @@ const lockedSupportUrl = computed(
 	() =>
 		`https://${DIRECTUS_DOMAIN}/license-request?utm_source=self_hosted&utm_medium=product&utm_campaign=2026_05_licensing&utm_term=${serverStore.info?.version}&utm_content=status_notice_locked_contact_support_link`,
 );
+
+const coreGraceLicensingUrl = `https://${DIRECTUS_DOMAIN}/docs/licensing/overview`;
 </script>
 
 <template>
@@ -36,7 +43,7 @@ const lockedSupportUrl = computed(
 			<span class="message">
 				<I18nT keypath="license.locked_status_notice" tag="span">
 					<template #contactSupport>
-						<a :href="lockedSupportUrl" target="_blank" rel="noopener noreferrer">{{ t('contact_support') }}</a>
+						<a :href="lockedSupportUrl" target="_blank" rel="noopener noreferrer">{{ $t('contact_support') }}</a>
 					</template>
 				</I18nT>
 			</span>
@@ -45,24 +52,40 @@ const lockedSupportUrl = computed(
 	<VNotice v-else-if="show && isCoreGrace" :type="severity" multiline class="status-notice">
 		<template #title>
 			<span class="message">
-				{{
-					t(
-						'license.unlicensed_status_notice.grace_period',
-						{ days: gracePeriodDaysRemaining },
-						gracePeriodDaysRemaining ?? 0,
-					)
-				}}
+				{{ $t('license.unlicensed_status_notice.grace_period_prefix') }}
+				<strong>
+					{{
+						$t(
+							'license.unlicensed_status_notice.grace_days_left',
+							{ days: gracePeriodDaysRemaining },
+							gracePeriodDaysRemaining ?? 0,
+						)
+					}}
+				</strong>
+				{{ $t('license.unlicensed_status_notice.grace_period_middle') }}
+				<strong>{{ formattedCoreGraceDate }}.</strong>
+				{{ $t('license.unlicensed_status_notice.grace_period_suffix') }}
+				<a :href="coreGraceLicensingUrl" target="_blank" rel="noopener noreferrer">
+					{{ $t('license.unlicensed_status_notice.retrieve_license_key') }}
+				</a>
+				{{ $t('license.unlicensed_status_notice.grace_period_cta_separator') }}
+				<a :href="coreGraceLicensingUrl" target="_blank" rel="noopener noreferrer">
+					{{ $t('license.unlicensed_status_notice.upgrade') }}
+				</a>
+				{{ $t('license.unlicensed_status_notice.grace_period_cta_suffix') }}
 			</span>
 		</template>
 	</VNotice>
 	<VNotice v-else-if="show" :type="severity" multiline class="status-notice">
 		<template #title>
 			<span class="message">
-				{{ t('license.status_notice.grace_period_prefix') }}
+				{{ $t('license.status_notice.grace_period_prefix') }}
 				<strong>
-					{{ t('license.status_notice.grace_days', { days: gracePeriodDaysRemaining }, gracePeriodDaysRemaining ?? 0) }}
+					{{
+						$t('license.status_notice.grace_days', { days: gracePeriodDaysRemaining }, gracePeriodDaysRemaining ?? 0)
+					}}
 				</strong>
-				{{ t('license.status_notice.grace_period_suffix') }}
+				{{ $t('license.status_notice.grace_period_suffix') }}
 			</span>
 		</template>
 	</VNotice>
@@ -76,6 +99,16 @@ const lockedSupportUrl = computed(
 .message {
 	flex: 1;
 	font-weight: normal;
+	color: var(--theme--foreground);
+}
+
+.message a {
+	color: var(--theme--primary);
+	text-decoration: underline;
+	transition: color var(--fast) var(--transition);
+}
+
+.message a:hover {
 	color: var(--theme--foreground);
 }
 </style>
