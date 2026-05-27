4074493e08fea3237ee6214fd69708b43ab8c461
Brainslug <tim@brainslug.nl>
2026-05-27 17:43:15 +0200

    prettier

--- api/src/request/is-denied-ip.ts
+++ api/src/request/is-denied-ip.ts
@@ -19,7 +19,7 @@ export function isDeniedIp(ip: string): boolean {
 			if (blockNetwork === '0.0.0.0') {
 				blockNetworkInterfaces = true;
 				blockList.parseSubnet('0.0.0.0/8');
-    			blockList.parseAddress('::');
+				blockList.parseAddress('::');
 				continue;
 			}
 
