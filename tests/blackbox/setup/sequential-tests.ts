f50403a6d834005696d30d8d269c488c4f3dd748
Nitwel <mail@nitwel.de>
2026-05-28 13:26:34 +0200

    fix bb

--- tests/blackbox/setup/sequential-tests.ts
+++ tests/blackbox/setup/sequential-tests.ts
@@ -16,7 +16,6 @@ export const sequentialTestsList: Record<'db' | 'common', SequentialTestsList> =
 			'/tests/db/routes/collections/crud.test.ts',
 			'/tests/db/routes/fields/change-fields.test.ts',
 			'/tests/db/routes/fields/crud.test.ts',
-			'/tests/db/routes/items/version.test.ts',
 		],
 		after: [
 			'/tests/db/websocket/collab/multi-instance.test.ts',
@@ -31,7 +30,7 @@ export const sequentialTestsList: Record<'db' | 'common', SequentialTestsList> =
 			'/tests/db/routes/collections/schema-cache.test.ts',
 		],
 		// If specified, only run these tests sequentially
-		only: ['/tests/db/seed-database.test.ts', '/common/common.test.ts', '/tests/db/routes/items/version.test.ts'],
+		// only: ['/tests/db/seed-database.test.ts', '/common/common.test.ts'],
 	},
 };
 
