a7c400332402a774cd406ff80275ffb95431dbca
Alvaro Saburido <alvaro.saburido@gmail.com>
2026-06-29 22:42:54 +0200

    Add carets to popover items in toolbar (#27795)

---
+++ app/src/interfaces/input-rich-text-html/toolbar/toolbar-caret.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import VIcon from '@/components/v-icon/v-icon.vue';
+</script>
+
+<template>
+	<VIcon class="toolbar-caret" name="expand_more" small />
+</template>
+
+<style lang="scss" scoped>
+// Shared dropdown-caret glyph; consumers position it (margin/flex) via their own class on the root.
+.toolbar-caret {
+	--v-icon-size: 1rem;
+}
+</style>
