2e996efd67bc4938d100101688c733e07b2eed50
K Heiner <open-source-activity-for+directus@heiner.work>
2026-07-18 12:46:49 -0700

    fix(sdk): correct drift on SDKSchema for directus_users
    - sorted to match @directus/types declaration

--- sdk/src/schema/user.ts
+++ sdk/src/schema/user.ts
@@ -11,32 +11,32 @@ export type DirectusUser<Schema = any> = MergeCoreCollection<
 	'directus_users',
 	{
 		id: string; // uuid
+		status: 'draft' | 'invited' | 'unverified' | 'active' | 'suspended' | 'archived' | 'inactive-license';
 		first_name: string | null;
 		last_name: string | null;
 		email: string | null;
 		password: string | null; // will just be *s
-		location: string | null;
-		title: string | null;
-		description: string | null;
-		tags: string[] | null;
-		avatar: DirectusFile<Schema> | string | null;
-		language: string | null;
-		theme: string | null;
-		tfa_secret: string | null;
-		status: string;
-		role: DirectusRole<Schema> | string | null;
 		token: string | null;
 		last_access: 'datetime' | null;
 		last_page: string | null;
-		provider: string;
 		external_identifier: string | null;
+		tfa_secret: string | null;
 		auth_data: Record<string, any> | null;
-		email_notifications: boolean | null;
-		appearance: string | null;
-		theme_dark: string | null;
+		provider: string;
+		appearance: 'auto' | 'dark' | 'light' | null;
 		theme_light: string | null;
+		theme_dark: string | null;
 		theme_light_overrides: Record<string, unknown> | null;
 		theme_dark_overrides: Record<string, unknown> | null;
+		role: DirectusRole<Schema> | string | null;
 		policies: string[] | DirectusAccess<Schema>[];
+		language: string | null;
+		text_direction: 'ltr' | 'rtl' | 'auto';
+		avatar: DirectusFile<Schema> | string | null;
+		title: string | null;
+		description: string | null;
+		location: string | null;
+		tags: string[] | null;
+		email_notifications: boolean | null;
 	}
 >;
