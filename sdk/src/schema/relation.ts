3846a2d984f59b58a3781cbdb416e6273dcbba75
K Heiner <open-source-activity-for+directus@heiner.work>
2026-07-22 20:09:29 -0700

    fix(sdk): correct directus_relations field types

--- sdk/src/schema/relation.ts
+++ sdk/src/schema/relation.ts
@@ -1,34 +1,34 @@
-import type { MergeCoreCollection } from '../index.js';
+import type { AllCollections, MergeCoreCollection } from '../index.js';
 
 export type DirectusRelation<Schema = any> = {
-	collection: string; // TODO keyof complete schema
+	collection: AllCollections<Schema>;
 	field: string;
-	related_collection: string; // TODO keyof complete schema
+	related_collection: AllCollections<Schema> | null;
 	meta: MergeCoreCollection<
 		Schema,
 		'directus_relations',
 		{
-			id: number;
+			id?: number;
 			junction_field: string | null;
-			many_collection: string | null; // TODO keyof complete schema
-			many_field: string | null;
+			many_collection: AllCollections<Schema>;
+			many_field: string;
 			one_allowed_collections: string | null;
-			one_collection: string | null; // TODO keyof complete schema
+			one_collection: AllCollections<Schema> | null;
 			one_collection_field: string | null;
 			one_deselect_action: string;
 			one_field: string | null;
 			sort_field: string | null;
 			system: boolean | null;
 		}
-	>;
+	> | null;
 	schema: {
 		column: string;
-		constraint_name: string;
+		constraint_name: string | null;
 		foreign_key_column: string;
 		foreign_key_schema: string;
 		foreign_key_table: string;
-		on_delete: string;
-		on_update: string;
+		on_delete: string | null;
+		on_update: string | null;
 		table: string;
-	};
+	} | null;
 };
