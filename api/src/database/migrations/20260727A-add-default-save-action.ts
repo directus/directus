6dc03e1fabd35a7ba1af9442329383eba20a6545
Rob Luton <rob.luton@gmail.com>
2026-07-28 14:43:03 -0600

    set default for save action setting

--- api/src/database/migrations/20260727A-add-default-save-action.ts
+++ api/src/database/migrations/20260727A-add-default-save-action.ts
@@ -2,7 +2,7 @@ import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.alterTable('directus_settings', (table) => {
-		table.string('default_save_action').nullable();
+		table.string('default_save_action').notNullable().defaultTo('save-and-quit');
 	});
 }
 
