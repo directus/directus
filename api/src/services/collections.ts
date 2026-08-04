39438cd37bd5a84be9147147770d9b80a0437030
Lazizbek Ergashev <lazerg2@gmail.com>
2026-08-04 22:33:04 +0500

    Trim collection name once instead of twice

--- api/src/services/collections.ts
+++ api/src/services/collections.ts
@@ -69,11 +69,13 @@ export class CollectionsService {
 
 		if (!('collection' in payload)) throw new InvalidPayloadError({ reason: `"collection" is required` });
 
-		if (typeof payload.collection !== 'string' || payload.collection.trim() === '') {
-			throw new InvalidPayloadError({ reason: `"collection" must be a non-empty string` });
+		if (typeof payload.collection === 'string') {
+			payload.collection = payload.collection.trim();
 		}
 
-		payload.collection = payload.collection.trim();
+		if (typeof payload.collection !== 'string' || payload.collection === '') {
+			throw new InvalidPayloadError({ reason: `"collection" must be a non-empty string` });
+		}
 
 		if (payload.collection.startsWith('directus_')) {
 			throw new InvalidPayloadError({ reason: `Collections can't start with "directus_"` });
