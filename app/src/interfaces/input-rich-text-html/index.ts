191a2df0c204d71af48f4619b8f2a9321557c563
alvarosabu <alvaro.saburido@gmail.com>
2026-08-04 18:09:38 +0200

    Add block-level custom formats to WYSIWYG editor

--- app/src/interfaces/input-rich-text-html/index.ts
+++ app/src/interfaces/input-rich-text-html/index.ts
@@ -298,6 +298,15 @@ export default defineInterface({
 									styles: { color: '#00ff00', 'font-size': '20px' },
 									attributes: { title: 'My Custom Wrapper' },
 								},
+								{
+									title: 'My Custom Block',
+									block: 'p',
+									classes: 'dropcap',
+								},
+								{
+									title: 'My Custom Group',
+									items: [{ title: 'Eyebrow', selector: 'h2', classes: 'eyebrow' }],
+								},
 							],
 							null,
 							4,
