ba179767c2b6b15b8c24a4e08b9e97a12df6acab
K Heiner <open-source-activity-for+directus@heiner.work>
2026-08-07 17:16:46 -0700

    fix(sdk): update extensionSchema with entries

--- sdk/src/schema/extension.ts
+++ sdk/src/schema/extension.ts
@@ -3,7 +3,7 @@ import type { MergeCoreCollection } from '../index.js';
 export type DirectusExtension<Schema = any> = {
 	id: string;
 	bundle: string | null;
-	schema: ExtensionSchema | null;
+	schema: ExtensionSchema | ExtensionSchemaEntry | null;
 	meta: MergeCoreCollection<
 		Schema,
 		'directus_extensions',
@@ -11,10 +11,21 @@ export type DirectusExtension<Schema = any> = {
 	>;
 };
 
-export type ExtensionSchema = {
-	type: ExtensionTypes;
+export type ExtensionSchema = Partial<{
+	path: string;
+	name: string;
 	local: boolean;
-	version?: string;
+	version: string;
+	host: string;
+	type: ExtensionTypes;
+	entrypoint: string | { app: string; api: string };
+	partial: boolean;
+	entries: ExtensionSchemaEntry[];
+}>;
+
+export type ExtensionSchemaEntry = {
+	name: string;
+	type: ExtensionTypes;
 };
 
 export type ExtensionTypes =
