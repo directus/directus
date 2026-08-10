2aa51c2c4f75f0f1779210fe2400fe48d05af8c6
K Heiner <open-source-activity-for+directus@heiner.work>
2026-08-10 09:06:45 -0700

    fix(sdk): permissions.policy is nullable

--- sdk/src/schema/permission.ts
+++ sdk/src/schema/permission.ts
@@ -6,7 +6,7 @@ export type DirectusPermission<Schema = any> = MergeCoreCollection<
 	'directus_permissions',
 	{
 		id: number;
-		policy: DirectusPolicy<Schema> | string;
+		policy: DirectusPolicy<Schema> | string | null;
 		collection: keyof CompleteSchema<Schema>;
 		action: string;
 		permissions: Record<string, any> | null;
