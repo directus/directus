f1ac25fa146df4469f6fd30709734284bb3b6fc8
K Heiner <open-source-activity-for+directus@heiner.work>
2026-09-08 11:42:32 -0700

    feat(system-data): add HARDCODED_AUTH_REQUIREMENTS

--- packages/system-data/src/index.ts
+++ packages/system-data/src/index.ts
@@ -2,4 +2,5 @@ export * from './collections/index.js';
 export * from './relations/index.js';
 export * from './fields/index.js';
 export * from './app-access-permissions/index.js';
+export * from './hardcoded-auth-requirements.js';
 export * from './types.js';
