db6b38325c9757960eb31b9d50a999f971d7b515
Brainslug <tim@brainslug.nl>
2026-09-09 16:45:55 +0200

    fixed incorrect comment

--- api/src/utils/schema/get-collection-from-schema.ts
+++ api/src/utils/schema/get-collection-from-schema.ts
@@ -5,8 +5,6 @@ type OptionalString = string | null | undefined;
 
 /**
  * Returns a collection from the schema and errors if it does not exist
- *
- * Do not use this util in the CollectionService
  */
 export function getCollectionFromSchema(
 	schema: SchemaOverview,
