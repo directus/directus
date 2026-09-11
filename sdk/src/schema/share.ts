3ff092eb0a118a57cbee93cfeef826eaaa467110
K Heiner <open-source-activity-for+directus@heiner.work>
2026-09-11 09:04:44 -0700

    fix(sdk): allow expanded DirectusCollection for share.collection

--- sdk/src/schema/share.ts
+++ sdk/src/schema/share.ts
@@ -1,4 +1,5 @@
 import type { CollectionName, MergeCoreCollection } from '../index.js';
+import type { DirectusCollection } from './collection.js';
 import type { DirectusRole } from './role.js';
 import type { DirectusUser } from './user.js';
 
@@ -8,7 +9,7 @@ export type DirectusShare<Schema = any> = MergeCoreCollection<
 	{
 		id: string;
 		name: string | null;
-		collection: CollectionName<Schema>;
+		collection: DirectusCollection<Schema> | CollectionName<Schema>;
 		item: string;
 		role: DirectusRole<Schema> | string | null;
 		password: string | null; // will just be *s
