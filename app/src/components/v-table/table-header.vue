ecb2572c2c3b0ea889e0a40f09e55c1e9cb02df2
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2026-09-14 10:12:14 -0400

    Improve Flows list column sizing and ordering (#28225)

--- app/src/components/v-table/table-header.vue
+++ app/src/components/v-table/table-header.vue
@@ -61,6 +61,8 @@ useEventListener(window, 'pointerup', onMouseUp);
 
 const headersWritable = useSync(props, 'headers', emit);
 
+const hasFlexColumn = computed(() => props.headers.some((header) => header.flex));
+
 function getClassesForHeader(header: Header) {
 	const classes: string[] = [];
 
@@ -217,7 +219,12 @@ function toggleManualSort() {
 			</template>
 
 			<template #item="{ element: header }">
-				<th :class="getClassesForHeader(header)" class="cell" scope="col" :style="{ inlineSize: header.width + 'px' }">
+				<th
+					:class="getClassesForHeader(header)"
+					class="cell"
+					scope="col"
+					:style="{ inlineSize: header.flex ? '100%' : header.width + 'px' }"
+				>
 					<VMenu v-if="hasHeaderContextMenuSlot" show-arrow placement="bottom-start">
 						<template #activator="{ toggle }">
 							<div class="content reorder-handle">
@@ -265,7 +272,7 @@ function toggleManualSort() {
 					</div>
 
 					<span
-						v-if="showResize"
+						v-if="showResize && !header.flex"
 						class="resize-handle"
 						@click.stop
 						@pointerdown="onResizeHandleMouseDown(header, $event)"
@@ -278,7 +285,13 @@ function toggleManualSort() {
 				<td v-if="$slots['header-append']" class="manual append cell" @click.stop>
 					<slot name="header-append" />
 				</td>
-				<th v-if="hasItemAppendSlot && !$slots['header-append']" class="spacer cell" scope="col" />
+				<!-- The append placeholder mirrors the standard item-append cell (24px icon + padding) -->
+				<th
+					v-if="hasItemAppendSlot && !$slots['header-append']"
+					class="spacer cell"
+					scope="col"
+					:style="hasFlexColumn ? { inlineSize: '2.875rem' } : undefined"
+				/>
 			</template>
 		</Draggable>
 		<!-- </tr> -->
