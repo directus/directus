c7059bcee0a5d8047f102333d41b0bd404ad4972
Nitwel <mail@nitwel.de>
2026-09-16 12:14:47 +0200

    remove ip-matching dependency

--- api/src/services/policies.ts
+++ api/src/services/policies.ts
@@ -1,10 +1,60 @@
+import { isIP } from 'node:net';
 import { InvalidPayloadError } from '@directus/errors';
 import type { AbstractServiceOptions, MutationOptions, Policy, PrimaryKey } from '@directus/types';
 import { UserIntegrityCheckFlag } from '@directus/types';
-import { getMatch } from 'ip-matching';
+import { type IPv4, type IPv6, isValidCIDR, parse as parseIp } from 'ipaddr.js';
 import { clearSystemCache } from '../cache.js';
 import { clearCache as clearPermissionsCache } from '../permissions/cache.js';
 import { ItemsService } from './items.js';
+
+function isIpLessOrEqual(ipA: IPv4 | IPv6, ipB: IPv4 | IPv6) {
+	const bytesA = ipA.toByteArray();
+	const bytesB = ipB.toByteArray();
+
+	for (let i = 0; i < bytesA.length; i++) {
+		if (bytesA[i]! < bytesB[i]!) return true;
+		if (bytesA[i]! > bytesB[i]!) return false;
+	}
+
+	return true;
+}
+
+export function isIpAccessValid(value?: any[] | null): boolean {
+	if (value === undefined) return false;
+	if (value === null) return true;
+	if (Array.isArray(value) && value.length === 0) return true;
+
+	for (let ip of value) {
+		if (typeof ip !== 'string' || ip.includes('*')) return false;
+		ip = ip.trim();
+
+		// Validate IP Range
+		if (ip.includes('-')) {
+			const parts = ip.split('-');
+
+			if (parts.length !== 2) return false;
+			const ipMin = isIP(parts[0]!);
+			const ipMax = isIP(parts[1]!);
+			if (ipMin === 0 || ipMax === 0) return false;
+			if (ipMin !== ipMax) return false;
+			if (!isIpLessOrEqual(parseIp(parts[0]), parseIp(parts[1]))) return false;
+			continue;
+		}
+
+		const parts = ip.split('/');
+
+		// Validate singular IPs
+		if (isIP(parts[0]!) === 0) return false;
+
+		// Validate IP Subnetwork, but reject IP Masks
+		if (parts.length == 2 && !isValidCIDR(ip)) return false;
+
+		// Reject invalid IPs
+		if (parts.length > 2) return false;
+	}
+
+	return true;
+}
 
 export class PoliciesService extends ItemsService<Policy> {
 	constructor(options: AbstractServiceOptions) {
@@ -19,27 +69,8 @@ export class PoliciesService extends ItemsService<Policy> {
 		}
 	}
 
-	private isIpAccessValid(value?: any[] | null): boolean {
-		if (value === undefined) return false;
-		if (value === null) return true;
-		if (Array.isArray(value) && value.length === 0) return true;
-
-		for (const ip of value) {
-			if (typeof ip !== 'string' || ip.includes('*')) return false;
-
-			try {
-				const match = getMatch(ip);
-				if (match.type == 'IPMask') return false;
-			} catch {
-				return false;
-			}
-		}
-
-		return true;
-	}
-
 	private assertValidIpAccess(partialItem: Partial<Policy>): void {
-		if ('ip_access' in partialItem && !this.isIpAccessValid(partialItem['ip_access'])) {
+		if ('ip_access' in partialItem && !isIpAccessValid(partialItem['ip_access'])) {
 			throw new InvalidPayloadError({
 				reason: 'IP Access contains an incorrect value. Valid values are: IP addresses, IP ranges and CIDR blocks',
 			});
