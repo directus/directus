ede84d84d358dcf30417bae14babee6503127733
alvarosabu <alvaro.saburido@gmail.com>
2026-09-17 13:42:57 +0200

    Enhance `use-relation-multiple` by replacing `ref` with `shallowRef` for improved reactivity management.

--- app/src/composables/use-relation-multiple.ts
+++ app/src/composables/use-relation-multiple.ts
@@ -1,7 +1,7 @@
 import { ContentVersion, Filter, Item } from '@directus/types';
 import { getEndpoint, toArray } from '@directus/utils';
 import { clamp, cloneDeep, get, isEqual, merge, mergeWith } from 'lodash-es';
-import { computed, ref, Ref, watch } from 'vue';
+import { computed, ref, Ref, shallowRef, watch } from 'vue';
 import { useRefreshSignal } from '@/composables/use-refresh-signal';
 import { RelationM2A } from '@/composables/use-relation-m2a';
 import { RelationM2M } from '@/composables/use-relation-m2m';
