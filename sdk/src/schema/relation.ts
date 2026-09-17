8f0b9464c6b8adc915d9e0017766e09dbd6cd70f
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2026-09-17 13:09:04 -0400

    don't indicate schema is only directus tables

--- sdk/src/schema/relation.ts
+++ sdk/src/schema/relation.ts
@@ -25,10 +25,10 @@ export type DirectusRelation<Schema = any> = {
 		column: string;
 		constraint_name: string | null;
 		foreign_key_column: string;
-		foreign_key_table: CollectionName<Schema>;
 		foreign_key_schema?: string;
+		foreign_key_table: string;
 		on_delete: 'NO ACTION' | 'RESTRICT' | 'CASCADE' | 'SET NULL' | 'SET DEFAULT' | null;
 		on_update: 'NO ACTION' | 'RESTRICT' | 'CASCADE' | 'SET NULL' | 'SET DEFAULT' | null;
-		table: CollectionName<Schema>;
+		table: string;
 	} | null;
 };
