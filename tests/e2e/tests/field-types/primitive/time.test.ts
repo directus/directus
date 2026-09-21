fe6be478eb8c10728170623242c3aaeeac084eec
K Heiner <open-source-activity-for+directus@heiner.work>
2026-09-21 08:17:55 -0700

    add e2e for `time`

---
+++ tests/e2e/tests/field-types/primitive/time.test.ts
@@ -0,0 +1,30 @@
+import { createDirectus, createItem, rest, staticToken } from '@directus/sdk';
+import { port } from '@utils/constants.js';
+import { useSnapshot } from '@utils/use-snapshot.js';
+import { expect, test } from 'vitest';
+import type { Schema } from './schema.js';
+
+const api = createDirectus<Schema>(`http://localhost:${port}`).with(rest()).with(staticToken('admin'));
+const { collections } = await useSnapshot<Schema>(api);
+
+for (const time of ['10:10:01', '23:59:59']) {
+	test(`valid time ${time}`, async () => {
+		const result = await api.request(
+			createItem(collections.fields, {
+				time,
+			}),
+		);
+
+		expect(result.time).toBe(time);
+	});
+}
+
+test(`invalid time`, async () => {
+	await expect(() =>
+		api.request(
+			createItem(collections.fields, {
+				time: 'test',
+			}),
+		),
+	).rejects.toThrowError();
+});
